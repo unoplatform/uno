955b430f7b777e45b227666daa6226c39551e522
Jérôme Laban <jerome.laban@nventive.com>
2018-08-13 13:38:17 -0400

    Add automation exception classes
    Adds ElementNotAvailableException and ElementNotEnabledException

---
+++ src/Uno.UI/UI/Xaml/Automation/ElementNotEnabledException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Windows.UI.Xaml.Automation
+{
+	public class ElementNotEnabledException : Exception
+	{
+		public ElementNotEnabledException() : base()
+		{
+		}
+
+		public ElementNotEnabledException(string message) : base(message)
+		{
+		}
+
+		public ElementNotEnabledException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
