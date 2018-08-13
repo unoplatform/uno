c3d0181f910a117c5282553fabcfbe84ed4e2a18
Jérôme Laban <jerome.laban@nventive.com>
2018-08-13 14:42:31 -0400

    Add support for Cross-platform library template

---
+++ src/SolutionTemplate/UnoLibraryTemplate/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $safeprojectname$
+{
+	public class Class1
+{
+}
+}
