dba7dd7c159aec05bd15705d89f86468fc4b5dc8
Jérôme Laban <jerome.laban@nventive.com>
2018-08-13 12:38:39 -0400

    Add stubbed PackageId class

---
+++ src/Uno.UWP/ApplicationModel/PackageVersion.cs
@@ -0,0 +1,10 @@
+namespace Windows.ApplicationModel
+{
+	public partial struct PackageVersion
+	{
+		public ushort Major;
+		public ushort Minor;
+		public ushort Build;
+		public ushort Revision;
+	}
+}
--- src/Uno.UWP/Generated/3.0.0.0/Windows.ApplicationModel/PackageVersion.cs
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.ApplicationModel/PackageVersion.cs
@@ -2,23 +2,8 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.ApplicationModel
 {
-	#if __ANDROID__ || __IOS__ || NET46 || __WASM__
-	[global::Uno.NotImplemented]
-	#endif
 	public  partial struct PackageVersion 
 	{
-		// Forced skipping of method Windows.ApplicationModel.PackageVersion.PackageVersion()
-		#if __ANDROID__ || __IOS__ || NET46 || __WASM__
-		public  ushort Major;
-		#endif
-		#if __ANDROID__ || __IOS__ || NET46 || __WASM__
-		public  ushort Minor;
-		#endif
-		#if __ANDROID__ || __IOS__ || NET46 || __WASM__
-		public  ushort Build;
-		#endif
-		#if __ANDROID__ || __IOS__ || NET46 || __WASM__
-		public  ushort Revision;
-		#endif
+
 	}
 }
