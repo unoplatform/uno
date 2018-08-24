922677c3a9353b65952faf9c483d58cc62e93992
Baptiste Guillory <baptiste.guillory@nventive.com>
2018-08-24 12:33:36 -0400

    Apply suggestion

--- src/Uno.UWP/Media/SpeechRecognition/SpeechRecognizerState.cs
+++ src/Uno.UWP/Media/SpeechRecognition/SpeechRecognizerState.cs
@@ -3,11 +3,29 @@ namespace Windows.Media.SpeechRecognition
 	public enum SpeechRecognizerState 
 	{
 		Idle,
+
 		Capturing,
+
+		#if __ANDROID__ || __IOS__ || NET46 || __WASM__
+		[global::Uno.NotImplemented]
+		#endif
 		Processing,
+
+		#if __ANDROID__ || __IOS__ || NET46 || __WASM__
+		[global::Uno.NotImplemented]
+		#endif
 		SoundStarted,
+
+		#if __ANDROID__ || __IOS__ || NET46 || __WASM__
+		[global::Uno.NotImplemented]
+		#endif
 		SoundEnded,
+
 		SpeechDetected,
-		Paused,
+
+		#if __ANDROID__ || __IOS__ || NET46 || __WASM__
+		[global::Uno.NotImplemented]
+		#endif
+		Paused
 	}
 }
