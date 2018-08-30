43845558aea181053f210eb8b2ae27d1f49c02c3
Dr.Rx <david.rey@nventive.com>
2018-08-30 15:37:42 -0400

    Add tests for the string to `Type` conversion

---
+++ src/Uno.UI.Tests/XamlReaderTests/TypeConvertersControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Uno.UI.Tests.XamlReaderTests
+{
+	public class TypeConvertersControl : FrameworkElement
+	{
+		public Type TypeProperty { get; set; }
+
+		public Uri UriProperty { get; set; }
+	}
+}
