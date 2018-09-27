818cf474047787dce8a431fa86a6e90d7e27cc68
Jérôme Laban <jerome.laban@nventive.com>
2018-09-26 22:39:22 -0400

    Add support for ApplicationData.Current.LocalFolder persistence.
    - Added requirement for AppManifest.js/RequireJS dependency
    - Adjust ApplicationDataContainer default paths for Wasm
    - Add IDBFS persistence with tentative 10s sync

--- src/Uno.UI.Wasm/ts/Interop/_global.ts
+++ src/Uno.UI.Wasm/ts/Interop/_global.ts
@@ -3,4 +3,3 @@
 declare const MonoRuntime: Uno.UI.Interop.IMonoRuntime;
 declare const WebAssemblyApp: Uno.UI.Interop.IWebAssemblyApp;
 declare const UnoAppManifest: Uno.UI.IAppManifest;
-declare const Module: Uno.UI.Interop.IWebAssemblyModule;
