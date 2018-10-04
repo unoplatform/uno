97caf23eca1d75afc5e769a6c7b877bdd6f63eca
Carl de Billy <carl.debilly@nventive.com>
2018-10-04 15:49:14 -0400

    Added "RoutedEventsPage" to sample app

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Input/RoutedEvents/RoutedEventsPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace SamplesApp.Samples.RoutedEvents
+{
+	[SampleControlInfo("Inputs", "Routed Events", description: "Test surface for routed events.")]
+	public sealed partial class RoutedEventsPage : Page
+	{
+		public RoutedEventsPage()
+		{
+			this.InitializeComponent();
+
+			HookEvents(outer, resultOuter);
+			HookEvents(middle, resultMiddle);
+			HookEvents(inner, resultInner);
+			HookEvents(scroll, resultScroll);
+		}
+
+		private void HookEvents(Grid grid, TextBlock textBlock)
+		{
+			void TapHandler(object snd, TappedRoutedEventArgs evt)
+			{
+				textBlock.Text += $".T({evt.GetPosition(null)})";
+				evt.Handled = true;
+			}
+
+			void TapHandler2(object snd, TappedRoutedEventArgs evt)
+			{
+				textBlock.Text += $".t({GetPosition(evt)})";
+				//evt.Handled = false;
+			}
+
+			grid.AddHandler(TappedEvent, (TappedEventHandler) TapHandler, false);
+			grid.AddHandler(TappedEvent, (TappedEventHandler) TapHandler2, true);
+
+			string GetPosition(TappedRoutedEventArgs evt)
+			{
+				var pos = evt.GetPosition(null);
+				return $"{Math.Round(pos.X, 1)}, {Math.Round(pos.Y, 1)}";
+			}
+
+			textBlock.Tapped += TapHandler3;
+
+			void TapHandler3(object sender, TappedRoutedEventArgs evt)
+			{
+				textBlock.Text += $".e({GetPosition(evt)})";
+			}
+
+			grid.AddHandler(DoubleTappedEvent, (DoubleTappedEventHandler) DoubleTappedHandler, true);
+
+			void DoubleTappedHandler(object sender, DoubleTappedRoutedEventArgs e)
+			{
+				textBlock.Text += $".dt";
+				e.Handled = true;
+			}
+
+			grid.PointerEntered += (s, e) =>
+			{
+				textBlock.Text += "<<";
+				e.Handled = true;
+			};
+
+			grid.PointerExited += (s, e) => textBlock.Text += ">>";
+
+			grid.PointerPressed += (s, e) =>
+			{
+				textBlock.Text += "_";
+				grid.BorderThickness = new Thickness(3);
+			};
+			grid.PointerReleased += (s, e) =>
+			{
+				textBlock.Text += "-";
+				grid.BorderThickness = new Thickness(0);
+			};
+
+			grid.GotFocus += (s, e) => textBlock.Text += ".F";
+			grid.LostFocus += (s, e) => textBlock.Text += ".f";
+
+			grid.KeyDown += (s, e) => textBlock.Text += ".K";
+			grid.KeyUp += (s, e) => textBlock.Text += ".k";
+		}
+	}
+}
