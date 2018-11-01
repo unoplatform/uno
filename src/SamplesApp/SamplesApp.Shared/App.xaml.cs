a9db7da621ea51f5204bb415be9054c42b8bede4
Carl de Billy <carl.debilly@nventive.com>
2018-11-01 13:19:44 -0400

    Update App.xaml.cs

--- src/SamplesApp/SamplesApp.Shared/App.xaml.cs
+++ src/SamplesApp/SamplesApp.Shared/App.xaml.cs
@@ -78,7 +78,7 @@ namespace SamplesApp
 					// When the navigation stack isn't restored navigate to the first page,
 					// configuring the new page by passing required information as a navigation
 					// parameter
-					rootFrame.Navigate(typeof(RoutedEventsPage), e.Arguments);
+					rootFrame.Navigate(typeof(MainPage), e.Arguments);
 				}
 				// Ensure the current window is active
 				Windows.UI.Xaml.Window.Current.Activate();
