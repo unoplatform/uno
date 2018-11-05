02f7a491bf1024d0d616606d1d29f01e3a4d6b58
Carl de Billy <carl.debilly@nventive.com>
2018-11-05 11:20:31 -0500

    Added name to RoutedEvent for diagnostic purposes.

--- src/Uno.UI/UI/Xaml/RoutedEvent.cs
+++ src/Uno.UI/UI/Xaml/RoutedEvent.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
 
 namespace Windows.UI.Xaml
 {
-    public partial class RoutedEvent
-    {
-    }
+	public partial class RoutedEvent
+	{
+		public string Name { get; private set; }
+
+		public RoutedEvent([CallerMemberName] string name = null)
+		{
+			Name = name;
+		}
+
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
 }
