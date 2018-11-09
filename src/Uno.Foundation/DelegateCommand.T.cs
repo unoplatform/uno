071ca6b29d2dfd1ab97382c9c8db604f2e47f594
baptiste-guillory <baptiste.guillory@nventive.com>
2018-11-09 08:23:51 -0500

    Add DelegateCommand<T>

---
+++ src/Uno.Foundation/DelegateCommand.T.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Uno.UI.Common
+{
+	public class DelegateCommand<T> : ICommand
+	{
+		private Action<T> _action;
+
+		public event EventHandler CanExecuteChanged;
+
+		public DelegateCommand(Action<T> action)
+		{
+			_action = action;
+			OnCanExecuteChanged(true);
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object parameter)
+		{
+			_action?.Invoke((T)parameter);
+		}
+
+		private void OnCanExecuteChanged(bool canExecute)
+		{
+			CanExecuteChanged?.Invoke(this, new EventArgs());
+		}
+	}
+}
