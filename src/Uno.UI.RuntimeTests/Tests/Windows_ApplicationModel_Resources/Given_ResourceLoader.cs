f215a85a7f1157713b605ef58384e4ca0bfc058d
Jérôme Laban <jerome.laban@nventive.com>
2018-12-06 10:23:29 -0500

    Add resouces runtime tests

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_ApplicationModel_Resources/Given_ResourceLoader.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.ApplicationModel.Resources;
+
+namespace Uno.UI.RuntimeTests.Tests
+{
+	[TestClass]
+	public class Given_ResourceLoader
+	{
+		[TestMethod]
+		public void When_SimpleString()
+		{
+			var SUT = ResourceLoader.GetForCurrentView();
+			Assert.AreEqual("My Simple String (en-US)", SUT.GetString("Given_ResourceLoader.When_SimpleString"));
+		}
+	}
+}
