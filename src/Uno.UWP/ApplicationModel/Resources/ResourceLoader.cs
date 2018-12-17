d44b7c1ba5c3c158cc75230fc696bf3a17551bac
Jérôme Laban <jerome.laban@nventive.com>
2018-12-17 12:42:33 -0500

    Adjust resource loader tests for current-culture loading

--- src/Uno.UWP/ApplicationModel/Resources/ResourceLoader.cs
+++ src/Uno.UWP/ApplicationModel/Resources/ResourceLoader.cs
@@ -45,7 +45,7 @@ namespace Windows.ApplicationModel.Resources
 				return parentValue;
 			}
 
-#if !__WASM__
+#if !__WASM__ && !NET46
 			if (GetStringInternal == null)
 			{
 				throw new InvalidOperationException($"ResourceLoader.GetStringInternal hasn't been set. Make sure ResourceHelper is initialized properly.");
