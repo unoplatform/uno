6ec693428639523429bafc05a3d648d4a53f0a01
Carl de Billy <carl.debilly@nventive.com>
2019-01-09 14:42:43 -0500

    Imported transform tests from private repo.

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Media/Transform/Polygon_With_RotateTransform.xaml.cs
@@ -0,0 +1,14 @@
+using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace SamplesApp.Wasm.Windows_UI_Xaml_Media.Transform
+{
+	[SampleControlInfo("Transform", "Polygon_With_RotateTransform")]
+	public sealed partial class Polygon_With_RotateTransform : UserControl
+	{
+		public Polygon_With_RotateTransform()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
