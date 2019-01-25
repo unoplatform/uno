cbed35803de9d9cec3e413184de1926e472877fe
Carl de Billy <carl.debilly@nventive.com>
2019-01-25 14:42:43 -0500

    Slider Tests

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Slider/SliderViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Core;
+using Uno.UI.Samples.UITests.Helpers;
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.Slider
+{
+	internal class SliderViewModel : ViewModelBase
+	{
+		private int _sliderValue;
+
+		public SliderViewModel(CoreDispatcher dispatcher) : base(dispatcher)
+		{
+		}
+
+		public int SliderValue
+		{
+			get => _sliderValue;
+			set
+			{
+				_sliderValue = value;
+				RaisePropertyChanged();
+			}
+		}
+	}
+}
