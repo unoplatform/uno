654004806511e25196fd4009284f0a9b64ebb961
Carl de Billy <carl.debilly@nventive.com>
2019-01-25 14:42:43 -0500

    Slider Tests

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Slider/Slider_Styled.xaml.cs
@@ -0,0 +1,14 @@
+using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.Samples.Content.UITests.Slider
+{
+	[SampleControlInfoAttribute("Slider", "Slider_Styled", description: "Slider using the UWP default style (instead of native styling)")]
+	public sealed partial class Slider_Styled : UserControl
+	{
+		public Slider_Styled()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
