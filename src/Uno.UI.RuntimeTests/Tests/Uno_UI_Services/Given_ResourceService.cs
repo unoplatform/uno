c733a6c561d463ec04a95a922ef4014f663417b9
Jérôme Laban <jerome.laban@nventive.com>
2019-01-31 15:38:45 -0500

    Fix generated resources are not injected early enough

---
+++ src/Uno.UI.RuntimeTests/Tests/Uno_UI_Services/Given_ResourceService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+
+namespace Uno.UI.RuntimeTests.Tests.Uno_UI_Services
+{
+#if __IOS__ || __ANDROID__
+	[TestClass]
+	public class Given_ResourceService
+	{
+		[TestMethod]
+		public void When_Valid_NativeResource()
+		{
+			Assert.AreEqual("SamplesApp", ResourceHelper.ResourcesService.Get("ApplicationName"));
+			Assert.AreEqual("ValidResource (en)", ResourceHelper.ResourcesService.Get("Given_ResourceService.ValidResource"));
+		}
+	}
+#endif
+}
