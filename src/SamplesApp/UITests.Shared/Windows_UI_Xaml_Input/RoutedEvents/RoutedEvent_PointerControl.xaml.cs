44d072d1b3d2d33c2363b24e88cc2b76f03f037f
Carl de Billy <carl.debilly@nventive.com>
2019-02-11 16:47:51 -0500

    New RoutedEvents samples

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Input/RoutedEvents/RoutedEvent_PointerControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace UITests.Shared.Windows_UI_Xaml_Input.RoutedEvents
+{
+	public sealed partial class RoutedEvent_PointerControl : UserControl
+	{
+		public RoutedEvent_PointerControl()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
