01d5c9e0fe9f9ab697d9dacc1d571522353a6b9d
Martin Zikmund <martinzikmund@live.com>
2019-02-13 23:33:41 +0100

    Moved display information properties among implemented

--- src/Uno.UWP/Graphics/Display/DisplayInformation.cs
+++ src/Uno.UWP/Graphics/Display/DisplayInformation.cs
@@ -30,6 +30,22 @@ namespace Windows.Graphics.Display
 
 		public DisplayOrientations NativeOrientation { get; private set; }
 
+		public float LogicalDpi { get; private set; }
+
+		public uint ScreenHeightInRawPixels { get; private set; }
+
+        public uint ScreenWidthInRawPixels { get; private set; }
+
+        public double? DiagonalSizeInInches { get; private set; }
+
+        public double RawPixelsPerViewPixel { get; private set; }
+
+		public float RawDpiX { get; private set; }
+
+		public float RawDpiY { get; private set; }
+
+        public ResolutionScale ResolutionScale { get; private set; }
+
 		private DisplayInformation()
 		{
 			Initialize();
