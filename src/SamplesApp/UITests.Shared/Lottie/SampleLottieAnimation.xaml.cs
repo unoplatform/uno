bf8590fcd2fd34dc6448d0fb1ba556e31c59244c
Carl de Billy <carl.debilly@nventive.com>
2019-03-07 13:43:30 -0500

    Created "Lottie" project and sample on UWP using "official" implementation.

---
+++ src/SamplesApp/UITests.Shared/Lottie/SampleLottieAnimation.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Lottie
+{
+	[SampleControlInfo("Lottie", "Sample animation")]
+	public sealed partial class SampleLottieAnimation : Page
+	{
+		public SampleLottieAnimation()
+		{
+			this.InitializeComponent();
+
+			stretch.ItemsSource = Enum.GetValues(typeof(Stretch))
+				.Cast<Stretch>()
+				.Select(x => x.ToString());
+			stretch.SelectedIndex = 3;
+		}
+	}
+}
