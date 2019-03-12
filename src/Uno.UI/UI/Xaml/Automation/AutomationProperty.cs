d2534ccc436a33899dcc0adb54453a1348afcecc
Jérôme Laban <jerome.laban@nventive.com>
2019-03-11 22:11:43 -0400

    Added non-throwing stubs for AutomationProperty

--- src/Uno.UI/UI/Xaml/Automation/AutomationProperty.cs
+++ src/Uno.UI/UI/Xaml/Automation/AutomationProperty.cs
@@ -2,7 +2,10 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.UI.Xaml.Automation
 {
-	public  partial class ScrollPatternIdentifiers 
+	#if __ANDROID__ || __IOS__ || NET46 || __WASM__ || __MACOS__
+	[global::Uno.NotImplemented]
+	#endif
+	public  partial class AutomationProperty 
 	{
 	}
 }
