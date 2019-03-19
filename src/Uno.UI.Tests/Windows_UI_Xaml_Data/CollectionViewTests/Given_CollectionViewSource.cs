fd3c0c3e02cf22d4e5b45a073d552cec7a93add4
Jérôme Laban <jerome.laban@nventive.com>
2019-03-19 09:53:41 -0400

    Make CollectionViewSource.View a proper DependencyProperty

---
+++ src/Uno.UI.Tests/Windows_UI_Xaml_Data/CollectionViewTests/Given_CollectionViewSource.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Uno.UI.Tests.Windows_UI_Xaml_Data.CollectionViewTests
+{
+	[TestClass]
+	public class Given_CollectionViewSource
+	{
+		[TestMethod]
+		public void When_Source_Is_Not_Enumerable()
+		{
+			var SUT = new CollectionViewSource();
+
+			Assert.IsNull(SUT.View);
+
+			SUT.Source = 42;
+
+			Assert.IsNull(SUT.View);
+		}
+
+		[TestMethod]
+		public void When_Source_IsArray()
+		{
+			var SUT = new CollectionViewSource();
+
+			Assert.IsNull(SUT.View);
+
+			SUT.Source = new[] { 42 };
+
+			Assert.IsNotNull(SUT.View);
+			Assert.AreEqual(1, SUT.View.Count);
+		}
+
+		[TestMethod]
+		public void When_Source_Notify()
+		{
+			var SUT = new CollectionViewSource();
+			int viewChanged = 0;
+			SUT.RegisterPropertyChangedCallback(CollectionViewSource.ViewProperty, (s, e) => viewChanged++);
+
+			Assert.IsNull(SUT.View);
+
+			SUT.Source = new[] { 42 };
+
+			Assert.IsNotNull(SUT.View);
+			Assert.AreEqual(1, SUT.View.Count);
+			Assert.AreEqual(1, viewChanged);
+		}
+	}
+}
--- src/Uno.UI/UI/Xaml/Data/CollectionViewGroup.cs
+++ src/Uno.UI/UI/Xaml/Data/CollectionViewGroup.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Uno.UI.DataBinding;
 using Windows.Foundation.Collections;
 
 namespace Windows.UI.Xaml.Data
 {
 	internal class CollectionViewGroup : ICollectionViewGroup
 	{
-		public CollectionViewGroup(object group)
+		private readonly BindingPath _bindingPath;
+
+		public CollectionViewGroup(object group, PropertyPath itemsPath)
 		{
 			Group = group;
 
-			GroupItems = ObservableVectorWrapper.Create(group);
+			if (itemsPath != null)
+			{
+				_bindingPath = new BindingPath(itemsPath.Path, null);
+				_bindingPath.DataContext = group;
+
+				GroupItems = ObservableVectorWrapper.Create(_bindingPath.Value);
+			}
+			else
+			{
+				GroupItems = ObservableVectorWrapper.Create(group);
+			}
 		}
 		public object Group { get; }
+
 
 		public IObservableVector<object> GroupItems { get; }
 	}
