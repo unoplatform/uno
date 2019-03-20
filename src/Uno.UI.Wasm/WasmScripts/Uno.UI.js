9d4d2d8b6d0db8d52afe348435bcc93be30e4f3d
warrenbenyahia <46033284+warrenbenyahia@users.noreply.github.com>
2019-03-20 09:55:24 -0400

    Removed useless file

--- src/Uno.UI.Wasm/WasmScripts/Uno.UI.js
+++ src/Uno.UI.Wasm/WasmScripts/Uno.UI.js
@@ -970,10 +970,7 @@ var Uno;
                 }
                 const shouldRaiseLoadEvents = WindowManager.isLoadEventsEnabled
                     && this.getIsConnectedToRootElement(childElement);
-                parentElement.removeChild(childElement);
-                // Mark the element as unarranged, so if it gets measured while being
-                // disconnected from the root element, it won't be visible.
-                childElement.classList.add(WindowManager.unoUnarrangedClassName);
+                parentElement.removeChild(childElement);             
                 if (shouldRaiseLoadEvents) {
                     this.dispatchEvent(childElement, "unloaded");
                 }
@@ -1006,8 +1003,8 @@ var Uno;
                 }
                 if (element.parentElement) {
                     element.parentElement.removeChild(element);
+                    delete this.allActiveElementsById[viewId];
                 }
-                delete this.allActiveElementsById[viewId];
             }
             getBoundingClientRect(elementId) {
                 const htmlElement = this.allActiveElementsById[elementId];
