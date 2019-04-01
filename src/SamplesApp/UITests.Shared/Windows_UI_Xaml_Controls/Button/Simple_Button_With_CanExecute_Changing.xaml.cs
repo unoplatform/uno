2b18d96ba5c7b6131fec4f52e2558ed4d3767141
Carl de Billy <carl.debilly@nventive.com>
2019-04-01 16:30:02 -0400

    Imported "Button" samples from internal project

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Button/Simple_Button_With_CanExecute_Changing.xaml.cs
@@ -0,0 +1,16 @@
+using Uno.UI.Samples.Controls;
+using Uno.UI.Samples.Presentation.SamplePages;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.Samples.Content.UITests.ButtonTestsControl
+{
+	[SampleControlInfo("Button", "Simple_Button_With_CanExecute_Changing", typeof(ButtonTestsViewModel))]
+
+	public sealed partial class Simple_Button_With_CanExecute_Changing : UserControl
+	{
+		public Simple_Button_With_CanExecute_Changing()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
