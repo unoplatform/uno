75478e723c81ba1784526b9d77edf8b112dda3a9
Warren Benyahia <warren.benyahia@nventive.com>
2019-04-09 15:02:55 -0400

    - Added new sample "DateTimePicker" based on VCA.Messenger style to investigate VCA.Messenger's bug easier - Cleaned some files

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Models/DateTimePickerViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Windows.UI.Core;
+using Uno.UI.Samples.UITests.Helpers;
+using System;
+
+namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.Models
+{
+	public class DateTimePickerViewModel : ViewModelBase
+	{
+		public DateTimePickerViewModel(CoreDispatcher dispatcher) : base(dispatcher)
+		{
+			m_Date = DateTimeOffset.Now.Date;
+			m_Time = DateTimeOffset.Now.TimeOfDay;
+		}
+
+		private DateTimeOffset m_Date;
+
+		public DateTimeOffset Date
+		{
+			get { return m_Date; }
+			set
+			{
+				m_Date = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		private TimeSpan m_Time;
+
+		public TimeSpan Time
+		{
+			get { return m_Time; }
+			set
+			{
+				m_Time = value;
+				RaisePropertyChanged();
+			}
+		}
+	}
+}
