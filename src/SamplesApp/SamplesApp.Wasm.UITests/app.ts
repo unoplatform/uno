569cf6e78245dc5ca93d97d8834646da9e0c21e9
Jérôme Laban <jerome.laban@nventive.com>
2019-04-15 15:56:41 -0400

    Add WebAssembly UI Testing
    - Add linux build support
    - Add puppeteer based UI Testing

---
+++ src/SamplesApp/SamplesApp.Wasm.UITests/app.ts
@@ -0,0 +1,24 @@
+﻿import { TestRunner } from "./TestRunner";
+
+const puppeteer = require('puppeteer');
+const path = require("path");
+const fs = require('fs');
+
+(async () => {
+	console.log(`Init puppeteer`);
+	const browser = await puppeteer.launch({
+		"headless": false,
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+		"defaultViewport": { "width": 1280, "height": 1024 }
+	});
+
+	const page = await browser.newPage();
+	await page.goto("http://localhost:8000/");
+
+	var runner = new TestRunner(page);
+	await runner.runTests();
+
+	console.log(`Tests done`);
+
+	await browser.close();
+})();
