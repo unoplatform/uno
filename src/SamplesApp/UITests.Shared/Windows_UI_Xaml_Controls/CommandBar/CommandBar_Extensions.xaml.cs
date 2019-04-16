38e40ecbc3a397eb3b02103d9d9819d262fab658
Jérôme Laban <jerome.laban@nventive.com>
2019-04-16 13:58:34 -0400

    Add CommandBar samples

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/CommandBar/CommandBar_Extensions.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Uno.UI.Samples.Content.UITests.CommandBar
+{
+	[SampleControlInfo("CommandBar", "Extensions")]
+    public sealed partial class CommandBar_Extensions : UserControl
+    {
+        public CommandBar_Extensions()
+        {
+            this.InitializeComponent();
+
+			this.DataContext = new CommandBarCommandViewModel();
+        }
+    }
+}
