f0a724436c52ad807111017e21441a69c36d9d39
Martin Zikmund <martinzikmund@live.com>
2019-04-23 13:53:33 +0200

    iOS implementation of CurrentInputMethodLanguageTag, TrySetInputMethodLanguageTag

---
+++ src/Uno.UWP/Globalization/Language.iOS.cs
@@ -0,0 +1,42 @@
+﻿#if __IOS__
+using UIKit;
+
+namespace Windows.Globalization
+{
+	public partial class Language
+	{
+		public static string CurrentInputMethodLanguageTag
+		{
+			get
+			{
+				UIView responder = null;
+				if (UIApplication.SharedApplication?.KeyWindow?.RootViewController?.View != null)
+				{
+					responder = FindFirstResponder(UIApplication.SharedApplication.KeyWindow.RootViewController.View);
+				}
+				var inputMode = responder?.TextInputMode;
+				return inputMode?.PrimaryLanguage ?? "";
+			}
+		}
+
+		private static UIView FindFirstResponder(UIView view)
+		{
+			if (view.IsFirstResponder)
+			{
+				return view;
+			}
+			foreach (var subView in view.Subviews)
+			{
+				var firstResponder = FindFirstResponder(subView);
+				if (firstResponder != null)
+				{
+					return firstResponder;
+				}
+			}
+			return null;
+		}
+
+		public static bool TrySetInputMethodLanguageTag(string languageTag) => false;
+	}
+}
+#endif
