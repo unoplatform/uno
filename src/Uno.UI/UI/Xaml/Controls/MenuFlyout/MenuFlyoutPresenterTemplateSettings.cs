3d2c144b55c4da3219870fa6add572790496a56d
Jérôme Laban <jerome.laban@nventive.com>
2019-04-24 10:21:23 -0400

    Add missing NotImplemented

--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutPresenterTemplateSettings.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutPresenterTemplateSettings.cs
@@ -1,7 +1,11 @@
 #pragma warning disable 108 // new keyword hiding
 #pragma warning disable 114 // new keyword hiding
+
+using Uno;
+
 namespace Windows.UI.Xaml.Controls.Primitives
 {
+	[NotImplemented]
 	public  partial class MenuFlyoutPresenterTemplateSettings : global::Windows.UI.Xaml.DependencyObject
 	{
 		public  double FlyoutContentMinWidth
