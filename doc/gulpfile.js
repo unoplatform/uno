601759f688e945c6fb18d1de26494315258bebee
Florent Cima <florent.cima@nventive.com>
2019-04-29 16:18:21 -0400

    Add serve task to gulpfile to run docs locally.

--- doc/gulpfile.js
+++ doc/gulpfile.js
@@ -1,14 +1,16 @@
-var gulp        = require('gulp'),
-	autoprefixer= require('gulp-autoprefixer'),
-	livereload  = require('gulp-livereload'),
-	notify      = require('gulp-notify'),
-	sass        = require('gulp-sass'),
-	gulpif      = require('gulp-if'),
-	sassLint    = require('gulp-sass-lint'),
-	sourcemaps  = require('gulp-sourcemaps');
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload'),
+    notify = require('gulp-notify'),
+    sass = require('gulp-sass'),
+    gulpif = require('gulp-if'),
+    sassLint = require('gulp-sass-lint'),
+    serve = require('gulp-serve'),
+    sourcemaps = require('gulp-sourcemaps'),
+    exec = require('child_process').exec;
 
 var debug = false;
-var assets      = 'templates/uno';
+var assets = 'templates/uno';
 /**
  * Put relative path to your assets :
  * - Wordpress  : 'public/wp-content/themes/YOUR_THEME/assets';
@@ -16,44 +18,67 @@ var assets      = 'templates/uno';
  */
 
 gulp.task('styles', function() {
-    let output = debug ? 'nested' : 'compressed';
-    return gulp.src(assets + '/styles/scss/main.scss')
-        .pipe(gulpif(debug,sourcemaps.init()))
-        .pipe(gulpif(debug,sassLint()))
-        .pipe(gulpif(debug,sassLint.format()))
-        .pipe(gulpif(debug,sassLint.failOnError()))
-        .pipe(sass({includePaths: ['./node_modules/'], outputStyle: output}) .on('error', sass.logError))
+    var output = debug ? 'nested' : 'compressed';
+    return gulp
+        .src(assets + '/styles/scss/main.scss')
+        .pipe(gulpif(debug, sourcemaps.init()))
+        .pipe(gulpif(debug, sassLint()))
+        .pipe(gulpif(debug, sassLint.format()))
+        .pipe(gulpif(debug, sassLint.failOnError()))
+        .pipe(
+            sass({ includePaths: ['./node_modules/'], outputStyle: output }).on(
+                'error',
+                sass.logError
+            )
+        )
         .pipe(autoprefixer({ browsers: ['last 2 versions', '> 5%'] }))
-        .pipe(gulpif(debug,sourcemaps.write()))
+        .pipe(gulpif(debug, sourcemaps.write()))
         .pipe(gulp.dest(assets + '/styles'))
         .pipe(livereload())
         .pipe(notify({ message: 'CSS complete' }));
 });
 
 gulp.task('watch', function() {
-	livereload.listen();
+    livereload.listen();
 
-	gulp.watch([assets + '/styles/scss/**/*.scss', assets + '/styles/scss/**/*.sass' ], ['styles'])
-		.on('change', function(event) {
-				console.log('File ' + event.path + ' was ' + event.type + ', running CSS task...');
-			}
-		);
+    gulp.watch(
+        [assets + '/styles/scss/**/*.scss', assets + '/styles/scss/**/*.sass'],
+        ['styles', 'build']
+    ).on('change', function(event) {
+        console.log(
+            'File ' +
+                event.path +
+                ' was ' +
+                event.type +
+                ', running CSS task...'
+        );
+    });
 });
 
 gulp.task('default', function() {
-	gulp.start('styles', 'watch');
+    gulp.start('styles', 'watch', 'build', 'serve');
 });
 
-gulp.task('debug', function(){
-	debug = true;
-	gulp.start('styles');
+gulp.task('debug', function() {
+    debug = true;
+    gulp.start('styles');
+});
+
+gulp.task('serve', serve('_site'));
+
+gulp.task('build', function(cb) {
+    exec('docfx build', function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
 });
 
 /**
  * Handle errors and displays them in console
  * @param error
  */
-function swallowError (error) {
-	console.log(error.toString());
-	this.emit('end');
+function swallowError(error) {
+    console.log(error.toString());
+    this.emit('end');
 }
