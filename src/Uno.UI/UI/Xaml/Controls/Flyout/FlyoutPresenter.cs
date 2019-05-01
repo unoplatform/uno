3516400f0f88ab657c10a6fc2086240549608c15
Carl de Billy <carl.debilly@nventive.com>
2019-05-01 14:27:53 -0400

    Clicking in the flyout shouldn't dimiss it anymore

--- src/Uno.UI/UI/Xaml/Controls/Flyout/FlyoutPresenter.cs
+++ src/Uno.UI/UI/Xaml/Controls/Flyout/FlyoutPresenter.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Windows.UI.Xaml.Input;
 
 namespace Windows.UI.Xaml.Controls
 {
-    public partial class FlyoutPresenter : ContentControl
-    {
+	public partial class FlyoutPresenter : ContentControl
+	{
 		public FlyoutPresenter()
 		{
+		}
 
+		protected override void OnPointerPressed(PointerRoutedEventArgs args)
+		{
+			// Stop the propagation of the pressed
+			// to prevent dismissal of the flyout
+			// when clicking inside it.
+			args.Handled = true;
 		}
 
 		protected override bool CanCreateTemplateWithoutParent { get; } = true;
