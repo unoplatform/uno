daa053eca66af75beb30691414ca6d467c8d76b4
w4384 <w4384@DESKTOP-ATHUNVA>
2019-05-02 23:11:35 +0800

    Modify the binding example

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ListView/ListView_ElementBinding.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.ListView
+{
+	/// <summary>
+	/// 可用于自身或导航至 Frame 内部的空白页。
+	/// </summary>
+
+	[SampleControlInfoAttribute("ListView", "ListView_ElementBinding", description: "ListView with items that can be expanded using the toggle buttons.")]
+	public sealed partial class ListView_ElementBinding : Page
+	{
+		public ListView_ElementBinding()
+		{
+			this.InitializeComponent();
+			this.DataContext = "test113";
+		}
+	}
+}
