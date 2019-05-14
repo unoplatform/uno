8c09e35422cb77cedc8ad880c181d65987cfeffe
Martin Zikmund <martinzikmund@live.com>
2019-05-14 22:10:23 +0200

    Common PowerManager for mobile platforms

---
+++ src/Uno.UWP/System/Power/PowerManager.cs
@@ -0,0 +1,19 @@
+﻿#if __MOBILE__
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Windows.System.Power
+{
+	public partial class PowerManager
+	{
+		public static BatteryStatus BatteryStatus => GetBatteryStatus();
+
+		public static EnergySaverStatus EnergySaverStatus => GetEnergySaverStatus();
+
+		public static PowerSupplyStatus PowerSupplyStatus => GetPowerSupplyStatus();
+
+		public static int RemainingChargePercent => GetRemainingChargePercent();
+	}
+}
+#endif
