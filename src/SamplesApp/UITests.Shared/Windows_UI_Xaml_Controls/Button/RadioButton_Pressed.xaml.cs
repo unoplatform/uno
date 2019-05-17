1e3c0e2517468eeefb31e3a6ad81ae29f5465f7f
Papus Koulibaly <papus.koulibaly@nventive.com>
2019-05-17 15:50:27 -0400

    Sample to demonstrate radiobuttons with a pressed states and a changing style

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Button/RadioButton_Pressed.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.Button
+{
+	[SampleControlInfo("Button", "RadioButton_Pressed", description: "RadioButtons with a lower opacity when in pressed state. Tapping on any zone should have the same result.")]
+	public sealed partial class RadioButton_Pressed : UserControl
+    {
+        public RadioButton_Pressed()
+        {
+            this.InitializeComponent();
+        }
+	}
+
+
+	public class FromNullableBoolToCustomValueConverter : IValueConverter
+	{
+		public object NullOrFalseValue { get; set; }
+		public object TrueValue { get; set; }
+
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (value == null || !System.Convert.ToBoolean(value, CultureInfo.InvariantCulture))
+			{
+				return NullOrFalseValue;
+			}
+			else
+			{
+				return TrueValue;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
