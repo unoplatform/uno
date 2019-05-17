157e7243c4b142615e8d91f782b9cef2d3289c5e
Nicolas Champagne <nicolas.champagne@nventive.com>
2019-05-17 11:24:32 -0400

    update to methods used for calculations of header/footer

--- src/Uno.UI/UI/Xaml/Controls/ListViewBase/VirtualizingPanelLayout.Android.cs
+++ src/Uno.UI/UI/Xaml/Controls/ListViewBase/VirtualizingPanelLayout.Android.cs
@@ -581,8 +581,8 @@ namespace Windows.UI.Xaml.Controls
 			var remainingLines = remainingItems / leadingLine.NumberOfViews;
 			var remainingItemExtent = remainingLines * leadingLine.Extent;
 
-			int headerExtent = HeaderViewCount > 0 ? GetChildEndWithMargin(GetChildAt(GetHeaderViewIndex())) - GetChildStartWithMargin(GetChildAt(GetHeaderViewIndex())) : 0;
-			int footerExtent = FooterViewCount > 0 ? GetChildEndWithMargin(GetChildAt(GetFooterViewIndex())) - GetChildStartWithMargin(GetChildAt(GetFooterViewIndex())) : 0;
+			int headerExtent = HeaderViewCount > 0 ? GetChildExtentWithMargins(GetChildAt(GetHeaderViewIndex())) : 0;
+			int footerExtent = FooterViewCount > 0 ? GetChildExtentWithMargins(GetChildAt(GetFooterViewIndex())) : 0;
 
 			int remainingGroupExtent = 0;
 			if (XamlParent.NumberOfDisplayGroups > 0 && RelativeGroupHeaderPlacement == RelativeHeaderPlacement.Inline)
