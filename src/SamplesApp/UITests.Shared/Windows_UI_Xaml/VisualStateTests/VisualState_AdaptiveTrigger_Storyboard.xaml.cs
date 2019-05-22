8e63b1850336b56e6d9a293259ac7d7d847ee2d5
Carl de Billy <carl.debilly@nventive.com>
2019-05-22 16:23:48 -0400

    Fix type in folder for VisualStateManager

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/VisualStateTests/VisualState_AdaptiveTrigger_Storyboard.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Windows_UI_Xaml.ViusalStateTests
+{
+	[SampleControlInfo("Visual states", "VisualState_AdaptiveTrigger_Storyboard")]
+	public sealed partial class VisualState_AdaptiveTrigger_Storyboard : Page
+	{
+		public VisualState_AdaptiveTrigger_Storyboard()
+		{
+			this.InitializeComponent();
+
+			void OnSizeChanged(object snd, SizeChangedEventArgs evt)
+			{
+				txt.Text += $"Control Size: {evt?.NewSize.Width}, Window Size:{Window.Current.Bounds.Width}\n";
+			}
+
+			SizeChanged += OnSizeChanged;
+
+			OnSizeChanged(null, null);
+		}
+
+		private void OnSmall(object sender, object e)
+		{
+			txt.Text += "Trigger: OnSmall()\n";
+		}
+
+		private void OnLarge(object sender, object e)
+		{
+			txt.Text += "Trigger: OnLarge()\n";
+		}
+	}
+}
