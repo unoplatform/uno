5204d03e906b3bb4d4fd575ddf4de8fd79b5e754
David Oliver <david.oliver@nventive.com>
2019-06-03 09:05:53 -0400

    ComboBox virtualization
     - Inherit CarouselPanel from VirtualizingPanel, use ItemsStackPanelLayout
     - amend ComboBox style to support virtualization

---
+++ src/Uno.UI/UI/Xaml/Controls/Primitives/CarouselPanel.wasm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Foundation;
+
+namespace Windows.UI.Xaml.Controls.Primitives
+{
+	public partial class CarouselPanel
+	{
+		protected override Size MeasureOverride(Size availableSize) => _layout.MeasureOverride(availableSize);
+
+		protected override Size ArrangeOverride(Size finalSize) => _layout.ArrangeOverride(finalSize);
+	}
+}
--- src/Uno.UI/UI/Xaml/Controls/VirtualizingPanel.cs
+++ src/Uno.UI/UI/Xaml/Controls/VirtualizingPanel.cs
@@ -1,17 +1,17 @@
 #pragma warning disable 108 // new keyword hiding
+using System;
+
 namespace Windows.UI.Xaml.Controls
 {
-	#if __ANDROID__
-	[global::Uno.NotImplemented]
-	#endif
-	#if false
-	[global::Uno.NotImplemented]
-	#endif
-	public  partial class VirtualizingPanel : global::Windows.UI.Xaml.Controls.Panel
+	public  partial class VirtualizingPanel : Panel, IVirtualizingPanel
 	{
 		public VirtualizingPanel()
 		{
 
 		}
+
+		public VirtualizingPanelLayout GetLayouter() => GetLayouterCore();
+
+		private protected virtual VirtualizingPanelLayout GetLayouterCore() => throw new NotSupportedException($"This method must be overridden by implementing classes.");
 	}
 }
