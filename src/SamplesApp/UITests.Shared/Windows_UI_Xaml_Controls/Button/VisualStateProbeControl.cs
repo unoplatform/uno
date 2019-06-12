314e49e1196845559841381c601b2f66e14aee91
David Oliver <david.oliver@nventive.com>
2019-06-12 08:52:08 -0400

    RadioButton - support non-standard style
    Support the case where, pace the default Style, Checked/Unchecked and Pressed/etc states are mixed in the same VisualStateGroup. In this case UWP always reapplies Checked/Unchecked last after any other state is applied.

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Button/VisualStateProbeControl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.Button
+{
+	public partial class VisualStateProbeControl : ContentControl
+	{
+
+		protected override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			ApplyValue();
+		}
+
+		public string CurrentState
+		{
+			get { return (string)GetValue(CurrentStateProperty); }
+			set { SetValue(CurrentStateProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for CurrentState.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty CurrentStateProperty =
+			DependencyProperty.Register("CurrentState", typeof(string), typeof(VisualStateProbeControl), new PropertyMetadata("<Unset>", (o, e) => ((VisualStateProbeControl)o).OnCurrentStateChanged((string)e.NewValue)));
+
+		public string ProbeLabel
+		{
+			get { return (string)GetValue(ProbeLabelProperty); }
+			set { SetValue(ProbeLabelProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for ProbeLabel.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty ProbeLabelProperty =
+			DependencyProperty.Register("ProbeLabel", typeof(string), typeof(VisualStateProbeControl), new PropertyMetadata("", (o, e) => ((VisualStateProbeControl)o).ApplyValue()));
+
+		private void OnCurrentStateChanged(string newValue)
+		{
+			ApplyValue();
+		}
+
+		private void ApplyValue()
+		{
+			Content = Content ?? new TextBlock();
+			(Content as TextBlock).Text = $"{ProbeLabel}-{CurrentState}";
+			WriteLine($"{nameof(VisualStateProbeControl)}-{ProbeLabel} Setting state {CurrentState}");
+		}
+
+		private void WriteLine(string line)
+		{
+#if NETFX_CORE
+			Debug.WriteLine(line);
+#else
+			Console.WriteLine(line);
+#endif
+		}
+	}
+}
