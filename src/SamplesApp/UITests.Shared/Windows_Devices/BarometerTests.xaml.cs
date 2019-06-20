30aa6f0bf003ee7aba20fb8f4c37b7d9c1ac9f60
Geoffrey Huntley <ghuntley@ghuntley.com>
2019-06-20 23:39:05 +1000

    removed default file->new template comment

--- src/SamplesApp/UITests.Shared/Windows_Devices/BarometerTests.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_Devices/BarometerTests.xaml.cs
@@ -15,8 +15,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
-
 namespace UITests.Shared.Windows_Devices
 {
 	[SampleControlInfo("Windows.Devices", "Barometer", description: "Demonstrates use of Windows.Devices.Sensors.Barometer")]
