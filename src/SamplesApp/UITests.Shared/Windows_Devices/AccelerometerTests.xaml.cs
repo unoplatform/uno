162d0072fb934093f637e5d72ca110a2a3c2c5fd
Martin Zikmund <martinzikmund@live.com>
2019-06-28 18:45:27 +0200

    Implemented Accelerometer sample

---
+++ src/SamplesApp/UITests.Shared/Windows_Devices/AccelerometerTests.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Sensors;
+using Uno.UI.Samples.UITests.Helpers;
+using Windows.UI.Core;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UITests.Shared.Windows_Devices
+{
+	public sealed partial class AccelerometerTests : UserControl
+	{
+
+		public AccelerometerTests()
+		{
+			this.InitializeComponent();
+			DataContext = new AccelerometerTestsViewModel(Dispatcher);
+		}
+
+		private class AccelerometerTestsViewModel : ViewModelBase
+		{
+			private Accelerometer _accelerometer = null;
+			private AccelerometerReading _lastReading;
+			private DateTimeOffset? _lastShake;
+
+			public AccelerometerTestsViewModel(CoreDispatcher dispatcher) : base(dispatcher)
+			{
+				_accelerometer = Accelerometer.GetDefault();
+				_accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+				_accelerometer.Shaken += Accelerometer_Shaken;
+			}
+
+			public AccelerometerReading LastReading
+			{
+				get => _lastReading;
+				set
+				{
+					_lastReading = value;
+					RaisePropertyChanged();
+				}
+			}
+
+			public DateTimeOffset? LastShake
+			{
+				get => _lastShake;
+				set
+				{
+					_lastShake = value;
+					RaisePropertyChanged();
+				}
+			}
+
+			private async void Accelerometer_ReadingChanged(Accelerometer sender, AccelerometerReadingChangedEventArgs args)
+			{
+				await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
+					() => LastReading = args.Reading);
+			}
+
+			private async void Accelerometer_Shaken(Accelerometer sender, AccelerometerShakenEventArgs args)
+			{
+				await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
+					() => LastShake = args.Timestamp);
+			}
+		}
+	}
+}
--- src/Uno.UWP/Devices/Sensors/Accelerometer.iOS.cs
+++ src/Uno.UWP/Devices/Sensors/Accelerometer.iOS.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿#if __IOS__
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Windows.Devices.Sensors
 {
-	class Accelerometer
+	public partial class Accelerometer
 	{
+		private static Accelerometer TryCreateInstance(AccelerometerReadingType type)
+		{
+			if (type != AccelerometerReadingType.Standard) return null;
+		}
 	}
 }
+#endif
