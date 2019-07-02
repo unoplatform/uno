bfbae91ac9a122c1c0c8f8069aec5040373ef4fc
Martin Zikmund <martinzikmund@live.com>
2019-07-02 12:04:31 +0200

    Implemented ReportInterval in WASM

--- src/Uno.UWP/Devices/Sensors/Accelerometer.wasm.cs
+++ src/Uno.UWP/Devices/Sensors/Accelerometer.wasm.cs
@@ -14,10 +14,13 @@ namespace Windows.Devices.Sensors
 
 		private ShakeDetector _shakeDetector = null;
 
+		private DateTimeOffset _lastReading = DateTimeOffset.MinValue;
+
 		private Accelerometer()
 		{
 		}
 
+		public uint ReportInterval { get; set; }
 
 		private static Accelerometer TryCreateInstance()
 		{
@@ -68,15 +71,31 @@ namespace Windows.Devices.Sensors
 			}
 		}
 
+		/// <summary>
+		/// Handles readings from Accelerometer.
+		/// Filters the readings if too frequent to match chosen ReportInterval.
+		/// Uses value ReportInterval * 0.8 to make sure that reporting is
+		/// still more frequent rather than less frequent than requested,
+		/// which is in line with documentation
+		/// </summary>
+		/// <param name="x">Accelerometer X</param>
+		/// <param name="y">Accelerometer Y</param>
+		/// <param name="z">Accelerometer Z</param>
+		/// <returns>0 - needed to bind method from WASM</returns>
 		[Preserve]
 		public static int DispatchReading(float x, float y, float z)
 		{
-			_instance?.OnReadingChanged(
-				new AccelerometerReading(
-					x / Gravity * -1,
-					y / Gravity * -1,
-					z / Gravity * -1,
-					DateTimeOffset.UtcNow));
+			var now = DateTimeOffset.UtcNow;
+			if ((now - _instance._lastReading).TotalMilliseconds >= _instance?.ReportInterval * 0.8)
+			{
+				_instance._lastReading = now;
+				_instance?.OnReadingChanged(
+					new AccelerometerReading(
+						x / Gravity * -1,
+						y / Gravity * -1,
+						z / Gravity * -1,
+						now));				
+			}
 			_instance?._shakeDetector?.OnSensorChanged(x, y, z, DateTimeOffset.UtcNow);
 			return 0;
 		}
