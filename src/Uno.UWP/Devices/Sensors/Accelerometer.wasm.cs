fcc04dca18ed2a4782e70ffeef09334085760013
Martin Zikmund <martinzikmund@live.com>
2019-07-02 10:31:37 +0200

    ShakedDetector started

--- src/Uno.UWP/Devices/Sensors/Accelerometer.wasm.cs
+++ src/Uno.UWP/Devices/Sensors/Accelerometer.wasm.cs
@@ -9,6 +9,8 @@ namespace Windows.Devices.Sensors
 	public partial class Accelerometer
 	{
 		private const string JsType = "Windows.Devices.Sensors.Accelerometer";
+		private const float Gravity = 9.81f;
+
 
 		private Accelerometer()
 		{
@@ -26,30 +28,46 @@ namespace Windows.Devices.Sensors
 			return null;
 		}
 
-		private void StartReadingChanged()
+		private void StartReadingChanged() => AttachDeviceMotion();
+	
+		private void StopReadingChanged() => DetachDeviceMotion();
+		
+		private void StartShaken() => AttachDeviceMotion();
+
+		private void StopShaken() => DetachDeviceMotion();
+
+
+		private void AttachDeviceMotion()
 		{
-			var command = $"{JsType}.startReading()";
-			Uno.Foundation.WebAssemblyRuntime.InvokeJS(command);
+			//if both delegates are not null,
+			//we have already started reading previously
+			if (_shaken == null || _readingChanged == null)
+			{
+				var command = $"{JsType}.startReading()";
+				Uno.Foundation.WebAssemblyRuntime.InvokeJS(command);
+			}
 		}
 
-		private void StopReadingChanged()
-		{
-			var command = $"{JsType}.stopReading()";
-			Uno.Foundation.WebAssemblyRuntime.InvokeJS(command);
-		}
 
-		private void StartShaken()
+		private void DetachDeviceMotion()
 		{
-		}
-
-		private void StopShaken()
-		{
+			//we only stop when both are null
+			if (_shaken == null && _readingChanged == null)
+			{
+				var command = $"{JsType}.stopReading()";
+				Uno.Foundation.WebAssemblyRuntime.InvokeJS(command);
+			}
 		}
 
 		[Preserve]
 		public static int DispatchReading(float x, float y, float z)
 		{
-			_instance.OnReadingChanged(new AccelerometerReading(x, y, z, DateTimeOffset.UtcNow));
+			_instance.OnReadingChanged(
+				new AccelerometerReading(
+					x / Gravity * -1,
+					y / Gravity * -1,
+					z / Gravity * -1,
+					DateTimeOffset.UtcNow));
 			return 0;
 		}
 	}
