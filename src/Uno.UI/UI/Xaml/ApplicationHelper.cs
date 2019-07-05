ebb523c5d4014246f55882162eea6b26feddbe62
Carl de Billy <carl.debilly@nventive.com>
2019-07-05 11:27:00 -0400

    Setting custom theme to "Dark" or "Light" will set Application.RequestedTheme

--- src/Uno.UI/UI/Xaml/ApplicationHelper.cs
+++ src/Uno.UI/UI/Xaml/ApplicationHelper.cs
@@ -1,11 +1,27 @@
-﻿namespace Uno.UI
+﻿using Windows.UI.Xaml;
+
+namespace Uno.UI
 {
 	public class ApplicationHelper
 	{
+		private static string _requestedCustomTheme;
+
 		public static string RequestedCustomTheme
 		{
-			get;
-			set;
+			get => _requestedCustomTheme;
+			set
+			{
+				_requestedCustomTheme = value;
+
+				if (_requestedCustomTheme.Equals("Dark"))
+				{
+					Application.Current.RequestedTheme = ApplicationTheme.Dark;
+				}
+				else if (_requestedCustomTheme.Equals("Light"))
+				{
+					Application.Current.RequestedTheme = ApplicationTheme.Light;
+				}
+			}
 		}
 	}
 }
