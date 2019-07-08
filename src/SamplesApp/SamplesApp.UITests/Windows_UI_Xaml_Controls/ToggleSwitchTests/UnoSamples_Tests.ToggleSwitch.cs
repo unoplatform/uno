6780a4eb7417a92f8d8287f8ee6e325f67952bfe
Nicolas Champagne <nicolas.champagne@nventive.com>
2019-07-08 10:09:29 -0400

    ToggleSwitch unittest for tempalte reuse

---
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/ToggleSwitchTests/UnoSamples_Tests.ToggleSwitch.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Uno.UITest.Helpers;
+using Uno.UITest.Helpers.Queries;
+
+namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.ToggleSwitchTests
+{
+	[TestFixture]
+	public partial class ToggleSwitch_Tests : SampleControlUITestBase
+	{
+		[Test]
+		[Ignore("Not available yet")]
+		public void ToggleSwitch_TemplateReuseTest()
+		{
+			Run("Uno.UI.Samples.Content.UITests.ToggleSwitchControl.ToggleSwitch_TemplateReuse");
+
+			var toggleSwitchGroup = _app.Marked("toggleSwitchGroup");
+
+			_app.WaitForElement(toggleSwitchGroup);
+
+			var separatedToggleSwitch = _app.Marked("separatedToggleSwitch");
+			var unloadButton = _app.Marked("unload");
+			var reloadButton = _app.Marked("reload");
+
+			// Assert inital state 
+			Assert.AreEqual("False", toggleSwitchGroup.GetDependencyPropertyValue("IsOn")?.ToString());
+			Assert.AreEqual("False", separatedToggleSwitch.GetDependencyPropertyValue("IsOn")?.ToString());
+
+			// Toggle group and unload/reload to cause templateReuse on lone toggleSwitch
+			toggleSwitchGroup.Tap();
+			_app.Wait(2);
+
+			unloadButton.Tap();
+			_app.Wait(2);
+
+			reloadButton.Tap();
+			_app.Wait(2);
+
+			//Assert final state
+			Assert.AreEqual("True", toggleSwitchGroup.GetDependencyPropertyValue("IsOn")?.ToString());
+			Assert.AreEqual("False", separatedToggleSwitch.GetDependencyPropertyValue("IsOn")?.ToString());
+		}
+	}
+}
---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ToggleSwitchControl/ToggleSwitch_TemplateReuse.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml;
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.ToggleSwitchControl
+{
+	[SampleControlInfo("ToggleSwitch", "ToggleSwitch_TemplateReuse")]
+	public sealed partial class ToggleSwitch_TemplateReuse : Page
+	{
+		public ToggleSwitch_TemplateReuse()
+		{
+			this.InitializeComponent();
+
+			var c = root.Child;
+
+			unload.Tapped += (snd, evt) =>
+			{
+				c = root.Child;
+				root.Child = null;
+				unload.IsEnabled = false;
+				reload.IsEnabled = true;
+			};
+
+			reload.Tapped += (snd, evt) =>
+			{
+				root.Child = c;
+				unload.IsEnabled = true;
+				reload.IsEnabled = false;
+				PropagateOnTemplateReusedTest(separatedToggleSwitch);
+			};
+		}
+
+		// Test is a copy of method PropagateOnTemplateReused which is inaccessible due to protection level and isn't called due to no caching in Samples
+		// Therefore this method 
+		internal static void PropagateOnTemplateReusedTest(ToggleSwitch instance)
+		{
+			if (instance is ToggleSwitch templateAwareElement && (instance as IFrameworkElement).DataContext == null)
+			{
+				templateAwareElement.OnTemplateRecycled();
+			}
+		}
+	}
+}
