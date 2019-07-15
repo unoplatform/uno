f9d43482aa24561cbe3f246d1bad9c9471dbee59
Jerome Laban <jerome.laban@nventive.com>
2019-07-15 14:53:52 -0400

    Adjust the ability to ignore platforms during UI Tests

---
+++ src/SamplesApp/SamplesApp.UITests/TestFramework/ActivePlatformsAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Uno.UITest.Helpers.Queries;
+
+namespace SamplesApp.UITests.TestFramework
+{
+	/// <summary>
+	/// Defines a list of platforms for which the test will be executed. Other platforms will mark the test as ignored.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false)]
+	internal class ActivePlatformsAttribute : PropertyAttribute
+	{
+		public ActivePlatformsAttribute(params Platform[] platforms)
+		{
+			base.Properties.Add("ActivePlatforms", platforms);
+		}
+	}
+}
