e030121982150b54313476de206701f5491743ff
Jerome Laban <jerome.laban@nventive.com>
2019-07-16 10:06:55 -0400

    Remove unused permissions

--- src/SamplesApp/SamplesApp.Droid/Main.cs
+++ src/SamplesApp/SamplesApp.Droid/Main.cs
@@ -27,10 +27,6 @@ namespace SamplesApp.Droid
 		{
 			Android.App.Application.Context.Resources.GetIdentifier("String1", "string", Android.App.Application.Context.PackageName);
 
-			KeyguardManager mKeyGuardManager = (KeyguardManager)GetSystemService(Android.Content.Context.KeyguardService);
-			KeyguardManager.KeyguardLock mLock = mKeyGuardManager.NewKeyguardLock("Springboard");
-			mLock.DisableKeyguard();
-
 			ConfigureUniversalImageLoader();
 		}
 
