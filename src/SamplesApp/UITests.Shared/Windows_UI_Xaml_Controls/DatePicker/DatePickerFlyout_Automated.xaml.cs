d0b20c3c1d035186891a7492f82144af7bd84672
Xiaoy312 <xiaoyao312@gmail.com>
2019-07-19 12:53:47 -0400

    added uitest for date & time pickers flyout

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/DatePicker/DatePickerFlyout_Automated.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.DatePicker
+{
+	[SampleControlInfo("Date Picker", nameof(DatePickerFlyout_Automated))]
+	public sealed partial class DatePickerFlyout_Automated : UserControl
+	{
+		public DatePickerFlyout_Automated()
+		{
+			this.InitializeComponent();
+
+			this.TestDatePickerFlyout.Date = new DateTimeOffset(new DateTime(2019, 3, 12));
+		}
+	}
+}
---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/TimePicker/TimePicker_Flyout_Automated.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.TimePicker
+{
+
+	[SampleControlInfo("Time Picker", nameof(TimePicker_Flyout_Automated))]
+	public sealed partial class TimePicker_Flyout_Automated : UserControl
+	{
+		public TimePicker_Flyout_Automated()
+		{
+			this.InitializeComponent();
+			this.TestTimePicker.Time = new TimeSpan(3, 12, 0);
+		}
+	}
+}
