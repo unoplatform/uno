422a9698f940080da780274b19f26ea0f81f42e1
Martin Zikmund <martinzikmund@live.com>
2019-08-09 08:48:45 +0200

    Accidental change reverted, whitespace change

--- src/Uno.UI/DebugHelper.Android.cs
+++ src/Uno.UI/DebugHelper.Android.cs
@@ -7,13 +7,11 @@ using Java.Lang;
 
 namespace Uno.UI
 {
-	[EditorBrowsable(EditorBrowsableState.Never)]
 	public static partial class DebugHelper
 	{
 		/// <summary>
 		/// Returns the complete current native stack. Useful in advanced debugging scenarios.
-		/// </summary>
-		[EditorBrowsable(EditorBrowsableState.Never)]
+		/// </summary>		
 		public static string[] NativeStackTrace => Thread.CurrentThread().GetStackTrace().Select(ste => ste.ToString()).ToArray();
 	}
 }
