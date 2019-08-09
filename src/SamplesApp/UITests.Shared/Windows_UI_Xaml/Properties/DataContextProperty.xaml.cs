93cc46d84e964c3556a714deb15b5fac2495bd35
Carl de Billy <carl.debilly@nventive.com>
2019-08-09 12:50:40 -0400

    Added a sample for bug #856

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/Properties/DataContextProperty.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Windows_UI_Xaml.Properties
+{
+	[SampleControlInfo("XAML", "DataContextProperty")]
+	public sealed partial class DataContextProperty : Page
+	{
+		public DataContextProperty()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
