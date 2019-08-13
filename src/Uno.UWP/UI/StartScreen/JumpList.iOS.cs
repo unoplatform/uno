466cda5d60b429261ecedfb6f11b7e90a160aee4
Martin Zikmund <martinzikmund@live.com>
2019-08-13 09:42:29 +0200

    Unreversing

--- src/Uno.UWP/UI/StartScreen/JumpList.iOS.cs
+++ src/Uno.UWP/UI/StartScreen/JumpList.iOS.cs
@@ -17,8 +17,7 @@ namespace Windows.UI.StartScreen
 		private void LoadItems()
 		{
 			var shortcuts = UIApplication.SharedApplication.ShortcutItems
-				.Where(s => s.IsUnoShortcut())
-				.Reverse()
+				.Where(s => s.IsUnoShortcut())				
 				.ToArray();
 			Items.Clear();
 			foreach (var shortcut in shortcuts)
@@ -32,10 +31,9 @@ namespace Windows.UI.StartScreen
 		{
 			var nonUnoShortcuts = UIApplication.SharedApplication.ShortcutItems.Where(s => !s.IsUnoShortcut()).ToArray();
 			var convertedItems = Items
-				.Select(item => item.ToShortcutItem())
-				.Reverse()
+				.Select(item => item.ToShortcutItem())				
 				.ToArray();
-			UIApplication.SharedApplication.ShortcutItems = convertedItems.Union(nonUnoShortcuts).ToArray();
+			UIApplication.SharedApplication.ShortcutItems = nonUnoShortcuts.Union(convertedItems).ToArray();
 			return Task.CompletedTask.AsAsyncAction();
 		}
 	}
