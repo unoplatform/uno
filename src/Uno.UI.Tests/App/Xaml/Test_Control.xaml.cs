95a59eac2d9b82c6c8130b1c855157cf890ffba9
David Oliver <david.oliver@nventive.com>
2019-08-28 11:52:39 -0400

    Add static resource unit tests

--- src/Uno.UI.Tests/App/Xaml/Test_Control.xaml.cs
+++ src/Uno.UI.Tests/App/Xaml/Test_Control.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.UI.Tests.App.Views;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +26,10 @@ namespace Uno.UI.Tests.App.Xaml
 		public Grid TopGrid => topGrid;
 		public CheckBox TestCheckBox => testCheckBox;
 		public TextBlock TestTextBlock => testTextBlock;
+		public Border TestBorder => testBorder;
+		public Grid TestGrid => testGrid;
+		public Button StyledButton => styledButton;
+		public MyControl TestMyControl => testMyControl;
 
         public Test_Control()
         {
--- src/Uno.UI.Tests/Windows_UI_Xaml/Given_StaticResource.cs
+++ src/Uno.UI.Tests/Windows_UI_Xaml/Given_StaticResource.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Uno.UI.Tests.App.Xaml;
 using Windows.UI;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media;
 
 namespace Uno.UI.Tests.Windows_UI_Xaml
@@ -22,6 +23,73 @@ namespace Uno.UI.Tests.Windows_UI_Xaml
 			app.HostView.Children.Add(control);
 
 			Assert.AreEqual(Colors.MediumSpringGreen, (control.TopGrid.Background as SolidColorBrush).Color);
+		}
+
+		[TestMethod]
+		public void When_Double_Resource_In_Application_Merged()
+		{
+			var app = UnitTestsApp.App.EnsureApplication();
+
+			var control = new Test_Control();
+			app.HostView.Children.Add(control);
+
+			Assert.AreEqual(256, control.TestBorder.Width);
+		}
+
+		[TestMethod]
+		public void When_Double_Resource_In_Application_Merged_Non_View()
+		{
+			var app = UnitTestsApp.App.EnsureApplication();
+
+			var control = new Test_Control();
+			app.HostView.Children.Add(control);
+
+			var rowDef = control.TestGrid.RowDefinitions.First();
+			Assert.AreEqual(256, rowDef.Height.Value);
+		}
+
+		[TestMethod]
+		public void When_In_Same_Dictionary()
+		{
+			var app = UnitTestsApp.App.EnsureApplication();
+
+			var control = new Test_Control();
+			app.HostView.Children.Add(control);
+
+			Assert.AreEqual(33, control.StyledButton.FontSize);
+		}
+
+		[TestMethod]
+		public void When_In_Dictionary_Merged()
+		{
+			var app = UnitTestsApp.App.EnsureApplication();
+
+			var control = new Test_Control();
+			app.HostView.Children.Add(control);
+
+			Assert.AreEqual(new Thickness(1,2,3,4), control.StyledButton.BorderThickness);
+		}
+
+		[TestMethod]
+		public void When_Set_On_Non_DependencyObject()
+		{
+			var app = UnitTestsApp.App.EnsureApplication();
+
+			var control = new Test_Control();
+			app.HostView.Children.Add(control);
+
+			Assert.AreEqual(33, control.TestMyControl.Poco.Bogosity);
+		}
+
+		[TestMethod]
+		public void When_Setting_Non_DependencyProperty()
+		{
+			var app = UnitTestsApp.App.EnsureApplication();
+
+			var control = new Test_Control();
+			app.HostView.Children.Add(control);
+
+			Assert.AreEqual(33, control.TestMyControl.Splinitude);
 		}
 	}
 }
