a86331f566eb6fcd1839c2c3d64a8010996442ab
Martin Zikmund <martinzikmund@live.com>
2019-09-09 19:45:05 +0100

    Implemented QueryUriSupportAsync on Android

--- src/Uno.UWP/System/Launcher.cs
+++ src/Uno.UWP/System/Launcher.cs
@@ -1,17 +1,16 @@
 ﻿#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
 
 using System;
-using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Uno.Extensions;
 using Uno.Logging;
-using Windows.ApplicationModel;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Foundation.Metadata;
-using Windows.UI.Core;
 using Microsoft.Extensions.Logging;
+using System.Linq;
+#if __ANDROID__
+using Android.Content;
+using Android.Content.PM;
+#endif
 
 namespace Windows.System
 {
@@ -24,9 +23,15 @@ namespace Windows.System
 #if __IOS__
 				return UIKit.UIApplication.SharedApplication.OpenUrl(new global::Foundation.NSUrl(uri.OriginalString));
 #elif __ANDROID__
-				var androidUri = global::Android.Net.Uri.Parse(uri.OriginalString);
-				var intent = new global::Android.Content.Intent(global::Android.Content.Intent.ActionView, androidUri);
+				var androidUri = Android.Net.Uri.Parse(uri.OriginalString);
+				var intent = new Intent(Intent.ActionView, androidUri);
 
+				if ( Uno.UI.ContextHelper.Current == null)
+				{
+					throw new InvalidOperationException(
+						"LaunchUriAsync was called too early in application lifetime. " +
+						"App context needs to be initialized");
+				}
 				((Android.App.Activity)Uno.UI.ContextHelper.Current).StartActivity(intent);
 
 				return true;
@@ -34,6 +39,47 @@ namespace Windows.System
 				var command = $"Uno.UI.WindowManager.current.open(\"{uri.OriginalString}\");";
 				var result = Uno.Foundation.WebAssemblyRuntime.InvokeJS(command);
 				return result == "True";
+#else
+				throw new NotImplementedException();
+#endif
+			}
+			catch (Exception exception)
+			{
+				if (typeof(Launcher).Log().IsEnabled(LogLevel.Error))
+				{
+					typeof(Launcher).Log().Error($"Failed to {nameof(LaunchUriAsync)}.", exception);
+				}
+
+				return false;
+			}
+		}
+
+		public static IAsyncOperation<LaunchQuerySupportStatus> QueryUriSupportAsync(
+			Uri uri,
+			LaunchQuerySupportType launchQuerySupportType)
+		{
+			try
+			{
+#if __IOS__
+				
+#elif __ANDROID__
+				var androidUri = Android.Net.Uri.Parse(uri.OriginalString);
+				var intent = new Intent(Intent.ActionView, androidUri);
+
+				if (Uno.UI.ContextHelper.Current == null)
+				{
+					throw new InvalidOperationException(
+						"LaunchUriAsync was called too early in application lifetime. " +
+						"App context needs to be initialized");
+				}
+
+				var manager = Uno.UI.ContextHelper.Current.PackageManager;
+				var supportedResolvedInfos = manager.QueryIntentActivities(
+					intent,
+					PackageInfoFlags.MatchDefaultOnly);
+				var result = supportedResolvedInfos.Any() ?
+					LaunchQuerySupportStatus.Available : LaunchQuerySupportStatus.NotSupported;
+				return Task.FromResult(result).AsAsyncOperation();
 #else
 				throw new NotImplementedException();
 #endif
