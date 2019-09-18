d730f7b3602ca505c3d520546bb43feac4532c75
Massimo <Massimo.Cacchiotti@nventive.com>
2019-09-18 12:59:16 -0400

    Applied some code optimizations in the xaml file generator

--- src/Uno.UI/UI/Xaml/Markup/MarkupExtensions.cs
+++ src/Uno.UI/UI/Xaml/Markup/MarkupExtensions.cs
@@ -9,11 +9,6 @@ namespace Windows.UI.Xaml.Markup
 	public partial class MarkupExtension : IMarkupExtensionOverrides
 	{
 #if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		//protected virtual object ProvideValue()
-		//{
-		//	return null;
-		//}
-
 		object IMarkupExtensionOverrides.ProvideValue()
 		{
 			return ProvideValue();
