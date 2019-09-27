4fa29be239fca9c631675b38a0a13630b0f193b4
Jerome Laban <jerome.laban@nventive.com>
2019-09-27 14:32:23 -0400

    Adjust the AppBar and Command bar styles
    Fixes an image positioning issue

---
+++ src/Uno.UI/UI/Xaml/Controls/AppBar/AppBarButtonTemplateSettings.cs
@@ -0,0 +1,15 @@
+
+namespace Windows.UI.Xaml.Controls.Primitives
+{
+	public  partial class AppBarButtonTemplateSettings : DependencyObject
+	{
+		public double KeyboardAcceleratorTextMinWidth
+		{
+			get
+			{
+				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Controls.Primitives.AppBarButtonTemplateSettings", "KeyboardAcceleratorTextMinWidth");
+				return 0;
+			}
+		}
+	}
+}
---
+++ src/Uno.UI/UI/Xaml/Controls/AppBar/AppBarToggleButtonTemplateSettings.cs
@@ -0,0 +1,14 @@
+namespace Windows.UI.Xaml.Controls.Primitives
+{
+	public partial class AppBarToggleButtonTemplateSettings : global::Windows.UI.Xaml.DependencyObject
+	{
+		public double KeyboardAcceleratorTextMinWidth
+		{
+			get
+			{
+				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Controls.Primitives.AppBarToggleButtonTemplateSettings", "KeyboardAcceleratorTextMinWidth");
+				return 0;
+			}
+		}
+	}
+}
