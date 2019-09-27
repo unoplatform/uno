092f0780a626ce79877d3d792a60d2eb3cf0a987
Artemious <16724889+artemious7@users.noreply.github.com>
2019-09-28 02:09:26 +0300

    feat: [Android] PdfDocument
    Inport initial implementation from PR #1796

---
+++ src/Uno.UWP/Data/Pdf/PdfPageDimensions.Android.cs
@@ -0,0 +1,18 @@
+﻿using Uno;
+using Windows.Foundation;
+
+namespace Windows.Data.Pdf;
+
+public sealed partial class PdfPageDimensions
+{
+	[NotImplemented("__ANDROID__")]
+	public Rect ArtBox { get; }
+	[NotImplemented("__ANDROID__")]
+	public Rect BleedBox { get; }
+	[NotImplemented("__ANDROID__")]
+	public Rect CropBox { get; }
+	[NotImplemented("__ANDROID__")]
+	public Rect MediaBox { get; }
+	[NotImplemented("__ANDROID__")]
+	public Rect TrimBox { get; }
+}
