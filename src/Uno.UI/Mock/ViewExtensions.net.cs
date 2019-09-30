4ebd39a4559292bfd3439838c75195bf992353b0
David Oliver <david.oliver@nventive.com>
2019-09-30 12:38:40 -0400

    chore(styles): [NET461] Add FindFirstChild extension

--- src/Uno.UI/Mock/ViewExtensions.net.cs
+++ src/Uno.UI/Mock/ViewExtensions.net.cs
@@ -17,5 +17,23 @@ namespace Uno.UI
 		public static IEnumerable<object> GetChildren(this object group) => (IEnumerable<object>)(group as FrameworkElement)?._children ?? new object[0];
 
 		public static FrameworkElement GetTopLevelParent(this UIElement view) => throw new NotImplementedException();
+
+		public static T FindFirstChild<T>(this FrameworkElement root) where T : FrameworkElement
+		{
+			return root.GetDescendants().OfType<T>().FirstOrDefault();
+		}
+
+		private static IEnumerable<FrameworkElement> GetDescendants(this FrameworkElement root)
+		{
+			foreach (var child in root._children)
+			{
+				yield return child as FrameworkElement;
+
+				foreach (var descendant in (child as FrameworkElement).GetDescendants())
+				{
+					yield return descendant;
+				}
+			}
+		}
 	}
 }
