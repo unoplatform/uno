48021106f109fafba0b4f681d37a7c97342edc28
David Oliver <david.oliver@nventive.com>
2019-09-30 13:17:56 -0400

    test(styles): Add tests for attached property set to StaticResource

---
+++ src/Uno.UI.Tests/App/Views/MyBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Uno.UI.Tests.App.Views
+{
+	public class MyBehavior
+	{
+
+		public static double GetBulbousness(DependencyObject obj)
+		{
+			return (double)obj.GetValue(BulbousnessProperty);
+		}
+
+		public static void SetBulbousness(DependencyObject obj, double value)
+		{
+			obj.SetValue(BulbousnessProperty, value);
+		}
+
+		// Using a DependencyProperty as the backing store for Bulbousness.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty BulbousnessProperty =
+			DependencyProperty.RegisterAttached("Bulbousness", typeof(double), typeof(MyBehavior), new PropertyMetadata(0d));
+	}
+}
