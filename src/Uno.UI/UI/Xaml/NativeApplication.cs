ddaece94cb1c6ba6919e303a7f31e7979ec5f53f
Martin Zikmund <martinzikmund@live.com>
2019-10-02 01:24:11 +0200

    Forgotten prefix

--- src/Uno.UI/UI/Xaml/NativeApplication.cs
+++ src/Uno.UI/UI/Xaml/NativeApplication.cs
@@ -30,7 +30,7 @@ namespace Windows.UI.Xaml
 
 		private void OnActivityStarted(Activity activity)
 		{
-			InitializationCompleted();
+			_app.InitializationCompleted();
 			if (_lastHandledIntent != activity.Intent &&
 			    activity.Intent?.Extras?.ContainsKey(JumpListItem.ArgumentsExtraKey) == true)
 			{
