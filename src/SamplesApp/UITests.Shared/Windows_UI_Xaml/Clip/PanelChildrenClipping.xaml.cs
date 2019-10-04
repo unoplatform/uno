db01c3162f4224e42f6ecc150083175b29f54215
Carl de Billy <carl.debilly@nventive.com>
2019-10-04 11:11:06 -0400

    New sample for clipping

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/Clip/PanelChildrenClipping.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Windows_UI_Xaml.Clip
+{
+	[SampleControlInfo(category:"Clip")]
+	public sealed partial class PanelChildrenClipping : Page
+	{
+		public PanelChildrenClipping()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
