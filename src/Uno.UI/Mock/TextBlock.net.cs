9c7204ae4bfbb579d44fb0602ef0de4e450a2b2a
David Oliver <david.oliver@nventive.com>
2019-10-04 11:51:52 -0400

    [NET461] Unit-test main TextBlock code

--- src/Uno.UI/Mock/TextBlock.net.cs
+++ src/Uno.UI/Mock/TextBlock.net.cs
@@ -1,46 +1,14 @@
 #pragma warning disable 108 // new keyword hiding
 #pragma warning disable 114 // new keyword hiding
+using Windows.Foundation;
 using Windows.UI.Text;
 
 namespace Windows.UI.Xaml.Controls
 {
-	public  partial class TextBlock : FrameworkElement
+	public partial class TextBlock : FrameworkElement
 	{
-		public TextBlock()
-		{
-			Inlines = new Documents.InlineCollection(this);
-		}
+		private void InitializePartial() { }
 
-		public global::Windows.UI.Xaml.Documents.InlineCollection Inlines { get; }
-		
-		#region TextDecorations
-
-		public TextDecorations TextDecorations
-		{
-			get { return (TextDecorations)this.GetValue(TextDecorationsProperty); }
-			set { this.SetValue(TextDecorationsProperty, value); }
-		}
-
-		public static DependencyProperty TextDecorationsProperty =
-			DependencyProperty.Register(
-				"TextDecorations",
-				typeof(int),
-				typeof(TextBlock),
-				new FrameworkPropertyMetadata(
-					defaultValue: TextDecorations.None,
-					options: FrameworkPropertyMetadataOptions.Inherits,
-					propertyChangedCallback: (s, e) => ((TextBlock)s).OnTextDecorationsChanged()
-				)
-			);
-
-		private void OnTextDecorationsChanged()
-		{
-			OnTextDecorationsChangedPartial();
-			this.InvalidateMeasure();
-		}
-
-		partial void OnTextDecorationsChangedPartial();
-
-		#endregion
+		private int GetCharacterIndexAtPoint(Point point) => -1;
 	}
 }
--- src/Uno.UI/UI/Xaml/Controls/TextBlock/TextBlock.cs
+++ src/Uno.UI/UI/Xaml/Controls/TextBlock/TextBlock.cs
@@ -1,6 +1,5 @@
 ﻿#pragma warning disable CS0109
 
-#if !NET461
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
@@ -815,4 +814,3 @@ namespace Windows.UI.Xaml.Controls
 		}
 	}
 }
-#endif
