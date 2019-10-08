3d5f280e89085bc5d0559e0d5d7f4f6a05451183
anchepiece <anchepiece@gmail.com>
2019-10-07 23:06:25 -0400

    surface UISlider iOS constructors

--- src/Uno.UI/Controls/BindableUISlider.iOS.cs
+++ src/Uno.UI/Controls/BindableUISlider.iOS.cs
@@ -2,32 +2,69 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
-using UIKit;
 using Windows.UI.Xaml;
+using CoreGraphics;
+
+#if XAMARIN_IOS_UNIFIED
+using Foundation;
+using UIKit;
+#elif XAMARIN_IOS
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+#endif
 
 namespace Uno.UI.Controls
 {
-    public partial class BindableUISlider : UISlider, DependencyObject, INotifyPropertyChanged
-    {
-        public BindableUISlider()
-        {
-            this.ValueChanged += (s, e) =>
-            {
-                SetBindingValue(Value, nameof(Value));
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
-            };
-        }
+	public partial class BindableUISlider : UISlider, DependencyObject, INotifyPropertyChanged
+	{
+		public BindableUISlider()
+		{
+			Initialize();
+		}
 
-        public override float Value
-        {
-            get { return base.Value; }
-            set
-            {
-                base.Value = value;
-                
-            }
-        }
+		public BindableUISlider(CGRect frame)
+			: base(frame)
+		{
+			Initialize();
+		}
 
-        public event PropertyChangedEventHandler PropertyChanged;
-    }
+		public BindableUISlider(NSCoder coder)
+			: base(coder)
+		{
+			Initialize();
+		}
+
+		public BindableUISlider(NSObjectFlag t)
+			: base(t)
+		{
+			Initialize();
+		}
+
+		public BindableUISlider(IntPtr handle)
+			: base(handle)
+		{
+			Initialize();
+		}
+
+		private void Initialize()
+		{
+			this.ValueChanged += (s, e) =>
+			{
+				SetBindingValue(Value, nameof(Value));
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+			};
+		}
+
+		public override float Value
+		{
+			get { return base.Value; }
+			set
+			{
+				base.Value = value;
+
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
 }
