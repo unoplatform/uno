206899bf131a26ec16f450e11c4dd96402ce830e
anchepiece <anchepiece@gmail.com>
2019-10-07 23:05:41 -0400

    surface UIActivityIndicatorView iOS constructors

--- src/Uno.UI/Controls/BindableUIActivityIndicatorView.iOS.cs
+++ src/Uno.UI/Controls/BindableUIActivityIndicatorView.iOS.cs
@@ -1,15 +1,46 @@
-﻿using Foundation;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using UIKit;
 using Windows.UI.Xaml;
+using CoreGraphics;
+
+#if XAMARIN_IOS_UNIFIED
+using Foundation;
+using UIKit;
+#elif XAMARIN_IOS
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+#endif
 
 namespace Uno.UI.Views.Controls
 {
 	public partial class BindableUIActivityIndicatorView : UIActivityIndicatorView, DependencyObject
 	{
 		public BindableUIActivityIndicatorView()
+		{
+			InitializeBinder();
+		}
+
+		public BindableUIActivityIndicatorView(CGRect frame)
+			: base(frame)
+		{
+			InitializeBinder();
+		}
+
+		public BindableUIActivityIndicatorView(NSCoder coder)
+			: base(coder)
+		{
+			InitializeBinder();
+		}
+
+		public BindableUIActivityIndicatorView(NSObjectFlag t)
+			: base(t)
+		{
+			InitializeBinder();
+		}
+
+		public BindableUIActivityIndicatorView(IntPtr handle)
+			: base(handle)
 		{
 			InitializeBinder();
 		}
