ab26c4b6055c0ae3c24048ae0e6f75e9b7c26fa0
anchepiece <anchepiece@gmail.com>
2019-10-07 23:07:46 -0400

    surface UINavigationBar iOS constructors

--- src/Uno.UI/Controls/UnoNavigationBar.iOS.cs
+++ src/Uno.UI/Controls/UnoNavigationBar.iOS.cs
@@ -2,13 +2,50 @@
 using System.Collections.Generic;
 using System.Text;
 using CoreGraphics;
+using Windows.UI.Xaml;
+
+#if XAMARIN_IOS_UNIFIED
+using Foundation;
 using UIKit;
+#elif XAMARIN_IOS
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+#endif
 
 namespace Uno.UI.Controls
 {
-	public partial class UnoNavigationBar : UINavigationBar
+	public partial class UnoNavigationBar : UINavigationBar, DependencyObject
 	{
 		internal event Action SizeChanged;
+
+		public UnoNavigationBar()
+		{
+			InitializeBinder();
+		}
+
+		public UnoNavigationBar(CGRect frame)
+			: base(frame)
+		{
+			InitializeBinder();
+		}
+
+		public UnoNavigationBar(NSCoder coder)
+			: base(coder)
+		{
+			InitializeBinder();
+		}
+
+		public UnoNavigationBar(NSObjectFlag t)
+			: base(t)
+		{
+			InitializeBinder();
+		}
+
+		public UnoNavigationBar(IntPtr handle)
+			: base(handle)
+		{
+			InitializeBinder();
+		}
 
 		public override CGRect Frame
 		{
