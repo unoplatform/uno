c101872a8d6c3e96006acd913676f9c0cc072f60
David Oliver <david.oliver@nventive.com>
2019-10-09 14:23:02 -0400

    Add styles test for control in external library
    Note: this verifies that Uno behaves correctly in the 'sunny path'. There are edge cases where Uno is too permissive (we currently consider all styles without a key in an external library as default styles). We also don't treat Themes/generic.xaml properly when defined in the app itself.

---
+++ src/Uno.UI.Tests.ViewLibrary/MyExtControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.Tests.ViewLibrary
+{
+	public partial class MyExtControl : Control
+	{
+		public MyExtControl()
+		{
+			DefaultStyleKey = typeof(MyExtControl);
+		}
+		public string MyTag
+		{
+			get { return (string)GetValue(MyTagProperty); }
+			set { SetValue(MyTagProperty, value); }
+		}
+		
+		public static readonly DependencyProperty MyTagProperty =
+			DependencyProperty.Register("MyTag", typeof(string), typeof(MyExtControl), new PropertyMetadata("Default"));
+		
+		public string MyTag2
+		{
+			get { return (string)GetValue(MyTag2Property); }
+			set { SetValue(MyTag2Property, value); }
+		}
+		
+		public static readonly DependencyProperty MyTag2Property =
+			DependencyProperty.Register("MyTag2", typeof(string), typeof(MyExtControl), new PropertyMetadata("Default"));
+
+
+	}
+}
