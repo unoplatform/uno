f2d036fa5bdc4fbff1a902b546146d24de697754
Artemious <16724889+artemious7@users.noreply.github.com>
2019-10-10 17:15:38 +0300

    revert deleting Windows.Data.pDf files from Generated folder

---
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.Data.Pdf/PdfDocument.cs
@@ -0,0 +1,68 @@
+#pragma warning disable 108 // new keyword hiding
+#pragma warning disable 114 // new keyword hiding
+namespace Windows.Data.Pdf
+{
+	#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+	[global::Uno.NotImplemented]
+	#endif
+	public  partial class PdfDocument 
+	{
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public  bool IsPasswordProtected
+		{
+			get
+			{
+				throw new global::System.NotImplementedException("The member bool PdfDocument.IsPasswordProtected is not implemented in Uno.");
+			}
+		}
+		#endif
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public  uint PageCount
+		{
+			get
+			{
+				throw new global::System.NotImplementedException("The member uint PdfDocument.PageCount is not implemented in Uno.");
+			}
+		}
+		#endif
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public  global::Windows.Data.Pdf.PdfPage GetPage( uint pageIndex)
+		{
+			throw new global::System.NotImplementedException("The member PdfPage PdfDocument.GetPage(uint pageIndex) is not implemented in Uno.");
+		}
+		#endif
+		// Forced skipping of method Windows.Data.Pdf.PdfDocument.PageCount.get
+		// Forced skipping of method Windows.Data.Pdf.PdfDocument.IsPasswordProtected.get
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public static global::Windows.Foundation.IAsyncOperation<global::Windows.Data.Pdf.PdfDocument> LoadFromFileAsync( global::Windows.Storage.IStorageFile file)
+		{
+			throw new global::System.NotImplementedException("The member IAsyncOperation<PdfDocument> PdfDocument.LoadFromFileAsync(IStorageFile file) is not implemented in Uno.");
+		}
+		#endif
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public static global::Windows.Foundation.IAsyncOperation<global::Windows.Data.Pdf.PdfDocument> LoadFromFileAsync( global::Windows.Storage.IStorageFile file,  string password)
+		{
+			throw new global::System.NotImplementedException("The member IAsyncOperation<PdfDocument> PdfDocument.LoadFromFileAsync(IStorageFile file, string password) is not implemented in Uno.");
+		}
+		#endif
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public static global::Windows.Foundation.IAsyncOperation<global::Windows.Data.Pdf.PdfDocument> LoadFromStreamAsync( global::Windows.Storage.Streams.IRandomAccessStream inputStream)
+		{
+			throw new global::System.NotImplementedException("The member IAsyncOperation<PdfDocument> PdfDocument.LoadFromStreamAsync(IRandomAccessStream inputStream) is not implemented in Uno.");
+		}
+		#endif
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public static global::Windows.Foundation.IAsyncOperation<global::Windows.Data.Pdf.PdfDocument> LoadFromStreamAsync( global::Windows.Storage.Streams.IRandomAccessStream inputStream,  string password)
+		{
+			throw new global::System.NotImplementedException("The member IAsyncOperation<PdfDocument> PdfDocument.LoadFromStreamAsync(IRandomAccessStream inputStream, string password) is not implemented in Uno.");
+		}
+		#endif
+	}
+}
---
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.Data.Pdf/PdfPage.cs
@@ -0,0 +1,95 @@
+#pragma warning disable 108 // new keyword hiding
+#pragma warning disable 114 // new keyword hiding
+namespace Windows.Data.Pdf
+{
+	#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+	[global::Uno.NotImplemented]
+	#endif
+	public  partial class PdfPage : global::System.IDisposable
+	{
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public  global::Windows.Data.Pdf.PdfPageDimensions Dimensions
+		{
+			get
+			{
+				throw new global::System.NotImplementedException("The member PdfPageDimensions PdfPage.Dimensions is not implemented in Uno.");
+			}
+		}
+		#endif
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public  uint Index
+		{
+			get
+			{
+				throw new global::System.NotImplementedException("The member uint PdfPage.Index is not implemented in Uno.");
+			}
+		}
+		#endif
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public  float PreferredZoom
+		{
+			get
+			{
+				throw new global::System.NotImplementedException("The member float PdfPage.PreferredZoom is not implemented in Uno.");
+			}
+		}
+		#endif
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public  global::Windows.Data.Pdf.PdfPageRotation Rotation
+		{
+			get
+			{
+				throw new global::System.NotImplementedException("The member PdfPageRotation PdfPage.Rotation is not implemented in Uno.");
+			}
+		}
+		#endif
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public  global::Windows.Foundation.Size Size
+		{
+			get
+			{
+				throw new global::System.NotImplementedException("The member Size PdfPage.Size is not implemented in Uno.");
+			}
+		}
+		#endif
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public  global::Windows.Foundation.IAsyncAction RenderToStreamAsync( global::Windows.Storage.Streams.IRandomAccessStream outputStream)
+		{
+			throw new global::System.NotImplementedException("The member IAsyncAction PdfPage.RenderToStreamAsync(IRandomAccessStream outputStream) is not implemented in Uno.");
+		}
+		#endif
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public  global::Windows.Foundation.IAsyncAction RenderToStreamAsync( global::Windows.Storage.Streams.IRandomAccessStream outputStream,  global::Windows.Data.Pdf.PdfPageRenderOptions options)
+		{
+			throw new global::System.NotImplementedException("The member IAsyncAction PdfPage.RenderToStreamAsync(IRandomAccessStream outputStream, PdfPageRenderOptions options) is not implemented in Uno.");
+		}
+		#endif
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public  global::Windows.Foundation.IAsyncAction PreparePageAsync()
+		{
+			throw new global::System.NotImplementedException("The member IAsyncAction PdfPage.PreparePageAsync() is not implemented in Uno.");
+		}
+		#endif
+		// Forced skipping of method Windows.Data.Pdf.PdfPage.Index.get
+		// Forced skipping of method Windows.Data.Pdf.PdfPage.Size.get
+		// Forced skipping of method Windows.Data.Pdf.PdfPage.Dimensions.get
+		// Forced skipping of method Windows.Data.Pdf.PdfPage.Rotation.get
+		// Forced skipping of method Windows.Data.Pdf.PdfPage.PreferredZoom.get
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		[global::Uno.NotImplemented]
+		public  void Dispose()
+		{
+			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.Data.Pdf.PdfPage", "void PdfPage.Dispose()");
+		}
+		#endif
+		// Processing: System.IDisposable
+	}
+}
