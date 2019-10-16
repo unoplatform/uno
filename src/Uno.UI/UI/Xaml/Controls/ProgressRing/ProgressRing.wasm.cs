9fda5c326238f691bbf3bd6c9ea5c9b85cc3fddf
gevart <gevart@gmail.com>
2019-10-16 21:32:52 +0300

    Moving class into separate file

--- src/Uno.UI/UI/Xaml/Controls/ProgressRing/ProgressRing.wasm.cs
+++ src/Uno.UI/UI/Xaml/Controls/ProgressRing/ProgressRing.wasm.cs
@@ -48,16 +48,4 @@ namespace Windows.UI.Xaml.Controls
 	}
 }
 
-namespace Windows.UI.Xaml.Controls.Primitives
-{//TODO: I'm not an expert in TemplateSettings logic so it's just a stub to make standard ProgressRing UWP style bindings work in Wasm
-	public partial class ProgressRingTemplateSettings : DependencyObject
-	{
-		public double EllipseDiameter { get; set; }
-
-		public Thickness EllipseOffset { get; set; }
-
-		public double MaxSideLength { get; set; }
-	}
-}
-
 #endif
---
+++ src/Uno.UI/UI/Xaml/Controls/ProgressRing/ProgressRingTemplateSettings.wasm.cs
@@ -0,0 +1,15 @@
+﻿#if __WASM__
+
+namespace Windows.UI.Xaml.Controls.Primitives
+{//TODO: I'm not an expert in TemplateSettings logic so it's just a stub to make standard ProgressRing UWP style bindings work in Wasm
+	public partial class ProgressRingTemplateSettings : DependencyObject
+	{
+		public double EllipseDiameter { get; set; }
+
+		public Thickness EllipseOffset { get; set; }
+
+		public double MaxSideLength { get; set; }
+	}
+}
+
+#endif
