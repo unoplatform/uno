2b19ba6150b93b412cfe36c19b2e3c93471ea6fe
David Oliver <david.oliver@nventive.com>
2019-10-16 14:15:27 -0400

    test(styles): Add UI test for UseUWPDefaultStyles

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Button/Button_UseUWPDefaultStyles.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+#if __ANDROID__
+using Uno.UI;
+using _NativeType = Windows.UI.Xaml.Controls.BindableButtonEx;
+#elif __IOS__
+using UIKit;
+using _NativeType = Uno.UI.Views.Controls.BindableUIButton;
+#else
+using _NativeType = Windows.UI.Xaml.Controls.Grid; // We use a 'fake' native style on WASM
+#endif
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.Button
+{
+	[SampleControlInfo("Button", "Button_UseUWPDefaultStyles", description: "Demonstrates correct operation of UseUWPDefaultStyles flag.")]
+	public sealed partial class Button_UseUWPDefaultStyles : UserControl
+	{
+		public Button_UseUWPDefaultStyles()
+		{
+			this.InitializeComponent();
+			Loaded += Button_UseUWPDefaultStyles_Loaded;
+		}
+
+		private async void Button_UseUWPDefaultStyles_Loaded(object sender, RoutedEventArgs e)
+		{
+#if !NETFX_CORE
+			var originalFlagValue = Uno.UI.FeatureConfiguration.Style.UseUWPDefaultStyles;
+			Uno.UI.FeatureConfiguration.Style.UseUWPDefaultStyles = false;
+#endif
+
+			var button = new Windows.UI.Xaml.Controls.Button() { Content = "Native button" };
+			RootPanel.Children.Add(button);
+			await Task.Yield();
+			var nativeView = button.FindFirstChild<_NativeType>();
+			if (nativeView != null)
+			{
+				ResultsTextBlock.Text = "Native view found";
+			}
+			else
+			{
+				ResultsTextBlock.Text = "Native view not found";
+			}
+#if !NETFX_CORE
+			Uno.UI.FeatureConfiguration.Style.UseUWPDefaultStyles = originalFlagValue;
+#endif
+		}
+	}
+}
