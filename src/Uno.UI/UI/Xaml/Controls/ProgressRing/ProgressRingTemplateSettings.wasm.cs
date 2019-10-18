e97797bb80bb9fe5e8b2292e386c6f9284a08296
Arthur Gevorkian <gevart@gmail.com>
2019-10-18 17:55:13 +0300

    Update src/Uno.UI/UI/Xaml/Controls/ProgressRing/ProgressRingTemplateSettings.wasm.cs
    Co-Authored-By: David Oliver <david.john.oliver@gmail.com>

--- src/Uno.UI/UI/Xaml/Controls/ProgressRing/ProgressRingTemplateSettings.wasm.cs
+++ src/Uno.UI/UI/Xaml/Controls/ProgressRing/ProgressRingTemplateSettings.wasm.cs
@@ -1,7 +1,7 @@
 ﻿#if __WASM__
 
 namespace Windows.UI.Xaml.Controls.Primitives
-{//TODO: I'm not an expert in TemplateSettings logic so it's just a stub to make standard ProgressRing UWP style bindings work in Wasm
+{//TODO: Fully implement this class, matching UWP https://github.com/unoplatform/uno/issues/1872
 	public partial class ProgressRingTemplateSettings : DependencyObject
 	{
 		public double EllipseDiameter { get; set; }
