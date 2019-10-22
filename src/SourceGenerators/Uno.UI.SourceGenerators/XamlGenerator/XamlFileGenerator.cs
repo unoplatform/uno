8e9679773299d4d457220761e78fc7999a4331ab
David Oliver <david.oliver@nventive.com>
2019-10-21 21:42:24 -0400

    chore(styles): Support StaticResource alias in local dictionary

--- src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlFileGenerator.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlFileGenerator.cs
@@ -1975,6 +1975,10 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 					{
 						writer.AppendLineInvariant(directproperty);
 					}
+					else if (resource.Type.Name == "StaticResource")
+					{
+						BuildStaticResourceResourceKeyReference(writer, resource);
+					}
 					else
 					{
 						BuildChild(writer, null, resource);
