006b68501607bf979fc3535a022556d3e666aa6d
JP Levesque <jp.levesque@nventive.com>
2019-10-22 11:04:18 -0400

    Add UI Test

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ListView/ListView_Header_DataContextChanging.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls
+{
+	[SampleControlInfo("ListView", "ListView_Header_DataContextChanging")]
+	public sealed partial class ListView_Header_DataContextChanging : UserControl
+	{
+		public ListView_Header_DataContextChanging()
+		{
+			this.InitializeComponent();
+
+			// Delay to refresh the content
+			_ = Dispatcher.RunIdleAsync(
+				async _ =>
+				{
+					DataContext = "InitialDataContext";
+				}
+			);
+		}
+
+		public void OnChangedDataContext(object sender, object args)
+		{
+			DataContext = "UpdatedDataContext";
+		}
+
+		public void OnDataContextChanged(object sender, object args)
+		{
+			// On android text updates from the header are not propagated during
+			// DataContext changed ?
+			_ = Dispatcher.RunAsync(
+				CoreDispatcherPriority.Normal,
+				async () =>
+				{
+					var dobj = (FrameworkElement)sender;
+					var change = dobj.DataContext?.ToString() ?? "null";
+
+					MyTextBlock.Text += " " + change;
+				}
+			);
+		}
+	}
+}
