d349bd19fb486dc4f162fa1e0e716dd8b25d15d2
Jerome Laban <jerome.laban@nventive.com>
2019-10-24 14:28:08 -0400

    Enable caching for iOS build

--- build/ios-uitest-run.sh
+++ build/ios-uitest-run.sh
@@ -17,7 +17,7 @@ cd $BUILD_SOURCESDIRECTORY/build
 mono nuget/nuget.exe install NUnit.ConsoleRunner -Version 3.10.0
 
 export UNO_UITEST_PLATFORM=iOS
-export UNO_UITEST_IOSBUNDLE_PATH=$BUILD_SOURCESDIRECTORY/src/SamplesApp/SamplesApp.iOS/bin/iPhoneSimulator/Release/SampleApp.app
+export UNO_UITEST_IOSBUNDLE_PATH=$BUILD_SOURCESDIRECTORY/src/SamplesApp/SamplesApp.iOS/bin/iPhoneSimulator/Release/SamplesApp.app
 export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/ios
 
 mkdir -p $UNO_UITEST_SCREENSHOT_PATH
