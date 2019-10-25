667cceb201543e6e13d3ebaea3fc7d5610d9d968
David Oliver <david.oliver@nventive.com>
2019-10-25 14:30:48 -0400

    fix(layout): [iOS] Don't layout collapsed views
    This fixes freeze when loading DatePicker caused by infinite loop of Frame being set to zero and non-zero values. Can be repro'ed with DatePicker_SampleContent sample.

--- src/Uno.UI/UI/Xaml/FrameworkElement.iOS.cs
+++ src/Uno.UI/UI/Xaml/FrameworkElement.iOS.cs
@@ -29,6 +29,12 @@ namespace Windows.UI.Xaml
 
 		public override void LayoutSubviews()
 		{
+			if (Visibility == Visibility.Collapsed)
+			{
+				// //Don't layout collapsed views
+				return; 
+			}
+
 			try
 			{
 				try
