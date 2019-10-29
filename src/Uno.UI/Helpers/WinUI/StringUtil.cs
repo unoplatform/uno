1b029f47ed2d0bc5e21d63725b893d7c2f24dd6f
Jerome Laban <jerome.laban@nventive.com>
2019-10-28 21:03:58 -0400

    Import PersonPicture
    Source release/2.2 from WinUI (https://github.com/microsoft/microsoft-ui-xaml/commit/63c8ddb1714daa3040e25471343e658ee89adf53)

---
+++ src/Uno.UI/Helpers/WinUI/StringUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Uno.UI.Helpers.WinUI
+{
+	internal static class StringUtil
+	{
+		private static Regex _cppFormat = new Regex(@"\$(\d+)!.*?!", RegexOptions.Singleline);
+
+		internal static string FormatString(string format, params object[] parms)
+			=> string.Format(_cppFormat.Replace(format, "{$1}"), parms);
+	}
+}
---
+++ src/Uno.UI/UI/Xaml/Controls/PersonPicture/PersonPictureTemplateSettings.cs
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Uno.UI.Helpers.WinUI;
+using Windows.ApplicationModel.Contacts;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Automation;
+using Windows.UI.Xaml.Automation.Peers;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Shapes;
+
+namespace Windows.UI.Xaml.Controls
+{
+	public partial class PersonPictureTemplateSettings : DependencyObject
+	{
+		public ImageBrush ActualImageBrush
+		{
+			get { return (ImageBrush)GetValue(ActualImageBrushProperty); }
+			internal set { SetValue(ActualImageBrushProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for ActualImageBrush.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty ActualImageBrushProperty =
+			DependencyProperty.Register("ActualImageBrush", typeof(ImageBrush), typeof(PersonPictureTemplateSettings), new PropertyMetadata(null));
+
+
+
+		public string ActualInitials
+		{
+			get { return (string)GetValue(ActualInitialsProperty); }
+			internal set { SetValue(ActualInitialsProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for ActualInitials.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty ActualInitialsProperty =
+			DependencyProperty.Register("ActualInitials", typeof(string), typeof(PersonPictureTemplateSettings), new PropertyMetadata(""));
+	}
+}
