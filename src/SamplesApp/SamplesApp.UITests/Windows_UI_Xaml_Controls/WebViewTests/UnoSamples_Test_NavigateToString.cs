a1a163eeb21183182af901f54406a620a2a05de0
Piotr Karocki <pkar@ieee.org>
2019-10-31 12:35:53 +0100

    Update UnoSamples_Test_NavigateToString.cs

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/WebViewTests/UnoSamples_Test_NavigateToString.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/WebViewTests/UnoSamples_Test_NavigateToString.cs
@@ -18,7 +18,7 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.WebView
 		[AutoRetry]
 		public void WebView_NavigateToLongString()
 		{
-			Run("UITests.Shared.Windows_UI_Xaml_Controls.WebView.WebView_NavigateToString2");
+			Run("Uno.UI.Samples.Content.UITests.WebView.WebView_NavigateToString2");
 
 			var startButton = _app.Marked("startButton");
 			var clickResult = _app.Marked("WebView_NavigateToStringResult");
