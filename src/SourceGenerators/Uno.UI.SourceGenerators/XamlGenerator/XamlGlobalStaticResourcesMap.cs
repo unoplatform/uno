73065c77d8d54253952937d4d8c7206cda44b34f
David Oliver <david.oliver@nventive.com>
2019-10-31 11:45:08 -0400

    Fix ResourceDictionary.Source resolution error

--- src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlGlobalStaticResourcesMap.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlGlobalStaticResourcesMap.cs
@@ -105,19 +105,19 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 
 		private string GetAbsolutePath(string originDirectory, string relativeTargetPath)
 		{
-			var noRoot = Path.GetPathRoot(originDirectory).Length == 0;
-			if (noRoot)
+			var addedRootLength = 0;
+			if (Path.GetPathRoot(originDirectory).Length == 0)
 			{
+				var localRoot = Path.GetPathRoot(Directory.GetCurrentDirectory());
+				addedRootLength = localRoot.Length;
 				// Prepend a dummy root so that GetFullPath doesn't try to add the working directory. We remove it immediately afterward.
-				originDirectory = "C:\\" + originDirectory;
+				originDirectory = localRoot + originDirectory;
 			}
 			var absoluteTargetPath = Path.GetFullPath(
 					Path.Combine(originDirectory, relativeTargetPath)
 				);
-			if (noRoot)
-			{
-				absoluteTargetPath = absoluteTargetPath.Substring(3);
-			}
+
+			absoluteTargetPath = absoluteTargetPath.Substring(addedRootLength);
 
 			return absoluteTargetPath;
 		}
