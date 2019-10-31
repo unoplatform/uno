49acce696a8dbfbea482d02ae424e1d64128c07f
Carl de Billy <carl.debilly@nventive.com>
2019-10-31 16:31:08 -0400

    Fixed TextBox's "NaturalSize" sample

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/TextBoxTests/TextBoxTests.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/TextBoxTests/TextBoxTests.cs
@@ -18,7 +18,19 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.TextBoxTests
 			var sut = _app.Marked("textbox_sut").FirstResult().Rect;
 			var recth = _app.Marked("recth").FirstResult().Rect;
 
-			sut.Width.Should().Be(recth.Width);
+			sut.Width.Should().Be(recth.Width, "Invalid Width");
+		}
+
+		[Test]
+		[AutoRetry]
+		public void TextBox_NaturalSize_When_Empty_Is_Right_XPos()
+		{
+			Run("UITests.Shared.Windows_UI_Xaml_Controls.TextBoxes.TextBox_NaturalSize");
+
+			var sut = _app.Marked("textbox_sut").FirstResult().Rect;
+			var recth = _app.Marked("recth").FirstResult().Rect;
+
+			sut.X.Should().Be(recth.X, "Invalid X position");
 		}
 	}
 }
