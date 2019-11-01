4d08065d41f039e3054359df163d18d77da7c150
Jerome Laban <jerome.laban@nventive.com>
2019-11-01 16:57:54 -0400

    Adjust remote host to use the generated port export hostname

--- build/gitpod/build-wasm.sh
+++ build/gitpod/build-wasm.sh
@@ -2,6 +2,8 @@
 
 pushd src/SamplesApp/SamplesApp.Wasm
 
-msbuild /r /bl SamplesApp.Wasm.csproj /p:UnoSourceGeneratorUseGenerationHost=true /p:UnoSourceGeneratorUseGenerationController=false /p:UnoRemoteControlPort=443
+GITPOD_HOSTNAME=`echo $GITPOD_WORKSPACE_URL | sed -s 's/https:\/\///g'`
+
+msbuild /r /bl SamplesApp.Wasm.csproj /p:UnoSourceGeneratorUseGenerationHost=true /p:UnoSourceGeneratorUseGenerationController=false /p:UnoRemoteControlPort=443 "/p:UnoRemoteControlHost=https://53487-$GITPOD_HOSTNAME"
 
 popd
