a4012baf964e0c92b9753eb2917b18a4138480c1
David Oliver <david.oliver@nventive.com>
2019-11-04 22:17:29 -0500

    Support conditional XAML
    For now only IsApiContractPresent()/IsApiContractNotPresent() is supported.

---
+++ src/Uno.UI.Tests/Windows_UI_Xaml/Given_Conditional_Xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uno.UI.Tests.App.Xaml;
+
+namespace Uno.UI.Tests.Windows_UI_Xaml
+{
+	[TestClass]
+	public class Given_Conditional_Xaml
+	{
+		[TestMethod]
+		public void When_Conditional_IsApiContractPresent()
+		{
+			var page = new Test_Page();
+
+			var tb = page.TestConditionalTextBlock;
+			Assert.AreEqual("Contract 1", tb.Text);
+		}
+	}
+}
