cb518a5ea8d410c0e30ba300972276b3e87d4688
Jérôme Laban <jerome.laban@nventive.com>
2019-11-07 13:34:12 -0500

    Update build-wasm.sh
    Adjust for nuget_path location in prebuild

--- build/gitpod/build-wasm.sh
+++ build/gitpod/build-wasm.sh
@@ -2,6 +2,8 @@
 
 pushd src/SamplesApp/SamplesApp.Wasm
 
+export NUGET_PACKAGES=/workspace/.nuget
+
 GITPOD_HOSTNAME=`echo $GITPOD_WORKSPACE_URL | sed -s 's/https:\/\///g'`
 
 msbuild /r /bl SamplesApp.Wasm.csproj /p:UnoSourceGeneratorUseGenerationHost=true /p:UnoSourceGeneratorUseGenerationController=false /p:UnoRemoteControlPort=443 "/p:UnoRemoteControlHost=https://53487-$GITPOD_HOSTNAME"
