0ec42afa6747fdc6910c4e2080fad7a621a93953
Sébastien Goyette <sgoyette@isaac.ca>
2019-11-08 08:56:17 -0500

    Fixed file formatting

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Core/Given_CoreDispatcher.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Core/Given_CoreDispatcher.cs
@@ -10,54 +10,54 @@ using Windows.UI.Core;
 
 namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Core
 {
-  #if __ANDROID__ || __IOS__
-  [TestClass]
-  public class Given_CoreDispatcher
-  {
-	[TestMethod]
-	public async void When_RunAsync()
+#if __ANDROID__ || __IOS__
+	[TestClass]
+	public class Given_CoreDispatcher
 	{
-	  const string Success = "success";
-	  string result = string.Empty;
-	  try
-	  {
-		await CoreDispatcher.Main.RunAsync(async () => result = Success);
-	  }
-	  catch (Exception ex)
-	  {
-		result = "failure";
-	  }
-	  Assert.AreEqual(Success, result);
+		[TestMethod]
+		public async void When_RunAsync()
+		{
+			const string Success = "success";
+			string result = string.Empty;
+			try
+			{
+			  await CoreDispatcher.Main.RunAsync(async () => result = Success);
+			}
+			catch (Exception ex)
+			{
+			  result = "failure";
+			}
+			Assert.AreEqual(Success, result);
 
-	  result = string.Empty;
-	  try
-	  {
-		await CoreDispatcher.Main.RunAsync(CoreDispatcherPriority.Normal, () => result = Success);
-	  }
-	  catch (Exception ex)
-	  {
-		result = "failure";
-	  }
-	  Assert.AreEqual(Success, result);
+			result = string.Empty;
+			try
+			{
+			  await CoreDispatcher.Main.RunAsync(CoreDispatcherPriority.Normal, () => result = Success);
+			}
+			catch (Exception ex)
+			{
+			  result = "failure";
+			}
+			Assert.AreEqual(Success, result);
 
-	  result = string.Empty;
-	  try
-	  {
-		await Task.Run(async () => result = Success);
-	  }
-	  catch (Exception ex)
-	  {
-		result = "failure";
-	  }
-	  Assert.AreEqual(Success, result);
+			result = string.Empty;
+			try
+			{
+			  await Task.Run(async () => result = Success);
+			}
+			catch (Exception ex)
+			{
+			  result = "failure";
+			}
+			Assert.AreEqual(Success, result);
+		}
 	}
-  }
-  internal static class CoreDispatcherExtensions
-  {
-	internal static IAsyncAction RunAsync(this CoreDispatcher coreDispatcher, DispatchedHandler dispatchedHandler)
+	internal static class CoreDispatcherExtensions
 	{
-	  return coreDispatcher.RunAsync(CoreDispatcherPriority.Normal, dispatchedHandler);
+		internal static IAsyncAction RunAsync(this CoreDispatcher coreDispatcher, DispatchedHandler dispatchedHandler)
+		{
+			return coreDispatcher.RunAsync(CoreDispatcherPriority.Normal, dispatchedHandler);
+		}
 	}
-  }
 #endif
 }
