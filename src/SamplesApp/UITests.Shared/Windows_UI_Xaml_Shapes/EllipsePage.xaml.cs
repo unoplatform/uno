28dc70c8ef9d8f5e094d1493a886b86eb210a701
Carl de Billy <carl.debilly@nventive.com>
2019-11-19 11:45:57 -0500

    Improved the EllipsePage sample

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Shapes/EllipsePage.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Shapes/EllipsePage.xaml.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Uno.UI.Samples.Controls;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Uno.UI.Samples.Controls;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace SamplesApp.Windows_UI_Xaml_Shapes
 {
-	[SampleControlInfo("Shapes", "EllipsePage")]
+	[SampleControlInfo("Shapes")]
 	public sealed partial class EllipsePage : Page
 	{
 		public EllipsePage()
