64879da400eb18c3b60c898af3cc72bb8e08eb17
Dr.Rx <david.rey@nventive.com>
2019-11-21 15:38:02 -0500

    Add UI tests for the scrolling issue with shapes (SVG) on WASM

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Shapes/MeasurePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Windows_UI_Xaml_Shapes
+{
+	[SampleControlInfo("Shapes", "Measure")]
+	public sealed partial class MeasurePage : Page
+	{
+		public MeasurePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
