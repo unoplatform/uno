69fae346d63332a495698fb900924046cef2e340
Martin Zikmund <martinzikmund@live.com>
2019-11-23 13:27:56 +0100

    Updated generated classes

--- src/Uno.UWP/Generated/3.0.0.0/Windows.ApplicationModel.Email/EmailRecipient.cs
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.ApplicationModel.Email/EmailRecipient.cs
@@ -2,62 +2,18 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.ApplicationModel.Email
 {
-	#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+	#if false || false || false || false || false
 	[global::Uno.NotImplemented]
 	#endif
 	public  partial class EmailRecipient 
 	{
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public  string Name
-		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member string EmailRecipient.Name is not implemented in Uno.");
-			}
-			set
-			{
-				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.ApplicationModel.Email.EmailRecipient", "string EmailRecipient.Name");
-			}
-		}
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public  string Address
-		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member string EmailRecipient.Address is not implemented in Uno.");
-			}
-			set
-			{
-				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.ApplicationModel.Email.EmailRecipient", "string EmailRecipient.Address");
-			}
-		}
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public EmailRecipient( string address) 
-		{
-			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.ApplicationModel.Email.EmailRecipient", "EmailRecipient.EmailRecipient(string address)");
-		}
-		#endif
+		// Skipping already declared property Name
+		// Skipping already declared property Address
+		// Skipping already declared method Windows.ApplicationModel.Email.EmailRecipient.EmailRecipient(string)
 		// Forced skipping of method Windows.ApplicationModel.Email.EmailRecipient.EmailRecipient(string)
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public EmailRecipient( string address,  string name) 
-		{
-			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.ApplicationModel.Email.EmailRecipient", "EmailRecipient.EmailRecipient(string address, string name)");
-		}
-		#endif
+		// Skipping already declared method Windows.ApplicationModel.Email.EmailRecipient.EmailRecipient(string, string)
 		// Forced skipping of method Windows.ApplicationModel.Email.EmailRecipient.EmailRecipient(string, string)
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public EmailRecipient() 
-		{
-			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.ApplicationModel.Email.EmailRecipient", "EmailRecipient.EmailRecipient()");
-		}
-		#endif
+		// Skipping already declared method Windows.ApplicationModel.Email.EmailRecipient.EmailRecipient()
 		// Forced skipping of method Windows.ApplicationModel.Email.EmailRecipient.EmailRecipient()
 		// Forced skipping of method Windows.ApplicationModel.Email.EmailRecipient.Name.get
 		// Forced skipping of method Windows.ApplicationModel.Email.EmailRecipient.Name.set
