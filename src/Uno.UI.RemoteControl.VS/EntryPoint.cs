633c20be035d7585c81fc8847107b5d3208d4968
Jerome Laban <jerome.laban@nventive.com>
2019-11-29 14:22:05 -0500

    [VSIX] Fix Folder recursion to apply port properties

--- src/Uno.UI.RemoteControl.VS/EntryPoint.cs
+++ src/Uno.UI.RemoteControl.VS/EntryPoint.cs
@@ -120,11 +120,27 @@ namespace Uno.UI.RemoteControl.VS
 
 		private async Task BuildEvents_OnBuildProjConfigBeginAsync(string project, string projectConfig, string platform, string solutionConfig)
 		{
-			await StartServerAsync();
-
-			foreach(var p in await GetProjectsAsync())
+			try
 			{
-				SetGlobalProperty(p.FileName, RemoteControlServerPortProperty, RemoteControlServerPort.ToString(CultureInfo.InvariantCulture));
+				await StartServerAsync();
+
+				await UpdateProjectsAsync();
+			}
+			catch (Exception e)
+			{
+				_debugAction($"BuildEvents_OnBuildProjConfigBeginAsync failed: {e}");
+			}
+		}
+
+		private async Task UpdateProjectsAsync()
+		{
+			foreach (var p in await GetProjectsAsync())
+			{
+				if (GetMsbuildProject(p.FileName) is Microsoft.Build.Evaluation.Project msbProject && IsApplication(msbProject))
+				{
+					var portString = RemoteControlServerPort.ToString(CultureInfo.InvariantCulture);
+					SetGlobalProperty(p.FileName, RemoteControlServerPortProperty, portString);
+				}
 			}
 		}
 
@@ -236,11 +252,16 @@ namespace Uno.UI.RemoteControl.VS
 			}
 		}
 
-		private IEnumerable<EnvDTE.Project> EnumSubProjects(EnvDTE.Project solutionFolder)
+		private IEnumerable<EnvDTE.Project> EnumSubProjects(EnvDTE.Project folder)
 		{
-			if (solutionFolder.ProjectItems != null)
+			if (folder.ProjectItems != null)
 			{
-				foreach(var project in solutionFolder.ProjectItems.OfType<EnvDTE.Project>())
+				var subProjects = folder.ProjectItems
+					.OfType<EnvDTE.ProjectItem>()
+					.Select(p => p.Object)
+					.Cast<EnvDTE.Project>();
+
+				foreach (var project in subProjects)
 				{
 					if(project.Kind == FolderKind)
 					{
@@ -259,7 +280,7 @@ namespace Uno.UI.RemoteControl.VS
 
 		public void SetGlobalProperty(string projectFullName, string propertyName, string propertyValue)
 		{
-			var msbuildProject = ProjectCollection.GlobalProjectCollection.GetLoadedProjects(projectFullName).FirstOrDefault();
+			var msbuildProject = GetMsbuildProject(projectFullName);
 			if (msbuildProject == null)
 			{
 				_debugAction($"Failed to find project {projectFullName}, cannot provide listen port to the app.");
@@ -269,6 +290,9 @@ namespace Uno.UI.RemoteControl.VS
 				SetGlobalProperty(msbuildProject, propertyName, propertyValue);
 			}
 		}
+
+		private static Microsoft.Build.Evaluation.Project GetMsbuildProject(string projectFullName)
+			=> ProjectCollection.GlobalProjectCollection.GetLoadedProjects(projectFullName).FirstOrDefault();
 
 		public void SetGlobalProperties(string projectFullName, IDictionary<string, string> properties)
 		{
@@ -289,6 +313,21 @@ namespace Uno.UI.RemoteControl.VS
 		private void SetGlobalProperty(Microsoft.Build.Evaluation.Project msbuildProject, string propertyName, string propertyValue)
 		{
 			msbuildProject.SetGlobalProperty(propertyName, propertyValue);
+
+		}
+
+		private bool IsApplication(Microsoft.Build.Evaluation.Project project)
+		{
+			var isAndroidApp = project.GetPropertyValue("AndroidApplication")?.Equals("true", StringComparison.OrdinalIgnoreCase) ?? false;
+			var isiOSApp = project.GetPropertyValue("ProjectTypeGuids")?.Equals("{FEACFBD2-3405-455C-9665-78FE426C6842};{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}", StringComparison.OrdinalIgnoreCase) ?? false;
+			var ismacOSApp = project.GetPropertyValue("ProjectTypeGuids")?.Equals("{A3F8F2AB-B479-4A4A-A458-A89E7DC349F1};{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}", StringComparison.OrdinalIgnoreCase) ?? false;
+			var isExe = project.GetPropertyValue("OutputType")?.Equals("Exe", StringComparison.OrdinalIgnoreCase) ?? false;
+			var isWasm = project.GetPropertyValue("WasmHead")?.Equals("true", StringComparison.OrdinalIgnoreCase) ?? false;
+
+			return isAndroidApp
+				|| (isiOSApp && isExe)
+				|| (ismacOSApp && isExe)
+				|| isWasm;
 		}
 	}
 }
