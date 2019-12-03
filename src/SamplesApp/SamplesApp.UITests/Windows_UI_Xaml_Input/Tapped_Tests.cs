33c8c194bb4532b3d54309cfba99d275d43d708c
Dr.Rx <david.rey@nventive.com>
2019-12-03 12:41:21 -0500

    Automate test for tapped when a child control handles all pointer events

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Input/Tapped_Tests.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Input/Tapped_Tests.cs
@@ -14,23 +14,37 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Input
 	public class Tapped_Tests : SampleControlUITestBase
 	{
 		[Test]
-		public void TappedLocationTest()
+		public void When_Tapped_Then_ArgsLocationIsValid()
 		{
 			Run("UITests.Shared.Windows_UI_Input.GestureRecognizerTests.TappedTest");
 
-			var root = _app.WaitForElement("LocationTestRoot").Single();
-			var target = _app.WaitForElement("LocationTestTarget").Single();
+			var root = _app.WaitForElement("WhenTappedThenArgsLocationIsValid_Root").Single();
+			var target = _app.WaitForElement("WhenTappedThenArgsLocationIsValid_Target").Single();
 
-			int tapX = 10, tapY = 10;
+			const int tapX = 10, tapY = 10;
 			(int x, int y) targetToRoot = ((int)(target.Rect.X - root.Rect.X), (int)(target.Rect.Y - root.Rect.Y));
 
 			_app.TapCoordinates(target.Rect.X + tapX, target.Rect.Y + tapY);
 
-			var relativeToRoot = _app.Marked("LocationTestRelativeToRootLocation").GetDependencyPropertyValue<string>("Text");
-			var relativeToTarget = _app.Marked("LocationTestRelativeToTargetLocation").GetDependencyPropertyValue<string>("Text");
+			var relativeToRoot = _app.Marked("WhenTappedThenArgsLocationIsValid_Result_RelativeToRoot").GetDependencyPropertyValue<string>("Text");
+			var relativeToTarget = _app.Marked("WhenTappedThenArgsLocationIsValid_Result_RelativeToTarget").GetDependencyPropertyValue<string>("Text");
 
 			relativeToTarget.Should().Be($"({tapX:D},{tapY:D})");
 			relativeToRoot.Should().Be($"({tapX+targetToRoot.x:D},{tapY + targetToRoot.y:D})");
+		}
+
+		[Test]
+		public void When_ChildHandlesPointers()
+		{
+			Run("UITests.Shared.Windows_UI_Input.GestureRecognizerTests.TappedTest");
+
+			var target = _app.WaitForElement("WhenChildHandlesPointers_Target").Single();
+
+			const int tapX = 10, tapY = 10;
+			_app.TapCoordinates(target.Rect.X + tapX, target.Rect.Y + tapY);
+
+			var result = _app.Marked("WhenChildHandlesPointers_Result").GetDependencyPropertyValue<string>("Text");
+			result.Should().Be("Tapped");
 		}
 	}
 }
