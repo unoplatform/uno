72d285ba25d25fc0c10ed3e0825bfb73ea64d847
Dr.Rx <david.rey@nventive.com>
2019-12-09 10:29:35 -0500

    Add support of PhysicialToLogicaPixels for UWP in UI tests

---
+++ src/SamplesApp/UITests.Shared/Helpers/UWPViewHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Foundation;
+
+namespace Uno.UI
+{
+	public static class UWPViewHelper
+	{
+		public static Point LogicalToPhysicalPixels(this Point point)
+#if XAMARIN
+			=> Uno.UI.ViewHelper.LogicalToPhysicalPixels(point);
+#else
+			=> point;
+#endif
+	}
+}
