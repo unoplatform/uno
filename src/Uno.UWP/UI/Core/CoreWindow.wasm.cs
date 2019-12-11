46e11c0bdccb8bc656c06f4846b8a70dc39fb9a1
Carl de Billy <carl.debilly@nventive.com>
2019-12-11 10:05:10 -0500

    Clean-up refactoring

--- src/Uno.UWP/UI/Core/CoreWindow.wasm.cs
+++ src/Uno.UWP/UI/Core/CoreWindow.wasm.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Runtime.InteropServices;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Foundation.Metadata;
 using Uno.Foundation;
 
 namespace Windows.UI.Core
 {
-	public partial class CoreWindow 
+	public partial class CoreWindow
 	{
-		private CoreCursor _pointerCursor;
+		private CoreCursor _pointerCursor = new CoreCursor(CoreCursorType.Arrow, 0);
 
 		public global::Windows.UI.Core.CoreCursor PointerCursor
 		{
@@ -24,59 +19,14 @@ namespace Windows.UI.Core
 
 		private void Internal_SetPointerCursor()
 		{
-			string cssCoreCursorType = GetCssCursor(_pointerCursor.Type);
-			WebAssemblyRuntime.InvokeJS($"Uno.UI.WindowManager.current.setCursor(\"{cssCoreCursorType}\");");
-		}
-
-		string GetCssCursor(CoreCursorType coreCursorType)
-		{
-			//best matches between 
-			// https://docs.microsoft.com/id-id/uwp/api/windows.ui.core.corecursortype
-			//and 
-			//https://developer.mozilla.org/fr/docs/Web/CSS/cursor
-			// no support for Custom Cursor for now
-
-			switch (coreCursorType)
+			var command = string.Concat(new[]
 			{
-				case CoreCursorType.Custom:
-				case CoreCursorType.Arrow:
-					return "auto";
-				case CoreCursorType.Cross:
-					return "crosshair";
-				case CoreCursorType.Hand:
-					return "pointer";
-				case CoreCursorType.Help:
-					return "help";
-				case CoreCursorType.IBeam:
-					return "text";
-				case CoreCursorType.SizeAll:
-					return "move";
-				case CoreCursorType.SizeNortheastSouthwest:
-					return "nesw-resize";
-				case CoreCursorType.SizeNorthSouth:
-					return "ns-resize";
-				case CoreCursorType.SizeNorthwestSoutheast:
-					return "nwse-resize";
-				case CoreCursorType.SizeWestEast:
-					return "ew-resize";
-				case CoreCursorType.UniversalNo:
-					return "not-allowed";
-				case CoreCursorType.UpArrow:
-					return "n-resize";
-				case CoreCursorType.Wait:
-					return "wait";
+				"Uno.UI.WindowManager.current.setCursor(\"",
+				_pointerCursor.Type.ToCssCursor(),
+				"\");"
+			});
 
-				case CoreCursorType.Pin:
-				case CoreCursorType.Person:
-					return "pointer";
-				default:
-					return "auto";
-			}
-		}
-		[global::Uno.NotImplemented]
-		public global::Windows.UI.Core.CoreVirtualKeyStates GetKeyState(global::Windows.System.VirtualKey virtualKey)
-		{
-			return CoreVirtualKeyStates.None;
+			WebAssemblyRuntime.InvokeJS(command);
 		}
 	}
 }
