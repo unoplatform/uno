2c1c6e66bd232babbfac5602d8f7b3951a4322ac
Carl de Billy <carl.debilly@nventive.com>
2019-12-13 13:02:58 -0500

    New sample for image alignment

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ImageTests/Image_Stretch_Alignment_Smaller.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ImageTests/Image_Stretch_Alignment_Smaller.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
 
 namespace Uno.UI.Samples.UITests.Image
 {
-	[Controls.SampleControlInfo("Image")]
-	public sealed partial class Image_Stretch_Alignment_Smaller : UserControl
+	[SampleControlInfo("Image")]
+	public sealed partial class Image_Stretch_Alignment_Smaller : Page
 	{
 		public Image_Stretch_Alignment_Smaller()
 		{
