2bf2ed7d0a313c843730bdfbffcd98a206533bae
David Oliver <david.oliver@nventive.com>
2019-12-18 15:14:05 -0500

    [WASM] Fix breaking binary TimePicker changes

--- src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Controls/TimePicker.cs
+++ src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Controls/TimePicker.cs
@@ -64,6 +64,20 @@ namespace Windows.UI.Xaml.Controls
 		}
 		#endif
 		// Skipping already declared property ClockIdentifier
+		#if false || false || NET461 || __WASM__ || false
+		[global::Uno.NotImplemented]
+		public  global::Windows.UI.Xaml.Controls.LightDismissOverlayMode LightDismissOverlayMode
+		{
+			get
+			{
+				return (global::Windows.UI.Xaml.Controls.LightDismissOverlayMode)this.GetValue(LightDismissOverlayModeProperty);
+			}
+			set
+			{
+				this.SetValue(LightDismissOverlayModeProperty, value);
+			}
+		}
+		#endif
 		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
 		[global::Uno.NotImplemented]
 		public  global::System.TimeSpan? SelectedTime
@@ -110,6 +124,14 @@ namespace Windows.UI.Xaml.Controls
 			"Time", typeof(global::System.TimeSpan), 
 			typeof(global::Windows.UI.Xaml.Controls.TimePicker), 
 			new FrameworkPropertyMetadata(default(global::System.TimeSpan)));
+		#endif
+		#if false || false || NET461 || __WASM__ || false
+		[global::Uno.NotImplemented]
+		public static global::Windows.UI.Xaml.DependencyProperty LightDismissOverlayModeProperty { get; } = 
+		Windows.UI.Xaml.DependencyProperty.Register(
+			"LightDismissOverlayMode", typeof(global::Windows.UI.Xaml.Controls.LightDismissOverlayMode), 
+			typeof(global::Windows.UI.Xaml.Controls.TimePicker), 
+			new FrameworkPropertyMetadata(default(global::Windows.UI.Xaml.Controls.LightDismissOverlayMode)));
 		#endif
 		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
 		[global::Uno.NotImplemented]
