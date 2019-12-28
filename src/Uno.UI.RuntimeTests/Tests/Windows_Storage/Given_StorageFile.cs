190c64c22d917d96ccb80d6ff090b73a5705a77b
Piotr Karocki <pkar@ieee.org>
2019-12-28 14:16:28 +0100

    test for deleting file
    Maybe it should be some wait added...

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_Storage/Given_StorageFile.cs
@@ -0,0 +1,45 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+using Windows.ApplicationModel.Resources;
+using Windows.ApplicationModel.Resources.Core;
+
+namespace Uno.UI.RuntimeTests.Tests
+{
+	[TestClass]
+	public class Given_StorageFile
+	{
+		[TestInitialize]
+		public void Init()
+		{
+		}
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+		}
+
+		[TestMethod]
+		public async void When_DeleteFile()
+		{
+      var folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+      Assert.IsNotNull(folder, "cannot get LocalFolder - error outside tested method");
+
+      var file = await folder.CreateFileAsync("test-deletingfile.txt", CreationCollisionOption.FailIfExists);
+      Assert.IsNotNull(file, "cannot create file - error outside tested method");
+
+      // try delete file
+      await file.DeleteAsync();
+      
+      // check if method works
+      var fileAfter = await folder.TryGetItemAsync("test-deletingfile.txt");
+      Assert.IsNull(file, "file is not deleted - tested method fails");
+      
+		}
+
+
+	}
+}
