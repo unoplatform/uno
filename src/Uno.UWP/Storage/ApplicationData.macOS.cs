e64de679ab0c0822e72ea5da5049340ce77b3c5c
Martin Zikmund <martin@zikmund.dev>
2020-01-01 14:32:07 +0100

    Fixed macOS application data path (and for sandbox), added NSUserDefaults support

--- src/Uno.UWP/Storage/ApplicationData.macOS.cs
+++ src/Uno.UWP/Storage/ApplicationData.macOS.cs
@@ -7,6 +7,8 @@ namespace Windows.Storage
 {
 	partial class ApplicationData 
 	{
+		private const string SandboxContainerIdKey = "APP_SANDBOX_CONTAINER_ID";
+
 		private static string GetLocalCacheFolder()
 		{
 			var url = NSFileManager.DefaultManager.GetUrls(NSSearchPathDirectory.CachesDirectory, NSSearchPathDomain.User)[0];
@@ -18,14 +20,29 @@ namespace Windows.Storage
 
 		private static string GetLocalFolder()
 		{
-			var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-			var applicationDataPath = Path.GetFullPath(
+			var homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+			string applicationDataPath;
+			if (IsAppSandboxed())
+			{
+				// In a sandbox, the home directory refers to the app's root directory
+				applicationDataPath = Path.GetFullPath(
 				Path.Combine(
-					documents,
+					homeDirectory,
+					"Data",
+					"Application Support",
+					"Local"));
+			}
+			else
+			{
+				applicationDataPath = Path.GetFullPath(
+				Path.Combine(
+					homeDirectory,
 					"Library",
 					"Application Support",
 					NSBundle.MainBundle.BundleIdentifier,
 					"Local"));
+			}
+			
 			// Ensure LocalFolder directory exists
 			Directory.CreateDirectory(applicationDataPath);
 			return applicationDataPath;
@@ -37,6 +54,11 @@ namespace Windows.Storage
 		private static string GetSharedLocalFolder()
 			=> Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
 
+		private static bool IsAppSandboxed()
+		{
+			var environment = NSProcessInfo.ProcessInfo.Environment;
+			return environment[SandboxContainerIdKey] != null;
+		}
 	}
 }
 #endif
