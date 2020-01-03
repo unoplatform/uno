1290962309199101d6144a1e2cd46739cc4fa01f
Piotr Karocki <pkar@ieee.org>
2020-01-03 10:00:30 +0100

    Update Given_StorageFile.cs
    Catching also exceptions, not only incorrect behaviour of tested method.

--- src/Uno.UI.RuntimeTests/Tests/Windows_Storage/Given_StorageFile.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_Storage/Given_StorageFile.cs
@@ -12,9 +12,13 @@ namespace Uno.UI.RuntimeTests.Tests
 	[TestClass]
 	public class Given_StorageFile
 	{
+		
+		String _filename;
+		
 		[TestInitialize]
 		public void Init()
 		{
+			_filename = DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt";
 		}
 
 		[TestCleanup]
@@ -25,18 +29,33 @@ namespace Uno.UI.RuntimeTests.Tests
 		[TestMethod]
 		public async void When_DeleteFile()
 		{
-		  var folder = Windows.Storage.ApplicationData.Current.LocalFolder;
-		  Assert.IsNotNull(folder, "cannot get LocalFolder - error outside tested method");
+			
+			var folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+			Assert.IsNotNull(folder, "cannot get LocalFolder - error outside tested method");
 
-		  var file = await folder.CreateFileAsync("test-deletingfile.txt", Windows.Storage.CreationCollisionOption.FailIfExists);
-		  Assert.IsNotNull(file, "cannot create file - error outside tested method");
+			try
+			{
+				var file = await folder.CreateFileAsync( _filename, Windows.Storage.CreationCollisionOption.FailIfExists);
+				Assert.IsNotNull(file, "cannot create file - error outside tested method");
+			}
+			catch
+			{
+			  Assert.IsNotNull(null, "CreateFile exception - error outside tested method");
+			}
 
 		  // try delete file
-		  await file.DeleteAsync();
+			try
+			{
+		  		await file.DeleteAsync();
+			}
+			catch
+			{
+				Assert.IsNotNull(null, "DeleteAsync exception - error in tested method");
+			}
 
-		  // check if method works
-		  var fileAfter = await folder.TryGetItemAsync("test-deletingfile.txt");
-		  Assert.IsNull(file, "file is not deleted - tested method fails");
+			// check if method works
+			var fileAfter = await folder.TryGetItemAsync("test-deletingfile.txt");
+			Assert.IsNull(file, "file is not deleted - tested method fails");
       
 		}
 
