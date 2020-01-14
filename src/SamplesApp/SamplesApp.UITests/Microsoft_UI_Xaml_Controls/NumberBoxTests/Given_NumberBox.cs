12f5ce4a1081a0af57a7cbcac4a3aa8831a51489
Jerome Laban <jerome.laban@nventive.com>
2020-01-14 11:05:24 -0500

    Adjust indentation

--- src/SamplesApp/SamplesApp.UITests/Microsoft_UI_Xaml_Controls/NumberBoxTests/Given_NumberBox.cs
+++ src/SamplesApp/SamplesApp.UITests/Microsoft_UI_Xaml_Controls/NumberBoxTests/Given_NumberBox.cs
@@ -185,50 +185,51 @@ namespace SamplesApp.UITests.Microsoft_UI_Xaml_Controls.NumberBoxTests
 			const double resetValue = double.NaN;
 
 			Dictionary<string, double> expressions = new Dictionary<string, double>
-				{
-                    // Valid expressions. None of these should evaluate to the reset value.
-                    { "5", 5 },
-					{ "-358", -358 },
-					{ "12.34", 12.34 },
-					{ "5 + 3", 8 },
-					{ "12345 + 67 + 890", 13302 },
-					{ "000 + 0011", 11 },
-					{ "5 - 3 + 2", 4 },
-					{ "3 + 2 - 5", 0 },
-					{ "9 - 2 * 6 / 4", 6 },
-					{ "9 - -7",  16 },
-					{ "9-3*2", 3 },         // no spaces
-                    { " 10  *   6  ", 60 }, // extra spaces
-                    { "10 /( 2 + 3 )", 2 },
-					{ "5 * -40", -200 },
-					{ "(1 - 4) / (2 + 1)", -1 },
-					{ "3 * ((4 + 8) / 2)", 18 },
-					{ "23 * ((0 - 48) / 8)", -138 },
-					{ "((74-71)*2)^3", 216 },
-					{ "2 - 2 ^ 3", -6 },
-					{ "2 ^ 2 ^ 2 / 2 + 9", 17 },
-					{ "5 ^ -2", 0.04 },
-					{ "5.09 + 14.333", 19.423 },
-					{ "2.5 * 0.35", 0.875 },
-					{ "-2 - 5", -7 },       // begins with negative number
-                    { "(10)", 10 },         // number in parens
-                    { "(-9)", -9 },         // negative number in parens
-                    { "0^0", 1 },           // who knew?
+			{ 
+               // Valid expressions. None of these should evaluate to the reset value.
+				{ "5", 5 },
+				{ "-358", -358 },
+				{ "12.34", 12.34 },
+				{ "5 + 3", 8 },
+				{ "12345 + 67 + 890", 13302 },
+				{ "000 + 0011", 11 },
+				{ "5 - 3 + 2", 4 },
+				{ "3 + 2 - 5", 0 },
+				{ "9 - 2 * 6 / 4", 6 },
+				{ "9 - -7",  16 },
+				{ "9-3*2", 3 },         // no spaces
+				{ " 10  *   6  ", 60 }, // extra spaces
+				{ "10 /( 2 + 3 )", 2 },
+				{ "5 * -40", -200 },
+				{ "(1 - 4) / (2 + 1)", -1 },
+				{ "3 * ((4 + 8) / 2)", 18 },
+				{ "23 * ((0 - 48) / 8)", -138 },
+				{ "((74-71)*2)^3", 216 },
+				{ "2 - 2 ^ 3", -6 },
+				{ "2 ^ 2 ^ 2 / 2 + 9", 17 },
+				{ "5 ^ -2", 0.04 },
+				{ "5.09 + 14.333", 19.423 },
+				{ "2.5 * 0.35", 0.875 },
+				{ "-2 - 5", -7 },       // begins with negative number
+				{ "(10)", 10 },         // number in parens
+				{ "(-9)", -9 },         // negative number in parens
+				{ "0^0", 1 },           // who knew?
 
-                    // These should not parse, which means they will reset back to the previous value.
-                    { "5x + 3y", resetValue },        // invalid chars
-                    { "5 + (3", resetValue },         // mismatched parens
-                    { "9 + (2 + 3))", resetValue },
-					{ "(2 + 3)(1 + 5)", resetValue }, // missing operator
-                    { "9 + + 7", resetValue },        // extra operators
-                    { "9 - * 7",  resetValue },
-					{ "9 - - 7",  resetValue },
-					{ "+9", resetValue },
-					{ "1 / 0", resetValue },          // divide by zero
+				// These should not parse, which means they will reset back to the previous value.
+				{ "5x + 3y", resetValue },        // invalid chars
+				{ "5 + (3", resetValue },         // mismatched parens
+				{ "9 + (2 + 3))", resetValue },
+				{ "(2 + 3)(1 + 5)", resetValue }, // missing operator
+				{ "9 + + 7", resetValue },        // extra operators
+				{ "9 - * 7",  resetValue },
+				{ "9 - - 7",  resetValue },
+				{ "+9", resetValue },
+				{ "1 / 0", resetValue },          // divide by zero
 
-                    // These don't currently work, but maybe should.
-                    { "-(3 + 5)", resetValue }, // negative sign in front of parens -- should be -8
-                };
+				// These don't currently work, but maybe should.
+				{ "-(3 + 5)", resetValue }, // negative sign in front of parens -- should be -8
+			};
+
 			foreach (KeyValuePair<string, double> pair in expressions)
 			{
 				numBox.ClearText();
