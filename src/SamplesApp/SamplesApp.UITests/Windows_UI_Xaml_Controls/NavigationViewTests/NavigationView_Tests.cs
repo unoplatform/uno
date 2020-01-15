cf1cc4fc92b0f0bf4c899b355461dc5fe67051f2
Jerome Laban <jerome.laban@nventive.com>
2020-01-15 07:42:01 -0500

    Adjust NavigationView menu item lookup for android

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/NavigationViewTests/NavigationView_Tests.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/NavigationViewTests/NavigationView_Tests.cs
@@ -18,14 +18,18 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.NavigationViewTests
 		{
 			Run("SamplesApp.Samples.Windows_UI_Xaml_Controls.NavigationViewTests.NavigationView_Pane_Automated");
 
-			var itemPlay = _app.Marked("Item Play").Descendant().Marked("IconBox");
-			var itemSave = _app.Marked("Item Save").Descendant().Marked("IconBox");
+			var itemPlay = _app.Marked("Item Play");
+			var itemSave = _app.Marked("Item Save");
 			var selectedItemText = _app.Marked("selectedItemText");
 
-			_app.Tap(itemPlay);
+			// Work around for Xamarin.Android not showing the icon (VS2017 only)
+			var itemPlayRect = _app.Query(itemPlay).First().Rect;
+			var itemSaveRect = _app.Query(itemSave).First().Rect;
+
+			_app.TapCoordinates(itemPlayRect.X + 5, itemPlayRect.Y + 5);
 			_app.WaitForDependencyPropertyValue(selectedItemText, "Text", "Play");
 
-			_app.Tap(itemSave);
+			_app.TapCoordinates(itemSaveRect.X + 5, itemSaveRect.Y + 5);
 			_app.WaitForDependencyPropertyValue(selectedItemText, "Text", "Save");
 		}
 	}
