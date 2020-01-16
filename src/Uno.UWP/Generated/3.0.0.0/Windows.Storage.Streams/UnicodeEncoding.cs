1d969f887b1273e8a07e24c807c2fd0aefa5bc90
Piotr Karocki <pkar@ieee.org>
2020-01-16 08:55:24 +0100

    Removing unimplemented from encoding

--- src/Uno.UWP/Generated/3.0.0.0/Windows.Storage.Streams/UnicodeEncoding.cs
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.Storage.Streams/UnicodeEncoding.cs
@@ -2,19 +2,19 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.Storage.Streams
 {
-	#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-	#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+	#if false || false || false || false || false
+	#if false || false || false || false || false
 	[global::Uno.NotImplemented]
 	#endif
 	public   enum UnicodeEncoding 
 	{
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		#if false || false || false || false || false
 		Utf8,
 		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		#if false || false || false || false || false
 		Utf16LE,
 		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
+		#if false || false || false || false || false
 		Utf16BE,
 		#endif
 	}
