3e4ce7cdc0fed821270f2abbbfa8cac20adcdf15
Piotr Karocki <pkar@ieee.org>
2020-01-17 20:39:04 +0100

    Update FileIO.cs

--- src/Uno.UWP/Storage/FileIO.cs
+++ src/Uno.UWP/Storage/FileIO.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using System.Globalization;
 using System.IO;
 using System.Threading;
@@ -21,17 +22,17 @@ namespace Windows.Storage
 				throw new NullReferenceException("StorageFile cannot be null");
 			}
 
-			Text.Encoding encodingForWriter = Text.Encoding.UTF8;
+			Encoding encodingForWriter = Encoding.UTF8;
 			switch (encoding)
 			{
 				case Streams.UnicodeEncoding.Utf8:
-					encodingForWriter = Text.Encoding.UTF8;
+					encodingForWriter = Encoding.UTF8;
 					break;
 				case Streams.UnicodeEncoding.Utf16LE:
-					encodingForWriter = Text.Encoding.Unicode;
+					encodingForWriter = Encoding.Unicode;
 					break;
 				case Streams.UnicodeEncoding.Utf16BE:
-					encodingForWriter = Text.Encoding.BigEndianUnicode;
+					encodingForWriter = Encoding.BigEndianUnicode;
 					break;
 			}
 
@@ -42,18 +43,18 @@ namespace Windows.Storage
 					fileStream.Seek(0, SeekOrigin.End);
 				}
 
-				using (StreamWriter streamWriter = new StreamWriter(fileStream,encodingForWriter))
+				using (StreamWriter streamWriter = new StreamWriter(fileStream, encodingForWriter))
 				{
 					await streamWriter.WriteAsync(contents);
 				}
 			}
 		}
-		
-		public static IAsyncAction AppendTextAsync( IStorageFile file,  string contents,  Streams.UnicodeEncoding encoding) => AppendWriteTextAsync(file, contents, encoding, true) as IAsyncAction;
 
-		public static IAsyncAction WriteTextAsync( IStorageFile file,  string contents,  Streams.UnicodeEncoding encoding) => AppendWriteTextAsync(file, contents, encoding, false) as IAsyncAction;
+		public static IAsyncAction AppendTextAsync(IStorageFile file, string contents, Streams.UnicodeEncoding encoding) => AppendWriteTextAsync(file, contents, encoding, true) as IAsyncAction;
 
-		private static IAsyncAction AppendWriteTextAsync( IStorageFile file,  string contents, bool append)
+		public static IAsyncAction WriteTextAsync(IStorageFile file, string contents, Streams.UnicodeEncoding encoding) => AppendWriteTextAsync(file, contents, encoding, false) as IAsyncAction;
+
+		private static async Task AppendWriteTextAsync(IStorageFile file, string contents, bool append)
 		{
 			Streams.UnicodeEncoding unicodeEncoding = Streams.UnicodeEncoding.Utf8;
 
@@ -76,17 +77,17 @@ namespace Windows.Storage
 					}
 					catch
 					{
-					   // probably file is empty
+						// probably file is empty
 					}
-					
+
 				}
 			}
 
-			AppendWriteTextAsync( file,  contents, unicodeEncoding, append);
+			AppendWriteTextAsync(file, contents, unicodeEncoding, append);
 		}
-	
-		public static IAsyncAction WriteTextAsync( IStorageFile file,  string contents) => AppendWriteTextAsync(file, contents, false);
 
-		public static IAsyncAction AppendTextAsync( IStorageFile file,  string contents) => AppendWriteTextAsync(file, contents, true);
+		public static IAsyncAction WriteTextAsync(IStorageFile file, string contents) => AppendWriteTextAsync(file, contents, false) as IAsyncAction;
+
+		public static IAsyncAction AppendTextAsync(IStorageFile file, string contents) => AppendWriteTextAsync(file, contents, true) as IAsyncAction;
 	}
 }
