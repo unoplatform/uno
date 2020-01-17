29ee0c64fb5fca4d0f4d618bc1a6671d68bf1110
Piotr Karocki <pkar@ieee.org>
2020-01-17 14:53:21 +0100

    chore: Removing global::
    I left one level up from Encoding, as we have here two different encodings - one from Streams, and second from Text.

--- src/Uno.UWP/Storage/FileIO.cs
+++ src/Uno.UWP/Storage/FileIO.cs
@@ -7,30 +7,31 @@ using Uno.Extensions;
 using Windows.Foundation;
 using Windows.Storage.FileProperties;
 using Windows.Storage.Streams;
+using Windows.Storage;
 
 namespace Windows.Storage
 {
 	public partial class FileIO
 	{
 
-		private static async Task AppendWriteTextAsync(global::Windows.Storage.IStorageFile file, string contents, global::Windows.Storage.Streams.UnicodeEncoding encoding, bool append)
+		private static async Task AppendWriteTextAsync(IStorageFile file, string contents, Streams.UnicodeEncoding encoding, bool append)
 		{
 			if (file is null)
 			{
 				throw new NullReferenceException("StorageFile cannot be null");
 			}
 
-			global::System.Text.Encoding encodingForWriter = global::System.Text.Encoding.UTF8;
+			Text.Encoding encodingForWriter = Text.Encoding.UTF8;
 			switch (encoding)
 			{
-				case global::Windows.Storage.Streams.UnicodeEncoding.Utf8:
-					encodingForWriter = global::System.Text.Encoding.UTF8;
+				case Streams.UnicodeEncoding.Utf8:
+					encodingForWriter = Text.Encoding.UTF8;
 					break;
-				case global::Windows.Storage.Streams.UnicodeEncoding.Utf16LE:
-					encodingForWriter = global::System.Text.Encoding.Unicode;
+				case Streams.UnicodeEncoding.Utf16LE:
+					encodingForWriter = Text.Encoding.Unicode;
 					break;
-				case global::Windows.Storage.Streams.UnicodeEncoding.Utf16BE:
-					encodingForWriter = global::System.Text.Encoding.BigEndianUnicode;
+				case Streams.UnicodeEncoding.Utf16BE:
+					encodingForWriter = Text.Encoding.BigEndianUnicode;
 					break;
 			}
 
@@ -48,13 +49,13 @@ namespace Windows.Storage
 			}
 		}
 		
-		public static global::Windows.Foundation.IAsyncAction AppendTextAsync( global::Windows.Storage.IStorageFile file,  string contents,  global::Windows.Storage.Streams.UnicodeEncoding encoding) => AppendWriteTextAsync(file, contents, encoding, true) as global::Windows.Foundation.IAsyncAction;
+		public static IAsyncAction AppendTextAsync( IStorageFile file,  string contents,  Streams.UnicodeEncoding encoding) => AppendWriteTextAsync(file, contents, encoding, true) as IAsyncAction;
 
-		public static global::Windows.Foundation.IAsyncAction WriteTextAsync( global::Windows.Storage.IStorageFile file,  string contents,  global::Windows.Storage.Streams.UnicodeEncoding encoding) => AppendWriteTextAsync(file, contents, encoding, false) as global::Windows.Foundation.IAsyncAction;
+		public static IAsyncAction WriteTextAsync( IStorageFile file,  string contents,  Streams.UnicodeEncoding encoding) => AppendWriteTextAsync(file, contents, encoding, false) as IAsyncAction;
 
-		private static global::Windows.Foundation.IAsyncAction AppendWriteTextAsync( global::Windows.Storage.IStorageFile file,  string contents, bool append)
+		private static IAsyncAction AppendWriteTextAsync( IStorageFile file,  string contents, bool append)
 		{
-			global::Windows.Storage.Streams.UnicodeEncoding unicodeEncoding = global::Windows.Storage.Streams.UnicodeEncoding.Utf8;
+			Streams.UnicodeEncoding unicodeEncoding = Streams.UnicodeEncoding.Utf8;
 
 			using (Stream fileReadStream = await file.OpenStreamForReadAsync())
 			{
@@ -66,10 +67,10 @@ namespace Windows.Storage
 						switch (firstByte)
 						{
 							case 0xfe:
-								unicodeEncoding = global::Windows.Storage.Streams.UnicodeEncoding.Utf16BE;
+								unicodeEncoding = Streams.UnicodeEncoding.Utf16BE;
 								break;
 							case 0xff:
-								unicodeEncoding = global::Windows.Storage.Streams.UnicodeEncoding.Utf16LE;
+								unicodeEncoding = Streams.UnicodeEncoding.Utf16LE;
 								break;
 						}
 					}
@@ -84,8 +85,8 @@ namespace Windows.Storage
 			AppendWriteTextAsync( file,  contents, unicodeEncoding, append);
 		}
 	
-		public static global::Windows.Foundation.IAsyncAction WriteTextAsync( global::Windows.Storage.IStorageFile file,  string contents) => AppendWriteTextAsync(file, contents, false);
+		public static IAsyncAction WriteTextAsync( IStorageFile file,  string contents) => AppendWriteTextAsync(file, contents, false);
 
-		public static global::Windows.Foundation.IAsyncAction AppendTextAsync( global::Windows.Storage.IStorageFile file,  string contents) => AppendWriteTextAsync(file, contents, true);
+		public static IAsyncAction AppendTextAsync( IStorageFile file,  string contents) => AppendWriteTextAsync(file, contents, true);
 	}
 }
