46261903ea25c0218c01ea63e443ec82dce12cc7
David Oliver <david.oliver@nventive.com>
2020-01-17 09:49:28 -0500

    UI tests - add ImageAssert.AreAlmostEqual

--- src/SamplesApp/SamplesApp.UITests/TestFramework/ImageAssert.cs
+++ src/SamplesApp/SamplesApp.UITests/TestFramework/ImageAssert.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Uno.UITest;
+using static System.Math;
 
 namespace SamplesApp.UITests.TestFramework
 {
@@ -52,13 +53,91 @@ namespace SamplesApp.UITests.TestFramework
 				);
 
 				for (var x = 0; x < width; x++)
-				for (var y = 0; y < height; y++)
-				{
-					Assert.AreEqual(
-						expectedBitmap.GetPixel(x + expectedOffset.x, y + expectedOffset.y),
-						actualBitmap.GetPixel(x + actualOffset.x, y + actualOffset.y),
-						$"Pixel {x},{y}");
-				}
+					for (var y = 0; y < height; y++)
+					{
+						Assert.AreEqual(
+							expectedBitmap.GetPixel(x + expectedOffset.x, y + expectedOffset.y),
+							actualBitmap.GetPixel(x + actualOffset.x, y + actualOffset.y),
+							$"Pixel {x},{y}");
+					}
+			}
+		}
+
+		/// <summary>
+		/// Asserts that two screenshots are equal to each other inside the area of the nominated rect to within the given pixel error
+		/// (ie, neither the A, R, G, or B values differ by more than the permitted error for any pixel).
+		/// </summary>
+		public static void AreAlmostEqual(FileInfo expected, FileInfo actual, IAppRect rect, int permittedPixelError)
+			=> AreAlmostEqual(expected, actual, new Rectangle((int)rect.X, (int)rect.Y, (int)rect.Width, (int)rect.Height), permittedPixelError);
+
+		/// <summary>
+		/// Asserts that two screenshots are equal to each other inside the area of the nominated rect to within the given pixel error
+		/// (ie, neither the A, R, G, or B values differ by more than the permitted error for any pixel).
+		/// </summary>
+		public static void AreAlmostEqual(FileInfo expected, FileInfo actual, Rectangle? rect = null, int permittedPixelError = 5)
+		{
+			rect = rect ?? new Rectangle(0, 0, int.MaxValue, int.MaxValue);
+			AreAlmostEqual(expected, rect.Value, actual, rect.Value, permittedPixelError);
+		}
+
+		/// <summary>
+		/// Asserts that two screenshots are equal to each other inside the area of the nominated rect to within the given pixel error
+		/// (ie, neither the A, R, G, or B values differ by more than the permitted error for any pixel).
+		/// </summary>
+		public static void AreAlmostEqual(FileInfo expected, IAppRect expectedRect, FileInfo actual, IAppRect actualRect, int permittedPixelError)
+			=> AreAlmostEqual(
+				expected,
+				new Rectangle((int)expectedRect.X, (int)expectedRect.Y, (int)expectedRect.Width, (int)expectedRect.Height),
+				actual,
+				new Rectangle((int)actualRect.X, (int)actualRect.Y, (int)actualRect.Width, (int)actualRect.Height),
+				permittedPixelError);
+
+		/// <summary>
+		/// Asserts that two screenshots are equal to each other inside the area of the nominated rect to within the given pixel error
+		/// (ie, neither the A, R, G, or B values differ by more than the permitted error for any pixel).
+		/// </summary>
+		public static void AreAlmostEqual(FileInfo expected, Rectangle expectedRect, FileInfo actual, Rectangle actualRect, int permittedPixelError)
+		{
+			Assert.AreEqual(expectedRect.Width, actualRect.Width, "Rect Width");
+			Assert.AreEqual(expectedRect.Height, actualRect.Height, "Rect Height");
+
+			using (var expectedBitmap = new Bitmap(expected.FullName))
+			using (var actualBitmap = new Bitmap(actual.FullName))
+			{
+				Assert.AreEqual(expectedBitmap.Size.Width, actualBitmap.Size.Width, "Screenshot Width");
+				Assert.AreEqual(expectedBitmap.Size.Height, actualBitmap.Size.Height, "Screenshot Height");
+
+				var width = Math.Min(expectedRect.Width, expectedBitmap.Size.Width);
+				var height = Math.Min(expectedRect.Height, expectedBitmap.Size.Height);
+
+				(int x, int y) expectedOffset = (
+					expectedRect.X < 0 ? expectedBitmap.Size.Width + expectedRect.X : expectedRect.X,
+					expectedRect.Y < 0 ? expectedBitmap.Size.Height + expectedRect.Y : expectedRect.Y
+				);
+				(int x, int y) actualOffset = (
+					actualRect.X < 0 ? actualBitmap.Size.Width + actualRect.X : actualRect.X,
+					actualRect.Y < 0 ? actualBitmap.Size.Height + actualRect.Y : actualRect.Y
+				);
+
+				for (var x = 0; x < width; x++)
+					for (var y = 0; y < height; y++)
+					{
+						var expectedPixel = expectedBitmap.GetPixel(x + expectedOffset.x, y + expectedOffset.y);
+						var actualPixel = actualBitmap.GetPixel(x + actualOffset.x, y + actualOffset.y);
+						if (expectedPixel == actualPixel)
+						{
+							continue;
+						}
+
+						if (Abs(expectedPixel.R - actualPixel.R) > permittedPixelError
+							|| Abs(expectedPixel.G - actualPixel.G) > permittedPixelError
+							|| Abs(expectedPixel.B - actualPixel.B) > permittedPixelError
+							|| Abs(expectedPixel.A - actualPixel.A) > permittedPixelError
+						)
+						{
+							Assert.Fail($"Difference between expected={expectedPixel} and actual={actualPixel} at {x},{y} exceeds permitted error of {permittedPixelError}");
+						}
+					}
 			}
 		}
 
@@ -101,13 +180,13 @@ namespace SamplesApp.UITests.TestFramework
 				);
 
 				for (var x = 0; x < width; x++)
-				for (var y = 0; y < height; y++)
-				{
-					if (expectedBitmap.GetPixel(x + expectedOffset.x, y + expectedOffset.y) != actualBitmap.GetPixel(x + actualOffset.x, y + actualOffset.y))
+					for (var y = 0; y < height; y++)
 					{
-						return;
+						if (expectedBitmap.GetPixel(x + expectedOffset.x, y + expectedOffset.y) != actualBitmap.GetPixel(x + actualOffset.x, y + actualOffset.y))
+						{
+							return;
+						}
 					}
-				}
 
 				Assert.Fail("Screenshots are equals.");
 			}
