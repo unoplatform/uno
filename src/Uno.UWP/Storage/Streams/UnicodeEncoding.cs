03e0718cbd983dd851698837c46fa532fbc6af95
Piotr Karocki <pkar@ieee.org>
2020-01-21 16:00:19 +0100

    Update UnicodeEncoding.cs
    changing tabs to spaces, etc.

--- src/Uno.UWP/Storage/Streams/UnicodeEncoding.cs
+++ src/Uno.UWP/Storage/Streams/UnicodeEncoding.cs
@@ -1,9 +1,9 @@
 namespace Windows.Storage.Streams
 {
-	public   enum UnicodeEncoding 
-	{
-		Utf8,
-		Utf16LE,
-		Utf16BE
-	}
+    public enum UnicodeEncoding 
+    {
+        Utf8,
+        Utf16LE,
+        Utf16BE
+    }
 }
