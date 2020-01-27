e39d2ade95c8cacf4ac1ab940af31d76a48f0faf
Martin Zikmund <martinzikmund@live.com>
2020-01-27 08:36:16 +0100

    Added missing using

--- src/SamplesApp/UITests.Shared/Windows_UI_ViewManagement/IsScreenCaptureEnabledTests.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_ViewManagement/IsScreenCaptureEnabledTests.xaml.cs
@@ -14,7 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using Windows.Foundation.Metadata;
 
 namespace UITests.Shared.Windows_UI_ViewManagement
 {
