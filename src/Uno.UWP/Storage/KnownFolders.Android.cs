00b3b66228f3a30a9b48217d8bf2943f359387c2
pkar <pkar@ieee.org>
2020-01-31 10:33:12 +0100

    KnownFolders - movies and music

---
+++ src/Uno.UWP/Storage/KnownFolders.Android.cs
@@ -0,0 +1,14 @@
+﻿#if __ANDROID__
+
+namespace Windows.Storage
+{
+	public static partial class KnownFolders
+	{
+		internal static StorageFolder FolderFromAndroidName(string name) => new StorageFolder(Android.OS.Environment.GetExternalStoragePublicDirectory(name).CanonicalPath);
+		public static StorageFolder MusicLibrary { get => FolderFromAndroidName(Android.OS.Environment.DirectoryMusic); }
+		public static StorageFolder VideosLibrary { get => FolderFromAndroidName(Android.OS.Environment.DirectoryMovies); }
+
+	}
+}
+
+#endif 
