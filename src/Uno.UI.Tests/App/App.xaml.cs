7d5892b38b2e410b3f14d0e53cba3297560901b9
Carl de Billy <carl.debilly@nventive.com>
2020-02-01 00:27:54 -0500

    Fixed accessibility of App

--- src/Uno.UI.Tests/App/App.xaml.cs
+++ src/Uno.UI.Tests/App/App.xaml.cs
@@ -1,27 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace UnitTestsApp
 {
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
-	internal sealed partial class App : Application
+	public sealed partial class App : Application
 	{
 		public Grid HostView { get; private set; }
 		public App()
