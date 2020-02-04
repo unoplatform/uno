a2969470cb855448fa10bd29d3e31d95ede9055e
Martin Zikmund <martin@zikmund.dev>
2020-02-04 02:44:28 +0100

    Surface Duo implementation for HingeAngleSensor

--- src/Uno.UI.DualScreen/AssemblyInfo.cs
+++ src/Uno.UI.DualScreen/AssemblyInfo.cs
@@ -5,3 +5,4 @@ using System.Text;
 using System.Threading.Tasks;
 
 [assembly: Uno.Foundation.Extensibility.ApiExtension(typeof(Windows.UI.ViewManagement.IApplicationViewSpanningRects), typeof(Uno.UI.DualScreen.DuoApplicationViewSpanningRects))]
+[assembly: Uno.Foundation.Extensibility.ApiExtension(typeof(Uno.Devices.Sensors.INativeHingeAngleSensor), typeof(Uno.UI.DualScreen.DuoHingeAngleSensor))]
