888f1d7895e642bc7107c245e47ec86b9bee17f9
Carl de Billy <carl.debilly@nventive.com>
2020-03-12 16:39:11 -0400

    Image wrongly layouted when a transform is applied Fix https://github.com/unoplatform/uno/issues/2441

--- src/Uno.UI/Extensions/CGSizeExtensions.iOSmacOS.cs
+++ src/Uno.UI/Extensions/CGSizeExtensions.iOSmacOS.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 using System.Text;
 using CoreGraphics;
 using Windows.Foundation;
@@ -9,21 +11,25 @@ namespace CoreGraphics
 {
 	public static class CGSizeExtensions
 	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		[Pure]
 		public static Windows.Foundation.Size ToFoundationSize(this CGSize size)
 			=> new Size(
 				size.Width,
 				size.Height
 			);
 
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		[Pure]
 		public static bool HasZeroArea(this CGSize size)
 			=> size.Height == 0 || size.Width == 0;
 
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		[Pure]
 		internal static CGSize Add(this CGSize left, Thickness right)
-		{
-			return new CGSize(
+			=> new CGSize(
 				left.Width + right.Left + right.Right,
 				left.Height + right.Top + right.Bottom
 			);
-		}
 	}
 }
