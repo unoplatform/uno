e59acae9c1034cd83ffb41430dd91b1fec2ed52b
Martin Zikmund <martin@zikmund.dev>
2020-03-13 16:54:37 +0900

    test: Added first TreeView test page

---
+++ src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/TreeView/TreeViewNode2.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Windows.UI.Xaml.Controls;
+
+using TreeViewNode = Microsoft.UI.Xaml.Controls.TreeViewNode;
+
+namespace UITests.Shared.Microsoft_UI_Xaml_Controls.TreeViewTests
+{
+	// An empty class to test TreeViewNode inheritance
+	class TreeViewNode2 : TreeViewNode
+	{
+
+	}
+}
---
+++ src/Uno.UI.RuntimeTests/MUX/Utilities/MUXTestPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace MUXControlsTestApp.Utilities
+{
+    public class MUXTestPage : Page
+    {
+		public static List<T> FindVisualChildrenByType<T>(FrameworkElement parent) where T : class
+		{
+			List<T> children = new List<T>();
+			T parentAsT = parent as T;
+
+			if (parentAsT != null)
+			{
+				children.Add(parentAsT);
+			}
+
+			int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+
+			for (int i = 0; i < childrenCount; i++)
+			{
+				FrameworkElement childAsFE = VisualTreeHelper.GetChild(parent, i) as FrameworkElement;
+
+				if (childAsFE != null)
+				{
+					List<T> result = FindVisualChildrenByType<T>(childAsFE);
+					children.AddRange(result);
+				}
+			}
+
+			return children;
+		}
+	}
+}
