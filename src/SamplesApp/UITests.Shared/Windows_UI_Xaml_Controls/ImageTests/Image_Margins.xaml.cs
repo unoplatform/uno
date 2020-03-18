70f3babd93a86e858db9dfc1970be55a6fce7034
Carl de Billy <carl.debilly@gmail.com>
2020-03-17 23:36:01 -0400

    test(image): added tests for images with margin (for Android)
    (cherry picked from commit 8b8a3e20e29164417ade8d9edac6976d15747dc3)

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ImageTests/Image_Margins.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.ImageTests
+{
+	[Sample("Image")]
+	public sealed partial class Image_Margins : Page
+	{
+		public Image_Margins()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
