bcbe073cff2a9bc0f5bc4db8f7999f8e3bf8a820
Carl de Billy <carl.debilly@gmail.com>
2020-03-19 10:36:58 -0400

    fix(image): wrong measure for images on iOS
    (cherry picked from commit 4f825baffa9d11b7abea3d50fa5dc7a586626db3)

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ImageTests/Image_Margins.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ImageTests/Image_Margins.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using Windows.UI.Xaml.Controls;
 using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml;
+using System;
+using System.Threading.Tasks;
 
 namespace UITests.Windows_UI_Xaml_Controls.ImageTests
 {
-	[Sample("Image")]
+	[SampleControlInfo(category: "Image")]
 	public sealed partial class Image_Margins : Page
 	{
 		public Image_Margins()
