9cfb29fd490a424fd57da0cec080a9c62098b874
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-03-22 17:46:16 +0100

    feat: [WASM] Added the ability to load images from bytes

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ImageTests/LoadFromBytes.xaml.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Uno.Extensions;
+
+namespace Uno.UI.Samples.UITests.ImageTestsControl
+{
+	[SampleControlInfo("Image", "LoadFromBytes")]
+	public sealed partial class LoadFromBytes
+	{
+		public LoadFromBytes()
+		{
+			InitializeComponent();
+			Loaded += OnLoaded;
+		}
+
+		private async void OnLoaded(object sender, RoutedEventArgs e)
+		{
+			var assembly = typeof(LoadFromBytes).Assembly;
+
+			var query = from name in assembly.GetManifestResourceNames()
+						where name.EndsWith("mslug.png")
+						select assembly.GetManifestResourceStream(name);
+
+			using (var stream = query.First())
+			{
+				MyImage.Source = await CreateImageSource(stream);
+			}
+		}
+
+		private async Task<ImageSource> CreateImageSource(Stream stream)
+		{
+			if (stream == null)
+			{
+				throw new ArgumentNullException(nameof(stream));
+			}
+
+			var image = new BitmapImage();
+
+#if WINDOWS_UWP
+			await image.SetSourceAsync(stream.AsRandomAccessStream()).AsTask();
+#else
+			var copy = stream.ToMemoryStream();
+			await image.SetSourceAsync(copy);
+#endif
+			return image;
+		}
+	}
+}
--- src/Uno.UI/UI/Xaml/Controls/Image/Image.wasm.cs
+++ src/Uno.UI/UI/Xaml/Controls/Image/Image.wasm.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Globalization;
+using System.IO;
 using Windows.Foundation;
 using Windows.UI.Xaml.Media;
-using Uno.Diagnostics.Eventing;
 using Uno.Extensions;
 using Uno.Foundation;
 using Uno.Logging;
@@ -102,7 +101,15 @@ namespace Windows.UI.Xaml.Controls
 
 			_lastMeasuredSize = _zeroSize;
 
-			if (source is WriteableBitmap wb)
+			var stream = source?.Stream;
+			if (stream != null)
+			{
+				stream.Position = 0;
+				var encodedBytes = Convert.ToBase64String(stream.ReadBytes());
+				var url = "data:application/octet-stream;base64," + encodedBytes;
+				SetImageUrl(url);
+			}
+			else if (source is WriteableBitmap wb)
 			{
 				void setImageContent()
 				{
