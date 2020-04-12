3ede85c9999f4fb9342dff8544ce312b1a502b0f
Martin Zikmund <martin@zikmund.dev>
2020-04-12 23:26:54 +0200

    feat: Support ComboBox on macOS

---
+++ src/Uno.UI/UI/Xaml/Controls/ItemsControl/ItemsControl.macOS.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Uno.Extensions;
+using Uno.UI;
+
+using Foundation;
+using AppKit;
+using CoreGraphics;
+
+namespace Windows.UI.Xaml.Controls
+{
+	public partial class ItemsControl : Control
+	{
+		partial void InitializePartial()
+		{
+
+		}
+
+		partial void RequestLayoutPartial()
+		{
+			NeedsLayout = true;
+		}
+
+		partial void RemoveViewPartial(NSView current)
+		{
+			current.RemoveFromSuperview();
+		}
+
+		public override void Layout()
+		{
+			UpdateItemsIfNeeded();
+			base.Layout();
+		}
+
+		public override CGSize SizeThatFits(CGSize size)
+		{
+			UpdateItemsIfNeeded();
+			return base.SizeThatFits(size);
+		}
+	}
+}
+
