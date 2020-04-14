80ce63ef8a4dac26e6206c199b9f3ea15b1539e3
Martin Zikmund <martin@zikmund.dev>
2020-04-14 21:00:36 +0200

    feat(DisplayRequest): macOS support

---
+++ src/Uno.UWP/System/Display/DisplayRequest.macOS.cs
@@ -0,0 +1,21 @@
+﻿#if __MACOS__
+using Uno.Helpers;
+
+namespace Windows.System.Display
+{
+	public partial class DisplayRequest
+	{
+		private uint _displayRequestId;
+
+		partial void ActivateScreenLock()
+		{
+			IOKit.PreventUserIdleDisplaySleep("DisplayRequest", out _displayRequestId);
+		}
+
+		partial void DeactivateScreenLock()
+		{
+			IOKit.AllowUserIdleDisplaySleep(_displayRequestId);
+		}
+	}
+}
+#endif
