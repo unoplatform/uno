a69380ab58248a6d7c13a30c3983c77fe77d24ad
Martin Zikmund <martin@zikmund.dev>
2020-04-26 12:00:36 +0200

    chore: Continued implementation

--- src/Uno.UWP/Data/Xml/Dom/XmlDocument.cs
+++ src/Uno.UWP/Data/Xml/Dom/XmlDocument.cs
@@ -7,18 +7,39 @@ namespace Windows.Data.Xml.Dom
 	public partial class XmlDocument : IXmlNode, IXmlNodeSerializer, IXmlNodeSelector
 	{
 		private SystemXmlDocument _backingDocument = new SystemXmlDocument();
-		private Dictionary<object, object> _systemXmlElementWrappings = new Dictionary<object, object>();
+		private Dictionary<object, object> _uwpToSystemXml = new Dictionary<object, object>();
+		private Dictionary<object, object> _systemXmlToUwp = new Dictionary<object, object>();
 
 		public void LoadXml(string xml) => _backingDocument.LoadXml(xml);
 
 		public IXmlNode SelectSingleNode(string xpath) => (IXmlNode)Wrap(_backingDocument.SelectSingleNode(xpath));
 
+		/// <summary>
+		/// Wraps System.Xml node to UWP XML node.
+		/// </summary>
+		/// <param name="node">System.Xml node.</param>
+		/// <returns>UWP XML node.</returns>
 		internal object Wrap(object node)
 		{
-			if (!_systemXmlElementWrappings.TryGetValue(node, out var wrapped))
+			if (!_systemXmlToUwp.TryGetValue(node, out var wrapped))
 			{
 				wrapped = CreateWrapper(node);
-				_systemXmlElementWrappings.Add(node, wrapped);
+				_systemXmlToUwp.Add(node, wrapped);
+			}
+			return wrapped;
+		}
+
+		/// <summary>
+		/// Wraps UWP XML node to System.Xml node.
+		/// </summary>
+		/// <param name="node">UWP XML node.</param>
+		/// <returns>System.Xml node.</returns>
+		internal object Unwrap(object node)
+		{
+			if (!_uwpToSystemXml.TryGetValue(node, out var wrapped))
+			{
+				wrapped = CreateWrapper(node);
+				_uwpToSystemXml.Add(node, wrapped);
 			}
 			return wrapped;
 		}
--- src/Uno.UWP/Data/Xml/Dom/XmlText.cs
+++ src/Uno.UWP/Data/Xml/Dom/XmlText.cs
@@ -24,7 +24,7 @@ namespace Windows.Data.Xml.Dom
 		public object Prefix
 		{
 			get => _backingText.Prefix;
-			set => _backingText.Prefix = value;
+			set => _backingText.Prefix = value?.ToString();
 		}
 
 		public object NodeValue
@@ -54,7 +54,7 @@ namespace Windows.Data.Xml.Dom
 			}
 		}
 
-		public XmlDocument OwnerDocument => _owner;
+		public XmlDocument OwnerDocument => _owner; 
 
 		public XmlNodeList ChildNodes
 		{
@@ -64,173 +64,63 @@ namespace Windows.Data.Xml.Dom
 			}
 		}
 
-		public IXmlNode ParentNode => _owner.Wrap(_backingText.ParentNode);
+		public IXmlNode ParentNode => (IXmlNode)_owner.Wrap(_backingText.ParentNode);
 
 		public IXmlNode PreviousSibling => (IXmlNode)_owner.Wrap(_backingText.PreviousSibling);
 
 		public string InnerText
 		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member string XmlText.InnerText is not implemented in Uno.");
-			}
-			set
-			{
-				global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.Data.Xml.Dom.XmlText", "string XmlText.InnerText");
-			}
+			get => _backingText.InnerText;
+			set => _backingText.InnerText = value;
 		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public global::Windows.Data.Xml.Dom.IXmlText SplitText(uint offset)
-		{
-			throw new global::System.NotImplementedException("The member IXmlText XmlText.SplitText(uint offset) is not implemented in Uno.");
-		}
-#endif
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.Data.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.Data.set
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.Length.get
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public string SubstringData(uint offset, uint count)
-		{
-			throw new global::System.NotImplementedException("The member string XmlText.SubstringData(uint offset, uint count) is not implemented in Uno.");
-		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public void AppendData(string data)
-		{
-			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.Data.Xml.Dom.XmlText", "void XmlText.AppendData(string data)");
-		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public void InsertData(uint offset, string data)
-		{
-			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.Data.Xml.Dom.XmlText", "void XmlText.InsertData(uint offset, string data)");
-		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public void DeleteData(uint offset, uint count)
-		{
-			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.Data.Xml.Dom.XmlText", "void XmlText.DeleteData(uint offset, uint count)");
-		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public void ReplaceData(uint offset, uint count, string data)
-		{
-			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.Data.Xml.Dom.XmlText", "void XmlText.ReplaceData(uint offset, uint count, string data)");
-		}
-#endif
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.NodeValue.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.NodeValue.set
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.NodeType.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.NodeName.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.ParentNode.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.ChildNodes.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.FirstChild.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.LastChild.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.PreviousSibling.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.NextSibling.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.Attributes.get
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public bool HasChildNodes()
-		{
-			throw new global::System.NotImplementedException("The member bool XmlText.HasChildNodes() is not implemented in Uno.");
-		}
-#endif
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.OwnerDocument.get
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public global::Windows.Data.Xml.Dom.IXmlNode InsertBefore(global::Windows.Data.Xml.Dom.IXmlNode newChild, global::Windows.Data.Xml.Dom.IXmlNode referenceChild)
-		{
-			throw new global::System.NotImplementedException("The member IXmlNode XmlText.InsertBefore(IXmlNode newChild, IXmlNode referenceChild) is not implemented in Uno.");
-		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public global::Windows.Data.Xml.Dom.IXmlNode ReplaceChild(global::Windows.Data.Xml.Dom.IXmlNode newChild, global::Windows.Data.Xml.Dom.IXmlNode referenceChild)
-		{
-			throw new global::System.NotImplementedException("The member IXmlNode XmlText.ReplaceChild(IXmlNode newChild, IXmlNode referenceChild) is not implemented in Uno.");
-		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public global::Windows.Data.Xml.Dom.IXmlNode RemoveChild(global::Windows.Data.Xml.Dom.IXmlNode childNode)
-		{
-			throw new global::System.NotImplementedException("The member IXmlNode XmlText.RemoveChild(IXmlNode childNode) is not implemented in Uno.");
-		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public global::Windows.Data.Xml.Dom.IXmlNode AppendChild(global::Windows.Data.Xml.Dom.IXmlNode newChild)
-		{
-			throw new global::System.NotImplementedException("The member IXmlNode XmlText.AppendChild(IXmlNode newChild) is not implemented in Uno.");
-		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public global::Windows.Data.Xml.Dom.IXmlNode CloneNode(bool deep)
-		{
-			throw new global::System.NotImplementedException("The member IXmlNode XmlText.CloneNode(bool deep) is not implemented in Uno.");
-		}
-#endif
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.NamespaceUri.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.LocalName.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.Prefix.get
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public void Normalize()
-		{
-			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.Data.Xml.Dom.XmlText", "void XmlText.Normalize()");
-		}
-#endif
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.Prefix.set
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public global::Windows.Data.Xml.Dom.IXmlNode SelectSingleNode(string xpath)
-		{
-			throw new global::System.NotImplementedException("The member IXmlNode XmlText.SelectSingleNode(string xpath) is not implemented in Uno.");
-		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public global::Windows.Data.Xml.Dom.XmlNodeList SelectNodes(string xpath)
-		{
-			throw new global::System.NotImplementedException("The member XmlNodeList XmlText.SelectNodes(string xpath) is not implemented in Uno.");
-		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
+
+		public IXmlText SplitText(uint offset) => (IXmlText)_owner.Wrap(_backingText.SplitText((int)offset));
+
+		public string SubstringData(uint offset, uint count) => _backingText.Substring((int)offset, (int)count);
+
+		public void AppendData(string data) => _backingText.AppendData(data);
+
+		public void InsertData(uint offset, string data) => _backingText.InsertData((int)offset, data);
+
+		public void DeleteData(uint offset, uint count) => _backingText.DeleteData((int)offset, (int)count);
+
+		public void ReplaceData(uint offset, uint count, string data) => _backingText.ReplaceData((int)offset, (int)count, data);
+
+		public bool HasChildNodes() => _backingText.HasChildNodes;
+
+		public IXmlNode InsertBefore(IXmlNode newChild, IXmlNode referenceChild) =>
+			_owner.Wrap(
+				_backingText.InsertBefore(
+					_owner.Unwrap(newChild),
+					_owner.Unwrap(referenceChild));
+
+		public IXmlNode ReplaceChild(IXmlNode newChild, IXmlNode referenceChild) =>
+			_owner.Wrap(
+				_backingText.ReplaceChild(
+					_owner.Unwrap(newChild),
+					_owner.Unwrap(referenceChild));
+
+		public IXmlNode RemoveChild(IXmlNode childNode) => _backingText.RemoveChild(childNode);
+
+		public IXmlNode AppendChild(IXmlNode newChild) => _backingText.AppendChild(newChild);
+
+		public IXmlNode CloneNode(bool deep) => _backingText.CloneNode(deep);
+
+		public void Normalize() => _backingText.Normalize();
+
+		public IXmlNode SelectSingleNode(string xpath) => _backingText.SelectSingleNode(xpath);
+
+		public XmlNodeList SelectNodes(string xpath) => _backingText.SelectNodes(xpath);
+
 		public global::Windows.Data.Xml.Dom.IXmlNode SelectSingleNodeNS(string xpath, object namespaces)
 		{
 			throw new global::System.NotImplementedException("The member IXmlNode XmlText.SelectSingleNodeNS(string xpath, object namespaces) is not implemented in Uno.");
 		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
 		public global::Windows.Data.Xml.Dom.XmlNodeList SelectNodesNS(string xpath, object namespaces)
 		{
 			throw new global::System.NotImplementedException("The member XmlNodeList XmlText.SelectNodesNS(string xpath, object namespaces) is not implemented in Uno.");
 		}
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __MACOS__
-		[global::Uno.NotImplemented]
-		public string GetXml()
-		{
-			throw new global::System.NotImplementedException("The member string XmlText.GetXml() is not implemented in Uno.");
-		}
-#endif
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.InnerText.get
-		// Forced skipping of method Windows.Data.Xml.Dom.XmlText.InnerText.set
-		// Processing: Windows.Data.Xml.Dom.IXmlText
-		// Processing: Windows.Data.Xml.Dom.IXmlCharacterData
-		// Processing: Windows.Data.Xml.Dom.IXmlNode
-		// Processing: Windows.Data.Xml.Dom.IXmlNodeSelector
-		// Processing: Windows.Data.Xml.Dom.IXmlNodeSerializer
+
+		public string GetXml() => _backingText.OuterXml;
 	}
 }
