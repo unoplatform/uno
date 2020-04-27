efe2e89feb42b4bca5776ecc197a832a9833536f
Jeremie Thibeault <jeremie.thibeault@nventive.com>
2020-04-27 10:16:14 -0400

    fix(mediaplayer): Fix invalid stop of the player when toggling the fullscreen mode.

--- src/Uno.UI/UI/Xaml/Controls/MediaPlayer/MediaPlayerPresenter.cs
+++ src/Uno.UI/UI/Xaml/Controls/MediaPlayer/MediaPlayerPresenter.cs
@@ -85,9 +85,20 @@ namespace Windows.UI.Xaml.Controls
 		{
 		}
 
+		/// <summary>
+		/// Indicates whether or not the player is currently toggling the fullscreen mode.
+		/// </summary>
+		internal bool IsTogglingFullscreen { get; set; }
+
 		protected override void OnUnloaded()
 		{
-			MediaPlayer.Stop();
+			// The control will get unloaded when going to full screen mode.
+			// Similar to UWP, the video should keep playing while changing mode.
+			if (!IsTogglingFullscreen)
+			{
+				MediaPlayer.Stop();
+			}
+
 			base.OnUnloaded();
 		}
 
