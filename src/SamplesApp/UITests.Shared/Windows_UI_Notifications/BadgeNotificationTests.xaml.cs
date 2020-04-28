459d3abb99a0a03c2339df667a7e6659de3ee883
Martin Zikmund <martin@zikmund.dev>
2020-04-28 18:59:06 +0200

    test(BadgeNotification): Add UI test for Badge notification

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Notifications/BadgeNotificationTests.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.UI.Samples.Controls;
+using Windows.Data.Xml.Dom;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace UITests.Windows_UI_Notifications
+{
+	[SampleControlInfo("Windows.UI.Notifications")]
+	public sealed partial class BadgeNotificationTests : Page
+    {
+        public BadgeNotificationTests()
+        {
+            this.InitializeComponent();
+        }
+
+		private void SetBadge_Click(object sender, RoutedEventArgs e)
+		{
+			var badgeXml = BadgeUpdateManager.GetTemplateContent(BadgeTemplateType.BadgeNumber);
+			var badgeGlyphXml = BadgeUpdateManager.GetTemplateContent(BadgeTemplateType.BadgeGlyph);
+			XmlElement badgeElement = badgeXml.SelectSingleNode("/badge") as XmlElement;
+			badgeElement.SetAttribute("value", BadgeTextBox.Text);
+
+			var badgeNotification = new BadgeNotification(badgeXml);
+
+			BadgeUpdateManager.CreateBadgeUpdaterForApplication().Update(badgeNotification);
+		}
+
+		private void ClearBadge_Click(object sender, RoutedEventArgs e)
+		{
+			BadgeUpdateManager.CreateBadgeUpdaterForApplication().Clear();
+		}
+	}
+}
