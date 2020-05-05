06621fd840f44715dba9efea0076297d55498345
Carl de Billy <carl.debilly@gmail.com>
2020-05-05 12:55:10 -0400

    feat(progressring): Transposed C++ code from WinUI

---
+++ src/AddIns/Uno.UI.Lottie/LottieVisualSourceProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Toolkit.Uwp.UI.Lottie;
+using Microsoft.UI.Xaml.Controls.AnimatedVisualPlayer;
+using Uno.Foundation.Extensibility;
+
+[assembly: ApiExtension(typeof(ILottieVisualSourceProvider), typeof(Uno.UI.Lottie.LottieVisualSourceProvider))]
+
+namespace Uno.UI.Lottie
+{
+	public class LottieVisualSourceProvider : ILottieVisualSourceProvider
+	{
+		public LottieVisualSourceProvider(object owner)
+		{
+		}
+
+		public IAnimatedVisualSource CreateFromLottieAsset(Uri sourceFile) => new LottieVisualSource {UriSource = sourceFile};
+	}
+}
