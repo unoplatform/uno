58c5f3fc3bc1760f210a6a676d71c613299f668c
Carl de Billy <carl.debilly@nventive.com>
2020-05-15 15:51:44 -0400

    docs: removed now useless code comment.

--- src/Uno.UI/UI/Xaml/Media/Animation/Animators/RenderingLoopColorAnimator.wasm.cs
+++ src/Uno.UI/UI/Xaml/Media/Animation/Animators/RenderingLoopColorAnimator.wasm.cs
@@ -13,8 +13,6 @@ namespace Windows.UI.Xaml.Media.Animation
 
 		protected override ColorOffset GetUpdatedValue(long frame, ColorOffset from, ColorOffset to)
 		{
-			// TODO: apply easing, if any - https://github.com/unoplatform/uno/issues/2948
-
 			if (_easing != null)
 			{
 				return _easing.Ease(frame, from, to, Duration);
