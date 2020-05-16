8e2696447812def08a84a019a1d02f7d97c1dfba
Martin Zikmund <martin@zikmund.dev>
2020-05-16 23:25:51 +0200

    test: SplitView clipping sample

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/SplitView/SplitViewClip.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.SplitView
+{
+	[SampleControlInfo("SplitView")]
+	public sealed partial class SplitViewClip : Page
+    {
+        public SplitViewClip()
+        {
+            this.InitializeComponent();
+        }
+
+		public void Button_Click(object sender, RoutedEventArgs args)
+		{
+			Split.IsPaneOpen = !Split.IsPaneOpen;
+		}
+    }
+}
