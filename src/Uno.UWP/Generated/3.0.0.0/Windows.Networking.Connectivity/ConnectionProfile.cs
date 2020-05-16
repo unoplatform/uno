eac4d56ea2ab99dff22049f837ab3efc786a5b33
Martin Zikmund <martin@zikmund.dev>
2020-05-16 17:39:12 +0200

    feat(NetworkInformation): WASM support for network status

--- src/Uno.UWP/Generated/3.0.0.0/Windows.Networking.Connectivity/ConnectionProfile.cs
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.Networking.Connectivity/ConnectionProfile.cs
@@ -2,7 +2,7 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.Networking.Connectivity
 {
-	#if false || __IOS__ || NET461 || __WASM__ || __MACOS__
+	#if false || __IOS__ || NET461 || false || __MACOS__
 	[global::Uno.NotImplemented]
 	#endif
 	public  partial class ConnectionProfile 
@@ -98,7 +98,7 @@ namespace Windows.Networking.Connectivity
 		}
 		#endif
 		// Forced skipping of method Windows.Networking.Connectivity.ConnectionProfile.ProfileName.get
-		#if false || __IOS__ || NET461 || __WASM__ || __MACOS__
+		#if false || __IOS__ || NET461 || false || __MACOS__
 		[global::Uno.NotImplemented]
 		public  global::Windows.Networking.Connectivity.NetworkConnectivityLevel GetNetworkConnectivityLevel()
 		{
---
+++ src/Uno.UWP/Networking/Connectivity/ConnectionProfile.wasm.cs
@@ -0,0 +1,23 @@
+﻿#if __WASM__
+
+namespace Windows.Networking.Connectivity
+{
+	public partial class ConnectionProfile
+    {
+		private const string JsType = "Windows.Networking.Connectivity.ConnectionProfile";
+		private readonly bool _isInternetProfile;
+
+		internal ConnectionProfile(bool isInternetProfile)
+		{
+			_isInternetProfile = isInternetProfile;
+		}
+
+		public NetworkConnectivityLevel GetNetworkConnectivityLevel()
+		{
+			var command = $"{JsType}.hasInternetAccess()";
+			var result = Uno.Foundation.WebAssemblyRuntime.InvokeJS(command);
+			return bool.Parse(result) ? NetworkConnectivityLevel.InternetAccess : NetworkConnectivityLevel.None;
+		}
+	}
+}
+#endif
