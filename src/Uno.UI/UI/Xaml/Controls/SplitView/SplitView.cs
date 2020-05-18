9e1aad750f8204237f0d0e27a8786bb4bcc82530
David Oliver <david.oliver@nventive.com>
2020-05-18 15:53:56 -0400

    feat(splitview)!: Change SplitView.Content and SplitView.Pane to be UIElements
    BREAKING CHANGE: Aligns with Windows.

--- src/Uno.UI/UI/Xaml/Controls/SplitView/SplitView.cs
+++ src/Uno.UI/UI/Xaml/Controls/SplitView/SplitView.cs
@@ -8,13 +8,6 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Markup;
 using Uno.UI.DataBinding;
-#if XAMARIN_IOS
-using View = UIKit.UIView;
-#elif XAMARIN_ANDROID
-using View = Android.Views.View;
-#else
-using View = Windows.UI.Xaml.UIElement;
-#endif
 
 namespace Windows.UI.Xaml.Controls
 {
@@ -72,16 +65,16 @@ namespace Windows.UI.Xaml.Controls
 
 		#region Content DependencyProperty
 
-		public View Content
+		public UIElement Content
 		{
-			get { return (View)this.GetValue(ContentProperty); }
+			get { return (UIElement)this.GetValue(ContentProperty); }
 			set { this.SetValue(ContentProperty, value); }
 		}
 
 		public static DependencyProperty ContentProperty { get ; } =
 			DependencyProperty.Register(
 				"Content",
-				typeof(View),
+				typeof(UIElement),
 				typeof(SplitView),
 				new PropertyMetadata(
 					null,
@@ -98,16 +91,16 @@ namespace Windows.UI.Xaml.Controls
 
 		#region Pane DependencyProperty
 
-		public View Pane
+		public UIElement Pane
 		{
-			get { return (View)this.GetValue(PaneProperty); }
+			get { return (UIElement)this.GetValue(PaneProperty); }
 			set { this.SetValue(PaneProperty, value); }
 		}
 
 		public static DependencyProperty PaneProperty { get ; } =
 			DependencyProperty.Register(
 				"Pane",
-				typeof(View),
+				typeof(UIElement),
 				typeof(SplitView),
 				new PropertyMetadata(
 					null,
