d197b3aa0fc5591fcb826d78e5cd8bae7cd231ad
Martin Zikmund <martin@zikmund.dev>
2020-05-18 20:35:27 +0200

    chore: WASM clipboard start

---
+++ src/Uno.UI.Wasm/ts/Windows/ApplicationModel/DataTransfer/Clipboard.ts
@@ -0,0 +1,25 @@
+﻿namespace Windows.ApplicationModel.DataTransfer {
+	export class Clipboard {
+		private static dispatchContentChanged: () => number;
+
+		public static clear() {
+			navigator.clipboard.
+		}
+
+		public static startContentChanged() {
+			['cut', 'copy', 'paste'].forEach(function (event) {
+				document.addEventListener(event, Clipboard.onClipboardChanged);
+			});
+		}
+
+		public static stopContentChanged() {
+			['cut', 'copy', 'paste'].forEach(function (event) {
+				document.removeEventListener(event, Clipboard.onClipboardChanged);
+			});
+		}
+
+		private static onClipboardChanged() {
+			Clipboard.dispatchContentChanged();
+		}
+	}
+}
