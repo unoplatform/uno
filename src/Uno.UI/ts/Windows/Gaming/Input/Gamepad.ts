1160c40ba78b8d7005e05fdb5444b7bbdfb43416
Martin Zikmund <martin@zikmund.dev>
2020-05-26 20:18:23 +0200

    feat(Gamepad): GamepadAdded/Removed for WASM

---
+++ src/Uno.UI/ts/Windows/Gaming/Input/Gamepad.ts
@@ -0,0 +1,51 @@
+﻿namespace Windows.Gaming.Input {
+	export class Gamepad {
+
+		private static dispatchGamepadAdded: (id: string) => number;
+		private static dispatchGamepadRemoved: (id: string) => number;
+
+		public static getConnectedGamepadIds(): string {
+			const gamepads = navigator.getGamepads();
+			const separator = ";";
+			var result = '';
+			for (var i = 0; i < gamepads.length; i++) {
+				if (gamepads[i]) {
+					result += gamepads[i].index + separator;
+				}
+			}
+			return result;
+		}
+
+		public static startGamepadAdded() {
+			window.addEventListener("gamepadconnected", Gamepad.onGamepadConnected);
+		}
+
+		public static endGamepadAdded() {
+			window.removeEventListener("gamepadconnected", Gamepad.onGamepadConnected);
+		}
+
+		public static startGamepadRemoved() {
+			window.addEventListener("gamepaddisconnected", Gamepad.onGamepadDisconnected);
+		}
+
+		public static endGamepadRemoved() {
+			window.removeEventListener("gamepaddisconnected", Gamepad.onGamepadDisconnected);
+		}
+
+		private static onGamepadConnected(e : any) {
+			if (!Gamepad.dispatchGamepadAdded) {
+				Gamepad.dispatchGamepadAdded = (<any>Module).mono_bind_static_method(
+					"[Uno] Windows.Gaming.Input.Gamepad:DispatchGamepadAdded");
+			}
+			Gamepad.dispatchGamepadAdded(e.gamepad.index.toString());
+		}
+
+		private static onGamepadDisconnected(e : any) {
+			if (!Gamepad.dispatchGamepadRemoved) {
+				Gamepad.dispatchGamepadRemoved = (<any>Module).mono_bind_static_method(
+					"[Uno] Windows.Gaming.Input.Gamepad:DispatchGamepadRemoved");
+			}
+			Gamepad.dispatchGamepadRemoved(e.gamepad.index.toString());
+		}
+	}
+}
---
+++ src/Uno.UWP/Gaming/Input/Gamepad.wasm.cs
@@ -0,0 +1,111 @@
+﻿#if __WASM__
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Uno;
+using Uno.Extensions;
+using Uno.Foundation;
+
+namespace Windows.Gaming.Input
+{
+	public partial class Gamepad
+    {
+		private const string JsType = "Windows.Gaming.Input.Gamepad";
+		private const char IdSeparator = ';';
+
+		private readonly static Dictionary<string, Gamepad> _gamepadCache =
+			 new Dictionary<string, Gamepad>();
+
+		private readonly string _id;
+
+		private Gamepad(string id)
+		{
+			_id = id;
+		}
+
+		[Preserve]
+		public static int DispatchGamepadAdded(string id)
+		{
+			Gamepad gamepad;
+			lock (_gamepadCache)
+			{
+				if (!_gamepadCache.TryGetValue(id, out gamepad))
+				{
+					gamepad = new Gamepad(id);
+					_gamepadCache.Add(id, gamepad);
+				}
+			}
+			_gamepadAdded?.Invoke(null, gamepad);
+			return 0;
+		}
+
+		[Preserve]
+		public static int DispatchGamepadRemoved(string id)
+		{
+			Gamepad gamepad;
+			lock (_gamepadCache)
+			{
+				if (!_gamepadCache.TryGetValue(id, out gamepad))
+				{
+					gamepad = new Gamepad(id);
+					_gamepadCache.Add(id, gamepad);
+				}
+			}
+			_gamepadRemoved?.Invoke(null, gamepad);
+			return 0;
+		}
+
+		private static IReadOnlyList<Gamepad> GetGamepadsInternal()
+		{
+			var getConnectedGamepadIdsCommand = $"{JsType}.getConnectedGamepadIds()";
+			var serializedIds = WebAssemblyRuntime.InvokeJS(getConnectedGamepadIdsCommand);
+			var connectedGamepadIds = serializedIds.Split(new[] { IdSeparator }, StringSplitOptions.RemoveEmptyEntries);
+
+			lock (_gamepadCache)
+			{
+				var cachedGCControllers = _gamepadCache.Keys.ToArray();
+				
+				//remove disconnected
+				var disconnectedDevices = cachedGCControllers.Except(connectedGamepadIds);
+				_gamepadCache.RemoveKeys(disconnectedDevices);
+
+				//add newly connected
+				foreach (var id in connectedGamepadIds)
+				{
+					if (!_gamepadCache.TryGetValue(id, out var gamepad))
+					{
+						gamepad = new Gamepad(id);
+						_gamepadCache.Add(id, gamepad);
+					}
+				}
+
+				return _gamepadCache.Values.OrderBy(g => g._id).ToArray();
+			}
+		}
+
+		private static void StartGamepadAdded()
+		{
+			var startGamepadAddedCommand = $"{JsType}.startGamepadAdded()";
+			WebAssemblyRuntime.InvokeJS(startGamepadAddedCommand);
+		}
+
+		private static void EndGamepadAdded()
+		{
+			var endGamepadAddedCommand = $"{JsType}.endGamepadAdded()";
+			WebAssemblyRuntime.InvokeJS(endGamepadAddedCommand);
+		}
+
+		private static void StartGamepadRemoved()
+		{
+			var startGamepadRemovedCommand = $"{JsType}.startGamepadRemoved()";
+			WebAssemblyRuntime.InvokeJS(startGamepadRemovedCommand);
+		}
+
+		private static void EndGamepadRemoved()
+		{
+			var endGamepadRemovedCommand = $"{JsType}.endGamepadRemoved()";
+			WebAssemblyRuntime.InvokeJS(endGamepadRemovedCommand);
+		}
+	}
+}
+#endif
