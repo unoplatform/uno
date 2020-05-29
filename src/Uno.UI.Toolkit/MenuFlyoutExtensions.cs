36ba9342151957190127fd51b7a3cade981a9031
xiaoy312 <xiaoyao312@gmail.com>
2020-05-29 15:52:13 -0400

    feat(ios): customizable MenuFlyout cancel text

---
+++ src/Uno.UI.Toolkit/MenuFlyoutExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.Toolkit
+{
+#if __IOS__
+	[global::Foundation.PreserveAttribute(AllMembers = true)]
+#elif __ANDROID__
+	[Android.Runtime.PreserveAttribute(AllMembers = true)]
+#endif
+	public class MenuFlyoutExtensions
+	{
+		#region Property: OverrideIosCancelText
+		public static DependencyProperty OverrideIosCancelTextProperty { get; } = DependencyProperty.RegisterAttached(
+			"OverrideIosCancelText",
+			typeof(string),
+			typeof(MenuFlyoutExtensions),
+			new PropertyMetadata(default(string)));
+
+		public static string GetOverrideIosCancelText(MenuFlyout obj) => (string)obj.GetValue(OverrideIosCancelTextProperty);
+
+		public static void SetOverrideIosCancelText(MenuFlyout obj, string value) => obj.SetValue(OverrideIosCancelTextProperty, value);
+		#endregion
+	}
+}
--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyout.iOS8.iOS.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyout.iOS8.iOS.cs
@@ -35,7 +35,7 @@ namespace Windows.UI.Xaml.Controls
 					}
 				))
 				.Concat(UIAlertAction.Create(
-					LocalizedCancelString,
+					GetValue(OverrideIosCancelTextProperty) as string ?? LocalizedCancelString,
 					UIAlertActionStyle.Cancel,
 					_ => this.Hide()
 				))
