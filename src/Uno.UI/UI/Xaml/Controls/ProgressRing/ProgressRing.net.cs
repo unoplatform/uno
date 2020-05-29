436b24e946f75bf35bd978d6803a5bd5dd7a758b
David Oliver <david.oliver@nventive.com>
2020-05-29 16:16:52 -0400

    chore: [NET461] Attempted fix for WinUI build

--- src/Uno.UI/UI/Xaml/Controls/ProgressRing/ProgressRing.net.cs
+++ src/Uno.UI/UI/Xaml/Controls/ProgressRing/ProgressRing.net.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft/*Intentional space for WinUI upgrade tooling*/.UI.Xaml.Controls;
+
 namespace Windows.UI.Xaml.Controls
 {
 	public partial class ProgressRing : Control
