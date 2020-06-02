9c0517a25d1a35b624f684c8458a79b55e770785
Jerome Laban <jerome.laban@nventive.com>
2020-06-02 15:45:22 -0400

    fix(Android): Support for number leading and invalid characters in android resource names

--- src/Uno.UWP/Helpers/AndroidResourceNameEncoder.cs
+++ src/Uno.UWP/Helpers/AndroidResourceNameEncoder.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Uno.UI
+namespace Uno
 {
     internal static class AndroidResourceNameEncoder
     {
