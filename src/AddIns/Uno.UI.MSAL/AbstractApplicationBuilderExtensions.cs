eb651fa33d9701b7e39022776a81fc0aaea5ecb6
Carl de Billy <carl.debilly@gmail.com>
2020-06-09 09:17:47 -0400

    feat(msal): Added new Uno.UI.MSAL add-in project file into solution.

---
+++ src/AddIns/Uno.UI.MSAL/AbstractApplicationBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using Windows.UI.Xaml;
+using Microsoft.Identity.Client;
+
+namespace Uno.UI.MSAL
+{
+	public static class AbstractApplicationBuilderExtensions
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T WithUnoHelpers<T>(this T builder)
+			where T : AbstractApplicationBuilder<T>
+		{
+#if __ANDROID__
+			(builder as PublicClientApplicationBuilder)?.WithParentActivityOrWindow(() => Uno.UI.ContextHelper.Current as Android.App.Activity);
+#elif __IOS__ || __MACOS__
+			(builder as PublicClientApplicationBuilder)?.WithParentActivityOrWindow(() => Window.Current.Content.Window.RootViewController);
+#elif __WASM__
+			builder.WithHttpClientFactory(WasmHttpFactory.Instance);
+#endif
+			return builder;
+		}
+	}
+}
---
+++ src/AddIns/Uno.UI.MSAL/AcquireTokenInteractiveParameterBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.Extensibility;
+
+namespace Uno.UI.MSAL
+{
+	public static class AcquireTokenInteractiveParameterBuilderExtensions
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static AcquireTokenInteractiveParameterBuilder WithUnoHelpers(this AcquireTokenInteractiveParameterBuilder builder)
+		{
+#if __WASM__
+			builder.WithCustomWebUi(WasmWebUi.Instance);
+#endif
+			return builder;
+		}
+	}
+}
