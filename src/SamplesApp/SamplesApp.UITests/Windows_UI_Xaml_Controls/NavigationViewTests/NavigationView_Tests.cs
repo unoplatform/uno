d73528b5153a96c3eb9de132a9f5c24c9d7e143e
xiaoy312 <xiaoyao312@gmail.com>
2020-06-10 18:33:15 -0400

    test(NavigationView): verify TogglePaneButton size

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/NavigationViewTests/NavigationView_Tests.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/NavigationViewTests/NavigationView_Tests.cs
@@ -33,7 +33,6 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.NavigationViewTests
 			_app.WaitForDependencyPropertyValue(selectedItemText, "Text", "Save");
 		}
 
-
 		[Test]
 		[AutoRetry()]
 		public void NavigateBackAndForthBetweenMenuItemsAndSettings()
@@ -62,7 +61,34 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.NavigationViewTests
 			firstMenuItem.FastTap();
 
 			_app.WaitForElement("Page1NavViewContent");
+		}
 
+		[Test]
+		[AutoRetry()]
+		public void NavigationView_OnLightDismiss_TogglePaneButton_IsSizedCorrectly()
+		{
+			Run("SamplesApp.Samples.NavigationViewSample.NavigationViewSample");
+
+			var descendants = _app.Marked("nvSample").Descendant();
+
+			var lightDismissLayer = descendants.Marked("LightDismissLayer").FirstResult();
+			var paneRoot = descendants.Marked("PaneRoot").FirstResult();
+			var togglePaneButton = descendants.Marked("TogglePaneButton").FirstResult();
+
+			Assert.AreEqual(paneRoot.Rect.Width, togglePaneButton.Rect.Width, "when NavigationView is opened, PaneRoot and TogglePaneButton should shared the same width");
+
+			// to light-dismiss the flyout, we need to tap the right side of LightDismissLayer that isnt occupied by PaneRoot
+			var dismissibleArea = new
+			{
+				CenterX = (paneRoot.Rect.GetRight() + lightDismissLayer.Rect.GetRight()) / 2,
+				CenterY = lightDismissLayer.Rect.CenterY,
+			};
+			_app.TapCoordinates(dismissibleArea.CenterX, dismissibleArea.CenterY);
+
+			// refresh because FirstResult snapshots values
+			togglePaneButton = descendants.Marked("TogglePaneButton").FirstResult();
+
+			Assert.Less(togglePaneButton.Rect.Width, paneRoot.Rect.Width, "when NavigationView is closed, TogglePaneButton should not take the width of PaneRoot");
 		}
 	}
 }
