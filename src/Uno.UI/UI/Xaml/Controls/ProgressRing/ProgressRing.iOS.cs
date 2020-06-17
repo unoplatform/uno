0d4ef8376349ff3e6a52fad18d372bfd307f385d
Carl de Billy <carl.debilly@gmail.com>
2020-06-17 13:24:36 -0400

    fix(progressring): ProgressRing were still visible on iOS when setting Visibility=Collapsed
    Fix https://github.com/unoplatform/uno/issues/1220

--- src/Uno.UI/UI/Xaml/Controls/ProgressRing/ProgressRing.iOS.cs
+++ src/Uno.UI/UI/Xaml/Controls/ProgressRing/ProgressRing.iOS.cs
@@ -50,22 +50,29 @@ namespace Windows.UI.Xaml.Controls
 			}
 		}
 
-        private static void OnIsActiveChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
-        {
-            var progressRing = dependencyObject as ProgressRing;
-            var isActive = args.NewValue as bool?;
+		partial void OnVisibilityChangedPartial(Visibility oldValue, Visibility newValue)
+		{
+			SetIsActive(newValue == Visibility.Visible && IsActive);
+		}
 
-            if (progressRing != null && isActive != null)
-            {
-                if (isActive.Value)
-                {
-                    progressRing.StartAnimating();
-                }
-                else
-                {
-                    progressRing.StopAnimating();                    
-                }
-            }
-        }
-    }
+		private static void OnIsActiveChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+		{
+			(dependencyObject as ProgressRing)?.SetIsActive(args.NewValue as bool?);
+		}
+
+		private void SetIsActive(bool? isActive)
+		{
+			if (isActive is { } b)
+			{
+				if(b)
+				{
+					StartAnimating();
+				}
+				else
+				{
+					StopAnimating();
+				}
+			}
+		}
+	}
 }
