91d3300d5f39cfbff5d7718498d3cdeafaaabe91
Martin Zikmund <martin@zikmund.dev>
2020-06-27 17:53:24 +0200

    feat(DisplayRequest): WASM support

---
+++ src/Uno.UI.Wasm/ts/Windows/System/Display/DisplayRequest.ts
@@ -0,0 +1,33 @@
+﻿interface Navigator {
+	wakeLock : WakeLock;
+}
+
+enum WakeLockType { "screen" };
+
+interface WakeLock {
+	request(type: WakeLockType): Promise<WakeLockSentinel>;
+};
+
+interface WakeLockSentinel {    
+	release(): Promise<void>;	
+};
+
+namespace Windows.System.Display {
+
+	export class DisplayRequest {
+		private static activeScreenLockPromise: Promise<WakeLockSentinel>;
+
+		public static activateScreenLock() {
+			if (navigator.wakeLock) {
+				DisplayRequest.activeScreenLockPromise = navigator.wakeLock.request(WakeLockType.screen);
+			}
+		}
+
+		public static deactivateScreenLock() {
+			if (DisplayRequest.activeScreenLockPromise) {
+				DisplayRequest.activeScreenLockPromise.then(sentinel => sentinel.release());
+				DisplayRequest.activeScreenLockPromise = null;
+			}
+		}
+	}
+}
