bd0c2c8e93b03c1534d327504cf16bb9045d3492
Jérôme Laban <jerome.laban@nventive.com>
2020-07-03 07:59:03 -0400

    fix: Adjust gitpod serve script

--- build/gitpod/serve-sampleapp-wasm.sh
+++ build/gitpod/serve-sampleapp-wasm.sh
@@ -2,6 +2,6 @@
 
 pushd /workspace/uno/src/SamplesApp/SamplesApp.Wasm
 
-dotnet unowasm serve --urls=http://*:8000 --pathbase /workspace/uno/src/SamplesApp/SamplesApp.Wasm/bin/Debug/netstandard2.0/dist
+dotnet $NUGET_PACKAGES/uno.wasm.bootstrap.devserver/*/tools/server/dotnet-unowasm.dll serve --urls=http://*:8000 --pathbase /workspace/uno/src/SamplesApp/SamplesApp.Wasm/bin/Debug/netstandard2.0/dist
 
 popd
