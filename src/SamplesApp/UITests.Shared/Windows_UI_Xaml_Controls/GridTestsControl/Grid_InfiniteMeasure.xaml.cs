db15cbec48b7b4e3e3e09b7df86b3d30d1706d22
Carl de Billy <carl.debilly@gmail.com>
2020-07-03 14:37:05 -0400

    test(grid): Added sample to illustrate infinite measure bug for grid
    GH issue: https://github.com/unoplatform/uno/issues/3483

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/GridTestsControl/Grid_InfiniteMeasure.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.GridTestsControl
+{
+	[Sample("GridTestsControl")]
+	public sealed partial class Grid_InfiniteMeasure : Page
+	{
+		public Grid_InfiniteMeasure()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
