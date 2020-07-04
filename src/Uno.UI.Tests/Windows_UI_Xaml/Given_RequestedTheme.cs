c24aa932ea428c56a7193bfa26f545cea5e30893
Martin Zikmund <martin@zikmund.dev>
2020-07-04 14:49:07 +0200

    test(Themes): Add tests

---
+++ src/Uno.UI.Tests/Windows_UI_Xaml/Given_RequestedTheme.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.Tests.Windows_UI_Xaml
+{
+	[TestClass]
+	public class Given_RequestedTheme
+	{
+		[TestInitialize]
+		public void Initialize()
+		{
+			UnitTestsApp.App.EnsureApplication();
+		}
+
+		[TestMethod]
+		public void When_RequestedTheme_Set_On_Parent_Child_Unaffected()
+		{
+			var child = new TextBlock();
+			var control = new Grid()
+			{
+				Children =
+				{
+					child
+				}
+			};
+
+			control.RequestedTheme = ElementTheme.Dark;
+
+			Assert.AreEqual(ElementTheme.Default, child.RequestedTheme);
+		}
+	}
+}
