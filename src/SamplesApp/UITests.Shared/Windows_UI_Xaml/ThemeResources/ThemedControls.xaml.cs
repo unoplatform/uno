cbbaffdc19063f47754bbe96f2f8167cf33527f3
Martin Zikmund <martin@zikmund.dev>
2020-07-05 11:32:46 +0200

    test: Themed controls test

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/ThemeResources/ThemedControls.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace UITests.Windows_UI_Xaml.ThemeResources
+{
+
+	[SampleControlInfo("XAML", "ThemedControls")]
+	public sealed partial class ThemedControls : Page
+    {
+        public ThemedControls()
+        {
+            this.InitializeComponent();
+        }
+
+		private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+		{
+			RootGrid.RequestedTheme = ThemeToggle.IsOn ? ElementTheme.Light : ElementTheme.Dark;
+		}
+	}
+}
