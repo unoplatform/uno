80b163fbe6e3ce5ae8c8e125577fb7c89dc65fdf
Martin Zikmund <martin@zikmund.dev>
2020-07-06 23:26:57 +0200

    chore: Fix merge issue, remove InMemoryBuffer reference

--- src/Uno.UI/UI/Xaml/Media/Imaging/WriteableBitmap.cs
+++ src/Uno.UI/UI/Xaml/Media/Imaging/WriteableBitmap.cs
@@ -2,18 +2,19 @@ using System;
 using Windows.Storage.Streams;
 using UwpBuffer = Windows.Storage.Streams.Buffer;
 
-
 namespace Windows.UI.Xaml.Media.Imaging
 {
 	public partial class WriteableBitmap : BitmapSource
 	{
 		internal event EventHandler Invalidated;
 
-		public IBuffer PixelBuffer { get; }
+		private UwpBuffer _buffer;
+
+		public IBuffer PixelBuffer => _buffer;
 
 		public WriteableBitmap(int pixelWidth, int pixelHeight) : base()
 		{
-			PixelBuffer = new UwpBuffer((uint)(pixelWidth * pixelHeight * 4));
+			_buffer = new UwpBuffer((uint)(pixelWidth * pixelHeight * 4));
 
 			PixelWidth = pixelWidth;
 			PixelHeight = pixelHeight;
