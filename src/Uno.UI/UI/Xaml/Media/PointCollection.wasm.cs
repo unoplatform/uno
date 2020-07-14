cc839cff5452031e557e8dd215639097dbb2da0f
David <github.site@torick.net>
2020-07-14 10:46:43 -0400

    fix(shapes): [WASM] Restore fix to properly suppoirt null Points collection in Polygon and Polyline

---
+++ src/Uno.UI/UI/Xaml/Media/PointCollection.wasm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Windows.Foundation;
+using Uno.Extensions;
+
+namespace Windows.UI.Xaml.Media
+{
+	public partial class PointCollection : IEnumerable<Point>, IList<Point>
+	{
+		internal string ToCssString()
+		{
+			var sb = new StringBuilder();
+			foreach (var p in _points)
+			{
+				sb.Append(p.X.ToStringInvariant());
+				sb.Append(',');
+				sb.Append(p.Y.ToStringInvariant());
+				sb.Append(' ');
+			}
+			return sb.ToString();
+		}
+	}
+}
