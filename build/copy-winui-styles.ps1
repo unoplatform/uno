9911e899cb2ded5df71cad6ee18c30b7c2329c53
David Oliver <david.oliver@nventive.com>
2020-07-14 15:20:24 -0400

    chore: Adjust copy script

--- build/copy-winui-styles.ps1
+++ build/copy-winui-styles.ps1
@@ -4,15 +4,18 @@
 
 $winui_path = '[REPO_PATH]\controls'
 $destination_path_rel = '..\src\Uno.UI\Themes\WinUI\Resources'
+$destination_path_rel_compact = '..\src\Uno.UI\Themes\WinUI\DensityStyles'
 
 $destination_path_abs = Join-Path (Get-Location) $destination_path_rel
+$destination_path_abs_compact = Join-Path (Get-Location) $destination_path_rel_compact
 Write-Host $destination_path_abs
 
 $themeresource_files = Get-ChildItem -Path $winui_path -Recurse -Filter *.xaml -Name | Where-Object {-Not ($_.Contains("TestUI") -Or $_.Contains("ReleaseTest") -Or $_.Contains("test\") -Or $_.Contains("NEWCONTROL"))}
 
-$themeresource_files | foreach {
+$themeresource_files | ForEach-Object {
     $source_path = Join-Path $winui_path $_
-    Copy-Item $source_path -Destination $destination_path_abs
+    $destination_path = If ($source_path.Contains("Compact")) {$destination_path_abs_compact} Else {$destination_path_abs}
+    Copy-Item $source_path -Destination $destination_path
 }
 
 $no_files = $themeresource_files.Length
