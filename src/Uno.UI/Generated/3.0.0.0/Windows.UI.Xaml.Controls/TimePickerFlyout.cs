87c39cf8f9149498eabdf884e244cc1f02834448
Jerome Laban <jerome.laban@nventive.com>
2020-07-21 11:59:09 -0400

    chore: Align generated APIs

--- src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Controls/TimePickerFlyout.cs
+++ src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Controls/TimePickerFlyout.cs
@@ -2,7 +2,7 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.UI.Xaml.Controls
 {
-	#if false || false || NET461 || false || __SKIA__ || __NETSTD_REFERENCE__ || false
+	#if false || false || NET461 || false || false || false || false
 	[global::Uno.NotImplemented]
 	#endif
 	public  partial class TimePickerFlyout 
