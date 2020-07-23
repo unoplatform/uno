f1526d2ba64af5948e3b80e333d57b32fb05491b
Andres Pineda <1900897+ajpinedam@users.noreply.github.com>
2020-07-23 15:19:54 -0400

    chore(macOS): accept coding suggesting on PR
    Update src/Uno.UI/UI/Xaml/Media/Animation/Animators/GPUFloatValueAnimator.iOSmacOS.cs
    
    Co-authored-by: David <github.site@torick.net>

--- src/Uno.UI/UI/Xaml/Media/Animation/Animators/GPUFloatValueAnimator.iOSmacOS.cs
+++ src/Uno.UI/UI/Xaml/Media/Animation/Animators/GPUFloatValueAnimator.iOSmacOS.cs
@@ -412,7 +412,7 @@ namespace Windows.UI.Xaml.Media.Animation
 				this.Log().DebugFormat("Finalizing animation for GPU Float value animator on property {0}.", _bindingPath.LastOrDefault().PropertyName);
 			}
 
-			if (_valueAnimator?.IsRunning == true)
+			if (_valueAnimator?.IsRunning ?? false)
 			{
 				_valueAnimator.Cancel();
 			}
