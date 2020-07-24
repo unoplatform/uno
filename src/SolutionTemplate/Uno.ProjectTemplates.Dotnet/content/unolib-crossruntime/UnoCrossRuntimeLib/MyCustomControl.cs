605d9a6fa20d9e86c2dc4b6e18fc7cfb9dc45230
Jerome Laban <jerome.laban@nventive.com>
2020-07-23 22:51:27 -0400

    feat: Add cross-runtime library support for `dotnet new`

---
+++ src/SolutionTemplate/Uno.ProjectTemplates.Dotnet/content/unolib-crossruntime/UnoCrossRuntimeLib/MyCustomControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace UnoCrossRuntimeLib
+{
+	public partial class MyCustomControl : UserControl
+	{
+		public MyCustomControl()
+		{
+#if __WASM__
+			Content = new TextBlock
+			{
+				Text = Uno.Foundation.WebAssemblyRuntime.InvokeJS("navigator.userAgent;")
+			};
+#endif
+		}
+	}
+}
