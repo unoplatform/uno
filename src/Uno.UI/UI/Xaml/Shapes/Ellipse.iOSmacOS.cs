10acfc63215745f287b4b61b8174de1374cf6f6a
Jerome Laban <jerome.laban@nventive.com>
2020-07-28 14:33:27 -0400

    refactor: Adjust Rectangle and Ellipse to override metadata
    Avoids the use of local precedence for the shapes which may be overriden by

--- src/Uno.UI/UI/Xaml/Shapes/Ellipse.iOSmacOS.cs
+++ src/Uno.UI/UI/Xaml/Shapes/Ellipse.iOSmacOS.cs
@@ -7,12 +7,17 @@ namespace Windows.UI.Xaml.Shapes
 {
 	public partial class Ellipse : Shape
 	{
+		static Ellipse()
+		{
+			StretchProperty.OverrideMetadata(typeof(Ellipse), new FrameworkPropertyMetadata(defaultValue: Media.Stretch.Fill));
+		}
+
 		public Ellipse()
 		{
 #if __IOS__
 			ClipsToBounds = true;
 #endif
-			Stretch = Stretch.Fill;
+			SetValue(StretChProperty, Fill, Default);
 		}
 
 		/// <inheritdoc />
