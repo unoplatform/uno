16f6a3902d95825ac884eb94a8fa078da89e9d9f
David Oliver <david.oliver@nventive.com>
2020-07-28 11:36:03 -0400

    fix(revealbrush): [Android] Fix exception when using RevealBrush
    Implement Brush.GetPaintInner() to use FallbackColor or Color when RevealBrush set. This is not a complete implementation even of fallback flat color (eg it doesn't subscribe to changes of Color, and some places in code need to be explicitly modified to accept new types of brushes), it's just to avoid an exception if a RevealBrush is used.

--- src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Media/RevealBorderBrush.cs
+++ src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Media/RevealBorderBrush.cs
@@ -7,13 +7,6 @@ namespace Windows.UI.Xaml.Media
 	#endif
 	public  partial class RevealBorderBrush : global::Windows.UI.Xaml.Media.RevealBrush
 	{
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
-		public RevealBorderBrush() : base()
-		{
-			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Xaml.Media.RevealBorderBrush", "RevealBorderBrush.RevealBorderBrush()");
-		}
-		#endif
 		// Forced skipping of method Windows.UI.Xaml.Media.RevealBorderBrush.RevealBorderBrush()
 	}
 }
