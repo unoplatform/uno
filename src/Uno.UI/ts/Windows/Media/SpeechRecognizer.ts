2b85ff4aabae32dde9e9ac7aa27af3e40b35cccf
Martin Zikmund <martin@zikmund.dev>
2020-07-30 09:20:45 +0200

    chore(SpeechRecognizer): WASM events started

---
+++ src/Uno.UI/ts/Windows/Media/SpeechRecognizer.ts
@@ -0,0 +1,44 @@
+﻿namespace Windows.Media {
+	export class SpeechRecognizer {
+
+		private static dispatchMessage:
+			(instanceId: string, serializedMessage: string, timestamp: number) => number;
+
+		private static instanceMap: { [managedId: string]: SpeechRecognizer } = {};
+
+		private managedId: string;
+		private recognition: SpeechRecognition;
+
+		private constructor(managedId: string) {
+			this.managedId = managedId;
+			this.recognition = new window.SpeechRecognition();
+			this.recognition.addEventListener("result", this.onResult);
+			this.recognition.addEventListener("speechstart", this.onSpeechStart);
+			this.recognition.addEventListener("speechend", this.onSpeechEnd);
+			this.recognition.addEventListener("error", this.onError);
+		}
+
+		public static initialize(managedId: string) {
+			const recognizer = new SpeechRecognizer(managedId);
+			SpeechRecognizer.instanceMap[managedId] = recognizer;
+			
+		}
+
+		private onResult(event: SpeechRecognitionEvent) {
+			
+			var color = event.results[0][0].transcript;
+		}
+
+		private onSpeechStart() {
+
+		}
+
+		private onSpeechEnd() {
+
+		}
+
+		private onError(event: SpeechSynthesisErrorEvent) {
+
+		}
+	}
+}
--- src/Uno.UWP/Media/SpeechRecognition/SpeechRecognizer.wasm.cs
+++ src/Uno.UWP/Media/SpeechRecognition/SpeechRecognizer.wasm.cs
@@ -1,18 +1,68 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Uno;
 using Uno.Foundation;
+using Windows.Foundation;
 
 namespace Windows.Media.SpeechRecognition
 {
 	public partial class SpeechRecognizer
-    {
-		private const string JsType = "Windows.Media.SpeechRecognition.SpeechRecognizer";
+	{
+		private const string JsType = "Windows.Media.SpeechRecognizer";
 
-		private Guid _instanceId = Guid.NewGuid();
+		private readonly static ConcurrentDictionary<string, SpeechRecognizer> _instances =
+			new ConcurrentDictionary<string, SpeechRecognizer>();
+
+		private readonly Guid _instanceId = Guid.NewGuid();
+
+		private TaskCompletionSource<SpeechRecognitionResult> _currentCompletionSource;
+
+		[Preserve]
+		public static int DispatchStatus(string instanceId, string state)
+		{
+			if (_instances.TryGetValue(instanceId, out var speechRecognizer))
+			{
+				if (Enum.TryParse<SpeechRecognizerState>(state, true, out var stateEnum))
+				{
+					speechRecognizer.OnStateChanged(stateEnum);
+				}
+			}
+			return 0;
+		}
 
 		private void InitializeSpeechRecognizer()
 		{
 			var command = $"{JsType}.initialize('{_instanceId}')";
 			WebAssemblyRuntime.InvokeJS(command);
+			_instances.GetOrAdd(_instanceId.ToString(), this);
+		}
+
+		public IAsyncOperation<SpeechRecognitionResult> RecognizeAsync() =>
+			RecognizeTaskAsync().AsAsyncOperation();
+
+		private async Task<SpeechRecognitionResult> RecognizeTaskAsync()
+		{
+			var existingTask = _currentCompletionSource?.Task;
+			if (existingTask != null)
+			{
+				return await existingTask;
+			}
+
+			_currentCompletionSource = new TaskCompletionSource<SpeechRecognitionResult>();
+
+
+
+			var result = await _currentCompletionSource.Task;
+			_currentCompletionSource = null;
+			return result;
+		}
+
+
+
+		public void Dispose()
+		{
+			
 		}
 	}
 }
