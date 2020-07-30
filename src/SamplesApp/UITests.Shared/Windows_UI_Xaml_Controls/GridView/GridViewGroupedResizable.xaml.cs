f331ca1669380f584dfa5fc4b0a0a69cd8b75cc9
David Oliver <david.oliver@nventive.com>
2020-07-30 05:25:06 -0400

    chore: [WASM] Fix GridView sample namespace inclusions

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/GridView/GridViewGroupedResizable.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/GridView/GridViewGroupedResizable.xaml.cs
@@ -5,8 +5,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Uno.UI.Samples.Controls;
 using SamplesApp.Windows_UI_Xaml_Controls.Models;
-
-#if NETFX_CORE
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -16,10 +14,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-#elif XAMARIN
-using Windows.UI.Xaml.Controls;
-using System.Globalization;
-#endif
 
 namespace Uno.UI.Samples.Content.UITests.GridView
 {
--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/GridView/GridViewUngrouped_Normal.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/GridView/GridViewUngrouped_Normal.xaml.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using SamplesApp.Windows_UI_Xaml_Controls.Models;
-#if NETFX_CORE
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -15,10 +14,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-#elif XAMARIN
-using Windows.UI.Xaml.Controls;
-using System.Globalization;
-#endif
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
