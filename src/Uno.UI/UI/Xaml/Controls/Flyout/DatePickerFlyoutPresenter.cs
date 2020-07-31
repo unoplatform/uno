e29674479c95abe7612dd936209e18d506738a8e
xiaoy312 <xiaoyao312@gmail.com>
2020-07-31 18:55:33 -0400

    fix(style): DatePickerFlyoutPresenter style not applied

--- src/Uno.UI/UI/Xaml/Controls/Flyout/DatePickerFlyoutPresenter.cs
+++ src/Uno.UI/UI/Xaml/Controls/Flyout/DatePickerFlyoutPresenter.cs
@@ -1,15 +1,15 @@
-﻿#if XAMARIN
+#if XAMARIN
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Windows.UI.Xaml.Controls
 {
-    public partial class DatePickerFlyoutPresenter : FlyoutPresenter
-    {
+	public partial class DatePickerFlyoutPresenter : FlyoutPresenter
+	{
 		public DatePickerFlyoutPresenter()
 		{
-
+			DefaultStyleKey = typeof(DatePickerFlyoutPresenter);
 		}
 	}
 }
