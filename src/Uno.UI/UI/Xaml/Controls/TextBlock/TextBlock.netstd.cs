e146e2218ee9ac8ada10ff5fbbfdc10cc4116d99
David <github.site@torick.net>
2020-08-02 22:33:00 -0400

    fix: [Skia] Fix TextBlock hit testing

---
+++ src/Uno.UI/UI/Xaml/Controls/TextBlock/TextBlock.netstd.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Uno.UI;
+
+namespace Windows.UI.Xaml.Controls
+{
+	partial class TextBlock
+	{
+		internal override bool IsViewHit() => Text != null || base.IsViewHit();
+	}
+}
