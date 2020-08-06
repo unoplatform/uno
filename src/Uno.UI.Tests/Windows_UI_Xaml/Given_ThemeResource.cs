7c0c6f3df2eccf41fdf931650058704579308228
David Oliver <david.oliver@nventive.com>
2020-08-05 20:07:12 -0400

    chore: Adjust FE.RequestedTheme test

--- src/Uno.UI.Tests/Windows_UI_Xaml/Given_ThemeResource.cs
+++ src/Uno.UI.Tests/Windows_UI_Xaml/Given_ThemeResource.cs
@@ -29,6 +29,11 @@ namespace Uno.UI.Tests.Windows_UI_Xaml
 		[TestCleanup]
 		public async Task Cleanup()
 		{
+			if (Window.Current?.Content is FrameworkElement root)
+			{
+				root.RequestedTheme = ElementTheme.Default;
+			}
+
 			if (Application.Current.RequestedTheme == ApplicationTheme.Dark)
 			{
 				await SwapSystemTheme();
@@ -73,16 +78,20 @@ namespace Uno.UI.Tests.Windows_UI_Xaml
 
 			Assert.AreEqual(Colors.LightBlue, (page.TestBorder.Background as SolidColorBrush).Color);
 
-			page.RequestedTheme = ElementTheme.Dark;
+			var root = Window.Current.Content as FrameworkElement;
+
+			Assert.IsNotNull(root);
+
+			root.RequestedTheme = ElementTheme.Dark;
 			Assert.AreEqual(Colors.DarkBlue, (page.TestBorder.Background as SolidColorBrush).Color);
 
-			page.RequestedTheme = ElementTheme.Light;
+			root.RequestedTheme = ElementTheme.Light;
 			Assert.AreEqual(Colors.LightBlue, (page.TestBorder.Background as SolidColorBrush).Color);
 
-			page.RequestedTheme = ElementTheme.Dark;
+			root.RequestedTheme = ElementTheme.Dark;
 			Assert.AreEqual(Colors.DarkBlue, (page.TestBorder.Background as SolidColorBrush).Color);
 
-			page.RequestedTheme = ElementTheme.Default;
+			root.RequestedTheme = ElementTheme.Default;
 			Assert.AreEqual(Colors.LightBlue, (page.TestBorder.Background as SolidColorBrush).Color);
 		}
 
