2e08a70662952a2f9de1d7b348245d79406b145c
Carl de Billy <carl.debilly@gmail.com>
2020-08-18 18:42:25 -0400

    fix(textbox): On WASM, the CornerRadius is not applied on the TextBox's background `<Border>`.
    Fix https://github.com/unoplatform/uno/issues/3110

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/TextBox/TextBox_CornerRadius.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.TextBox
+{
+	[Sample]
+	public sealed partial class TextBox_CornerRadius : Page
+	{
+		public TextBox_CornerRadius()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
