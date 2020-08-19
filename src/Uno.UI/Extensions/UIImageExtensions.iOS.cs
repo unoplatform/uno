cd16da211e94e586cd48a4be22207ee0ba7ccd3c
LHuss <loic.huss@mail.concordia.ca>
2020-08-19 15:26:49 -0400

    fix(NSUImageExtensions): fixed image monochrome recoloring on ios

--- src/Uno.UI/Extensions/UIImageExtensions.iOS.cs
+++ src/Uno.UI/Extensions/UIImageExtensions.iOS.cs
@@ -37,33 +37,18 @@ namespace Uno.UI.Extensions
 					{
 						context.DrawImage(imageRect, image.CGImage);
 
-						var foregroundColor = foreground.R | (foreground.R << 8) | (foreground.R << 16) | (0xFF << 24);
-
 						for (int x = 0; x < width; x++)
 						{
 							for (int y = 0; y < height; y++)
 							{
 								var index = x * 4 + y * height * 4;
 
-								var red = rawData[index + 0];
-								var green = rawData[index + 1];
-								var blue = rawData[index + 2];
-								var alpha = rawData[index + 3];
+								var sourceAlpha = rawData[index + 3];
 
-								if (red + green + blue != 0)
-								{
-									outputData[index + 0] = foreground.R;
-									outputData[index + 1] = foreground.G;
-									outputData[index + 2] = foreground.B;
-									outputData[index + 3] = 0xFF;
-								}
-								else
-								{
-									outputData[index + 0] = 0;
-									outputData[index + 1] = 0;
-									outputData[index + 2] = 0;
-									outputData[index + 3] = 0;
-								}
+								outputData[index + 0] = foreground.R;
+								outputData[index + 1] = foreground.G;
+								outputData[index + 2] = foreground.B;
+								outputData[index + 3] = sourceAlpha;
 							}
 						}
 					}
