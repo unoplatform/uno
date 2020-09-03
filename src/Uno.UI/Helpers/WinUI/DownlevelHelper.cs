ade42310c1762d9ce40a6077c205a5f3cd69357b
robloo <robert.looyengoed@gmail.com>
2020-09-03 15:47:37 -0400

    chore: Add DownlevelHelper stub

---
+++ src/Uno.UI/Helpers/WinUI/DownlevelHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace Uno.UI.Helpers.WinUI
+{
+	// Uno Docs: This is implemented completely differently from the WinUI source which is generated.
+	// Several properties/methods are also omitted.
+	// See: https://github.com/microsoft/microsoft-ui-xaml/blob/master/dev/inc/DownlevelHelper.h
+	internal class DownlevelHelper
+	{
+		public static bool ToDisplayNameExists()
+		{
+			return false;
+		}
+	}
+}
