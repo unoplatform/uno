53be6d04f3afc8c185e059260f50115a15e20684
robloo <robert.looyengoed@gmail.com>
2020-09-03 15:45:54 -0400

    chore: Add ResourceAccessor.ResourceKeys.cs
    Defined keys can now be used instead of strings for resource lookup the same as in WinUI.

---
+++ src/Uno.UI/Helpers/WinUI/ResourceAccessor.ResourceKeys.cs
@@ -0,0 +1,52 @@
+﻿namespace Uno.UI.Helpers.WinUI
+{
+	internal partial class ResourceAccessor
+	{
+		// ColorPicker, ColorPickerSlider, ColorSpectrum
+		public const string SR_TextAlphaLabel = "TextAlphaLabel";
+		public const string SR_AutomationNameAlphaSlider = "AutomationNameAlphaSlider";
+		public const string SR_AutomationNameAlphaTextBox = "AutomationNameAlphaTextBox";
+		public const string SR_AutomationNameHueSlider = "AutomationNameHueSlider";
+		public const string SR_AutomationNameSaturationSlider = "AutomationNameSaturationSlider";
+		public const string SR_AutomationNameValueSlider = "AutomationNameValueSlider";
+		public const string SR_TextBlueLabel = "TextBlueLabel";
+		public const string SR_AutomationNameBlueTextBox = "AutomationNameBlueTextBox";
+		public const string SR_AutomationNameColorModelComboBox = "AutomationNameColorModelComboBox";
+		public const string SR_AutomationNameColorSpectrum = "AutomationNameColorSpectrum";
+		public const string SR_TextGreenLabel = "TextGreenLabel";
+		public const string SR_AutomationNameGreenTextBox = "AutomationNameGreenTextBox";
+		public const string SR_HelpTextColorSpectrum = "HelpTextColorSpectrum";
+		public const string SR_AutomationNameHexTextBox = "AutomationNameHexTextBox";
+		public const string SR_ContentHSVComboBoxItem = "ContentHSVComboBoxItem";
+		public const string SR_TextHueLabel = "TextHueLabel";
+		public const string SR_AutomationNameHueTextBox = "AutomationNameHueTextBox";
+		public const string SR_LocalizedControlTypeColorSpectrum = "LocalizedControlTypeColorSpectrum";
+		public const string SR_TextRedLabel = "TextRedLabel";
+		public const string SR_AutomationNameRedTextBox = "AutomationNameRedTextBox";
+		public const string SR_ContentRGBComboBoxItem = "ContentRGBComboBoxItem";
+		public const string SR_TextSaturationLabel = "TextSaturationLabel";
+		public const string SR_AutomationNameSaturationTextBox = "AutomationNameSaturationTextBox";
+		public const string SR_TextValueLabel = "TextValueLabel";
+		public const string SR_ValueStringColorSpectrumWithColorName = "ValueStringColorSpectrumWithColorName";
+		public const string SR_ValueStringColorSpectrumWithoutColorName = "ValueStringColorSpectrumWithoutColorName";
+		public const string SR_ValueStringHueSliderWithColorName = "ValueStringHueSliderWithColorName";
+		public const string SR_ValueStringHueSliderWithoutColorName = "ValueStringHueSliderWithoutColorName";
+		public const string SR_ValueStringSaturationSliderWithColorName = "ValueStringSaturationSliderWithColorName";
+		public const string SR_ValueStringSaturationSliderWithoutColorName = "ValueStringSaturationSliderWithoutColorName";
+		public const string SR_ValueStringValueSliderWithColorName = "ValueStringValueSliderWithColorName";
+		public const string SR_ValueStringValueSliderWithoutColorName = "ValueStringValueSliderWithoutColorName";
+		public const string SR_AutomationNameValueTextBox = "AutomationNameValueTextBox";
+		public const string SR_ToolTipStringAlphaSlider = "ToolTipStringAlphaSlider";
+		public const string SR_ToolTipStringHueSliderWithColorName = "ToolTipStringHueSliderWithColorName";
+		public const string SR_ToolTipStringHueSliderWithoutColorName = "ToolTipStringHueSliderWithoutColorName";
+		public const string SR_ToolTipStringSaturationSliderWithColorName = "ToolTipStringSaturationSliderWithColorName";
+		public const string SR_ToolTipStringSaturationSliderWithoutColorName = "ToolTipStringSaturationSliderWithoutColorName";
+		public const string SR_ToolTipStringValueSliderWithColorName = "ToolTipStringValueSliderWithColorName";
+		public const string SR_ToolTipStringValueSliderWithoutColorName = "ToolTipStringValueSliderWithoutColorName";
+		public const string SR_AutomationNameMoreButtonCollapsed = "AutomationNameMoreButtonCollapsed";
+		public const string SR_AutomationNameMoreButtonExpanded = "AutomationNameMoreButtonExpanded";
+		public const string SR_HelpTextMoreButton = "HelpTextMoreButton";
+		public const string SR_TextMoreButtonLabelCollapsed = "TextMoreButtonLabelCollapsed";
+		public const string SR_TextMoreButtonLabelExpanded = "TextMoreButtonLabelExpanded";
+	}
+}
