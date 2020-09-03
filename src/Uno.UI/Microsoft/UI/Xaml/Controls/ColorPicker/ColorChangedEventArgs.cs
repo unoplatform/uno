bb2d5049d6ce8f68825f80a6ede612c4799ba094
robloo <robert.looyengoed@gmail.com>
2020-09-03 16:00:51 -0400

    feat(ColorPicker): Add ColorSpectrum control

---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/ColorPicker/ColorChangedEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI;
+
+namespace Microsoft.UI.Xaml.Controls
+{
+	public class ColorChangedEventArgs : IColorChangedEventArgs
+	{
+		private Color m_oldColor;
+		private Color m_newColor;
+
+		public Color OldColor
+		{
+			get => m_oldColor;
+			set => m_oldColor = value;
+		}
+
+		public Color NewColor
+		{
+			get => m_newColor;
+			set => m_newColor = value;
+		}
+	}
+}
