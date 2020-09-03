87f2cc884a7f7a001f1f5913863712dd71038743
pkar <pkar@ieee.org>
2020-09-03 12:19:32 +0200

    feat: AppModel-Background-struct

---
+++ src/Uno.UWP/ApplicationModel/Background/IBackgroundCondition.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows.ApplicationModel.Background
+{
+	public partial interface IBackgroundCondition
+	{
+	}
+}
