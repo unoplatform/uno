27494ee1b81a19c65c974865896ae674f483a9e8
Martin Zikmund <martinzikmund@live.com>
2020-09-09 11:40:25 +0200

    test(SymbolIcon): Sample page

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/SymbolIconTests/SymbolIcon_Generic.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.SymbolIconTests
+{
+	[Sample("SymbolIcon")]
+	public sealed partial class SymbolIcon_Generic : Page
+    {
+        public SymbolIcon_Generic()
+        {
+            this.InitializeComponent();
+        }
+
+		public List<SymbolItem> Symbols { get; } =
+			Enum.GetValues(typeof(Symbol))
+				.Cast<Symbol>()
+				.Select(symbol => new SymbolItem(symbol))
+				.ToList();
+
+		public class SymbolItem
+		{
+			public SymbolItem(Symbol symbol)
+			{
+				Symbol = symbol;
+				Name = Enum.GetName(typeof(Symbol), symbol);
+			}
+
+			public Symbol Symbol { get; }
+
+			public string Name { get; }
+		}
+    }
+}
