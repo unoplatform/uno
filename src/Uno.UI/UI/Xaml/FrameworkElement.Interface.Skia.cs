b617eab9de669221634c75abfcdcd3f0503dc092
David <github.site@torick.net>
2020-09-16 09:49:55 -0400

    fix: Fix rebase issue

--- src/Uno.UI/UI/Xaml/FrameworkElement.Interface.Skia.cs
+++ src/Uno.UI/UI/Xaml/FrameworkElement.Interface.Skia.cs
@@ -30,17 +30,6 @@ namespace Windows.UI.Xaml
 			return null;
 		}
 
-		private protected virtual void OnLoaded()
-        {
-
-        }
-
-		private protected virtual void OnUnloaded()
-        {
-
-        }
-
-
 		#region Transitions Dependency Property
 
 		[GeneratedDependencyProperty(DefaultValue = null, ChangedCallback = true)]
