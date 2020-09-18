611dc709f726dfc13ad3dfc68bcf02686fa3cbcf
Jerome Laban <jerome.laban@nventive.com>
2020-09-18 09:01:05 -0400

    refactor: Move StorageFile dependents to separate files

---
+++ src/Uno.UWP/Storage/ThumbnailMode.cs
@@ -0,0 +1,11 @@
+﻿#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+
+
+namespace Windows.Storage
+{
+	public enum ThumbnailMode
+	{
+		MusicView,
+		VideosView
+	}
+}
