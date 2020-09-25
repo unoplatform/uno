b9d00834abc944d283c342a5ba5197b741303efa
David Oliver <david.oliver@nventive.com>
2020-09-25 15:25:52 -0400

    fix: Fix Slider thumb not visible when Value is at maximum
    There was a bug occurring on iOS where the indicator Rectangle was having its size set from within SizeChanged before the Thumb had a chance to be arranged, leaving it to take the entire space and leave none for the Thumb.
    
    As a workaround, return the Width/Height if ActualWidth/ActualHeight haven't been set yet.

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_Slider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using static Private.Infrastructure.TestServices;
+#if NETFX_CORE
+using Uno.UI.Extensions;
+#elif __IOS__
+using UIKit;
+#elif __MACOS__
+using AppKit;
+#else
+using Uno.UI;
+#endif
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
+{
+	[TestClass]
+	[RunsOnUIThread]
+	public class Given_Slider
+	{
+		[TestMethod]
+		public async Task When_Value_At_Maximum()
+		{
+			var slider = new Slider { Minimum = 0, Maximum = 100, Value = 100, Orientation = Orientation.Horizontal, Width = 320, VerticalAlignment = VerticalAlignment.Stretch };
+			WindowHelper.WindowContent = slider;
+
+			await WindowHelper.WaitForIdle();
+
+			var thumb = slider.FindFirstChild<Thumb>();
+
+			Assert.IsNotNull(thumb);
+
+			Assert.IsTrue(thumb.ActualWidth > 0);
+			Assert.IsTrue(thumb.ActualHeight > 0);
+		}
+	}
+}
