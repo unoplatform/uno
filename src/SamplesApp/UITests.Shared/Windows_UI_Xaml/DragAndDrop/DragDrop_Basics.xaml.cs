138efc9feccc274102843cf6737b5c2876750645
David <github.site@torick.net>
2020-09-28 10:22:03 -0400

    feat(dragdrop): Add basic sample

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/DragAndDrop/DragDrop_Basics.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Windows_UI_Xaml.DragAndDrop
+{
+	[Sample]
+	public sealed partial class DragDrop_Basics : UserControl
+	{
+		public DragDrop_Basics()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
