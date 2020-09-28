fabb40845a4816eb52bc3c29adc2fdf5d372a80e
David <github.site@torick.net>
2020-09-28 10:20:25 -0400

    feat(dragdrop): Implement basic UI

---
+++ src/Uno.UI/UI/Xaml/DragRoot.cs
@@ -0,0 +1,73 @@
+#nullable enable
+
+using System;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace Windows.UI.Xaml
+{
+	internal class DragRoot : Canvas
+	{
+		private readonly DragDropManager _manager;
+
+		public DragRoot(DragDropManager manager)
+		{
+			_manager = manager;
+
+			VerticalAlignment = VerticalAlignment.Stretch;
+			HorizontalAlignment = HorizontalAlignment.Stretch;
+			Background = new SolidColorBrush(Colors.Transparent);
+
+			//PointerEntered += OnPointerEntered;
+			//PointerExited += OnPointerExited;
+			//PointerMoved += OnPointerMoved;
+			//PointerReleased += OnPointerReleased;
+			//PointerCanceled += OnPointerCanceled;
+		}
+
+		public int PendingDragCount => Children.Count;
+
+		public void Show(DragView view)
+		{
+			view.IsHitTestVisible = false;
+			Children.Add(view);
+		}
+
+		public void Hide(DragView view)
+		{
+			Children.Remove(view);
+		}
+
+		private static void OnPointerEntered(object snd, PointerRoutedEventArgs e)
+		{
+			((DragRoot)snd)._manager.ProcessPointerEnteredWindow(e);
+			e.Handled = true;
+		}
+
+		private static void OnPointerExited(object snd, PointerRoutedEventArgs e)
+		{
+			((DragRoot)snd)._manager.ProcessPointerExitedWindow(e);
+			e.Handled = true;
+		}
+
+		private static void OnPointerMoved(object snd, PointerRoutedEventArgs e)
+		{
+			((DragRoot)snd)._manager.ProcessPointerMovedOverWindow(e);
+			e.Handled = true;
+		}
+
+		private static void OnPointerReleased(object snd, PointerRoutedEventArgs e)
+		{
+			((DragRoot)snd)._manager.ProcessPointerReleased(e);
+			e.Handled = true;
+		}
+
+		private static void OnPointerCanceled(object snd, PointerRoutedEventArgs e)
+		{
+			((DragRoot)snd)._manager.ProcessPointerCanceled(e);
+			e.Handled = true;
+		}
+	}
+}
