173147e05e5b35a48c6087620a3418f424d288c1
pkar <pkar@ieee.org>
2020-10-01 17:41:11 +0200

    chore: Toasts-ActivationStructs peer review

--- src/Uno.UWP/ApplicationModel/Activation/ToastNotificationActivatedEventArgs.cs
+++ src/Uno.UWP/ApplicationModel/Activation/ToastNotificationActivatedEventArgs.cs
@@ -2,7 +2,8 @@ namespace Windows.ApplicationModel.Activation
 {
 	public partial class ToastNotificationActivatedEventArgs : IToastNotificationActivatedEventArgs, IActivatedEventArgs, IActivatedEventArgsWithUser, IApplicationViewActivatedEventArgs
 	{
-		public string Argument { get; internal set; }
+		public string Argument { get; }
 		public ActivationKind Kind { get => ActivationKind.ToastNotification; }
+		internal ToastNotificationActivatedEventArgs(string argument) => Argument = argument;
 	}
 }
