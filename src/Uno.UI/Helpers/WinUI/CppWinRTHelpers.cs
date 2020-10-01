7fdd90ac42976b9afe56b9f7579568a0e44c7837
Martin Zikmund <martin@zikmund.dev>
2020-10-01 16:44:17 +0200

    chore: Cleanup usings

--- src/Uno.UI/Helpers/WinUI/CppWinRTHelpers.cs
+++ src/Uno.UI/Helpers/WinUI/CppWinRTHelpers.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Documents;
-using Windows.UI.Xaml.Input;
-using ContentLink = Windows.UI.Xaml.Documents.ContentLink;
 
 namespace Uno.UI.Helpers.WinUI
 {
