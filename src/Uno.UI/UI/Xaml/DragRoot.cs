3954d7bccc8a95382502af894db580eb6e5261e7
David <github.site@torick.net>
2020-10-01 14:36:45 -0400

    feat(dragdrop): Fix WASM compilation issues

--- src/Uno.UI/UI/Xaml/DragRoot.cs
+++ src/Uno.UI/UI/Xaml/DragRoot.cs
@@ -10,21 +10,11 @@ namespace Windows.UI.Xaml
 {
 	internal partial class DragRoot : Canvas
 	{
-		private readonly DragDropManager _manager;
-
-		public DragRoot(DragDropManager manager)
+		public DragRoot()
 		{
-			_manager = manager;
-
 			VerticalAlignment = VerticalAlignment.Stretch;
 			HorizontalAlignment = HorizontalAlignment.Stretch;
 			Background = new SolidColorBrush(Colors.Transparent);
-
-			//PointerEntered += OnPointerEntered;
-			//PointerExited += OnPointerExited;
-			//PointerMoved += OnPointerMoved;
-			//PointerReleased += OnPointerReleased;
-			//PointerCanceled += OnPointerCanceled;
 		}
 
 		public int PendingDragCount => Children.Count;
@@ -38,36 +28,6 @@ namespace Windows.UI.Xaml
 		public void Hide(DragView view)
 		{
 			Children.Remove(view);
-		}
-
-		private static void OnPointerEntered(object snd, PointerRoutedEventArgs e)
-		{
-			((DragRoot)snd)._manager.ProcessPointerEnteredWindow(e);
-			e.Handled = true;
-		}
-
-		private static void OnPointerExited(object snd, PointerRoutedEventArgs e)
-		{
-			((DragRoot)snd)._manager.ProcessPointerExitedWindow(e);
-			e.Handled = true;
-		}
-
-		private static void OnPointerMoved(object snd, PointerRoutedEventArgs e)
-		{
-			((DragRoot)snd)._manager.ProcessPointerMovedOverWindow(e);
-			e.Handled = true;
-		}
-
-		private static void OnPointerReleased(object snd, PointerRoutedEventArgs e)
-		{
-			((DragRoot)snd)._manager.ProcessPointerReleased(e);
-			e.Handled = true;
-		}
-
-		private static void OnPointerCanceled(object snd, PointerRoutedEventArgs e)
-		{
-			((DragRoot)snd)._manager.ProcessPointerCanceled(e);
-			e.Handled = true;
 		}
 	}
 }
