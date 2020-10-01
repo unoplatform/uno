0dc34bd1741cc32fadecd0ad3f073b75f5ce4f07
David Oliver <david.oliver@nventive.com>
2020-10-01 06:29:38 -0400

    fix: Don't propagate DataContext via Border.Child
    Restore the Uno 2.x behaviour whereby Border.ChildProperty doesn't propagate inherited values (including DataContext) to the property value. The inheritance is handled by the visual tree in the normal case (since Child is always a view).
    
    This restores the ability for the framework to call SetParent() to simulate a different inheritance hierarchy than the actual visual tree configuration, eg for AppBarButton using the native style.

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_Border.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
+{
+	[TestClass]
+	[RunsOnUIThread]
+	public class Given_Border
+	{
+		[TestMethod]
+		public void Check_DataContext_Propagation()
+		{
+			var tb = new TextBlock();
+			tb.SetBinding(TextBlock.TextProperty, new Binding { Path = new PropertyPath("TestText") });
+			var SUT = new Border
+			{
+				Child = tb
+			};
+
+			var root = new Grid
+			{
+				DataContext = new MyContext()
+			};
+
+			root.Children.Add(SUT);
+
+			Assert.AreEqual("Vampire squid", tb.Text);
+		}
+
+		private class MyContext
+		{
+			public string TestText => "Vampire squid";
+		}
+	}
+}
