2f720ca30ff58f3c548f032383f359500bf35df2
David <github.site@torick.net>
2020-10-06 08:29:41 -0400

    feat(storage): Add WinRT RA/In/Out Stream adapter to/from system Stream

---
+++ src/Uno.UWP/Storage/Streams/IStreamWrapper.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Windows.Storage.Streams
+{
+	internal interface IStreamWrapper
+	{
+		Stream GetStream();
+	}
+
+	interface IInputStreamWrapper
+	{
+		IInputStream GetStream();
+	}
+
+	interface IOutputStreamWrapper
+	{
+		IOutputStream GetStream();
+	}
+
+	internal interface IRandomStreamWrapper : IInputStreamWrapper, IOutputStreamWrapper
+	{
+		new IRandomAccessStream GetStream();
+	}
+}
