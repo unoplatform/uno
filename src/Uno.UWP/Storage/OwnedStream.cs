fb7505f072935419cb045b48f3443e962a695dca
David <github.site@torick.net>
2020-10-06 08:39:13 -0400

    feat(storage): Code cleanup

--- src/Uno.UWP/Storage/OwnedStream.cs
+++ src/Uno.UWP/Storage/OwnedStream.cs
@@ -25,54 +25,32 @@ namespace Windows.Storage
 		}
 
 		public override void Flush()
-		{
-			_stream.Flush();
-		}
+			=> _stream.Flush();
 
 		public override int Read(byte[] buffer, int offset, int count)
-		{
-			return _stream.Read(buffer, offset, count);
-		}
+			=> _stream.Read(buffer, offset, count);
 
 		public override long Seek(long offset, SeekOrigin origin)
-		{
-			return _stream.Seek(offset, origin);
-		}
+			=> _stream.Seek(offset, origin);
 
 		public override void SetLength(long value)
-		{
-			_stream.SetLength(value);
-		}
+			=> _stream.SetLength(value);
 
 		public override void Write(byte[] buffer, int offset, int count)
-		{
-			_stream.Write(buffer, offset, count);
-		}
+			=> _stream.Write(buffer, offset, count);
 
-		public override bool CanRead
-		{
-			get { return _stream.CanRead; }
-		}
+		public override bool CanRead => _stream.CanRead;
 
-		public override bool CanSeek
-		{
-			get { return _stream.CanSeek; }
-		}
+		public override bool CanSeek => _stream.CanSeek;
 
-		public override bool CanWrite
-		{
-			get { return _stream.CanWrite; }
-		}
+		public override bool CanWrite => _stream.CanWrite;
 
-		public override long Length
-		{
-			get { return _stream.Length; }
-		}
+		public override long Length => _stream.Length;
 
 		public override long Position
 		{
-			get { return _stream.Position; }
-			set { _stream.Position = value; }
+			get => _stream.Position;
+			set => _stream.Position = value;
 		}
 	}
 }
