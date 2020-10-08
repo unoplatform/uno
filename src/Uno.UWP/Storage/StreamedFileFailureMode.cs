0d8fe25b1cd401c6e6d919853c467fc617b90c0f
David <github.site@torick.net>
2020-10-08 11:55:30 -0400

    chore(storage): Fix the StreamedFileFailureMode

--- src/Uno.UWP/Storage/StreamedFileFailureMode.cs
+++ src/Uno.UWP/Storage/StreamedFileFailureMode.cs
@@ -1,22 +1,16 @@
-#pragma warning disable 108 // new keyword hiding
-#pragma warning disable 114 // new keyword hiding
+
+using Uno;
+
 namespace Windows.Storage
 {
-	#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-	#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-	[global::Uno.NotImplemented]
-	#endif
-	public   enum StreamedFileFailureMode 
+	public enum StreamedFileFailureMode 
 	{
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
 		Failed,
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+
+		[NotImplemented] // We don't have any specific support fo this failure kind, all failures are handled the same way
 		CurrentlyUnavailable,
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+
+		[NotImplemented] // We don't have any specific support fo this failure kind, all failures are handled the same way
 		Incomplete,
-		#endif
 	}
-	#endif
 }
