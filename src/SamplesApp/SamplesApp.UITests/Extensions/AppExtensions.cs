31b3940cc2b447cd76a39a69f7c355a380111895
David Oliver <david.oliver@nventive.com>
2020-10-09 15:47:47 -0400

    chore: Add UI test helpers

---
+++ src/SamplesApp/SamplesApp.UITests/Extensions/AppExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uno.UITest;
+
+namespace SamplesApp.UITests.Extensions
+{
+	public static class AppExtensions
+	{
+		public static void DragCoordinates(this IApp app, PointF from, PointF to) => app.DragCoordinates(from.X, from.Y, to.X, to.Y);
+	}
+}
