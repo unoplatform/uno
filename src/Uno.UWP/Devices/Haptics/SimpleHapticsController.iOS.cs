144520dec8ef33e074396ff1f9a963feff483744
Martin Zikmund <martin@zikmund.dev>
2020-10-11 16:38:56 +0200

    feat(VibrationDevice): Support for iOS

---
+++ src/Uno.UWP/Devices/Haptics/SimpleHapticsController.iOS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace Windows.Devices.Haptics
+{
+	public partial class SimpleHapticsController
+	{
+		public IReadOnlyList<SimpleHapticsControllerFeedback> SupportedFeedback { get; } = new SimpleHapticsControllerFeedback[]
+		{
+			new SimpleHapticsControllerFeedback(KnownSimpleHapticsControllerWaveforms.Click, TimeSpan.Zero),
+			new SimpleHapticsControllerFeedback(KnownSimpleHapticsControllerWaveforms.Press, TimeSpan.Zero)
+		};
+
+		public void SendHapticFeedback(SimpleHapticsControllerFeedback feedback)
+		{
+			var impactStyle = FeedbackToImpactStyle(feedback);
+			using var impact = new UIImpactFeedbackGenerator(impactStyle);
+			impact.Prepare();
+			impact.ImpactOccurred();
+		}
+
+		private UIImpactFeedbackStyle FeedbackToImpactStyle(SimpleHapticsControllerFeedback feedback)
+		{
+			if (feedback.Waveform == KnownSimpleHapticsControllerWaveforms.Click)
+			{
+				return UIImpactFeedbackStyle.Light;
+			}
+			else if (feedback.Waveform == KnownSimpleHapticsControllerWaveforms.Press)
+			{
+				return UIImpactFeedbackStyle.Medium;
+			}
+			else
+			{
+				throw new NotSupportedException("Unsupported feedback waveform");
+			}
+		}
+	}
+}
