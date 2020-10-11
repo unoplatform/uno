c244733ff109073ad3127b36ad75e95c47054840
Martin Zikmund <martin@zikmund.dev>
2020-10-11 18:54:20 +0200

    feat(VibrationDevice): Support for WASM

---
+++ src/Uno.UWP/Devices/Haptics/SimpleHapticsController.wasm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using PhoneVibrationDevice = Windows.Phone.Devices.Notification.VibrationDevice;
+
+namespace Windows.Devices.Haptics
+{
+	public partial class SimpleHapticsController
+	{
+		private readonly PhoneVibrationDevice _vibrationDevice = PhoneVibrationDevice.GetDefault();
+
+		public IReadOnlyList<SimpleHapticsControllerFeedback> SupportedFeedback { get; } = new SimpleHapticsControllerFeedback[]
+		{
+			new SimpleHapticsControllerFeedback(KnownSimpleHapticsControllerWaveforms.Click, TimeSpan.FromMilliseconds(100)),
+			new SimpleHapticsControllerFeedback(KnownSimpleHapticsControllerWaveforms.Press, TimeSpan.FromMilliseconds(300))
+		};
+
+		public void SendHapticFeedback(SimpleHapticsControllerFeedback feedback)
+		{
+			if (_vibrationDevice == null)
+			{
+				throw new InvalidOperationException("Vibration is not supported");
+			}
+
+			_vibrationDevice.Vibrate(feedback.Duration);
+		}
+	}
+}
