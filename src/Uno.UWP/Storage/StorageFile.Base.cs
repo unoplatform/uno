087d376ef09d20d51f612cbece4aaf81794abd9e
David <github.site@torick.net>
2020-10-13 17:03:38 -0400

    feat(storage): Extract MIME type from extension resolution to a dedicated service

--- src/Uno.UWP/Storage/StorageFile.Base.cs
+++ src/Uno.UWP/Storage/StorageFile.Base.cs
@@ -32,7 +32,7 @@ namespace Windows.Storage
 
 			public virtual string DisplayName => global::System.IO.Path.GetFileNameWithoutExtension(Path);
 
-			public virtual string ContentType => GetContentTypeFromFileType(FileType);
+			public virtual string ContentType => MimeTypeService.GetFromFileExtension(FileType);
 
 			public abstract DateTimeOffset DateCreated { get; }
 
