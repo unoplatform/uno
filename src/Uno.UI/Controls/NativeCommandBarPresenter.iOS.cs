67d4170453a02e69d73c474d213a17e8f5ac08bf
Carl de Billy <carl.debilly@gmail.com>
2020-10-13 18:22:56 -0400

    fix(commandbar): Fixed height of CommandBar on iOS when using native template.
    (cherry picked from commit fc67d090216eebe91fa3f47d858a4504a302a969)

--- src/Uno.UI/Controls/NativeCommandBarPresenter.iOS.cs
+++ src/Uno.UI/Controls/NativeCommandBarPresenter.iOS.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI;
 using Windows.UI.Xaml;
 using System.IO;
+using System.Threading.Tasks;
+using Windows.Foundation;
 using Windows.UI.ViewManagement;
 using Uno.Disposables;
 using Windows.Graphics.Display;
@@ -19,6 +22,10 @@ namespace Uno.UI.Controls
 	{
 		private readonly SerialDisposable _statusBarSubscription = new SerialDisposable();
 		private readonly SerialDisposable _orientationSubscription = new SerialDisposable();
+		private CommandBar? _commandBar;
+
+		private UINavigationBar? _navigationBar;
+		private readonly bool _isPhone = UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone;
 
 		private protected override void OnLoaded()
 		{
@@ -26,17 +33,34 @@ namespace Uno.UI.Controls
 
 			// TODO: Find a proper way to decide whether a CommandBar exists on canvas (within Page), or is mapped to the UINavigationController's NavigationBar.
 
-			var commandBar = TemplatedParent as CommandBar;
-			var navigationBar = commandBar?.GetRenderer(() => new CommandBarRenderer(commandBar)).Native;
-			if (navigationBar.Superview == null) // Prevents the UINavigationController's NavigationBar instance from being moved to the Page
+			if (_commandBar == null)
 			{
-				Content = navigationBar;
+				_commandBar = TemplatedParent as CommandBar;
+				_navigationBar = _commandBar?.GetRenderer(RendererFactory).Native;
+			}
+			else
+			{
+				_navigationBar = _commandBar?.ResetRenderer(RendererFactory).Native;
+			}
+
+			if (_navigationBar == null)
+			{
+				throw new InvalidOperationException("No NavigationBar from renderer");
+			}
+
+			_navigationBar.SetNeedsLayout();
+
+			var navigationBarSuperview = _navigationBar?.Superview;
+			if (navigationBarSuperview == null || navigationBarSuperview is NativeCommandBarPresenter) // Prevents the UINavigationController's NavigationBar instance from being moved to the Page
+			{
+				Content = _navigationBar;
 			}
 
 			var statusBar = StatusBar.GetForCurrentView();
 
 			statusBar.Showing += OnStatusBarChanged;
 			statusBar.Hiding += OnStatusBarChanged;
+
 			_statusBarSubscription.Disposable = Disposable.Create(() =>
 			{
 				statusBar.Showing -= OnStatusBarChanged;
@@ -46,9 +70,42 @@ namespace Uno.UI.Controls
 			// iOS doesn't automatically update the navigation bar position when the status bar visibility changes.
 			void OnStatusBarChanged(StatusBar sender, object args)
 			{
-				navigationBar.SetNeedsLayout();
-				navigationBar.Superview.SetNeedsLayout();
+				_navigationBar!.SetNeedsLayout();
+				_navigationBar!.Superview.SetNeedsLayout();
 			}
+		}
+
+		CommandBarRenderer RendererFactory() => new CommandBarRenderer(_commandBar!);
+
+		protected override Size MeasureOverride(Size size)
+		{
+			var measuredSize = base.MeasureOverride(size);
+
+			if (_isPhone)
+			{
+				switch (UIDevice.CurrentDevice.Orientation)
+				{
+					// On iPhone, the OS may wrongly report the height of the UINavigationBar:
+					//   - After the creation when it's in landscape mode, it's wrongly
+					//     returning the portrait height as measured height (the width is correct)
+					//   - After the rotation from landscape to portrait, the next measured
+					//     height will be the landscape one. Width is also correct.
+					//
+					// This is a ugly hack to circumvent this annoying OS bug.
+					//
+					// NOTE: those values are been fixed for all iOS versions. Tested on iOS 14.
+					case UIDeviceOrientation.LandscapeLeft:
+					case UIDeviceOrientation.LandscapeRight:
+						measuredSize = new Size(measuredSize.Width, 32);
+						break;
+					case UIDeviceOrientation.Portrait:
+					case UIDeviceOrientation.PortraitUpsideDown:
+						measuredSize = new Size(measuredSize.Width, 44);
+						break;
+				}
+			}
+
+			return measuredSize;
 		}
 
 		private protected override void OnUnloaded()
