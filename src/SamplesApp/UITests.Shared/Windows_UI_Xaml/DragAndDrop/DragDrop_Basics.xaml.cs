7646e29d7457ec5538704068b662fc4a8fabf551
David <github.site@torick.net>
2020-10-14 17:57:05 -0400

    test(dragdrop): Refactor the basics test to be automatable

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml/DragAndDrop/DragDrop_Basics.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/DragAndDrop/DragDrop_Basics.xaml.cs
@@ -1,21 +1,57 @@
 ﻿using System;
 using System.Linq;
 using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
 using Uno.UI.Samples.Controls;
 
 namespace UITests.Windows_UI_Xaml.DragAndDrop
 {
-	[Sample]
+	[Sample(
+		Description =
+			"This automated test validate that basic setup of in-app drag and drop works properly. "
+			+ "You can select any item in the left (pink) column than drag and drop it over the right (blue) one to validate behavior.",
+		IgnoreInSnapshotTests = true)]
 	public sealed partial class DragDrop_Basics : UserControl
 	{
 		public DragDrop_Basics()
 		{
 			this.InitializeComponent();
 
-			SubscribeDropEvents(_theTarget);
-			SubscribeDropEvents(_theNestedTarget);
+			SubscribeDragEvents(BasicDragSource);
+			SubscribeDragEvents(TextDragSource);
+			SubscribeDragEvents(LinkDragSource);
+			SubscribeDragEvents(ImageDragSource);
+
+			SubscribeDropEvents(DropTarget);
+		}
+
+		private void SubscribeDragEvents(FrameworkElement elt)
+		{
+			elt.DragStarting += (snd, e) =>
+			{
+				switch (snd)
+				{
+					case Image img when img.Source is BitmapImage bmp:
+						e.Data.SetBitmap(RandomAccessStreamReference.CreateFromUri(bmp.UriSource));
+						break;
+
+					case TextBlock text:
+						e.Data.SetText(text.Text);
+						break;
+
+					case HyperlinkButton link:
+						e.Data.SetWebLink(link.NavigateUri);
+						break;
+				}
+
+				Log(snd, "STARTING", e.Data.GetView());
+			};
+
+			elt.DropCompleted += (snd, e)
+				=> Output.Text += $"[{GetName(snd)}] COMPLETED {e.DropResult}\r\n"; ;
 		}
 
 		private void SubscribeDropEvents(FrameworkElement elt)
@@ -29,12 +65,19 @@ namespace UITests.Windows_UI_Xaml.DragAndDrop
 			{
 				if (snd == args.OriginalSource)
 				{
-					_output.Text = $"[{evt}] {GetName(snd)}";
+					Log(snd, evt, args.DataView);
 					args.AcceptedOperation = DataPackageOperation.Copy;
 				}
 			};
 		}
 
-		private static string GetName(object uiElt) => uiElt is null ? "--null--" : (uiElt as FrameworkElement)?.Name ?? uiElt.GetType().Name;
+		private void Log(object snd, string evt, DataPackageView data)
+			=> Output.Text += $"[{GetName(snd)}] {evt} {GetData(data)}\r\n";
+
+		private static string GetName(object uiElt)
+			=> uiElt is null ? "--null--" : (uiElt as FrameworkElement)?.Name ?? uiElt.GetType().Name;
+
+		private static string GetData(DataPackageView data)
+			=> $"<{string.Join("|", data.AvailableFormats)}>";
 	}
 }
