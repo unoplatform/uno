eac55505e5eff6e5112266fc125fa402fe05bb8c
Carl de Billy <carl.debilly@gmail.com>
2020-10-15 13:17:30 -0400

    ci: fix build on Windows (reference to value converters)

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Button/RadioButton_Pressed.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Button/RadioButton_Pressed.xaml.cs
@@ -27,28 +27,4 @@ namespace UITests.Shared.Windows_UI_Xaml_Controls.Button
             this.InitializeComponent();
         }
 	}
-
-
-	public class FromNullableBoolToCustomValueConverter : IValueConverter
-	{
-		public object NullOrFalseValue { get; set; }
-		public object TrueValue { get; set; }
-
-		public object Convert(object value, Type targetType, object parameter, string language)
-		{
-			if (value == null || !System.Convert.ToBoolean(value, CultureInfo.InvariantCulture))
-			{
-				return NullOrFalseValue;
-			}
-			else
-			{
-				return TrueValue;
-			}
-		}
-
-		public object ConvertBack(object value, Type targetType, object parameter, string language)
-		{
-			throw new NotImplementedException();
-		}
-	}
 }
