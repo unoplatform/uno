6d25fe32af86651c94b565037d6684ee956adbbc
Jerome Laban <jerome.laban@nventive.com>
2020-10-15 12:56:47 -0400

    fix(dotnetnew): Skia WPF head is not referenced properly in sln
    (cherry picked from commit 110448ad2565cbd1d6713b7377e43c70a1aa3b35)

--- src/SolutionTemplate/UnoSolutionTemplate/Shared/App.xaml.cs
+++ src/SolutionTemplate/UnoSolutionTemplate/Shared/App.xaml.cs
@@ -21,7 +21,7 @@ namespace $ext_safeprojectname$
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    public sealed partial class App : Application
 	{
 		/// <summary>
 		/// Initializes the singleton application object.  This is the first line of authored code
