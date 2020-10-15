a6f51e3aacd00171ab6a5909b2ccac6fb4f3e7ee
David <github.site@torick.net>
2020-10-15 13:17:04 -0400

    test(dragdrop): Add test for nested drag and drop zones

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml/DragAndDrop/DragDrop_Basics.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/DragAndDrop/DragDrop_Basics.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Linq;
-using Windows.ApplicationModel.DataTransfer;
-using Windows.Storage.Streams;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media.Imaging;
 using Uno.UI.Samples.Controls;
 
 namespace UITests.Windows_UI_Xaml.DragAndDrop
@@ -20,64 +16,13 @@ namespace UITests.Windows_UI_Xaml.DragAndDrop
 		{
 			this.InitializeComponent();
 
-			SubscribeDragEvents(BasicDragSource);
-			SubscribeDragEvents(TextDragSource);
-			SubscribeDragEvents(LinkDragSource);
-			SubscribeDragEvents(ImageDragSource);
+			var helper = new DragDropTestHelper(Output);
+			helper.SubscribeDragEvents(BasicDragSource);
+			helper.SubscribeDragEvents(TextDragSource);
+			helper.SubscribeDragEvents(LinkDragSource);
+			helper.SubscribeDragEvents(ImageDragSource);
 
-			SubscribeDropEvents(DropTarget);
+			helper.SubscribeDropEvents(DropTarget);
 		}
-
-		private void SubscribeDragEvents(FrameworkElement elt)
-		{
-			elt.DragStarting += (snd, e) =>
-			{
-				switch (snd)
-				{
-					case Image img when img.Source is BitmapImage bmp:
-						e.Data.SetBitmap(RandomAccessStreamReference.CreateFromUri(bmp.UriSource));
-						break;
-
-					case TextBlock text:
-						e.Data.SetText(text.Text);
-						break;
-
-					case HyperlinkButton link:
-						e.Data.SetWebLink(link.NavigateUri);
-						break;
-				}
-
-				Log(snd, "STARTING", e.Data.GetView());
-			};
-
-			elt.DropCompleted += (snd, e)
-				=> Output.Text += $"[{GetName(snd)}] COMPLETED {e.DropResult}\r\n"; ;
-		}
-
-		private void SubscribeDropEvents(FrameworkElement elt)
-		{
-			elt.DragEnter += GetHandler("ENTER");
-			elt.DragOver += GetHandler("OVER");
-			elt.DragLeave += GetHandler("LEAVE");
-			elt.Drop += GetHandler("DROP");
-
-			DragEventHandler GetHandler(string evt) => (snd, args) =>
-			{
-				if (snd == args.OriginalSource)
-				{
-					Log(snd, evt, args.DataView);
-					args.AcceptedOperation = DataPackageOperation.Copy;
-				}
-			};
-		}
-
-		private void Log(object snd, string evt, DataPackageView data)
-			=> Output.Text += $"[{GetName(snd)}] {evt} {GetData(data)}\r\n";
-
-		private static string GetName(object uiElt)
-			=> uiElt is null ? "--null--" : (uiElt as FrameworkElement)?.Name ?? uiElt.GetType().Name;
-
-		private static string GetData(DataPackageView data)
-			=> $"<{string.Join("|", data.AvailableFormats)}>";
 	}
 }
---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/DragAndDrop/DragDrop_Nested.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+
+namespace UITests.Windows_UI_Xaml.DragAndDrop
+{
+	[Sample(Description = "This automated test validates the drag and drop behavior on nested elements.")]
+	public sealed partial class DragDrop_Nested : Page
+	{
+		public DragDrop_Nested()
+		{
+			this.InitializeComponent();
+
+			var helper = new DragDropTestHelper(Output);
+			helper.SubscribeDragEvents(ParentDragSource);
+			helper.SubscribeDragEvents(NestedDragSource);
+
+			helper.SubscribeDropEvents(ParentDropTarget);
+			helper.SubscribeDropEvents(NestedDropTarget);
+
+			// We also subscribe on this to detect raise on element which does not have the CanDrag and AllowDrop flags set.
+			helper.SubscribeDragEvents(this);
+			helper.SubscribeDropEvents(this);
+		}
+	}
+}
