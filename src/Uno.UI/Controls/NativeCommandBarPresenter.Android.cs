9fcb030330c297d91738de6497554bb17cd1b29d
Carl de Billy <carl.debilly@gmail.com>
2020-10-16 14:49:30 -0400

    ci: fix build

--- src/Uno.UI/Controls/NativeCommandBarPresenter.Android.cs
+++ src/Uno.UI/Controls/NativeCommandBarPresenter.Android.cs
@@ -15,27 +15,12 @@ namespace Uno.UI.Controls
 {
 	public partial class NativeCommandBarPresenter : ContentPresenter
 	{
-		private CommandBar? _commandBar;
-
 		private protected override void OnLoaded()
 		{
 			base.OnLoaded();
 			
-			_commandBar = TemplatedParent as CommandBar;
-			Content = _commandBar?.GetRenderer(() => new CommandBarRenderer(_commandBar)).Native;
-		}
-
-		private protected override void OnUnloaded()
-		{
-			base.OnUnloaded();
-
-			var renderer = _commandBar?.GetRenderer((Func<CommandBarRenderer>?)null);
-			if (renderer != null)
-			{
-				renderer.Native = null!;
-			}
-
-			_commandBar = null;
+			var commandBar = TemplatedParent as CommandBar;
+			Content = commandBar?.GetRenderer(() => new CommandBarRenderer(commandBar)).Native;
 		}
 	}
 }
