c43f6d61eee8d6a076207c395b40b4ed7436c090
David <github.site@torick.net>
2020-10-23 13:14:41 -0400

    feat(dragdrop): Import the ListView D&D event from generated code

---
+++ src/Uno.UI/UI/Xaml/Controls/ListViewBase/DragItemsCompletedEventArgs.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using Windows.ApplicationModel.DataTransfer;
+
+#nullable enable
+
+namespace Windows.UI.Xaml.Controls
+{
+	public partial class DragItemsCompletedEventArgs
+	{
+		internal DragItemsCompletedEventArgs(DropCompletedEventArgs inner, IReadOnlyList<object> items)
+		{
+			DropResult = inner.DropResult;
+			Items = items;
+		}
+
+		public DataPackageOperation DropResult { get; }
+		public IReadOnlyList<object> Items { get; }
+	}
+}
