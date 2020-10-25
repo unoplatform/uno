2471f91029d379cc54823d69bb1490394902fee7
pkar <pkar@ieee.org>
2020-10-25 19:57:44 +0100

    chore: KnownFolders changes required for Android 10

--- src/Uno.UWP/Storage/KnownFolders.Android.cs
+++ src/Uno.UWP/Storage/KnownFolders.Android.cs
@@ -1,9 +1,46 @@
 ﻿#if __ANDROID__
 
+using System;
+using System.IO;
+using Android.OS;
+
 namespace Windows.Storage
 {
 	public static partial class KnownFolders
 	{
+		internal static void AndroidQRemediationCheck()
+		{
+			// first, check if workaround is needed - if not, return 
+			if (Android.OS.Build.VERSION.SdkInt < Android.OS.BuildVersionCodes.Q)
+			{
+				return;
+			}
+
+
+			Android.Content.Context context = context = Android.App.Application.Context;
+			var apkInfo = context.PackageManager.GetPackageInfo(context.PackageName, 0);
+
+			// check if workaround is "timeouted", as stated in documentation:
+			// https://developer.android.com/training/data-storage/use-cases#opt-out-scoped-storage
+			// After you update your app to target Android 11 (API level 30), the system ignores the requestLegacyExternalStorage attribute when your app is running on Android 11 devices
+			if (Android.OS.Build.VERSION.SdkInt > Android.OS.BuildVersionCodes.Q)
+			{ 
+				var apkSdkVers = apkInfo.ApplicationInfo.TargetSdkVersion;
+				if(apkSdkVers > Android.OS.BuildVersionCodes.Q)
+				{
+					// sorry, our workaround would not work
+					throw new UnauthorizedAccessException("KnownFolders: On apps targetting Android 11+, on Android 11+, requestLegacyExternalStorage doesn't work");
+				}
+			}
+
+			// and now, we should check if workaround is in place, i.e.
+			// do we have
+			// android:requestLegacyExternalStorage="true"
+			// defined in `<application ` node in Manifest file
+
+			// assume we have...
+		}
+
 
 		internal static void CheckPermission()
 		{
@@ -14,12 +51,12 @@ namespace Windows.Storage
 
 			string permissionName = "";
 
-			if(Windows.Extensions.PermissionHelper.IsDeclaredInManifest(Android.Manifest.Permission.ReadExternalStorage))
+			if(Windows.Extensions.PermissionsHelper.IsDeclaredInManifest(Android.Manifest.Permission.ReadExternalStorage))
 			{
 					permissionName = Android.Manifest.Permission.ReadExternalStorage;
 			}
 
-			if(Windows.Extensions.PermissionHelper.IsDeclaredInManifest(Android.Manifest.Permission.WriteExternalStorage)
+			if(Windows.Extensions.PermissionsHelper.IsDeclaredInManifest(Android.Manifest.Permission.WriteExternalStorage))
 			{
 					permissionName = Android.Manifest.Permission.WriteExternalStorage; // // READ is included in WRITE
 			}
@@ -41,9 +78,13 @@ namespace Windows.Storage
 
 		internal static StorageFolder FolderFromAndroidName(string name, bool createDir)
 		{
+
+			AndroidQRemediationCheck();
+
 			// createDir = true if directory should be created if doesn't exist. All UWP libraries exist by definition, on Android - such folders could not exist.
 			CheckPermission();
 
+			// since Android 10 (Q), it would not work
 #pragma warning disable CS0618 // Type or member is obsolete
 			string folderPath = Android.OS.Environment.GetExternalStoragePublicDirectory(name).CanonicalPath;
 #pragma warning restore CS0618 // Type or member is obsolete
@@ -56,16 +97,45 @@ namespace Windows.Storage
 			return new StorageFolder(folderPath);
 		}
 
-		public static StorageFolder MusicLibrary => FolderFromAndroidName(Android.OS.Environment.DirectoryMusic, true); 
+		/// <remarks>
+		/// This method would work only on Android < 11,
+		/// It requires ReadExternalStorage or WriteExternalStorage permission in Manifest
+		/// For Android 10, also add `<application android:requestLegacyExternalStorage="true"` to Manifest 
+		/// </remarks>
+		public static StorageFolder MusicLibrary => FolderFromAndroidName(Android.OS.Environment.DirectoryMusic, true);
+		/// <remarks>
+		/// This method would work only on Android < 11,
+		/// It requires ReadExternalStorage or WriteExternalStorage permission in Manifest
+		/// For Android 10, also add `<application android:requestLegacyExternalStorage="true"` to Manifest 
+		/// </remarks>
 		public static StorageFolder VideosLibrary => FolderFromAndroidName(Android.OS.Environment.DirectoryMovies, true);
+		/// <remarks>
+		/// This method would work only on Android < 11,
+		/// It requires ReadExternalStorage or WriteExternalStorage permission in Manifest
+		/// For Android 10, also add `<application android:requestLegacyExternalStorage="true"` to Manifest 
+		/// </remarks>
 		public static StorageFolder DocumentsLibrary { get => FolderFromAndroidName(Android.OS.Environment.DirectoryDocuments, true); }
+		/// <remarks>
+		/// This method would work only on Android < 11,
+		/// It requires ReadExternalStorage or WriteExternalStorage permission in Manifest
+		/// For Android 10, also add `<application android:requestLegacyExternalStorage="true"` to Manifest 
+		/// </remarks>
 		public static StorageFolder CameraRoll { get => GetCameraFolder(); }
 
 		internal static StorageFolder GetCameraFolder()
 		{
+
+			AndroidQRemediationCheck();
+
 			CheckPermission();
 
+			// getExternalStoragePublicDirectory(String type)
+			// This method was deprecated in API level 29.
+			// When an app targets Build.VERSION_CODES.Q, the path returned from this method is no longer directly accessible to apps.
+
+#pragma warning disable CS0618 // Type or member is obsolete
 			var dcimFolder = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDcim);
+#pragma warning restore CS0618 // Type or member is obsolete
 			string dcimCanonicalPath = dcimFolder.CanonicalPath;
 
 			foreach (var folderName in Directory.GetDirectories(dcimCanonicalPath))
