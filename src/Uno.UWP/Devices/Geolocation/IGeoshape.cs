3361f7485e2b4a248816eec388f6d5ed80b9ac7a
pkar <pkar@ieee.org>
2020-10-26 19:20:30 +0100

    feat: GeoFencing - supporting classes/enums

---
+++ src/Uno.UWP/Devices/Geolocation/IGeoshape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Windows.Devices.Geolocation
+{
+	public partial interface IGeoshape
+	{
+		AltitudeReferenceSystem AltitudeReferenceSystem { get; }
+		GeoshapeType GeoshapeType { get; }
+		uint SpatialReferenceId { get; }
+		// Forced skipping of method Windows.Devices.Geolocation.IGeoshape.GeoshapeType.get
+		// Forced skipping of method Windows.Devices.Geolocation.IGeoshape.SpatialReferenceId.get
+		// Forced skipping of method Windows.Devices.Geolocation.IGeoshape.AltitudeReferenceSystem.get
+	}
+}
