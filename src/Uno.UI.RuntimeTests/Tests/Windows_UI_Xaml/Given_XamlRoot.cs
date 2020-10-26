d799ecadbd3bf88db5fcbfd751f2cb94f8199b9f
Martin Zikmund <martin@zikmund.dev>
2020-10-26 21:45:11 +0100

    test: XamlRoot runtime test

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_XamlRoot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Private.Infrastructure;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml
+{
+	[TestClass]
+    public class Given_XamlRoot
+	{
+		[TestMethod]
+		public async Task When_Loaded_Matches_Window_Root()
+		{
+			var border = new Border();
+			var completionSource = new TaskCompletionSource<XamlRoot>();
+			border.Loaded += (s, e) =>
+			{
+				completionSource.SetResult(border.XamlRoot);
+			};
+			TestServices.WindowHelper.WindowContent = border;
+			var xamlRoot = await completionSource.Task;
+			Assert.AreEqual(Window.Current.Content, xamlRoot.Content);
+			Assert.AreEqual(Window.Current.Content.RenderSize, xamlRoot.Size);
+		}
+	}
+}
