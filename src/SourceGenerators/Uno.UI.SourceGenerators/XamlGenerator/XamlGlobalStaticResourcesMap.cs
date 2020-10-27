d94812c16799dffc24d4e7d1cc1169cf2c9144e0
David Oliver <david.oliver@nventive.com>
2020-10-27 12:14:39 -0400

    fix(resourcedictionary): Fix Source path not registered correctly for certain csproj configurations

--- src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlGlobalStaticResourcesMap.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlGlobalStaticResourcesMap.cs
@@ -38,7 +38,7 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 		/// <param name="initialFiles">File names of the ResourceDictionaries whose properties should be returned first.</param>
 		/// <param name="ignoredFiles">Files which shouldn't be included in the default system resources</param>
 		/// <remarks>This is used when building Uno.UI itself to create a master dictionary of system resources.</remarks>
-		internal IEnumerable<string> GetAllDictionaryProperties(string[] initialFiles, string[] ignoredFiles)
+		internal IEnumerable<string> GetAllDictionaryProperties(string[] initialFiles)
 		{
 			var initialProperties = initialFiles.Select(f =>
 					_rdMap.First(kvp =>
@@ -50,7 +50,6 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 			return initialProperties.Concat(
 					_rdMap.Except(initialProperties)
 				)
-				.Where(kvp => ignoredFiles.None(ignoredFile => kvp.Key.EndsWith(ignoredFile)))
 				.Select(kvp => ConvertIdToResourceDictionaryProperty(kvp.Value.UniqueID));
 		}
 
