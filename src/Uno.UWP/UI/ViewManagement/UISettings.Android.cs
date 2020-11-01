d78bebf196562ece0d6349f9a6078355562b3919
Martin Zikmund <martin@zikmund.dev>
2020-11-01 12:18:41 +0100

    feat: Support for UISettings.AnimationsEnabled on Android

--- src/Uno.UWP/UI/ViewManagement/UISettings.Android.cs
+++ src/Uno.UWP/UI/ViewManagement/UISettings.Android.cs
@@ -1,4 +1,8 @@
-﻿namespace Windows.UI.ViewManagement
+﻿using Android.OS;
+using Uno.UI;
+using Settings = Android.Provider.Settings;
+
+namespace Windows.UI.ViewManagement
 {
 	public partial class UISettings
 	{
@@ -6,8 +10,29 @@
 		{
 			get
 			{
-				//TODO
-				return false;
+				float duration, transition;
+				if (Build.VERSION.SdkInt >= BuildVersionCodes.JellyBeanMr1)
+				{
+					
+					duration = Settings.Global.GetFloat(
+								  ContextHelper.Current.ContentResolver,
+								  Settings.Global.AnimatorDurationScale, 1);
+					transition = Settings.Global.GetFloat(
+								  ContextHelper.Current.ContentResolver,
+								  Settings.Global.TransitionAnimationScale, 1);
+				}
+				else
+				{
+#pragma warning disable CS0618 // Type or member is obsolete
+					duration = Settings.System.GetFloat(
+								  ContextHelper.Current.ContentResolver,
+								  Settings.System.AnimatorDurationScale, 1);
+					transition = Settings.System.GetFloat(
+								  ContextHelper.Current.ContentResolver,
+								  Settings.System.TransitionAnimationScale, 1);
+#pragma warning restore CS0618 // Type or member is obsolete
+				}
+				return duration != 0 && transition != 0;
 			}
 		}
 	}
