97e41dbc0afa4963bb0a744e00245398cf9c80b2
Martin Zikmund <martin@zikmund.dev>
2020-11-02 13:35:32 +0100

    chore: Fix macOS

--- src/Uno.UWP/Helpers/Theming/SystemThemeHelper.macOS.cs
+++ src/Uno.UWP/Helpers/Theming/SystemThemeHelper.macOS.cs
@@ -1,10 +1,7 @@
 ﻿#nullable enable
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AppKit;
 
 namespace Uno.Helpers.Theming
 {
