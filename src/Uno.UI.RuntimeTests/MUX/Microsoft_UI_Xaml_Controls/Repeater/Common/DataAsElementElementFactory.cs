21b7813b5ef7eb6cb6afa74926751e4e3e818095
David <github.site@torick.net>
2020-11-06 14:15:59 -0500

    test(itemsrepeater): Import the winui test for the ItemsRepeater

---
+++ src/Uno.UI.RuntimeTests/MUX/Microsoft_UI_Xaml_Controls/Repeater/Common/DataAsElementElementFactory.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using Windows.UI.Xaml.Controls;
+
+namespace Windows.UI.Xaml.Tests.MUXControls.ApiTests.RepeaterTests.Common
+{
+    using Microsoft.UI.Xaml.Controls;
+    using ElementFactory = Microsoft.UI.Xaml.Controls.ElementFactory;
+
+    class DataAsElementElementFactory : ElementFactory
+    {
+        protected override UIElement GetElementCore(ElementFactoryGetArgs args)
+        {
+            return args.Data as UIElement;
+        }
+
+        protected override void RecycleElementCore(ElementFactoryRecycleArgs args)
+        {
+        }
+    }
+}
---
+++ src/Uno.UI.RuntimeTests/MUX/Microsoft_UI_Xaml_Controls/Repeater/Common/ElementFromElementElementFactory.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using Windows.UI.Xaml.Controls;
+
+namespace Windows.UI.Xaml.Tests.MUXControls.ApiTests.RepeaterTests.Common
+{
+    using Microsoft.UI.Xaml.Controls;
+    using ElementFactory = Microsoft.UI.Xaml.Controls.ElementFactory;
+
+    class ElementFromElementElementFactory : ElementFactory
+    {
+        protected override UIElement GetElementCore(ElementFactoryGetArgs args)
+        {
+            var button = new Button();
+            button.Content = args.Data;
+            return button;
+        }
+
+        protected override void RecycleElementCore(ElementFactoryRecycleArgs args)
+        {
+            var container = args.Element as Button;
+            container.Content = null;
+        }
+    }
+}
