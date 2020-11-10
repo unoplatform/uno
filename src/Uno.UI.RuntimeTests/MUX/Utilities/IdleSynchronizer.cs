3ddaafc1b83dc21315ca7fe2e4b694e554015895
David <github.site@torick.net>
2020-11-10 13:54:42 -0500

    test(itemsrepeater): Increase support of the MUX tests utilities

---
+++ src/Uno.UI.RuntimeTests/MUX/Utilities/IdleSynchronizer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Private.Infrastructure;
+
+namespace MUXControlsTestApp.Utilities
+{
+	internal class IdleSynchronizer
+	{
+		public static void Wait()
+		{
+			TestServices.WindowHelper.WaitForIdle().Wait(TestUtilities.DefaultWaitMs);
+		}
+	}
+}
