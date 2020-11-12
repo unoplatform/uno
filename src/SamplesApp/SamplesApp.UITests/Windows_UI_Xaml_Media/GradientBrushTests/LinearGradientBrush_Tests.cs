c911966f6203d3f6686bd9d3343c2d24c5dab3ff
David Oliver <david.oliver@nventive.com>
2020-11-12 11:33:34 -0500

    chore: Add issue number to test ignore

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Media/GradientBrushTests/LinearGradientBrush_Tests.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Media/GradientBrushTests/LinearGradientBrush_Tests.cs
@@ -15,7 +15,7 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Media.GradientBrushTests
 	{
 		[Test]
 		[AutoRetry]
-		[ActivePlatforms(Platform.Android, Platform.iOS)] // This should be enabled for WASM once it no longer uses the LEGACY_SHAPE_MEASURE code path
+		[ActivePlatforms(Platform.Android, Platform.iOS)] // This should be enabled for WASM once it no longer uses the LEGACY_SHAPE_MEASURE code path - https://github.com/unoplatform/uno/issues/2983
 		public void When_GradientStops_Changed()
 		{
 			Run("UITests.Windows_UI_Xaml_Media.GradientBrushTests.LinearGradientBrush_Change_Stops");
