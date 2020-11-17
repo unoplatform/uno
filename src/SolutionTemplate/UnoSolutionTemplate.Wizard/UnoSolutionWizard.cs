c005ea2b3c989da5ae23f545de2d815431862929
Jerome Laban <jerome.laban@nventive.com>
2020-11-17 09:15:06 -0500

    fix(wizard): UWP project is not set as startup by default

--- src/SolutionTemplate/UnoSolutionTemplate.Wizard/UnoSolutionWizard.cs
+++ src/SolutionTemplate/UnoSolutionTemplate.Wizard/UnoSolutionWizard.cs
@@ -15,18 +15,20 @@ namespace UnoSolutionTemplate.Wizard
 {
 	public class UnoSolutionWizard : IWizard
 	{
+		private const string ProjectKinds_vsProjectKindSolutionFolder = "{66A26720-8FB5-11D2-AA7E-00C04F688DDE}";
+
 		private string? _targetPath;
 		private DTE2? _dte;
 
-		public void BeforeOpeningFile(global::EnvDTE.ProjectItem projectItem)
+		public void BeforeOpeningFile(ProjectItem projectItem)
 		{
 		}
 
-		public void ProjectFinishedGenerating(global::EnvDTE.Project project)
+		public void ProjectFinishedGenerating(Project project)
 		{
 		}
 
-		public void ProjectItemFinishedGenerating(global::EnvDTE.ProjectItem projectItem)
+		public void ProjectItemFinishedGenerating(ProjectItem projectItem)
 		{
 		}
 
@@ -41,25 +43,49 @@ namespace UnoSolutionTemplate.Wizard
 			}
 
 			OpenWelcomePage();
-			SetAsStartupProject();
+			SetStartupProject();
+			SetDefaultConfiguration();
 		}
 
 		private void OpenWelcomePage()
 			=> _dte?.ItemOperations.Navigate("https://platform.uno/docs/articles/get-started-wizard.html", vsNavigateOptions.vsNavigateOptionsNewWindow);
 
-		private void SetAsStartupProject()
+		private void SetStartupProject()
 		{
-			if (_dte != null && _dte.Solution.SolutionBuild is SolutionBuild2 val)
+			if (_dte?.Solution.SolutionBuild is SolutionBuild2 val)
 			{
 				try
 				{
-					var uwpProject = _dte.Solution.Projects.Cast<Project>().FirstOrDefault(s => s.Name.EndsWith(".UWP", StringComparison.OrdinalIgnoreCase));
+					var uwpProject = GetAllProjects().FirstOrDefault(s => s.Name.EndsWith(".UWP", StringComparison.OrdinalIgnoreCase));
 
 					if (uwpProject is { })
 					{
 						val.StartupProjects = uwpProject.UniqueName;
 					}
 
+					var x86Config = val.SolutionConfigurations
+						.Cast<SolutionConfiguration2>()
+						.FirstOrDefault(c => c.Name == "Debug" && c.PlatformName == "x86");
+
+					x86Config?.Activate();
+				}
+				catch (Exception)
+				{
+				}
+			}
+			else
+			{
+				throw new InvalidOperationException();
+			}
+		}
+
+
+		private void SetDefaultConfiguration()
+		{
+			if (_dte?.Solution.SolutionBuild is SolutionBuild2 val)
+			{
+				try
+				{
 					var x86Config = val.SolutionConfigurations
 						.Cast<SolutionConfiguration2>()
 						.FirstOrDefault(c => c.Name == "Debug" && c.PlatformName == "x86");
@@ -89,6 +115,60 @@ namespace UnoSolutionTemplate.Wizard
 		public bool ShouldAddProjectItem(string filePath)
 		{
 			return true;
+		}
+
+		public Project[] GetAllProjects()
+		{
+			var list = new List<Project>();
+			if (_dte != null)
+			{
+				var projects = _dte.Solution.Projects;
+				var item = projects.GetEnumerator();
+				while (item.MoveNext())
+				{
+					var project = item.Current as Project;
+					if (project == null)
+					{
+						continue;
+					}
+
+					if (project.Kind == ProjectKinds_vsProjectKindSolutionFolder)
+					{
+						list.AddRange(GetSolutionFolderProjects(project));
+					}
+					else
+					{
+						list.Add(project);
+					}
+				}
+			}
+
+			return list.ToArray();
+		}
+
+		private Project[] GetSolutionFolderProjects(Project solutionFolder)
+		{
+			var list = new List<Project>();
+			for (var i = 1; i <= solutionFolder.ProjectItems.Count; i++)
+			{
+				var subProject = solutionFolder.ProjectItems.Item(i).SubProject;
+				if (subProject == null)
+				{
+					continue;
+				}
+
+				// If this is another solution folder, do a recursive call, otherwise add
+				if (subProject.Kind == ProjectKinds_vsProjectKindSolutionFolder)
+				{
+					list.AddRange(GetSolutionFolderProjects(subProject));
+				}
+				else
+				{
+					list.Add(subProject);
+				}
+			}
+
+			return list.ToArray();
 		}
 	}
 }
