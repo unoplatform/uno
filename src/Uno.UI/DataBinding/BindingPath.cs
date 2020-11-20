1dcca1102f327b93f0ab1b6501d52c0e1995dbb4
Martin Zikmund <martin@zikmund.dev>
2020-11-20 14:45:22 +0100

    feat(Binding): PropertyChanged update with null

--- src/Uno.UI/DataBinding/BindingPath.cs
+++ src/Uno.UI/DataBinding/BindingPath.cs
@@ -355,7 +355,7 @@ namespace Uno.UI.DataBinding
 
 				System.ComponentModel.PropertyChangedEventHandler handler = (s, args) =>
 				{
-					if (args.PropertyName == propertyName || args.PropertyName == string.Empty)
+					if (args.PropertyName == propertyName || string.IsNullOrEmpty(args.PropertyName))
 					{
 						if (typeof(BindingPath).Log().IsEnabled(Microsoft.Extensions.Logging.LogLevel.Debug))
 						{
