aa3e7d0f497d86a89b649fd9e8f43c0b33684078
Martin Zikmund <martin@zikmund.dev>
2020-11-21 09:55:29 +0100

    test: Sample pages for RadioButtons

---
+++ src/SamplesApp/SamplesApp.UITests/Microsoft_UI_Xaml_Controls/RadioButtonsTests/Given_RadioButtons.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using SamplesApp.UITests.TestFramework;
+using Uno.UITest;
+using Uno.UITest.Helpers;
+using Uno.UITest.Helpers.Queries;
+using Uno.UITests.Helpers;
+
+namespace SamplesApp.UITests.Microsoft_UI_Xaml_Controls.NumberBoxTests
+{
+	public class Given_RadioButtons : SampleControlUITestBase
+	{
+		[Test]
+		[AutoRetry]
+		public void SelectionTest()
+		{
+			Run("UITests.Microsoft_UI_Xaml_Controls.RadioButtonsTests.RadioButtonsPage");
+
+			SetItemType(RadioButtonsSourceType.RadioButton);
+
+			foreach (RadioButtonsSourceLocation location in Enum.GetValues(typeof(RadioButtonsSourceLocation)))
+			{
+				SetSource(location);
+
+				SelectByIndex(1);
+				VerifySelectedIndex(1);
+				var item1 = _app.Marked("Radio Button 1");
+				Assert.IsTrue(item1.GetDependencyPropertyValue<string>("IsChecked") == "True");
+
+				SelectByItem(3);
+				VerifySelectedIndex(3);
+				var item3 = _app.Marked("Radio Button 3");
+				Assert.IsTrue(item3.GetDependencyPropertyValue<string>("IsChecked") == "True");
+				Assert.IsFalse(item1.GetDependencyPropertyValue<string>("IsChecked") == "True");
+			}
+		}
+
+		private void SelectByItem(int v)
+		{
+			SetIndexToSelect(v);
+
+			var selectByIndexButton = _app.Marked("SelectByItemButton");
+			selectByIndexButton.FastTap();
+		}
+
+		private void VerifySelectedIndex(int v)
+		{
+			var selectedIndexTextBlock = _app.Marked("SelectedIndexTextBlock");
+			//var v2 = selectedIndexTextBlock.GetDependencyPropertyValue("Inlines");
+			_app.WaitForText(selectedIndexTextBlock, v.ToString(CultureInfo.InvariantCulture));
+		}
+		private void SelectByIndex(int v)
+		{
+			SetIndexToSelect(v);
+
+			var selectByIndexButton = _app.Marked("SelectByIndexButton");
+			selectByIndexButton.FastTap();
+		}
+
+		private void SetIndexToSelect(int v)
+		{
+			var indexToSelectTextBlock = _app.Marked("IndexToSelectTextBlock");
+			indexToSelectTextBlock.SetDependencyPropertyValue("Text", v.ToString(CultureInfo.InvariantCulture));
+		}
+
+		private void SetSource(RadioButtonsSourceLocation location)
+		{
+			var sourceComboBox = _app.Marked("SourceComboBox");
+
+			sourceComboBox.SetDependencyPropertyValue("SelectedIndex", "1");
+			sourceComboBox.SetDependencyPropertyValue("SelectedIndex", "0");
+			sourceComboBox.SetDependencyPropertyValue("SelectedIndex", "-1");
+
+			switch (location)
+			{
+				case RadioButtonsSourceLocation.Items:
+					sourceComboBox.SetDependencyPropertyValue("SelectedIndex", "0");
+					break;
+				case RadioButtonsSourceLocation.ItemSource:
+					sourceComboBox.SetDependencyPropertyValue("SelectedIndex", "1");
+					break;
+			}
+		}
+		private void SetItemType(RadioButtonsSourceType type)
+		{
+			var itemTypeComboBox = _app.Marked("ItemTypeComboBox");
+
+			switch (type)
+			{
+				case RadioButtonsSourceType.String:
+					itemTypeComboBox.SetDependencyPropertyValue("SelectedIndex", "0");
+					// elements.GetItemTypeComboBox().SelectItemByName("StringsComboBoxItem");
+					break;
+				case RadioButtonsSourceType.RadioButton:
+					itemTypeComboBox.SetDependencyPropertyValue("SelectedIndex", "1");
+					// elements.GetItemTypeComboBox().SelectItemByName("RadioButtonElementsComboBoxItem");
+					break;
+			}
+		}
+
+		public enum RadioButtonsSourceType
+		{
+			String,
+			RadioButton
+		}
+
+		public enum RadioButtonsSourceLocation
+		{
+			Items,
+			ItemSource
+		}
+	}
+}
---
+++ src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/RadioButtonsTests/RadioButtonsFocusPage.xaml.cs
@@ -0,0 +1,140 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Uno.UI.Samples.Controls;
+using Microsoft.UI.Xaml.Controls;
+using MUXControlsTestApp;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.Microsoft_UI_Xaml_Controls.RadioButtonsTests
+{
+	[Sample("RadioButtons", "WinUI")]
+	public sealed partial class RadioButtonsFocusPage : Page
+	{
+		public RadioButtonsFocusPage()
+		{
+			this.InitializeComponent();
+
+			Loaded += OnLoaded;
+		}
+
+		private void OnLoaded(object sender, RoutedEventArgs e)
+		{
+			ChangeTestFrameVisibility(Visibility.Collapsed);
+		}
+
+		private void ChangeTestFrameVisibility(Visibility visibility)
+		{
+			//TODO: MZ: Do we need this?
+			//var testFrame = Window.Current.Content as TestFrame;
+			//testFrame.ChangeBarVisibility(visibility);
+		}
+
+		private void TestRadioButtons_SelectionChanged(object sender, Windows.UI.Xaml.Controls.SelectionChangedEventArgs e)
+		{
+			var index = TestRadioButtons1.SelectedIndex;
+			SelectedIndexTextBlock1.Text = index.ToString();
+			if (TestRadioButtons1.SelectedItem != null)
+			{
+				SelectedItemTextBlock1.Text = TestRadioButtons1.SelectedItem.ToString();
+			}
+			else
+			{
+				SelectedItemTextBlock1.Text = "null";
+			}
+
+		}
+
+		private void TestRadioButtons2_SelectionChanged(object sender, Windows.UI.Xaml.Controls.SelectionChangedEventArgs e)
+		{
+			var index = TestRadioButtons2.SelectedIndex;
+			SelectedIndexTextBlock2.Text = index.ToString();
+			if (TestRadioButtons2.SelectedItem != null)
+			{
+				SelectedItemTextBlock2.Text = TestRadioButtons2.SelectedItem.ToString();
+			}
+			else
+			{
+				SelectedItemTextBlock2.Text = "null";
+			}
+
+		}
+
+		private void TestRadioButtons_GotFocus(object sender, RoutedEventArgs e)
+		{
+			var stackPanel = VisualTreeHelper.GetChild(TestRadioButtons1, 0);
+			var repeater = (ItemsRepeater)VisualTreeHelper.GetChild(stackPanel, 1);
+			FocusedIndexTextBlock1.Text = repeater.GetElementIndex((UIElement)e.OriginalSource).ToString();
+			TestRadioButtons1HasFocusCheckBox.IsChecked = true;
+		}
+
+		private void TestRadioButtons_LostFocus(object sender, RoutedEventArgs e)
+		{
+			FocusedIndexTextBlock1.Text = "-1";
+			TestRadioButtons1HasFocusCheckBox.IsChecked = false;
+		}
+
+		private void TestRadioButtons2_GotFocus(object sender, RoutedEventArgs e)
+		{
+			var stackPanel = VisualTreeHelper.GetChild(TestRadioButtons2, 0);
+			var repeater = (ItemsRepeater)VisualTreeHelper.GetChild(stackPanel, 1);
+			FocusedIndexTextBlock2.Text = repeater.GetElementIndex((UIElement)e.OriginalSource).ToString();
+			TestRadioButtons2HasFocusCheckBox.IsChecked = true;
+		}
+
+		private void TestRadioButtons2_LostFocus(object sender, RoutedEventArgs e)
+		{
+			FocusedIndexTextBlock2.Text = "-1";
+			TestRadioButtons2HasFocusCheckBox.IsChecked = false;
+		}
+
+		private void SelectByIndexButton1_Click(object sender, RoutedEventArgs e)
+		{
+			var index = getIndexToSelect(false);
+			if (index != -2)
+			{
+				TestRadioButtons1.SelectedIndex = index;
+			}
+		}
+
+		private void SelectByIndexButton2_Click(object sender, RoutedEventArgs e)
+		{
+			var index = getIndexToSelect(true);
+			if (index != -2)
+			{
+				TestRadioButtons2.SelectedIndex = index;
+			}
+		}
+
+		private int getIndexToSelect(bool selectRadioButtons2)
+		{
+			int value;
+			bool success;
+			if (selectRadioButtons2)
+			{
+				success = Int32.TryParse(IndexToSelectTextBox2.Text, out value);
+			}
+			else
+			{
+				success = Int32.TryParse(IndexToSelectTextBox1.Text, out value);
+			}
+
+			if (success)
+			{
+				if (value >= 3)
+				{
+					return 1;
+				}
+				if (value < -1)
+				{
+					return -2;
+				}
+				return value;
+			}
+			return -2;
+		}
+	}
+}
