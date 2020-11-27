e8086398e9737ab273a68f4081397a23697f321e
David <github.site@torick.net>
2020-11-27 14:11:20 -0500

    test(dragdrop): Sample for the ListView reorder

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/DragAndDrop/DragDrop_ListView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Windows_UI_Xaml.DragAndDrop
+{
+	[Sample("DragAndDrop", "ListView")]
+	public sealed partial class DragDrop_ListView : Page
+	{
+		public DragDrop_ListView()
+		{
+			this.InitializeComponent();
+
+			Bla.ItemsSource = new ObservableCollection<string>
+			{
+				"#FF0018",
+				"#FFA52C",
+				"#FFFF41",
+				"#008018",
+				"#0000F9",
+				"#86007D"
+			};
+		}
+	}
+}
