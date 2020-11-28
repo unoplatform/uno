b26efef8686586a014425575dad3e7aed2f470a9
Arlo <arlo.godfrey@outlook.com>
2020-11-28 15:30:35 -0600

    fix: Update src/Uno.UI/ts/Guid.ts
    Invoke .NET GUID creation
    
    Co-authored-by: Jérôme Laban <jlaban@gmail.com>

--- src/Uno.UI/ts/Guid.ts
+++ src/Uno.UI/ts/Guid.ts
@@ -1,9 +1,11 @@
 ﻿namespace Uno.Utils {
 	export class Guid {
 		public static NewGuid(): string {
-			return (([1e7] as any) + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c: any) =>
-				(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-			);
+						if (!Guid.newGuidMethod) {
+				Guid.newGuidMethod = (<any>Module).mono_bind_static_method("[mscorlib] System.Guid:NewGuid");
+			}
+
+      return Guid.newGuidMethod();
 		}
 	}
 }
