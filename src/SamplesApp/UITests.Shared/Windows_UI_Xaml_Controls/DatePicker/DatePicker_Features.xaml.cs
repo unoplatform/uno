d0679f3bbdf82b65ffec77ccff37a57d5695f5c8
Carl de Billy <carl.debilly@gmail.com>
2020-12-07 15:05:26 -0500

    test(datepicker): Added a sample to illustrate features of the DatePicker & DatePickerFlyout

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/DatePicker/DatePicker_Features.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.DatePicker
+{
+	[Sample]
+	public sealed partial class DatePicker_Features : Page
+	{
+		public DatePicker_Features()
+		{
+			this.InitializeComponent();
+		}
+
+		public DateTimeOffset Dt(object o)
+		{
+			if (o is SelectorItem item && item.Tag is string tag && !string.IsNullOrEmpty(tag))
+			{
+				var year = Convert.ToUInt16(tag);
+				return new DateTime(year, 1, 1, 0, 0, 0);
+			}
+			return DateTimeOffset.Now;
+		}
+	}
+}
