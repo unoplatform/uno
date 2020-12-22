e4aec211bd17b1a91cc8923d3afd2bc697983711
Steve Bilogan <steve.bilogan@gmail.com>
2020-12-22 17:03:46 -0500

    test(ProgressRing): Add UI Tests for Determinate ProgressRingTests

--- src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/ProgressRing/WinUIProgressRing_Slider.xaml.cs
+++ src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/ProgressRing/WinUIProgressRing_Slider.xaml.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -12,16 +22,11 @@ namespace UITests.Microsoft_UI_Xaml_Controls.ProgressRing
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
 	[Sample("Progress", "WinUI")]
-	public sealed partial class WinUIDeterminateProgressRing : Page
+	public sealed partial class WinUIProgressRing_Slider : Page
     {
-        public WinUIDeterminateProgressRing()
+        public WinUIProgressRing_Slider()
         {
             this.InitializeComponent();
-		}
-
-		private void ProgressValue_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			ProgressRing.Value = double.Parse(ProgressValue.SelectedValue as string);
-		}
-	}
+        }
+    }
 }
