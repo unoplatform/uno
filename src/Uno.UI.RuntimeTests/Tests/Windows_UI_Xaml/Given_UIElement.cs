d909ab318de1a9173daf534b52a6497ac0d88077
Martin Zikmund <martin@zikmund.dev>
2021-01-07 09:39:19 +0100

    test: Verify IsArrangeDirty for visible and collapsed UIElements

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_UIElement.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.UI.Xaml.Controls;
+using Private.Infrastructure;
+using Windows.UI.Xaml;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml
+{
+	[TestClass]
+    public partial class Given_UIElement
+	{
+		[TestMethod]
+		[RunsOnUIThread]
+		public async Task When_Visible_InvalidateArrange()
+		{
+			var sut = new Border()
+			{
+				Width = 100,
+				Height = 10
+			};
+
+			TestServices.WindowHelper.WindowContent = sut;
+			await TestServices.WindowHelper.WaitForIdle();
+			sut.InvalidateArrange();
+			await TestServices.WindowHelper.WaitForIdle();
+
+			Assert.IsFalse(sut.IsArrangeDirty);
+		}
+
+		[TestMethod]
+		[RunsOnUIThread]
+		public async Task When_Collapsed_InvalidateArrange()
+		{
+			var sut = new Border()
+			{
+				Width = 100,
+				Height = 10,
+				Visibility = Visibility.Collapsed
+			};
+
+			TestServices.WindowHelper.WindowContent = sut;
+			await TestServices.WindowHelper.WaitForIdle();
+			sut.InvalidateArrange();
+			await TestServices.WindowHelper.WaitForIdle();
+
+			Assert.IsFalse(sut.IsArrangeDirty);
+		}
+	}
+}
