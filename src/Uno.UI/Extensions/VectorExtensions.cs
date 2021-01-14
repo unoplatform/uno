bfdd3df96d5a967e6dc543b752ddf29475bece9b
Andres Pineda <pineda.andres@gmail.com>
2021-01-13 23:41:54 -0500

    feat: Add VectorExtensions

---
+++ src/Uno.UI/Extensions/VectorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using Windows.Foundation;
+
+namespace Uno.UI.Extensions
+{
+	public static class VectorExtensions
+	{
+		public static Point ToPoint(this Vector2 vector) => throw new NotImplementedException();
+		public static Size ToSize(this Vector2 vector) => throw new NotImplementedException();
+		public static Vector2 ToVector2(this Point point) => throw new NotImplementedException();
+		public static Vector2 ToVector2(this Size size) => throw new NotImplementedException();
+	}
+}
