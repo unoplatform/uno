78f885c48436c37289f1e73bd9714ae73475b06a
Martin Zikmund <martin@zikmund.dev>
2021-01-15 13:03:10 +0100

    test: Runtime tests for ActualSize

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_UIElement.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_UIElement.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Windows.UI.Xaml.Controls;
 using Private.Infrastructure;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Shapes;
 
 namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml
 {
@@ -47,5 +48,62 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml
 			Assert.IsFalse(sut.IsArrangeDirty);
 		}
 #endif
+
+		[TestMethod]
+		[RunsOnUIThread]
+		public async Task When_TextBlock_ActualSize()
+		{
+			Border border = new Border();
+			TextBlock text = new TextBlock()
+			{
+				HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center,
+				VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Center,
+				Text = "Short text"
+			};
+			border.Child = text;
+
+			TestServices.WindowHelper.WindowContent = border;
+			await TestServices.WindowHelper.WaitForIdle();
+
+			border.UpdateLayout();
+
+			Assert.AreEqual(text.ActualWidth, text.ActualSize.X, 0.01);
+			Assert.AreEqual(text.ActualHeight, text.ActualSize.Y, 0.01);
+
+			text.Text = "This is a longer text";
+			border.UpdateLayout();
+
+			Assert.AreEqual(text.ActualWidth, text.ActualSize.X, 0.01);
+			Assert.AreEqual(text.ActualHeight, text.ActualSize.Y, 0.01);
+		}
+
+		public async Task When_Rectangle_Set_ActualSize()
+		{
+			Border border = new Border();
+
+			Rectangle rectangle = new Rectangle()
+			{
+				HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Center,
+				VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Center,
+				Width = 42,
+				Height = 24,
+			};
+			border.Child = rectangle;
+
+			TestServices.WindowHelper.WindowContent = border;
+			await TestServices.WindowHelper.WaitForIdle();
+
+			border.UpdateLayout();
+
+			Assert.AreEqual(rectangle.ActualWidth, rectangle.ActualSize.X, 0.01);
+			Assert.AreEqual(rectangle.ActualHeight, rectangle.ActualSize.Y, 0.01);
+
+			rectangle.Width = 16;
+			rectangle.Height = 32;
+			border.UpdateLayout();
+
+			Assert.AreEqual(rectangle.ActualWidth, rectangle.ActualSize.X, 0.01);
+			Assert.AreEqual(rectangle.ActualHeight, rectangle.ActualSize.Y, 0.01);
+		}
 	}
 }
