d2b455feeb1d4844dc038efff5471126558a3226
Andres Pineda <pineda.andres@gmail.com>
2021-01-14 22:34:35 -0500

    feat: Implement Vector Extensions

--- src/Uno.UI/Extensions/VectorExtensions.cs
+++ src/Uno.UI/Extensions/VectorExtensions.cs
@@ -8,9 +8,9 @@ namespace Uno.UI.Extensions
 {
 	public static class VectorExtensions
 	{
-		public static Point ToPoint(this Vector2 vector) => throw new NotImplementedException();
-		public static Size ToSize(this Vector2 vector) => throw new NotImplementedException();
-		public static Vector2 ToVector2(this Point point) => throw new NotImplementedException();
-		public static Vector2 ToVector2(this Size size) => throw new NotImplementedException();
+		public static Point ToPoint(this Vector2 vector) => new Point(vector.X, vector.Y);
+		public static Size ToSize(this Vector2 vector) => new Size(vector.X, vector.Y);
+		public static Vector2 ToVector2(this Point point) => new Vector2((float)point.X, (float)point.Y);
+		public static Vector2 ToVector2(this Size size) => new Vector2((float)size.Width, (float)size.Height);
 	}
 }
