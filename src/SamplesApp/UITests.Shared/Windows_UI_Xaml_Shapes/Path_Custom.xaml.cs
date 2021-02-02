34e3a4c9354ffc87833df3b15646fff6cbd02a96
Daoting <Daoting@outlook.com>
2021-02-02 15:01:43 +0800

    wasm中子类继承父类标签

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Shapes/Path_Custom.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
+
+namespace UITests.Windows_UI_Xaml_Shapes
+{
+	[Sample("Shapes")]
+	public sealed partial class Path_Custom : Page
+	{
+		public Path_Custom()
+		{
+			this.InitializeComponent();
+		}
+	}
+
+//#if __WASM__
+//	[Uno.UI.Runtime.WebAssembly.HtmlElement("svg")]
+//#endif
+	public partial class MyPath : Path
+	{
+		
+	}
+}
