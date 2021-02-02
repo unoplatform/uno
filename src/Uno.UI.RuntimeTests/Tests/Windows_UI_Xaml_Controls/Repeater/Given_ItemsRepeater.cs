ab8bee6a973e682a7f48cb726c1acf3a35bd2fe0
David <github.site@torick.net>
2021-02-02 11:25:23 -0500

    test(itemsrepeater): Validate that viewport is properly filled.

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Repeater/Given_ItemsRepeater.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Private.Infrastructure;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls.Repeater
+{
+	[TestClass]
+	public class Given_ItemsRepeater
+	{
+		[TestMethod]
+		[RunsOnUIThread]
+		public async Task When_NoScrollViewer_Then_ShowMoreThanFirstItem()
+		{
+			var sut = new ItemsRepeater
+			{
+				ItemsSource = new[] {"Item_1", "Item_2"}
+			};
+			var popup = new Popup
+			{
+				Background = new SolidColorBrush(Colors.Red),
+				Child = new Grid
+				{
+					Width = 100,
+					Height = 200,
+					Children = {sut}
+				}
+			};
+
+			TestServices.WindowHelper.WindowContent = popup;
+			await TestServices.WindowHelper.WaitForIdle();
+
+			popup.IsOpen = true;
+
+			await TestServices.WindowHelper.WaitForIdle();
+			sut.UpdateLayout();
+
+			var second = sut
+				.EnumerateAllChildren()
+				.OfType<TextBlock>()
+				.FirstOrDefault(t => t.Text == "Item_2");
+
+			popup.IsOpen = false;
+
+			Assert.IsNotNull(second);
+		}
+	}
+}
