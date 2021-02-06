55aa697bbffcc121b443595b84c15f44442923e0
Martin Zikmund <martin@zikmund.dev>
2021-02-06 15:57:26 +0100

    test(VisualStates): Setter value local override

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml/VisualStateManagerTests/VisualStateManagerTest.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml/VisualStateManagerTests/VisualStateManagerTest.cs
@@ -32,5 +32,24 @@ namespace SamplesApp.UITests.Windows_UI_Xaml.VisualStateManagerTests
 			_app.WaitForDependencyPropertyValue(border02, "Background", "[SolidColorBrush [Color: 000000FF;00000080;00000000;00000080]]");
 			_app.WaitForDependencyPropertyValue(border03, "Background", "[SolidColorBrush [Color: 000000FF;000000FF;000000A5;00000000]]");
 		}
+
+		[Test]
+		[AutoRetry]
+		public void When_Value_Overriden_Locally()
+		{
+			Run("UITests.Shared.Windows_UI_Xaml.VisualStateTests.VisualState_LocalOverride");
+			
+			var rootGrid = _app.Marked("RootGrid");
+			_app.WaitForElement(rootGrid);
+
+			// Initially, the background should be red.
+			_app.WaitForDependencyPropertyValue(rootGrid, "Background", "[SolidColorBrush [Color: 000000FF;000000FF;00000000;00000000]]");
+
+			var setColorButton = _app.Marked("SetColorButton");
+			_app.FastTap(setColorButton);
+
+			// The button should override the value set by VisualState's Setter.
+			_app.WaitForDependencyPropertyValue(rootGrid, "Background", "[SolidColorBrush [Color: 000000FF;00000000;00000000;000000FF]]");
+		}
 	}
 }
---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/VisualStateTests/VisualState_LocalOverride.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Uno.UI.Samples.Controls;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace UITests.Windows_UI_Xaml.VisualStateTests
+{
+	[Sample("Visual states", Description = "Background should start off as red, but turn blue after the button is clicked.")]
+
+	public sealed partial class VisualState_LocalOverride : Page
+    {
+        public VisualState_LocalOverride()
+        {
+            this.InitializeComponent();
+        }
+
+		private void SetColorButton_Click(object sender, RoutedEventArgs e)
+		{
+			RootGrid.Background = new SolidColorBrush(Color.FromArgb(255, 0, 0, 255));
+		}
+	}
+}
