0754994610c90539477b80be00e189cd4bc37f55
Martin Zikmund <martin@zikmund.dev>
2021-02-09 22:07:11 +0100

    feat: Launcher support on Tizen

--- src/Uno.UWP/System/Launcher.Skia.cs
+++ src/Uno.UWP/System/Launcher.Skia.cs
@@ -1,22 +1,57 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Uno.Extensions;
+using Uno.Extensions.System;
+using Uno.Foundation.Extensibility;
 
 namespace Windows.System
 {
 	public static partial class Launcher
 	{
-		public static Task<bool> LaunchUriPlatformAsync(Uri uri)
+		private static readonly Lazy<ILauncherExtension?> _launcherExtension = new Lazy<ILauncherExtension?>(() =>
 		{
-			var processStartInfo = new ProcessStartInfo(uri.OriginalString)
+			if (ApiExtensibility.CreateInstance<ILauncherExtension>(typeof(Launcher), out var launcherExtension))
 			{
-				UseShellExecute = true,
-				Verb = "open"
-			};
+				return launcherExtension;
+			}
+			return null;
+		});
 
-			var process = new Process();
-			process.StartInfo = processStartInfo;
-			return Task.FromResult(process.Start());
+		public static async Task<bool> LaunchUriPlatformAsync(Uri uri)
+		{
+			if (_launcherExtension.Value != null)
+			{
+				return await _launcherExtension.Value.LaunchUriAsync(uri);
+			}
+			return await LaunchUriFallbackAsync(uri);
+		}
+
+		private static Task<bool> LaunchUriFallbackAsync(Uri uri)
+		{
+			try
+			{
+				var processStartInfo = new ProcessStartInfo(uri.OriginalString)
+				{
+					UseShellExecute = true,
+					Verb = "open"
+				};
+
+				var process = new Process();
+				process.StartInfo = processStartInfo;
+				return Task.FromResult(process.Start());
+			}
+			catch (Exception ex)
+			{
+				if (typeof(Launcher).Log().IsEnabled(LogLevel.Error))
+				{
+					typeof(Launcher).Log().LogError($"Could not launch URI - {ex}");
+				}
+				return Task.FromResult(false);
+			}
 		}
 	}
 }
