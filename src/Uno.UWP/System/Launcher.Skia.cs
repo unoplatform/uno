a3dada52c736b169ddf991ac2a94cec5517c017a
Martin Zikmund <martin@zikmund.dev>
2021-02-09 17:32:14 +0100

    feat: Basic Launcher.OpenUriAsync support on Skia

--- src/Uno.UWP/System/Launcher.Skia.cs
+++ src/Uno.UWP/System/Launcher.Skia.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
-using Uno.Foundation;
 
 namespace Windows.System
 {
 	public static partial class Launcher
 	{
-		public static async Task<bool> LaunchUriPlatformAsync(Uri uri)
+		public static Task<bool> LaunchUriPlatformAsync(Uri uri)
 		{
-			throw new NotImplementedException();
+			var processStartInfo = new ProcessStartInfo(uri.OriginalString)
+			{
+				UseShellExecute = true,
+				Verb = "open"
+			};
+
+			var process = new Process();
+			process.StartInfo = processStartInfo;
+			return Task.FromResult(process.Start());
 		}
 	}
 }
