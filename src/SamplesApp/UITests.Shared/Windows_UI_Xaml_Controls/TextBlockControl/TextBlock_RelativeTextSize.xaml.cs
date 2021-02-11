d19c612b280f508f1668a23d6a477ef35caec39c
Carl de Billy <carl.debilly@gmail.com>
2021-02-11 12:54:36 -0500

    test(textblock): Added a sample to illustrate the expected related text size vs logical units

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/TextBlockControl/TextBlock_RelativeTextSize.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.TextBlockControl
+{
+	[Sample]
+	public sealed partial class TextBlock_RelativeTextSize : Page
+	{
+		public TextBlock_RelativeTextSize()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
