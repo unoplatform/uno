a685b731b0371b8c815be324a9e82969f7453bc3
Jerome Laban <jerome.laban@nventive.com>
2021-02-11 13:37:05 -0500

    chore: adjust for preview 4
    (cherry picked from commit 26adfb74cb82f285bcf66c56a895e502bcfde7c1)

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/Expander/ExpanderCollapsedEventArgs.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/Expander/ExpanderCollapsedEventArgs.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Microsoft.UI.Xaml.Controls
 {
-	public class ExpanderCollapsedEventArgs : EventArgs
+	public partial class ExpanderCollapsedEventArgs : EventArgs
 	{
 	}
 }
