5690ce9686e3fb5651d1aeb741bce046c6db8022
Martin Zikmund <martin@zikmund.dev>
2021-02-13 20:24:03 +0100

    chore: Skia fallback for Folder and FileSave pickers

--- src/Uno.UWP/Generated/3.0.0.0/Windows.Storage.Pickers/FolderPicker.cs
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.Storage.Pickers/FolderPicker.cs
@@ -2,7 +2,7 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.Storage.Pickers
 {
-	#if __ANDROID__ || __IOS__ || NET461 || false || __SKIA__ || __NETSTD_REFERENCE__ || false
+	#if __ANDROID__ || __IOS__ || NET461 || false || false || __NETSTD_REFERENCE__ || false
 	[global::Uno.NotImplemented]
 	#endif
 	public  partial class FolderPicker 
