02b1ad0919fd974c07cb75be6669b4b71d83737f
David <github.site@torick.net>
2021-02-16 07:23:32 -0500

    fix(perf): [ArrayPool]  Remove useless abstract method

--- src/Uno.Foundation/Buffer/ArrayPool.cs
+++ src/Uno.Foundation/Buffer/ArrayPool.cs
@@ -22,7 +22,7 @@ namespace Uno.Buffers
     /// This class is thread-safe.  All members may be used by multiple threads concurrently.
     /// </para>
     /// </remarks>
-    internal abstract class ArrayPool<T>
+    internal partial class ArrayPool<T>
     {
         /// <summary>The lazily-initialized shared pool instance.</summary>
         private static ArrayPool<T> s_sharedInstance = null;
@@ -58,7 +58,7 @@ namespace Uno.Buffers
         /// <returns>A new <see cref="ArrayPool{T}"/> instance.</returns>
         public static ArrayPool<T> Create()
         {
-            return new DefaultArrayPool<T>();
+            return new ArrayPool<T>();
         }
 
         /// <summary>
@@ -76,45 +76,7 @@ namespace Uno.Buffers
         /// </remarks>
         public static ArrayPool<T> Create(int maxArrayLength, int maxArraysPerBucket)
         {
-            return new DefaultArrayPool<T>(maxArrayLength, maxArraysPerBucket);
+            return new ArrayPool<T>(maxArrayLength, maxArraysPerBucket);
         }
-
-        /// <summary>
-        /// Retrieves a buffer that is at least the requested length.
-        /// </summary>
-        /// <param name="minimumLength">The minimum length of the array needed.</param>
-        /// <returns>
-        /// An <see cref="T:T[]"/> that is at least <paramref name="minimumLength"/> in length.
-        /// </returns>
-        /// <remarks>
-        /// This buffer is loaned to the caller and should be returned to the same pool via 
-        /// <see cref="Return"/> so that it may be reused in subsequent usage of <see cref="Rent"/>.  
-        /// It is not a fatal error to not return a rented buffer, but failure to do so may lead to 
-        /// decreased application performance, as the pool may need to create a new buffer to replace
-        /// the one lost.
-        /// </remarks>
-        public abstract T[] Rent(int minimumLength);
-
-        /// <summary>
-        /// Returns to the pool an array that was previously obtained via <see cref="Rent"/> on the same 
-        /// <see cref="ArrayPool{T}"/> instance.
-        /// </summary>
-        /// <param name="array">
-        /// The buffer previously obtained from <see cref="Rent"/> to return to the pool.
-        /// </param>
-        /// <param name="clearArray">
-        /// If <c>true</c> and if the pool will store the buffer to enable subsequent reuse, <see cref="Return"/>
-        /// will clear <paramref name="array"/> of its contents so that a subsequent consumer via <see cref="Rent"/> 
-        /// will not see the previous consumer's content.  If <c>false</c> or if the pool will release the buffer,
-        /// the array's contents are left unchanged.
-        /// </param>
-        /// <remarks>
-        /// Once a buffer has been returned to the pool, the caller gives up all ownership of the buffer 
-        /// and must not use it. The reference returned from a given call to <see cref="Rent"/> must only be
-        /// returned via <see cref="Return"/> once.  The default <see cref="ArrayPool{T}"/>
-        /// may hold onto the returned buffer in order to rent it again, or it may release the returned buffer
-        /// if it's determined that the pool already has enough buffers stored.
-        /// </remarks>
-        public abstract void Return(T[] array, bool clearArray = false);
     }
 }
