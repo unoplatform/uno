d47211ed67d50eedd935043b7ffbb2cc11ee68d2
Ben Askren <ben@buildcalc.com>
2021-02-18 15:11:29 -0500

    Proof of concept for review

--- src/Uno.UI/UI/Xaml/Controls/WebView/WebView.wasm.cs
+++ src/Uno.UI/UI/Xaml/Controls/WebView/WebView.wasm.cs
@@ -13,11 +13,111 @@ using System.Threading.Tasks;
 using System.Threading;
 using Windows.UI.Core;
 using Uno.Logging;
+using Uno.UI.Xaml;
 
 namespace Windows.UI.Xaml.Controls
 {
 	public partial class WebView
 	{
+		private NativeWebView _nativeWebView;
+
+		protected override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			_nativeWebView = this
+				.EnumerateAllChildren().Where(c => c is NativeWebView).FirstOrDefault() as NativeWebView;
+
+			if (_nativeWebView == null)
+			{
+				this.Log().Error($"No view of type {nameof(NativeWebView)} found in children, are you missing one of these types in a template ? ");
+			}
+			UpdateFromInternalSource();
+		}
+
+		partial void OnScrollEnabledChangedPartial(bool scrollingEnabled)
+		{
+			if (scrollingEnabled)
+				//_nativeWebView.SetCssStyle(("overflow", "hidden"), ("height", "100%"), ("width", "100%"));
+				WindowManagerInterop.SetStyles(_nativeWebView.HtmlId, new[] { ("overflow", "hidden"), ("height", "100%"), ("width", "100%") });
+			else
+				WindowManagerInterop.SetStyles(_nativeWebView.HtmlId, new[] { ("overflow", "initial"), ("height", "auto"), ("width", "auto") });
+		}
+
+		internal bool OnNavigationStarted(Uri uri)
+		{
+			var args = new WebViewNavigationStartingEventArgs() { Uri = uri };
+			NavigationStarting?.Invoke(this, args);
+			return args.Cancel;
+		}
+
+		internal void OnNavigationCompleted(bool isSuccess, Uri uri, WebErrorStatus status)
+		{
+			var args = new WebViewNavigationCompletedEventArgs()
+			{
+				IsSuccess = isSuccess,
+				Uri = uri,
+				WebErrorStatus = status
+			};
+			NavigationCompleted?.Invoke(this, args);
+		}
+
+		internal bool OnNewWindowRequested(Uri referrer, Uri uri)
+		{
+			var args = new WebViewNewWindowRequestedEventArgs(referrer, uri);
+			NewWindowRequested?.Invoke(this, args);
+			return args.Handled;
+		}
+
+		internal void OnNavigationFailed(Uri uri, WebErrorStatus status)
+		{
+			var args = new WebViewNavigationFailedEventArgs()
+			{
+				Uri = uri,
+				WebErrorStatus = status
+			};
+			NavigationFailed?.Invoke(this, args);
+		}
+
+		partial void GoBackPartial()
+		{
+			if (CanGoBack)
+				_nativeWebView.GoBack();
+		}
+
+		partial void GoForwardPartial()
+		{
+			if (CanGoForward)
+				_nativeWebView.GoForward();
+		}
+
+		partial void NavigatePartial(Uri uri)
+		{
+			if (!VerifyNativeWebViewAvailability())
+			{
+				return;
+			}
+
+			if (uri.Scheme.Equals("local", StringComparison.OrdinalIgnoreCase))
+			{
+				// can we access the bundle?
+				//var path = $"{NSBundle.MainBundle.BundlePath}/{uri.PathAndQuery}";
+
+				//_nativeWebView.LoadRequest(new NSUrlRequest(new NSUrl(path, false)));
+				throw new NotSupportedException("Local loading yet to be supported");
+			}
+			else
+			{
+				_nativeWebView?.SetInternalSource(uri);
+			}
+		}
+
+		partial void NavigateToStringPartial(string text)
+		{
+			_nativeWebView?.SetInternalSource(text);
+		}
+
+
 		//This should be IAsyncOperation<string> instead of Task<string> but we use an extension method to enable the same signature in Win.
 		//IAsyncOperation is not available in Xamarin.
 		public async Task<string> InvokeScriptAsync(CancellationToken ct, string script, string[] arguments)
@@ -27,8 +127,31 @@ namespace Windows.UI.Xaml.Controls
 
 		public async Task<string> InvokeScriptAsync(string script, string[] arguments)
 		{
-			throw new NotSupportedException();
+			return await _nativeWebView.InvokeScriptAsync(script, arguments);
+			//_nativeWebView.PostMessageToTennant
 		}
+
+
+		private bool VerifyNativeWebViewAvailability()
+		{
+			if (_nativeWebView == null)
+			{
+				if (_isLoaded)
+				{
+					this.Log().Warn("This WebView control instance does not have a native web view child, a Control template may be missing.");
+				}
+
+				return false;
+			}
+
+			return true;
+		}
+
+		internal void InternalSetCanGoBack(bool value)
+			=> CanGoBack = value;
+
+		internal void InternalSetCanGoForward(bool value)
+			=> CanGoForward = value;
 	}
 }
 
---
+++ src/Uno.UI/WasmScripts/Uno.UI.WebViewBridge.js
@@ -0,0 +1,121 @@
+﻿if (window.parent !== window) {
+
+    function createUUID() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
+    var InstanceGuid = createUUID();
+
+    function GetGuids() {
+        var session = "";
+        var instance = "";
+        if (window.name !== undefined && window.name !== null) {
+            var guids = window.name.split(':');
+            session = guids[0];
+            if (guids.length > 1)
+                instance = guids[1];
+        }
+        return { session: session, instance: instance };
+    }
+
+    function UnoWebViewBridge_PostMessage(message) {
+        console.log('UnoWebViewBridge[' + InstanceGuid + ']_PostMessage ENTER message: ' + JSON.stringify(message));
+        let guids = GetGuids();
+        message.Target = guids.session;
+        message.Source = guids.instance;
+        message.InstanceGuid = InstanceGuid;
+        window.parent.postMessage(message, "*");
+        console.log('UnoWebViewBridge[' + InstanceGuid + ']_PostMessage EXIT message: ' + JSON.stringify(message));
+    }
+
+    function UnoWebViewBridge_ReceiveMessage(event) {
+        var guids = GetGuids();
+
+        if (event.data.Source == guids.session && event.data.Target == guids.instance) {
+
+            if (event.data.Method == 'Navigate') {
+                console.log('UnoWebViewBridge[' + InstanceGuid + ']_ReceiveMessage : Navigate ' + event.data.Payload);
+                window.location.assign(event.data.Payload);
+                return;
+            }
+            else if (event.data.Method == 'NavigateToText') {
+                console.log('UnoWebViewBridge[' + InstanceGuid + ']_ReceiveMessage : NavigateToText ' + event.data.Payload);
+                window.location.assign(event.data.Payload);
+                return;
+            }
+            else if (event.data.Method == 'Reload') {
+                window.location.reload(event.data.Payload);
+                return;
+            }
+            else if (event.data.Method == 'GoForward') {
+                window.history.forward();
+            }
+            else if (event.data.Method == 'GoBack') {
+                window.history.back();
+            }
+            else if (event.data.Method == 'InvokeScriptAsync') {
+                try {
+                    let args = "";
+                    if (event.data.Payload !== undefined && event.data.Payload !== null)
+                        args = event.data.Payload.join();
+                    let script = event.data.FunctionName + '(' + args + ');'
+                    console.log('script: ' + script);
+                    var result = eval(script);
+                    console.log('resutl: ' + result);
+                    if (result === undefined || result === null)
+                        result = "";
+                    UnoWebViewBridge_PostMessage({ Method: event.data.Method, TaskId: event.data.TaskId, Result: result });
+                } catch (error) {
+                    UnoWebViewBridge_PostMessage({ Method: event.data.Method, TaskId: event.data.TaskId, Error: error });
+                }
+                return;
+            }
+
+            // note that navigation echo messages never get sent
+            UnoWebViewBridge_PostMessage({ Method: "echo", Arguments: [event.data.Method] });
+        }
+        else {
+            console.log('unknown message [' + InstanceGuid + ']: ' + JSON.stringify(event.data));
+        }
+    }
+
+    function UnoWebViewBridge_InvokeScriptAsync(json) {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    function UnoWebViewBridge_Initiate() {
+
+        const currentWindowOnLoad = window.onload;
+        window.onload = function () {
+
+            console.log('UnoWebViewBridge[' + InstanceGuid + '] OnLoad ENTER : ' + window.location.href);
+
+            let title = "";
+            if (document.title !== undefined && document.title !== null)
+                title = document.title;
+
+            console.log('UnoWebViewBridge[' + InstanceGuid + '] Pages: ' + window.history.length);
+            if (!history.state && typeof (history.replaceState) == "function")
+                history.replaceState({ page: history.length, href: location.href, title: title }, title);
+            console.log('UnoWebViewBridge[' + InstanceGuid + '] Pages: ' + window.history.length + '  Page: ' + window.history.state.page);
+
+            if (currentWindowOnLoad !== undefined && currentWindowOnLoad !== null)
+                currentWindowOnLoad();
+
+            window.addEventListener("message", UnoWebViewBridge_ReceiveMessage, false);
+
+            UnoWebViewBridge_PostMessage({ Method: "OnBridgeLoaded", InstanceGuid: InstanceGuid, Pages: window.history.length, Page: window.history.state.page, Href: window.location.href });
+
+            console.log('UnoWebViewBridge[' + InstanceGuid + '] OnLoad EXIT : ' + window.location.href);
+        }
+    }
+
+    UnoWebViewBridge_Initiate();
+}
