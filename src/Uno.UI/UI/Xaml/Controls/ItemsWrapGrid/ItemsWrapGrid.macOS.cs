f14ecfa6d9d2c359dcc6697baf90857054a43171
David Oliver <david.oliver@nventive.com>
2021-02-22 06:05:16 -0500

    fix(gridview): [macOS] Ensure GridView shows items
    Correctly fall back on non-virtualized WrapPanel items panel, since ItemsWrapGrid isn't supported for now on macOS.

---
+++ src/Uno.UI/UI/Xaml/Controls/ItemsWrapGrid/ItemsWrapGrid.macOS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Uno;
+
+namespace Windows.UI.Xaml.Controls
+{
+	[NotImplemented]
+	partial class ItemsWrapGrid
+	{
+	}
+}
