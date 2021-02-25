50bcfe92f74befc2d4b7100250380d61af691599
Jean-Philippe Levesque <jp.levesque@nventive.com>
2021-02-25 11:26:01 -0500

    fix(WebView): Raise NavigationStarting and NavigationCompleted when doing anchor navigation on iOS.
    (cherry picked from commit f569465b36add2c000e65d631785f710c83b9dc5)

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/WebView/WebView_AnchorNavigation.xaml.cs
@@ -0,0 +1,39 @@
+using System;
+using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.Samples.Content.UITests.WebView
+{
+	[SampleControlInfo("WebView", "WebView_AnchorNavigation", description:"This sample tests that anchor navigation raises the proper events. The 2 uris received from the NavigationStarting and NavigationCompleted must update whether you tap the NavigateToAnchor button or tap on anchors from the web content.")]
+	public sealed partial class WebView_AnchorNavigation : UserControl
+	{
+		//Windows.UI.Xaml.Controls.WebView webView;
+
+		public WebView_AnchorNavigation()
+		{
+			InitializeComponent();
+
+#if HAS_UNO
+			webView.Navigate(new Uri("https://tools.ietf.org/html/rfc6749"));
+			webView.NavigationStarting += WebView_NavigationStarting;
+			webView.NavigationCompleted += WebView_NavigationCompleted;
+#endif
+		}
+
+		private void WebView_NavigationStarting(Windows.UI.Xaml.Controls.WebView sender, WebViewNavigationStartingEventArgs args)
+		{
+			NavigationStartingTextBlock.Text = args.Uri.AbsoluteUri;
+		}
+
+		private void WebView_NavigationCompleted(Windows.UI.Xaml.Controls.WebView sender, WebViewNavigationCompletedEventArgs args)
+		{
+			NavigationCompletedTextBlock.Text = args.Uri.AbsoluteUri;
+		}
+
+		private void ButtonClicked(object sender, RoutedEventArgs args)
+		{
+			webView.Navigate(new Uri("https://tools.ietf.org/html/rfc6749#section-1"));
+		}
+	}
+}
