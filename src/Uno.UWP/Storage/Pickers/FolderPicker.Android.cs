2db906a044145a76e08e2fac54adfd986ab3b6d8
Martin Zikmund <martin@zikmund.dev>
2021-03-02 17:30:16 +0100

    feat: Android FolderPicker

---
+++ src/Uno.UWP/Storage/Pickers/FolderPicker.Android.cs
@@ -0,0 +1,27 @@
+﻿#nullable enable
+
+using System.Threading;
+using System.Threading.Tasks;
+using Android.Content;
+using Uno.Helpers.Activities;
+
+namespace Windows.Storage.Pickers
+{
+	public partial class FolderPicker
+	{
+		public async Task<StorageFolder?> PickSingleFolderTaskAsync(CancellationToken token)
+		{
+			var intent = new Intent(Intent.ActionOpenDocumentTree);
+			intent.AddFlags(ActivityFlags.GrantReadUriPermission | ActivityFlags.GrantWriteUriPermission);
+			var awaitableResultActivity = await AwaitableResultActivity.StartAsync();
+			var result = await awaitableResultActivity.StartActivityForResultAsync(intent);
+
+			if (result.Intent?.Data == null)
+			{
+				return null;
+			}
+
+			return StorageFolder.GetFromSafUri(result.Intent.Data);
+		}
+	}
+}
