fb772af608ae8c782cb6f848865e9aa56c00cdaa
Martin Zikmund <martin@zikmund.dev>
2021-03-04 11:56:31 +0100

    feat: Android FileSavePicker

---
+++ src/Uno.UWP/Storage/Pickers/FileSavePicker.Android.cs
@@ -0,0 +1,91 @@
+﻿#nullable enable
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Uno.UI;
+
+namespace Windows.Storage.Pickers
+{
+	public partial class FileSavePicker
+	{
+		internal const int RequestCode = 6003;
+		private static TaskCompletionSource<Intent?>? _currentFileOpenPickerRequest;
+
+		internal static bool TryHandleIntent(Intent intent, Result resultCode)
+		{
+			if (_currentFileOpenPickerRequest == null)
+			{
+				return false;
+			}
+			if (resultCode == Result.Canceled)
+			{
+				_currentFileOpenPickerRequest.SetResult(null);
+			}
+			else
+			{
+				_currentFileOpenPickerRequest.SetResult(intent);
+			}
+			return true;
+		}
+
+		private async Task<StorageFile?> PickSaveFileTaskAsync(CancellationToken token)
+		{
+			if (!(ContextHelper.Current is Activity appActivity))
+			{
+				throw new InvalidOperationException("Application activity is not yet set, API called too early.");
+			}
+
+			var action = Intent.ActionCreateDocument;
+
+			var intent = new Intent(action);
+			intent.SetType("*/*");
+
+			var mimeTypes = GetMimeTypes();
+			intent.PutExtra(Intent.ExtraMimeTypes, mimeTypes);
+
+			if (!string.IsNullOrEmpty(SuggestedFileName))
+			{
+				intent.PutExtra(Intent.ExtraTitle, SuggestedFileName);
+			}
+
+			_currentFileOpenPickerRequest = new TaskCompletionSource<Intent?>();
+
+			appActivity.StartActivityForResult(intent, RequestCode);
+
+			var resultIntent = await _currentFileOpenPickerRequest.Task;
+			_currentFileOpenPickerRequest = null;
+
+			if (resultIntent?.Data != null)
+			{
+				return StorageFile.GetFromSafUri(resultIntent.Data);
+			}
+			else if (resultIntent?.ClipData != null && resultIntent.ClipData.ItemCount > 0)
+			{
+				for (int itemIndex = 0; itemIndex < resultIntent.ClipData.ItemCount; itemIndex++)
+				{
+					var uri = resultIntent.ClipData.GetItemAt(itemIndex)?.Uri;
+					if (uri != null)
+					{
+						return StorageFile.GetFromSafUri(uri);
+					}
+				}
+			}
+
+			return null;
+		}
+
+		private string[] GetMimeTypes()
+		{
+			return FileTypeChoices
+				.SelectMany(choice => choice.Value)
+				.Select(extension => MimeTypeService.GetFromExtension(extension))
+				.Distinct()
+				.ToArray();
+		}
+
+	}
+}
---
+++ src/Uno.UWP/Storage/Streams/FileRandomAccessStream.saf.Android.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Android.App;
+
+namespace Windows.Storage.Streams
+{
+	public partial class FileRandomAccessStream
+	{
+		internal static async Task<FileRandomAccessStream> CreateFromSafUriAsync(Android.Net.Uri fileUri, FileAccessMode access) =>
+			new FileRandomAccessStream(await SafStream.CreateAsync(fileUri, access));
+
+		private class SafStream : ImplementationBase
+		{
+			private SafStream(Stream stream) : base(stream)
+			{
+			}
+
+			public static async Task<SafStream> CreateAsync(Android.Net.Uri fileUri, FileAccessMode access)
+			{
+				if (fileUri is null)
+				{
+					throw new ArgumentNullException(nameof(fileUri));
+				}
+
+				Stream backingStream;
+				if (access == FileAccessMode.Read)
+				{
+					backingStream = Application.Context.ContentResolver.OpenInputStream(fileUri);
+				}
+				else
+				{
+					backingStream = Application.Context.ContentResolver.OpenOutputStream(fileUri);
+				}
+				return new SafStream(backingStream);
+			}
+
+			public override IRandomAccessStream CloneStream()
+			{
+				return new FileRandomAccessStream(new SafStream(_stream));
+			}
+
+			public override IInputStream GetInputStreamAt(ulong position)
+			{
+				return new FileInputStream(_stream);
+			}
+
+			public override IOutputStream GetOutputStreamAt(ulong position)
+			{
+				return new FileOutputStream(_stream);
+			}
+		}
+	}
+}
