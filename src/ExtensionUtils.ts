0b5f9841eb1fdace223baf536912d0a9b1efc60e
Matheus Castello <matheus@castello.eng.br>
2021-03-10 17:26:20 -0300

    ExtensionUtils: Fix date logging
    The +1 is needed only for the date.getDate that january corresponds to
    0.
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- src/ExtensionUtils.ts
+++ src/ExtensionUtils.ts
@@ -14,11 +14,11 @@ export class ExtensionUtils {
     static getTimeStampFormated (): string {
         const date = new Date();
         const month = `0${(date.getMonth() + 1)}`.slice(-2);
-        const day = `0${(date.getDate() + 1)}`.slice(-2);
-        const hours = `0${(date.getHours() + 1)}`.slice(-2);
-        const minutes = `0${(date.getMinutes() + 1)}`.slice(-2);
-        const seconds = `0${(date.getSeconds() + 1)}`.slice(-2);
-        const milliseconds = `00${date.getMilliseconds()}`.slice(-3);
+        const day = `0${(date.getDate())}`.slice(-2);
+        const hours = `0${(date.getHours())}`.slice(-2);
+        const minutes = `0${(date.getMinutes())}`.slice(-2);
+        const seconds = `0${(date.getSeconds())}`.slice(-2);
+        const milliseconds = `0${(date.getMilliseconds())}`.slice(-3);
 
         return `${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
     }
