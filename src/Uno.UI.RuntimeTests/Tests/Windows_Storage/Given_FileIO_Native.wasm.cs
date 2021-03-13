306f30bdddd0934109ba29d19561fc54688913f2
Martin Zikmund <martin@zikmund.dev>
2021-03-13 16:50:15 +0100

    test: Native FileIO operations

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_Storage/Given_FileIO_Native.wasm.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.Storage;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_Storage
+{
+	[TestClass]
+	public class Given_FileIO_Native : Given_FileIO_Native_Base
+	{
+		protected override async Task<StorageFolder> GetRootFolderAsync()
+		{
+			return await StorageFolder.GetPrivateRootAsync();
+		}
+
+		protected override async Task CleanupRootFolderAsync()
+		{
+		}
+	}
+}
