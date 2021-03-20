754dda486fc92052b3685d90278f33c3e9801945
Jerome Laban <jerome.laban@nventive.com>
2021-03-20 12:55:50 -0400

    test: Disable NavigateBackAndForthBetweenMenuItemsAndSettings_Fluent on wasm

--- src/SamplesApp/SamplesApp.UITests/Microsoft_UI_Xaml_Controls/NavigationViewTests/NavigationView_Tests.cs
+++ src/SamplesApp/SamplesApp.UITests/Microsoft_UI_Xaml_Controls/NavigationViewTests/NavigationView_Tests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
+using OpenQA.Selenium;
 using SamplesApp.UITests.TestFramework;
 using Uno.UITest.Helpers;
 using Uno.UITest.Helpers.Queries;
