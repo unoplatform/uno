8739af149802f2aaec16fd7d435e18b11f2256ef
pkar <pkar@ieee.org>
2021-03-26 15:12:58 +0100

    chore: dummy change - for restarting build

--- src/Uno.UWP/UI/Notifications/ToastNotification.Android.cs
+++ src/Uno.UWP/UI/Notifications/ToastNotification.Android.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// dummy comment - for restarting build
+
 namespace Windows.UI.Notifications
 {
 	public partial class ToastNotification
