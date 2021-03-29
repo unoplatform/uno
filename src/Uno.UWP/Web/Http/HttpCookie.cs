90b0cc854263fe4b18da96719d127c2231cf1956
Martin Zikmund <martin@zikmund.dev>
2021-03-29 13:30:37 +0200

    feat: Support for HttpCookie class

--- src/Uno.UWP/Web/Http/HttpCookie.cs
+++ src/Uno.UWP/Web/Http/HttpCookie.cs
@@ -1,4 +1,6 @@
+#nullable enable
 using System;
+using System.Text;
 using Windows.Foundation;
 
 namespace Windows.Web.Http
@@ -8,6 +10,9 @@ namespace Windows.Web.Http
 	/// </summary>
 	public partial class HttpCookie : IStringable
 	{
+		private const char ValueSeparator = '=';
+		private const string AttributeSeparator = "; ";
+
 		/// <summary>
 		/// Initializes a new instance of the HttpCookie class with a specified name, domain, and path.
 		/// </summary>
@@ -27,22 +32,22 @@ namespace Windows.Web.Http
 		/// <summary>
 		/// Get the domain for which the HttpCookie is valid.
 		/// </summary>
-		public string Domain { get; }
+		public string Domain { get; } = string.Empty;
 
 		/// <summary>
 		/// Get the token that represents the HttpCookie name.
 		/// </summary>
-		public string Name { get; }
+		public string Name { get; } = string.Empty;
 
 		/// <summary>
 		/// Get the URI path component to which the HttpCookie applies.
 		/// </summary>
-		public string Path { get; }
+		public string Path { get; } = string.Empty;
 
 		/// <summary>
 		/// Get or set the value for the HttpCookie.
 		/// </summary>
-		public string Value { get; set; }
+		public string Value { get; set; } = string.Empty;
 
 		/// <summary>
 		/// Get or set the security level for the HttpCookie.
@@ -65,7 +70,37 @@ namespace Windows.Web.Http
 		/// <returns>A string that represents the current object.</returns>
 		public override string ToString()
 		{
-			throw new NotImplementedException();
+			static void AppendAttribute(StringBuilder builder, string name, string? value)
+			{
+				if (string.IsNullOrEmpty(value))
+				{
+					return;
+				}
+
+				builder.Append(AttributeSeparator);
+				builder.Append(name);
+				builder.Append(ValueSeparator);
+				builder.Append(value);
+			}
+
+			var builder = new StringBuilder();
+			builder.Append(Name);
+			builder.Append(ValueSeparator);
+			builder.Append(Value);
+			AppendAttribute(builder, "Path", Path);
+			AppendAttribute(builder, "Domain", Domain);
+			if (Secure)
+			{
+				builder.Append(AttributeSeparator);
+				builder.Append("Secure");
+			}
+			if (HttpOnly)
+			{
+				builder.Append(AttributeSeparator);
+				builder.Append("HttpOnly");
+			}
+			AppendAttribute(builder, "Expires", Expires?.ToString("r"));
+			return builder.ToString();
 		}
 	}
 }
