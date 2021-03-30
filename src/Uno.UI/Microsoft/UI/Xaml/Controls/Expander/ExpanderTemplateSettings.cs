30946c4206823654d662253b955f863838be6a4e
robloo <robert.looyengoed@gmail.com>
2021-03-30 19:15:06 -0400

    chore(Expander): Update to WinUI at 9929977855fb9f3ab2927089fef5819f4913e0db
    Initial code import

---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/Expander/ExpanderTemplateSettings.cs
@@ -0,0 +1,13 @@
+using System;
+using Windows.UI.Xaml;
+
+namespace Microsoft.UI.Xaml.Controls
+{
+	public partial class ExpanderTemplateSettings : DependencyObject
+	{
+		public ExpanderTemplateSettings()
+		{
+
+		}
+	}
+}
