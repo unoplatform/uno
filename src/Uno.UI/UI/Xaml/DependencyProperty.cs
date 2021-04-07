4bce59b8c745c144d5c05ea0701110c964238c9e
Jerome Laban <jerome.laban@nventive.com>
2021-04-06 21:05:39 -0400

    perf: Improve name to DependencyProperty lookup

--- src/Uno.UI/UI/Xaml/DependencyProperty.cs
+++ src/Uno.UI/UI/Xaml/DependencyProperty.cs
@@ -30,7 +30,7 @@ namespace Windows.UI.Xaml
 			= new Dictionary<Type, Dictionary<string, DependencyProperty>>(Uno.Core.Comparison.FastTypeComparer.Default);
 
 		private readonly static Dictionary<Type, DependencyProperty[]> _getPropertiesForType = new Dictionary<Type, DependencyProperty[]>(Uno.Core.Comparison.FastTypeComparer.Default);
-		private readonly static Dictionary<PropertyCacheEntry, DependencyProperty> _getPropertyCache = new Dictionary<PropertyCacheEntry, DependencyProperty>(PropertyCacheEntry.DefaultComparer);
+		private readonly static NameToPropertyDictionary _getPropertyCache = new NameToPropertyDictionary();
 
 		private readonly static Dictionary<CachedTuple<Type, FrameworkPropertyMetadataOptions>, DependencyProperty[]> _getFrameworkPropertiesForType = new Dictionary<CachedTuple<Type, FrameworkPropertyMetadataOptions>, DependencyProperty[]>(CachedTuple<Type, FrameworkPropertyMetadataOptions>.Comparer);
 		private readonly static Dictionary<Type, DependencyProperty[]> _getDependencyObjectPropertiesForType = new Dictionary<Type, DependencyProperty[]>(Uno.Core.Comparison.FastTypeComparer.Default);
