f2380ef597c83caae31c396105ab6efe2b4f1560
Carl de Billy <carl.debilly@gmail.com>
2021-04-09 15:47:52 -0400

    feat(layoutpanel): Implementation of LayoutPanel

---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/LayoutPanel/LayoutPanel.Properties.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml;
+
+namespace Microsoft.UI.Xaml.Controls
+{
+	partial class LayoutPanel
+	{
+		public static DependencyProperty LayoutProperty { get; } = DependencyProperty.Register(
+			"Layout", typeof(Layout), typeof(LayoutPanel), new PropertyMetadata(default(Layout)));
+
+		public Layout Layout
+		{
+			get => (Layout)GetValue(LayoutProperty);
+			set => SetValue(LayoutProperty, value);
+		}
+	}
+}
