a3cada93d8c16556bac7f61a0a7d5c40142bf0df
Carl de Billy <carl.debilly@gmail.com>
2021-04-12 16:17:14 -0400

    fix(showlocalvisualtree): Were only available in DEBUG on MacOS.

---
+++ src/Uno.UI/Extensions/ViewExtensions.iOSmacOS.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+
+using Windows.UI.Xaml;
+
+namespace Uno.UI
+{
+	public static partial class ViewExtensions
+	{
+		public static string ShowLocalVisualTree(this UIElement element, int fromHeight = 0)
+		{
+#if __MACOS__
+			return AppKit.UIViewExtensions.ShowLocalVisualTree(element as AppKit.NSView, fromHeight);
+#else
+			return UIKit.UIViewExtensions.ShowLocalVisualTree(element as UIKit.UIView, fromHeight);
+#endif
+		}
+	}
+}
