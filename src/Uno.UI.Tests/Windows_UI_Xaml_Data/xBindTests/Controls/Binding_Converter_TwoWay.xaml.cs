64458f2807eedc839fae881d091b07a157becbfc
David Oliver <david.oliver@nventive.com>
2021-04-13 15:32:38 -0400

    chore: Fix WinUI build

--- src/Uno.UI.Tests/Windows_UI_Xaml_Data/xBindTests/Controls/Binding_Converter_TwoWay.xaml.cs
+++ src/Uno.UI.Tests/Windows_UI_Xaml_Data/xBindTests/Controls/Binding_Converter_TwoWay.xaml.cs
@@ -9,7 +9,6 @@ using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
