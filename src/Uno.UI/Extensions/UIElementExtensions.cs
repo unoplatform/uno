d2a05835bdf0f6875a2036dba130f511f84d6ac7
David <github.site@torick.net>
2021-04-14 14:23:38 -0400

    fix: Improve internal debug info

--- src/Uno.UI/Extensions/UIElementExtensions.cs
+++ src/Uno.UI/Extensions/UIElementExtensions.cs
@@ -18,12 +18,17 @@ namespace Uno.UI.Extensions
 			=> elt switch
 			{
 				null => "--null--",
-				FrameworkElement fwElt when fwElt.Name.HasValue() => $"{fwElt.Name} -{elt.GetHashCode():X8}",
-				_ => $"{elt.GetType().Name} -{elt.GetHashCode():X8}",
+#if __WASM__
+				FrameworkElement fwElt when fwElt.Name.HasValue() => $"{fwElt.Name}-{fwElt.HtmlId}",
+				UIElement uiElt => $"{elt.GetType().Name}-{uiElt.HtmlId}",
+#else
+				FrameworkElement fwElt when fwElt.Name.HasValue() => $"{fwElt.Name}-{elt.GetHashCode():X8}",
+#endif
+				_ => $"{elt.GetType().Name}-{elt.GetHashCode():X8}",
 			};
 
 		internal static string GetDebugIdentifier(this object? elt)
-			=> $"{new string('\t', elt.GetDebugDepth())} [{elt.GetDebugName()}]";
+			=> $"{new string('\t', Math.Max(0, elt.GetDebugDepth()))} [{elt.GetDebugName()}]";
 
 		internal static int GetDebugDepth(this object? elt) =>
 			elt switch
