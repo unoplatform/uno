6427e28da7443a823e305db78be54abf37d5788c
pkar <pkar@ieee.org>
2021-05-03 18:00:01 +0200

    feat: ScheduledToastNotification

---
+++ src/Uno.UWP/UI/Notifications/ScheduledToastNotification.cs
@@ -0,0 +1,51 @@
+using Uno.Logging;
+using Uno.Extensions;
+using Microsoft.Extensions.Logging;
+using Windows.Data.Xml.Dom;
+using System;
+
+namespace Windows.UI.Notifications
+{
+
+	public partial class ScheduledToastNotification
+	{
+		public ScheduledToastNotification(XmlDocument content, DateTimeOffset deliveryTime)
+		{
+			Content = content;
+			DeliveryTime = deliveryTime;
+		}
+
+		public XmlDocument Content { get; internal set; }
+		public DateTimeOffset DeliveryTime { get; internal set; }
+
+		private string _tag;
+
+		public string Tag
+		{
+			get
+			{
+				return _tag;
+			}
+			set
+			{
+				_tag = value;
+				if (_tag.Length > 64)
+				{
+					// UWP limit: 16 chars, since Creators Update (15063) - 64 characters
+					if (this.Log().IsEnabled(LogLevel.Warning))
+					{
+						this.Log().LogWarning("Windows.UI.Notifications.ScheduledToastNotification.Tag is set to string longer than UWP limit");
+					}
+				}
+			}
+		}
+
+#if __ANDROID__
+		public  NotificationMirroring NotificationMirroring { get; set; }
+
+		public global::System.DateTimeOffset? ExpirationTime { get; set; }
+#endif
+
+
+	}
+}
