590350b44ff0aa250d523be95200d21bc94b6b5a
David <github.site@torick.net>
2021-05-18 15:56:32 -0400

    feat: Add the BorderLayerRenderer on wasm

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/CalendarView/CalendarViewBaseItem.uno.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/CalendarView/CalendarViewBaseItem.uno.cs
@@ -9,5 +9,14 @@ namespace Windows.UI.Xaml.Controls
 		/// <inheritdoc />
 		internal override bool IsViewHit()
 			=> true;
+
+#if __WASM__
+		/// <inheritdoc />
+		protected override void OnBackgroundChanged(DependencyPropertyChangedEventArgs e)
+		{
+			// We prevent propagation to the parent, so we can set the right background based on the IsToday, IsSelected, etc.
+			// base.OnBackgroundChanged(e);
+		}
+#endif
 	}
 }
