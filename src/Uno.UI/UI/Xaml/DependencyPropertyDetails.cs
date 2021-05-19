34923c4eb3c814c4ec74a18a256ecc8c703a5d35
Jerome Laban <jerome.laban@nventive.com>
2021-05-19 13:56:41 -0400

    perf: Explicitly limit DependencyPropertyDetail array pool max length

--- src/Uno.UI/UI/Xaml/DependencyPropertyDetails.cs
+++ src/Uno.UI/UI/Xaml/DependencyPropertyDetails.cs
@@ -21,7 +21,7 @@ namespace Windows.UI.Xaml
 		private readonly Type _dependencyObjectType;
 		private object? _fastLocalValue;
 		private BindingExpression? _lastBindings;
-		private static readonly ArrayPool<object?> _pool = ArrayPool<object?>.Create(100, 100);
+		private static readonly ArrayPool<object?> _pool = ArrayPool<object?>.Create(_stackLength, 100);
 		private object?[]? _stack;
 		private PropertyMetadata? _metadata;
 		private object? _defaultValue;
