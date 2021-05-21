c3550b9e472940352a326e7c77d69197596f430f
Jerome Laban <jerome.laban@nventive.com>
2021-05-21 19:55:01 +0000

    feat: Add support for Linux FrameBuffer and libinput

---
+++ src/Uno.UI.Runtime.Skia.Linux.FrameBuffer/DisplayInformationExtension.cs
@@ -0,0 +1,46 @@
+﻿using Uno.WinUI.Runtime.Skia.LinuxFB;
+using Windows.Graphics.Display;
+
+namespace Uno.UI.Runtime.Skia
+{
+	internal class DisplayInformationExtension : IDisplayInformationExtension
+	{
+		private readonly DisplayInformation _displayInformation;
+		public Renderer? Renderer { get; set; }
+
+		public DisplayInformationExtension(object owner)
+		{
+			_displayInformation = (DisplayInformation)owner;
+		}
+
+		public DisplayOrientations CurrentOrientation => DisplayOrientations.Landscape;
+
+		public uint ScreenHeightInRawPixels
+			=> (uint)(Renderer?.PixelSize.Height ?? 0);
+
+		public uint ScreenWidthInRawPixels
+			=> (uint)(Renderer?.PixelSize.Width ?? 0);
+
+		public float LogicalDpi
+		{
+			get
+			{
+				return 96.0f;
+			}
+		}
+
+		public double RawPixelsPerViewPixel => LogicalDpi / 96.0f;
+
+		public ResolutionScale ResolutionScale => (ResolutionScale)(int)(RawPixelsPerViewPixel * 100.0);
+
+		public void StartDpiChanged()
+		{
+
+		}
+
+		public void StopDpiChanged()
+		{
+
+		}
+	}
+}
