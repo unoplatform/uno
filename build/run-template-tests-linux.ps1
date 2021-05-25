ba04fc81066225bdba245d532f314a48d6bf08a5
Jerome Laban <jerome.laban@nventive.com>
2021-05-25 16:10:55 -0400

    feat: Add template support for Linux FrameBuffer

--- build/run-template-tests-linux.ps1
+++ build/run-template-tests-linux.ps1
@@ -17,6 +17,7 @@ function Get-TemplateConfiguration(
     [bool]$wasm = $false,
     [bool]$skiaGtk = $false,
     [bool]$skiaWpf = $false,
+    [bool]$skiaLinuxFB = $false,
     [bool]$wasmVsCode = $false)
 {
     $androidFlag = '-android'
@@ -26,16 +27,18 @@ function Get-TemplateConfiguration(
     $wasmVsCodeFlag = '--vscodeWasm'
     $skiaWpfFlag = '--skia-wpf'
     $skiaGtkFlag = '--skia-gtk'
+    $skiaLinuxFBFlag = '--skia-linux-fb'
 
-    $a = If ($android)    { $androidFlag }    Else { $androidFlag    + '=false' }
-    $b = If ($iOS)        { $iOSFlag }        Else { $iOSFlag        + '=false' }
-    $c = If ($macOS)      { $macOSFlag }      Else { $macOSFlag      + '=false' }
-    $d = If ($wasm)       { $wasmFlag }       Else { $wasmFlag       + '=false' }
-    $e = If ($wasmVsCode) { $wasmVsCodeFlag } Else { $wasmVsCodeFlag + '=false' }
-    $f = If ($skiaWpf)    { $skiaWpfFlag    } Else { $skiaWpfFlag    + '=false' }
-    $g = If ($skiaGtk)    { $skiaGtkFlag    } Else { $skiaGtkFlag    + '=false' }
+    $a = If ($android)     { $androidFlag }    Else { $androidFlag     + '=false' }
+    $b = If ($iOS)         { $iOSFlag }        Else { $iOSFlag         + '=false' }
+    $c = If ($macOS)       { $macOSFlag }      Else { $macOSFlag       + '=false' }
+    $d = If ($wasm)        { $wasmFlag }       Else { $wasmFlag        + '=false' }
+    $e = If ($wasmVsCode)  { $wasmVsCodeFlag } Else { $wasmVsCodeFlag  + '=false' }
+    $f = If ($skiaWpf)     { $skiaWpfFlag    } Else { $skiaWpfFlag     + '=false' }
+    $g = If ($skiaGtk)     { $skiaGtkFlag    } Else { $skiaGtkFlag     + '=false' }
+    $h = If ($skiaLinuxFB) { $skiaLinuxFB    } Else { $skiaLinuxFBFlag + '=false' }
 
-    @($a, $b, $c, $d, $e, $f, $g)
+    @($a, $b, $c, $d, $e, $f, $g, $h)
 }
 
 $default = @('-v', 'detailed', "-p:RestoreConfigFile=$env:NUGET_CI_CONFIG")
@@ -44,7 +47,7 @@ $debug = $default + '-c' + 'Debug'
 $release = $default + '-c' + 'Release'
 
 # UWP
-dotnet new unoapp -n UnoApp ((Get-TemplateConfiguration -wasm 1 -wasmVsCode 1 -skiaGtk 1) + '--skia-tizen=false')
+dotnet new unoapp -n UnoApp ((Get-TemplateConfiguration -wasm 1 -wasmVsCode 1 -skiaGtk 1 -skiaLinuxFB 1) + '--skia-tizen=false')
 
 dotnet build $debug UnoApp/UnoApp.Wasm/UnoApp.Wasm.csproj
 Assert-ExitCodeIsZero
@@ -58,8 +61,14 @@ Assert-ExitCodeIsZero
 dotnet build $release UnoApp/UnoApp.Skia.Gtk/UnoApp.Skia.Gtk.csproj
 Assert-ExitCodeIsZero
 
+dotnet build $debug UnoApp/UnoApp.Skia.Linux.FrameBuffer/UnoApp.Skia.Linux.FrameBuffer.csproj
+Assert-ExitCodeIsZero
+
+dotnet build $release UnoApp/UnoApp.Skia.Linux.FrameBuffer/UnoApp.Skia.Linux.FrameBuffer.csproj
+Assert-ExitCodeIsZero
+
 # WinUI
-dotnet new unoapp-winui -n UnoAppWinUI (Get-TemplateConfiguration -wasm 1 -wasmVsCode 1 -skiaGtk 1)
+dotnet new unoapp-winui -n UnoAppWinUI (Get-TemplateConfiguration -wasm 1 -wasmVsCode 1 -skiaGtk 1 -skiaLinuxFB 1)
 
 dotnet build $debug UnoAppWinUI/UnoAppWinUI.Wasm/UnoAppWinUI.Wasm.csproj
 Assert-ExitCodeIsZero
@@ -71,4 +80,10 @@ dotnet build $debug UnoAppWinUI/UnoAppWinUI.Skia.Gtk/UnoAppWinUI.Skia.Gtk.csproj
 Assert-ExitCodeIsZero
 
 dotnet build $release UnoAppWinUI/UnoAppWinUI.Skia.Gtk/UnoAppWinUI.Skia.Gtk.csproj
+Assert-ExitCodeIsZero
+
+dotnet build $debug UnoAppWinUI/UnoAppWinUI.Skia.Linux.FrameBuffer/UnoAppWinUI.Skia.Linux.FrameBuffer.csproj
+Assert-ExitCodeIsZero
+
+dotnet build $release UnoAppWinUI/UnoAppWinUI.Skia.Linux.FrameBuffer/UnoAppWinUI.Skia.Linux.FrameBuffer.csproj
 Assert-ExitCodeIsZero
