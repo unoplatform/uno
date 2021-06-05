fdc482493d23efbacd33e302758fb032936379e4
Martin Zikmund <martin@zikmund.dev>
2021-06-05 18:33:42 +0200

    feat: Implement WUX IconSources, update MUX IconSources
    (cherry picked from commit bbc56d9923883219dbfe22e17756347309d3413d)

--- src/Uno.UI/UI/Xaml/Controls/Icon/IconSource.cs
+++ src/Uno.UI/UI/Xaml/Controls/Icon/IconSource.cs
@@ -1,10 +1,24 @@
-#nullable enable
+using Windows.UI.Xaml.Media;
 
 namespace Windows.UI.Xaml.Controls
 {
-	public partial class IconSource : global::Windows.UI.Xaml.DependencyObject
+	public partial class IconSource : DependencyObject
 	{
-		internal virtual IconElement? CreateIconElement()
-			=> default;
+		protected IconSource()
+		{
+		}
+
+		public Brush Foreground
+		{
+			get => (Brush)GetValue(ForegroundProperty);
+			set => SetValue(ForegroundProperty, value);
+		}
+
+		public static DependencyProperty ForegroundProperty { get; } =
+			DependencyProperty.Register(nameof(Foreground), typeof(Brush), typeof(IconSource), new PropertyMetadata(null));
+
+#nullable enable
+
+		public virtual IconElement? CreateIconElement() => default;
 	}
 }
