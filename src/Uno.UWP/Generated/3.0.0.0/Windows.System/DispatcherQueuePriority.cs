9a59c559fd52696d031a201c94bf572b86548e7a
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2021-06-08 17:20:46 -0400

    chore: Run SyncGenerator

--- src/Uno.UWP/Generated/3.0.0.0/Windows.System/DispatcherQueuePriority.cs
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.System/DispatcherQueuePriority.cs
@@ -2,18 +2,12 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.System
 {
-	#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+	#if false || false || false || false || false || false || false
 	public   enum DispatcherQueuePriority 
 	{
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		Low = -10,
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		Normal = 0,
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		High = 10,
-		#endif
+		// Skipping already declared field Windows.System.DispatcherQueuePriority.Low
+		// Skipping already declared field Windows.System.DispatcherQueuePriority.Normal
+		// Skipping already declared field Windows.System.DispatcherQueuePriority.High
 	}
 	#endif
 }
