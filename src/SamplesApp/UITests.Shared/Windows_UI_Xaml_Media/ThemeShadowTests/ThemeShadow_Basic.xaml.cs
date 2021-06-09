876bdb2ab6969adff56d5531ef2436bd3cec8a65
Martin Zikmund <martin@zikmund.dev>
2021-06-09 15:03:03 +0200

    test: ThemeShadow samples

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Media/ThemeShadowTests/ThemeShadow_Basic.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml.Shapes;
+
+namespace UITests.Windows_UI_Xaml_Media.ThemeShadowTests
+{
+	[Sample("Windows.UI.Xaml.Media")]
+    public sealed partial class ThemeShadow_Basic : Page
+    {
+		public ThemeShadow_Basic()
+		{
+			InitializeComponent();
+
+			SharedShadow.Receivers.Add(BackgroundGrid);
+		}
+	}
+}
