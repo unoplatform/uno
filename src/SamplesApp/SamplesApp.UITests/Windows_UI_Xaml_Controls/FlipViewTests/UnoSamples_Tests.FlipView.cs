cb117cda1ad61c00b3aeb88608d31d6c359fa713
Andres Pineda <pineda.andres@gmail.com>
2021-06-11 01:45:14 -0400

    test: Add FlipView UITest

---
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/FlipViewTests/UnoSamples_Tests.FlipView.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Uno.UITest.Helpers;
+using Uno.UITest.Helpers.Queries;
+using Uno.UITests.Helpers;
+
+namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.FlipViewTests
+{
+    [TestFixture]
+    public class UnoSamples_Tests : SampleControlUITestBase
+    {
+		[Test]
+		[AutoRetry]
+		public void FlipView_WithButtons_FlipForward()
+		{
+			Run("UITests.Windows_UI_Xaml_Controls.FlipView.FlipView_Buttons");
+
+			_app.WaitForElement("Button1");
+
+			_app.WaitForElement("NextButtonHorizontal");
+			_app.FastTap("NextButtonHorizontal");
+
+			_app.WaitForElement("Button2");
+		}
+
+		[Test]
+		[AutoRetry]
+		public void FlipView_WithButtons_FlipBackward()
+		{
+			Run("UITests.Windows_UI_Xaml_Controls.FlipView.FlipView_Buttons");
+
+			_app.WaitForElement("NextButtonHorizontal");
+			_app.FastTap("NextButtonHorizontal");
+
+			_app.WaitForElement("Button2");
+
+			_app.WaitForElement("PreviousButtonHorizontal");
+			_app.FastTap("PreviousButtonHorizontal");
+
+			_app.WaitForElement("Button1");
+		}
+	}
+}
