f5de894460964aab3619beebb840ea5cc7d18b31
David Oliver <david.oliver@nventive.com>
2021-06-14 10:04:58 -0400

    fix(textblock): [Android] Fix TextBlock color not updating
    Certain paths, such as a combination of infinite-width containers and calling the UpdateLayout() method, could result in parts of the visual tree falling into a bad layout state where IsLayoutRequested=true is interleaved with IsLayoutRequested=false, resulting in the bad subtrees never updating. To prevent this, check for this state and repair it by setting the layout request on the parent of the affected view.
    
    This ensures that TextBlock will update correctly when its Foreground color changes (since it relies on a measure+arrange pass to be redrawn).

---
+++ src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_NavigationView.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Private.Infrastructure;
+using Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls.ListViewPages;
+#if NETFX_CORE
+using Uno.UI.Extensions;
+#elif __IOS__
+using UIKit;
+#elif __MACOS__
+using AppKit;
+#else
+using Uno.UI;
+#endif
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using static Private.Infrastructure.TestServices;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Uno.Extensions;
+using Uno.UI.RuntimeTests.Helpers;
+using System.ComponentModel;
+using Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls;
+
+namespace Uno.UI.RuntimeTests.Tests.Microsoft_UI_Xaml_Controls
+{
+	[TestClass]
+	[RunsOnUIThread]
+	public partial class Given_NavigationView
+	{
+		[TestMethod]
+		[RequiresFullWindow]
+		public async Task When_SelectedItem_Set_Before_Load_And_Theme_Changed()
+		{
+			using (StyleHelper.UseFluentStyles())
+			{
+
+				var navView = new Microsoft.UI.Xaml.Controls.NavigationView()
+				{
+					MenuItems =
+				{
+					new Microsoft.UI.Xaml.Controls.NavigationViewItem {Content = "Item 1"},
+					new Microsoft.UI.Xaml.Controls.NavigationViewItem {Content = "Item 2"},
+					new Microsoft.UI.Xaml.Controls.NavigationViewItem {Content = "Item 3"},
+				},
+					PaneDisplayMode = Microsoft.UI.Xaml.Controls.NavigationViewPaneDisplayMode.LeftMinimal,
+					IsBackButtonVisible = Microsoft.UI.Xaml.Controls.NavigationViewBackButtonVisible.Collapsed,
+				};
+				navView.SelectedItem = navView.MenuItems[1];
+				var hostGrid = new Grid() { MinWidth = 20, MinHeight = 20 };
+
+				WindowHelper.WindowContent = hostGrid;
+
+				await WindowHelper.WaitForLoaded(hostGrid);
+
+				hostGrid.Children.Add(navView);
+
+				await WindowHelper.WaitForLoaded(navView);
+
+				navView.IsPaneOpen = true;
+
+				await WindowHelper.WaitForIdle();
+
+				var togglePaneButton = navView.FindFirstChild<Button>(b => b.Name == "TogglePaneButton");
+				var iconTextBlock = togglePaneButton?.FindFirstChild<TextBlock>(t => t.Name == "Icon");
+
+				Assert.AreEqual(Colors.Black, (iconTextBlock.Foreground as SolidColorBrush)?.Color);
+				using (ThemeHelper.UseDarkTheme())
+				{
+					await WindowHelper.WaitForIdle();
+					Assert.AreEqual(Colors.White, (iconTextBlock.Foreground as SolidColorBrush)?.Color);
+#if __ANDROID__
+					// This is the meat of the test - we verify that the actual color of the TextBlock matches the managed Color, which will only be the
+					// case if it was correctly measured and arranged as requested after the theme changed.
+					Assert.AreEqual(false, iconTextBlock.IsLayoutRequested);
+					Assert.AreEqual(Android.Graphics.Color.White, iconTextBlock.NativeArrangedColor);
+#endif
+				}
+			}
+		}
+	}
+}
