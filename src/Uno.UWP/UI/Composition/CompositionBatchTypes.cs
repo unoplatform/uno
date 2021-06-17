f54a846094046f62419321bea28037923adb9faa
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2021-06-17 01:18:34 -0400

    feat: Align enums (netstandard2.0)

--- src/Uno.UWP/UI/Composition/CompositionBatchTypes.cs
+++ src/Uno.UWP/UI/Composition/CompositionBatchTypes.cs
@@ -1,11 +1,32 @@
-#nullable enable
+using System;
 
 namespace Windows.UI.Composition
 {
-	public enum CompositionBatchTypes 
+	/// <summary>
+	/// Batch types for CompositionCommitBatch and CompositionScopedBatch.
+	/// </summary>
+	[Flags]
+	public enum CompositionBatchTypes : uint
 	{
-		None,
-		Animation,
-		Effect,
+		/// <summary>
+		/// None.
+		/// </summary>
+		None = 0U,
+		/// <summary>
+		/// The batch contains animations.
+		/// </summary>
+		Animation = 1U,
+		/// <summary>
+		/// The batch contains effects.
+		/// </summary>
+		Effect = 2U,
+		/// <summary>
+		/// The batch contains an infinite animation.
+		/// </summary>
+		InfiniteAnimation = 4U,
+		/// <summary>
+		/// The batch contains all animations.
+		/// </summary>
+		AllAnimations = InfiniteAnimation | Animation
 	}
 }
