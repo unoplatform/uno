547020f41933e4950fe0e8f393bb9316a2d5ac34
Martin Zikmund <martin@zikmund.dev>
2021-06-19 15:51:21 +0200

    feat: WinUI ImageIcon

---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/ImageIcon/ImageIcon.Uno.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Microsoft.UI.Xaml.Controls
+{
+	public partial class ImageIcon
+	{
+		private bool _initialized = false;
+
+		private void InitializeVisualTree()
+		{
+			if (!_initialized)
+			{
+				var image = new Image
+				{
+					Stretch = Stretch.Uniform
+				};
+
+				var grid = new Grid();
+				grid.Children.Add(image);
+
+				AddIconElementView(grid);
+			}
+		}
+	}
+}
---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/ImageIcon/ImageIcon.cs
@@ -0,0 +1,44 @@
+﻿#nullable enable
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Microsoft.UI.Xaml.Controls
+{
+	public partial class ImageIcon : IconElement
+	{
+		private Image? m_rootImage = null;
+
+		public ImageIcon()
+		{
+		}
+
+		protected override void OnApplyTemplate()
+		{
+#if HAS_UNO
+			// Uno specific: WinUI shares the visual tree initialziation with BitmapIcon
+			InitializeVisualTree();
+#endif
+
+			if (VisualTreeHelper.GetChild(this, 0) is Grid grid)
+			{
+				var image = (Image)VisualTreeHelper.GetChild(grid, 0);
+				image.Source = Source;
+				m_rootImage = image;
+			}
+			else
+			{
+				m_rootImage = null;
+			}
+		}
+
+		private void OnSourcePropertyChanged(DependencyPropertyChangedEventArgs agrs)
+		{
+			if (m_rootImage is { } image)
+			{
+				image.Source = Source;
+			}
+		}
+	}
+}
