160c75294729db1bebb4327ffebefe4299254763
Martin Zikmund <martin@zikmund.dev>
2021-06-24 14:43:07 +0200

    chore: Animated visuals stubs

---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/AnimatedIcon/AnimatedVisuals/AnimatedChevronUpDownSmallVisualSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+
+namespace Microsoft.UI.Xaml.Controls.AnimatedVisuals
+{
+	// TODO Uno: This is currently a stub, as animated visuals are not properly supported
+	public class AnimatedChevronUpDownSmallVisualSource : IAnimatedVisualSource, IAnimatedVisualSource2
+	{
+		public IReadOnlyDictionary<string, double> Markers => throw new NotImplementedException();
+		public void Load() => throw new NotImplementedException();
+		public Size Measure(Size availableSize) => throw new NotImplementedException();
+		public void Pause() => throw new NotImplementedException();
+		public void Play(double fromProgress, double toProgress, bool looped) => throw new NotImplementedException();
+		public void Resume() => throw new NotImplementedException();
+		public void SetColorProperty(string propertyName, Color value) => throw new NotImplementedException();
+		public void SetProgress(double progress) => throw new NotImplementedException();
+		public void Stop() => throw new NotImplementedException();
+		public void Unload() => throw new NotImplementedException();
+		public void Update(AnimatedVisualPlayer player) => throw new NotImplementedException();
+	}
+}
