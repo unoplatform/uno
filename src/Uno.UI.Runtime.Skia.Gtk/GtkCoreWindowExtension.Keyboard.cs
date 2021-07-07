b6207bee9b784032da4f5d4379787a0447ac296e
Luke Blevins <lukeblevins15@outlook.com>
2021-07-07 09:39:52 -0400

    feat: Map Key.KP_Enter to VirtualKey.Enter

--- src/Uno.UI.Runtime.Skia.Gtk/GtkCoreWindowExtension.Keyboard.cs
+++ src/Uno.UI.Runtime.Skia.Gtk/GtkCoreWindowExtension.Keyboard.cs
@@ -118,6 +118,7 @@ namespace Uno.UI.Runtime.Skia
 				Gdk.Key.Tab => VirtualKey.Tab,
 				Gdk.Key.Clear => VirtualKey.Clear,
 				Gdk.Key.Return => VirtualKey.Enter,
+				Gdk.Key.KP_Enter => VirtualKey.Enter,
 				// Gdk.Key.Shift => VirtualKey.Shift,
 				// Gdk.Key.Control => VirtualKey.Control,
 				Gdk.Key.Menu => VirtualKey.Menu,
