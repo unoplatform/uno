2b07ad800f9a56e3e642d3ff3dab5cfa1e4c723e
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2021-07-14 23:05:50 -0400

    fix: Generate WebView.XYFocus properties

--- src/Uno.UWPSyncGenerator/Generator.cs
+++ src/Uno.UWPSyncGenerator/Generator.cs
@@ -1388,22 +1388,6 @@ namespace Uno.UWPSyncGenerator
 
 		private bool SkipProperty(IPropertySymbol property)
 		{
-			if (property.ContainingType.Name == "WebView")
-			{
-				switch (property.Name)
-				{
-					case "XYFocusRight":
-					case "XYFocusLeft":
-					case "XYFocusDown":
-					case "XYFocusUp":
-					case "XYFocusRightProperty":
-					case "XYFocusLeftProperty":
-					case "XYFocusDownProperty":
-					case "XYFocusUpProperty":
-						return true;
-				}
-			}
-
 			if (property.ContainingType.Name == "WebView2")
 			{
 				switch (property.Name)
