65d00b5827ecb92b2521d4b3c7e79fab6e17d092
David <github.site@torick.net>
2021-07-20 15:33:32 -0400

    test(pointers): Automate nested pointer events handling test
    (cherry picked from commit 28328b6dff0f23a7cc78d72b210b163b12dd444c)

---
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Input/NestedHandling_Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using SamplesApp.UITests.Extensions;
+using SamplesApp.UITests.TestFramework;
+using Uno.UITest.Helpers;
+using Uno.UITest.Helpers.Queries;
+
+namespace SamplesApp.UITests.Windows_UI_Xaml_Input
+{
+	[TestFixture]
+	public partial class NestedHandling_Tests : SampleControlUITestBase
+	{
+		private const string _sample = "UITests.Windows_UI_Input.PointersTests.NestedHandling";
+
+		[Test]
+		[AutoRetry]
+		public void When_NestedHandlesPressed_Then_ContainerStillGetsSubsequentEvents()
+		{
+			Run(_sample);
+
+			var target = _app.WaitForElement("_nested").Single().Rect;
+			_app.DragCoordinates(target.X + 10, target.Y + 10, target.Right - 10, target.Bottom - 10);
+
+			var result = _app.Marked("_result").GetDependencyPropertyValue("Text");
+			result.Should().Be("Pressed SUCCESS | Released SUCCESS");
+		}
+	}
+}
