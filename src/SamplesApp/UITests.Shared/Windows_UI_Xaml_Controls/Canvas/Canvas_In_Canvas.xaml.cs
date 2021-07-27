385813626a4c6e55eaf491b9160d4002beae1bd6
Jerome Laban <jerome.laban@nventive.com>
2021-07-27 14:12:12 -0400

    fix: Ensure content in Canvas in Canvas is visible

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Canvas/Canvas_In_Canvas.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.Canvas
+{
+	[SampleControlInfo(description: "Demonstrates the ability to display content in a canvas inside another canvas")]
+	public sealed partial class Canvas_In_Canvas : UserControl
+	{
+		public Canvas_In_Canvas()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
