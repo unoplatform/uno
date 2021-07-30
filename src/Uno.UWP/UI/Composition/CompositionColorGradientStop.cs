14210e68779b568295f93a33a7e46aa87f316f16
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2021-07-30 14:05:31 +0200

    feat: Added CompositionColorGradientStop implementation

---
+++ src/Uno.UWP/UI/Composition/CompositionColorGradientStop.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+namespace Windows.UI.Composition
+{
+	public partial class CompositionColorGradientStop : CompositionObject
+	{
+		private float _offset;
+		private Color _color;
+
+		internal CompositionColorGradientStop(Compositor compositor)
+			: base(compositor)
+		{
+
+		}
+
+		public float Offset
+		{
+			get => _offset;
+			set => SetProperty(ref _offset, value);
+		}
+
+		public Color Color
+		{
+			get => _color;
+			set => SetProperty(ref _color, value);
+		}
+	}
+}
