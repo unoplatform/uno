dfee43457eeed08b4fbc7199e0116d511b8430ed
Martin Zikmund <martin@zikmund.dev>
2021-08-01 13:08:55 +0200

    feat: BackdropMaterial fallback support

---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/Materials/Backdrop/BackdropMaterial.Properties.cs
@@ -0,0 +1,34 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Microsoft.UI.Xaml.Controls
+{
+	public partial class BackdropMaterial
+	{
+		/// <summary>
+		/// Gets the value of the BackdropMaterial.ApplyToRootOrPageBackground XAML attached property for the target element.
+		/// </summary>
+		/// <param name="element">The object from which the property value is read.</param>
+		/// <returns>The BackdropMaterial.ApplyToRootOrPageBackground XAML attached property value of the requested object.</returns>
+		public static bool GetApplyToRootOrPageBackground(Control element) =>
+			(bool)element.GetValue(ApplyToRootOrPageBackgroundProperty);
+
+		/// <summary>
+		/// Sets the value of the BackdropMaterial.ApplyToRootOrPageBackground XAML attached property for a target element.
+		/// </summary>
+		/// <param name="element">The object to which the property value is written.</param>
+		/// <param name="value">The value to set.</param>
+		public static void SetApplyToRootOrPageBackground(Control element, bool value) =>
+			element.SetValue(ApplyToRootOrPageBackgroundProperty, value);
+
+		/// <summary>
+		/// Applies the backdrop material to the root or background of the XAML content.
+		/// </summary>
+		public static DependencyProperty ApplyToRootOrPageBackgroundProperty { get; } =
+			DependencyProperty.RegisterAttached(
+				"ApplyToRootOrPageBackground",
+				typeof(bool),
+				typeof(BackdropMaterial),
+				new PropertyMetadata(false, OnApplyToRootOrPageBackgroundChanged));
+	}
+}
---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/Materials/Backdrop/MicaController.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI;
+
+namespace Microsoft.UI.Xaml.Controls
+{
+    internal class MicaController
+    {
+		internal static readonly Color DarkThemeColor = Color.FromArgb(255, 32, 32, 32);
+		internal const float DarkThemeTintOpacity = 0.8f;
+
+		internal static readonly Color LightThemeColor = Color.FromArgb(255, 243, 243, 243);
+		internal const float LightThemeTintOpacity = 0.5f;
+
+		internal bool SetTarget(Windows.UI.Xaml.Window xamlWindow)
+		{
+			// Uno specific: Actual Mica is not yet supported on any target.
+			return false;
+		}
+    }
+}
