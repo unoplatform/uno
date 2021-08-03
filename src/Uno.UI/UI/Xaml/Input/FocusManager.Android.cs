d02e0fcd9f01a58c95bc3050f309dbe58e085383
Martin Zikmund <martin@zikmund.dev>
2021-08-03 17:03:46 +0200

    fix: Initial focus on Page
    Previously, the first focusable child was focused on Page, even if the Page itself was focusable. Now the Page can be focused itself, which matches UWP. In addition, on initial focus we avoid opening the InputPane in case the initial focused element is a TextBox.
    
    (cherry picked from commit a444b7a41f05157f5b546e1fbbc0bb38388e3aac)

--- src/Uno.UI/UI/Xaml/Input/FocusManager.Android.cs
+++ src/Uno.UI/UI/Xaml/Input/FocusManager.Android.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using Windows.UI.Xaml.Controls;
 using Android.Graphics;
 using Windows.UI.ViewManagement;
+using Uno.UI.Xaml.Core;
 
 namespace Windows.UI.Xaml.Input
 {
@@ -18,7 +19,13 @@ namespace Windows.UI.Xaml.Input
 			// TODO Uno: Handle Hyperlink focus
 			if (element is Control control)
 			{
-				control.RequestFocus();
+				var focusManager = VisualTree.GetFocusManagerForElement(control);
+				if (element is Android.Views.View androidView &&
+					androidView.Focusable &&
+					focusManager?.InitialFocus == false) // Do not focus natively on initial focus so the soft keyboard is not opened
+				{
+					control.RequestFocus();
+				}
 
 				// Forcefully try to bring the control into view when keyboard is open to accommodate adjust nothing mode
 				if (InputPane.GetForCurrentView().Visible)
