ab3672c44091080ece64ba49dc9175bf7aa53b3d
Youssef1313 <youssefvictor00@gmail.com>
2021-08-04 12:40:48 +0200

    test: Test only

---
+++ src/SamplesApp/UITests.Shared/MessageDialog/MessageDialogTest.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UITests.MessageDialogTests
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MessageDialogTest : Page
+    {
+		private IAsyncOperation<IUICommand> _asyncOperation;
+
+		public MessageDialogTest()
+        {
+            this.InitializeComponent();
+        }
+
+		private async void OnClick(object sender, RoutedEventArgs e)
+		{
+			var dialog = new Windows.UI.Popups.MessageDialog("Content", "Title");
+			_asyncOperation = dialog.ShowAsync();
+			_ = await _asyncOperation;
+		}
+
+		private void OnCheckedChanged(object sender, RoutedEventArgs e)
+		{
+			_asyncOperation.Cancel();
+		}
+	}
+}
