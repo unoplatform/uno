1fe777a2fb8b39827e50cabecb60c7880b5ca9e2
David Oliver <david.oliver@nventive.com>
2021-08-13 10:24:32 -0400

    fix(dragdrop): [iOS] [Android] Support drag over events when native view in hierarchy
    Search within native views when global hit-testing. This allows DragOver, DragEnter etc to work properly when target view has a native view in its ancestry (eg for native Frame navigation on iOS, using a native SplitView style on Android, etc).

---
+++ src/Uno.UI.RuntimeTests/Helpers/RectAssert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.Foundation;
+
+namespace Uno.UI.RuntimeTests.Helpers
+{
+	internal static class RectAssert
+	{
+		public static void AreEqual(Rect expected, Rect actual, double delta = 1)
+		{
+			Assert.AreEqual(expected.Left, actual.Left, delta);
+			Assert.AreEqual(expected.Top, actual.Top, delta);
+			Assert.AreEqual(expected.Width, actual.Width, delta);
+			Assert.AreEqual(expected.Height, actual.Height, delta);
+		}
+	}
+}
