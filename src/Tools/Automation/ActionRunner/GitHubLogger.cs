339f114ba53a01c1f41781e781b3433af7a76f36
Youssef1313 <youssefvictor00@gmail.com>
2021-08-17 19:59:17 +0200

    feat: Automate syncing resources from mux repository

---
+++ src/Tools/Automation/ActionRunner/GitHubLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ActionRunner
+{
+	internal static class GitHubLogger
+	{
+		public static void LogInformation(string text) => Console.WriteLine(text);
+
+		public static void LogError(string text) => Console.WriteLine($"::error::{text}");
+
+		public static void LogWarning(string text) => Console.WriteLine($"::warning::{text}");
+	}
+}
---
+++ src/Tools/Automation/ActionRunner/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace ActionRunner
+{
+	internal class Program
+    {
+		private const string UnoPathPrefix = "uno/src/Uno.UI/Microsoft/UI/Xaml/Controls";
+		private const string MUXPathPrefix = "microsoft-ui-xaml/dev";
+
+		private static int Main()
+        {
+			if (!Directory.Exists(MUXPathPrefix))
+			{
+				GitHubLogger.LogError($"Can't find '{MUXPathPrefix}' directory.");
+				return 1;
+			}
+			else if (!Directory.Exists(UnoPathPrefix))
+			{
+				GitHubLogger.LogError($"Can't find '{UnoPathPrefix}' directory.");
+			}
+
+			string[] unoResourceFiles = CollectResourcesFromUno();
+			GitHubLogger.LogInformation($"Found {unoResourceFiles.Length} resources files.");
+
+			foreach (string unoPath in unoResourceFiles)
+			{
+				var muxPath = MapUnoToMUX(unoPath);
+				if (!File.Exists(muxPath))
+				{
+					GitHubLogger.LogWarning($"Path '{muxPath}' was not found.");
+					continue;
+				}
+
+				var muxContent = File.ReadAllText(muxPath);
+				var unoContent = File.ReadAllText(unoPath);
+				if (muxContent != unoContent)
+				{
+					GitHubLogger.LogError($"Files '{muxPath}' and '{unoPath}' are not identical!!!");
+					File.Copy(muxPath, unoPath, overwrite: true);
+				}
+			}
+
+			return 0;
+        }
+
+		private static string[] CollectResourcesFromUno()
+			=> Directory.GetFiles(UnoPathPrefix, "*.resw", SearchOption.AllDirectories);
+
+		private static string MapUnoToMUX(string unoPath)
+		{
+			if (!unoPath.StartsWith(UnoPathPrefix))
+			{
+				throw new InvalidOperationException($"Expected path to start with '{UnoPathPrefix}'. Found: '{MUXPathPrefix}'.");
+			}
+
+			return Path.Combine(MUXPathPrefix, Path.GetRelativePath(relativeTo: UnoPathPrefix, unoPath));
+		}
+    }
+}
