dbbcecc63e8ccf0a89d33755e9e59e942955e3c6
Carl de Billy <carl.debilly@nventive.com>
2021-08-18 19:12:01 -0400

    feat(background_sizing): Fixed compilation for iOS & MacOS (background sizing not implemented yet)

--- src/Uno.UI/UI/Xaml/Controls/Page/Page.iOS.cs
+++ src/Uno.UI/UI/Xaml/Controls/Page/Page.iOS.cs
@@ -33,6 +33,7 @@ namespace Windows.UI.Xaml.Controls
 				_borderRenderer.UpdateLayer(
 					this,
 					Background,
+					BackgroundSizing,
 					Thickness.Empty,
 					null,
 					CornerRadius.None,
--- src/Uno.UI/UI/Xaml/Controls/Panel/Panel.iOS.cs
+++ src/Uno.UI/UI/Xaml/Controls/Panel/Panel.iOS.cs
@@ -96,6 +96,7 @@ namespace Windows.UI.Xaml.Controls
 				_borderRenderer.UpdateLayer(
 					this,
 					Background,
+					BackgroundSizing,
 					BorderThickness,
 					BorderBrush,
 					CornerRadius,
