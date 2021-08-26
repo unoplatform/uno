738137393c62c3cb8cd865a8a2e56cc79211c389
Hudney <33006894+Hudney@users.noreply.github.com>
2021-08-26 12:06:04 -0300

    fixup: move android impl to its own file

---
+++ src/Uno.UWP/System/MemoryManager.Android.cs
@@ -0,0 +1,30 @@
+﻿#if __ANDROID__
+using System;
+using Android.App;
+using Uno.UI;
+
+namespace Windows.System
+{
+	partial class MemoryManager
+	{
+		public static ulong AppMemoryUsage
+		{
+			get
+			{
+				return (ulong) GC.GetTotalMemory(false);
+			}
+		}
+
+		public static ulong AppMemoryUsageLimit
+		{
+			get
+			{
+				ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+				ActivityManager.FromContext(ContextHelper.Current)?.GetMemoryInfo(memoryInfo);
+
+				return AppMemoryUsage + (ulong)memoryInfo.AvailMem - (ulong)memoryInfo.Threshold;
+			}
+		}
+	}
+}
+#endif
