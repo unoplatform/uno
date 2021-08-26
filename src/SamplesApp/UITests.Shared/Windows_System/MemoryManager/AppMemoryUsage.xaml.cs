09507772c001a1b3f6dd49fe186777c72300758e
Hudney <33006894+Hudney@users.noreply.github.com>
2021-08-26 14:18:28 -0300

    fixup: adds missing samples

---
+++ src/SamplesApp/UITests.Shared/Windows_System/MemoryManager/AppMemoryUsage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Windows_System.MemoryManager
+{
+	[SampleControlInfo("Windows.System", "MemoryManager.AppMemoryUsage",
+		description: "Shows the amount of memory used by the application")]
+	public sealed partial class AppMemoryUsage : Page
+	{
+		public AppMemoryUsage()
+		{
+			this.InitializeComponent();
+			SetValueAppMemoryUsage();
+		}
+
+		public void SetValueAppMemoryUsage()
+		{
+			ValueAppMemoryUsage.Text = Windows.System.MemoryManager.AppMemoryUsage.ToString();
+		}
+
+	}
+}
---
+++ src/SamplesApp/UITests.Shared/Windows_System/MemoryManager/AppMemoryUsageLimit.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Windows_System.MemoryManager
+{
+	[SampleControlInfo("Windows.System", "MemoryManager.AppMemoryUsageLimit",
+		description: "Shows the limit amount of memory that can be used by the application")]
+	public sealed partial class AppMemoryUsageLimit : Page
+	{
+		public AppMemoryUsageLimit()
+		{
+			this.InitializeComponent();
+			SetValueAppMemoryUsageLimit();
+		}
+
+		public void SetValueAppMemoryUsageLimit()
+		{
+			ValueAppMemoryUsageLimit.Text = Windows.System.MemoryManager.AppMemoryUsageLimit.ToString();
+		}
+
+	}
+}
