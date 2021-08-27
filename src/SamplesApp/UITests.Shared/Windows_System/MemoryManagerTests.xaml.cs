a24eff6f01665e9551052c25a69c607722f986e1
Hudney <33006894+Hudney@users.noreply.github.com>
2021-08-27 17:40:20 -0300

    fixup: refactored samples into a single one

---
+++ src/SamplesApp/UITests.Shared/Windows_System/MemoryManagerTests.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Windows_System
+{
+	[Sample("Windows.System", "MemoryManagerTests")]
+    public sealed partial class MemoryManagerTests : Page
+    {
+        public MemoryManagerTests()
+        {
+            this.InitializeComponent();
+        }
+
+		public void SetValueMemoryManager()
+		{
+			ValueMemoryManager.Text = "AppMemoryUsage: " + Windows.System.MemoryManager.AppMemoryUsage.ToString() + " and AppMemoryUsageLimit: " + Windows.System.MemoryManager.AppMemoryUsageLimit.ToString();
+		}
+	}
+}
