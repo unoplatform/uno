c0e8fd7568df3c42a0e973adb56de8d870f15793
pkar <pkar@ieee.org>
2021-08-31 16:42:20 +0200

    chore: GetRadiosAsync sampleApp

---
+++ src/SamplesApp/UITests.Shared/Windows_Devices/RadioTests.xaml.cs
@@ -0,0 +1,43 @@
+using System;
+using Uno.UI.Samples.Controls;
+using Uno.UI.Samples.UITests.Helpers;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Devices.Radios;
+
+namespace UITests.Shared.Windows_Devices
+{
+	[SampleControlInfo("Windows.Devices", "RadioTests", description: "Demonstrates use of Windows.Devices.Radio.GetRadiosAsyncTask")]
+	public sealed partial class RadioTests : UserControl
+	{
+		public RadioTests()
+		{
+			this.InitializeComponent();
+		}
+
+		private async void uiCreate_Click(object sender, RoutedEventArgs e)
+		{
+			uiResultMsg.Text = "Calling GetRadiosAsyncTask...";
+			try
+			{
+				var radiosList = await Windows.Devices.Radios.Radio.GetRadiosAsync();
+
+				uiListItems.ItemsSource = radiosList;
+				uiResultMsg.Text = "Your current Radios:";
+
+				foreach (var oneRadio in radiosList)
+                {
+					if(oneRadio.State == RadioState.Unknown)
+                    {
+						uiResultMsg.Text = "Your current Radios (RadioState.Unknown means: you probably have no appropriate permission declared):";
+					}
+                }
+			}
+			catch (Exception ex)
+			{
+				uiResultMsg.Text = "Got exception (maybe API is not supported on your platform): " + ex.Message;
+			}
+		}
+	}
+}
