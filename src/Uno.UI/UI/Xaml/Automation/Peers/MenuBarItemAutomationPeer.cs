ddd0bed17a7f081b7ab93dac971fef5a612b60f3
David Oliver <david.oliver@nventive.com>
2021-09-01 06:09:47 -0400

    feat: Implement MenuBarItemAutomationPeer

---
+++ src/Uno.UI/UI/Xaml/Automation/Peers/MenuBarItemAutomationPeer.cs
@@ -0,0 +1,19 @@
+using Windows.UI.Xaml.Controls;
+
+namespace Windows.UI.Xaml.Automation.Peers
+{
+	public partial class MenuBarItemAutomationPeer : FrameworkElementAutomationPeer
+	{
+		private MenuBarItem MenuBarItemOwner => Owner as MenuBarItem;
+
+		public MenuBarItemAutomationPeer(global::Windows.UI.Xaml.Controls.MenuBarItem owner) : base(owner)
+		{
+		}
+
+		public void Invoke() => MenuBarItemOwner?.Invoke();
+
+		public void Expand() => MenuBarItemOwner?.ShowMenuFlyout();
+
+		public void Collapse() => MenuBarItemOwner?.CloseMenuFlyout();
+	}
+}
