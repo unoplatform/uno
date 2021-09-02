414f7081a65f295030d9097d104a39fd75db3f41
Hudney <33006894+Hudney@users.noreply.github.com>
2021-09-02 17:05:50 -0300

    feat: implements AppMemoryUsage and AppMemoryUsageLimit for Android
    Addresses #5891
    
    Co-Authored-By: @flsobral

---
+++ src/Uno.UWP/System/MemoryManager.Android.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.OS;
+using Android.App;
+using Uno.UI;
+
+namespace Windows.System
+{
+	partial class MemoryManager
+	{
+		public static ulong AppMemoryUsage
+		{
+			get
+			{
+				Debug.MemoryInfo mi = new Debug.MemoryInfo();
+				Debug.GetMemoryInfo(mi);
+				var totalMemory = mi.NativePrivateDirty + mi.OtherSharedDirty;
+				return (ulong)totalMemory;
+			}
+		}
+
+		public static ulong AppMemoryUsageLimit
+		{
+			get
+			{
+				ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+				ActivityManager.FromContext(ContextHelper.Current)?.GetMemoryInfo(memoryInfo);
+
+				return AppMemoryUsage + (ulong)memoryInfo.AvailMem - (ulong)memoryInfo.Threshold;
+			}
+		}
+	}
+}
