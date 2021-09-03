d03e44d79d4801444d67d99e87fdcb4d2a29fd86
Martin Zikmund <martin@zikmund.dev>
2021-09-03 08:58:42 +0200

    test: Flyout should return focus to previously focused

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_Flyout.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_Flyout.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
 
@@ -457,6 +458,38 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 			var stackPanel = content as StackPanel;
 			Assert.AreEqual("My Data Context", (stackPanel.Children[0] as TextBlock).Text);
 #endif
+		}
+
+		[TestMethod]
+		[RunsOnUIThread]
+		public async Task When_Flyout_Content_Takes_Focus()
+		{
+			var stackPanel = new StackPanel();
+			var button = new Button() { Content = "Flyout owner" };
+			stackPanel.Children.Add(button);
+			TestServices.WindowHelper.WindowContent = stackPanel;
+			await TestServices.WindowHelper.WaitForIdle();
+
+			var flyout = new Flyout();
+			var flyoutButton = new Button() { Content = "Flyout content" };
+			flyout.Content = flyoutButton;
+			FlyoutBase.SetAttachedFlyout(button, flyout);
+			button.Focus(FocusState.Pointer);
+
+			Assert.AreEqual(button, FocusManager.GetFocusedElement());
+
+			FlyoutBase.ShowAttachedFlyout(button);
+			flyoutButton.Focus(FocusState.Pointer);
+			await TestServices.WindowHelper.WaitForIdle();
+
+			Assert.AreNotEqual(button, FocusManager.GetFocusedElement());
+
+			flyout.Hide();
+			await TestServices.WindowHelper.WaitForIdle();
+
+			Assert.AreEqual(button, FocusManager.GetFocusedElement());
+
+			TestServices.WindowHelper.WindowContent = null;
 		}
 
 		private static void VerifyRelativeContentPosition(HorizontalPosition horizontalPosition, VerticalPosition verticalPosition, FrameworkElement content, double minimumTargetOffset, FrameworkElement target)
