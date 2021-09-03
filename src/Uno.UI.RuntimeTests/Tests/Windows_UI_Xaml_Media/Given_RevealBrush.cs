9f5e1a8c9083f98fe01c1e05691b7734e25ae772
David Oliver <david.oliver@nventive.com>
2021-09-03 11:54:43 -0400

    fix: [Skia] Fix layout when RevealBrush is used
    Fall back on FallbackColor when RevealBrush is assigned, instead of trying to use unimplemented CompositionBrush property which throws an exception.

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Media/Given_RevealBrush.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using static Private.Infrastructure.TestServices;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Media
+{
+	[TestClass]
+	[RunsOnUIThread]
+	public class Given_RevealBrush
+	{
+		[TestMethod]
+		public async Task When_RevealBrush_Assigned()
+		{
+			// Basic smoke test - RevealBrush is currently unimplemented, but it shouldn't break the layout
+
+			var siblingBorder = new Border { Width = 43, Height = 22 };
+			var gridWithRevealBackground = new Grid
+			{
+				Width = 57,
+				Height = 34,
+				Background = new RevealBackgroundBrush() { Color = Colors.Brown, FallbackColor = Colors.Brown },
+			};
+			var parentSP = new StackPanel
+			{
+				Children =
+				{
+					gridWithRevealBackground,
+					siblingBorder
+				}
+			};
+
+			WindowHelper.WindowContent = parentSP;
+			await WindowHelper.WaitForLoaded(parentSP);
+
+			await WindowHelper.WaitForEqual(57, () => gridWithRevealBackground.ActualWidth);
+			Assert.AreEqual(34, gridWithRevealBackground.ActualHeight);
+
+			Assert.AreEqual(43, siblingBorder.ActualWidth);
+			Assert.AreEqual(22, siblingBorder.ActualHeight);
+		}
+	}
+}
