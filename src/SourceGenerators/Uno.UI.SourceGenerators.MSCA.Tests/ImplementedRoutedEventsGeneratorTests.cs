4a4c2c9ad6013d604d94d20b12515aae67931c4c
Youssef1313 <youssefvictor00@gmail.com>
2021-09-03 13:07:55 +0200

    test: MS.CA.Testing framework, not working currently

---
+++ src/SourceGenerators/Uno.UI.SourceGenerators.MSCA.Tests/ImplementedRoutedEventsGeneratorTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uno.UI.SourceGenerators.ImplementedRoutedEvents;
+using Uno.UI.SourceGenerators.Tests.Verifiers;
+
+namespace Uno.UI.SourceGenerators.Tests
+{
+	using Verify = CSharpSourceGeneratorVerifier<ImplementedRoutedEventsGenerator>;
+
+	[TestClass]
+	public class ImplementedRoutedEventsGeneratorTests
+	{
+		private const string ControlStub = @"
+namespace Windows.UI.Xaml.Controls
+{
+	public class Control
+	{
+		protected virtual RoutedEventFlag GetImplementedRoutedEvents()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}";
+		[TestMethod]
+		public async Task Test()
+		{
+			const string inputSource = @"
+using Windows.UI.Xaml.Controls;
+
+public partial class MyAwesomeControl : Control
+{
+}
+";
+			await new Verify.Test
+			{
+				TestState =
+				{
+					Sources = { ControlStub, inputSource },
+					GeneratedSources =
+					{
+					},
+				},
+			}.RunAsync();
+		}
+	}
+}
