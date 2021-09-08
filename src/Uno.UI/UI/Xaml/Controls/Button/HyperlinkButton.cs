c1ef8c12e5c215fac1023ae4288dbc1b770ce68b
Youssef1313 <youssefvictor00@gmail.com>
2021-09-08 21:52:03 +0200

    fix: Don't underline HyperlinkButton if not ImplicitTextBlock

--- src/Uno.UI/UI/Xaml/Controls/Button/HyperlinkButton.cs
+++ src/Uno.UI/UI/Xaml/Controls/Button/HyperlinkButton.cs
@@ -19,9 +19,26 @@ namespace Windows.UI.Xaml.Controls
 			DefaultStyleKey = typeof(HyperlinkButton);
 		}
 
-		/// <summary>
-		/// Gets or sets the Uniform Resource Identifier (URI) to navigate to when the HyperlinkButton is clicked.
-		/// </summary>
+		protected override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			// This differs from UWP, where it looks for a template child named "ContentPresenter", 
+			// but ultimately sets the underline on the TextBlock of the first ContentPresenter with a {TemplateBinding Content}.
+			// UWP also doesn't seem to do this in OnApplyTemplate (it's done between the first Measure and the first Arrange).
+			if (GetTemplateChild("ContentPresenter") is ContentPresenter contentPresenter)
+			{
+				// Forces ContentPresenter to materialize its template.
+				contentPresenter.Measure(new Size(0, 0));
+				if (VisualTreeHelper.GetChildrenCount(contentPresenter) == 1 && VisualTreeHelper.GetChild(contentPresenter, 0) is ImplicitTextBlock textBlock)
+				{
+					textBlock.TextDecorations = Windows.UI.Text.TextDecorations.Underline;
+				}
+			}
+		}
+
+		#region NavigateUri
+
 		public Uri NavigateUri
 		{
 			get => (Uri)GetValue(NavigateUriProperty);
