d110581f526eeb84411f57166f070c5b47b77a31
Jerome Laban <jerome.laban@nventive.com>
2021-09-11 14:02:05 -0400

    feat: Upgrade to .NET 6 RC1

--- src/Uno.UWP/ApplicationModel/Calls/Internal/CallStateListener.Android.cs
+++ src/Uno.UWP/ApplicationModel/Calls/Internal/CallStateListener.Android.cs
@@ -7,6 +7,7 @@ using Android.Telephony;
 
 namespace Windows.ApplicationModel.Calls
 {
+#pragma warning disable CS0618, CS0672 // PhoneStateListener is obsolete
 	internal class CallStateListener : PhoneStateListener
 	{
 		public override void OnCallStateChanged([GeneratedEnum] CallState state, string phoneNumber)
@@ -14,5 +15,6 @@ namespace Windows.ApplicationModel.Calls
 			PhoneCallManager.RaiseCallStateChanged();
 		}
 	}
+#pragma warning restore CS0618, CS0672 // PhoneStateListener is obsolete
 }
 #endif
