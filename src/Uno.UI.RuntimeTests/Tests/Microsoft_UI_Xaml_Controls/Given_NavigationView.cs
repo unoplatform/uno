6b50f1ca11b6d3689dbb1b5275bcad36c85804c0
David Oliver <david.oliver@nventive.com>
2021-09-14 15:12:44 -0400

    chore: Adjust NavView test for WinUI 2.6 styles
    Fixes for template and color values changes.

--- src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_NavigationView.cs
+++ src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_NavigationView.cs
@@ -38,7 +38,6 @@ namespace Uno.UI.RuntimeTests.Tests.Microsoft_UI_Xaml_Controls
 	{
 		[TestMethod]
 		[RequiresFullWindow]
-		[Ignore("Disabled to enable WinUI 2.6 merge")]
 		public async Task When_SelectedItem_Set_Before_Load_And_Theme_Changed()
 		{
 			using (StyleHelper.UseFluentStyles())
@@ -71,18 +70,22 @@ namespace Uno.UI.RuntimeTests.Tests.Microsoft_UI_Xaml_Controls
 				await WindowHelper.WaitForIdle();
 
 				var togglePaneButton = navView.FindFirstChild<Button>(b => b.Name == "TogglePaneButton");
-				var iconTextBlock = togglePaneButton?.FindFirstChild<TextBlock>(t => t.Name == "Icon");
+				var icon = togglePaneButton?.FindFirstChild<FrameworkElement>(f => f.Name == "Icon");
+				var iconTextBlock = icon?.FindFirstChild<TextBlock>(includeCurrent: true);
 
-				Assert.AreEqual(Colors.Black, (iconTextBlock.Foreground as SolidColorBrush)?.Color);
+				Assert.IsNotNull(iconTextBlock);
+
+				ColorAssert.IsDark((iconTextBlock.Foreground as SolidColorBrush)?.Color);
 				using (ThemeHelper.UseDarkTheme())
 				{
 					await WindowHelper.WaitForIdle();
 					Assert.AreEqual(Colors.White, (iconTextBlock.Foreground as SolidColorBrush)?.Color);
+					ColorAssert.IsLight((iconTextBlock.Foreground as SolidColorBrush)?.Color);
 #if __ANDROID__
 					// This is the meat of the test - we verify that the actual color of the TextBlock matches the managed Color, which will only be the
 					// case if it was correctly measured and arranged as requested after the theme changed.
 					Assert.AreEqual(false, iconTextBlock.IsLayoutRequested);
-					Assert.AreEqual(Android.Graphics.Color.White, iconTextBlock.NativeArrangedColor);
+					Assert.AreEqual((Android.Graphics.Color)((iconTextBlock.Foreground as SolidColorBrush).Color), iconTextBlock.NativeArrangedColor);
 #endif
 				}
 			}
