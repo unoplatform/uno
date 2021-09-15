2f03cedd49dd7d7422e70c6a6505b732f13e1c52
Youssef1313 <youssefvictor00@gmail.com>
2021-09-15 12:50:23 +0200

    test: Add analyzer tests

---
+++ src/Uno.Analyzers.Tests/UnoStringDependencyPropertyAnalyzerTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uno.NonShippingAnalyzers;
+
+namespace Uno.Analyzers.Tests.Verifiers
+{
+	using VerifyCS = CSharpCodeFixVerifier<UnoStringDependencyPropertyAnalyzer, EmptyCodeFixProvider>;
+
+	[TestClass]
+	public class UnoStringDependencyPropertyAnalyzerTests
+	{
+		private const string Stub = @"
+using System;
+
+namespace Windows.UI.Xaml
+{
+	public sealed class DependencyProperty
+	{
+		public static DependencyProperty Register(string name, Type propertyType, Type ownerType, PropertyMetadata typeMetadata) => throw new System.NotImplementedException();
+		internal static DependencyProperty Register(string name, Type propertyType, Type ownerType, FrameworkPropertyMetadata typeMetadata) => throw new System.NotImplementedException();
+		public static DependencyProperty RegisterAttached(string name, Type propertyType, Type ownerType, PropertyMetadata typeMetadata) => throw new System.NotImplementedException();
+		internal static DependencyProperty RegisterAttached(string name, Type propertyType, Type ownerType, FrameworkPropertyMetadata typeMetadata) => throw new System.NotImplementedException();
+	}
+
+	public class PropertyMetadata
+	{
+		public PropertyMetadata() { }
+		public PropertyMetadata(object defaultValue) { }
+	}
+
+	public class FrameworkPropertyMetadata
+	{
+		public FrameworkPropertyMetadata() { }
+		public FrameworkPropertyMetadata(object defaultValue) { }
+	}
+}
+";
+		private static async Task VerifyAsync(string code)
+		{
+			await new VerifyCS.Test
+			{
+				TestState =
+				{
+					Sources = { Stub, code },
+				}
+			}.RunAsync();
+		}
+
+		[TestMethod]
+		public async Task When_No_Default_Value_Is_Given()
+		{
+			await VerifyAsync(@"
+using Windows.UI.Xaml;
+
+public class C
+{
+    public static DependencyProperty MyProperty { get; } = DependencyProperty.Register(string.Empty, typeof(string), typeof(C), [|new PropertyMetadata()|]);
+	public static DependencyProperty MySecondProperty { get; } = DependencyProperty.RegisterAttached(string.Empty, typeof(string), typeof(C), [|new PropertyMetadata()|]);
+
+    public static DependencyProperty MyThirdProperty { get; } = DependencyProperty.Register(string.Empty, typeof(string), typeof(C), [|new FrameworkPropertyMetadata()|]);
+	public static DependencyProperty MyForthProperty { get; } = DependencyProperty.RegisterAttached(string.Empty, typeof(string), typeof(C), [|new FrameworkPropertyMetadata()|]);
+}
+");
+		}
+
+		[TestMethod]
+		public async Task When_Null_Default_Value_Is_Given()
+		{
+			await VerifyAsync(@"
+using Windows.UI.Xaml;
+
+public class C
+{
+    public static DependencyProperty MyProperty { get; } = DependencyProperty.Register(string.Empty, typeof(string), typeof(C), [|new PropertyMetadata(defaultValue: null)|]);
+	public static DependencyProperty MySecondProperty { get; } = DependencyProperty.RegisterAttached(string.Empty, typeof(string), typeof(C), [|new PropertyMetadata(defaultValue: null)|]);
+
+    public static DependencyProperty MyThirdProperty { get; } = DependencyProperty.Register(string.Empty, typeof(string), typeof(C), [|new FrameworkPropertyMetadata(defaultValue: null)|]);
+	public static DependencyProperty MyForthProperty { get; } = DependencyProperty.RegisterAttached(string.Empty, typeof(string), typeof(C), [|new FrameworkPropertyMetadata(defaultValue: null)|]);
+}
+");
+		}
+
+		[TestMethod]
+		public async Task When_Constant_Default_Value_Is_Given()
+		{
+			await VerifyAsync(@"
+using Windows.UI.Xaml;
+
+public class C
+{
+    public static DependencyProperty MyProperty { get; } = DependencyProperty.Register(string.Empty, typeof(string), typeof(C), new PropertyMetadata(defaultValue: """"));
+	public static DependencyProperty MySecondProperty { get; } = DependencyProperty.RegisterAttached(string.Empty, typeof(string), typeof(C), new PropertyMetadata(defaultValue: """"));
+
+    public static DependencyProperty MyThirdProperty { get; } = DependencyProperty.Register(string.Empty, typeof(string), typeof(C), new FrameworkPropertyMetadata(defaultValue: """"));
+	public static DependencyProperty MyForthProperty { get; } = DependencyProperty.RegisterAttached(string.Empty, typeof(string), typeof(C), new FrameworkPropertyMetadata(defaultValue: """"));
+}
+");
+		}
+
+		[TestMethod]
+		public async Task When_Non_Constant_Default_Value_Is_Given()
+		{
+			await VerifyAsync(@"
+using Windows.UI.Xaml;
+
+public class C
+{
+    public static DependencyProperty MyProperty { get; } = DependencyProperty.Register(string.Empty, typeof(string), typeof(C), new PropertyMetadata(defaultValue: string.Empty));
+	public static DependencyProperty MySecondProperty { get; } = DependencyProperty.RegisterAttached(string.Empty, typeof(string), typeof(C), new PropertyMetadata(defaultValue: string.Empty));
+
+    public static DependencyProperty MyThirdProperty { get; } = DependencyProperty.Register(string.Empty, typeof(string), typeof(C), new FrameworkPropertyMetadata(defaultValue: string.Empty));
+	public static DependencyProperty MyForthProperty { get; } = DependencyProperty.RegisterAttached(string.Empty, typeof(string), typeof(C), new FrameworkPropertyMetadata(defaultValue: string.Empty));
+}
+");
+		}
+	}
+}
--- src/Uno.NonShippingAnalyzers/Uno.NonShippingAnalyzers/UnoStringDependencyPropertyAnalyzer.cs
+++ src/Uno.NonShippingAnalyzers/Uno.NonShippingAnalyzers/UnoStringDependencyPropertyAnalyzer.cs
@@ -9,23 +9,23 @@ using Microsoft.CodeAnalysis.Operations;
 namespace Uno.NonShippingAnalyzers
 {
 	[DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public sealed class UnoStringDependencyPropertyAnalyzer : DiagnosticAnalyzer
-    {
-        public const string DiagnosticId = "Uno0002_Internal";
+	public sealed class UnoStringDependencyPropertyAnalyzer : DiagnosticAnalyzer
+	{
+		public const string DiagnosticId = "Uno0002_Internal";
 
-        private static readonly string s_title = "String dependency properties (in *most* cases) shouldn't have null default value.";
+		private static readonly string s_title = "String dependency properties (in *most* cases) shouldn't have null default value.";
 		private static readonly string s_messageFormat = "Review dependency property and make sure whether it should be null or not.";
 
-        private const string Category = "Reliability";
+		private const string Category = "Reliability";
 
-        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, s_title, s_messageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true);
+		private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, s_title, s_messageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
 
-        public override void Initialize(AnalysisContext context)
-        {
-            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
-            context.EnableConcurrentExecution();
+		public override void Initialize(AnalysisContext context)
+		{
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+			context.EnableConcurrentExecution();
 
 			context.RegisterCompilationStartAction(context =>
 			{
@@ -68,14 +68,20 @@ namespace Uno.NonShippingAnalyzers
 				return true;
 			}
 
-			if (!defaultValueArgumentOperation.ConstantValue.HasValue)
+			IOperation operation = defaultValueArgumentOperation;
+			if (defaultValueArgumentOperation.Value is IConversionOperation conversionOperation)
+			{
+				operation = conversionOperation;
+			}
+
+			if (!operation.ConstantValue.HasValue)
 			{
 				// Not a constant value, e.g: a method call or a field reference like string.Empty.
 				return false;
 			}
 
 			// Report a diagnostic if a null constant value is specified.
-			return defaultValueArgumentOperation.ConstantValue.Value is null;
+			return operation.ConstantValue.Value is null;
 		}
 	}
 }
