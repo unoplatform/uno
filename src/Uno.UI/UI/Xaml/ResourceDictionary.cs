57068a37f86291a5274ca04fa56995f88c93a32e
Youssef1313 <youssefvictor00@gmail.com>
2021-09-17 14:55:02 +0200

    fix: Invalidate cached theme dictionary when theme dictionaries are added/removed

--- src/Uno.UI/UI/Xaml/ResourceDictionary.cs
+++ src/Uno.UI/UI/Xaml/ResourceDictionary.cs
@@ -22,6 +22,30 @@ namespace Windows.UI.Xaml
 		private ResourceDictionary _themeDictionaries;
 
 		/// <summary>
+		/// This event is fired when a key that has value of type <see cref="ResourceDictionary"/> is added or changed in the current <see cref="ResourceDictionary" />
+		/// </summary>
+		private event EventHandler<ResourceDictionaryChangedEventArgs> ResourceDictionaryValueChange;
+
+		private class ResourceDictionaryChangedEventArgs
+		{
+			public ResourceDictionaryChangedEventArgs(object changedKey, ResourceDictionary newValue)
+			{
+				ChangedKey = changedKey;
+				NewValue = newValue;
+			}
+
+			/// <summary>
+			/// The key that was added or removed. This can be null if the dictionary is cleared, meaning all keys are removed.
+			/// </summary>
+			public object ChangedKey { get; }
+
+			/// <summary>
+			/// The <see cref="ResourceDictionary" /> that was added. This can be null if the theme was removed.
+			/// </summary>
+			public ResourceDictionary NewValue { get; }
+		}
+
+		/// <summary>
 		/// If true, there may be lazily-set values in the dictionary that need to be initialized.
 		/// </summary>
 		private bool _hasUnmaterializedItems = false;
@@ -48,7 +72,25 @@ namespace Windows.UI.Xaml
 		}
 
 		public IList<ResourceDictionary> MergedDictionaries => _mergedDictionaries;
-		public IDictionary<object, object> ThemeDictionaries => _themeDictionaries ??= new ResourceDictionary();
+		public IDictionary<object, object> ThemeDictionaries => GetOrCreateThemeDictionaries();
+
+		private ResourceDictionary GetOrCreateThemeDictionaries()
+		{
+			if (_themeDictionaries is null)
+			{
+				_themeDictionaries = new ResourceDictionary();
+				_themeDictionaries.ResourceDictionaryValueChange += (sender, e) =>
+				{
+					// Invalidate the cache whenever a theme dictionary is added/removed.
+					// This is safest and avoids handling edge cases.
+					// Note that adding or removing theme dictionary isn't very common,
+					// so invalidating the cache shouldn't be a performance issue.
+					_activeTheme = ResourceKey.Empty;
+				};
+			}
+
+			return _themeDictionaries;
+		}
 
 		/// <summary>
 		/// Is this a ResourceDictionary created from system resources, ie within the Uno.UI assembly?
@@ -96,11 +138,25 @@ namespace Windows.UI.Xaml
 			}
 		}
 
-		public bool Remove(object key) => _values.Remove(new ResourceKey(key));
+		public bool Remove(object key)
+		{
+			var keyToRemove = new ResourceKey(key);
+			if (_values.Remove(keyToRemove))
+			{
+				ResourceDictionaryValueChange?.Invoke(this, new ResourceDictionaryChangedEventArgs(keyToRemove, null));
+				return true;
+			}
 
-		public bool Remove(KeyValuePair<object, object> key) => _values.Remove(new ResourceKey(key.Key));
+			return false;
+		}
 
-		public void Clear() => _values.Clear();
+		public bool Remove(KeyValuePair<object, object> key) => Remove(key.Key);
+
+		public void Clear()
+		{
+			_values.Clear();
+			ResourceDictionaryValueChange?.Invoke(this, new ResourceDictionaryChangedEventArgs(null, null));
+		}
 
 		public void Add(object key, object value) => Set(new ResourceKey(key), value, throwIfPresent: true);
 
@@ -189,7 +245,7 @@ namespace Windows.UI.Xaml
 			}
 			set
 			{
-				if(!(key is null))
+				if (!(key is null))
 				{
 					Set(new ResourceKey(key), value, throwIfPresent: false);
 				}
@@ -216,6 +272,10 @@ namespace Windows.UI.Xaml
 			else
 			{
 				_values[resourceKey] = value;
+				if (value is ResourceDictionary addedOrChangedResourceDictionary)
+				{
+					ResourceDictionaryValueChange?.Invoke(this, new ResourceDictionaryChangedEventArgs(resourceKey, addedOrChangedResourceDictionary));
+				}
 			}
 		}
 
@@ -241,6 +301,10 @@ namespace Windows.UI.Xaml
 				{
 					value = newValue;
 					_values[key] = newValue; // If Initializer threw an exception this will push null, to avoid running buggy initialization again and again (and avoid surfacing initializer to consumer code)
+					if (newValue is ResourceDictionary addedOrChangedResourceDictionary)
+					{
+						ResourceDictionaryValueChange?.Invoke(this, new ResourceDictionaryChangedEventArgs(key, addedOrChangedResourceDictionary));
+					}
 					ResourceResolver.PopScope();
 				}
 			}
@@ -381,8 +445,7 @@ namespace Windows.UI.Xaml
 			_mergedDictionaries.AddRange(source._mergedDictionaries);
 			if (source._themeDictionaries != null)
 			{
-				_themeDictionaries ??= new ResourceDictionary();
-				_themeDictionaries.CopyFrom(source._themeDictionaries);
+				GetOrCreateThemeDictionaries().CopyFrom(source._themeDictionaries);
 			}
 		}
 
