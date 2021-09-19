5bf7a807f5f645f542913ea9e3f1be11885975a7
Martin Zikmund <martin@zikmund.dev>
2021-09-19 17:17:32 +0200

    refactor: Concrete versions of StartStopDelegateWrapper

--- src/Uno.UWP/Helpers/StartStopDelegateWrapper.cs
+++ src/Uno.UWP/Helpers/StartStopDelegateWrapper.cs
@@ -2,89 +2,73 @@
 
 using System;
 
-namespace Uno.Helpers;
-
-/// <summary>
-/// Creates a wrapper around a generic event, which needs to be synchronized
-/// and needs to run an action when first subscriber is added and when
-/// last subscriber is removed. The operations executed when first subscriber
-/// is added and last subscriber is removed will execute within
-/// a synchronization lock, so please avoid blocking within the actions.
-/// </summary>
-internal class StartStopDelegateWrapper<TDelegate>
-	where TDelegate : Delegate
+namespace Uno.Helpers
 {
-	private readonly object _syncLock;
-	private readonly Action _onFirst;
-	private readonly Action _onLast;
-
 	/// <summary>
-	/// Creates a new instance of start-stop delegate wrapper.
+	/// Creates a wrapper around a generic event, which needs to be synchronized
+	/// and needs to run an action when first subscriber is added and when
+	/// last subscriber is removed. The operations executed when first subscriber
+	/// is added and last subscriber is removed will execute within
+	/// a synchronization lock, so please avoid blocking within the actions.
 	/// </summary>
-	/// <param name="onFirst">Action to run when first subscriber is added.
-	/// This will run within a synchronization lock so it should not involve blocking operations.</param>
-	/// <param name="onLast">Action to run when last subscriber is removed.
-	/// This will run within a synchronization lock so it should not involve blocking operations.</param>
-	/// <param name="sharedLock">Optional shared object to lock on (when multiple events
-	/// rely on the same native platform operation.</param>
-	public StartStopDelegateWrapper(
-		Action onFirst,
-		Action onLast,
-		object? sharedLock = null)
+	internal class StartStopDelegateWrapper<TDelegate>
+		where TDelegate : Delegate
 	{
-		_onFirst = onFirst;
-		_onLast = onLast;
-		_syncLock = sharedLock ?? new object();
-	}
+		private readonly object _syncLock;
+		private readonly Action _onFirst;
+		private readonly Action _onLast;
 
-	/// <summary>
-	/// Backing delegate.
-	/// </summary>
-	public TDelegate? Event { get; private set; }
-
-	/// <summary>
-	/// Indicates if there is at least one active subscription.
-	/// </summary>
-	public bool IsActive => Event != null;
-
-	/// <summary>
-	/// Synchronization lock.
-	/// </summary>
-	public object SyncLock => _syncLock;
-
-	/// <summary>
-	/// Adds a handler.
-	/// </summary>
-	/// <param name="handler">Handler.</param>
-	public void AddHandler(TDelegate handler)
-	{
-		lock (_syncLock)
+		/// <summary>
+		/// Creates a new instance of start-stop delegate wrapper.
+		/// </summary>
+		/// <param name="onFirst">Action to run when first subscriber is added.
+		/// This will run within a synchronization lock so it should not involve blocking operations.</param>
+		/// <param name="onLast">Action to run when last subscriber is removed.
+		/// This will run within a synchronization lock so it should not involve blocking operations.</param>
+		/// <param name="sharedLock">Optional shared object to lock on (when multiple events
+		/// rely on the same native platform operation.</param>
+		public StartStopDelegateWrapper(
+			Action onFirst,
+			Action onLast,
+			object? sharedLock = null)
 		{
-			var firstSubscriber = Event == null;
-			Event = (TDelegate)Delegate.Combine(Event, handler);
-			if (firstSubscriber)
-			{
-				_onFirst();
-			}
+			_onFirst = onFirst;
+			_onLast = onLast;
+			_syncLock = sharedLock ?? new object();
 		}
-	}
 
-	/// <summary>
-	/// Removes a handler.
-	/// </summary>
-	/// <param name="handler">Handler.</param>
-	public void RemoveHandler(TDelegate handler)
-	{
-		lock (_syncLock)
+		public TDelegate? Event { get; private set; } = null;
+
+		public object SyncLock => _syncLock;
+
+		public void AddHandler(TDelegate handler)
 		{
-			if (Event != null)
+			lock (_syncLock)
 			{
-				Event = (TDelegate?)Delegate.Remove(Event, handler);
-				if (Event == null)
+				var firstSubscriber = Event == null;
+				Event = (TDelegate)Delegate.Combine(Event, handler);
+				if (firstSubscriber)
 				{
-					_onLast();
+					_onFirst();
 				}
 			}
 		}
+
+		public void RemoveHandler(TDelegate handler)
+		{
+			lock (_syncLock)
+			{
+				if (Event != null)
+				{
+					Event = (TDelegate?)Delegate.Remove(Event, handler);
+					if (Event == null)
+					{
+						_onLast();
+					}
+				}
+			}
+		}
+
+		public bool IsActive => Event != null;
 	}
 }
--- src/Uno.UWP/Helpers/StartStopEventWrapper.cs
+++ src/Uno.UWP/Helpers/StartStopEventWrapper.cs
@@ -11,23 +11,30 @@ namespace Uno.Helpers;
 internal class StartStopEventWrapper<TEventArgs> : StartStopDelegateWrapper<EventHandler<TEventArgs>>
 {
 	/// <summary>
-	/// Creates a new instance of start-stop event wrapper.
+	/// Start stop wrapper for EventHandler<T> events.
 	/// </summary>
-	/// <param name="onFirst">Action to run when first subscriber is added.
-	/// This will run within a synchronization lock so it should not involve blocking operations.</param>
-	/// <param name="onLast">Action to run when last subscriber is removed.
-	/// This will run within a synchronization lock so it should not involve blocking operations.</param>
-	/// <param name="sharedLock">Optional shared object to lock on (when multiple events
-	/// rely on the same native platform operation.</param>
-	public StartStopEventWrapper(Action onFirst, Action onLast, object? sharedLock = null) :
-		base(onFirst, onLast, sharedLock)
+	/// <typeparam name="TEventArgs">Event args type.</typeparam>
+	internal class StartStopEventWrapper<TEventArgs> : StartStopDelegateWrapper<EventHandler<TEventArgs>>
 	{
-	}
+		/// <summary>
+		/// Creates a new instance of start-stop event wrapper.
+		/// </summary>
+		/// <param name="onFirst">Action to run when first subscriber is added.
+		/// This will run within a synchronization lock so it should not involve blocking operations.</param>
+		/// <param name="onLast">Action to run when last subscriber is removed.
+		/// This will run within a synchronization lock so it should not involve blocking operations.</param>
+		/// <param name="sharedLock">Optional shared object to lock on (when multiple events
+		/// rely on the same native platform operation.</param>
+		public StartStopEventWrapper(Action onFirst, Action onLast, object? sharedLock = null) :
+			base(onFirst, onLast, sharedLock)
+		{
+		}
 
-	/// <summary>
-	/// Invokes the event.
-	/// </summary>
-	/// <param name="sender">Sender.</param>
-	/// <param name="args">Args.</param>
-	public void Invoke(object sender, TEventArgs args) => Event?.Invoke(sender, args);
+		/// <summary>
+		/// Invokes the event.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="args">Args.</param>
+		public void Invoke(object sender, TEventArgs args) => Event?.Invoke(sender, args);
+	}
 }
