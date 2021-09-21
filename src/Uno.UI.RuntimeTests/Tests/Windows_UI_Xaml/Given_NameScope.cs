72f6cd21464900e328b318e7ab7647bb6c5c59ff
Youssef1313 <youssefvictor00@gmail.com>
2021-09-21 20:28:06 +0200

    test: Add test for NameScope.FindName inside resource dictionary

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_NameScope.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using static Private.Infrastructure.TestServices;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml
+{
+	[TestClass]
+	public partial class Given_NameScope
+	{
+		[TestMethod]
+		[RunsOnUIThread]
+		public async Task When_Inside_ResourceDictionary()
+		{
+			var SUT = new Given_NameScope_When_Inside_ResourceDictionary();
+			WindowHelper.WindowContent = SUT;
+			await WindowHelper.WaitForIdle();
+			var namescope = NameScope.GetNameScope(SUT);
+			var brush = (SolidColorBrush)namescope.FindName("MyBrush");
+			Assert.AreEqual(Colors.Blue, brush.Color);
+		}
+	}
+}
