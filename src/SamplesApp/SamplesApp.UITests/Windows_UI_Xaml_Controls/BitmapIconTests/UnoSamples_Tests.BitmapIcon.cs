89e205bedd92dcd95382542989e03b69277e1e00
Youssef1313 <youssefvictor00@gmail.com>
2021-09-21 11:55:22 +0200

    test: Assert monochrome color taking effect

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/BitmapIconTests/UnoSamples_Tests.BitmapIcon.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/BitmapIconTests/UnoSamples_Tests.BitmapIcon.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Uno.UITest;
 using Uno.UITest.Helpers;
 using Uno.UITest.Helpers.Queries;
 
@@ -14,6 +15,22 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.BitmapIconTests
 	[TestFixture]
 	public partial class BitmapIcon_Tests : SampleControlUITestBase
 	{
+		private bool ContainsColor(Bitmap bitmap, Rectangle rect, Color color)
+		{
+			for (var x = rect.Left; x < rect.Right; x++)
+			{
+				for (var y = rect.Top; y < rect.Bottom; y++)
+				{
+					if (bitmap.GetPixel(x, y) == color)
+					{
+						return true;
+					}
+				}
+			}
+
+			return false;
+		}
+
 		[Test]
 		[AutoRetry]
 		public void When_BitmapIcon_Generic()
@@ -21,21 +38,25 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.BitmapIconTests
 			Run("UITests.Shared.Windows_UI_Xaml_Controls.BitmapIconTests.BitmapIcon_Generic");
 
 			var colorChange = _app.Marked("colorChange");
-			var icon1 = _app.Marked("icon1");
-			var icon2 = _app.Marked("icon2");
+			var icon1 = _app.Query("icon1").Single();
+			var icon2 = _app.Query("icon2").Single();
 			_app.WaitForElement(colorChange);
 
-			TakeScreenshot("Initial");
+			using var initial = TakeScreenshot("Initial");
+			
+			var bitmap = initial.GetBitmap();
+			Assert.IsTrue(ContainsColor(bitmap, icon1.Rect.ToRectangle(), Color.Red));
+			Assert.IsTrue(ContainsColor(bitmap, icon2.Rect.ToRectangle(), Color.Blue));
 
 			_app.FastTap(colorChange);
 
-			var color1 = icon1.GetDependencyPropertyValue<string>("Foreground");
-			var color2 = icon2.GetDependencyPropertyValue<string>("Foreground");
+			using var afterColorChange = TakeScreenshot("Changed");
+			bitmap = afterColorChange.GetBitmap();
+			Assert.IsTrue(ContainsColor(bitmap, icon1.Rect.ToRectangle(), Color.Yellow));
+			Assert.IsTrue(ContainsColor(bitmap, icon2.Rect.ToRectangle(), Color.Green));
 
-			_app.WaitForDependencyPropertyValue(icon1, "Foreground", "[SolidColorBrush #FFFFFF00]");
-			_app.WaitForDependencyPropertyValue(icon2, "Foreground", "[SolidColorBrush #FF008000]");
-
-			TakeScreenshot("Changed");
+			_app.WaitForDependencyPropertyValue(_app.Marked("icon1"), "Foreground", "[SolidColorBrush #FFFFFF00]");
+			_app.WaitForDependencyPropertyValue(_app.Marked("icon2"), "Foreground", "[SolidColorBrush #FF008000]");
 		}
 	}
 }
