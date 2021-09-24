ce4f024200ca2e7865d3b3fd3041b3ae28cd2b1c
Carl de Billy <carl.debilly@gmail.com>
2021-09-24 09:10:30 -0400

    ci: apply PR review

--- src/Uno.UI/UI/Xaml/UIElement.netstd.cs
+++ src/Uno.UI/UI/Xaml/UIElement.netstd.cs
@@ -170,7 +170,10 @@ namespace Windows.UI.Xaml
 
 			if (child.IsLoaded)
 			{
-				this.Log().Debug($"{this.GetDebugName()}: Inconsistent state: child {child} is already loaded (OnChildAdded). Common cause for this is an exception during Unloaded handling.");
+				if(if(this.Log().IsDebug())
+				{
+					this.Log().Debug($"{this.GetDebugName()}: Inconsistent state: child {child} is already loaded (OnChildAdded). Common cause for this is an exception during Unloaded handling.");
+				}
 			}
 			else
 			{
@@ -196,7 +199,10 @@ namespace Windows.UI.Xaml
 			}
 			else
 			{
-				this.Log().Debug($"{this.GetDebugName()}: Inconsistent state: child {child} is not loaded (OnChildRemoved). Common cause for this is an exception during Loaded handling.");
+				if(if(this.Log().IsDebug())
+				{
+					this.Log().Debug($"{this.GetDebugName()}: Inconsistent state: child {child} is not loaded (OnChildRemoved). Common cause for this is an exception during Loaded handling.");
+				}
 			}
 		}
 
