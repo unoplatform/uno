308ee44e928ee48afdc87e279d764afad78e1b6b
Martin Zikmund <martin@zikmund.dev>
2021-11-06 14:28:32 +0100

    chore: TabView update

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/TabView/TabViewItemAutomationPeer.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/TabView/TabViewItemAutomationPeer.cs
@@ -1,4 +1,6 @@
-﻿// MUX Reference: TabView.idl, commit 542e6f9
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference: TabViewItemAutomationPeer.cpp, commit 542e6f9
 
 using Microsoft.UI.Xaml.Controls;
 using Uno.UI.Helpers.WinUI;
