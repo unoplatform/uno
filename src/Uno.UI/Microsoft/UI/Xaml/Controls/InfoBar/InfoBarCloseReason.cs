f9344df83ee9ea804a25e3ac87c411e028ec39d0
Martin Zikmund <martin@zikmund.dev>
2021-11-07 21:10:51 +0100

    chore: Update InfoBar control

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/InfoBar/InfoBarCloseReason.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/InfoBar/InfoBarCloseReason.cs
@@ -1,10 +1,22 @@
-﻿// MUX reference InfoBar.idl, commit 3125489
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX reference InfoBar.idl, commit 3125489
 
 namespace Microsoft.UI.Xaml.Controls
 {
+	/// <summary>
+	/// Defines constants that indicate the cause of the InfoBar closure.
+	/// </summary>
 	public enum InfoBarCloseReason
 	{
+		/// <summary>
+		/// The InfoBar was closed by the user clicking the close button.
+		/// </summary>
 		CloseButton = 0,
+
+		/// <summary>
+		/// The InfoBar was programmatically closed.
+		/// </summary>
 		Programmatic = 1
 	}
 }
