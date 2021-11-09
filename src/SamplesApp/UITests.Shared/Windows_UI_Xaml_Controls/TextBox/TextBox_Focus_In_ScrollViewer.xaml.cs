b66891a43f17d77ff8605f11f5fe261636cf0fa6
Martin Zikmund <martin@zikmund.dev>
2021-11-09 21:07:21 +0100

    chore: Add sample description for manual testing

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/TextBox/TextBox_Focus_In_ScrollViewer.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/TextBox/TextBox_Focus_In_ScrollViewer.xaml.cs
@@ -3,7 +3,10 @@ using Windows.UI.Xaml.Controls;
 
 namespace Uno.UI.Samples.Content.UITests.TextBoxControl
 {
-	[Sample("TextBox", "TextBox_Focus")]
+	[Sample(
+		"TextBox",
+		Description = "When using touch to scroll, the TextBoxes should not get focus when pressed/released.",
+		IsManualTest = true)]
 	public sealed partial class TextBox_Focus_In_ScrollViewer : Page
     {
         public TextBox_Focus_In_ScrollViewer()
