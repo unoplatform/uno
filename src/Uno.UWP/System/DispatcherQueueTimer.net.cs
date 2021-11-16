afff690ffc6f7cd8229a063d212b79de1e866a72
Jerome Laban <jerome.laban@nventive.com>
2021-11-16 11:05:12 -0500

    chore: Adjust nuget, dispatcher move

--- src/Uno.UWP/System/DispatcherQueueTimer.net.cs
+++ src/Uno.UWP/System/DispatcherQueueTimer.net.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.System.Threading;
 
 #if HAS_UNO_WINUI && IS_UNO_UI_DISPATCHING_PROJECT
 namespace Microsoft.UI.Dispatching
