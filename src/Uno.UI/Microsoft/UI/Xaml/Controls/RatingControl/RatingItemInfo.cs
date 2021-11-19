7df83bcf8412011a892aef0991cbb689ca2f2b0a
Martin Zikmund <martin@zikmund.dev>
2021-11-19 21:47:48 +0100

    chore: RatingControl updates

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/RatingControl/RatingItemInfo.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/RatingControl/RatingItemInfo.cs
@@ -1,19 +1,22 @@
-// MUX Reference RatingItemInfo.properties.cpp, commit de78834
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference RatingItemInfo.cpp, commit 46f9da3
+
+#nullable enable
 
 using Windows.UI.Xaml;
 
-namespace Microsoft.UI.Xaml.Controls
+namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Represents information about the visual states of the elements that represent a rating.
+/// </summary>
+public partial class RatingItemInfo : DependencyObject
 {
 	/// <summary>
-	/// Represents information about the visual states of the elements that represent a rating.
+	/// Initializes a new instance of the RatingItemInfo class.
 	/// </summary>
-	public partial class RatingItemInfo : DependencyObject
+	public RatingItemInfo()
 	{
-		/// <summary>
-		/// Initializes a new instance of the RatingItemInfo class.
-		/// </summary>
-		public RatingItemInfo()
-		{
-		}
 	}
 }
