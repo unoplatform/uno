01e23e0f2fc6b97b07efc0e2534a8351ba73c7b0
David Oliver <david.oliver@nventive.com>
2021-12-03 11:37:29 -0500

    test: Add light-dismiss tests for Popup usage

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/PopupTests/UnoSamples_Popup_LightDismiss_Tests.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/PopupTests/UnoSamples_Popup_LightDismiss_Tests.cs
@@ -192,5 +192,107 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.PopupTests
 			Assert.AreEqual(null, fourthPopupTextBlock.GetDependencyPropertyValue("Opacity")?.ToString());
 			Assert.AreEqual(null, close5.GetDependencyPropertyValue("Opacity")?.ToString());
 		}
+
+		[Test]
+		[AutoRetry]
+		public void When_Dismissible_Popup()
+		{
+			Run("UITests.Windows_UI_Input.PointersTests.HitTest_LightDismiss");
+
+			_app.FastTap("LaunchDismissiblePopupButton");
+
+			_app.WaitForElement("TargetPopupContent");
+
+			Assert.AreEqual("True", _app.GetText("PopupStatusTextBlock"));
+
+			_app.FastTap("TargetPopupContent");
+
+			_app.WaitForText("ResultTextBlock", "Popup content pressed");
+			Assert.AreEqual("True", _app.GetText("PopupStatusTextBlock"));
+
+			_app.FastTap("ActionButton"); // Should dismiss popup, without executing button click event
+
+			_app.WaitForText("False", _app.GetText("PopupStatusTextBlock"));
+			Assert.AreEqual("Popup content pressed", _app.GetText("ResultTextBlock"));
+
+			_app.FastTap("ResetButton");
+		}
+
+		[Test]
+		[AutoRetry]
+		public void When_Undismissible_Popup()
+		{
+			Run("UITests.Windows_UI_Input.PointersTests.HitTest_LightDismiss");
+
+			_app.FastTap("LaunchUndismissiblePopupButton");
+
+			_app.WaitForElement("TargetPopupContent");
+
+			Assert.AreEqual("True", _app.GetText("PopupStatusTextBlock"));
+
+			_app.FastTap("TargetPopupContent");
+
+			_app.WaitForText("ResultTextBlock", "Popup content pressed");
+			Assert.AreEqual("True", _app.GetText("PopupStatusTextBlock"));
+
+			_app.FastTap("ActionButton"); // Should execute button click event, without dismissing popup
+
+			_app.WaitForText("ResultTextBlock", "Button pressed");
+			Assert.AreEqual("True", _app.GetText("PopupStatusTextBlock"));
+
+			_app.FastTap("ResetButton"); // Dismiss popup programmatically
+			_app.WaitForText("False", _app.GetText("PopupStatusTextBlock"));
+		}
+
+		[Test]
+		[AutoRetry]
+		public void When_Flyout()
+		{
+			Run("UITests.Windows_UI_Input.PointersTests.HitTest_LightDismiss");
+
+			_app.FastTap("FlyoutButton");
+
+			_app.WaitForElement("FlyoutContentGrid");
+			Assert.AreEqual("True", _app.GetText("FlyoutStatusTextBlock"));
+
+			_app.FastTap("FlyoutContentBorderNoPressed");
+			_app.FastTap("FlyoutContentBorderWithPressed");
+
+			_app.WaitForText("ResultTextBlock", "Flyout content pressed");
+			Assert.AreEqual("True", _app.GetText("FlyoutStatusTextBlock")); // Tapping flyout content should not have dismissed flyout
+
+			_app.FastTap("FlyoutContentGrid");
+			_app.WaitForText("FlyoutStatusTextBlock", "False"); // FlyoutContentGrid is hit-transparent at its centre, tapping it should dismiss the flyout
+
+			_app.FastTap("ResetButton");
+		}
+
+		[Test]
+		[AutoRetry]
+		public void When_ComboBox()
+		{
+			Run("UITests.Windows_UI_Input.PointersTests.HitTest_LightDismiss");
+
+			_app.FastTap("TargetComboBox");
+			_app.WaitForText("ComboBoxStatusTextBlock", "True");
+
+			Assert.AreEqual("None", _app.GetText("ResultTextBlock"));
+
+			_app.FastTap("ActionButton"); // Should dismiss ComboBox dropdown, without executing button click event
+
+			_app.WaitForText("False", _app.GetText("ComboBoxStatusTextBlock"));
+			Assert.AreEqual("None", _app.GetText("ResultTextBlock"));
+
+			_app.FastTap("ResetButton");
+
+			_app.FastTap("TargetComboBox");
+			_app.WaitForText("ComboBoxStatusTextBlock", "True");
+
+			_app.FastTap("TargetComboBox"); // Should select an item, dismissing the ComboBox
+			_app.WaitForText("ComboBoxStatusTextBlock", "False");
+			Assert.AreEqual("Item selected", _app.GetText("ResultTextBlock"));
+
+			_app.FastTap("ResetButton");
+		}
 	}
 }
