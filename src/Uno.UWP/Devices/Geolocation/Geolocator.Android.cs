117e991b44104e3b6bbd9af6f217e2b32c167e79
Martin Zikmund <martin@zikmund.dev>
2022-01-04 12:39:47 +0100

    chore: Geolocator adjustments

--- src/Uno.UWP/Devices/Geolocation/Geolocator.Android.cs
+++ src/Uno.UWP/Devices/Geolocation/Geolocator.Android.cs
@@ -1,13 +1,10 @@
 #if __ANDROID__
 #pragma warning disable 67
 
-
-
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Android.Locations;
@@ -16,28 +13,169 @@ using Android.Runtime;
 using Uno.Extensions;
 using Windows.ApplicationModel.Core;
 using Windows.Extensions;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Foundation.Metadata;
 using Windows.UI.Core;
-using System.Diagnostics;
 
 namespace Windows.Devices.Geolocation
 {
 	public sealed partial class Geolocator : Java.Lang.Object, ILocationListener
 	{
-		//using ConcurrentDictionary as concurrent HashSet (https://stackoverflow.com/questions/18922985/concurrent-hashsett-in-net-framework), byte is throwaway
-		private static ConcurrentDictionary<Geolocator, byte> _positionChangedSubscriptions = new ConcurrentDictionary<Geolocator, byte>();
+		// Only locations not older than this const can be used as current
+		// (used only in GetGeopositionAsync with parameters, parameterless call can return older location)
+		private const int MaxLocationAgeInSeconds = 60;
+
+		// Using ConcurrentDictionary as concurrent HashSet (https://stackoverflow.com/questions/18922985/concurrent-hashsett-in-net-framework), byte is throwaway.
+		private static readonly ConcurrentDictionary<Geolocator, byte> _positionChangedSubscriptions = new ConcurrentDictionary<Geolocator, byte>();
+
+		private readonly Criteria _locationCriteria = new() { HorizontalAccuracy = Accuracy.Medium };
 
 		private LocationManager _locationManager;
 		private string _locationProvider;
 
-		// only locations not older than this const can be used as current
-		// (used only in GetGeopositionAsync with parameters, parameterless call can return older location)
-		private const int MAX_SECONDS_AGE = 60;
+		private double _movementThreshold = 0;
+		private uint _reportInterval = 1000;
 
-		public Geolocator()
+		static bool _locChanged = false;
+		static Location _location;
+
+		partial void PlatformDestruct()
 		{
+			RemoveUpdates();
+			_locationManager?.Dispose();
+		}
+
+		/// <summary>
+		/// The distance of movement, in meters, relative to the coordinate from the last PositionChanged event,
+		/// that is required for the Geolocator to raise a PositionChanged event. The default value is 0.
+		/// </summary>
+		public double MovementThreshold
+		{
+			get => _movementThreshold;
+			set
+			{
+				_movementThreshold = value;
+				RestartUpdates();
+			}
+		}
+
+		/// <summary>
+		/// The requested minimum time interval between location updates, in milliseconds.
+		/// If your application requires updates infrequently, set this value so that
+		/// location services can conserve power by calculating location only when needed.
+		/// The default value is 1000.
+		/// </summary>
+		public uint ReportInterval
+		{
+			get => _reportInterval;
+			set
+			{
+				_reportInterval = value;
+				RestartUpdates();
+			}
+		}
+
+		/// <summary>
+		/// Requests permission to access location data.
+		/// </summary>
+		/// <returns>A GeolocationAccessStatus that indicates if permission to location data has been granted.</returns>
+		public static async Task<GeolocationAccessStatus> RequestAccessAsync()
+		{
+			var status = GeolocationAccessStatus.Allowed;
+
+			if (!await PermissionsHelper.CheckFineLocationPermission(CancellationToken.None))
+			{
+				status = await PermissionsHelper.TryGetFineLocationPermission(CancellationToken.None)
+					? GeolocationAccessStatus.Allowed
+					: GeolocationAccessStatus.Denied;
+
+				BroadcastStatusChanged(PositionStatus.Initializing);
+				if (status == GeolocationAccessStatus.Allowed)
+				{
+					BroadcastStatusChanged(PositionStatus.Ready);
+
+					// If geolocators subscribed to PositionChanged before the location permission was granted,
+					// make sure to initialize these geolocators now so they can start broadcasting.
+					foreach (var subscriber in _positionChangedSubscriptions)
+					{
+						subscriber.Key.TryInitialize();
+					}
+				}
+				else
+				{
+					BroadcastStatusChanged(PositionStatus.Disabled);
+
+					foreach (var subscriber in _positionChangedSubscriptions)
+					{
+						subscriber.Key.WaitForPermissionFromBackground();
+					}
+				}
+			}
+
+			return status;
+		}
+
+		/// <summary>
+		/// Starts an asynchronous operation to retrieve the current location of the device.
+		/// </summary>
+		/// <returns>An asynchronous operation that, upon completion, returns a Geoposition marking the found location.</returns>
+		public Task<Geoposition> GetGeopositionAsync()
+		{
+			if (CoreDispatcher.Main.HasThreadAccess)
+			{
+				TryInitialize();
+
+				BroadcastStatusChanged(PositionStatus.Initializing);
+				var location = _locationManager.GetLastKnownLocation(_locationProvider);
+				BroadcastStatusChanged(PositionStatus.Ready);
+				return Task.FromResult(location.ToGeoPosition());
+			}
+			else
+			{
+				return CoreDispatcher.Main.RunWithResultAsync(
+					priority: CoreDispatcherPriority.Normal,
+					task: () => GetGeopositionAsync()
+				);
+			}
+		}
+
+		/// <summary>
+		/// Starts an asynchronous operation to retrieve the current location of the device.
+		/// </summary>
+		/// <param name="maximumAge">The maximum acceptable age of cached location data. A TimeSpan is a time period expressed in 100-nanosecond units.</param>
+		/// <param name="timeout">The timeout. A TimeSpan is a time period expressed in 100-nanosecond units.</param>
+		/// <returns>An asynchronous operation that, upon completion, returns a Geoposition marking the found location.</returns>
+		/// <exception cref="TimeoutException">Thrown when the request times out.</exception>
+		public async Task<Geoposition> GetGeopositionAsync(TimeSpan maximumAge, TimeSpan timeout)
+		{
+			_locationManager = (LocationManager)Android.App.Application.Context.GetSystemService(Android.Content.Context.LocationService);
+
+			_reportInterval = 1000;
+			_movementThreshold = 0;
+
+			RequestUpdates();
+
+			BroadcastStatusChanged(PositionStatus.Initializing);
+
+			Location bestLocation = TryGetCachedGeoposition(maximumAge);
+			if (bestLocation != null)
+			{
+				RemoveUpdates();
+				BroadcastStatusChanged(PositionStatus.Ready);
+				return bestLocation.ToGeoPosition();
+			}
+
+			// wait for fix
+			if (await TryWaitForGetGeopositionAsync(timeout))
+			{
+				// success
+				RemoveUpdates();
+				BroadcastStatusChanged(PositionStatus.Ready);
+				return _location.ToGeoPosition();
+			}
+
+			// timeout
+			BroadcastStatusChanged(PositionStatus.Disabled);
+			RemoveUpdates();
+			throw new TimeoutException("Timeout in GetGeopositionAsync(TimeSpan,TimeSpan)");
 		}
 
 		private void TryInitialize()
@@ -50,64 +188,7 @@ namespace Windows.Devices.Geolocation
 			}
 		}
 
-		public Task<Geoposition> GetGeopositionAsync()
-		{
-			if (CoreDispatcher.Main.HasThreadAccess)
-			{
-				TryInitialize();
-
-				BroadcastStatus(PositionStatus.Initializing);
-				var location = _locationManager.GetLastKnownLocation(_locationProvider);
-				BroadcastStatus(PositionStatus.Ready);
-				return Task.FromResult(location.ToGeoPosition());
-			}
-			else
-			{
-				return CoreDispatcher.Main.RunWithResultAsync(
-					priority: CoreDispatcherPriority.Normal,
-					task: () => GetGeopositionAsync()
-				);
-			}
-		}
-
-
-		public static async Task<GeolocationAccessStatus> RequestAccessAsync()
-		{
-			var status = GeolocationAccessStatus.Allowed;
-
-			if (!await PermissionsHelper.CheckFineLocationPermission(CancellationToken.None))
-			{
-				status = await PermissionsHelper.TryGetFineLocationPermission(CancellationToken.None)
-					? GeolocationAccessStatus.Allowed
-					: GeolocationAccessStatus.Denied;
-
-				BroadcastStatus(PositionStatus.Initializing);
-				if (status == GeolocationAccessStatus.Allowed)
-				{
-					BroadcastStatus(PositionStatus.Ready);
-
-					// If geolocators subscribed to PositionChanged before the location permission was granted,
-					// make sure to initialize these geolocators now so they can start broadcasting.
-					foreach (var subscriber in _positionChangedSubscriptions)
-					{
-						subscriber.Key.TryInitialize();
-					}
-				}
-				else
-				{
-					BroadcastStatus(PositionStatus.Disabled);
-
-					foreach (var subscriber in _positionChangedSubscriptions)
-					{
-						subscriber.Key.WaitForPermissionFromBackground();
-					}
-				}
-			}
-
-			return status;
-		}
-
-		public void WaitForPermissionFromBackground()
+		internal void WaitForPermissionFromBackground()
 		{
 			CoreApplication.Resuming -= CoreApplication_Resuming;
 			CoreApplication.Resuming += CoreApplication_Resuming;
@@ -133,7 +214,7 @@ namespace Windows.Devices.Geolocation
 
 		private LocationManager InitializeLocationProvider(double desiredAccuracy)
 		{
-			var locationManager = (LocationManager)global::Android.App.Application.Context.GetSystemService(Android.Content.Context.LocationService);
+			var locationManager = (LocationManager)Android.App.Application.Context.GetSystemService(Android.Content.Context.LocationService);
 
 			var criteriaForLocationService = new Criteria
 			{
@@ -154,15 +235,9 @@ namespace Windows.Devices.Geolocation
 			return locationManager;
 		}
 
-		partial void StartPositionChanged()
-		{
-			_positionChangedSubscriptions.TryAdd(this, 0);
-		}
+		partial void StartPositionChanged() => _positionChangedSubscriptions.TryAdd(this, 0);
 
-		partial void StopPositionChanged()
-		{
-			_positionChangedSubscriptions.TryRemove(this, out var _);
-		}
+		partial void StopPositionChanged() => _positionChangedSubscriptions.TryRemove(this, out var _);
 
 		private async Task<bool> TryWaitForGetGeopositionAsync(TimeSpan timeout)
 		{
@@ -183,7 +258,6 @@ namespace Windows.Devices.Geolocation
 
 		private Location TryGetCachedGeoposition(TimeSpan maximumAge)
 		{
-
 			var providers = _locationManager.GetProviders(_locationCriteria, true);
 			int bestAccuracy = 10000;
 			Location bestLocation = null;
@@ -222,52 +296,17 @@ namespace Windows.Devices.Geolocation
 			return null;
 		}
 
-		public async Task<Windows.Devices.Geolocation.Geoposition> GetGeopositionAsync(TimeSpan maximumAge, TimeSpan timeout)
+		private void RestartUpdates()
 		{
-
-			_locationManager = (LocationManager)Android.App.Application.Context.GetSystemService(Android.Content.Context.LocationService);
-
-			_reportInterval = 1000;
-			_movementThreshold = 0;
-
+			RemoveUpdates();
 			RequestUpdates();
-
-			BroadcastStatus(PositionStatus.Initializing);
-
-			Location bestLocation = TryGetCachedGeoposition(maximumAge);
-			if (bestLocation != null)
-			{
-				RemoveUpdates();
-				BroadcastStatus(PositionStatus.Ready);
-				return bestLocation.ToGeoPosition();
-			}
-
-			// wait for fix
-			if (await TryWaitForGetGeopositionAsync(timeout))
-			{
-				// success
-				RemoveUpdates();
-				BroadcastStatus(PositionStatus.Ready);
-				return _location.ToGeoPosition();
-			}
-
-			// timeout
-			BroadcastStatus(PositionStatus.Disabled);
-			RemoveUpdates();
-			throw new TimeoutException("Timeout in GetGeopositionAsync(TimeSpan,TimeSpan)");
-
-		}
-
-		~Geolocator()
-		{
-			RemoveUpdates();
-			_locationManager?.Dispose();
 		}
 
 		private void RemoveUpdates()
 		{
 			if (this is null)
-			{ // when caled from destructor
+			{
+				// when caled from destructor
 				return;
 			}
 			_locationManager?.RemoveUpdates(this);
@@ -297,58 +336,30 @@ namespace Windows.Devices.Geolocation
 				_locationCriteria.HorizontalAccuracy = Accuracy.Medium;
 			}
 
-			_locationManager?.RequestLocationUpdates(_reportInterval, (float)_movementThreshold, _locationCriteria, this, Looper.MainLooper);
-		}
+			var providers = _locationManager.GetProviders(_locationCriteria, true);
 
-		private uint _reportInterval = 1000;
-		private double _movementThreshold = 0;
-
-		static bool _locChanged = false;
-		static Location _location;
-
-		private Criteria _locationCriteria = new Android.Locations.Criteria() { HorizontalAccuracy = Accuracy.Medium };
-
-
-		public uint ReportInterval
-		{
-			get => _reportInterval;
-			set
+			foreach (var provider in providers)
 			{
-				_reportInterval = value;
-				RemoveUpdates();
-				RequestUpdates();
-			}
-		}
-
-		public double MovementThreshold
-		{
-			get => _movementThreshold;
-			set
-			{
-				_movementThreshold = value;
-				RemoveUpdates();
-				RequestUpdates();
+				_locationManager?.RequestLocationUpdates(provider, _reportInterval, (float)_movementThreshold, this, Looper.MainLooper);
 			}
 		}
 
 		partial void OnDesiredAccuracyInMetersChanged()
 		{
-			// reset request for updates from Android - with new desired accuracy
-			RemoveUpdates();
-			RequestUpdates();
+			// Reset request for updates from Android - with new desired accuracy
+			RestartUpdates();
 		}
 
-		#region Android ILocationListener
 		public void OnLocationChanged(Location location)
 		{
 			DateTimeOffset date = DateTimeOffset.FromUnixTimeMilliseconds(location.Time);
-			if (date.AddSeconds(MAX_SECONDS_AGE) > DateTimeOffset.UtcNow)
+			if (date.AddSeconds(MaxLocationAgeInSeconds) > DateTimeOffset.UtcNow)
 			{// only from last minute (we don't want to get some obsolete location)
 				_locChanged = true;
 				_location = location;
 
-				BroadcastStatus(PositionStatus.Ready);
-				this._positionChanged?.Invoke(this, new PositionChangedEventArgs(location.ToGeoPosition()));
+				BroadcastStatusChanged(PositionStatus.Ready);
+				_positionChangedWrapper.Event?.Invoke(this, new PositionChangedEventArgs(location.ToGeoPosition()));
 			}
 		}
 
@@ -364,15 +375,13 @@ namespace Windows.Devices.Geolocation
 		{
 			// This method was deprecated in API level 29 (Android 10). This callback will never be invoked.
 		}
-
-		#endregion
 	}
 
 	static class Extensions
 	{
 		private const uint Wgs84SpatialReferenceId = 4326;
 
-		public static Windows.Devices.Geolocation.Geoposition ToGeoPosition(this Location location)
+		public static Geoposition ToGeoPosition(this Location location)
 		{
 			double? geoheading = null;
 			if (location.HasBearing)
@@ -380,28 +389,27 @@ namespace Windows.Devices.Geolocation
 				geoheading = location.Bearing;
 			}
 
-			Windows.Devices.Geolocation.PositionSource posSource;
+			PositionSource posSource;
 			switch (location.Provider)
 			{
-				case Android.Locations.LocationManager.NetworkProvider:
-					posSource = Windows.Devices.Geolocation.PositionSource.Cellular;    // cell, wifi
+				case LocationManager.NetworkProvider:
+					posSource = PositionSource.Cellular;    // cell, wifi
 					break;
-				case Android.Locations.LocationManager.PassiveProvider:
-					posSource = Windows.Devices.Geolocation.PositionSource.Unknown;  // other apps
+				case LocationManager.PassiveProvider:
+					posSource = PositionSource.Unknown;  // other apps
 					break;
-				case Android.Locations.LocationManager.GpsProvider:
-					posSource = Windows.Devices.Geolocation.PositionSource.Satellite;
+				case LocationManager.GpsProvider:
+					posSource = PositionSource.Satellite;
 					break;
 				default:
 					// ex.: "fused" - all merged, also e.g. Google Play
-					posSource = Windows.Devices.Geolocation.PositionSource.Unknown;
+					posSource = PositionSource.Unknown;
 					break;
 			}
 
-
 			double? locVertAccuracy = null;
 			// VerticalAccuracy is since API 26
-			if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
+			if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
 			{
 				if (location.HasVerticalAccuracy)
 				{
@@ -409,8 +417,7 @@ namespace Windows.Devices.Geolocation
 				}
 			}
 
-
-			return new Windows.Devices.Geolocation.Geoposition(
+			return new Geoposition(
 				new Geocoordinate(
 					latitude: location.Latitude,
 					longitude: location.Longitude,
@@ -434,9 +441,7 @@ namespace Windows.Devices.Geolocation
 					positionSource: posSource
 				)
 			);
-
 		}
-
 	}
 }
 #endif
