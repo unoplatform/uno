1494519d163a376044f5c9f89de81c86b20cdcaa
Andres Pineda <pineda.andres@gmail.com>
2022-01-07 01:20:28 -0500

    chore: Add documentation on feature flags

--- src/Uno.UI/FeatureConfiguration.cs
+++ src/Uno.UI/FeatureConfiguration.cs
@@ -560,6 +560,12 @@ namespace Uno.UI
 		public static class DatePicker
 		{
 #if __IOS__
+			/// <summary>
+			/// Gets or set whether the DatePicker rendered matches the Legacy Style or not.
+			/// </summary>
+			/// <remarks>
+			/// Important: This flag will only have an impact on iOS 14 devices
+			/// </remarks>
 			public static bool UseLegacyStyle { get; set; } = false;
 #endif
 		}
@@ -567,6 +573,12 @@ namespace Uno.UI
 		public static class TimePicker
 		{
 #if __IOS__
+			/// <summary>
+			/// Gets or set whether the TimePicker rendered matches the Legacy Style or not.
+			/// </summary>
+			/// <remarks>
+			/// Important: This flag will only have an impact on iOS 14 devices
+			/// </remarks>
 			public static bool UseLegacyStyle { get; set; } = false;
 #endif
 		}
