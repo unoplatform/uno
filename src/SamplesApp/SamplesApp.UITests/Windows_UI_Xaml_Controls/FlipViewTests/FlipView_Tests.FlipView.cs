ada63f308a193a9f99c14995c6051347346faeb6
xiaoy312 <xiaoyao312@gmail.com>
2022-01-11 16:03:35 -0500

    test: update wasm flipview tests

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/FlipViewTests/FlipView_Tests.FlipView.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/FlipViewTests/FlipView_Tests.FlipView.cs
@@ -14,53 +14,66 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.FlipViewTests
     [TestFixture]
     public partial class FlipView_Tests : SampleControlUITestBase
     {
+		// Green, Blue, Red, Fuchsia, Orange
+		const string ButtonColors = "#008000,#0000FF,#FF0000,#FF00FF,#FFA500";
+
 		[Test]
 		[AutoRetry]
 		[ActivePlatforms(Platform.Browser)]
 		public void FlipView_WithButtons_FlipForward()
 		{
-			Run("UITests.Windows_UI_Xaml_Controls.FlipView.FlipView_Buttons");
-
-			_app.WaitForElement("NextButtonHorizontal");
-
-			_app.WaitForElement("Button1");
-
-			_app.WaitForElement("NextButtonHorizontal");
-
-			// FlipView buttons are Collapsed by default.
-			var nextButton = _app.Marked("NextButtonHorizontal");			
-			nextButton.SetDependencyPropertyValue("Visibility", "Visible");
-
-			_app.FastTap("NextButtonHorizontal");
-
-			_app.WaitForElement("Button2");
+			FlipView_WithButtons_BtnNavTest(1, true);
 		}
 
 		[Test]
-		[AutoRetry]
+		[AutoRetry(tryCount: 1)]
 		[ActivePlatforms(Platform.Browser)]
 		public void FlipView_WithButtons_FlipBackward()
 		{
+			FlipView_WithButtons_BtnNavTest(0, true, false);
+		}
+
+		private void FlipView_WithButtons_BtnNavTest(int expectedIndex, params bool[] forwardNavigations)
+		{
 			Run("UITests.Windows_UI_Xaml_Controls.FlipView.FlipView_Buttons");
 
-			_app.WaitForElement("NextButtonHorizontal");
-
-			// FlipView buttons are Collapsed by default.
+			var flipview = _app.Marked("SUT");
 			var nextButton = _app.Marked("NextButtonHorizontal");
-			nextButton.SetDependencyPropertyValue("Visibility", "Visible");
+			var previousButton = _app.Marked("PreviousButtonHorizontal");
 
-			_app.FastTap("NextButtonHorizontal");
+			// using tap to ensure navigation buttons are visible, since we cant mouse-over
+			_app.WaitForElement(flipview);
+			_app.FastTap(flipview);
 
-			_app.WaitForElement("Button2");
+			// perform navigations
+			foreach (var navigation in forwardNavigations.Select((x, i) => new { Step = i, Forward = x }))
+			{
+				string GetCurrentNavigationContext() =>
+					$"step#{navigation.Step} of" + new string(forwardNavigations
+						.Select(x => x ? 'N' : 'P').ToArray())
+						.Insert(navigation.Step, "*");
 
-			_app.WaitForElement("PreviousButtonHorizontal");
+				// tap previous or next
+				var targetButton = navigation.Forward ? nextButton : previousButton;
+				_app.WaitForElement(targetButton, $"Timeout waiting for the {(navigation.Forward ? "next" : "previous")} button in {GetCurrentNavigationContext()}");
+				_app.FastTap(targetButton);
+			}
 
-			var backButton = _app.Marked("PreviousButtonHorizontal");
-			backButton.SetDependencyPropertyValue("Visibility", "Visible");
-			
-			_app.FastTap("PreviousButtonHorizontal");
+			var flipviewRect = _app.GetPhysicalRect(flipview);
+			var navigationContext = new string(forwardNavigations.Select(x => x ? 'N' : 'P').ToArray());
+			var previousButtonRect = _app.GetPhysicalRect(previousButton);
 
-			_app.WaitForElement("Button1");
+			// check for selection index && content
+			Assert.AreEqual(expectedIndex, flipview.GetDependencyPropertyValue<int>("SelectedIndex"));
+			using (var screenshot = TakeScreenshot($"Post_{navigationContext}_Navigation", ignoreInSnapshotCompare: true))
+			{
+				ImageAssert.HasColorAt(
+					screenshot,
+					flipviewRect.X + previousButtonRect.Width + 2,
+					flipviewRect.CenterY,
+					ButtonColors.Split(',').ElementAtOrDefault(expectedIndex) ?? throw new IndexOutOfRangeException($"{nameof(expectedIndex)} is out of range")
+				);
+			}
 		}
 
 		[Test]
