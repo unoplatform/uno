2265fd5d45895b788c19405ce6786f9041163ebd
Carl de Billy <carl.debilly@nventive.com>
2022-01-12 14:38:50 -0500

    test: Illustrate the problem using a unit test
    This should illustrate the problem defined in https://github.com/unoplatform/uno/issues/1592

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Controls/ContentDialog_Leak.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml.Controls
+{
+	public partial class ContentDialog_Leak : ContentDialog
+	{
+		public ContentDialog_Leak()
+		{
+			InitializeComponent();
+		}
+	}
+}
