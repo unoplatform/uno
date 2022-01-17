3927436e7303fb461f1ed37874030c1afcdb064f
David <github.site@torick.net>
2022-01-17 16:24:37 -0500

    test(pointers): Run NestedHandling UI tests on Skia

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Input/NestedHandling_Tests.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Input/NestedHandling_Tests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Linq;
+using System.Threading.Tasks;
+using Windows.Devices.Input;
 using FluentAssertions;
 using NUnit.Framework;
-using SamplesApp.UITests.Extensions;
 using SamplesApp.UITests.TestFramework;
+using Uno.Testing;
 using Uno.UITest.Helpers;
 using Uno.UITest.Helpers.Queries;
 
@@ -16,30 +18,34 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Input
 
 		[Test]
 		[AutoRetry]
-		public void When_NestedHandlesPressed_Then_ContainerStillGetsSubsequentEvents()
+		[InjectedPointer(PointerDeviceType.Touch)]
+		public async Task When_NestedHandlesPressed_Then_ContainerStillGetsSubsequentEvents()
 		{
-			Run(_sample);
+			await RunAsync(_sample);
 
-			var target = _app.WaitForElement("_nested").Single().Rect;
-			_app.DragCoordinates(target.X + 10, target.Y + 10, target.Right - 10, target.Bottom - 10);
+			var target = App.WaitForElement("_nested").Single().Rect;
+			App.DragCoordinates(target.X + 10, target.Y + 10, target.Right - 10, target.Bottom - 10);
 
-			var result = _app.Marked("_result").GetDependencyPropertyValue("Text");
+			var result = App.Marked("_result").GetDependencyPropertyValue("Text");
 			result.Should().Be("Pressed SUCCESS | Released SUCCESS");
 		}
 
 		[Test]
 		[AutoRetry]
-		public void When_Nested_Then_EnterAndExitedDoesNotBubble()
+		[InjectedPointer(PointerDeviceType.Touch)]
+		public async Task When_Nested_Then_EnterAndExitedDoesNotBubble()
 		{
-			Run(_sample);
+			await RunAsync(_sample);
 
-			_app.FastTap("_nested");
-			_app.FastTap("_exitResult"); // Forces the pointer to move somewhere else on WASM to get the exit
+			var container = App.WaitForElement("_container").Single().Rect;
+			var nested = App.WaitForElement("_nested").Single().Rect;
 
-			var enterResult = _app.Marked("_enterResult").GetDependencyPropertyValue("Text");
+			App.DragCoordinates(container.X + 10, container.CenterY, container.Right - 10, nested.CenterY);
+
+			var enterResult = App.Marked("_enterResult").GetDependencyPropertyValue("Text");
 			enterResult.Should().Be("SUCCESS");
 
-			var exitResult = _app.Marked("_exitResult").GetDependencyPropertyValue("Text");
+			var exitResult = App.Marked("_exitResult").GetDependencyPropertyValue("Text");
 			exitResult.Should().Be("SUCCESS");
 		}
 	}
