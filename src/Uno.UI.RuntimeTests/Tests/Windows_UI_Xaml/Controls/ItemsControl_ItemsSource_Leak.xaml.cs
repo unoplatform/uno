0071affd4ea2d2348c86709714f9a2125d132c6e
Jerome Laban <jerome.laban@nventive.com>
2022-01-21 19:51:46 -0500

    chore: Adjust uno-only ObservableVector

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Controls/ItemsControl_ItemsSource_Leak.xaml.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Controls/ItemsControl_ItemsSource_Leak.xaml.cs
@@ -10,8 +10,11 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml.Controls
 	public partial class ItemsControl_ItemsSource_Leak : UserControl, IExtendedLeakTest
 	{
 		private static ObservableCollection<string> _staticObservableCollection = new();
-		private static ObservableVector<string> _staticObservableVector = new();
 		private static CollectionViewSource _staticCollectionViewSource = new();
+
+#if HAS_UNO
+		private static ObservableVector<string> _staticObservableVector = new();
+#endif
 
 		public ItemsControl_ItemsSource_Leak()
 		{
@@ -21,8 +24,11 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml.Controls
 		public async Task WaitForTestToComplete()
 		{
 			control1.ItemsSource = _staticObservableCollection;
-			control2.ItemsSource = _staticObservableVector;
 			control3.ItemsSource = _staticCollectionViewSource;
+
+#if HAS_UNO
+			control2.ItemsSource = _staticObservableVector;
+#endif
 		}
 	}
 }
