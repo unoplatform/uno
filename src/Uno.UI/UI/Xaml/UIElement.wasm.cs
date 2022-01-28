74ae034a1661026f0e35d7616d89a45df59ae887
Carl de Billy <carl.debilly@nventive.com>
2022-01-28 16:17:30 -0500

    ci: Applied PR reviews

--- src/Uno.UI/UI/Xaml/UIElement.wasm.cs
+++ src/Uno.UI/UI/Xaml/UIElement.wasm.cs
@@ -426,7 +426,7 @@ namespace Windows.UI.Xaml
 				UpdateDOMProperties();
 			}
 
-			if (FeatureConfiguration.UIElement.UseInvalidateMeasurePath && __Store.Parent is UIElement parent)
+			if (FeatureConfiguration.UIElement.UseInvalidateMeasurePath && this.GetParent() is UIElement parent)
 			{
 				// Need to invalidate the parent when the visibility changes to ensure its
 				// algorithm is doing its layout properly.
