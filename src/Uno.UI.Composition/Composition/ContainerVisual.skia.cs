f4021236e1fef16aabd9890962e538d162d7ddb6
Martin Zikmund <martin@zikmund.dev>
2022-02-01 14:29:28 +0100

    chore: Address comment

--- src/Uno.UI.Composition/Composition/ContainerVisual.skia.cs
+++ src/Uno.UI.Composition/Composition/ContainerVisual.skia.cs
@@ -7,7 +7,7 @@ namespace Windows.UI.Composition;
 
 public partial class ContainerVisual : Visual
 {
-	private List<Visual> _childrenInRenderOrder = new List<Visual>();
+	private List<Visual>? _childrenInRenderOrder;
 	private bool _hasCustomRenderOrder = false;
 
 	internal bool IsChildrenRenderOrderDirty { get; set; }
@@ -19,15 +19,16 @@ public partial class ContainerVisual : Visual
 			ResetRenderOrder();
 		}
 
-		return !_hasCustomRenderOrder ? Children.InnerList : _childrenInRenderOrder;
+		return !_hasCustomRenderOrder ? Children.InnerList : _childrenInRenderOrder!;
 	}
 
 	internal void ResetRenderOrder()
 	{
-		_childrenInRenderOrder.Clear();
+		_childrenInRenderOrder?.Clear();
 		_hasCustomRenderOrder = false;
 		if (Children.Any(c => c.ZIndex != 0))
 		{
+			_childrenInRenderOrder ??= new List<Visual>();
 			// We need to sort children in ZIndex order
 			foreach (var child in Children.OrderBy(c => c.ZIndex))
 			{
