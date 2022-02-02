14327a97872f67f895873b5e44af1bdd382626eb
Jerome Laban <jerome.laban@nventive.com>
2022-02-02 11:27:23 -0500

    test: Disable TextBox_No_Text_Entered for focus issue

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/TextBoxTests/TextBoxTests.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/TextBoxTests/TextBoxTests.cs
@@ -324,6 +324,7 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.TextBoxTests
 		[Test]
 		[AutoRetry]
 		[Timeout(400000)] // Increased iOS timeout for Xamarin.UITest 3.2
+		[Ignore("Failed to find [Pointer] on [FocusState] in last DefocusTextBox invocation")]
 		public void TextBox_No_Text_Entered()
 		{
 			Run("UITests.Shared.Windows_UI_Xaml_Controls.TextBoxControl.TextBox_Binding_Null");
