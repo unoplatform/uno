247bb78077c5b8ef7ebf56a55cec6fe31a83a228
David Oliver <david.oliver@nventive.com>
2022-02-02 05:57:03 -0500

    chore: Add capacity to override VisibleBounds for testing
    Allows visible bounds-related comportment to be tested on the CI.

--- src/Uno.UI.RuntimeTests/Helpers/RectAssert.cs
+++ src/Uno.UI.RuntimeTests/Helpers/RectAssert.cs
@@ -15,5 +15,21 @@ namespace Uno.UI.RuntimeTests.Helpers
 			Assert.AreEqual(expected.Width, actual.Width, delta);
 			Assert.AreEqual(expected.Height, actual.Height, delta);
 		}
+
+		public static void AreNotEqual(Rect rect1, Rect rect2)
+		{
+			if (rect1.Left == rect2.Left && rect1.Top == rect2.Top && rect1.Width == rect2.Width && rect1.Height == rect2.Height)
+			{
+				throw new AssertFailedException("Expected unequal rects, but they were equal.");
+			}
+		}
+
+		public static void Contains(Rect outer, Rect inner)
+		{
+			if (inner.Left < outer.Left || inner.Top < outer.Top || inner.Right > outer.Right || inner.Bottom > outer.Bottom)
+			{
+				throw new AssertFailedException($"Rect {inner} does not lie fully inside rect {outer}");
+			}
+		}
 	}
 }
