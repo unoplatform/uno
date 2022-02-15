b3c090bd4e6e436ab7e63c180e124795eb1e8dea
xiaoy312 <xiaoyao312@gmail.com>
2022-02-15 10:49:29 -0500

    chore: add manual test for NVI visual-state

---
+++ src/SamplesApp/UITests.Shared/Helpers/VisualTreeHelperEx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace UITests.Helpers
+{
+	/// <summary>
+	/// Contains various methods to debug the visual tree
+	/// </summary>
+	internal static class VisualTreeHelperEx
+	{
+		/// <summary>
+		/// Prints the visual tree starting from the <paramref name="reference"/> node.
+		/// </summary>
+		/// <param name="reference"></param>
+		/// <returns></returns>
+		public static string TreeGraph(this DependencyObject reference) => TreeGraph(reference, DebugVTNode);
+
+		/// <summary>
+		/// Prints the visual tree starting from the <paramref name="reference"/> node with custom way to <paramref name="describe"/> each node.
+		/// </summary>
+		/// <param name="reference"></param>
+		/// <param name="describe"></param>
+		/// <returns></returns>
+		public static string TreeGraph(this DependencyObject reference, Func<DependencyObject, string> describe)
+		{
+			var buffer = new StringBuilder();
+			Walk(reference);
+			return buffer.ToString();
+
+			void Walk(DependencyObject o, int depth = 0)
+			{
+				Print(o, depth);
+				foreach (var child in GetChildren(o))
+				{
+					Walk(child, depth + 1);
+				}
+			}
+			void Print(DependencyObject o, int depth)
+			{
+				buffer
+					.Append(new string(' ', depth * 4))
+					.Append(describe(o))
+					.AppendLine();
+			}
+		}
+
+		private static string DebugVTNode(DependencyObject x)
+		{
+			var xname = (x as FrameworkElement)?.Name;
+
+			return new StringBuilder()
+				.Append(x.GetType().Name)
+				.Append(xname != null ? $"#{xname}" : string.Empty)
+				.ToString();
+		}
+
+		public static T GetFirstAncestor<T>(this DependencyObject reference) => GetAncestors(reference)
+			.OfType<T>()
+			.FirstOrDefault();
+
+		public static T GetFirstAncestor<T>(this DependencyObject reference, Func<T, bool> predicate) => GetAncestors(reference)
+			.OfType<T>()
+			.FirstOrDefault(predicate);
+
+		public static T GetFirstDescendant<T>(DependencyObject reference) => GetDescendants(reference)
+			.OfType<T>()
+			.FirstOrDefault();
+
+		public static T GetFirstDescendant<T>(DependencyObject reference, Func<T, bool> predicate) => GetDescendants(reference)
+			.OfType<T>()
+			.FirstOrDefault(predicate);
+
+		public static IEnumerable<DependencyObject> GetAncestors(this DependencyObject o)
+		{
+			if (o is null) yield break;
+			while (VisualTreeHelper.GetParent(o) is DependencyObject parent)
+			{
+				yield return o = parent;
+			}
+		}
+
+		public static IEnumerable<DependencyObject> GetDescendants(DependencyObject reference)
+		{
+			foreach (var child in GetChildren(reference))
+			{
+				yield return child;
+				foreach (var grandchild in GetDescendants(child))
+				{
+					yield return grandchild;
+				}
+			}
+		}
+
+		public static IEnumerable<DependencyObject> GetChildren(DependencyObject reference)
+		{
+			return Enumerable
+				.Range(0, VisualTreeHelper.GetChildrenCount(reference))
+				.Select(x => VisualTreeHelper.GetChild(reference, x));
+		}
+	}
+}
---
+++ src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/NavigationViewTests/NavViewItemVisualState.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UITests.Helpers;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MUXC = Microsoft.UI.Xaml.Controls;
+using MUXCP = Microsoft.UI.Xaml.Controls.Primitives;
+
+namespace UITests.Microsoft_UI_Xaml_Controls.NavigationViewTests
+{
+	[SampleControlInfo("NavigationView", nameof(NavViewItemVisualState), Description = Description, IsManualTest = true, IgnoreInSnapshotTests = true)]
+	public sealed partial class NavViewItemVisualState : Page
+	{
+		private const string Description =
+			"[iOS]ManualTest: open the nav-view, and then expand 'asd'. Press and hold 'asd_4'." +
+			"While still holding, slowly drag vertically. When the scrollviewer moved, " +
+			"the visual-state should reset to 'Normal', instead of being stuck in 'PointerOver'.";
+
+		private bool _initialized = false;
+
+		public NavViewItemVisualState()
+		{
+			this.InitializeComponent();
+			InitializeNVItems();
+		}
+
+		private void InitializeNVItems()
+		{
+			// add a few items and some sub-items for each
+			foreach (var header in "qwe,asd,zxc".Split(','))
+			{
+				var item = new MUXC.NavigationViewItem { Content = new TextBlock { Text = header } };
+				InstallDebugHook(item);
+
+				for (int i = 0; i < 20; i++)
+				{
+					var subitem = new MUXC.NavigationViewItem { Content = new TextBlock { Text = $"{header}_{i}" } };
+					InstallDebugHook(subitem);
+
+					item.MenuItems.Add(subitem);
+				}
+				NavigationViewControl.MenuItems.Add(item);
+			}
+
+			void InstallDebugHook(MUXC.NavigationViewItem item)
+			{
+				var tb = item.Content as TextBlock;
+				tb.Tag = tb.Text;
+
+				tb.Loaded += (s, e) =>
+				{
+					// for tier-1 nvi, the textbox is loaded immediately
+					// for tier-2 nvi, the textbox is loaded twice and the 2nd time is when it is ready
+					var nvip = VisualTreeHelperEx.GetFirstDescendant<MUXCP.NavigationViewItemPresenter>(item, x => x.Name == "NavigationViewItemPresenter");
+					var root = nvip == null ? default : VisualTreeHelperEx.GetFirstDescendant<Grid>(nvip, x => x.Name == "LayoutRoot");
+
+					// check if the template is ready
+					if (root != null)
+					{
+						var group = VisualStateManager.GetVisualStateGroups(root).FirstOrDefault(x => x.Name == "PointerStates");
+						group.CurrentStateChanged += (s2, e2) =>
+							tb.Text = $"{tb.Tag}: {e2.NewState?.Name}";
+						tb.Text = $"{tb.Tag}: {group.CurrentState?.Name}";
+					}
+				};
+			}
+		}
+	}
+}
