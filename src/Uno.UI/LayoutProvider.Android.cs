607992d31de85ed9c4194e5d4410b17a5109d0a9
David Oliver <david.oliver@nventive.com>
2022-02-15 06:34:45 -0500

    fix(keyboard): [Android] Improve keyboard size info on 30+
    Use newer APIs for calculating keyboard size on Android 30 and above. This fixes some of the unreliability of keyboard handling, at least for newer devices.

--- src/Uno.UI/LayoutProvider.Android.cs
+++ src/Uno.UI/LayoutProvider.Android.cs
@@ -7,6 +7,7 @@ using Android.Graphics.Drawables;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using AndroidX.Core.View;
 using Windows.Devices.Sensors;
 using Windows.Foundation;
 using Windows.Graphics.Display;
@@ -25,7 +26,6 @@ namespace Uno.UI
 		public event InsetsChangedListener InsetsChanged;
 
 		public Thickness Insets { get; internal set; } = new Thickness(0, 0, 0, 0);
-		public Rect KeyboardRect { get; private set; } = new Rect(0, 0, 0, 0);
 
 		/// <summary>
 		/// // Used by legacy visual bounds calculation on devices below API 30. Will always be default value on devices running API 30 and above.
@@ -72,48 +72,63 @@ namespace Uno.UI
 
 		private void MeasureLayout(PopupWindow sender)
 		{
+			var osVersion = Android.OS.Build.VERSION.SdkInt;
+			if (osVersion >= Android.OS.BuildVersionCodes.R)
+			{
+				// Use newer API on 30 and above
+				var windowMetrics = _activity.WindowManager.CurrentWindowMetrics;
+				var imeInsets = windowMetrics.WindowInsets.GetInsets(WindowInsets.Type.Ime());
+				var windowBottom = windowMetrics.Bounds.Bottom;
+				var keyboardHeight = windowBottom - imeInsets.Bottom;
+				var keyboardRect = keyboardHeight > 0 ? new Rect(0, keyboardHeight, windowMetrics.Bounds.Right, windowBottom) : new Rect();
+
+				KeyboardChanged?.Invoke(keyboardRect);
+			}
+			else
+			{
 #pragma warning disable 618
-			// We can obtain the size of keyboard by comparing the layout of two popup windows
-			// where one (AdjustResize) resizes to keyboard and one(AdjustNothing) that doesn't:
-			// [size] realMetrics			: screen 
-			// [size] metrics				: screen - dead zones
-			// [rect] displayRect			: screen - (bottom: nav_bar)
-			// [rect] adjustNothingFrame	: screen - (top: status_bar) - (bottom: nav_bar)
-			// [rect] adjustResizeFrame		: screen - (top: status_bar) - (bottom: keyboard + nav_bar)
-			var realMetrics = Get<DisplayMetrics>(_activity.WindowManager.DefaultDisplay.GetRealMetrics);
-			var metrics = Get<DisplayMetrics>(_activity.WindowManager.DefaultDisplay.GetMetrics);
-			var displayRect = Get<Rect>(_activity.WindowManager.DefaultDisplay.GetRectSize);
-			var adjustNothingFrame = Get<Rect>(_adjustNothingLayoutProvider.ContentView.GetWindowVisibleDisplayFrame);
-			var adjustResizeFrame = Get<Rect>(_adjustResizeLayoutProvider.ContentView.GetWindowVisibleDisplayFrame);
+				// We can obtain the size of keyboard by comparing the layout of two popup windows
+				// where one (AdjustResize) resizes to keyboard and one(AdjustNothing) that doesn't:
+				// [size] realMetrics			: screen 
+				// [size] metrics				: screen - dead zones
+				// [rect] displayRect			: screen - (bottom: nav_bar)
+				// [rect] adjustNothingFrame	: screen - (top: status_bar) - (bottom: nav_bar)
+				// [rect] adjustResizeFrame		: screen - (top: status_bar) - (bottom: keyboard + nav_bar)
+				var realMetrics = Get<DisplayMetrics>(_activity.WindowManager.DefaultDisplay.GetRealMetrics);
+				var metrics = Get<DisplayMetrics>(_activity.WindowManager.DefaultDisplay.GetMetrics);
+				var displayRect = Get<Rect>(_activity.WindowManager.DefaultDisplay.GetRectSize);
+				var adjustNothingFrame = Get<Rect>(_adjustNothingLayoutProvider.ContentView.GetWindowVisibleDisplayFrame);
+				var adjustResizeFrame = Get<Rect>(_adjustResizeLayoutProvider.ContentView.GetWindowVisibleDisplayFrame);
 #pragma warning restore 618
 
-			var orientation = DisplayInformation.GetForCurrentView().CurrentOrientation;
+				var orientation = DisplayInformation.GetForCurrentView().CurrentOrientation;
 
-			KeyboardRect = new Rect(0, adjustResizeFrame.Bottom, realMetrics.WidthPixels, adjustNothingFrame.Bottom);
+				KeyboardRect = new Rect(0, adjustResizeFrame.Bottom, realMetrics.WidthPixels, adjustNothingFrame.Bottom);
 
-			switch (orientation)
-			{
-				case DisplayOrientations.Landscape:
-					NavigationBarRect = new Rect(0, 0, metrics.WidthPixels - displayRect.Width(), metrics.HeightPixels);
-					break;
-				case DisplayOrientations.LandscapeFlipped:
-					NavigationBarRect = new Rect(adjustNothingFrame.Width(), 0, metrics.WidthPixels - displayRect.Width(), metrics.HeightPixels);
-					break;
-				// Miss portrait flipped
-				case DisplayOrientations.Portrait:
-				default:
-					NavigationBarRect = new Rect(0, adjustNothingFrame.Bottom, realMetrics.WidthPixels, realMetrics.HeightPixels);
-					break;
-			}
+				switch (orientation)
+				{
+					case DisplayOrientations.Landscape:
+						NavigationBarRect = new Rect(0, 0, metrics.WidthPixels - displayRect.Width(), metrics.HeightPixels);
+						break;
+					case DisplayOrientations.LandscapeFlipped:
+						NavigationBarRect = new Rect(adjustNothingFrame.Width(), 0, metrics.WidthPixels - displayRect.Width(), metrics.HeightPixels);
+						break;
+					// Miss portrait flipped
+					case DisplayOrientations.Portrait:
+					default:
+						NavigationBarRect = new Rect(0, adjustNothingFrame.Bottom, realMetrics.WidthPixels, realMetrics.HeightPixels);
+						break;
+				}
 
-			KeyboardChanged?.Invoke(KeyboardRect);
+				KeyboardChanged?.Invoke(KeyboardRect);
 
-			T Get<T>(Action<T> getter) where T : new()
-			{
-				var result = new T();
-				getter(result);
+				T Get<T>(Action<T> getter) where T : new()
+				{
+					var result = new T();
+					getter(result);
 
-				return result;
+					return result;
+				}
 			}
 		}
 
