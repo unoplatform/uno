05e0c9319e9658375cf57c7a13702ad368adef5a
David <github.site@torick.net>
2022-02-25 14:09:56 -0500

    test(swipecontrol): Test SwipeControl behavior in ListView

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/SwipeControlTests/SwipeControl_ListView_ItemClick.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.SwipeControlTests
+{
+	[Sample("SwipeControl", "ListView")]
+	public sealed partial class SwipeControl_ListView_ItemClick : Page
+	{
+		public SwipeControl_ListView_ItemClick()
+		{
+			this.InitializeComponent();
+		}
+
+		private void SwipeItemInvoked(SwipeItem sender, SwipeItemInvokedEventArgs args)
+		{
+			LastSwipeInvoked.Text = $"{args.SwipeControl.DataContext}.{sender.Text}";
+		}
+
+		private void ItemClicked(object sender, ItemClickEventArgs e)
+		{
+			LastClicked.Text = e.ClickedItem?.ToString() ?? "--null--";
+		}
+
+		private void ItemSelected(object sender, SelectionChangedEventArgs e)
+		{
+			LastSelected.Text = e.AddedItems.Cast<object>().FirstOrDefault()?.ToString() ?? "--null--";
+		}
+	}
+}
