62001e7df6c152685afeb27470469a39546de323
Martin Zikmund <martin@zikmund.dev>
2022-03-03 11:09:12 +0100

    chore: Remove public constructor from SystemNavigationManagerPreview

--- src/Uno.UWP/UI/Core/Preview/SystemNavigationManagerPreview.cs
+++ src/Uno.UWP/UI/Core/Preview/SystemNavigationManagerPreview.cs
@@ -3,6 +3,9 @@ using System;
 
 namespace Windows.UI.Core.Preview;
 
+/// <summary>
+/// Provides a way for an app to respond to system provided close events.
+/// </summary>
 public partial class SystemNavigationManagerPreview
 {
 	private static readonly SystemNavigationManagerPreview _instance = new();
@@ -11,10 +14,17 @@ public partial class SystemNavigationManagerPreview
 
 	partial void InitializePlatform();
 
+	/// <summary>
+	/// Occurs when the user invokes the system button for close (the 'x' button in the corner of the app's title bar).
+	/// </summary>
 	public event EventHandler<SystemNavigationCloseRequestedPreviewEventArgs> CloseRequested;
 
 	internal bool HasConfirmedClose { get; private set; }
 
+	/// <summary>
+	/// Returns the SystemNavigationManagerPreview object associated with the current window.
+	/// </summary>
+	/// <returns>The SystemNavigationManagerPreview object associated with the current window.</returns>
 	public static SystemNavigationManagerPreview GetForCurrentView() => _instance;
 
 	internal bool RequestAppClose()
