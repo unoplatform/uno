d4bb2c1643a796f40b1a9f78931470a9b401e331
pkar <pkar@ieee.org>
2022-04-11 21:08:49 +0200

    chore: internal ctor to keep compiler happy

--- src/Uno.UWP/Devices/Bluetooth/Advertisement/BluetoothLEAdvertisement.cs
+++ src/Uno.UWP/Devices/Bluetooth/Advertisement/BluetoothLEAdvertisement.cs
@@ -12,6 +12,14 @@ namespace Windows.Devices.Bluetooth.Advertisement
 		public  IList<BluetoothLEAdvertisementDataSection> DataSections { get; internal set; }
 		public  IList<BluetoothLEManufacturerData> ManufacturerData { get; internal set; }
 		public IList<Guid> ServiceUuids { get; internal set; }
+
+		internal BluetoothLEAdvertisement(string localName, IList<BluetoothLEAdvertisementDataSection> dataSections, IList<BluetoothLEManufacturerData> manufacturerData, IList<Guid> serviceUuids)
+		{
+			LocalName = localName;
+			DataSections = dataSections;
+			ManufacturerData = manufacturerData;
+			ServiceUuids = serviceUuids;
+		}
 	}
 
 }
