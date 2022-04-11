eb6db9dacf0797e6b948b83c84119c346ded251f
Martin Zikmund <martin@zikmund.dev>
2022-04-11 20:36:38 +0200

    test: Flyout.ShowAt(Window.Content) sample

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Flyout/Flyout_ShowAt_Window_Content.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UITests.Windows_UI_Xaml_Controls.Flyout
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Sample]
+	public sealed partial class Flyout_ShowAt_Window_Content : Page
+	{
+		public Flyout_ShowAt_Window_Content()
+		{
+			this.InitializeComponent();
+
+			Button button = new Button
+			{
+				Content = "Show at Window",
+				Tag = Window.Current.Content,
+			};
+			Button button2 = new Button
+			{
+				Content = "Show at this button",				
+			};
+			button2.Tag = button2;
+
+			var stackPanel = new StackPanel();
+			stackPanel.Margin = new Thickness(60);
+			stackPanel.Children.Add(button);
+			stackPanel.Children.Add(button2);
+			button.Click += Button_Click;
+			button2.Click += Button_Click;
+
+			Content = stackPanel;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Windows.UI.Xaml.Controls.Flyout flyout = new Windows.UI.Xaml.Controls.Flyout();
+			flyout.Content = new TextBlock { Text = "Hi there" };
+
+			flyout.ShowAt(((Button)sender).Tag as FrameworkElement);
+		}
+	}
+}
