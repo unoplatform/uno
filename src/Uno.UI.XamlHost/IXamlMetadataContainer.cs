deead113b65ded2d92712cc918834b875d6d542b
Martin Zikmund <martin@zikmund.dev>
2022-04-12 18:03:33 +0200

    feat: Uno.UI.XamlHost

---
+++ src/Uno.UI.XamlHost/IXamlMetadataContainer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Markup;
+
+namespace Uno.UI.XamlHost;
+
+public interface IXamlMetadataContainer
+{
+	IList<IXamlMetadataProvider> MetadataProviders { get; }
+}
---
+++ src/Uno.UI.XamlHost/XamlApplication.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Markup;
+
+namespace Uno.UI.XamlHost;
+
+public class XamlApplication : Application, IXamlMetadataProvider, IXamlMetadataContainer, IDisposable
+{
+	public XamlApplication()
+	{
+	}
+	
+	public XamlApplication(List<IXamlMetadataProvider> providers)
+	{
+		MetadataProviders = providers;
+	}
+
+	public IList<IXamlMetadataProvider> MetadataProviders { get; }
+
+	public IXamlType GetXamlType(Type type) => throw new NotImplementedException();
+	public IXamlType GetXamlType(string fullName) => throw new NotImplementedException();
+	public XmlnsDefinition[] GetXmlnsDefinitions() => throw new NotImplementedException();
+	public void Dispose() => IsDisposed = true;
+
+	public bool IsDisposed { get; private set; }
+}
