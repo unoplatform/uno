a2aef585b6cb84e67fe33257da6a70d43099c77c
Martin Zikmund <martin@zikmund.dev>
2022-04-12 11:40:11 +0200

    fix: Improve IsApiContractPresent accuracy

--- src/Uno.Foundation/Metadata/ApiInformation.shared.cs
+++ src/Uno.Foundation/Metadata/ApiInformation.shared.cs
@@ -1,33 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Windows.Foundation.Metadata;
 
-namespace Windows.Foundation.Metadata
+public partial class ApiInformation
 {
-	public partial class ApiInformation
+	public static bool IsApiContractPresent(string contractName, ushort majorVersion)
+		=> IsApiContractPresent(contractName, majorVersion, 0);
+
+	public static bool IsApiContractPresent(string contractName, ushort majorVersion, ushort minorVersion)
 	{
-		public static bool IsApiContractPresent(string contractName, ushort majorVersion)
-			=> IsApiContractPresent(contractName, majorVersion, 0);
-
-		public static bool IsApiContractPresent(string contractName, ushort majorVersion, ushort minorVersion)
+		switch (contractName)
 		{
-			switch (contractName)
-			{
-				case "Windows.Foundation.UniversalApiContract":
-					// See C:\Program Files (x86)\Windows Kits\10\References\[version]\Windows.Foundation.UniversalApiContract
-					return majorVersion <= 10; // SDK 10.0.19041.1
+			case "Windows.Foundation.FoundationContract":
+				// See C:\Program Files (x86)\Windows Kits\10\References\[version]\Windows.Foundation.FoundationContract
+				return majorVersion <= 4; // SDK 10.0.22000.0
 
-				case "Uno.WinUI":
+			case "Windows.Foundation.UniversalApiContract":
+				// See C:\Program Files (x86)\Windows Kits\10\References\[version]\Windows.Foundation.UniversalApiContract
+				return majorVersion <= 14; // SDK 10.0.22000.0
+
+			case "Windows.Phone.PhoneContract":
+				// See C:\Program Files (x86)\Windows Kits\10\References\[version]\Windows.Phone.PhoneContract
+				return majorVersion <= 1; // SDK 10.0.22000.0					
+
+			case "Windows.Networking.Connectivity.WwanContract":
+				// See C:\Program Files (x86)\Windows Kits\10\References\[version]\Windows.Networking.Connectivity.WwanContract
+				return majorVersion <= 2; // SDK 10.0.22000.0
+
+			case "Windows.ApplicationModel.Calls.CallsPhoneContract":
+				// See C:\Program Files (x86)\Windows Kits\10\References\[version]\Windows.ApplicationModel.Calls.CallsPhoneContract
+				return majorVersion <= 6; // SDK 10.0.22000.0
+
+			case "Uno.WinUI":
 #if HAS_UNO_WINUI
-					return true;
+				return true;
 #else
-					return false;
+				return false;
 #endif
-				default:
-					return false;
-			}
+			default:
+				return false;
 		}
 	}
 }
