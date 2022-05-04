497bcbe3fc32cf62b0c641eb14932a3c5311f0bb
Giuseppe Lippolis <workgroupengineering@outlook.com>
2022-05-04 17:16:29 +0200

    fix(SourceGenerators): NativeCtorsGenerator will execute after XamlCodeGenerator

--- src/SourceGenerators/Uno.UI.SourceGenerators/NativeCtor/NativeCtorsGenerator.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators/NativeCtor/NativeCtorsGenerator.cs
@@ -15,6 +15,9 @@ using Uno.SourceGeneration;
 namespace Uno.UI.SourceGenerators.NativeCtor
 {
 	[Generator]
+#if NETFRAMEWORK
+	[GenerateAfter("Uno.UI.SourceGenerators.XamlGenerator." + nameof(XamlGenerator.XamlCodeGenerator))]
+#endif
 	public class NativeCtorsGenerator : ISourceGenerator
 	{
 		public void Initialize(GeneratorInitializationContext context)
