2fd14bcf6e0357dabad7d3474ef5df9cac96913b
Luke Blevins <lukeblevins15@outlook.com>
2022-05-10 11:20:42 -0400

    test: Added Expander content occlusion test

---
+++ src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/ExpanderTests/ExpanderColorValidationPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.Microsoft_UI_Xaml_Controls.ExpanderTests
+{
+	[SampleControlInfo("Expander", "WinUI", Name = "ExpanderClippingSample")]
+	public sealed partial class ExpanderColorValidationPage : UserControl
+	{
+		public ExpanderColorValidationPage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
