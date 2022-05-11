5a94463d6faa52ee8b3b00060854c8dabee22c36
Jerome Laban <jerome.laban@nventive.com>
2022-05-11 16:41:36 -0400

    docs: Aggregate external repositories documentation

---
+++ doc/import_external_docs.ps1
@@ -0,0 +1,46 @@
+Set-PSDebug -Trace 1
+
+$external_docs =
+@(
+    @("https://github.com/unoplatform/uno.wasm.bootstrap", "uno.wasm.bootstrap", "dev/jela/docs-aggregate"),
+    @("https://github.com/unoplatform/uno.themes", "uno.themes", "master"),
+    @("https://github.com/unoplatform/uno.toolkit.ui", "uno.toolkit.ui", "main"),
+    @("https://github.com/unoplatform/uno.check", "uno.check", "main"),
+    @("https://github.com/unoplatform/uno.extensions", "uno.extensions", "main")
+)
+
+$ErrorActionPreference = 'Stop'
+
+function Assert-ExitCodeIsZero()
+{
+    if ($LASTEXITCODE -ne 0)
+    {
+        throw "Exit code must be zero."
+	}
+}
+
+mkdir articles\external -ErrorAction Continue
+pushd articles\external
+
+# ensure long paths are supported on Windows
+git config --global core.longpaths true
+
+# Heads - Release
+for($i = 0; $i -lt $external_docs.Length; $i++)
+{
+    $repoUrl=$external_docs[$i][0]
+    $repoPath=$external_docs[$i][1]
+    $repoBranch=$external_docs[$i][2]
+
+    echo "Cloning $repoPath ($repoUrl@$repoBranch)"
+
+    git clone $repoUrl $repoPath
+    Assert-ExitCodeIsZero
+
+    pushd $repoPath
+    git checkout $repoBranch
+    Assert-ExitCodeIsZero
+    popd
+}
+
+popd
