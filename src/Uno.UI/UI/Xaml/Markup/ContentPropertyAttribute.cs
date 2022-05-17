2dd56121e2b7a6b601b5b8b13ddcc70f4b56270e
Martin Zikmund <martin@zikmund.dev>
2022-05-17 12:43:03 +0200

    feat: Marking additional Markup interfaces as implemented

--- src/Uno.UI/UI/Xaml/Markup/ContentPropertyAttribute.cs
+++ src/Uno.UI/UI/Xaml/Markup/ContentPropertyAttribute.cs
@@ -1,27 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Windows.UI.Xaml.Markup
+namespace Windows.UI.Xaml.Markup;
+
+/// <summary>
+/// Defines the property that will be used in Xaml when using implicit content.
+/// Indicates which property of a type is the XAML content property.
+/// A XAML processor uses this information when processing XAML child elements
+/// of XAML representations of the attributed type.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+public sealed partial class ContentPropertyAttribute : Attribute
 {
 	/// <summary>
-	/// Defines the property that will be used in Xaml when using implicit content.
+	/// Initializes a new instance of the ContentPropertyAttribute class.
 	/// </summary>
-	[AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
-	public sealed partial class ContentPropertyAttribute : Attribute
+	public ContentPropertyAttribute()
 	{
-		private string _name;
 
-		// This is a positional argument
-		public ContentPropertyAttribute()
-		{
-
-		}
-
-		public string Name
-		{
-			get { return _name; }
-			set { _name = value; }
-		}
 	}
+
+	/// <summary>
+	/// Gets or sets the Content property name.
+	/// </summary>
+	public string Name { get; set; }
 }
