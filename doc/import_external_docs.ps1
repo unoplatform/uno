97129d481787e98f44dd903cf21a8fe2de14d66c
Jérôme Laban <jlaban@gmail.com>
2022-05-23 16:57:01 -0400

    docs: Adjust figma docs import

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -7,7 +7,7 @@ $external_docs =
     @("https://github.com/unoplatform/uno.toolkit.ui", "uno.toolkit.ui", "7c31073584d54d306c4001d0905699d47473890a"),
     @("https://github.com/unoplatform/uno.check", "uno.check", "479b01d10240e2c506912c4af46197c1497c9971"),
     @("https://github.com/unoplatform/uno.xamlmerge.task", "uno.xamlmerge.task", "a6d2efa69e24e8280c38300b5c1b7a8f2033f9f9"),
-    @("https://github.com/unoplatform/figma-docs", "figma-docs", "74bf09fd2211ba30e4d763caf134010610443b9a"),
+    @("https://github.com/unoplatform/figma-docs", "figma-docs", "9bf27b6d629c1b8f27a8350010eebcaf39ea8baa"),
     @("https://github.com/unoplatform/uno.extensions", "uno.extensions", "87c21e1d94e05a1adb7712d601aa47ee4da38f3c")
 )
 
