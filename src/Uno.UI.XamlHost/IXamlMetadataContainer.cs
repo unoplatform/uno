c14d7732b1cd9c1c51758f08786611cf27d22f32
Martin Zikmund <martin@zikmund.dev>
2022-06-06 16:47:21 +0200

    chore: Address comments

--- src/Uno.UI.XamlHost/IXamlMetadataContainer.cs
+++ src/Uno.UI.XamlHost/IXamlMetadataContainer.cs
@@ -3,7 +3,14 @@ using Windows.UI.Xaml.Markup;
 
 namespace Uno.UI.XamlHost;
 
+/// <summary>
+/// Represents a container for XAML metadata providers.
+/// Usually this would be a class derived from Application.
+/// </summary>
 public interface IXamlMetadataContainer
 {
+	/// <summary>
+	/// Gets XAML metadata providers.
+	/// </summary>
 	IList<IXamlMetadataProvider> MetadataProviders { get; }
 }
