701fa5ee06b7bb1deb38f95696610c9a2fbb3a5b
Martin Zikmund <martin@zikmund.dev>
2022-06-07 13:06:08 +0200

    test: DispatcherTimer without explicit Interval

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_DispatcherTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Private.Infrastructure;
+using Windows.UI.Xaml;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml
+{
+	[TestClass]
+	public class Given_DispatcherTimer
+	{
+		[TestMethod]
+		[RunsOnUIThread]
+		public async Task When_No_Interval_Set()
+		{
+			var dispatcherTimer = new DispatcherTimer();
+			try
+			{
+				int tickCounter = 0;
+				dispatcherTimer.Tick += (s, e) =>
+				{
+					tickCounter++;
+				};
+				dispatcherTimer.Start();
+				await TestServices.WindowHelper.WaitFor(() => tickCounter > 0);
+				await TestServices.WindowHelper.WaitFor(() => tickCounter > 5);
+			}
+			finally
+			{
+				dispatcherTimer.Stop();
+			}
+		}
+	}
+}
