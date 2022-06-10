767a449501fe4b46c91fc1fb731a2d0af41fe180
Jérôme Laban <jerome.laban@nventive.com>
2022-06-10 16:28:27 -0400

    docs: Update toolkit and themes commits

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -3,8 +3,8 @@ Set-PSDebug -Trace 1
 $external_docs =
 @(
     @("https://github.com/unoplatform/uno.wasm.bootstrap", "uno.wasm.bootstrap", "3e7cc247cb2851ef9d8293071ed54433e20bfac2"),
-    @("https://github.com/unoplatform/uno.themes", "uno.themes", "b3656e715b3eb7349e1d11566640169691d7ea73"),
-    @("https://github.com/unoplatform/uno.toolkit.ui", "uno.toolkit.ui", "b450d2e072764ff4d824de6baacc78a05d1a7d26"),
+    @("https://github.com/unoplatform/uno.themes", "uno.themes", "2b9d747cc068a38acd75f544db3fa49b0694a035"),
+    @("https://github.com/unoplatform/uno.toolkit.ui", "uno.toolkit.ui", "5a395c9b69b176c722dbce0eb975f2e9284a9c80"),
     @("https://github.com/unoplatform/uno.check", "uno.check", "479b01d10240e2c506912c4af46197c1497c9971"),
     @("https://github.com/unoplatform/uno.xamlmerge.task", "uno.xamlmerge.task", "a6d2efa69e24e8280c38300b5c1b7a8f2033f9f9"),
     @("https://github.com/unoplatform/figma-docs", "figma-docs", "fd669ddc220cbf1a093854d0265a8804e9bc1e14"),
