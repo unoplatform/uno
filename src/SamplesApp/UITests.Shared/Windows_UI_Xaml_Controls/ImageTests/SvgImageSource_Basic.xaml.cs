418b2c09d8b58aa1da9259823bb2e05fdfbc3fa1
Martin Zikmund <martin@zikmund.dev>
2022-06-17 11:26:05 +0200

    test: Simple sample for SVG

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ImageTests/SvgImageSource_Basic.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.ImageTests
+{
+	[Sample("Image")]
+	public sealed partial class SvgImageSource_Basic : Page
+	{
+		public SvgImageSource_Basic()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
