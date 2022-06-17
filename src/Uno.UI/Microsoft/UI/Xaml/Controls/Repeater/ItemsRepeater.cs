2f13d2caf5ed81ed84b6b41901a11495cd2a6b5d
Martin Zikmund <martin@zikmund.dev>
2022-06-17 14:07:53 +0200

    fix: Avoid Layout property event subscription leaks

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/Repeater/ItemsRepeater.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/Repeater/ItemsRepeater.cs
@@ -33,6 +33,8 @@ namespace Microsoft.UI.Xaml.Controls
 		// with this value as small as 61. It was never reached with 60. 
 		private const uint MaxStackLayoutIterations = 60u;
 
+		private readonly SerialDisposable _layoutSubscriptionsRevoker = new SerialDisposable();
+
 		internal IElementFactoryShim ItemTemplateShim => m_itemTemplateWrapper;
 
 		internal object LayoutState
@@ -614,6 +616,20 @@ namespace Microsoft.UI.Xaml.Controls
 			}
 
 			++_loadedCounter;
+
+#if HAS_UNO
+			// Uno specific: If the control was unloaded but is loaded again, reattach layout events
+			if (_layoutSubscriptionsRevoker.Disposable is null && Layout is not null)
+			{
+				Layout.MeasureInvalidated += InvalidateMeasureForLayout;
+				Layout.ArrangeInvalidated += InvalidateArrangeForLayout;
+				_layoutSubscriptionsRevoker.Disposable = Disposable.Create(() =>
+				{
+					Layout.MeasureInvalidated -= InvalidateMeasureForLayout;
+					Layout.ArrangeInvalidated -= InvalidateArrangeForLayout;
+				});
+			}
+#endif
 		}
 
 		private void OnUnloaded(object sender, RoutedEventArgs args)
@@ -625,6 +641,12 @@ namespace Microsoft.UI.Xaml.Controls
 			{
 				m_viewportManager.ResetScrollers();
 			}
+
+#if HAS_UNO
+			// Uno specific: Ensure Layout subscriptions are unattached to avoid memory leaks
+			// because ItemsRepeater uses a "singleton" instance of default StackLayout.
+			_layoutSubscriptionsRevoker.Disposable = null;
+#endif
 		}
 
 		private void OnLayoutUpdated(object sender, object e)
@@ -776,8 +798,7 @@ namespace Microsoft.UI.Xaml.Controls
 			if (oldValue != null)
 			{
 				oldValue.UninitializeForContext(GetLayoutContext());
-				oldValue.MeasureInvalidated -= InvalidateMeasureForLayout;
-				oldValue.ArrangeInvalidated -= InvalidateArrangeForLayout;
+				_layoutSubscriptionsRevoker.Disposable = null;
 				_stackLayoutMeasureCounter = 0u;
 
 				// Walk through all the elements and make sure they are cleared
@@ -806,6 +827,11 @@ namespace Microsoft.UI.Xaml.Controls
 				newValue.InitializeForContext(GetLayoutContext());
 				newValue.MeasureInvalidated += InvalidateMeasureForLayout;
 				newValue.ArrangeInvalidated += InvalidateArrangeForLayout;
+				_layoutSubscriptionsRevoker.Disposable = Disposable.Create(() =>
+				{
+					newValue.MeasureInvalidated -= InvalidateMeasureForLayout;
+					newValue.ArrangeInvalidated -= InvalidateArrangeForLayout;
+				});
 			}
 
 			bool isVirtualizingLayout = newValue is VirtualizingLayout;
