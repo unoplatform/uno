9694bb6233a563451cad4351ac3a11bcb3bac8cb
Jérôme Laban <jlaban@gmail.com>
2022-06-29 13:51:40 -0400

    docs: Update figma docs to fix designer getting started

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -7,7 +7,7 @@ $external_docs =
     @("https://github.com/unoplatform/uno.toolkit.ui", "uno.toolkit.ui", "5a395c9b69b176c722dbce0eb975f2e9284a9c80"),
     @("https://github.com/unoplatform/uno.check", "uno.check", "479b01d10240e2c506912c4af46197c1497c9971"),
     @("https://github.com/unoplatform/uno.xamlmerge.task", "uno.xamlmerge.task", "a6d2efa69e24e8280c38300b5c1b7a8f2033f9f9"),
-    @("https://github.com/unoplatform/figma-docs", "figma-docs", "fd669ddc220cbf1a093854d0265a8804e9bc1e14"),
+    @("https://github.com/unoplatform/figma-docs", "figma-docs", "4904915548a31725d4ba81683f6e300a0a84c9b5"),
     @("https://github.com/unoplatform/uno.extensions", "uno.extensions", "87c21e1d94e05a1adb7712d601aa47ee4da38f3c")
 )
 
