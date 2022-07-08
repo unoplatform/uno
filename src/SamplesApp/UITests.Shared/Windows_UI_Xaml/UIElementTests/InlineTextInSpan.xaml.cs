1a30a08276e9e9ac24c332c980e1442eaa681b0a
Martin Zikmund <martin@zikmund.dev>
2022-07-08 13:55:36 +0200

    fix: Add code-behind for InlineTextInSpan to avoid build warning for WASM

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/UIElementTests/InlineTextInSpan.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml.UIElementTests
+{
+	[Sample("UIElement")]
+	public sealed partial class InlineTextInSpan : Page
+	{
+		public InlineTextInSpan()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
