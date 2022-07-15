73e28428453b57c657a2634c97fca024e13f3f55
David <github.site@torick.net>
2022-07-15 02:05:42 -0400

    test(pointers): Test hit test with clipping

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Input/PointersTests/HitTest_Clipping.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Windows_UI_Input.PointersTests;
+
+[Sample("Pointers")]
+public sealed partial class HitTest_Clipping : Page
+{
+	public HitTest_Clipping()
+	{
+		this.InitializeComponent();
+	}
+	private void PrepareScroller(object sender, RoutedEventArgs e)
+	{
+		The_Scroller.ScrollToVerticalOffset(100);
+	}
+
+	private void OnTargetClicked(object sender, RoutedEventArgs e)
+	{
+		The_Output.Text = (sender as FrameworkElement)?.Name ?? sender?.GetType().Name ?? "--unknown--";
+	}
+}
