80ca410b91885eb8b6129af4e55cecd0aa5da9da
Mike Marynowski <mikem@singulink.com>
2022-07-15 13:23:08 -0400

    feat(TextBlock): [Skia] Add support for TextAlignment.Justify and CharacterSpacing

--- src/Uno.UI/UI/Xaml/Documents/TextFormatting/RenderSegmentSpan.skia.cs
+++ src/Uno.UI/UI/Xaml/Documents/TextFormatting/RenderSegmentSpan.skia.cs
@@ -7,5 +7,5 @@ namespace Windows.UI.Xaml.Documents.TextFormatting
 	/// <summary>
 	/// Represents a span of a segment that is rendered on a render line.
 	/// </summary>
-	internal readonly record struct RenderSegmentSpan(Segment Segment, int GlyphsStart, int GlyphsLength, float Width, float WidthWithoutTrailingSpaces);
+	internal record RenderSegmentSpan(Segment Segment, int GlyphsStart, int GlyphsLength, int TrailingSpaces, float CharacterSpacing, float Width, float WidthWithoutTrailingSpaces);
 }
