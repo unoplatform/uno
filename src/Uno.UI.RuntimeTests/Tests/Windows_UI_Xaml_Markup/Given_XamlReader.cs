8723bed4d0a71a9893e46e29120e6b2427df8cbb
xiaoy312 <xiaoyao312@gmail.com>
2022-07-22 18:00:46 -0400

    feat(xaml): add support for raw numerical enum values

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Markup/Given_XamlReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uno.UI.RuntimeTests.Helpers;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using static Private.Infrastructure.TestServices;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Markup
+{
+	[TestClass]
+	[RunsOnUIThread]
+	public class Given_XamlReader
+	{
+		[TestMethod]
+		public void When_Enum_HasNumericalValue()
+		{
+			XamlReader.Load(@"
+				<StackPanel xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+					Orientation='0' />
+			".Replace('\'', '"'));
+		}
+	}
+}
