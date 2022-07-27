57cfd2fa29a56e2dfa1a0e23aa5470eac02fb833
Martin Zikmund <martin@zikmund.dev>
2022-07-27 12:32:50 +0200

    test: Avoid overriding default alignment of ContentPresenter child

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ContentPresenter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls;
+
+[TestClass]
+[RunsOnUIThread]
+[RequiresFullWindow]
+public class Given_ContentPresenter
+{
+	[TestMethod]
+	public void When_Content_Alignment_Set_Default_Alignment_Not_Overriden()
+	{
+		var contentPresenter = new ContentPresenter()
+		{
+			HorizontalContentAlignment = HorizontalAlignment.Center
+		};
+		var border = new Border();
+		contentPresenter.Content = border;
+
+		Assert.AreEqual(HorizontalAlignment.Center, contentPresenter.HorizontalContentAlignment);
+		Assert.AreEqual(HorizontalAlignment.Stretch, border.HorizontalAlignment);
+	}
+}
