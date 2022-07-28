69392bdc1fd39f8fa7bd9b1b6a21160fb2061ae0
Martin Zikmund <martin@zikmund.dev>
2022-07-28 12:24:33 +0200

    test: ContentPresenter alignment for all configurations

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ContentPresenter.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ContentPresenter.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Private.Infrastructure;
+using Windows.Foundation;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls;
 
@@ -25,5 +28,154 @@ public class Given_ContentPresenter
 
 		Assert.AreEqual(HorizontalAlignment.Center, contentPresenter.HorizontalContentAlignment);
 		Assert.AreEqual(HorizontalAlignment.Stretch, border.HorizontalAlignment);
+	}
+
+	public static IEnumerable<object[]> GetAlignments()
+	{
+		var configurations = new List<AlignmentTestConfiguration>();
+		// Centered content
+
+		foreach (var outerHorizontalAlignment in Enum.GetValues(typeof(HorizontalAlignment)).OfType<HorizontalAlignment>())
+		{
+			foreach (var outerVerticalAlignment in Enum.GetValues(typeof(VerticalAlignment)).OfType<VerticalAlignment>())
+			{
+				foreach (var innerHorizontalAlignment in Enum.GetValues(typeof(HorizontalAlignment)).OfType<HorizontalAlignment>())
+				{
+					foreach (var innerVerticalAlignment in Enum.GetValues(typeof(VerticalAlignment)).OfType<VerticalAlignment>())
+					{
+						double expectedX = 0;
+						if (outerHorizontalAlignment == HorizontalAlignment.Center)
+						{
+							expectedX = 50;
+						}
+						else if (outerHorizontalAlignment == HorizontalAlignment.Right)
+						{
+							expectedX = 100;
+						}
+						else if (outerHorizontalAlignment == HorizontalAlignment.Stretch)
+						{
+							// Inner alignment matters only now
+							if (innerHorizontalAlignment == HorizontalAlignment.Center)
+							{
+								expectedX = 50;
+							}
+							else if (innerHorizontalAlignment == HorizontalAlignment.Right)
+							{
+								expectedX = 100;
+							}
+						}
+
+						double expectedY = 0;
+						if (outerVerticalAlignment == VerticalAlignment.Center)
+						{
+							expectedY = 50;
+						}
+						else if (outerVerticalAlignment == VerticalAlignment.Bottom)
+						{
+							expectedY = 100;
+						}
+						else if (outerVerticalAlignment == VerticalAlignment.Stretch)
+						{
+							// Inner alignment matters only now
+							if (innerVerticalAlignment == VerticalAlignment.Center)
+							{
+								expectedY = 50;
+							}
+							else if (innerVerticalAlignment == VerticalAlignment.Bottom)
+							{
+								expectedY = 100;
+							}
+						}
+
+						double expectedWidth = 100;
+						if (outerHorizontalAlignment == HorizontalAlignment.Stretch &&
+							innerHorizontalAlignment == HorizontalAlignment.Stretch)
+						{
+							expectedWidth = 200;
+						}
+
+						double expectedHeight = 100;
+						if (outerVerticalAlignment == VerticalAlignment.Stretch &&
+							innerVerticalAlignment == VerticalAlignment.Stretch)
+						{
+							expectedHeight = 200;
+						}
+
+						configurations.Add(new AlignmentTestConfiguration(
+							outerHorizontalAlignment,
+							outerVerticalAlignment,
+							innerHorizontalAlignment,
+							innerVerticalAlignment,
+							new Point(expectedX, expectedY),
+							new Size(expectedWidth, expectedHeight)
+						));
+					}
+				}
+			}
+		}
+
+		return configurations.Select(c => new object[] { c });
+	}
+
+	[TestMethod]
+	[DynamicData(nameof(GetAlignments), DynamicDataSourceType.Method)]
+	public async Task When_Content_Aligned_Position_And_Size(AlignmentTestConfiguration configuration)
+	{
+		var contentPresenter = new ContentPresenter()
+		{
+			HorizontalContentAlignment = configuration.OuterHorizontal,
+			VerticalContentAlignment = configuration.OuterVertical,
+			Width = 200,
+			Height = 200,
+			Background = new SolidColorBrush(Colors.Red)
+		};
+		var border = new Border()
+		{
+			HorizontalAlignment = configuration.InnerHorizontal,
+			VerticalAlignment = configuration.InnerVertical,
+			Background = new SolidColorBrush(Colors.Blue),
+			MinWidth = 100,
+			MinHeight = 100
+		};
+		contentPresenter.Content = border;
+		TestServices.WindowHelper.WindowContent = contentPresenter;
+
+		await TestServices.WindowHelper.WaitForLoaded(contentPresenter);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		var transform = border.TransformToVisual(contentPresenter);
+		var point = transform.TransformPoint(new Point());
+		Assert.AreEqual(configuration.ExpectedPosition, point);
+		Assert.AreEqual(configuration.ExpectedSize, new Size(border.ActualWidth, border.ActualHeight));
+	}
+
+	public class AlignmentTestConfiguration
+	{
+		public AlignmentTestConfiguration(HorizontalAlignment outerHorizontal, VerticalAlignment outerVertical, HorizontalAlignment innerHorizontal, VerticalAlignment innerVertical, Point expectedPosition, Size expectedSize)
+		{
+			OuterHorizontal = outerHorizontal;
+			OuterVertical = outerVertical;
+			InnerHorizontal = innerHorizontal;
+			InnerVertical = innerVertical;
+			ExpectedPosition = expectedPosition;
+			ExpectedSize = expectedSize;
+		}
+
+		public HorizontalAlignment OuterHorizontal { get; }
+
+		public VerticalAlignment OuterVertical { get; }
+
+		public HorizontalAlignment InnerHorizontal { get; }
+
+		public VerticalAlignment InnerVertical { get; }
+
+		public Point ExpectedPosition { get; }
+
+		public Size ExpectedSize { get; }
+
+		public override string ToString()
+		{
+			return $"{OuterHorizontal}/{OuterVertical}/{InnerHorizontal}/{InnerVertical}/{ExpectedPosition}/{ExpectedSize}";
+		}
 	}
 }
