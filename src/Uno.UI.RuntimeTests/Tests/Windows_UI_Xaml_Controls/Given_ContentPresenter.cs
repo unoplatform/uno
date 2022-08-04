7018dd3f61e8c92e7d7e84732600c01c012dc5f6
Martin Zikmund <martin@zikmund.dev>
2022-08-04 16:57:55 +0200

    test: Enable ContentPresenter tests on macOS

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ContentPresenter.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ContentPresenter.cs
@@ -14,9 +14,6 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls;
 [TestClass]
 [RunsOnUIThread]
 [RequiresFullWindow]
-#if __MACOS__
-[Ignore("Currently fails on macOS, part of #9282! epic")]
-#endif
 public class Given_ContentPresenter
 {
 	[TestMethod]
