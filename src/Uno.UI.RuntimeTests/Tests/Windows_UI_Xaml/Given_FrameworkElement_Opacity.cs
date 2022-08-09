f762c2b00a83cfa15fd4ae98b0f94946cd3b635d
Jerome Laban <jerome.laban@nventive.com>
2022-08-09 13:13:33 -0400

    chore: Adjust test for skia only

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_FrameworkElement_Opacity.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_FrameworkElement_Opacity.cs
@@ -32,6 +32,7 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 
 	public class Given_FrameworkElement_Opacity
 	{
+#if __SKIA__
 		[TestMethod]
 		public async Task When_Opacity()
 		{
@@ -51,23 +52,23 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 
 			var width = SUT.tbOpacity1_0.ActualWidth;
 			var height = SUT.tbOpacity1_0.ActualHeight;
-			ImageAssert.HasColorAtChild(si, SUT.tbOpacity1_0, (width / 4) * 3, height / 2, Colors.Black);
+			await ImageAssert.HasColorAtChild(si, SUT.tbOpacity1_0, (width / 4) * 3, height / 2, Colors.Black);
 
 			width = SUT.tbOpacity0_5.ActualWidth;
 			height = SUT.tbOpacity0_5.ActualHeight;
-			ImageAssert.HasColorAtChild(si, SUT.tbOpacity0_5, (width / 4) * 3, height / 2, "#FF808080");
+			await ImageAssert.HasColorAtChild(si, SUT.tbOpacity0_5, (width / 4) * 3, height / 2, "#FF808080");
 
 			width = SUT.tbOpacity0_1.ActualWidth;
 			height = SUT.tbOpacity0_1.ActualHeight;
-			ImageAssert.HasColorAtChild(si, SUT.tbOpacity0_1, (width / 4) * 3, height / 2, "#0xFFE6E6E6");
+			await ImageAssert.HasColorAtChild(si, SUT.tbOpacity0_1, (width / 4) * 3, height / 2, "#0xFFE6E6E6");
 
 			width = SUT.border0_5.ActualWidth;
 			height = SUT.border0_5.ActualHeight;
-			ImageAssert.HasColorAtChild(si, SUT.border0_5, 2, 2, "#FFFF8080");
+			await ImageAssert.HasColorAtChild(si, SUT.border0_5, 2, 2, "#FFFF8080");
 
 			width = SUT.ImageOpacity0_5.ActualWidth;
 			height = SUT.ImageOpacity0_5.ActualHeight;
-			ImageAssert.HasColorAtChild(si, SUT.ImageOpacity0_5, width / 2, height / 2, "#FFFEF3C2");
+			await ImageAssert.HasColorAtChild(si, SUT.ImageOpacity0_5, width / 2, height / 2, "#FFFEF3C2");
 		}
 
 		[TestMethod]
@@ -86,27 +87,28 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 			var si = new RawBitmap(renderer, SUT);
 			var width = SUT.tbInnerOpacity1_0.ActualWidth;
 			var height = SUT.tbInnerOpacity1_0.ActualHeight;
-			ImageAssert.HasColorAtChild(si, SUT.tbInnerOpacity1_0, (width / 4) * 3.3, height / 2, "#FF808080");
+			await ImageAssert.HasColorAtChild(si, SUT.tbInnerOpacity1_0, (width / 4) * 3.3, height / 2, "#FF808080");
 
 			width = SUT.tbInnerOpacity0_5.ActualWidth;
 			height = SUT.tbInnerOpacity0_5.ActualHeight;
-			ImageAssert.HasColorAtChild(si, SUT.tbInnerOpacity0_5, (width / 4) * 3.3, height / 2, "#FFC0C0C0");
+			await ImageAssert.HasColorAtChild(si, SUT.tbInnerOpacity0_5, (width / 4) * 3.3, height / 2, "#FFC0C0C0");
 
 			width = SUT.tbInnerOpacity0_1.ActualWidth;
 			height = SUT.tbInnerOpacity0_1.ActualHeight;
-			ImageAssert.HasColorAtChild(si, SUT.tbInnerOpacity0_1, (width / 4) * 3.3, height / 2, "#FFF3F3F3");
+			await ImageAssert.HasColorAtChild(si, SUT.tbInnerOpacity0_1, (width / 4) * 3.3, height / 2, "#FFF3F3F3");
 
 			width = SUT.BorderInnerOpacity0_5.ActualWidth;
 			height = SUT.BorderInnerOpacity0_5.ActualHeight;
-			ImageAssert.HasColorAtChild(si, SUT.BorderInnerOpacity0_5, 2, 2, "#FFFFC0C0");
+			await ImageAssert.HasColorAtChild(si, SUT.BorderInnerOpacity0_5, 2, 2, "#FFFFC0C0");
 
 			width = SUT.tbBorderInnerOpacity0_5.ActualWidth;
 			height = SUT.tbBorderInnerOpacity0_5.ActualHeight;
-			ImageAssert.HasColorAtChild(si, SUT.tbBorderInnerOpacity0_5, (width / 4) * 3.3, height / 2, "#FFC09090");
+			await ImageAssert.HasColorAtChild(si, SUT.tbBorderInnerOpacity0_5, (width / 4) * 3.3, height / 2, "#FFC09090");
 
 			width = SUT.ImageInner0_5.ActualWidth;
 			height = SUT.ImageInner0_5.ActualHeight;
-			ImageAssert.HasColorAtChild(si, SUT.ImageInner0_5, width / 2, height / 2, "#FFFEF9E1");
+			await ImageAssert.HasColorAtChild(si, SUT.ImageInner0_5, width / 2, height / 2, "#FFFEF9E1");
 		}
+#endif
 	}
 }
