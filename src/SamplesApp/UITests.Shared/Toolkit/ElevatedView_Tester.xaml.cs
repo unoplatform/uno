0a7711168e92111237f8827778abff402515f483
Martin Zikmund <martin@zikmund.dev>
2022-08-12 14:30:54 +0200

    test: ElevatedView scenario tester
    (cherry picked from commit 80de1d447d549eff12f74d81ec968834287016db)

---
+++ src/SamplesApp/UITests.Shared/Toolkit/ElevatedView_Tester.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Uno.UI.Samples.Controls;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace UITests.Toolkit
+{
+	[Sample]
+	public sealed partial class ElevatedView_Tester : Page
+	{
+		private int _elevation;
+		private int _radius;
+		private string _color = "#000000";
+
+		public ElevatedView_Tester()
+		{
+			this.InitializeComponent();
+		}
+
+		public int Elevation
+		{
+			get => _elevation;
+			set
+			{
+				_elevation = value;
+				OnValuesChanged();
+			}
+		}
+
+		public int Radius
+		{
+			get => _radius;
+			set
+			{
+				_radius = value;
+				OnValuesChanged();
+			}
+		}
+
+		public string ColorString
+		{
+			get => _color;
+			set
+			{
+				_color = value;
+				OnValuesChanged();
+			}
+		}
+
+		private void OnValuesChanged()
+		{
+			Color color = ConvertColorFromHexString(ColorString);
+			ElevatedElement.ShadowColor = color;
+			ElevatedElement.Elevation = Elevation;
+			ElevatedElement.CornerRadius = new CornerRadius(Radius);
+		}
+
+		private Color ConvertColorFromHexString(string colorString)
+		{
+			try
+			{
+				//Target hex string
+				colorString = colorString.Replace("#", string.Empty);
+				// from #RRGGBB string
+				var a = (byte)System.Convert.ToUInt32(colorString.Substring(0, 2), 16);
+				var r = (byte)System.Convert.ToUInt32(colorString.Substring(2, 2), 16);
+				var g = (byte)System.Convert.ToUInt32(colorString.Substring(4, 2), 16);
+				var b = (byte)System.Convert.ToUInt32(colorString.Substring(6, 2), 16);
+				//get the color
+				return Color.FromArgb(255, r, g, b);
+			}
+			catch
+			{
+				return Colors.Black;
+			}
+		}
+	}
+}
