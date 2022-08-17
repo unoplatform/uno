1565ef00397c82d110442bf85c1f5715e2d1ed07
Andres Pineda <pineda.andres@gmail.com>
2022-08-17 14:13:11 -0400

    chore: fix typo in property

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/FlipViewPages/FlipViewTemplateSelector.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/FlipViewPages/FlipViewTemplateSelector.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -8,14 +6,14 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls.FlipViewPages
 {
 	public class FlipViewTemplateSelector : DataTemplateSelector
 	{
-		public DataTemplate TemplatePair { get; set; }
+		public DataTemplate TemplateEven { get; set; }
 
 		public DataTemplate TemplateOdd { get; set; }
 
 		protected override DataTemplate SelectTemplateCore(object item)
 		{
 			return Convert.ToInt32(item) % 2 == 0
-					? TemplatePair
+					? TemplateEven
 					: TemplateOdd;			
 		}
 	}
