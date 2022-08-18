bedad4360d10f84c43a21f6cd7373d02fb2d5d45
Martin Zikmund <martin@zikmund.dev>
2022-08-18 09:24:55 +0200

    chore: Basic SvgCanvas control on Skia

--- src/AddIns/Uno.UI.Svg/SvgProvider.cs
+++ src/AddIns/Uno.UI.Svg/SvgProvider.cs
@@ -1,22 +1,42 @@
-﻿using System.IO;
-using SkiaSharp;
-using SkiaSharp.Views.UWP;
-using Svg.Skia;
+﻿#nullable enable
+
+using System;
 using Uno.UI.Xaml.Media.Imaging.Svg;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Uno.UI.Svg;
 
 [Preserve]
-internal partial class SvgProvider : ISvgProvider
+public partial class SvgProvider : ISvgProvider
 {
-	public UIElement GetImage(Stream stream)
+	private SvgImageSource _owner;
+
+	public SvgProvider(object owner)
 	{
-		var svg = new SKSvg();
+		if (owner is not SvgImageSource svgImageSource)
+		{
+			throw new InvalidOperationException("Owner must be a SvgImageSource instance.");
+		}
 
-		svg.Load(stream);
+		_owner = svgImageSource;
+	}
 
-		SKXamlCanvas canvas = new SKXamlCanvas();
-		canvas.DrawPicture(svg.Picture);
+	public UIElement GetCanvas()
+	{
+		return new SvgCanvas(_owner);
+	}
+
+	public UIElement? GetImage(SvgImageSource imageSource)
+	{
+		_owner.RaiseImageOpened();
+		return null;
+		//imageSource.RasterizePixelHeight
+		//var svg = new SKSvg();
+
+		//svg.Load(stream);
+
+		//SKXamlCanvas canvas = new SKXamlCanvas();
+		//canvas.(svg.Picture);
 	}
 }
