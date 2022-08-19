ebed703d61e811720feb5d3268c5b71715933eb3
Martin Zikmund <martin@zikmund.dev>
2022-08-19 17:04:43 +0200

    feat: ElevatedView WASM unification

--- src/Uno.UI.Toolkit/UIElementExtensions.cs
+++ src/Uno.UI.Toolkit/UIElementExtensions.cs
@@ -133,10 +133,9 @@ namespace Uno.UI.Toolkit
 					// Values for 1dp elevation according to https://material.io/guidelines/resources/shadows.html#shadows-illustrator
 					const double x = 0.25d;
 					const double y = 0.92f * 0.5f; // Looks more accurate than the recommended 0.92f.
-					const double blur = 0.5f;
-					var color = Color.FromArgb((byte)(shadowColor.A * .35), shadowColor.R, shadowColor.G, shadowColor.B);
+					const double blur = 0.3f;
 
-					var str = $"{(x * elevation).ToStringInvariant()}px {(y * elevation).ToStringInvariant()}px {(blur * elevation).ToStringInvariant()}px {color.ToCssString()}";
+					var str = $"{(x * elevation).ToStringInvariant()}px {(y * elevation).ToStringInvariant()}px {(blur * elevation).ToStringInvariant()}px {shadowColor.ToCssString()}";
 					uiElement.SetStyle("box-shadow", str);
 					uiElement.SetCssClasses("noclip");
 				}
