e703783752b3a7a2d30b8e822c45bd90c620a0ed
Martin Zikmund <martin@zikmund.dev>
2022-08-21 21:43:10 +0200

    chore: Image SVG initialization on Skia

---
+++ src/Uno.UI/UI/Xaml/Media/Imaging/SvgImageSource.iOSmacOS.cs
@@ -0,0 +1,90 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using Uno.Foundation.Extensibility;
+using Uno.Foundation.Logging;
+using Uno.UI.Xaml.Media.Imaging.Svg;
+
+namespace Windows.UI.Xaml.Media.Imaging;
+
+partial class SvgImageSource
+{
+	private ISvgProvider _svgProvider;
+
+	partial void InitPartial()
+	{
+		if (!ApiExtensibility.CreateInstance(this, out _svgProvider))
+		{
+			if (this.Log().IsEnabled(LogLevel.Error))
+			{
+				this.Log().LogError("To use SVG on this platform, make sure to install the Uno.UI.Svg package.");
+			}
+		}
+	}
+
+	internal UIElement GetCanvas() => _svgProvider.GetCanvas();
+
+	private protected override bool TryOpenSourceAsync(CancellationToken ct, int? targetWidth, int? targetHeight, out Task<ImageData> asyncImage)
+	{
+		asyncImage = TryOpenSourceAsync(ct);
+		return true;
+	}
+
+	private async Task<ImageData> TryOpenSourceAsync(CancellationToken ct)
+	{
+		try
+		{
+			if (UriSource != null)
+			{
+				if (UriSource.Scheme == "http" || UriSource.Scheme == "https")
+				{
+					var client = new HttpClient();
+					var response = await client.GetAsync(UriSource, HttpCompletionOption.ResponseContentRead, ct);
+					using var imageStream = await response.Content.ReadAsStreamAsync();
+					return await ReadFromStreamAsync(imageStream);
+				}
+				else if (UriSource.Scheme == "ms-appx")
+				{
+					var path = UriSource.PathAndQuery;
+					path = GetApplicationPath(path);
+					using var fileStream = File.OpenRead(path);
+					return await ReadFromStreamAsync(fileStream);
+				}
+				else if (UriSource.Scheme == "ms-appdata")
+				{
+					using var fileStream = File.OpenRead(FilePath);
+					return await ReadFromStreamAsync(fileStream);
+				}
+			}
+			else if (_stream != null)
+			{
+				return await ReadFromStreamAsync(_stream.AsStream());
+			}
+		}
+		catch (Exception e)
+		{
+			return new ImageData() { Error = e };
+		}
+
+		return default;
+	}
+
+	private async Task<ImageData> ReadFromStreamAsync(Stream stream)
+	{
+		var memoryStream = new MemoryStream();
+		await stream.CopyToAsync(memoryStream);
+		var data = memoryStream.ToArray();
+		return ImageData.FromBytes(data);
+	}
+
+	private static string GetApplicationPath(string rawPath)
+	{
+		var originalLocalPath =
+			Path.Combine(Windows.Application­Model.Package.Current.Installed­Location.Path,
+				 rawPath.TrimStart('/').Replace('/', global::System.IO.Path.DirectorySeparatorChar)
+			);
+
+		return originalLocalPath;
+	}
+}
