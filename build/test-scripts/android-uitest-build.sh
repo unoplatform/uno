29f7d6e22e80631e6a276d1a8d1f13113dc194b6
Jerome Laban <jerome.laban@nventive.com>
2022-08-22 10:37:40 -0400

    ci: Use roslyn generators for android test app

--- build/test-scripts/android-uitest-build.sh
+++ build/test-scripts/android-uitest-build.sh
@@ -9,7 +9,7 @@ cd $BUILD_SOURCESDIRECTORY/build
 export IsUiAutomationMappingEnabled=true
 
 # build the sample and tests, while the emulator is starting
-mono '/Applications/Visual Studio.app/Contents/MonoBundle/MSBuild/Current/bin/MSBuild.dll' /m /r /p:Configuration=$BUILDCONFIGURATION /p:AndroidUseLatestPlatformSdk=true $BUILD_SOURCESDIRECTORY/src/SamplesApp/SamplesApp.Droid/SamplesApp.Droid.csproj
+mono '/Applications/Visual Studio.app/Contents/MonoBundle/MSBuild/Current/bin/MSBuild.dll' /m /r /p:Configuration=$BUILDCONFIGURATION /p:UnoUIUseRoslynSourceGenerators=true /p:AndroidUseLatestPlatformSdk=true $BUILD_SOURCESDIRECTORY/src/SamplesApp/SamplesApp.Droid/SamplesApp.Droid.csproj
 
 mkdir -p $BUILD_ARTIFACTSTAGINGDIRECTORY/android
 cp $BUILD_SOURCESDIRECTORY/src/SamplesApp/SamplesApp.Droid/bin/$BUILDCONFIGURATION/uno.platform.unosampleapp-Signed.apk $BUILD_ARTIFACTSTAGINGDIRECTORY/android
