28d98bf7868b34ed4d0c8db410efc00d08d7befb
Martin Zikmund <martin@zikmund.dev>
2022-08-25 16:42:56 +0200

    test: Add ImageIcon and BitmapIcon test for SVG

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ImageTests/SvgImageSource_Icons.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Uno.Extensions;
+using Uno.UI.Samples.Controls;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace UITests.Windows_UI_Xaml_Controls.ImageTests;
+
+[Sample("Image")]
+public sealed partial class SvgImageSource_Icons : Page
+{
+	public SvgImageSource_Icons()
+	{
+		this.InitializeComponent();
+	}
+}
