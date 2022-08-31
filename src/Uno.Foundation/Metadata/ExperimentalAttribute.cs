67e8dd83e25f4d7656bfecb02253a5e1442967f7
Youssef Victor <youssefvictor00@gmail.com>
2022-08-31 13:41:15 +0200

    docs: Copy docs from docs.microsoft.com

--- src/Uno.Foundation/Metadata/ExperimentalAttribute.cs
+++ src/Uno.Foundation/Metadata/ExperimentalAttribute.cs
@@ -2,6 +2,15 @@
 
 namespace Windows.Foundation.Metadata;
 
+/// <summary>
+/// Indicates that a type or member should be marked in metadata as experimental, and consequently may not be present in the final, released version of an SDK or library.
+/// </summary>
 public partial class ExperimentalAttribute : Attribute
 {
+	/// <summary>
+	/// Creates and initializes a new instance of the attribute.
+	/// </summary>
+	public ExperimentalAttribute()
+	{
+	}
 }
