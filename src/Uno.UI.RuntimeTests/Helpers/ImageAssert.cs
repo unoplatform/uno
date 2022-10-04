3f365146fa7dab29ee28440399ae29e7e029f266
Youssef Victor <youssefvictor00@gmail.com>
2022-10-04 20:31:46 +0200

    chore: Follow up to merge conflict

--- src/Uno.UI.RuntimeTests/Helpers/ImageAssert.cs
+++ src/Uno.UI.RuntimeTests/Helpers/ImageAssert.cs
@@ -41,7 +41,7 @@ public static partial class ImageAssert
 	public static void HasColorAtChild(RawBitmap screenshot, UIElement child, double x, double y, Color expectedColor, byte tolerance = 0, [CallerLineNumber] int line = 0)
 	{
 		var point = child.TransformToVisual(screenshot.RenderedElement).TransformPoint(new Windows.Foundation.Point(x, y));
-		await HasColorAtImpl(screenshot, (int)point.X, (int)point.Y, expectedColor, tolerance, line);
+		HasColorAtImpl(screenshot, (int)point.X, (int)point.Y, expectedColor, tolerance, line);
 	}
 
 	/// <summary>
