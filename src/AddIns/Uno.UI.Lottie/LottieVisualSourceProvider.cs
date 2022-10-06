c24976beae74927c1542443422536e0ad400cb69
Youssef1313 <youssefvictor00@gmail.com>
2022-10-06 16:26:09 +0200

    ci: Check formatting on CI and build

--- src/AddIns/Uno.UI.Lottie/LottieVisualSourceProvider.cs
+++ src/AddIns/Uno.UI.Lottie/LottieVisualSourceProvider.cs
@@ -5,9 +5,7 @@ using Microsoft.Toolkit.Uwp.UI.Lottie;
 using Uno.Foundation.Extensibility;
 
 #pragma warning disable 105
-// Keep the space as this will not be replaced by the
-// WinUI upgrade tool.
-using Microsoft. UI.Xaml.Controls;
+using Microsoft/*Intentional space for WinUI upgrade tool*/.UI.Xaml.Controls;
 #pragma warning restore 105
 
 [assembly: ApiExtension(typeof(ILottieVisualSourceProvider), typeof(Uno.UI.Lottie.LottieVisualSourceProvider))]
