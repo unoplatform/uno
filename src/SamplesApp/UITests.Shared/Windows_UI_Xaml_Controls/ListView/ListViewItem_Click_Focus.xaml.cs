b70b2e701f97facf45a6b5cf9e6ce4832956a353
Martin Zikmund <martin@zikmund.dev>
2022-11-03 10:56:42 +0100

    test: ListViewItem focus sample

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ListView/ListViewItem_Click_Focus.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace UITests.Windows_UI_Xaml_Controls.ListView
+{
+	[Sample("ListView")]
+	public sealed partial class ListViewItem_Click_Focus : Page
+	{
+		public ListViewItem_Click_Focus()
+		{
+			this.InitializeComponent();
+			ClearButton.Click += (s, e) => OutputTextBlock.Text = "";
+			TestListViewItem.GettingFocus += (s, e) => OutputTextBlock.Text = "F";
+		}
+	}
+}
