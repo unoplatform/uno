9f615ad16c4a50ba2375a799b6288da0d1a52251
Martin Zikmund <martin@zikmund.dev>
2022-11-11 22:56:38 +0100

    chore: Start porting CommandBarFlyout

---
+++ src/Uno.UI/UI/Xaml/Input/StandardUICommand/CommandBarFlyout.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Markup;
+
+namespace Microsoft.UI.Xaml.Controls;
+
+[ContentProperty(Name = nameof(PrimaryCommands))]
+public partial class CommandBarFlyout : FlyoutBase
+{
+}
