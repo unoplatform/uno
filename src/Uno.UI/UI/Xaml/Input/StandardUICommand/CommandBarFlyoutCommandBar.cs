d837bbbcf12f41cf2b8a46e60d92cfa40fb9af43
Martin Zikmund <martin@zikmund.dev>
2022-11-14 09:07:40 +0100

    feat: CommandBarFlyoutCommandBar

--- src/Uno.UI/UI/Xaml/Input/StandardUICommand/CommandBarFlyoutCommandBar.cs
+++ src/Uno.UI/UI/Xaml/Input/StandardUICommand/CommandBarFlyoutCommandBar.cs
@@ -1,7 +1,26 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Microsoft.UI.Xaml.Controls.Primitives;
 
 public partial class CommandBarFlyoutCommandBar : CommandBar
 {
+	/// <summary>
+	/// Gets an object that provides calculated values that can be referenced as {TemplateBinding} markup
+	/// extension sources when defining templates for a CommandBarFlyoutCommandBar control.
+	/// </summary>
+	public CommandBarFlyoutCommandBarTemplateSettings FlyoutTemplateSettings
+	{
+		get => (CommandBarFlyoutCommandBarTemplateSettings)GetValue(FlyoutTemplateSettingsProperty);
+		set => SetValue(FlyoutTemplateSettingsProperty, value);
+	}
+
+	internal static DependencyProperty FlyoutTemplateSettingsProperty { get; } =
+		DependencyProperty.Register(
+			nameof(FlyoutTemplateSettings),
+				typeof(CommandBarFlyoutCommandBarTemplateSettings),
+				typeof(CommandBarFlyoutCommandBar),
+				new FrameworkPropertyMetadata(null));
+
+
 }
