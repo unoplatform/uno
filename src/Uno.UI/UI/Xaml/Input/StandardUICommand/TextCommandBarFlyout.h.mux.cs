ae625e832372c32aea34ec9d4a6c0c829c64be7d
Martin Zikmund <martin@zikmund.dev>
2022-11-14 19:09:15 +0100

    chore: TextCommandBarFlyout header ported, command porting

--- src/Uno.UI/UI/Xaml/Input/StandardUICommand/TextCommandBarFlyout.h.mux.cs
+++ src/Uno.UI/UI/Xaml/Input/StandardUICommand/TextCommandBarFlyout.h.mux.cs
@@ -1,94 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Uno.Disposables;
+using Uno.UI.Helpers.WinUI;
+using Windows.UI.Xaml.Controls;
 
 namespace Microsoft.UI.Xaml.Controls;
 
+[Flags]
+internal enum  TextControlButtons
+{
+	None = 0x0000,
+    Cut = 0x0001,
+    Copy = 0x0002,
+    Paste = 0x0004,
+    Bold = 0x0008,
+    Italic = 0x0010,
+    Underline = 0x0020,
+    Undo = 0x0040,
+    Redo = 0x0080,
+    SelectAll = 0x0100,
+}
+
 partial class TextCommandBarFlyout
 {
-	//		enum class TextControlButtons
-	//		{
-	//			None = 0x0000,
-	//    Cut = 0x0001,
-	//    Copy = 0x0002,
-	//    Paste = 0x0004,
-	//    Bold = 0x0008,
-	//    Italic = 0x0010,
-	//    Underline = 0x0020,
-	//    Undo = 0x0040,
-	//    Redo = 0x0080,
-	//    SelectAll = 0x0100,
-	//};
+	private Dictionary<TextControlButtons, ICommandBarElement> m_buttons;
+	private AppBarButton m_proofingButton;
 
-	//		DECLARE_FLAG_ENUM_OPERATOR_OVERLOADS(TextControlButtons);
+	private List<SerialDisposable> m_buttonCommandRevokers;
+	private List<SerialDisposable> m_buttonClickRevokers;
+	private List<SerialDisposable> m_toggleButtonCheckedRevokers;
+	private List<SerialDisposable> m_toggleButtonUncheckedRevokers;
 
-	//		class TextCommandBarFlyout :
+	private SerialDisposable m_proofingButtonLoadedRevoker = new();
 
-	//	public implementation.TextCommandBarFlyoutT<TextCommandBarFlyout, CommandBarFlyout>
-	//{
-	//public:
-	//    ForwardRefToBaseReferenceTracker(CommandBarFlyout)
+	private List<SerialDisposable> m_proofingMenuItemClickRevokers;
+	private List<SerialDisposable> m_proofingMenuToggleItemClickRevokers;
+	private DispatcherHelper m_dispatcherHelper { this };
 
-
-	//	public TextCommandBarFlyout();
-
-	//		private:
-	//    void UpdateButtons();
-
-	//		TextControlButtons GetButtonsToAdd();
-	//		static TextControlButtons GetTextBoxButtonsToAdd(TextBox & textBox);
-	//		static TextControlButtons GetTextBlockButtonsToAdd(TextBlock & textBlock);
-	//		static TextControlButtons GetRichEditBoxButtonsToAdd(RichEditBox & richEditBox);
-	//		static TextControlButtons GetRichTextBlockButtonsToAdd(RichTextBlock & richTextBlock);
-	//		static TextControlButtons GetPasswordBoxButtonsToAdd(PasswordBox & passwordBox);
-
-	//		bool IsButtonInPrimaryCommands(TextControlButtons button);
-
-	//		void InitializeButtonWithUICommand(
-	//			ButtonBase & button,
-	//			XamlUICommand & uiCommand,
-	//			std.function<void()> & executeFunc);
-
-	//		void InitializeButtonWithProperties(
-	//			ButtonBase & button,
-	//			ResourceIdType labelResourceId,
-	//			ResourceIdType acceleratorKeyResourceId,
-	//			ResourceIdType descriptionResourceId,
-	//			std.function<void()> & executeFunc);
-
-	//		void InitializeButtonWithProperties(
-	//			ButtonBase & button,
-	//			ResourceIdType labelResourceId,
-	//			Symbol & symbol,
-	//			ResourceIdType acceleratorKeyResourceId,
-	//			ResourceIdType descriptionResourceId,
-	//			std.function<void()> & executeFunc);
-
-	//		void ExecuteCutCommand();
-	//		void ExecuteCopyCommand();
-	//		void ExecutePasteCommand();
-	//		void ExecuteBoldCommand();
-	//		void ExecuteItalicCommand();
-	//		void ExecuteUnderlineCommand();
-	//		void ExecuteUndoCommand();
-	//		void ExecuteRedoCommand();
-	//		void ExecuteSelectAllCommand();
-
-	//		ICommandBarElement GetButton(TextControlButtons button);
-
-	//		std.map<TextControlButtons, ICommandBarElement> m_buttons;
-	//		AppBarButton m_proofingButton { null };
-
-	//		std.vector<XamlUICommand.ExecuteRequested_revoker> m_buttonCommandRevokers;
-	//		std.vector<ButtonBase.Click_revoker> m_buttonClickRevokers;
-	//		std.vector<ToggleButton.Checked_revoker> m_toggleButtonCheckedRevokers;
-	//		std.vector<ToggleButton.Unchecked_revoker> m_toggleButtonUncheckedRevokers;
-
-	//		FrameworkElement.Loaded_revoker m_proofingButtonLoadedRevoker { };
-
-	//		std.vector<MenuFlyoutItem.Click_revoker> m_proofingMenuItemClickRevokers;
-	//		std.vector<ToggleMenuFlyoutItem.Click_revoker> m_proofingMenuToggleItemClickRevokers;
-	//		DispatcherHelper m_dispatcherHelper { this };
-
-	//		bool m_isSettingToggleButtonState = false;
+	private bool m_isSettingToggleButtonState = false;
 }
