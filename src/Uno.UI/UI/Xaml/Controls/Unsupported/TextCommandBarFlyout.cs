eab74779b7c03d5ae35bddfe59679882d2a76d8b
Martin Zikmund <martin@zikmund.dev>
2022-11-14 18:55:10 +0100

    chore: Mark WUX TextCommandBarFlyout unsupported

---
+++ src/Uno.UI/UI/Xaml/Controls/Unsupported/TextCommandBarFlyout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Windows.UI.Xaml.Controls;
+
+[Obsolete(
+		"The Windows.UI.Xaml.Controls version of this control is not supported. " +
+		"Please use Microsoft.UI.Xaml.Controls.TextCommandBarFlyout instead.")]
+public partial class TextCommandBarFlyout
+{
+	public TextCommandBarFlyout()
+	{
+		throw new NotImplementedException(
+			"The Windows.UI.Xaml.Controls version of this control is not supported. " +
+			"Please use Microsoft.UI.Xaml.Controls.TextCommandBarFlyout instead.");
+	}
+}
