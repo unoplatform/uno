3d549baad8cb1d4a14ad2328d46f4d60cda30f54
Martin Zikmund <martin@zikmund.dev>
2022-11-26 00:25:00 +0100

    chore: File-scoped namespaces

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/TabView/TabViewItem.Header.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/TabView/TabViewItem.Header.cs
@@ -5,29 +5,28 @@
 using Uno.Disposables;
 using Microsoft.UI.Xaml.Controls;
 
-namespace Microsoft/* UWP don't rename */.UI.Xaml.Controls
+namespace Microsoft/* UWP don't rename */.UI.Xaml.Controls;
+
+public partial class TabViewItem
 {
-	public partial class TabViewItem
-	{
-		private Button m_closeButton = null;
-		private ToolTip m_toolTip = null;
-		private ContentPresenter m_headerContentPresenter = null;
-		private TabViewWidthMode m_tabViewWidthMode = TabViewWidthMode.Equal;
-		private TabViewCloseButtonOverlayMode m_closeButtonOverlayMode = TabViewCloseButtonOverlayMode.Auto;
+	private Button m_closeButton = null;
+	private ToolTip m_toolTip = null;
+	private ContentPresenter m_headerContentPresenter = null;
+	private TabViewWidthMode m_tabViewWidthMode = TabViewWidthMode.Equal;
+	private TabViewCloseButtonOverlayMode m_closeButtonOverlayMode = TabViewCloseButtonOverlayMode.Auto;
 
-		private bool m_firstTimeSettingToolTip = true;
+	private bool m_firstTimeSettingToolTip = true;
 
-		//private readonly SerialDisposable m_closeButtonClickRevoker = new SerialDisposable();
-		//private readonly SerialDisposable m_tabDragStartingRevoker = new SerialDisposable();
-		//private readonly SerialDisposable m_tabDragCompletedRevoker = new SerialDisposable();
+	private readonly SerialDisposable m_closeButtonClickRevoker = new SerialDisposable();
+	private readonly SerialDisposable m_tabDragStartingRevoker = new SerialDisposable();
+	private readonly SerialDisposable m_tabDragCompletedRevoker = new SerialDisposable();
 
-		private bool m_hasPointerCapture = false;
-		private bool m_isMiddlePointerButtonPressed = false;
-		private bool m_isDragging = false;
-		private bool m_isPointerOver = false;
+	private bool m_hasPointerCapture = false;
+	private bool m_isMiddlePointerButtonPressed = false;
+	private bool m_isDragging = false;
+	private bool m_isPointerOver = false;
 
-		private object m_shadow = null;
+	private object m_shadow = null;
 
-		private TabView m_parentTabView = null;
-	}
+	private TabView m_parentTabView = null;
 }
--- src/Uno.UI/Microsoft/UI/Xaml/Controls/TabView/TabViewTabDragCompletedEventArgs.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/TabView/TabViewTabDragCompletedEventArgs.cs
@@ -5,35 +5,34 @@
 using Windows.ApplicationModel.DataTransfer;
 using Microsoft.UI.Xaml.Controls;
 
-namespace Microsoft/* UWP don't rename */.UI.Xaml.Controls
+namespace Microsoft/* UWP don't rename */.UI.Xaml.Controls;
+
+/// <summary>
+/// Provides data for the TabDragCompleted event.
+/// </summary>
+public sealed partial class TabViewTabDragCompletedEventArgs
 {
-	/// <summary>
-	/// Provides data for the TabDragCompleted event.
-	/// </summary>
-	public sealed partial class TabViewTabDragCompletedEventArgs
+	private readonly DragItemsCompletedEventArgs _args;
+
+	internal TabViewTabDragCompletedEventArgs(DragItemsCompletedEventArgs args, object item, TabViewItem tab)
 	{
-		private readonly DragItemsCompletedEventArgs _args;
-
-		internal TabViewTabDragCompletedEventArgs(DragItemsCompletedEventArgs args, object item, TabViewItem tab)
-		{
-			_args = args;
-			Item = item;
-			Tab = tab;
-		}
-
-		/// <summary>
-		/// Gets a value that indicates what operation was performed on the dragged data, and whether it was successful.
-		/// </summary>
-		public DataPackageOperation DropResult => _args.DropResult;
-
-		/// <summary>
-		/// Gets the item that was selected for the drag action.
-		/// </summary>
-		public object Item { get; }
-
-		/// <summary>
-		/// Gets the TabViewItem that was selected for the drag action.
-		/// </summary>
-		public TabViewItem Tab { get; }
+		_args = args;
+		Item = item;
+		Tab = tab;
 	}
+
+	/// <summary>
+	/// Gets a value that indicates what operation was performed on the dragged data, and whether it was successful.
+	/// </summary>
+	public DataPackageOperation DropResult => _args.DropResult;
+
+	/// <summary>
+	/// Gets the item that was selected for the drag action.
+	/// </summary>
+	public object Item { get; }
+
+	/// <summary>
+	/// Gets the TabViewItem that was selected for the drag action.
+	/// </summary>
+	public TabViewItem Tab { get; }
 }
