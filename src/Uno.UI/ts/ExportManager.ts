83f5da757862c2388b69b6fc031c10a2c5dbf80b
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2022-12-06 15:02:29 -0500

    feat: Initialize runtime exports at startup

---
+++ src/Uno.UI/ts/ExportManager.ts
@@ -0,0 +1,16 @@
+﻿// eslint-disable-next-line @typescript-eslint/no-namespace
+namespace Uno.UI {
+
+	export class ExportManager {
+
+		public static async initialize(): Promise<void> {
+
+			if ((<any>Module).getAssemblyExports !== undefined) {
+				const unoExports = await (<any>Module).getAssemblyExports("Uno");
+				const unoUIExports = await (<any>Module).getAssemblyExports("Uno.UI");
+
+				(<any>globalThis).DotnetExports = { Uno: unoExports, UnoUI: unoUIExports };
+			}
+		}
+	}
+}
