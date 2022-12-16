1cb346c4124fa10aed90fc536fa689f1d3d2ac98
Jerome Laban <jerome.laban@nventive.com>
2022-12-16 11:58:14 -0500

    fix(embeddedresources): Restore proper AssignTargetPaths/AssignLinkMetadata for EmbeddedResource LogicalName metadata

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_ApplicationModel_Resources/Given_EmbeddedResources.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions.Execution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+using System.Linq;
+using Windows.ApplicationModel.Resources;
+using Windows.ApplicationModel.Resources.Core;
+using Windows.UI.Xaml;
+
+namespace Uno.UI.RuntimeTests.Tests
+{
+	[TestClass]
+	public class Given_EmbeddedResources
+	{
+		[TestMethod]
+		public void When_EmbeddedResource()
+		{
+			var assembly = Application.Current.GetType().Assembly;
+
+			var resourceName = assembly.GetManifestResourceNames().FirstOrDefault(f => f.EndsWith("SubFolder1.SubFolder2.SharedProjectEmbeddedFile.txt"));
+			Assert.IsNotNull(resourceName);
+
+			using var s = assembly.GetManifestResourceStream(resourceName);
+
+			Assert.AreNotEqual(0, s.Length);
+		}
+	}
+}
