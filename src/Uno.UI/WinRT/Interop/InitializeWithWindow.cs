d6e9b26b63476b945685fce58e0012ee049ecce5
Martin Zikmund <martin@zikmund.dev>
2022-12-20 15:30:34 +0100

    feat: Add WinRT interop methods

---
+++ src/Uno.UI/WinRT/Interop/InitializeWithWindow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WinRT.Interop;
+
+public static class InitializeWithWindow
+{
+	public static void Initialize(object target, IntPtr hwnd)
+	{
+		// Intentionally a no-op, will need to be implemented
+		// when multi-window support is added #8341.
+	}
+}
---
+++ src/Uno.UI/WinRT/Interop/WindowNative.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WinRT.Interop;
+
+public static class WindowNative
+{
+	public static IntPtr GetWindowHandle(object target)
+	{
+		// Intentionally a no-op, will need to be implemented
+		// when multi-window support is added #8341.
+		return IntPtr.Zero;
+	}
+}
