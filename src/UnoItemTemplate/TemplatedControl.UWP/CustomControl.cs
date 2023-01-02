ca2e1d9adbdfbbd30c269e2f1e42337276075dc7
Joan Magnet <jmagnet@vilardellpurti.com>
2023-01-02 19:11:52 +0100

    Revert "fix Add partial modifier TemplatedControl"
    This reverts commit 3d8fb86127cd70583e2fd6de98efeae73cda46a1.

--- src/UnoItemTemplate/TemplatedControl.UWP/CustomControl.cs
+++ src/UnoItemTemplate/TemplatedControl.UWP/CustomControl.cs
@@ -14,7 +14,7 @@ using Windows.UI.Xaml.Media;
 
 namespace $rootnamespace$
 {
-    public partial sealed class $safeitemname$ : Control
+    public sealed class $safeitemname$ : Control
     {
         public $safeitemname$()
         {
