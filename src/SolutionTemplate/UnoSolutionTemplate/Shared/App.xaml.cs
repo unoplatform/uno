ef3df260527d19e537cd193098672b5c4f64d927
Martin Zikmund <martin@zikmund.dev>
2023-01-02 13:47:42 +0100

    chore: Make App._window field internal

--- src/SolutionTemplate/UnoSolutionTemplate/Shared/App.xaml.cs
+++ src/SolutionTemplate/UnoSolutionTemplate/Shared/App.xaml.cs
@@ -23,7 +23,7 @@ namespace $ext_safeprojectname$
     /// </summary>
     public sealed partial class App : Application
     {
-        private Window _window;
+        internal static Window _window;
 
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
