14f0a1ee25e3928e05778670f30d32c1d2b3f43e
Youssef1313 <youssefvictor00@gmail.com>
2023-01-13 17:11:00 +0200

    chore: One more fix

---
+++ src/Uno.UI/UI/Xaml/Shapes/ArbitraryShapeBase.skia.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Uno.Disposables;
+using System.Text;
+using Windows.Foundation;
+
+namespace Windows.UI.Xaml.Shapes
+{
+	public abstract partial class ArbitraryShapeBase : Shape
+	{
+	}
+}
