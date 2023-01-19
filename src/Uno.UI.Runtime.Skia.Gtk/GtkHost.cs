58f5bf6a29cc1264a1e7e3365967d7de01dfa6f2
Jerome Laban <jerome.laban@nventive.com>
2023-01-19 15:13:36 -0500

    fix(gtk): Add more descriptive message on initialization failure

--- src/Uno.UI.Runtime.Skia.Gtk/GtkHost.cs
+++ src/Uno.UI.Runtime.Skia.Gtk/GtkHost.cs
@@ -87,7 +87,11 @@ namespace Uno.UI.Runtime.Skia
 		{
 			Windows.UI.Xaml.Documents.Inline.ApplyHarfbuzzWorkaround();
 
-			Gtk.Application.Init();
+			if (!InitializeGtk())
+			{
+				return;
+			}
+			
 			SetupTheme();
 
 			ApiExtensibility.Register(typeof(Uno.ApplicationModel.Core.ICoreApplicationExtension), o => new CoreApplicationExtension(o));
@@ -135,6 +139,23 @@ namespace Uno.UI.Runtime.Skia
 			SetupRenderSurface();
 
 			Gtk.Application.Run();
+		}
+
+		private bool InitializeGtk()
+		{
+			try
+			{
+				Gtk.Application.Init();
+				return true;
+			}
+			catch(System.TypeInitializationException e)
+			{
+				if (this.Log().IsEnabled(LogLevel.Error))
+				{
+					this.Log().Error($"Unable to initialize Gtk, visit https://aka.platform.uno/gtk-install for more information.", e);
+				}
+				return false;
+			}
 		}
 
 		void DispatchNative(System.Action d)
