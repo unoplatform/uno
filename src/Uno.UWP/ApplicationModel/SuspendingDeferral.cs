f8887e7d22a762616c14749929cb3e6222d6a494
Youssef Victor <youssefvictor00@gmail.com>
2023-02-02 17:21:24 +0200

    fix!: Removed `SuspendingDeferral` constructor that takes an `Action`
    BREAKING CHANGE: Removed `SuspendingDeferral` constructor that takes an `Action`

--- src/Uno.UWP/ApplicationModel/SuspendingDeferral.cs
+++ src/Uno.UWP/ApplicationModel/SuspendingDeferral.cs
@@ -14,14 +14,6 @@ public sealed partial class SuspendingDeferral : ISuspendingDeferral
 	private readonly Action? _deferralDone;
 	private readonly DeferralCompletedHandler? _handler;
 
-	/// <summary>
-	/// This can be removed with other breaking changes
-	/// </summary>
-	/// <param name="deferralDone"></param>
-	[DebuggerHidden]
-	public SuspendingDeferral(Action? deferralDone) =>
-		_deferralDone = deferralDone;
-
 	internal SuspendingDeferral(DeferralCompletedHandler handler) =>
 		_handler = handler;
 
