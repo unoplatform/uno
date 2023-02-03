4b5bee59c5d734679dc1ff6d917678bae5990343
Jerome Laban <jerome.laban@nventive.com>
2023-02-03 09:25:48 -0500

    fix(resources): Resources loading from separate assemblies
    (cherry picked from commit 2f7d09c7b317772bfb37c07c6aa2bb9c0469f675)
    
    # Conflicts:
    #	src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlCodeGeneration.cs

--- src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlCodeGeneration.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlCodeGeneration.cs
@@ -293,8 +293,8 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 					|| PlatformHelper.IsXamarinMacOs(_generatorContext)
 					|| PlatformHelper.IsAndroid(_generatorContext);
 
-				var resourceKeys = GetResourceKeys(_generatorContext.CancellationToken);
-				TryGenerateUnoResourcesKeyAttribute(resourceKeys);
+				var resourceDetailsCollection = BuildResourceDetails(_generatorContext.CancellationToken);
+				TryGenerateUnoResourcesKeyAttribute(resourceDetailsCollection);
 
 				var filesFull = new XamlFileParser(_excludeXamlNamespaces, _includeXamlNamespaces, _metadataHelper)
 					.ParseFiles(_xamlSourceFiles, _projectDirectory, _generatorContext.CancellationToken);
@@ -348,7 +348,7 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 									lastReferenceUpdateTime: lastBinaryUpdateTime,
 									analyzerSuppressions: _analyzerSuppressions,
 									globalStaticResourcesMap: globalStaticResourcesMap,
-									resourceKeys: resourceKeys,
+									resourceDetailsCollection: resourceDetailsCollection,
 									isUiAutomationMappingEnabled: _isUiAutomationMappingEnabled,
 									uiAutomationMappings: _uiAutomationMappings,
 									defaultLanguage: _defaultLanguage,
@@ -402,10 +402,15 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 			}
 		}
 
-		private void TryGenerateUnoResourcesKeyAttribute(ImmutableHashSet<string> resourceKeys)
+		private void TryGenerateUnoResourcesKeyAttribute(ResourceDetailsCollection resourceDetailsCollection)
 		{
+<<<<<<< HEAD
 			var hasResources = !resourceKeys.IsEmpty;
 			
+=======
+			var hasResources = resourceDetailsCollection.HasLocalResources;
+
+>>>>>>> 2f7d09c7b3 (fix(resources): Resources loading from separate assemblies)
 			_generatorContext.AddSource(
 				"LocalizationResources",
 				$"[assembly: global::System.Reflection.AssemblyMetadata(" +
@@ -615,10 +620,21 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 			}
 		}
 
-		//get keys of localized strings
-		private ImmutableHashSet<string> GetResourceKeys(CancellationToken ct)
+		// Get keys of localized strings
+		private ResourceDetailsCollection BuildResourceDetails(CancellationToken ct)
 		{
-			ImmutableHashSet<string> resourceKeys = _resourceFiles
+			var localResources = BuildLocalResourceDetails(ct);
+
+			var collection = new ResourceDetailsCollection(_metadataHelper.AssemblyName);
+
+			collection.AddRange(localResources);
+
+			return collection;
+		}
+
+		private ResourceDetails[] BuildLocalResourceDetails(CancellationToken ct)
+		{
+			var resourceKeys = _resourceFiles
 				.AsParallel()
 				.WithCancellation(ct)
 				.SelectMany(file => {
@@ -629,6 +645,8 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 					{
 						var sourceText = file.File.GetText(ct)!;
 						var cachedFileKey = new ResourceCacheKey(file.Identity, sourceText.GetChecksum());
+						var resourceFileName = Path.GetFileNameWithoutExtension(file.Identity);
+
 						if (_cachedResources.TryGetValue(cachedFileKey, out var cachedResource))
 						{
 							_cachedResources[cachedFileKey] = cachedResource.WithUpdatedLastTimeUsed();
@@ -649,8 +667,8 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 						// Per this documentation, /root/data should be more performant than //data
 						var keys = doc.SelectNodes("/root/data")
 							?.Cast<XmlElement>()
-							.Select(node => RewriteResourceKeyName(rewriterBuilder, node.GetAttribute("name")))
-							.ToArray() ?? Array.Empty<string>();
+							.Select(node => RewriteResourceKeyName(rewriterBuilder, node.GetAttribute("name"), resourceFileName))
+							.ToArray() ?? Array.Empty<ResourceDetails>();
 						_cachedResources[cachedFileKey] = new CachedResource(DateTimeOffset.Now, keys);
 						return keys;
 					}
@@ -666,35 +684,44 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 
 							_generatorContext.ReportDiagnostic(diagnostic);
 
+<<<<<<< HEAD
 							return Array.Empty<string>();
+=======
+						return Array.Empty<ResourceDetails>();
+>>>>>>> 2f7d09c7b3 (fix(resources): Resources loading from separate assemblies)
 #else
 						throw new InvalidOperationException(message, e);
 #endif
 					}
 				})
 				.Distinct()
-				.ToImmutableHashSet();
+				.ToArray();
 
 #if DEBUG
-			Console.WriteLine(resourceKeys.Count + " localization keys found");
+			Console.WriteLine(resourceKeys.Length + " localization keys found");
 #endif
 			return resourceKeys;
 		}
 
-		private string RewriteResourceKeyName(StringBuilder builder, string keyName)
+		private ResourceDetails RewriteResourceKeyName(StringBuilder builder, string keyName, string resourceFileName)
 		{
-			var firstDotIndex = keyName.IndexOf('.');
-			if (firstDotIndex != -1)
-			{
-				builder.Clear();
-				builder.Append(keyName);
+			//var firstDotIndex = keyName.IndexOf('.');
+			//if (firstDotIndex != -1)
+			//{
+			//	builder.Clear();
+			//	builder.Append(keyName);
 
-				builder[firstDotIndex] = '/';
+			//	builder[firstDotIndex] = '/';
 
-				return builder.ToString();
-			}
+			//	keyName = builder.ToString();
+			//}
 
-			return keyName;
+#if true // false
+			// Uncomment for debugging
+			Console.WriteLine($"Build key {keyName} ({resourceFileName} / {_isUnoHead})");
+#endif
+
+			return new(_metadataHelper.AssemblyName, resourceFileName, keyName);
 		}
 
 		private DateTime GetLastBinaryUpdateTime()
