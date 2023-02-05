2979700e884a80cbb21bb2451a7967d258852a08
Youssef Victor <youssefvictor00@gmail.com>
2023-02-05 05:38:23 +0200

    fix!: Remove CalendarDatePicker._dateChanged
    BREAKING CHANGE: `CalendarDatePicker._dateChanged` shouldn't be public.

--- src/Uno.UI/UI/Xaml/Controls/CalendarView/CalendarDatePicker.Events.cs
+++ src/Uno.UI/UI/Xaml/Controls/CalendarView/CalendarDatePicker.Events.cs
@@ -5,29 +5,11 @@ namespace Windows.UI.Xaml.Controls
 {
 	partial class CalendarDatePicker
 	{
-		private event EventHandler<object> _opened;
+		public event EventHandler<object> Opened;
 
-		public event EventHandler<object> Opened
-		{
-			add => _opened += value;
-			remove => _opened -= value;
-		}
+		public event EventHandler<object> Closed;
 
-		private event EventHandler<object> _closed;
-
-		public event EventHandler<object> Closed
-		{
-			add => _closed += value;
-			remove => _closed -= value;
-		}
-
-		public event TypedEventHandler<CalendarDatePicker, CalendarDatePickerDateChangedEventArgs> _dateChanged;
-
-		public event TypedEventHandler<CalendarDatePicker, CalendarDatePickerDateChangedEventArgs> DateChanged
-		{
-			add => _dateChanged += value;
-			remove => _dateChanged -= value;
-		}
+		public event TypedEventHandler<CalendarDatePicker, CalendarDatePickerDateChangedEventArgs> DateChanged;
 
 		private protected override void OnUnloaded()
 		{
