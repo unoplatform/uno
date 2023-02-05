d032b36bff666d45beac7d37d24ec55312dc0073
Youssef Victor <youssefvictor00@gmail.com>
2023-02-05 15:14:01 +0200

    feat: Basic implementation for HttpRequestMessage

--- src/Uno.UWP/Generated/3.0.0.0/Windows.Web.Http/HttpMethod.cs
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.Web.Http/HttpMethod.cs
@@ -2,107 +2,13 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.Web.Http
 {
-	#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+	#if false || false || false || false || false || false || false
 	[global::Uno.NotImplemented]
 	#endif
-	public  partial class HttpMethod : global::Windows.Foundation.IStringable
+	public partial class HttpMethod : global::Windows.Foundation.IStringable
 	{
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
-		public  string Method
-		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member string HttpMethod.Method is not implemented. For more information, visit https://aka.platform.uno/notimplemented?m=string%20HttpMethod.Method");
-			}
-		}
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
-		public static global::Windows.Web.Http.HttpMethod Delete
-		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member HttpMethod HttpMethod.Delete is not implemented. For more information, visit https://aka.platform.uno/notimplemented?m=HttpMethod%20HttpMethod.Delete");
-			}
-		}
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
-		public static global::Windows.Web.Http.HttpMethod Get
-		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member HttpMethod HttpMethod.Get is not implemented. For more information, visit https://aka.platform.uno/notimplemented?m=HttpMethod%20HttpMethod.Get");
-			}
-		}
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
-		public static global::Windows.Web.Http.HttpMethod Head
-		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member HttpMethod HttpMethod.Head is not implemented. For more information, visit https://aka.platform.uno/notimplemented?m=HttpMethod%20HttpMethod.Head");
-			}
-		}
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
-		public static global::Windows.Web.Http.HttpMethod Options
-		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member HttpMethod HttpMethod.Options is not implemented. For more information, visit https://aka.platform.uno/notimplemented?m=HttpMethod%20HttpMethod.Options");
-			}
-		}
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
-		public static global::Windows.Web.Http.HttpMethod Patch
-		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member HttpMethod HttpMethod.Patch is not implemented. For more information, visit https://aka.platform.uno/notimplemented?m=HttpMethod%20HttpMethod.Patch");
-			}
-		}
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
-		public static global::Windows.Web.Http.HttpMethod Post
-		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member HttpMethod HttpMethod.Post is not implemented. For more information, visit https://aka.platform.uno/notimplemented?m=HttpMethod%20HttpMethod.Post");
-			}
-		}
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
-		public static global::Windows.Web.Http.HttpMethod Put
-		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member HttpMethod HttpMethod.Put is not implemented. For more information, visit https://aka.platform.uno/notimplemented?m=HttpMethod%20HttpMethod.Put");
-			}
-		}
-		#endif
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
-		public HttpMethod( string method) 
-		{
-			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.Web.Http.HttpMethod", "HttpMethod.HttpMethod(string method)");
-		}
-		#endif
 		// Forced skipping of method Windows.Web.Http.HttpMethod.HttpMethod(string)
 		// Forced skipping of method Windows.Web.Http.HttpMethod.Method.get
-		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
-		public override string ToString()
-		{
-			throw new global::System.NotImplementedException("The member string HttpMethod.ToString() is not implemented. For more information, visit https://aka.platform.uno/notimplemented?m=string%20HttpMethod.ToString%28%29");
-		}
-		#endif
 		// Forced skipping of method Windows.Web.Http.HttpMethod.Delete.get
 		// Forced skipping of method Windows.Web.Http.HttpMethod.Get.get
 		// Forced skipping of method Windows.Web.Http.HttpMethod.Head.get
