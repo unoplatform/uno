a622eef188759ee2e8331db5dbc5b0571595eaa7
Youssef Victor <youssefvictor00@gmail.com>
2023-02-07 14:51:19 +0200

    test: Add test for CalendarDatePicker

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_CalendarDatePicker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using static Private.Infrastructure.TestServices;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls;
+
+[TestClass]
+[RunsOnUIThread]
+public class Given_CalendarDatePicker
+{
+	[TestMethod]
+	public async Task TestCalendarPanelSize()
+	{
+		var SUT = new CalendarDatePicker();
+		WindowHelper.WindowContent = SUT;
+		await WindowHelper.WaitForIdle();
+
+		var root = (Grid)SUT.FindName("Root");
+		var flyout = (Flyout)FlyoutBase.GetAttachedFlyout(root);
+		flyout.Open();
+
+		await WindowHelper.WaitForIdle();
+		var calendarView = (CalendarView)flyout.Content;
+
+		Assert.AreEqual(352, calendarView.ActualHeight);
+
+		flyout.Close();
+	}
+}
