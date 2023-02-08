d6ec766ed03670b652ee8b00a41bf55c9c3dc516
Giuseppe Lippolis <workgroupengineering@outlook.com>
2023-02-08 09:53:00 +0100

    test: CurrencyFormatter

--- src/Uno.UI.Tests/Windows_Globalization/Given_IncrementNumberRounder.cs
+++ src/Uno.UI.Tests/Windows_Globalization/Given_IncrementNumberRounder.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿#nullable enable
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Windows.Globalization.NumberFormatting;
 
 
@@ -16,6 +14,16 @@ namespace Uno.UI.Tests.Windows_Globalization
 		public void Should_Throw_When_RoundingAlgorithm_Is_None()
 		{
 			var sut = new IncrementNumberRounder();
+
+			try
+			{
+				sut.RoundingAlgorithm = RoundingAlgorithm.None;
+			}
+			catch (Exception ex)
+			{
+				Assert.AreEqual("The parameter is incorrect.\r\n\r\nvalue", ex.Message);
+			}
+
 			Assert.ThrowsException<ArgumentException>(() => sut.RoundingAlgorithm = RoundingAlgorithm.None);
 		}
 
@@ -39,6 +47,15 @@ namespace Uno.UI.Tests.Windows_Globalization
 
 			if (shouldThrow)
 			{
+				try
+				{
+					sut.Increment = increment;
+				}
+				catch (Exception ex)
+				{
+					Assert.AreEqual("The parameter is incorrect.\r\n\r\nvalue", ex.Message);
+				}
+
 				Assert.ThrowsException<ArgumentException>(() => sut.Increment = increment);
 			}
 			else
