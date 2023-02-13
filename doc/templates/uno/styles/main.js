6cc52e89607b2f40284d5338b4a4d9bc8ad0a1d8
xiaoy312 <xiaoyao312@gmail.com>
2023-02-13 15:45:13 -0500

    docs: fix navigation breaking on .xref

--- doc/templates/uno/styles/main.js
+++ doc/templates/uno/styles/main.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded",function(){initializeNavbar(),document.addEventListener("click",function(n){const t=n.target;980<=window.innerWidth||!t.matches("#navbar .has-children a")||(n.stopImmediatePropagation(),t.parentElement.classList.toggle("open"))},!0)},!1);
+document.addEventListener("DOMContentLoaded",function(){initializeNavbar(),document.addEventListener("click",function(e){var n=e.target;980<=window.innerWidth||!n.matches("#navbar .has-children a")||(e.stopImmediatePropagation(),n.parentElement.classList.toggle("open"))},!0)},!1);
