db7957e7d21aa495668b0efe323dfde3d4a769e9
Martin Zikmund <martin@zikmund.dev>
2023-02-14 15:37:26 +0100

    refactor: Remove unnecessary Border-related partial methods in ContentPresenter

--- src/Uno.UI/UI/Xaml/Controls/ContentPresenter/ContentPresenter.cs
+++ src/Uno.UI/UI/Xaml/Controls/ContentPresenter/ContentPresenter.cs
@@ -495,12 +495,7 @@ namespace Windows.UI.Xaml.Controls
 				)
 			);
 
-		private void OnPaddingChanged(Thickness oldValue, Thickness newValue)
-		{
-			OnPaddingChangedPartial(oldValue, newValue);
-		}
-
-		partial void OnPaddingChangedPartial(Thickness oldValue, Thickness newValue);
+		private void OnPaddingChanged(Thickness oldValue, Thickness newValue) => UpdateBorder();
 
 		#endregion
 
@@ -577,10 +572,8 @@ namespace Windows.UI.Xaml.Controls
 			set => SetCornerRadiusValue(value);
 		}
 
-		private void OnCornerRadiusChanged(CornerRadius oldValue, CornerRadius newValue)
-		{
-			UpdateCornerRadius(newValue);
-		}
+		private void OnCornerRadiusChanged(CornerRadius oldValue, CornerRadius newValue) =>
+			UpdateBorder();
 
 		#endregion
 
@@ -648,6 +641,14 @@ namespace Windows.UI.Xaml.Controls
 				this.ClearValue(DataContextProperty, DependencyPropertyValuePrecedences.Local);
 			}
 		}
+
+		private void SetUpdateTemplate()
+		{
+			UpdateContentTemplateRoot();
+			SetUpdateTemplatePartial();
+		}
+
+		partial void SetUpdateTemplatePartial();
 
 		private void TrySetDataContextFromContent(object value)
 		{
