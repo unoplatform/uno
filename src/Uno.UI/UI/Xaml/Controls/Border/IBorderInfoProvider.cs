1d4908d170fc614027be5b980f1fbc718016b474
Martin Zikmund <martin@zikmund.dev>
2023-02-14 15:06:31 +0100

    chore: Documenting IBorderInfoProvider

--- src/Uno.UI/UI/Xaml/Controls/Border/IBorderInfoProvider.cs
+++ src/Uno.UI/UI/Xaml/Controls/Border/IBorderInfoProvider.cs
@@ -6,17 +6,38 @@ using Windows.UI.Xaml.Media;
 
 namespace Uno.UI.Xaml.Controls;
 
+/// <summary>
+/// Provides properties required for border rendering.
+/// </summary>
 internal partial interface IBorderInfoProvider
 {
+	/// <summary>
+	/// Gets the background brush.
+	/// </summary>
 	Brush? Background { get; }
 
+	/// <summary>
+	/// Gets the background sizing.
+	/// </summary>
 	BackgroundSizing BackgroundSizing { get; }
 
+	/// <summary>
+	/// Gets the border brush.
+	/// </summary>
 	Brush? BorderBrush { get; }
 
+	/// <summary>
+	/// Gets the border thickness.
+	/// </summary>
 	Thickness BorderThickness { get; }
 
+	/// <summary>
+	/// Gets the corner radius.
+	/// </summary>
 	CornerRadius CornerRadius { get; }
 
+	/// <summary>
+	/// Gets the background image.
+	/// </summary>
 	object? BackgroundImage { get; }
 }
