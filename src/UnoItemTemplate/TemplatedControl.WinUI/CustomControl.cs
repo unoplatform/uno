a23f1e1acee97d46ff32a3ae8daeff8775b68148
Martin Zikmund <martin@zikmund.dev>
2023-02-14 07:34:21 +0100

    chore: Apply suggestions from code review
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>

--- src/UnoItemTemplate/TemplatedControl.WinUI/CustomControl.cs
+++ src/UnoItemTemplate/TemplatedControl.WinUI/CustomControl.cs
@@ -10,7 +10,7 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 
 // To learn about building custom controls see 
-// https://learn.microsoft.com/en-us/windows/apps/winui/winui3/xaml-templated-controls-csharp-winui-3
+// https://learn.microsoft.com/windows/apps/winui/winui3/xaml-templated-controls-csharp-winui-3
 
 namespace $rootnamespace$
 {
