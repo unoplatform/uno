e3d0b8991b67c3d04901674b85fe6335648c749e
Martin Zikmund <martin@zikmund.dev>
2023-02-15 13:08:19 +0100

    chore: Remove duplicate JavaObjectExtensions

--- src/Uno.Foundation/IndexPath.iOS.cs
+++ src/Uno.Foundation/IndexPath.iOS.cs
@@ -1,11 +1,9 @@
-﻿using Foundation;
-
-namespace Uno.UI;
+﻿namespace Uno.UI;
 
 /// <summary>
 /// An index to an entry in a grouped items source.
 /// </summary>
 public partial struct IndexPath
 {
-    internal static IndexPath FromNSIndexPath(NSIndexPath indexPath) => new IndexPath(indexPath.Row, indexPath.Section);
+	internal static IndexPath FromNSIndexPath(NSIndexPath indexPath) => new IndexPath(indexPath.Row, indexPath.Section);
 }
