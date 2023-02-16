76647a04fcbc24b1dfd334e6cd029be9067e02af
Andres Pineda <pineda.andres@gmail.com>
2023-02-16 09:46:42 -0500

    test: assert GridView Items ItemWidth
    (cherry picked from commit 4677addbf7a58431881cb0ae38caf7ac895cce7d)

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_GridView_Items.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_GridView_Items.cs
@@ -15,11 +15,76 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 #if HAS_UNO
 	[TestClass]
 	[RunsOnUIThread]
-#if !__IOS__
-	[Ignore]
 #endif
 	public class Given_GridView_Items
 	{
+		[TestMethod]
+		public async Task When_GridViewItems_WillRespect_ItemsWidth()
+		{
+			var gridView = new GridView
+			{
+				ItemContainerStyle = TestsResourceHelper.GetResource<Style>("MyGridViewItemStyle")
+			};
+
+			var gvi = new GridViewItem();
+			var gvi2 = new GridViewItem();
+
+			gridView.ItemsSource = new[] { gvi, gvi2 };
+
+			var itemWrap = new ItemsWrapGrid()
+			{
+				MaximumRowsOrColumns = 2,
+				Orientation = Orientation.Horizontal,
+				ItemWidth = 30
+			};
+
+			var itemTemplate = new ItemsPanelTemplate(() => itemWrap);
+
+			gridView.SetValue(ItemsControl.ItemsPanelProperty, itemTemplate);
+
+			WindowHelper.WindowContent = gridView;
+			await WindowHelper.WaitForLoaded(gridView);
+			await WindowHelper.WaitForIdle();
+
+			Assert.AreEqual(30, gvi2.LayoutSlot.Width);
+		}
+
+		[TestMethod]
+		public async Task When_GridViewItems_ItemsWidthChanges_Refresh_Layout()
+		{
+			var gridView = new GridView
+			{
+				ItemContainerStyle = TestsResourceHelper.GetResource<Style>("MyGridViewItemStyle")
+			};
+
+			var gvi = new GridViewItem();
+			var gvi2 = new GridViewItem();
+
+			gridView.ItemsSource = new[] { gvi, gvi2 };
+
+			var itemWrap = new ItemsWrapGrid()
+			{
+				MaximumRowsOrColumns = 2,
+				Orientation = Orientation.Horizontal,
+				ItemWidth = 30
+			};
+
+			var itemTemplate = new ItemsPanelTemplate(() => itemWrap);
+
+			gridView.SetValue(GridViewVariableSizeBehavior.ShouldResizeProperty, true);
+			gridView.SetValue(ItemsControl.ItemsPanelProperty, itemTemplate);
+
+			WindowHelper.WindowContent = gridView;
+			await WindowHelper.WaitForLoaded(gridView);
+			await WindowHelper.WaitForIdle();
+
+			Assert.AreEqual(gridView.ActualWidth / 2, gvi2.LayoutSlot.Width, 1);
+		}
+
+
+#if !__IOS__
+		[Ignore]
+#endif
 		[TestMethod]
 		public async Task When_GridViewItems_LayoutSlots()
 		{
@@ -74,5 +139,45 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 			gvi2.LayoutSlotWithMarginsAndAlignments);
 		}
 	}
-#endif
+
+	#region Helper Behaviour Class
+	public class GridViewVariableSizeBehavior
+	{
+		public static bool GetShouldResize(DependencyObject obj) => (bool)obj.GetValue(ShouldResizeProperty);
+
+		public static void SetShouldResize(DependencyObject obj, bool value) => obj.SetValue(ShouldResizeProperty, value);
+
+		public static readonly DependencyProperty ShouldResizeProperty =
+			DependencyProperty.RegisterAttached(
+				"ShouldResize",
+				typeof(bool),
+				typeof(GridViewVariableSizeBehavior),
+				new PropertyMetadata(false, OnShouldResizeChanged));
+
+		private static void OnShouldResizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			if (d is GridView gridView)
+			{
+				if (e.NewValue is bool shouldResize && shouldResize)
+				{
+					gridView.SizeChanged += GridView_SizeChanged;
+				}
+				else
+				{
+					gridView.SizeChanged -= GridView_SizeChanged;
+				}
+			}
+		}
+
+		private static void GridView_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			if (sender is GridView gridView && gridView?.ItemsPanelRoot is ItemsWrapGrid wrapGrid)
+			{
+				var width = (e.NewSize.Width - gridView.Padding.Left - gridView.Padding.Right) / Math.Max(wrapGrid.MaximumRowsOrColumns, 1);
+				wrapGrid.ItemWidth = width;
+			}
+		}
+	}
+	#endregion
+
 }
