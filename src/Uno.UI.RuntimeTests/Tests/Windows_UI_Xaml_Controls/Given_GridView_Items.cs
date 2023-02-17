621ecaef6aa903cda2d420fa04442859c787a2f8
Youssef Victor <youssefvictor00@gmail.com>
2023-02-17 16:26:18 +0200

    chore: Fix formatting
    (cherry picked from commit 127061ed8972d30b89a118e515ae91b7634a7610)

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_GridView_Items.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_GridView_Items.cs
@@ -84,7 +84,7 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 		}
 
 #if !__IOS__
-	[Ignore]
+		[Ignore]
 #endif
 		[TestMethod]
 		public async Task When_GridViewItems_LayoutSlots()
