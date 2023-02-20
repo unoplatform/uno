ef0e490540f414e8b8a6164f983f5fbcd878a137
Youssef Victor <youssefvictor00@gmail.com>
2023-02-20 12:40:07 +0200

    chore: Fix failures

--- src/SourceGenerators/System.Xaml/System.Xaml/XamlXmlReader.cs
+++ src/SourceGenerators/System.Xaml/System.Xaml/XamlXmlReader.cs
@@ -518,7 +518,7 @@ namespace Uno.Xaml
 							atts.Add (r.LocalName, r.Value);
 							continue;
 						}
-						if (r.NamespaceURI.StartsWith("using:", StringComparison.Ordinal)) {
+						if (r.NamespaceURI.StartsWith("using:", StringComparison.Ordinal) || r.NamespaceURI.StartsWith("#using:", StringComparison.Ordinal)) {
 							atts.Add (r.Name, r.Value);
 							continue;
 						}
@@ -573,7 +573,7 @@ namespace Uno.Xaml
 					}
 					else
 					{
-						sti.Members.Add(new Pair(XamlMember.FromUnknown(p.Key, r.NamespaceURI, xt), p.Value));
+						sti.Members.Add(new Pair(XamlMember.FromUnknown(aname, r.NamespaceURI, xt), p.Value));
 					}
 				}
 			}
