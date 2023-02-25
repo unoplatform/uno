42bd63d7337a7c146fa68f8b6d7ac6ca7829069f
Martin Zikmund <martin@zikmund.dev>
2023-02-25 18:24:48 +0100

    test: AutoSuggestBox positioning tests

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_AutoSuggestBox.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_AutoSuggestBox.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MUXControlsTestApp.Utilities;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 using static Private.Infrastructure.TestServices;
 
 namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
@@ -133,6 +136,75 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 				SUT.Focus(FocusState.Programmatic);
 				SUT.Text = "ab";
 				await WindowHelper.WaitForIdle();
+			}
+			finally
+			{
+				button?.Focus(FocusState.Programmatic); // Unfocus the AutoSuggestBox to ensure popup is closed.
+				await WindowHelper.WaitForIdle();
+			}
+		}
+
+		[TestMethod]
+		[RequiresFullWindow]
+		public async Task When_Popup_Above()
+		{
+			await When_Popup_Position(VerticalAlignment.Bottom, (SUT, popup) =>
+			{
+				var popupPoint = popup.Child.TransformToVisual(WindowHelper.WindowContent).TransformPoint(default);
+				var suggestBoxPoint = SUT.TransformToVisual(WindowHelper.WindowContent).TransformPoint(default);
+				Assert.IsTrue(popupPoint.Y + popup.Child.ActualSize.Y <= suggestBoxPoint.Y + 1); // Added 1 to adjust for border on Windows
+			});
+		}
+
+		[TestMethod]
+		[RequiresFullWindow]
+		public async Task When_Popup_Below()
+		{
+			await When_Popup_Position(VerticalAlignment.Top, (SUT, popup) =>
+			{
+				var popupPoint = popup.Child.TransformToVisual(WindowHelper.WindowContent).TransformPoint(default);
+				var suggestBoxPoint = SUT.TransformToVisual(WindowHelper.WindowContent).TransformPoint(default);
+				Assert.IsTrue(popupPoint.Y + 1 >= suggestBoxPoint.Y + SUT.ActualHeight); // Added 1 to adjust for border on Windows
+			});
+		}
+
+		private async Task When_Popup_Position(VerticalAlignment verticalAlignment, Action<AutoSuggestBox, Popup> assert)
+		{
+			var SUT = new AutoSuggestBox();
+
+			Button button = null;
+			try
+			{
+				button = new Button();
+				var rootGrid = new Grid();
+				var stack = new StackPanel()
+				{
+					Children =
+					{
+						button,
+						SUT
+					},
+					VerticalAlignment = verticalAlignment
+				};
+
+				SUT.ItemsSource = Enumerable.Range(0, 10).ToArray();
+				rootGrid.Children.Add(stack);
+				WindowHelper.WindowContent = rootGrid;
+				await WindowHelper.WaitForIdle();
+
+				SUT.Focus(FocusState.Programmatic);
+				SUT.Text = "1";
+				await WindowHelper.WaitForIdle();
+
+				var popups = VisualTreeHelper.GetOpenPopupsForXamlRoot(SUT.XamlRoot);
+				var popup = popups[0];
+
+				Assert.IsNotNull(popup);
+				var child = popup.Child;
+				Assert.IsNotNull(child);
+				await WindowHelper.WaitFor(() => child.ActualSize.Y > 0);
+
+				assert(SUT, popup);
 			}
 			finally
 			{
