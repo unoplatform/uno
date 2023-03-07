35e4cf86386c89d4e17af0ee4acca7de2b109160
Youssef Victor <youssefvictor00@gmail.com>
2023-03-07 14:54:19 +0200

    chore: Fix build

--- src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlConstants.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlConstants.cs
@@ -101,6 +101,7 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 			// Misc
 			public const string Setter = BaseXamlNamespace + ".Setter";
 			public const string CornerRadius = BaseXamlNamespace + ".CornerRadius";
+			public const string SolidColorBrushHelper = BaseXamlNamespace + ".SolidColorBrushHelper";
 			public const string GridLength = BaseXamlNamespace + ".GridLength";
 			public const string GridUnitType = BaseXamlNamespace + ".GridUnitType";
 			public const string Color = RootWUINamespace + ".Color";
