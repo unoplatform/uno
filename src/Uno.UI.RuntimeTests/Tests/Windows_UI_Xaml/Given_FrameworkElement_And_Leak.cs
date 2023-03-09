1ec4bd5a4fc764eb35cd32803913f10ea25e869c
Martin Zikmund <martin@zikmund.dev>
2023-03-09 16:00:51 +0100

    fix: Avoid Background memory leak on Android

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_FrameworkElement_And_Leak.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_FrameworkElement_And_Leak.cs
@@ -8,16 +8,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Threading.Tasks;
-#if !HAS_UNO_WINUI
-using Microsoft.UI.Xaml.Controls;
-#endif
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Private.Infrastructure;
 using Uno.Extensions;
-using Uno.UI.Extensions;
 using Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml.Controls;
 using Windows.UI;
 using Windows.UI.Core;
@@ -369,9 +362,7 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml
 			await TestServices.WindowHelper.WaitForLoaded(lastButton);
 			await TestServices.WindowHelper.WaitForIdle();
 
-			Assert.IsNotNull(property.CallbackManager);
-
-			Assert.AreEqual(originalCount, property.CallbackManager.CallbacksCount);
+			Assert.AreEqual(originalCount, property.CallbackManager?.CallbacksCount ?? 0);
 		}
 
 		private class Holder
