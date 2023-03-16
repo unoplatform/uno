5d7817f4f606ab332ea46ac5b51b5d148a6773c1
Jérôme Laban <jlaban@gmail.com>
2023-03-15 20:30:29 -0400

    docs: Adjust imports

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -3,17 +3,10 @@ Set-PSDebug -Trace 1
 $external_docs =
 @(
     @("https://github.com/unoplatform/uno.wasm.bootstrap", "uno.wasm.bootstrap", "4abadfc93ffeddc82420cc28af04cd7f6b2693ab"),
-<<<<<<< HEAD
-    @("https://github.com/unoplatform/uno.themes", "uno.themes", "1d825c6fb599aeaffc52229d102949a3b7452b94"),
+    @("https://github.com/unoplatform/uno.themes", "uno.themes", "3763223a6337c318743879cda31eb9c226327e7f"),
     @("https://github.com/unoplatform/uno.toolkit.ui", "uno.toolkit.ui", "3edaf9fb29e95a8c4a24b5b43d273dd85bc1fa57"),
-    @("https://github.com/unoplatform/uno.check", "uno.check", "e1b260c2e112b7dd92ed77ff46020dd265cd6b90"),
-    @("https://github.com/unoplatform/uno.xamlmerge.task", "uno.xamlmerge.task", "a6d2efa69e24e8280c38300b5c1b7a8f2033f9f9"),
-=======
-    @("https://github.com/unoplatform/uno.themes", "uno.themes", "3e8e7dc8e816379d335cd13d11bd90227c7fcc44"),
-    @("https://github.com/unoplatform/uno.toolkit.ui", "uno.toolkit.ui", "d512cc88a541b2da858e7013b81d8a1b83d89272"),
     @("https://github.com/unoplatform/uno.check", "uno.check", "5dec33b3cb4c26f578c8d6bd7a84000bf265a14e"),
     @("https://github.com/unoplatform/uno.xamlmerge.task", "uno.xamlmerge.task", "7e8ffef206e87dfea90c53805c45e93a7d8c0b46"),
->>>>>>> ece6592b9f (docs: Update xamlmerge and uno.check)
     @("https://github.com/unoplatform/figma-docs", "figma-docs", "a740582020509f9947fbf991628075a4717bff0a"),
     @("https://github.com/unoplatform/uno.extensions", "uno.extensions", "b9c1b66ff45776b0bc9c5a2cb588fe3451046d1e")
 )
