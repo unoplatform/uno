6a72446830dcb9411ed7da3bdc101152cfee6fc0
Youssef Victor <youssefvictor00@gmail.com>
2023-03-16 10:04:06 +0200

    test: Add Height/Width test for ProgressRing

---
+++ src/Uno.UI.RuntimeTests/MUX/Microsoft_UI_Xaml_Controls/ProgressRing/ProgressRingTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using MUXControlsTestApp.Utilities;
+using Private.Infrastructure;
+using Uno.UI.RuntimeTests.Helpers;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.RuntimeTests.MUX.Microsoft_UI_Xaml_Controls.ProgressRingTests;
+
+[TestClass]
+public class ProgressRingTests
+{
+	[TestMethod]
+	[DataRow(true)]
+	[DataRow(false)]
+	public async Task ProgressRingDefaultHeightShouldBe32(bool useFluent)
+	{
+		using (useFluent ? StyleHelper.UseFluentStyles() : null)
+		{
+			var grid = new Grid();
+			grid.Width = 100;
+			grid.Height = 100;
+			var progressRing = new Microsoft.UI.Xaml.Controls.ProgressRing() { IsActive = true };
+			grid.Children.Add(progressRing);
+			RunOnUIThread.Execute(() => TestServices.WindowHelper.WindowContent = grid);
+
+			await TestServices.WindowHelper.WaitForLoaded(progressRing);
+
+			Assert.AreEqual(32, progressRing.ActualHeight);
+			Assert.AreEqual(32, progressRing.ActualWidth);
+		}
+	}
+}
