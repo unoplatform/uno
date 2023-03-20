b338eff8b0f08bb2b6c91f1734f7cea80b45fca1
Jérôme Laban <jerome@platform.uno>
2023-03-20 13:41:11 -0400

    docs: Update themes docs

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -3,7 +3,7 @@ Set-PSDebug -Trace 1
 $external_docs =
 @(
     @("https://github.com/unoplatform/uno.wasm.bootstrap", "uno.wasm.bootstrap", "4abadfc93ffeddc82420cc28af04cd7f6b2693ab"),
-    @("https://github.com/unoplatform/uno.themes", "uno.themes", "3763223a6337c318743879cda31eb9c226327e7f"),
+    @("https://github.com/unoplatform/uno.themes", "uno.themes", "3d12f341f3ce9ecd7738e163a3a0904e9b94466f"),
     @("https://github.com/unoplatform/uno.toolkit.ui", "uno.toolkit.ui", "e6bd3390e8b21ab649294ee591d7f134f8d56197"),
     @("https://github.com/unoplatform/uno.check", "uno.check", "5dec33b3cb4c26f578c8d6bd7a84000bf265a14e"),
     @("https://github.com/unoplatform/uno.xamlmerge.task", "uno.xamlmerge.task", "7e8ffef206e87dfea90c53805c45e93a7d8c0b46"),
