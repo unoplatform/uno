3f565437b3da1cd33b6cb2fd9038d3521742db2b
Martin Zikmund <martin@zikmund.dev>
2023-03-30 12:16:58 +0200

    test: WebMessageReceived runtime test support for iOS/macOS

--- src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_WebView2.cs
+++ src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_WebView2.cs
@@ -49,42 +49,43 @@ public class Given_WebView2
 		webView.Height = 200;
 		border.Child = webView;
 		TestServices.WindowHelper.WindowContent = border;
-		bool navigated = false;
 		await TestServices.WindowHelper.WaitForLoaded(border);
 		await webView.EnsureCoreWebView2Async();
-		webView.NavigationCompleted += (sender, e) => navigated = true;
-		webView.NavigateToString(
-			"""
-			<html>
-				<body>
-					<script type="text/javascript">
-						function sendMessage(){
-							try{
-								const message = {"some": ['values',"in","json",1]};
-										
-								if (typeof chrome !== 'undefined' &&
-									chrome?.webview?.postMessage){
-									chrome.webview.postMessage(message);
-								} else if (unoWebMessageHandler){
-									unoWebMessageHandler?.postMessage(JSON.stringify(message));
-								}
-							}
-							catch (ex){
-								alert("wtf" + ex);
-							}
-						}
-					</script>
-					<div id='test' style='width: 100px; height: 100px; background-color: blue;' />
-				</body>
-			</html>
-			""");
-		await TestServices.WindowHelper.WaitFor(() => navigated);
+
 		string message = null;
 		webView.WebMessageReceived += (s, e) =>
 		{
 			message = e.WebMessageAsJson;
 		};
-		await webView.ExecuteScriptAsync("sendMessage()");
+
+		webView.NavigateToString(
+			"""
+			<html>
+				<head>
+					<script type="text/javascript">
+						function sendMessage(){
+							try{
+								const message = {"some": ['values',"in","json",1]};
+										
+								if (window.hasOwnProperty("chrome") && chrome?.webview?.postMessage){
+									chrome.webview.postMessage(message);
+								} else if (window.hasOwnProperty("unoWebMessageHandler")){
+									unoWebMessageHandler?.postMessage(JSON.stringify(message));
+								} else if (window.hasOwnProperty("webkit") && webkit?.messageHandlers?.unoWebView?.postMessage){
+									webkit.messageHandlers.unoWebView.postMessage(JSON.stringify(message));
+								}
+							}
+							catch (ex){
+								alert("Error occurred: " + ex);
+							}
+						}
+					</script>
+				</head>
+				<body onload="sendMessage();">
+					<div id='test' style='width: 100px; height: 100px; background-color: blue;' />
+				</body>
+			</html>
+			""");
 
 		await TestServices.WindowHelper.WaitFor(() => message is not null);
 
