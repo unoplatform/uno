96eaaca7d74d9dfe7a6f1bbc80dec7f831e8ac4f
Martin Zikmund <martin@zikmund.dev>
2023-03-30 15:42:30 +0200

    test: VisualCollection changes should influence IsChildrenRenderOrderDirty

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Composition/Given_ContainerVisual.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Private.Infrastructure;
+using Windows.UI.Composition;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Composition;
+
+[TestClass]
+public class Given_ContainerVisual
+{
+#if __SKIA__
+	[TestMethod]
+	[RunsOnUIThread]
+	public void When_Children_Change()
+	{
+		var compositor = Windows.UI.Xaml.Window.Current.Compositor;
+		var containerVisual = compositor.CreateContainerVisual();
+		Assert.IsFalse(containerVisual.IsChildrenRenderOrderDirty);
+
+		var shape = compositor.CreateShapeVisual();
+		containerVisual.Children.InsertAtTop(shape);
+		Assert.IsTrue(containerVisual.IsChildrenRenderOrderDirty);
+		var children = containerVisual.GetChildrenInRenderOrder();
+		Assert.IsFalse(containerVisual.IsChildrenRenderOrderDirty);
+		Assert.AreEqual(1, children.Count);
+
+		containerVisual.Children.InsertAtTop(compositor.CreateShapeVisual());
+		Assert.IsTrue(containerVisual.IsChildrenRenderOrderDirty);
+		children = containerVisual.GetChildrenInRenderOrder();
+		Assert.IsFalse(containerVisual.IsChildrenRenderOrderDirty);
+		Assert.AreEqual(2, children.Count);
+
+		containerVisual.Children.Remove(shape);
+		Assert.IsTrue(containerVisual.IsChildrenRenderOrderDirty);
+		children = containerVisual.GetChildrenInRenderOrder();
+		Assert.IsFalse(containerVisual.IsChildrenRenderOrderDirty);
+		Assert.AreEqual(1, children.Count);
+	}
+#endif
+}
