3a3c143b0a89f93945b29b1f9ecfa757a44acfbe
Martin Zikmund <martin@zikmund.dev>
2023-03-30 16:16:25 +0200

    chore: Adjust compatibility with non-Skia targets

--- src/Uno.UI.Composition/Composition/ContainerVisual.cs
+++ src/Uno.UI.Composition/Composition/ContainerVisual.cs
@@ -2,21 +2,19 @@
 
 using System;
 
-namespace Windows.UI.Composition
+namespace Windows.UI.Composition;
+
+public partial class ContainerVisual : Visual
 {
-	public partial class ContainerVisual : Visual
+	internal ContainerVisual() : base(null!) => throw new NotSupportedException("Use the ctor with Compositor");
+
+	internal ContainerVisual(Compositor compositor) : base(compositor)
 	{
-		internal ContainerVisual() : base(null!) => throw new NotSupportedException("Use the ctor with Compositor");
-
-		internal ContainerVisual(Compositor compositor) : base(compositor)
-		{
-			Children = new VisualCollection(compositor, this);
-			Children.CollectionChanged += Children_CollectionChanged;
-		}
-
-		public VisualCollection Children { get; }
-
-		private void Children_CollectionChanged(object? sender, EventArgs e) =>
-			IsChildrenRenderOrderDirty = true;
+		Children = new VisualCollection(compositor, this);
+		InitializePartial();
 	}
+
+	partial void InitializePartial();
+
+	public VisualCollection Children { get; }
 }
