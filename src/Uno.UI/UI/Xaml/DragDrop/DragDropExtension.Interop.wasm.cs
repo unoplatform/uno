f735d89681ca3f1073265cf1dde0bbe4f95e5d25
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2023-04-04 15:49:03 -0400

    perf: Add RegisterNoOp binding

---
+++ src/Uno.UI/UI/Xaml/DragDrop/DragDropExtension.Interop.wasm.cs
@@ -0,0 +1,15 @@
+﻿#if NET7_0_OR_GREATER
+using System.Runtime.InteropServices.JavaScript;
+
+namespace __Windows.ApplicationModel.DataTransfer.DragDrop.Core
+{
+	internal partial class DragDropExtension
+	{
+		internal static partial class NativeMethods
+		{
+			[JSImport("globalThis.Windows.ApplicationModel.DataTransfer.DragDrop.Core.DragDropExtension.registerNoOp")]
+			internal static partial void RegisterNoOp();
+		}
+	}
+}
+#endif
