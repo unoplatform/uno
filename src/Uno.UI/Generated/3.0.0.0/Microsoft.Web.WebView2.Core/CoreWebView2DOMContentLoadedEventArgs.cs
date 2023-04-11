b008f9f36e78ed20ee709957cc35e3c3cd8ca4d6
Martin Zikmund <martin@zikmund.dev>
2023-04-11 20:18:44 +0200

    chore: Run sync generator

--- src/Uno.UI/Generated/3.0.0.0/Microsoft.Web.WebView2.Core/CoreWebView2DOMContentLoadedEventArgs.cs
+++ src/Uno.UI/Generated/3.0.0.0/Microsoft.Web.WebView2.Core/CoreWebView2DOMContentLoadedEventArgs.cs
@@ -2,12 +2,21 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Microsoft.Web.WebView2.Core
 {
-	#if false || false || false || false || false || false || false
+	#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
 	[global::Uno.NotImplemented]
 	#endif
 	public  partial class CoreWebView2DOMContentLoadedEventArgs 
 	{
-		// Skipping already declared property NavigationId
+		#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
+		public  ulong NavigationId
+		{
+			get
+			{
+				throw new global::System.NotImplementedException("The member ulong CoreWebView2DOMContentLoadedEventArgs.NavigationId is not implemented. For more information, visit https://aka.platform.uno/notimplemented?m=ulong%20CoreWebView2DOMContentLoadedEventArgs.NavigationId");
+			}
+		}
+		#endif
 		// Forced skipping of method Microsoft.Web.WebView2.Core.CoreWebView2DOMContentLoadedEventArgs.NavigationId.get
 	}
 }
