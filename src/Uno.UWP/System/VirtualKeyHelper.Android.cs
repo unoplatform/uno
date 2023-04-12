f2f5b0786b60434377c6256e7f1840474eede758
Martin Zikmund <martin@zikmund.dev>
2023-04-12 17:03:32 +0200

    refactor: VirtualKeyHelper to switch statement, remove obsolete converter

--- src/Uno.UWP/System/VirtualKeyHelper.Android.cs
+++ src/Uno.UWP/System/VirtualKeyHelper.Android.cs
@@ -3,164 +3,167 @@
 // https://lists.w3.org/Archives/Public/www-dom/2010JulSep/att-0182/keyCode-spec.html
 
 using Android.Views;
+using Uno.Foundation.Logging;
+using Windows.UI.Core;
 
 namespace Windows.System;
 
 internal static class VirtualKeyHelper
 {
-	//TODO: Add remaining keycodes
 	public static VirtualKey FromKeyCode(Keycode key)
-		=> key switch
+	{
+		switch (key)
 		{
-			Keycode.Num0 => VirtualKey.Number0,
-			Keycode.Num1 => VirtualKey.Number1,
-			Keycode.Num2 => VirtualKey.Number2,
-			Keycode.Num3 => VirtualKey.Number3,
-			Keycode.Num4 => VirtualKey.Number4,
-			Keycode.Num5 => VirtualKey.Number5,
-			Keycode.Num6 => VirtualKey.Number6,
-			Keycode.Num7 => VirtualKey.Number7,
-			Keycode.Num8 => VirtualKey.Number8,
-			Keycode.Num9 => VirtualKey.Number9,
+			case Keycode.Num0: return VirtualKey.Number0;
+			case Keycode.Num1: return VirtualKey.Number1;
+			case Keycode.Num2: return VirtualKey.Number2;
+			case Keycode.Num3: return VirtualKey.Number3;
+			case Keycode.Num4: return VirtualKey.Number4;
+			case Keycode.Num5: return VirtualKey.Number5;
+			case Keycode.Num6: return VirtualKey.Number6;
+			case Keycode.Num7: return VirtualKey.Number7;
+			case Keycode.Num8: return VirtualKey.Number8;
+			case Keycode.Num9: return VirtualKey.Number9;
 
-			Keycode.Numpad0 => VirtualKey.NumberPad0,
-			Keycode.Numpad1 => VirtualKey.NumberPad1,
-			Keycode.Numpad2 => VirtualKey.NumberPad2,
-			Keycode.Numpad3 => VirtualKey.NumberPad3,
-			Keycode.Numpad4 => VirtualKey.NumberPad4,
-			Keycode.Numpad5 => VirtualKey.NumberPad5,
-			Keycode.Numpad6 => VirtualKey.NumberPad6,
-			Keycode.Numpad7 => VirtualKey.NumberPad7,
-			Keycode.Numpad8 => VirtualKey.NumberPad8,
-			Keycode.Numpad9 => VirtualKey.NumberPad9,
+			case Keycode.Numpad0: return VirtualKey.NumberPad0;
+			case Keycode.Numpad1: return VirtualKey.NumberPad1;
+			case Keycode.Numpad2: return VirtualKey.NumberPad2;
+			case Keycode.Numpad3: return VirtualKey.NumberPad3;
+			case Keycode.Numpad4: return VirtualKey.NumberPad4;
+			case Keycode.Numpad5: return VirtualKey.NumberPad5;
+			case Keycode.Numpad6: return VirtualKey.NumberPad6;
+			case Keycode.Numpad7: return VirtualKey.NumberPad7;
+			case Keycode.Numpad8: return VirtualKey.NumberPad8;
+			case Keycode.Numpad9: return VirtualKey.NumberPad9;
 
-			Keycode.A => VirtualKey.A,
-			Keycode.B => VirtualKey.B,
-			Keycode.C => VirtualKey.C,
-			Keycode.D => VirtualKey.D,
-			Keycode.E => VirtualKey.E,
-			Keycode.F => VirtualKey.F,
-			Keycode.G => VirtualKey.G,
-			Keycode.H => VirtualKey.H,
-			Keycode.I => VirtualKey.I,
-			Keycode.J => VirtualKey.J,
-			Keycode.K => VirtualKey.K,
-			Keycode.L => VirtualKey.L,
-			Keycode.M => VirtualKey.M,
-			Keycode.N => VirtualKey.N,
-			Keycode.O => VirtualKey.O,
-			Keycode.P => VirtualKey.P,
-			Keycode.Q => VirtualKey.Q,
-			Keycode.R => VirtualKey.R,
-			Keycode.S => VirtualKey.S,
-			Keycode.T => VirtualKey.T,
-			Keycode.U => VirtualKey.U,
-			Keycode.V => VirtualKey.V,
-			Keycode.W => VirtualKey.W,
-			Keycode.X => VirtualKey.X,
-			Keycode.Y => VirtualKey.Y,
-			Keycode.Z => VirtualKey.Z,
+			case Keycode.A: return VirtualKey.A;
+			case Keycode.B: return VirtualKey.B;
+			case Keycode.C: return VirtualKey.C;
+			case Keycode.D: return VirtualKey.D;
+			case Keycode.E: return VirtualKey.E;
+			case Keycode.F: return VirtualKey.F;
+			case Keycode.G: return VirtualKey.G;
+			case Keycode.H: return VirtualKey.H;
+			case Keycode.I: return VirtualKey.I;
+			case Keycode.J: return VirtualKey.J;
+			case Keycode.K: return VirtualKey.K;
+			case Keycode.L: return VirtualKey.L;
+			case Keycode.M: return VirtualKey.M;
+			case Keycode.N: return VirtualKey.N;
+			case Keycode.O: return VirtualKey.O;
+			case Keycode.P: return VirtualKey.P;
+			case Keycode.Q: return VirtualKey.Q;
+			case Keycode.R: return VirtualKey.R;
+			case Keycode.S: return VirtualKey.S;
+			case Keycode.T: return VirtualKey.T;
+			case Keycode.U: return VirtualKey.U;
+			case Keycode.V: return VirtualKey.V;
+			case Keycode.W: return VirtualKey.W;
+			case Keycode.X: return VirtualKey.X;
+			case Keycode.Y: return VirtualKey.Y;
+			case Keycode.Z: return VirtualKey.Z;
 
-			Keycode.Comma => (VirtualKey)188,
-			Keycode.Period => (VirtualKey)190,
-			Keycode.Equals or Keycode.NumpadEquals => (VirtualKey)187,
-			Keycode.NumpadDot or Keycode.NumpadComma => VirtualKey.Decimal,
-			Keycode.NumpadDivide => VirtualKey.Divide,
-			Keycode.NumpadSubtract => VirtualKey.Subtract,
+			case Keycode.Comma: return (VirtualKey)188;
+			case Keycode.Period: return (VirtualKey)190;
+			case Keycode.Equals or Keycode.NumpadEquals: return (VirtualKey)187;
+			case Keycode.NumpadDot or Keycode.NumpadComma: return VirtualKey.Decimal;
+			case Keycode.NumpadDivide: return VirtualKey.Divide;
+			case Keycode.NumpadSubtract: return VirtualKey.Subtract;
 
-			Keycode.NumpadMultiply => VirtualKey.Multiply,
-			Keycode.NumpadAdd => VirtualKey.Add,
-			Keycode.Enter => VirtualKey.Enter,
-			Keycode.NumpadEnter => VirtualKey.Enter,
-			Keycode.Clear => VirtualKey.Clear,
+			case Keycode.NumpadMultiply: return VirtualKey.Multiply;
+			case Keycode.NumpadAdd: return VirtualKey.Add;
+			case Keycode.Enter: return VirtualKey.Enter;
+			case Keycode.NumpadEnter: return VirtualKey.Enter;
+			case Keycode.Clear: return VirtualKey.Clear;
 
-			Keycode.Space => VirtualKey.Space,
-			Keycode.Tab => VirtualKey.Tab,
-			Keycode.Del => VirtualKey.Back,
-			Keycode.ForwardDel => VirtualKey.Delete,
+			case Keycode.Space: return VirtualKey.Space;
+			case Keycode.Tab: return VirtualKey.Tab;
+			case Keycode.Del: return VirtualKey.Back;
+			case Keycode.ForwardDel: return VirtualKey.Delete;
 
 			// Modifiers
-			Keycode.Escape => VirtualKey.Escape,
-			Keycode.MetaLeft => VirtualKey.LeftWindows,
-			//Keycode.KeyboardRightGui => VirtualKey.RightWindows, // Command
-			//Keycode.KeyboardLeftShift => VirtualKey.Shift, // Left Shift
-			//Keycode.KeyboardCapsLock => VirtualKey.CapitalLock,
-			//Keycode.KeyboardLeftAlt => VirtualKey.LeftMenu,
-			//Keycode.KeyboardLeftControl => VirtualKey.Control, // Left control
-			//Keycode.KeyboardRightShift => VirtualKey.RightShift,
-			//Keycode.KeyboardRightAlt => VirtualKey.RightMenu, // Right option, a.k.a. Right Alt
-			//Keycode.KeyboardRightControl => VirtualKey.RightControl,
+			case Keycode.Escape: return VirtualKey.Escape;
+			case Keycode.MetaLeft: return VirtualKey.LeftWindows;
 
 			// Functions
-			Keycode.F1 => VirtualKey.F1,
-			Keycode.F2 => VirtualKey.F2,
-			Keycode.F3 => VirtualKey.F3,
-			Keycode.F4 => VirtualKey.F4,
-			Keycode.F5 => VirtualKey.F5,
-			Keycode.F6 => VirtualKey.F6,
-			Keycode.F7 => VirtualKey.F7,
-			Keycode.F8 => VirtualKey.F8,
-			Keycode.F9 => VirtualKey.F9,
-			Keycode.F10 => VirtualKey.F10,
-			Keycode.F11 => VirtualKey.F11,
-			Keycode.F12 => VirtualKey.F12,
+			case Keycode.F1: return VirtualKey.F1;
+			case Keycode.F2: return VirtualKey.F2;
+			case Keycode.F3: return VirtualKey.F3;
+			case Keycode.F4: return VirtualKey.F4;
+			case Keycode.F5: return VirtualKey.F5;
+			case Keycode.F6: return VirtualKey.F6;
+			case Keycode.F7: return VirtualKey.F7;
+			case Keycode.F8: return VirtualKey.F8;
+			case Keycode.F9: return VirtualKey.F9;
+			case Keycode.F10: return VirtualKey.F10;
+			case Keycode.F11: return VirtualKey.F11;
+			case Keycode.F12: return VirtualKey.F12;
 
-			Keycode.Semicolon => (VirtualKey)186,
+			case Keycode.Semicolon: return (VirtualKey)186;
 
 			// Navigation
-			Keycode.Help => VirtualKey.Help,
-			Keycode.NumLock => VirtualKey.NumberKeyLock,
-			Keycode.ScrollLock => VirtualKey.Scroll,
+			case Keycode.Help: return VirtualKey.Help;
+			case Keycode.NumLock: return VirtualKey.NumberKeyLock;
+			case Keycode.ScrollLock: return VirtualKey.Scroll;
 
-			Keycode.Search => VirtualKey.Search,
-			Keycode.Insert => VirtualKey.Insert,
-			Keycode.MoveHome => VirtualKey.Home,
-			Keycode.MoveEnd => VirtualKey.End,
-			Keycode.PageUp => VirtualKey.PageUp,
-			Keycode.PageDown => VirtualKey.PageDown,
-			Keycode.AppSwitch => VirtualKey.Application, // TODO: Verify mapping
+			case Keycode.Search: return VirtualKey.Search;
+			case Keycode.Insert: return VirtualKey.Insert;
+			case Keycode.MoveHome: return VirtualKey.Home;
+			case Keycode.MoveEnd: return VirtualKey.End;
+			case Keycode.PageUp: return VirtualKey.PageUp;
+			case Keycode.PageDown: return VirtualKey.PageDown;
+			case Keycode.AppSwitch: return VirtualKey.Application;
 
-			Keycode.CtrlLeft => VirtualKey.LeftControl,
-			Keycode.CtrlRight => VirtualKey.RightControl,
-			Keycode.ShiftLeft => VirtualKey.LeftShift,
-			Keycode.ShiftRight => VirtualKey.RightShift,
+			case Keycode.CtrlLeft: return VirtualKey.LeftControl;
+			case Keycode.CtrlRight: return VirtualKey.RightControl;
+			case Keycode.ShiftLeft: return VirtualKey.LeftShift;
+			case Keycode.ShiftRight: return VirtualKey.RightShift;
 
-			Keycode.SystemNavigationUp => VirtualKey.Up,
-			Keycode.SystemNavigationDown => VirtualKey.Down,
-			Keycode.SystemNavigationLeft => VirtualKey.Left,
-			Keycode.SystemNavigationRight => VirtualKey.Right,
+			case Keycode.SystemNavigationUp: return VirtualKey.Up;
+			case Keycode.SystemNavigationDown: return VirtualKey.Down;
+			case Keycode.SystemNavigationLeft: return VirtualKey.Left;
+			case Keycode.SystemNavigationRight: return VirtualKey.Right;
 
-			Keycode.DpadUp => VirtualKey.GamepadDPadUp,
-			Keycode.DpadDown => VirtualKey.GamepadDPadDown,
-			Keycode.DpadLeft => VirtualKey.GamepadDPadLeft,
-			Keycode.DpadRight => VirtualKey.GamepadDPadRight,
-			Keycode.DpadCenter => VirtualKey.GamepadA,
+			case Keycode.DpadUp: return VirtualKey.GamepadDPadUp;
+			case Keycode.DpadDown: return VirtualKey.GamepadDPadDown;
+			case Keycode.DpadLeft: return VirtualKey.GamepadDPadLeft;
+			case Keycode.DpadRight: return VirtualKey.GamepadDPadRight;
+			case Keycode.DpadCenter: return VirtualKey.GamepadA;
 
 			// Android TV Remote
-			Keycode.Bookmark => VirtualKey.Favorites,
-			Keycode.Menu => VirtualKey.Menu,
-			Keycode.Back => VirtualKey.GoBack,
-			Keycode.Home => VirtualKey.GoHome,
-			Keycode.Forward => VirtualKey.GoForward,
+			case Keycode.Bookmark: return VirtualKey.Favorites;
+			case Keycode.Menu: return VirtualKey.Menu;
+			case Keycode.Back: return VirtualKey.GoBack;
+			case Keycode.Home: return VirtualKey.GoHome;
+			case Keycode.Forward: return VirtualKey.GoForward;
 
-			Keycode.VolumeMute => (VirtualKey)173,
-			Keycode.VolumeDown => (VirtualKey)174,
-			Keycode.VolumeUp => (VirtualKey)175,
-			Keycode.MediaNext => (VirtualKey)176,
-			Keycode.MediaPrevious => (VirtualKey)177,
-			Keycode.MediaStop => (VirtualKey)178,
-			Keycode.MediaPlayPause => (VirtualKey)179,
-			Keycode.MediaPause => (VirtualKey)179,
-			Keycode.MediaPlay => (VirtualKey)250,
+			case Keycode.VolumeMute: return (VirtualKey)173;
+			case Keycode.VolumeDown: return (VirtualKey)174;
+			case Keycode.VolumeUp: return (VirtualKey)175;
+			case Keycode.MediaNext: return (VirtualKey)176;
+			case Keycode.MediaPrevious: return (VirtualKey)177;
+			case Keycode.MediaStop: return (VirtualKey)178;
+			case Keycode.MediaPlayPause: return (VirtualKey)179;
+			case Keycode.MediaPause: return (VirtualKey)179;
+			case Keycode.MediaPlay: return (VirtualKey)250;
 
-			Keycode.CapsLock => VirtualKey.CapitalLock,
+			case Keycode.CapsLock: return VirtualKey.CapitalLock;
 #if __ANDROID_16__
-			Keycode.Kana => VirtualKey.Kana,
+			case Keycode.Kana: return VirtualKey.Kana;
 #endif
 #if __ANDROID_20__
-			Keycode.Sleep => VirtualKey.Sleep,
-#endif
+			case Keycode.Sleep: return VirtualKey.Sleep;
 
-			_ => VirtualKey.None
-		};
+#endif
+			default:
+				{
+					if (typeof(CoreWindow).Log().IsEnabled(LogLevel.Warning))
+					{
+						typeof(CoreWindow).Log().Warn($"Key pressed but not mapped : " + key);
+					}
+
+					return VirtualKey.None;
+				}
+		}
+	}
 }
