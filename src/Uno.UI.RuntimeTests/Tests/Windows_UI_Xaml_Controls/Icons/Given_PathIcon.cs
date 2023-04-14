80119b1486f1d3e384740cb19e735dc4822ce829
Martin Zikmund <martin@zikmund.dev>
2023-04-14 10:58:31 +0200

    test: PathIcon runtime tests

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Icons/Given_PathIcon.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Icons/Given_PathIcon.cs
@@ -1,12 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using MUXControlsTestApp.Utilities;
+using Private.Infrastructure;
+using Uno.UI.RuntimeTests.Helpers;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
 
-namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls.Icons
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls.Icons;
+
+[TestClass]
+[RunsOnUIThread]
+public class Given_PathIcon
 {
-	internal class Given_PathIcon
+	[TestMethod]
+	public async Task When_Themed()
 	{
+		var textBlock = new TextBlock() { Text = "test" };
+		var ellipseGeometry = new EllipseGeometry()
+		{
+			RadiusX = 20,
+			RadiusY = 20,
+		};
+		var pathIcon = new PathIcon() { Data = ellipseGeometry };
+		var stackPanel = new StackPanel()
+		{
+			Children =
+			{
+				textBlock,
+				pathIcon
+			}
+		};
+		TestServices.WindowHelper.WindowContent = stackPanel;
+		await TestServices.WindowHelper.WaitForLoaded(stackPanel);
+
+		var textBlockBrush = (SolidColorBrush)textBlock.Foreground;
+		var pathIconBrush = (SolidColorBrush)pathIcon.Foreground;
+		Assert.AreEqual(textBlockBrush.Color, pathIconBrush.Color);
+
+		using (ThemeHelper.UseDarkTheme())
+		{
+			textBlockBrush = (SolidColorBrush)textBlock.Foreground;
+			pathIconBrush = (SolidColorBrush)pathIcon.Foreground;
+			Assert.AreEqual(textBlockBrush.Color, pathIconBrush.Color);
+		}
+	}
+
+	[TestMethod]
+	public async Task When_Themed_Path()
+	{
+		var textBlock = new TextBlock() { Text = "test" };
+		var ellipseGeometry = new EllipseGeometry()
+		{
+			RadiusX = 20,
+			RadiusY = 20,
+		};
+		var pathIcon = new PathIcon() { Data = ellipseGeometry };
+		var stackPanel = new StackPanel()
+		{
+			Children =
+			{
+				textBlock,
+				pathIcon
+			}
+		};
+		TestServices.WindowHelper.WindowContent = stackPanel;
+		await TestServices.WindowHelper.WaitForLoaded(stackPanel);
+
+		var innerPath = VisualTreeUtils.FindVisualChildByType<Path>(pathIcon);
+
+		var textBlockBrush = (SolidColorBrush)textBlock.Foreground;
+		var pathIconBrush = (SolidColorBrush)innerPath.Fill;
+		Assert.AreEqual(textBlockBrush.Color, pathIconBrush.Color);
+
+		using (ThemeHelper.UseDarkTheme())
+		{
+			textBlockBrush = (SolidColorBrush)textBlock.Foreground;
+			pathIconBrush = (SolidColorBrush)innerPath.Fill;
+			Assert.AreEqual(textBlockBrush.Color, pathIconBrush.Color);
+		}
+
+		pathIcon.Foreground = new SolidColorBrush(Colors.Red);
+		pathIconBrush = (SolidColorBrush)innerPath.Fill;
+		Assert.AreEqual(Colors.Red, pathIconBrush.Color);
 	}
 }
