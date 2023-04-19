42db9835cb7b9cacb41ec149a84441fa982d2eb9
Jerome Laban <jerome.laban@nventive.com>
2023-04-19 11:19:10 -0400

    ci: restore ios ui tests

--- src/SamplesApp/SamplesApp.Shared/App.xaml.cs
+++ src/SamplesApp/SamplesApp.Shared/App.xaml.cs
@@ -102,7 +102,7 @@ namespace SamplesApp
 #endif
 			override void OnLaunched(LaunchActivatedEventArgs e)
 		{
-#if __IOS__ && !NET6_0_OR_GREATER
+#if __IOS__
 			// requires Xamarin Test Cloud Agent
 			Xamarin.Calabash.Start();
 
