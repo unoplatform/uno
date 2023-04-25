6dff9931c8b421c2343cbdf31777167a99729f8b
Jerome Laban <jerome.laban@nventive.com>
2023-04-25 11:07:18 -0400

    ci: Adjust webview sample

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_WebView.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_WebView.cs
@@ -22,16 +22,16 @@ public class Given_WebView
 	}
 
 #if __ANDROID__ || __IOS__ || __MACOS__
-		[TestMethod]
-		public void When_NavigateWithHttpRequestMessage()
-		{
-			var webView = new WebView();
-			var uri = new Uri("https://bing.com");
-			webView.NavigateWithHttpRequestMessage(new Windows.Web.Http.HttpRequestMessage(Windows.Web.Http.HttpMethod.Get, uri));
-			Assert.IsNotNull(webView.Source);
-			Assert.AreEqual("https://bing.com/", webView.Source.OriginalString);
-			Assert.AreEqual("https://bing.com", uri.OriginalString);
-		}
+	[TestMethod]
+	public void When_NavigateWithHttpRequestMessage()
+	{
+		var webView = new WebView();
+		var uri = new Uri("https://bing.com");
+		webView.NavigateWithHttpRequestMessage(new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, uri));
+		Assert.IsNotNull(webView.Source);
+		Assert.AreEqual("https://bing.com/", webView.Source.OriginalString);
+		Assert.AreEqual("https://bing.com", uri.OriginalString);
+	}
 #endif
 
 	[TestMethod]
