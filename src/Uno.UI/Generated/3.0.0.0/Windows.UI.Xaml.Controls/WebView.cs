d5f223d48f5fe6d9a23a6136e29dbb48633205d8
Jerome Laban <jerome.laban@nventive.com>
2023-04-26 10:18:05 -0400

    ci: Adjust webview for unit tests

--- src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Controls/WebView.cs
+++ src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Controls/WebView.cs
@@ -243,7 +243,7 @@ namespace Windows.UI.Xaml.Controls
 		// Forced skipping of method Windows.UI.Xaml.Controls.WebView.UnsafeContentWarningDisplaying.remove
 		// Forced skipping of method Windows.UI.Xaml.Controls.WebView.UnviewableContentIdentified.add
 		// Forced skipping of method Windows.UI.Xaml.Controls.WebView.UnviewableContentIdentified.remove
-		#if false || false || IS_UNIT_TESTS || false || false || false || false
+		#if false || false || false || false || false || false || false
 		[global::Uno.NotImplemented("IS_UNIT_TESTS", "__NETSTD_REFERENCE__")]
 		public  void NavigateWithHttpRequestMessage( global::Windows.Web.Http.HttpRequestMessage requestMessage)
 		{
