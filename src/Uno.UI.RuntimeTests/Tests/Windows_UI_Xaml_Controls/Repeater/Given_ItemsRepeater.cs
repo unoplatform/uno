342b8a6a62130a3e1205456a26627a029504bbf6
David <github.site@torick.net>
2023-05-01 09:39:38 -0400

    fix(itemsRepeater): Fix regerssion that would prevent nested IR to render items after the first page if scrolling slowly

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Repeater/Given_ItemsRepeater.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Repeater/Given_ItemsRepeater.cs
@@ -115,6 +115,73 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls.Repeater
 
 			sut.Children.Count.Should().BeGreaterThan(1);
 		}
+
+		[TestMethod]
+		[RunsOnUIThread]
+#if __MACOS__
+		[Ignore("Currently fails on macOS, part of #9282 epic")]
+#endif
+		public async Task When_NestedIRSlowlyChangeViewport_Then_MaterializedNeededItems()
+		{
+			const int viewportHeight = 500;
+
+			var sut = default(ItemsRepeater);
+			var sv = new ScrollViewer
+			{
+				Height = viewportHeight,
+				Content = (sut = new ItemsRepeater()
+				{
+					ItemsSource = Enumerable.Range(0, 10).Select(i => $"Group #{i:D2}"),
+					ItemTemplate = new DataTemplate(() => new StackPanel
+					{
+						Children =
+						{
+							new Border
+							{
+								Background = new SolidColorBrush(Colors.DeepPink),
+								Height = 100,
+								Width = 150,
+								Child = new TextBlock().Apply(tb => tb.SetBinding(TextBlock.TextProperty, new Binding()))
+							},
+							new ItemsRepeater
+							{
+								ItemsSource = Enumerable.Range(0, 50).Select(i => $"Item #{i:D2}"),
+								ItemTemplate = new DataTemplate(() => new Border
+								{
+									Width = 150,
+									Height = 100,
+									Background = new SolidColorBrush(Colors.DeepSkyBlue),
+									Child = new TextBlock().Apply(tb => tb.SetBinding(TextBlock.TextProperty, new Binding()))
+								})
+							}
+						}
+					})
+				})
+			};
+
+			TestServices.WindowHelper.WindowContent = sv;
+			await TestServices.WindowHelper.WaitForIdle();
+
+			sv.ChangeView(null, sv.ExtentHeight/2, null, disableAnimation: true);
+			await TestServices.WindowHelper.WaitForIdle();
+
+			var groupView = sut.Children.Single(g => g.DataContext as string == "Group #05");
+			var groupIr = (ItemsRepeater)((StackPanel)groupView).Children[1];
+
+			var beforeVisibleItems = groupIr.Children.Select(i => i.DataContext?.ToString()).Order().ToArray();
+
+			// Scroll by baby step to not be above the threshold which would cause a complete redraw
+			const int step = 10;
+			for (var i = 0; i < viewportHeight * 5; i += step)
+			{
+				sv.ChangeView(null, sv.VerticalOffset + step, null, disableAnimation: true);
+				await TestServices.WindowHelper.WaitForIdle();
+			}
+
+			var afterVisibleItems = groupIr.Children.Select(i => i.DataContext?.ToString()).Order().ToArray();
+
+			afterVisibleItems.Should().NotContain(beforeVisibleItems);
+		}
 #endif
 
 
