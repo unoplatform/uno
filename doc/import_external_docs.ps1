2df5636defe5b4248154091fb0fe25383b06bf45
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2023-05-02 02:45:36 +0100

    chore: Reponding to PR comments

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -1,18 +1,35 @@
+param(
+  [Parameter(ValueFromPipeLineByPropertyName = $true)]
+  $branches = $null
+)
+
 Set-PSDebug -Trace 1
 
-$external_docs =
-@(
-    # use either commit or branch name (latest commit) in 3rd argument
-    @("https://github.com/unoplatform/uno.wasm.bootstrap", "uno.wasm.bootstrap", "main"),
-    @("https://github.com/unoplatform/uno.themes", "uno.themes", "master"),
-    @("https://github.com/unoplatform/uno.toolkit.ui", "uno.toolkit.ui", "main"),
-    @("https://github.com/unoplatform/uno.check", "uno.check", "main"),
-    @("https://github.com/unoplatform/uno.xamlmerge.task", "uno.xamlmerge.task", "main"),
-    @("https://github.com/unoplatform/figma-docs", "figma-docs", "main"),
-    @("https://github.com/unoplatform/uno.resizetizer", "uno.resizetizer", "main"),
-    @("https://github.com/unoplatform/uno.uitest", "uno.uitest", "master"),
-    @("https://github.com/unoplatform/uno.extensions", "uno.extensions", "main")
-)
+$external_docs = @{
+    # use either commit, or branch name to use its latest commit
+    "uno.wasm.bootstrap" = "4abadfc93ffeddc82420cc28af04cd7f6b2693ab"
+    "uno.themes"         = "3d12f341f3ce9ecd7738e163a3a0904e9b94466f"
+    "uno.toolkit.ui"     = "434712b657f479d1329ff60af3b6f22bb6fdb34c"
+    "uno.check"          = "5dec33b3cb4c26f578c8d6bd7a84000bf265a14e"
+    "uno.xamlmerge.task" = "7e8ffef206e87dfea90c53805c45e93a7d8c0b46"
+    "figma-docs"         = "f13d08f2bd7b62fc274b43a4ede4d75909d0f41f"
+    "uno.resizetizer"    = "f650e844cb66a7dafa084a7e778f14f4fd3ce8a2"
+    "uno.uitest"         = "555453c2985ef2745fe44503c5809a6168d063c2"
+    "uno.extensions"     = "539d6b0f2e61fbc2ae5d6e35a77de41cafacf5ce"      
+}
+
+if($branches -ne $null)
+{
+    foreach ($repo in $branches.keys)
+    {
+        $branch = $branches[$repo]
+
+        $external_docs[$repo] = $branch
+    }
+}
+
+echo "Current setup:"
+$external_docs
 
 $ErrorActionPreference = 'Stop'
 
@@ -39,12 +56,14 @@ pushd articles\external
 # ensure long paths are supported on Windows
 git config --global core.longpaths true
 
+$detachedHeadConfig = git config --get advice.detachedHead
+git config advice.detachedHead false
+
 # Heads - Release
-for ($i = 0; $i -lt $external_docs.Length; $i++)
+foreach ($repoPath in $external_docs.keys)
 {
-    $repoUrl=$external_docs[$i][0]
-    $repoPath=$external_docs[$i][1]
-    $repoBranch=$external_docs[$i][2]
+    $repoUrl = "https://github.com/unoplatform/$repoPath"
+    $repoBranch = $external_docs[$repoPath]
         
     if (-Not (Test-Path $repoPath))
     {        
@@ -59,12 +78,18 @@ for ($i = 0; $i -lt $external_docs.Length; $i++)
     git checkout $repoBranch
     Assert-ExitCodeIsZero
 
-    echo "Pulling $repoUrl@$repoBranch..."
-    git pull
-    Assert-ExitCodeIsZero
+    # if not detached
+    if ((git symbolic-ref -q HEAD) -ne $null)
+    {
+        echo "Pulling $repoUrl@$repoBranch..."
+        git pull
+        Assert-ExitCodeIsZero
+    }
 
     popd
 }
+
+git config advice.detachedHead $detachedHeadConfig
 
 popd
 
--- doc/import_external_docs_test.ps1
+++ doc/import_external_docs_test.ps1
@@ -1,4 +1,17 @@
-./import_external_docs
+$external_docs = @{
+    # use either commit, or branch name to use its latest commit
+    "uno.wasm.bootstrap" = "main"
+    "uno.themes"         = "master"
+    "uno.toolkit.ui"     = "main"
+    "uno.check"          = "main"
+    "uno.xamlmerge.task" = "main"
+    "figma-docs"         = "main"
+    "uno.resizetizer"    = "main"
+    "uno.uitest"         = "master"
+    "uno.extensions"     = "main"
+}
+
+./import_external_docs $external_docs
 
 docfx
 
