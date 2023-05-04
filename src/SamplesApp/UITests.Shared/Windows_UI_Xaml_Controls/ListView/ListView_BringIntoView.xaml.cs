cd1b6e7a5fc6c8f4715cc59f263d1bb4fb6dbca9
Andres Pineda <pineda.andres@gmail.com>
2023-05-03 22:10:43 -0400

    test(ListView): Add manual UI Test

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ListView/ListView_BringIntoView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml.Controls;
+using SamplesApp.Windows_UI_Xaml_Controls.Models;
+using Windows.UI.Xaml;
+using Android.Systems;
+
+namespace SamplesApp.Windows_UI_Xaml_Controls.ListView
+{
+	[SampleControlInfo("ListView", "ListView_BringIntoView",
+		typeof(ListViewViewModel),
+		isManualTest: true,
+		description: "Select item scroll and then click on the button")]
+	public sealed partial class ListView_BringIntoView : UserControl
+	{
+		public ListView_BringIntoView()
+		{
+			this.InitializeComponent();
+		}
+
+		public void BringIntoView(object sender, RoutedEventArgs e)
+		{
+			var list = (Windows.UI.Xaml.Controls.ListViewBase)this.myList;
+
+			list.ScrollIntoView(list.SelectedItem, ScrollIntoViewAlignment.Leading);
+		}
+	}
+}
