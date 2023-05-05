96323f16f0d20b365b34d26fb2164b1038909367
Jerome Laban <jerome.laban@nventive.com>
2023-05-04 22:12:08 -0400

    ci: Adjust macos build

--- src/Uno.UI.RuntimeTests/MUX/Microsoft_UI_Xaml_Controls/ProgressRing/ProgressRingTests.cs
+++ src/Uno.UI.RuntimeTests/MUX/Microsoft_UI_Xaml_Controls/ProgressRing/ProgressRingTests.cs
@@ -13,10 +13,7 @@ public class ProgressRingTests
 	[DataRow(true)]
 	[DataRow(false)]
 	[RunsOnUIThread]
-#if __MACOS__
-	[Ignore("Currently fails on macOS, part of #9282 epic")]
-#endif
-#if !(__WASM__ || (__ANDROID__ && !NET6_0_OR_GREATER) || (__IOS__ && !NET6_0_OR_GREATER) || (__MACOS__ && !NET6_0_OR_GREATER) || HAS_SKOTTIE)
+#if !(__WASM__ || (__ANDROID__ && !NET6_0_OR_GREATER) || (__IOS__ && !NET6_0_OR_GREATER) || __MACOS__ || HAS_SKOTTIE)
 	[Ignore("Skottie is not supported on net6+ UWP targets")]
 #endif
 	public async Task ProgressRingDefaultHeightShouldBe32(bool useFluent)
