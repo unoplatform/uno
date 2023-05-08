3197ab4e189772b57d211125b07813e784a67d07
Martin Zikmund <martin@zikmund.dev>
2023-05-08 11:59:12 +0200

    fix: Run VerifyFontFamilyForChevron on UI thread

--- src/Uno.UI.RuntimeTests/MUX/Windows_UI_Xaml_Controls/DropDownButtonTests.cs
+++ src/Uno.UI.RuntimeTests/MUX/Windows_UI_Xaml_Controls/DropDownButtonTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MUXControlsTestApp.Utilities;
 using Private.Infrastructure;
+using Uno.UI.RuntimeTests;
 using Uno.UI.RuntimeTests.Helpers;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
@@ -15,20 +16,18 @@ namespace Windows.UI.Xaml.Tests.MUXControls.ApiTests
 		[TestMethod]
 		[Description("Verifies that the TextBlock representing the Chevron glyph uses the correct font")]
 		[Ignore("Fluent styles V2 use AnimatedIcon instead of FontIcon")]
+		[RunsOnUIThread]
 		public void VerifyFontFamilyForChevron()
 		{
 			DropDownButton dropDownButton = null;
 			using (StyleHelper.UseFluentStyles())
 			{
-				RunOnUIThread.Execute(() =>
-				{
-					dropDownButton = new DropDownButton();
-					TestServices.WindowHelper.WindowContent = dropDownButton;
+				dropDownButton = new DropDownButton();
+				TestServices.WindowHelper.WindowContent = dropDownButton;
 
-					var chevronTextBlock = dropDownButton.GetTemplateChild("ChevronTextBlock") as TextBlock;
-					var font = chevronTextBlock.FontFamily;
-					Verify.AreEqual((FontFamily)Application.Current.Resources["SymbolThemeFontFamily"], font);
-				});
+				var chevronTextBlock = dropDownButton.GetTemplateChild("ChevronTextBlock") as TextBlock;
+				var font = chevronTextBlock.FontFamily;
+				Verify.AreEqual((FontFamily)Application.Current.Resources["SymbolThemeFontFamily"], font);
 			}
 		}
 #endif
