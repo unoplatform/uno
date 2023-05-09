0a825924eb770105d672bbc8a937c3b320578a26
Martin Zikmund <martin@zikmund.dev>
2023-05-09 13:33:39 +0200

    chore: Adjust generated files

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/TeachingTip/TeachingTipCloseReason.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/TeachingTip/TeachingTipCloseReason.cs
@@ -2,26 +2,25 @@
 
 #nullable enable
 
-namespace Microsoft.UI.Xaml.Controls
+namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Defines constants that indicate the cause of the TeachingTip closure.
+/// </summary>
+public enum TeachingTipCloseReason
 {
 	/// <summary>
-	/// Defines constants that indicate the cause of the TeachingTip closure.
+	/// The teaching tip was closed by the user clicking the close button.
 	/// </summary>
-	public enum TeachingTipCloseReason
-	{
-		/// <summary>
-		/// The teaching tip was closed by the user clicking the close button.
-		/// </summary>
-		CloseButton,
+	CloseButton,
 
-		/// <summary>
-		/// The teaching tip was closed by light-dismissal.
-		/// </summary>
-		LightDismiss,
+	/// <summary>
+	/// The teaching tip was closed by light-dismissal.
+	/// </summary>
+	LightDismiss,
 
-		/// <summary>
-		/// The teaching tip was programmatically closed.
-		/// </summary>
-		Programmatic,
-	}
+	/// <summary>
+	/// The teaching tip was programmatically closed.
+	/// </summary>
+	Programmatic,
 }
--- src/Uno.UI/Microsoft/UI/Xaml/Controls/TeachingTip/TeachingTipHeroContentPlacementMode.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/TeachingTip/TeachingTipHeroContentPlacementMode.cs
@@ -2,27 +2,26 @@
 
 #nullable enable
 
-namespace Microsoft.UI.Xaml.Controls
+namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Defines constants that indicate the preferred location of the HeroContent within a teaching tip.
+/// </summary>
+public enum TeachingTipHeroContentPlacementMode
 {
 	/// <summary>
-	/// Defines constants that indicate the preferred location of the HeroContent within a teaching tip.
+	/// The header of the teaching tip.
+	/// The hero content might be moved to the footer to avoid intersecting with the tail of the targeted teaching tip.
 	/// </summary>
-	public enum TeachingTipHeroContentPlacementMode
-	{
-		/// <summary>
-		/// The header of the teaching tip.
-		/// The hero content might be moved to the footer to avoid intersecting with the tail of the targeted teaching tip.
-		/// </summary>
-		Auto,
+	Auto,
 
-		/// <summary>
-		/// The header of the teaching tip.
-		/// </summary>
-		Top,
+	/// <summary>
+	/// The header of the teaching tip.
+	/// </summary>
+	Top,
 
-		/// <summary>
-		/// The footer of the teaching tip.
-		/// </summary>
-		Bottom,
-	}
+	/// <summary>
+	/// The footer of the teaching tip.
+	/// </summary>
+	Bottom,
 }
