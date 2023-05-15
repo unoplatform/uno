72ac37e90b87b8ef7f4ce6bbba462afc390f9430
Youssef Victor <youssefvictor00@gmail.com>
2023-05-15 18:32:21 +0300

    fix(SyncGenerator): Generate missing attributes

---
+++ src/Uno.UWPSyncGenerator/AttributeGeneration/AttributeDataClassComparer.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.CodeAnalysis;
+
+namespace Uno.UWPSyncGenerator.AttributeGeneration;
+
+internal sealed class AttributeDataClassComparer : IEqualityComparer<AttributeData>
+{
+	public static AttributeDataClassComparer Instance { get; } = new();
+
+	private AttributeDataClassComparer()
+	{
+	}
+
+	public bool Equals(AttributeData? x, AttributeData? y)
+	{
+		return x?.AttributeClass?.ToString() == y?.AttributeClass?.ToString();
+	}
+
+	public int GetHashCode([DisallowNull] AttributeData obj) => obj.AttributeClass?.ToString()?.GetHashCode() ?? 0;
+}
---
+++ src/Uno.UWPSyncGenerator/AttributeGeneration/AttributeDescriptionBase.cs
@@ -0,0 +1,50 @@
+﻿#nullable enable
+
+using Microsoft.CodeAnalysis;
+
+namespace Uno.UWPSyncGenerator.AttributeGeneration;
+
+internal abstract class AttributeDescriptionBase : IAttributeDescription
+{
+	private protected abstract bool CanHandle(string fullyQualifiedAttributeName);
+
+	public string? TryGenerateCodeFromAttributeData(AttributeData attributeData)
+	{
+		var attributeName = attributeData.AttributeClass?.ToString();
+		if (attributeName is null || !CanHandle(attributeName))
+		{
+			return null;
+		}
+
+		var parameters = TryGenerateAttributeParametersCommon(attributeData);
+		if (parameters is null)
+		{
+			return null;
+		}
+		else if (parameters.Length == 0)
+		{
+			return $"[global::{attributeName}]";
+		}
+		else
+		{
+			return $"[global::{attributeName}({parameters})]";
+		}
+	}
+
+	private string? TryGenerateAttributeParametersCommon(AttributeData attributeData)
+	{
+		if (attributeData.ConstructorArguments.Length == 0 && attributeData.NamedArguments.Length == 0)
+		{
+			return string.Empty;
+		}
+
+		return TryGenerateAttributeParameters(attributeData);
+	}
+
+	/// <summary>
+	/// Given <see cref="AttributeData"/>, try to generate parameters. If the current instance doesn't handle
+	/// the given <paramref name="attributeData"/>, return null.
+	/// This method is not called when both ConstructorArguments and NamedArguments are empty.
+	/// </summary>
+	protected abstract string? TryGenerateAttributeParameters(AttributeData attributeData);
+}
