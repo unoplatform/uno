99d30788a3e3c3af7f872a41093b82a2c10053c8
agneszitte <agnes@platform.uno>
2023-05-16 18:33:38 -0400

    feat(Wasm): Implement UIElement.ProtectedCursor

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Input/SetProtectedCursor.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+using System.Collections.Generic;
+using System;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Microsoft.UI.Input;
+
+namespace SamplesApp.Wasm.Windows_UI_Input
+{
+	[SampleControlInfo("Windows.UI.Input", "SetProtectedCursor", isManualTest: true, description: "Demonstrates use of UIElement.ProtectedCursor / InputSystemCursor / InputSystemCursorShape")]
+	public sealed partial class SetProtectedCursor : Page
+	{
+		public SetProtectedCursor()
+		{
+			this.InitializeComponent();
+		}
+
+		private void ChangeButtonCursor(object sender, PointerRoutedEventArgs e)
+		{
+			var SizeWestEastCursor = InputSystemCursor.Create(InputSystemCursorShape.SizeWestEast);
+			this.ProtectedCursor = SizeWestEastCursor;
+		}
+
+		private void ChangeBorderCursor(object sender, PointerRoutedEventArgs e)
+		{
+			var waitCursor = InputSystemCursor.Create(InputSystemCursorShape.Wait);
+			this.ProtectedCursor = waitCursor;
+		}
+
+		private void CaptureCursor(object sender, PointerRoutedEventArgs e)
+		{
+			var border = (Border)sender;
+			border.CapturePointer(e.Pointer);
+		}
+	}
+}
