07cefa4d519bfc042f59743d1fd44895f6f4cb13
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2023-05-17 19:53:39 -0400

    fix: avoid calls to CGColorCreateSRGB before iOS13
    (cherry picked from commit cb4a9838e120a620fa1bf6ef5d374f404e93023d)

--- src/Uno.UWP/UI/Color.iOS.cs
+++ src/Uno.UWP/UI/Color.iOS.cs
@@ -10,8 +10,17 @@ namespace Windows.UI
 {
 	public partial struct Color : IFormattable
 	{
+		private static bool legacy = !UIDevice.CurrentDevice.CheckSystemVersion(13, 0);
+
 		public static implicit operator UIKit.UIColor(Color color) => UIKit.UIColor.FromRGBA(color.R, color.G, color.B, color.A);
-		public static implicit operator CGColor(Color color) => CGColor.CreateSrgb(color.R / 255.0f, color.G / 255.0f, color.B / 255.0f, color.A / 255.0f);
+		public static implicit operator CGColor(Color color)
+		{
+			if (legacy) {
+				return AppKit.NSColor.FromRgba(color.R, color.G, color.B, color.A).CGColor;
+			} else {
+				return CGColor.CreateSrgb(color.R / 255.0f, color.G / 255.0f, color.B / 255.0f, color.A / 255.0f);
+			}
+		}
 
 		public static implicit operator Color(UIKit.UIColor color) => color.CGColor;
 
