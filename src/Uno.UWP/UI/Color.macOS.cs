53467fc749e207743b82dababce48a4906fcb674
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2023-05-17 21:56:54 -0400

    fix: style + case diff between iOS and Mac API

--- src/Uno.UWP/UI/Color.macOS.cs
+++ src/Uno.UWP/UI/Color.macOS.cs
@@ -17,9 +17,12 @@ namespace Windows.UI
 		public static implicit operator AppKit.NSColor(Color color) => AppKit.NSColor.FromRgba(color.R, color.G, color.B, color.A);
 		public static implicit operator CGColor(Color color)
 		{
-			if (legacy) {
-				return AppKit.NSColor.FromRGBA(color.R, color.G, color.B, color.A).CGColor;
-			} else {
+			if (legacy)
+			{
+				return AppKit.NSColor.FromRgba(color.R, color.G, color.B, color.A).CGColor;
+			}
+			else
+			{
 				return CGColor.CreateSrgb(color.R / 255.0f, color.G / 255.0f, color.B / 255.0f, color.A / 255.0f);
 			}
 		}
