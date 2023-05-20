a2c5dffb7ad3c84128e71d0456242b44b7705d6b
Youssef Victor <youssefvictor00@gmail.com>
2023-05-20 22:25:20 +0300

    chore: Simplify test

--- src/Uno.UI.RuntimeTests/Tests/Windows_Globalization/Given_ApplicationLanguages.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_Globalization/Given_ApplicationLanguages.cs
@@ -7,23 +7,6 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_Globalization;
 [TestClass]
 public class Given_ApplicationLanguages
 {
-	private const string Key =
-#if (__ANDROID__ || __IOS__ || __MACOS__) && NET6_0_OR_GREATER
-		"__Uno.PrimaryLanguageOverride.SamplesApp.netcoremobile";
-#elif __ANDROID__
-		"__Uno.PrimaryLanguageOverride.SamplesApp.Droid";
-#elif __IOS__
-		"__Uno.PrimaryLanguageOverride.SamplesApp.iOS";
-#elif __MACOS__
-		"__Uno.PrimaryLanguageOverride.SamplesApp.macOS";
-#elif __SKIA__
-		"__Uno.PrimaryLanguageOverride.SamplesApp.Skia";
-#elif __WASM__
-		"__Uno.PrimaryLanguageOverride.SamplesApp.Wasm";
-#else
-#error "Missing key value"
-#endif
-
 	[TestCleanup]
 	public void CleanUp()
 	{
@@ -35,8 +18,7 @@ public class Given_ApplicationLanguages
 	{
 		ApplicationLanguages.PrimaryLanguageOverride = "zh-Hans-CN";
 		ApplicationLanguages.Languages[0].Should().Be("zh-Hans-CN");
-		ApplicationLanguages.GetAppSpecificSettingKey().Should().Be(Key);
-		ApplicationData.Current.LocalSettings.Values[Key].Should().Be("zh-Hans-CN");
+		ApplicationData.Current.LocalSettings.Values[ApplicationLanguages.GetAppSpecificSettingKey()].Should().Be("zh-Hans-CN");
 	}
 
 	[TestMethod]
@@ -44,7 +26,6 @@ public class Given_ApplicationLanguages
 	{
 		ApplicationLanguages.PrimaryLanguageOverride = "fr-Latn-CA";
 		ApplicationLanguages.Languages[0].Should().Be("fr-Latn-CA");
-		ApplicationLanguages.GetAppSpecificSettingKey().Should().Be(Key);
-		ApplicationData.Current.LocalSettings.Values[Key].Should().Be("fr-Latn-CA");
+		ApplicationData.Current.LocalSettings.Values[ApplicationLanguages.GetAppSpecificSettingKey()].Should().Be("fr-Latn-CA");
 	}
 }
