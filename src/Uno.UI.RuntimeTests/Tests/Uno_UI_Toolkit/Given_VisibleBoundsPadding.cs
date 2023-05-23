22bffad30a2f31333b29d8e6f8a1d83938db2f1f
Steve Bilogan <steve.bilogan@gmail.com>
2023-05-23 10:10:39 -0400

    chore: test
    (cherry picked from commit 5dd6fbcf855d526064fbb4b2838d2f0e2b057c09)

--- src/Uno.UI.RuntimeTests/Tests/Uno_UI_Toolkit/Given_VisibleBoundsPadding.cs
+++ src/Uno.UI.RuntimeTests/Tests/Uno_UI_Toolkit/Given_VisibleBoundsPadding.cs
@@ -22,52 +22,6 @@ namespace Uno.UI.RuntimeTests.Tests.Uno_UI_Toolkit
 	[RunsOnUIThread]
 	public class Given_VisibleBoundsPadding
 	{
-		[TestMethod]
-		[RequiresFullWindow]
-#if __SKIA__ || __WASM__
-		[Ignore("VisibleBoundsPadding is not working correctly on these platforms - see https://github.com/unoplatform/uno/issues/7978")]
-#elif __ANDROID__
-		[Ignore("Flaky test on Android")]
-#endif
-#if __MACOS__
-		[Ignore("Currently fails on macOS, part of #9282 epic")]
-#endif
-		public async Task When_Mask_All()
-		{
-			using (ScreenHelper.OverrideVisibleBounds(new Thickness(0, 34, 0, 65)))
-			{
-				var inner = new Border
-				{
-					Background = new SolidColorBrush(Colors.AliceBlue),
-					Child = new Ellipse
-					{
-						Fill = new SolidColorBrush(Colors.DarkOrange)
-					}
-				};
-				var container = new Grid
-				{
-					Children =
-					{
-						inner
-					}
-				};
-
-				VisibleBoundsPadding.SetPaddingMask(container, VisibleBoundsPadding.PaddingMask.All);
-
-				WindowHelper.WindowContent = container;
-				await WindowHelper.WaitForLoaded(inner);
-
-				var visibleBounds = ApplicationView.GetForCurrentView().VisibleBounds;
-				var windowBounds = Window.Current.Bounds;
-				RectAssert.AreNotEqual(windowBounds, visibleBounds);
-
-				var containerBounds = container.GetOnScreenBounds();
-				var childBounds = inner.GetOnScreenBounds();
-				RectAssert.AreEqual(windowBounds, containerBounds);
-				RectAssert.AreEqual(visibleBounds, childBounds);
-			}
-		}
-
 #if __ANDROID__
 		[TestMethod]
 		public async Task Translucent_SystemBars()
