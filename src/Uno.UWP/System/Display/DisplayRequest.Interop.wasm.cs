537f09416a42debecaee1e0c84636d7d3c55208d
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2023-05-23 08:59:18 -0400

    perf: Add DisplayRequest bindings
    (cherry picked from commit 1b60854fee45dc3e3bf0fa2f90bbd0ae6d715746)

--- src/Uno.UWP/System/Display/DisplayRequest.Interop.wasm.cs
+++ src/Uno.UWP/System/Display/DisplayRequest.Interop.wasm.cs
@@ -7,6 +7,13 @@ namespace __Windows.__System.Display
 	{
 		internal static partial class NativeMethods
 		{
+			private const string JsType = "globalThis.Windows.System.Display.DisplayRequest";
+
+			[JSImport($"{JsType}.activateScreenLock")]
+			internal static partial void ActivateScreenLock();
+
+			[JSImport($"{JsType}.deactivateScreenLock")]
+			internal static partial void DeactivateScreenLock();
 		}
 	}
 }
