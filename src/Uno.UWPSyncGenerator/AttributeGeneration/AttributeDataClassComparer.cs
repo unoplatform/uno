a37652db21d502044fb4836e4b204decab9f8583
Youssef Victor <youssefvictor00@gmail.com>
2023-05-24 10:42:19 +0300

    chore: Adjust

--- src/Uno.UWPSyncGenerator/AttributeGeneration/AttributeDataClassComparer.cs
+++ src/Uno.UWPSyncGenerator/AttributeGeneration/AttributeDataClassComparer.cs
@@ -15,10 +15,21 @@ internal sealed class AttributeDataClassComparer : IEqualityComparer<AttributeDa
 	{
 	}
 
-	public bool Equals(AttributeData? x, AttributeData? y)
+	private static string? Coerce(string? attributeClass)
 	{
-		return x?.AttributeClass?.ToString() == y?.AttributeClass?.ToString();
+		if (attributeClass == "Windows" + ".UI.Xaml.Markup.ContentPropertyAttribute")
+		{
+			// For the purpose of attribute equality, we want to consider both attributes as equal.
+			return "Microsoft.UI.Xaml.Markup.ContentPropertyAttribute";
+		}
+
+		return attributeClass;
 	}
 
-	public int GetHashCode([DisallowNull] AttributeData obj) => obj.AttributeClass?.ToString()?.GetHashCode() ?? 0;
+	public bool Equals(AttributeData? x, AttributeData? y)
+	{
+		return Coerce(x?.AttributeClass?.ToString()) == Coerce(y?.AttributeClass?.ToString());
+	}
+
+	public int GetHashCode([DisallowNull] AttributeData obj) => Coerce(obj.AttributeClass?.ToString())?.GetHashCode() ?? 0;
 }
