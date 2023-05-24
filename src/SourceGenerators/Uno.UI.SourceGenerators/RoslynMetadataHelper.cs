610ce312b93eb77cb8a473ec08e7238ca840c99d
Youssef Victor <youssefvictor00@gmail.com>
2023-05-24 09:57:03 +0300

    perf: Move some XAML generator caches to compilation-level

--- src/SourceGenerators/Uno.UI.SourceGenerators/RoslynMetadataHelper.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators/RoslynMetadataHelper.cs
@@ -19,6 +19,9 @@ namespace Uno.Roslyn
 		private readonly Func<INamedTypeSymbol?, string, bool> _isAttachedProperty;
 		private readonly Func<INamedTypeSymbol, string, INamedTypeSymbol> _getAttachedPropertyType;
 		private readonly Func<INamedTypeSymbol, bool> _isTypeImplemented;
+		private readonly Func<INamedTypeSymbol?, string, INamedTypeSymbol?>? _findPropertyTypeByOwnerSymbol;
+		private readonly Func<INamedTypeSymbol, string[]>? _findLocalizableDeclaredProperties;
+		private readonly Func<INamedTypeSymbol?, string, IEventSymbol?>? _findEventType;
 
 		public Compilation Compilation { get; }
 
@@ -33,6 +36,22 @@ namespace Uno.Roslyn
 			_isAttachedProperty = Funcs.Create<INamedTypeSymbol?, string, bool>(SourceIsAttachedProperty).AsLockedMemoized();
 			_getAttachedPropertyType = Funcs.Create<INamedTypeSymbol, string, INamedTypeSymbol>(SourceGetAttachedPropertyType).AsLockedMemoized();
 			_isTypeImplemented = Funcs.Create<INamedTypeSymbol, bool>(SourceIsTypeImplemented).AsLockedMemoized();
+			_findPropertyTypeByOwnerSymbol = Funcs.Create<INamedTypeSymbol?, string, INamedTypeSymbol?>(SourceFindPropertyTypeByOwnerSymbol).AsLockedMemoized();
+			_findLocalizableDeclaredProperties = Funcs.Create<INamedTypeSymbol, string[]>(SourceFindLocalizableDeclaredProperties).AsLockedMemoized();
+			_findEventType = Funcs.Create<INamedTypeSymbol?, string, IEventSymbol?>(SourceFindEventType).AsLockedMemoized();
+		}
+
+		private static void ThrowOnErrorSymbol(ISymbol symbol)
+		{
+			if (symbol is IErrorTypeSymbol errorTypeSymbol)
+			{
+				var candidates = string.Join(";", errorTypeSymbol.CandidateSymbols);
+				var location = symbol.Locations.FirstOrDefault()?.ToString() ?? "Unknown";
+
+				throw new InvalidOperationException(
+					$"Unable to resolve {symbol} (Reason: {errorTypeSymbol.CandidateReason}, Location:{location}, Candidates: {candidates})"
+				);
+			}
 		}
 
 		public ITypeSymbol? FindTypeByFullName(string fullName)
@@ -141,5 +160,100 @@ namespace Uno.Roslyn
 
 		private static bool SourceIsTypeImplemented(INamedTypeSymbol type)
 			=> type.GetAttributes().None(a => a.AttributeClass?.GetFullyQualifiedTypeExcludingGlobal() == XamlConstants.Types.NotImplementedAttribute);
+
+		public INamedTypeSymbol? FindPropertyTypeByOwnerSymbol(INamedTypeSymbol? type, string propertyName) => _findPropertyTypeByOwnerSymbol!(type, propertyName);
+
+		private INamedTypeSymbol? SourceFindPropertyTypeByOwnerSymbol(INamedTypeSymbol? type, string propertyName)
+		{
+			if (type != null && !string.IsNullOrEmpty(propertyName))
+			{
+				do
+				{
+					ThrowOnErrorSymbol(type);
+
+					var resolvedType = type;
+
+					var property = resolvedType.GetPropertyWithName(propertyName);
+					var setMethod = resolvedType.GetFirstMethodWithName("Set" + propertyName);
+
+					if (property != null)
+					{
+						if (property.Type.OriginalDefinition is { SpecialType: SpecialType.System_Nullable_T })
+						{
+							//TODO
+							return (property.Type as INamedTypeSymbol)?.TypeArguments[0] as INamedTypeSymbol;
+						}
+						else
+						{
+							return property.Type as INamedTypeSymbol;
+						}
+					}
+					else
+					{
+						if (setMethod != null)
+						{
+							return setMethod.Parameters.ElementAt(1).Type as INamedTypeSymbol;
+						}
+						else
+						{
+							var baseType = type.BaseType;
+
+							if (baseType == null || baseType.SpecialType == SpecialType.System_Object)
+							{
+								return null;
+							}
+
+							type = baseType;
+						}
+					}
+				} while (true);
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		public string[] FindLocalizableDeclaredProperties(INamedTypeSymbol type) => _findLocalizableDeclaredProperties!(type);
+
+		private string[] SourceFindLocalizableDeclaredProperties(INamedTypeSymbol type)
+		{
+			return type.GetProperties()
+				.Where(p => !p.IsReadOnly &&
+					p.DeclaredAccessibility == Accessibility.Public &&
+					XamlFileGenerator.IsLocalizablePropertyType(p.Type as INamedTypeSymbol)
+				)
+				.Select(p => p.Name)
+				.ToArray();
+		}
+
+		public IEventSymbol? FindEventType(INamedTypeSymbol? ownerType, string eventName)
+			=> _findEventType!(ownerType, eventName);
+
+		private IEventSymbol? SourceFindEventType(INamedTypeSymbol? ownerType, string eventName)
+		{
+			if (ownerType != null)
+			{
+				ThrowOnErrorSymbol(ownerType);
+
+				do
+				{
+					foreach (var member in ownerType.GetMembers(eventName).OfType<IEventSymbol>())
+					{
+						return member;
+					}
+
+					ownerType = ownerType.BaseType;
+
+					if (ownerType == null)
+					{
+						break;
+					}
+
+				} while (ownerType.SpecialType != SpecialType.System_Object);
+			}
+
+			return null;
+		}
 	}
 }
