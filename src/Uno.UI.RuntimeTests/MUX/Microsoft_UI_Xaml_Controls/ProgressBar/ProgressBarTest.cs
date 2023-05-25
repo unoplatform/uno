11f40e36c1065bc7df8d5695383182161a22f1a5
Robert-Louis Milin <milinmt@gmail.com>
2023-05-25 11:11:37 -0400

    chore(ProgressBarTest): add exception for macos test

--- src/Uno.UI.RuntimeTests/MUX/Microsoft_UI_Xaml_Controls/ProgressBar/ProgressBarTest.cs
+++ src/Uno.UI.RuntimeTests/MUX/Microsoft_UI_Xaml_Controls/ProgressBar/ProgressBarTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Common;
@@ -14,10 +15,17 @@ using ProgressBar = Microsoft.UI.Xaml.Controls.ProgressBar;
 [TestClass]
 public class ProgressBarTest
 {
+
 	[RunsOnUIThread]
 	[TestMethod]
 	public async Task ProgressBarLayoutUpdate()
 	{
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+		{
+			Assert.Inconclusive("This test is not valid on macOS");
+			return;
+		}
+
 		var sut = new ProgressBar();
 		sut.Width = 200;
 		sut.Height = 40;
