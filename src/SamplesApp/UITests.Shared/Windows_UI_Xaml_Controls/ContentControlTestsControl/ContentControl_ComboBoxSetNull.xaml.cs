ea7a0ded788f5b4737565992a3367b4899fadd2e
Andres Pineda <pineda.andres@gmail.com>
2023-05-26 00:24:44 -0400

    chore: fix formatting for Ci
    (cherry picked from commit 34a68c3303c23596f5cc653e30070085294784d4)

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ContentControlTestsControl/ContentControl_ComboBoxSetNull.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ContentControlTestsControl/ContentControl_ComboBoxSetNull.xaml.cs
@@ -1,23 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Uno.UI.Samples.Controls;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Uno.UI.Samples.Controls;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace Uno.UI.Samples.Content.UITests.ContentControlTestsControl
 {
-	[SampleControlInfo("ContentControl", "ContentControl_ComboBoxSetNull", typeof(Presentation.SamplePages.ContentControlTestViewModel), isManualTest:true)]
+	[SampleControlInfo("ContentControl", "ContentControl_ComboBoxSetNull", typeof(Presentation.SamplePages.ContentControlTestViewModel), isManualTest: true)]
 	public sealed partial class ContentControl_ComboBoxSetNull : UserControl
 	{
 		public ContentControl_ComboBoxSetNull()
