0402548c5f91294b593d9db791acb853a5c9d63a
Youssef Victor <youssefvictor00@gmail.com>
2023-05-27 18:24:36 +0300

    chore: Fix for Skia

--- src/Uno.UI/UI/Xaml/Media/Imaging/BitmapSource.skia.cs
+++ src/Uno.UI/UI/Xaml/Media/Imaging/BitmapSource.skia.cs
@@ -5,7 +5,7 @@ namespace Windows.UI.Xaml.Media.Imaging;
 
 public partial class BitmapSource
 {
-	protected void UpdatePixelWidthAndHeight(Stream stream)
+	partial void UpdatePixelWidthAndHeightPartial(Stream stream)
 	{
 		using var codec = SKCodec.Create(stream);
 		var info = codec.Info;
