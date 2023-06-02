9d99a7b4493bbccfd9a9864f77f2ac549fe6f8ee
Matt Lacey <matt@mrlacey.co.uk>
2023-06-02 11:03:05 +0100

    chore: WIP/PoC - SharedProcessorPool

--- src/Uno.UI.RemoteControl.Host/RemoteControlServer.cs
+++ src/Uno.UI.RemoteControl.Host/RemoteControlServer.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net.WebSockets;
+using System.Runtime.Loader;
+using System.Runtime.Remoting.Messaging;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Uno.Extensions;
 using Uno.UI.RemoteControl.Helpers;
 using Uno.UI.RemoteControl.HotReload.Messages;
-using Microsoft.Extensions.Logging;
-using Uno.Extensions;
 using Uno.UI.RemoteControl.Messages;
-using System.Runtime.Loader;
-using Microsoft.Extensions.Configuration;
 
 namespace Uno.UI.RemoteControl.Host
 {
 	internal class RemoteControlServer : IRemoteControlServer, IDisposable
 	{
-		private readonly Dictionary<string, IServerProcessor> _processors = new Dictionary<string, IServerProcessor>();
+		////private readonly Dictionary<string, IServerProcessor> _processors = new();
 
 		private System.Reflection.Assembly? _loopAssembly;
 		private WebSocket? _socket;
@@ -35,17 +36,37 @@ namespace Uno.UI.RemoteControl.Host
 					this.Log().LogDebug("Unloading assembly context");
 				}
 			};
-			_loadContext.Resolving += (context, assemblyName) =>
-			{
-				if (_loopAssembly is not null)
-				{
-					return context.LoadFromAssemblyPath(Path.Combine(Path.GetDirectoryName(_loopAssembly.Location)!, assemblyName.Name + ".dll"));
-				}
-				else
-				{
-					return context.LoadFromAssemblyName(assemblyName);
-				}
-			};
+			//_loadContext.Resolving += (context, assemblyName) =>
+			//{
+			//	if (_loopAssembly is not null)
+			//	{
+			//		try
+			//		{
+			//			var loc = _loopAssembly.Location;
+
+			//			if (!string.IsNullOrWhiteSpace(loc))
+			//			{
+			//				var dir = Path.GetDirectoryName(loc);
+
+			//				if (!string.IsNullOrEmpty(dir))
+			//				{
+			//					var relPath = Path.Combine(dir, assemblyName.Name + ".dll");
+
+			//					if (File.Exists(relPath))
+			//					{
+			//						return context.LoadFromAssemblyPath(relPath);
+			//					}
+			//				}
+			//			}
+			//		}
+			//		catch (Exception exc)
+			//		{
+			//			System.Diagnostics.Debug.WriteLine(exc);
+			//		}
+			//	}
+
+			//	return context.LoadFromAssemblyName(assemblyName);
+			//};
 
 			if (this.Log().IsEnabled(LogLevel.Debug))
 			{
@@ -58,7 +79,14 @@ namespace Uno.UI.RemoteControl.Host
 
 		private void RegisterProcessor(IServerProcessor hotReloadProcessor)
 		{
-			_processors[hotReloadProcessor.Scope] = hotReloadProcessor;
+			if (SharedProcessorPool.Instance.Processors.ContainsKey(hotReloadProcessor.Scope))
+			{
+				SharedProcessorPool.Instance.Processors[hotReloadProcessor.Scope].Add(hotReloadProcessor);
+			}
+			else
+			{
+				SharedProcessorPool.Instance.Processors.Add(hotReloadProcessor.Scope, new List<IServerProcessor>() { hotReloadProcessor });
+			}
 		}
 
 		public async Task Run(WebSocket socket, CancellationToken ct)
@@ -85,18 +113,21 @@ namespace Uno.UI.RemoteControl.Host
 					}
 				}
 
-				if (_processors.TryGetValue(frame.Scope, out var processor))
+				if (SharedProcessorPool.Instance.Processors.TryGetValue(frame.Scope, out var processors))
 				{
 					if (this.Log().IsEnabled(LogLevel.Trace))
 					{
 						this.Log().LogTrace($"Received Frame [{frame.Scope} / {frame.Name}]");
 					}
 
-					await processor.ProcessFrame(frame);
+					foreach (var processor in processors)
+					{
+						await processor.ProcessFrame(frame);
+					}
 				}
 				else
 				{
-					if (this.Log().IsEnabled(LogLevel.Trace))
+					if (this.Log().IsEnabled(LogLevel.Trace) && frame.Scope != "RemoteControlServer")
 					{
 						this.Log().LogTrace($"Unknown Frame [{frame.Scope} / {frame.Name}]");
 					}
@@ -238,10 +269,22 @@ namespace Uno.UI.RemoteControl.Host
 
 		public void Dispose()
 		{
-			foreach (var processor in _processors)
+			foreach (var processors in SharedProcessorPool.Instance.Processors.Values)
 			{
-				processor.Value.Dispose();
+				foreach (var processor in processors)
+				{
+					processor.Dispose();
+				}
 			}
 		}
+	}
+
+	public class SharedProcessorPool
+	{
+		private readonly Dictionary<string, List<IServerProcessor>> _processors = new();
+
+		internal static SharedProcessorPool Instance { get; } = new SharedProcessorPool();
+
+		public Dictionary<string, List<IServerProcessor>> Processors => _processors;
 	}
 }
