7e46343436783b04fca4a99be54abee149784c05
Rafael Rosa <rafael.rosa@knowcode.app>
2023-06-05 11:30:52 -0300

    chore: add First Tests

---
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/MediaPlayerElement/MediaPlayerElementTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SamplesApp.UITests.TestFramework;
+using Uno.UITest.Helpers;
+using Uno.UITest.Helpers.Queries;
+using Uno.UITests.Helpers;
+
+namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.MediaPlayerElementTests;
+
+[TestFixture]
+class MediaPlayerElementTests : SampleControlUITestBase
+{
+	[Test]
+	[AutoRetry]
+	public void MediaPlayerElement_PlayPauseStopTest()
+	{
+		Run("UITests.Shared.Windows_UI_Xaml_Controls.MediaPlayerElement.MediaPlayerElement_Original");
+
+		_app.WaitForElement(_app.Marked("MpeOriginal"));
+
+		TakeScreenshot("Initial MpeOriginal");
+
+		// step 1: Test Auto Play
+		var itemQuery = _app.Marked("TimeElapsedElement");
+		_app.WaitForText(itemQuery, "0:00:03");
+		TakeScreenshot("TimeElapsedElement AutoPlay 03 seconds");
+
+		// step 2: press button PlayPause 
+		_app.Tap(_app.Marked("PlayPauseButton"));
+		TakeScreenshot("Pause Tapped");
+
+		// step 3: Test Stop
+		_app.Tap(_app.Marked("StopButton"));
+		TakeScreenshot("Stop Tapped");
+
+		// step 4: press button PlayPause
+		_app.Tap(_app.Marked("PlayPauseButton"));
+		TakeScreenshot("Play Tapped");
+
+		_app.WaitForText(itemQuery, "0:00:02");
+		TakeScreenshot("TimeElapsedElement Play 02 seconds");
+		//Assert.AreEqual(true, true);
+	}
+}
