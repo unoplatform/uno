95f7c0bf7dde21aeb6f15b0325a4d2a5dc5ed4e9
Martin Zikmund <martin@zikmund.dev>
2023-06-06 14:31:37 +0200

    chore: Adjust private members

--- src/Uno.UWP/Devices/Sensors/ProximitySensor.cs
+++ src/Uno.UWP/Devices/Sensors/ProximitySensor.cs
@@ -1,6 +1,3 @@
-using Uno.Helpers;
-using Windows.Foundation;
-
 namespace Windows.Devices.Sensors;
 
 /// <summary>
@@ -9,8 +6,6 @@ namespace Windows.Devices.Sensors;
 public partial class ProximitySensor
 {
 	internal const string InterfaceId = "{5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D}";
-
-	private readonly StartStopEventWrapper<TypedEventHandler<ProximitySensor, ProximitySensorReadingChangedEventArgs>> _readingChangedWrapper;
 
 	private ProximitySensor()
 	{
@@ -23,8 +18,4 @@ public partial class ProximitySensor
 	public static string GetDeviceSelector() =>
 		$"System.Devices.InterfaceClassGuid:=\"{InterfaceId}\" AND " +
 		"System.Devices.InterfaceEnabled:=System.StructuredQueryType.Boolean#True";
-
-	partial void StartReading();
-
-	partial void StopReading();
 }
