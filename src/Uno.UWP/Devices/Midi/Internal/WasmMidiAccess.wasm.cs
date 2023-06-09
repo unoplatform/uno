1a6e5984e4a4e6441eb294779f9750471a596fed
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2023-06-09 03:39:37 -0400

    perf: Add RequestAsync binding

--- src/Uno.UWP/Devices/Midi/Internal/WasmMidiAccess.wasm.cs
+++ src/Uno.UWP/Devices/Midi/Internal/WasmMidiAccess.wasm.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+
+#if NET7_0_OR_GREATER
+using System.Runtime.InteropServices.JavaScript;
+#else
 using static Uno.Foundation.WebAssemblyRuntime;
+#endif
 
 namespace Uno.Devices.Midi.Internal
 {
 	/// <summary>
 	/// Handles WASM MIDI access permission request
 	/// </summary>
-	public static class WasmMidiAccess
+	public static partial class WasmMidiAccess
 	{
+#if !NET7_0_OR_GREATER
 		private const string JsType = "Uno.Devices.Midi.Internal.WasmMidiAccess";
+#endif
 
 		private static bool _webMidiAccessible;
 
@@ -23,11 +30,26 @@ namespace Uno.Devices.Midi.Internal
 			}
 
 			var systemExclusiveRequested = WinRTFeatureConfiguration.Midi.RequestSystemExclusiveAccess;
+
+#if NET7_0_OR_GREATER
+			var result = await NativeMethods.RequestAsync(systemExclusiveRequested);
+#else
 			var serializedRequest = systemExclusiveRequested.ToString().ToLowerInvariant();
 			var command = $"{JsType}.request({serializedRequest})";
 			var result = await InvokeAsync(command);
+#endif
+
 			_webMidiAccessible = bool.Parse(result);
+
 			return _webMidiAccessible;
 		}
+
+#if NET7_0_OR_GREATER
+		internal static partial class NativeMethods
+		{
+			[JSImport("globalThis.Uno.Devices.Midi.Internal.WasmMidiAccess.request")]
+			internal static partial Task<string> RequestAsync(bool exclusive);
+		}
+#endif
 	}
 }
