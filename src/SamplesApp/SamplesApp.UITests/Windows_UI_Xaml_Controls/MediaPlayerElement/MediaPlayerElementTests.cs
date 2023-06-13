622518236339a803836ed9cf54bcfc88f0c37a4a
Rafael Rosa <rafael.rosa@knowcode.app>
2023-06-12 22:13:53 -0300

    chore: add SeekingTest

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/MediaPlayerElement/MediaPlayerElementTests.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/MediaPlayerElement/MediaPlayerElementTests.cs
@@ -45,4 +45,38 @@ class MediaPlayerElementTests : SampleControlUITestBase
 		_app.WaitForTextGreaterThanTimeSpan(itemQuery, TimeSpan.FromSeconds(2));
 		TakeScreenshot("TimeElapsedElement Play 02 seconds");
 	}
+
+	[Test]
+	[AutoRetry]
+	public void MediaPlayerElement_SeekingTest()
+	{
+		Run("UITests.Shared.Windows_UI_Xaml_Controls.MediaPlayerElement.MediaPlayerElement_Original");
+
+		_app.WaitForElement(_app.Marked("MpeOriginal"));
+		TakeScreenshot("Initial MpeOriginal");
+
+		// step 1: Pause
+		_app.Tap(_app.Marked("PlayPauseButton"));
+		TakeScreenshot("Pause Tapped");
+
+		// step 2: Move Progress
+		var progressSlider = _app.Marked("ProgressSlider");
+
+		var rect = _app.Query(progressSlider).First().Rect;
+		float middleX = rect.X + rect.Width / 2;
+		float middleY = rect.Y + rect.Height / 2;
+
+		// Realize o toque no meio do objeto
+		_app.TapCoordinates(middleX, middleY);
+
+		var itemQuery = _app.Marked("TimeElapsedElement");
+		_app.WaitForTextGreaterThanTimeSpan(itemQuery, TimeSpan.FromSeconds(3));
+		TakeScreenshot("TimeElapsedElement More than 03 seconds");
+
+		// step 4: press button PlayPause
+		_app.Tap(_app.Marked("PlayPauseButton"));
+		TakeScreenshot("Play Tapped");
+		_app.WaitForTextGreaterThanTimeSpan(itemQuery, TimeSpan.FromSeconds(8));
+		TakeScreenshot("TimeElapsedElement Play 08 seconds");
+	}
 }
