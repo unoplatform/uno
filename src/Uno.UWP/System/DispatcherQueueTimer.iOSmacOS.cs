683f04714c7320a2739170ec927d7ce2524ce0fa
Martin Zikmund <martin@zikmund.dev>
2023-06-15 21:30:51 +0200

    chore: Adjust timer on iOS and macOS

--- src/Uno.UWP/System/DispatcherQueueTimer.iOSmacOS.cs
+++ src/Uno.UWP/System/DispatcherQueueTimer.iOSmacOS.cs
@@ -14,14 +14,9 @@ partial class DispatcherQueueTimer
 {
 	private NSTimer _timer;
 
-	private void StartNative(TimeSpan interval)
+	private void ScheduleTickNative(TimeSpan interval)
 	{
-		Start(NSTimer.CreateScheduledTimer(interval.TotalSeconds, repeats: IsRepeating, block: OnRecurentTick));
-	}
-
-	private void StartNative(TimeSpan dueTime, TimeSpan interval)
-	{
-		Start(NSTimer.CreateScheduledTimer(dueTime.TotalSeconds, repeats: false, block: OnUniqueTick));
+		Start(NSTimer.CreateScheduledTimer(interval.TotalSeconds, repeats: false, block: OnRecurentTick));
 	}
 
 	private void Start(NSTimer timer)
@@ -33,19 +28,6 @@ partial class DispatcherQueueTimer
 	private void StopNative()
 	{
 		Interlocked.Exchange(ref _timer, null)?.Invalidate();
-	}
-
-	private void OnUniqueTick(NSTimer timer)
-	{
-		if (_timer == timer)
-		{
-			RaiseTick();
-
-			if (_timer == timer) // be sure to not self restart if the timer was Stopped by the Tick event handler
-			{
-				StartNative(Interval);
-			}
-		}
 	}
 
 	private void OnRecurentTick(NSTimer timer)
