fbf1e7a63dd58ed410c895c2b5eb9019c949891a
David <github.site@torick.net>
2023-06-15 15:30:32 -0400

    feat(perf): Fix lazy creation of ShapeCollection when not needed

--- src/Uno.UI.Composition/Composition/ShapeVisual.cs
+++ src/Uno.UI.Composition/Composition/ShapeVisual.cs
@@ -1,27 +1,38 @@
 #nullable enable
 
-namespace Windows.UI.Composition
+namespace Windows.UI.Composition;
+
+public partial class ShapeVisual : ContainerVisual
 {
-	public partial class ShapeVisual : ContainerVisual
+	private CompositionViewBox? _viewBox;
+	private CompositionShapeCollection? _shapes;
+
+	public ShapeVisual(Compositor compositor)
+		: base(compositor)
 	{
-		private CompositionViewBox? _viewBox;
-		private CompositionShapeCollection? _shapes;
+	}
 
-		public ShapeVisual(Compositor compositor)
-			: base(compositor)
+	public CompositionViewBox? ViewBox
+	{
+		get => _viewBox;
+		set => SetProperty(ref _viewBox, value);
+	}
+
+	// This is lazy as we are using the `ShapeVisual` for UIElement, but lot of them are not creating shapes, reduce memory pressure.
+	public CompositionShapeCollection Shapes
+	{
+		get
 		{
-			// Add this as context for the shape collection so we get
-			// notified about changes in the shapes object graph.
-			OnCompositionPropertyChanged(null, Shapes, nameof(Shapes));
-		}
+			if (_shapes is null)
+			{
+				_shapes = new CompositionShapeCollection(Compositor, this);
 
-		public CompositionViewBox? ViewBox
-		{
-			get => _viewBox;
-			set => SetProperty(ref _viewBox, value);
-		}
+				// Add this as context for the shape collection so we get
+				// notified about changes in the shapes object graph.
+				OnCompositionPropertyChanged(null, _shapes, nameof(Shapes));
+			}
 
-		// This ia lazy as we are using the `ShapeVisual` for UIElement, but lot of them are not creating shapes, reduce memory pressure.
-		public CompositionShapeCollection Shapes => _shapes ??= new CompositionShapeCollection(Compositor, this);
+			return _shapes;
+		}
 	}
 }
