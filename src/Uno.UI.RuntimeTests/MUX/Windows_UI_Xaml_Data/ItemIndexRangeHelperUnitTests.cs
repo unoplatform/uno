0396a72b775b6b6bcdc922277146a7a7f9a459b3
Jerome Laban <jerome.laban@nventive.com>
2023-06-19 16:31:03 -0400

    feat: Add support for ItemIndexRange and ItemIndexRangeHelper
    Co-authored-by: Martin Zikmund <martin@zikmund.dev>

---
+++ src/Uno.UI.RuntimeTests/MUX/Windows_UI_Xaml_Data/ItemIndexRangeHelperUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using DirectUI.Components;
+using Windows.UI.Xaml.Data;
+
+using Windows.UI.Xaml.Tests.Enterprise;
+
+namespace Uno.UI.RuntimeTests.MUX.Windows_UI_Xaml_Data;
+
+[TestClass]
+[RunsOnUIThread]
+public partial class ItemIndexRangeHelperUnitTests : BaseDxamlTestClass
+{
+	private void VerifyRange(ItemIndexRangeHelper.Range range, int firstIndex, uint length)
+	{
+		VERIFY_ARE_EQUAL(range.FirstIndex, firstIndex);
+		VERIFY_ARE_EQUAL(range.Length, length);
+	}
+}
---
+++ src/Uno.UI/UI/Xaml/Data/ItemIndexRange/ItemIndexRange.cs
@@ -0,0 +1,33 @@
+﻿namespace Windows.UI.Xaml.Data;
+
+/// <summary>
+/// Provides info about a range of items in the data source.
+/// </summary>
+public partial class ItemIndexRange
+{
+	/// <summary>
+	/// Initializes an instance of the ItemIndexRange class.
+	/// </summary>
+	/// <param name="firstIndex">The index of the first item in the instance of the ItemIndexRange class.</param>
+	/// <param name="length">The number of items in the instance of the ItemIndexRange class.</param>
+	public ItemIndexRange(int firstIndex, uint length)
+	{
+		FirstIndex = firstIndex;
+		Length = length;
+	}
+
+	/// <summary>
+	/// Gets the index of the first item in the instance of the ItemIndexRange class.
+	/// </summary>
+	public int FirstIndex { get; }
+
+	/// <summary>
+	/// Gets the index of the last item in the instance of the ItemIndexRange class.
+	/// </summary>
+	public int LastIndex => GetLastIndexImpl();
+
+	/// <summary>
+	/// Gets the number of items in the instance of the ItemIndexRange class.
+	/// </summary>
+	public uint Length { get; }
+}
