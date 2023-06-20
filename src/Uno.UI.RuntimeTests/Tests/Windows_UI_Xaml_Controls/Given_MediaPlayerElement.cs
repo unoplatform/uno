6d8c1e6cb7c7579e2a356303089591fd0c32bb39
Rafael Rosa <rafael.rosa@knowcode.app>
2023-06-20 19:33:53 -0300

    chore: not run for wasm

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_MediaPlayerElement.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_MediaPlayerElement.cs
@@ -21,6 +21,8 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls;
 [RunsOnUIThread]
 public partial class Given_MediaPlayerElement
 {
+	//Currently failing on WASM, due headless Chrome on CI
+#if !__WASM__
 	private static readonly Uri TestVideoUrl = new Uri("https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_5MB.mp4");
 
 	[TestMethod]
@@ -518,4 +520,5 @@ public partial class Given_MediaPlayerElement
 		}
 #endif
 	}
+#endif
 }
