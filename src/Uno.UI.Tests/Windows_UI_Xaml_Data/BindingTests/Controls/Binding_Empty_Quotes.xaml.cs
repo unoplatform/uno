630b590f32a402af0a3986bf36811770bc2cf8a4
Youssef Victor <youssefvictor00@gmail.com>
2023-06-26 12:46:52 +0300

    chore: Fix formatting

--- src/Uno.UI.Tests/Windows_UI_Xaml_Data/BindingTests/Controls/Binding_Empty_Quotes.xaml.cs
+++ src/Uno.UI.Tests/Windows_UI_Xaml_Data/BindingTests/Controls/Binding_Empty_Quotes.xaml.cs
@@ -20,11 +20,11 @@ namespace Uno.UI.Tests.Windows_UI_Xaml_Data.BindingTests.Controls
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Binding_Empty_Quotes : Page
-    {
-        public Binding_Empty_Quotes()
-        {
-            this.InitializeComponent();
-        }
-    }
+	public sealed partial class Binding_Empty_Quotes : Page
+	{
+		public Binding_Empty_Quotes()
+		{
+			this.InitializeComponent();
+		}
+	}
 }
