2e0ac4e2529447b54e3b3a1f5b30c9faaefa3310
Martin Zikmund <martin@zikmund.dev>
2023-07-05 17:31:58 +0200

    test: GoBack verification

--- src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_WebView2.cs
+++ src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_WebView2.cs
@@ -69,7 +69,54 @@ public class Given_WebView2
 		navigationDone = false;
 		webView.NavigateToString("<html></html>");
 		await TestServices.WindowHelper.WaitFor(() => navigationDone, 3000);
+#if HAS_UNO
 		Assert.AreEqual(CoreWebView2.BlankUri, webView.Source);
+#endif
+	}
+
+
+	[TestMethod]
+	public async Task When_GoBack()
+	{
+		var border = new Border();
+		var webView = new WebView2();
+		webView.Width = 200;
+		webView.Height = 200;
+		border.Child = webView;
+		TestServices.WindowHelper.WindowContent = border;
+		bool navigated = false;
+		await TestServices.WindowHelper.WaitForLoaded(border);
+		await webView.EnsureCoreWebView2Async();
+
+		Assert.IsFalse(webView.CoreWebView2.CanGoBack);
+		Assert.IsFalse(webView.CanGoBack);
+		Assert.IsFalse(webView.CoreWebView2.CanGoForward);
+		Assert.IsFalse(webView.CanGoForward);
+
+		webView.NavigationCompleted += (sender, e) => navigated = true;
+		webView.CoreWebView2.Navigate("https://www.google.com/");
+		await TestServices.WindowHelper.WaitFor(() => navigated);
+
+		Assert.IsFalse(webView.CoreWebView2.CanGoBack);
+		Assert.IsFalse(webView.CanGoBack);
+		Assert.IsFalse(webView.CoreWebView2.CanGoForward);
+		Assert.IsFalse(webView.CanGoForward);
+
+		navigated = false;
+		webView.CoreWebView2.Navigate("https://www.bing.com/");
+		await TestServices.WindowHelper.WaitFor(() => navigated);
+
+		Assert.IsTrue(webView.CoreWebView2.CanGoBack);
+		Assert.IsTrue(webView.CanGoBack);
+
+		navigated = false;
+		webView.GoBack();
+		await TestServices.WindowHelper.WaitFor(() => navigated);
+
+		Assert.IsFalse(webView.CoreWebView2.CanGoBack);
+		Assert.IsFalse(webView.CanGoBack);
+		Assert.IsTrue(webView.CoreWebView2.CanGoForward);
+		Assert.IsTrue(webView.CanGoForward);
 	}
 
 #if __ANDROID__ || __IOS__
@@ -227,7 +274,6 @@ public class Given_WebView2
 
 		await TestHelper.RetryAssert(Do, 3);
 	}
-
 
 	[TestMethod]
 	public async Task When_ExecuteScriptAsync_Non_String()
