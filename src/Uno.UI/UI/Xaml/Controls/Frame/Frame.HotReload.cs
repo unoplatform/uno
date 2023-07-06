25fe06fbc4f66dc81a87ddf144953c8f5609db91
Nick Randolph <nick@builttoroam.com>
2023-07-06 17:42:48 +1000

    feat: Adding handlers for frame and page

---
+++ src/Uno.UI/UI/Xaml/Controls/Frame/Frame.HotReload.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection.Metadata;
+using Uno.UI.Helpers;
+
+[assembly: ElementMetadataUpdateHandlerAttribute(typeof(Windows.UI.Xaml.Controls.Frame), typeof(Windows.UI.Xaml.Controls.FrameElementMetadataUpdateHandler))]
+
+namespace Windows.UI.Xaml.Controls
+{
+	public partial class FrameElementMetadataUpdateHandler
+	{
+		public static void ElementUpdate(FrameworkElement element, Type[] updatedTypes)
+		{
+			var frame = element as Frame;
+			if (frame is null)
+			{
+				return;
+			}
+
+			foreach (var entry in frame.BackStack)
+			{
+				var expectedType = entry.SourcePageType.GetReplacementType();
+				if (entry.Instance is not null &&
+					entry.Instance.GetType() != expectedType)
+				{
+					var dc = entry.Instance.DataContext;
+					entry.Instance = Activator.CreateInstance(expectedType) as Page;
+					entry.Instance.Frame = frame;
+					if (entry.Instance is not null)
+					{
+						entry.Instance.DataContext = dc;
+					}
+				}
+
+				if (entry.SourcePageType is not null &&
+					entry.SourcePageType != expectedType)
+				{
+					entry.SourcePageType = expectedType;
+				}
+			}
+		}
+	}
+}
---
+++ src/Uno.UI/UI/Xaml/Controls/Page/Page.HotReload.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection.Metadata;
+using Uno.UI.Helpers;
+
+[assembly: ElementMetadataUpdateHandlerAttribute(typeof(Windows.UI.Xaml.Controls.Page), typeof(Windows.UI.Xaml.Controls.PageElementMetadataUpdateHandler))]
+
+namespace Windows.UI.Xaml.Controls
+{
+	public partial class PageElementMetadataUpdateHandler
+	{
+		public static void AfterElementReplaced(FrameworkElement oldView, FrameworkElement newView, Type[] updatedTypes)
+		{
+			if (oldView is Page oldPage &&
+				newView is Page newPage)
+			{
+				newPage.Frame = oldPage.Frame;
+
+				// If we've replaced the Page in its frame, we may need to
+				// swap the content property as well. If may be required
+				// if the frame is handled by a (native) FramePresenter.
+				newPage.Frame.Content = newPage;
+			}
+		}
+	}
+}
