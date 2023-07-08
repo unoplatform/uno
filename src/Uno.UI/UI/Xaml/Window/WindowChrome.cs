2dd5f570631b4bf6911cc4a9be3a72c8bee0ccf7
Martin Zikmund <martin@zikmund.dev>
2023-07-08 13:48:17 +0200

    chore: WindowChrome

---
+++ src/Uno.UI/UI/Xaml/Window/WindowChrome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.UI.UI.Xaml.Window;
+public sealed class WindowChrome : Microsoft.UI.Xaml.Controls.ContentControl
+{
+	private WindowChrome()
+	{
+	}
+
+	public WindowChrome(Microsoft.UI.Xaml.Window parent)
+	{
+	}
+}
