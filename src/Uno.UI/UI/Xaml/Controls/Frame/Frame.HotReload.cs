fe566d0e9c447b70af3eb9f48d60cba170d50b7f
Nick Randolph <nick@builttoroam.com>
2023-07-10 14:52:31 +1000

    chore: Logging and comments

--- src/Uno.UI/UI/Xaml/Controls/Frame/Frame.HotReload.cs
+++ src/Uno.UI/UI/Xaml/Controls/Frame/Frame.HotReload.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Reflection.Metadata;
+using Uno.Foundation.Logging;
 using Uno.UI.Helpers;
 
 [assembly: ElementMetadataUpdateHandlerAttribute(typeof(Windows.UI.Xaml.Controls.Frame), typeof(Windows.UI.Xaml.Controls.FrameElementMetadataUpdateHandler))]
 
 namespace Windows.UI.Xaml.Controls
 {
-	public partial class FrameElementMetadataUpdateHandler
+	internal static partial class FrameElementMetadataUpdateHandler
 	{
 		public static void ElementUpdate(FrameworkElement element, Type[] updatedTypes)
 		{
 			var frame = element as Frame;
 			if (frame is null)
 			{
+				if (typeof(FrameElementMetadataUpdateHandler).Log().IsEnabled(LogLevel.Warning))
+				{
+					typeof(FrameElementMetadataUpdateHandler).Log().LogWarning($"AfterElementReplaced should only be called with Frame instances");
+				}
 				return;
 			}
 
@@ -22,11 +27,15 @@ namespace Windows.UI.Xaml.Controls
 				if (entry.Instance is not null &&
 					entry.Instance.GetType() != expectedType)
 				{
+					if (typeof(FrameElementMetadataUpdateHandler).Log().IsEnabled(LogLevel.Trace))
+					{
+						typeof(FrameElementMetadataUpdateHandler).Log().Trace($"Backstack entry instance {entry.Instance.GetType().Name} replaced by instance of {expectedType.Name}");
+					}
 					var dc = entry.Instance.DataContext;
 					entry.Instance = Activator.CreateInstance(expectedType) as Page;
-					entry.Instance.Frame = frame;
 					if (entry.Instance is not null)
 					{
+						entry.Instance.Frame = frame;
 						entry.Instance.DataContext = dc;
 					}
 				}
@@ -34,6 +43,10 @@ namespace Windows.UI.Xaml.Controls
 				if (entry.SourcePageType is not null &&
 					entry.SourcePageType != expectedType)
 				{
+					if (typeof(FrameElementMetadataUpdateHandler).Log().IsEnabled(LogLevel.Trace))
+					{
+						typeof(FrameElementMetadataUpdateHandler).Log().Trace($"Backstack entry SourcePageType changed from {entry.SourcePageType.Name} to {expectedType.Name}");
+					}
 					entry.SourcePageType = expectedType;
 				}
 			}
