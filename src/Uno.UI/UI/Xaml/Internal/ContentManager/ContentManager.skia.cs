434394e10e09ff97ec83af590cddaf30f4323551
Martin Zikmund <martin@zikmund.dev>
2023-07-12 18:17:51 +0200

    chore: Move Window/Island content management into ContentManager

---
+++ src/Uno.UI/UI/Xaml/Internal/ContentManager/ContentManager.skia.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Uno.UI.Xaml.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.Xaml.Controls;
+
+partial class ContentManager
+{
+	private void CustomSetContent(UIElement content)
+	{
+		//if (_rootVisual is null)
+		//{
+			//_rootBorder = new Border();
+			//CoreServices.Instance.PutVisualRoot(_rootBorder);
+		//	//_rootVisual = CoreServices.Instance.MainRootVisual;
+
+		//	if (_rootVisual?.XamlRoot is null)
+		//	{
+		//		throw new InvalidOperationException("The root visual was not created.");
+		//	}
+		//}
+
+		if (_rootBorder != null)
+		{
+			_rootBorder.Child = _content = content;
+		}
+
+		TryLoadRootVisual();
+	}
+
+	private void TryLoadRootVisual()
+	{
+		if (!_shown)
+		{
+			return;
+		}
+
+		UIElement.LoadingRootElement(_rootVisual);
+
+		_rootVisual.XamlRoot!.InvalidateMeasure();
+
+		UIElement.RootElementLoaded(_rootVisual);
+	}
+}
