1fd33efb5e69240ca911e6c064b4591684cdde13
Martin Zikmund <martin@zikmund.dev>
2023-07-12 18:19:18 +0200

    chore: Carve out Window implementation into separate classes

---
+++ src/Uno.UI/UI/Xaml/Window/Implementations/DesktopXamlSourceWindow.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Hosting;
+
+namespace Uno.UI.Xaml.Controls;
+
+internal class DesktopXamlSourceWindow : IWindowImplementation
+{
+	private readonly Window _window;
+
+	private readonly WindowChrome _windowChrome;
+	private readonly DesktopWindowXamlSource _desktopWindowXamlSource;
+
+	public DesktopXamlSourceWindow(Window window)
+	{
+		_window = window;
+		_windowChrome = new WindowChrome(window);
+		_desktopWindowXamlSource = new DesktopWindowXamlSource();
+		_desktopWindowXamlSource.Content = _windowChrome;
+	}
+
+	public UIElement? Content
+	{
+		get => _windowChrome.Content as UIElement;
+		set => _windowChrome.Content = value;
+	}
+}
