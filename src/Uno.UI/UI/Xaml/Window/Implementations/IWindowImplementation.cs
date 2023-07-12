63f4820c9f70f079b6fa7d85ac945100ba34b7bc
Martin Zikmund <martin@zikmund.dev>
2023-07-12 18:19:18 +0200

    chore: Carve out Window implementation into separate classes

---
+++ src/Uno.UI/UI/Xaml/Window/Implementations/IWindowImplementation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace Uno.UI.Xaml.Controls;
+
+internal interface IWindowImplementation
+{
+	//bool Visible { get; }
+
+	//Rect Bounds { get; }
+
+	//CoreWindow CoreWindow { get; }
+
+	UIElement Content { get; set; }
+
+	//event TypedEventHandler<object, WindowActivatedEventArgs> Activated;
+
+	//event TypedEventHandler<object, EventArgs> Closed;
+
+	//event TypedEventHandler<object, WindowSizeChangedEventArgs> SizeChanged;
+
+	//event TypedEventHandler<object, VisibilityChangedEventArgs> VisibilityChanged;
+
+	//void Activate();
+
+	//void Close();
+}
