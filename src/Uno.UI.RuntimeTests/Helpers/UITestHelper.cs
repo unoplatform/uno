50dc5917cde254bb3f35279305eb346cfb60241a
Ramez Ragaa <ramezragaa@proton.me>
2023-07-21 19:27:57 +0300

    chore: fix interpolation algorithm in UITestHelper and MouseHelper
    (cherry picked from commit 3326f51f8c69515eabcaeb873c84a4de0c491f39)

--- src/Uno.UI.RuntimeTests/Helpers/UITestHelper.cs
+++ src/Uno.UI.RuntimeTests/Helpers/UITestHelper.cs
@@ -313,8 +313,10 @@ public class Mouse : IInjectedPointer, IDisposable
 
 	private IEnumerable<InjectedInputMouseInfo> GetMoveTo(double x, double y, uint? steps)
 	{
-		var deltaX = x - Current.X;
-		var deltaY = y - Current.Y;
+		var x0 = Current.X;
+		var y0 = Current.Y;
+		var deltaX = x - x0;
+		var deltaY = y - y0;
 
 		steps ??= (uint)Math.Min(Math.Max(Math.Abs(deltaX), Math.Abs(deltaY)), 512);
 		if (steps is 0)
@@ -322,25 +324,22 @@ public class Mouse : IInjectedPointer, IDisposable
 			yield break;
 		}
 
+		// Could probably use Bresenham's algorithm if performance issues appear
 		var stepX = deltaX / steps.Value;
 		var stepY = deltaY / steps.Value;
 
-		stepX = stepX is > 0 ? Math.Ceiling(stepX) : Math.Floor(stepX);
-		stepY = stepY is > 0 ? Math.Ceiling(stepY) : Math.Floor(stepY);
-
-		for (var step = 0; step <= steps && (stepX is not 0 || stepY is not 0); step++)
+		var prevPositionX = (int)Math.Round(x0);
+		var prevPositionY = (int)Math.Round(y0);
+		
+		for (var i = 1; i <= steps; i++)
 		{
-			yield return GetMoveBy((int)stepX, (int)stepY);
+			var newPositionX = (int)Math.Round(x0 + i * stepX);
+			var newPositionY = (int)Math.Round(y0 + i * stepY);
 
-			if (Math.Abs(Current.X - x) < stepX)
-			{
-				stepX = 0;
-			}
+			yield return GetMoveBy(newPositionX - prevPositionX, newPositionY - prevPositionY);
 
-			if (Math.Abs(Current.Y - y) < stepY)
-			{
-				stepY = 0;
-			}
+			prevPositionX = newPositionX;
+			prevPositionY = newPositionY;
 		}
 	}
 
