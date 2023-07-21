c6a9b386556ea25c1a11b82967c64022ee6af85e
Youssef Victor <youssefvictor00@gmail.com>
2023-07-21 09:56:23 +0300

    refactor: Cleanup dead code in foundation

--- src/Uno.Foundation/Uno.Core.Extensions/Uno.Core.Extensions.Compatibility/Extensions/ObjectExtensions.cs
+++ src/Uno.Foundation/Uno.Core.Extensions/Uno.Core.Extensions.Compatibility/Extensions/ObjectExtensions.cs
@@ -22,43 +22,13 @@ namespace Uno.Extensions
 {
 	internal static class ObjectExtensions
 	{
-		public static ExtensionPoint<T> Extensions<T>(this T value)
-		{
-			return new ExtensionPoint<T>(value);
-		}
-
-		public static bool IsDefault<T>(this ExtensionPoint<T> extensionPoint)
-		{
-			return Equals(extensionPoint.ExtendedValue, default(T));
-		}
-
-		public static void Dispose<T>(this ExtensionPoint<T> extensionPoint)
-		{
-			var disposable = extensionPoint.ExtendedValue as IDisposable;
-
-			disposable.Maybe(d => d.Dispose());
-		}
-
-		public static void Maybe<TInstance>(this TInstance instance, Action action)
-		{
-			if (instance != null)
-			{
-				action();
-			}
-		}
-
-		public static void Maybe<TInstance>(this TInstance instance, Action<TInstance> action)
-		{
-			if (instance != null)
-			{
-				action(instance);
-			}
-		}
-
 		public static void Maybe<TInstance>(this object instance, Action<TInstance> action)
 			where TInstance : class
 		{
-			Maybe<TInstance>(instance as TInstance, action);
+			if (instance is TInstance tInstance)
+			{
+				action(tInstance);
+			}
 		}
 
 		public static TResult SelectOrDefault<TInstance, TResult>(this TInstance instance, Func<TInstance, TResult> selector)
@@ -85,15 +55,6 @@ namespace Uno.Extensions
 		}
 
 		/// <summary>
-		/// A helper method to allow for locally defined extension-method like methods. Avoids the creation of an external static class
-		/// in the context of fluent expressions.
-		/// </summary>
-		public static TResult Apply<TSource, TResult>(this TSource source, Func<TSource, TResult> selector)
-		{
-			return selector(source);
-		}
-
-		/// <summary>
 		/// A helper method that allows the execution of an action in a fluent expression.
 		/// </summary>
 		/// <param name="action">The action to execute on the source object</param>
@@ -103,53 +64,6 @@ namespace Uno.Extensions
 			action(source);
 
 			return source;
-		}
-
-		/// <summary>
-		/// A helper method that allows the execution of an action in a fluent expression. The action will be executed if the condition is true.
-		/// </summary>
-		/// <param name="condition">A boolean value that indicates if the action should be executed.</param>
-		/// <param name="action">The action to execute, the parameter will contain source</param>
-		/// <returns>Returns the source instance</returns>
-		public static TSource Apply<TSource>(this TSource source, bool condition, Action<TSource> action)
-		{
-			if (condition)
-			{
-				action(source);
-			}
-
-			return source;
-		}
-
-		/// <summary>
-		/// Gets a boolean value that determines if a specific value is within a list of accepted values.
-		/// Use this when it's not necessary or when it's overkill to declare the list of accepted values
-		/// as a readonly field. For example, can be used to check if one enum value is within a set without that
-		/// enum being marked as [Flags].
-		/// </summary>
-		/// <typeparam name="TSource"></typeparam>
-		/// <param name="source">The source value.</param>
-		/// <param name="acceptedValues">The list of accepted values.</param>
-		/// <returns></returns>
-		public static bool IsOneOf<TSource>(this TSource source, params TSource[] acceptedValues)
-		{
-			return acceptedValues?.Contains(source) ?? false;
-		}
-
-		/// <summary>
-		/// Gets a boolean value that determines if a specific value is within a list of accepted values.
-		/// Use this when it's not necessary or when it's overkill to declare the list of accepted values
-		/// as a readonly field. For example, can be used to check if one enum value is within a set without that
-		/// enum being marked as [Flags].
-		/// </summary>
-		/// <typeparam name="TSource"></typeparam>
-		/// <param name="source">The source value.</param>
-		/// <param name="comparer">The comparer to use to determine equality.</param>
-		/// <param name="acceptedValues">The list of accepted values.</param>
-		/// <returns></returns>
-		public static bool IsOneOf<TSource>(this TSource source, IEqualityComparer<TSource> comparer, params TSource[] acceptedValues)
-		{
-			return acceptedValues?.Contains(source, comparer) ?? false;
 		}
 	}
 }
