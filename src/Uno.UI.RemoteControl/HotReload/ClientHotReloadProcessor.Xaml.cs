3826337ea82cc1eb42be4bd9d0c550e0c7d28c0d
Nick Randolph <nick@builttoroam.com>
2023-07-25 00:49:08 +1000

    chore: Removing unused code

--- src/Uno.UI.RemoteControl/HotReload/ClientHotReloadProcessor.Xaml.cs
+++ src/Uno.UI.RemoteControl/HotReload/ClientHotReloadProcessor.Xaml.cs
@@ -106,19 +106,6 @@ namespace Uno.UI.RemoteControl.HotReload
 			{
 				parentAsContentControl.Content = newView;
 			}
-			else if (newView is Page newPage && oldView is Page oldPage)
-			{
-				// In the case of Page, swapping the actual page is not supported, so we
-				// need to swap the content of the page instead. This can happen if the Frame
-				// is using a native presenter which does not use the `Frame.Content` property.
-
-				// Clear any local context, so that the new page can inherit the value coming
-				// from the parent Frame. It may happen if the old page set it explicitly.
-				oldPage.ClearValue(Page.DataContextProperty, DependencyPropertyValuePrecedences.Local);
-
-				oldPage.Content = newPage;
-				newPage.Frame = oldPage.Frame;
-			}
 			else
 			{
 				VisualTreeHelper.SwapViews(oldView, newView);
