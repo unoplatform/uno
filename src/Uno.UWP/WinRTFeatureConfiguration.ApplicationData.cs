8d92d3e38bbe1c84ea8334e1542592844b1dcb9e
Martin Zikmund <martin@zikmund.dev>
2023-07-25 15:12:05 +0200

    feat: Add feature flag for path overriding

---
+++ src/Uno.UWP/WinRTFeatureConfiguration.ApplicationData.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+
+namespace Uno;
+
+partial class WinRTFeatureConfiguration
+{
+	public static class ApplicationData
+	{
+#if __SKIA__
+		/// <summary>
+		/// Allows overriding the root folder path that the application will use
+		/// to store its TempState folder.
+		/// </summary>
+		/// <remarks>Only applies to Skia targets.</remarks>
+		public static string? TemporaryFolderPathOverride { get; set; }
+
+		/// <summary>
+		/// Allows overriding the root folder that the application will use
+		/// to store its application data folders (LocalFolder, RoamingFolder, etc.).
+		/// Only applies to Skia targets.
+		/// </summary>
+		/// <remarks>Only applies to Skia targets.</remarks>
+		public static string? ApplicationDataPathOverride { get; set; }
+#endif
+	}
+}
