ce4d8ca797b062e43c468c621d7a8f665e3ac726
Youssef Victor <youssefvictor00@gmail.com>
2023-07-28 15:17:50 +0300

    chore: Fix build error

--- src/Uno.UI/UI/Xaml/Media/Imaging/SvgImageSource.skia.cs
+++ src/Uno.UI/UI/Xaml/Media/Imaging/SvgImageSource.skia.cs
@@ -20,7 +20,7 @@ partial class SvgImageSource
 			{
 				if (AbsoluteUri.Scheme.Equals("http", StringComparison.OrdinalIgnoreCase) ||
 					AbsoluteUri.Scheme.Equals("https", StringComparison.OrdinalIgnoreCase) ||
-					AbsoluteUri.Isfile)
+					AbsoluteUri.IsFile)
 				{
 					using var imageStream = await OpenStreamFromUriAsync(UriSource, ct);
 					return await ReadFromStreamAsync(imageStream, ct);
