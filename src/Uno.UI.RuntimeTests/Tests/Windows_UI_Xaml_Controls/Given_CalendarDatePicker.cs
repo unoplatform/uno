583ae1a925633690986e5fcceee82adb38bdabf2
Martin Zikmund <martin@zikmund.dev>
2023-08-01 13:29:40 +0200

    test: Verify CalendarDatePicker theming

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_CalendarDatePicker.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_CalendarDatePicker.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MUXControlsTestApp.Utilities;
+using Uno.UI.RuntimeTests.Helpers;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
 using static Private.Infrastructure.TestServices;
 
 namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls;
@@ -32,6 +35,98 @@ public class Given_CalendarDatePicker
 		Assert.IsTrue(calendarView.ActualHeight > 300);
 
 		flyout.Close();
+	}
+#endif
+
+#if !WINDOWS_UWP && !__MACOS__
+	[TestMethod]
+	public async Task When_Theme_Changes()
+	{
+		CalendarDatePicker datePicker = new CalendarDatePicker();
+		WindowHelper.WindowContent = datePicker;
+
+		await WindowHelper.WaitForLoaded(datePicker);
+
+		//Open flyout
+		var root = (Grid)datePicker.FindName("Root");
+		var flyout = (Flyout)FlyoutBase.GetAttachedFlyout(root);
+
+		try
+		{
+			bool opened = false;
+			flyout.Opened += async (sender, args) =>
+			{
+				opened = true;
+			};
+
+			datePicker.IsCalendarOpen = true;
+
+			await WindowHelper.WaitFor(() => opened);
+
+			var items = VisualTreeUtils.FindVisualChildrenByType<CalendarViewDayItem>(flyout.Content).ToArray();
+			Assert.IsTrue(items.Length > 0);
+			foreach (var item in items)
+			{
+				var foreground = ((SolidColorBrush)item.Foreground).Color;
+				Assert.IsTrue(foreground.Luminance < 0.5);
+
+				if (item.GetItemBackgroundBrush() is not { } backgroundBrush)
+				{
+					continue;
+				}
+				var background = ((SolidColorBrush)backgroundBrush).Color;
+
+				// Skip colored dates (selected), or those with opacity of zero.
+				if (background.R == background.G && background.G == background.B && background.A != 0)
+				{
+					Assert.IsTrue(background.Luminance > 0.5);
+				}
+			}
+
+			//Close flyout
+			datePicker.IsCalendarOpen = false;
+
+			await WindowHelper.WaitForIdle();
+
+			//Change to dark theme
+			using var _ = ThemeHelper.UseDarkTheme();
+
+			opened = false;
+			//Open flyout
+			datePicker.IsCalendarOpen = true;
+
+			await WindowHelper.WaitFor(() => opened);
+
+			items = VisualTreeUtils.FindVisualChildrenByType<CalendarViewDayItem>(flyout.Content).ToArray();
+			Assert.IsTrue(items.Length > 0);
+			foreach (var item in items)
+			{
+				var foreground = ((SolidColorBrush)item.Foreground).Color;
+				Assert.IsTrue(foreground.Luminance > 0.5);
+
+				if (item.GetItemBackgroundBrush() is not { } backgroundBrush)
+				{
+					continue;
+				}
+				var background = ((SolidColorBrush)backgroundBrush).Color;
+
+				// Skip colored dates (selected), or those with opacity of zero.
+				if (background.R == background.G && background.G == background.B && background.A != 0)
+				{
+					Assert.IsTrue(background.Luminance < 0.5);
+				}
+			}
+		}
+		catch(Exception)
+		{
+		}
+		finally
+		{
+			if (datePicker is not null)
+			{
+				datePicker.IsCalendarOpen = false;
+			}
+		}
 	}
 #endif
 }
