fe21790b54124fb02f1fe1ee5e67d7e0a22d8e04
Martin Zikmund <martin@zikmund.dev>
2023-08-02 15:36:51 +0200

    test: Recycling TextBox with TwoWay binding

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/TextBox/RecyclingTextBoxPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Uno.UI.RuntimeTests.TextBoxPages
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class RecyclingTextBoxPage : Page
+	{
+		public RecyclingTextBoxPage()
+		{
+			this.InitializeComponent();
+
+			DataContext = new RecyclingTextBoxPageItemViewModel();
+		}
+	}
+
+	public class RecyclingTextBoxPageItemViewModel : INotifyPropertyChanged
+	{
+		private string _text = Guid.NewGuid().ToString();
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public string Text
+		{
+			get => _text; 
+			set
+			{
+				_text = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+			}
+		}
+	}
+}
