4f384bbd05940678f53a7ca0179e27498d38d760
Martin Zikmund <martin@zikmund.dev>
2023-08-04 15:17:12 +0200

    test: Add editor switching with recycling test

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/TextBox/EditorsIssue/Editor2.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+
+using System.ComponentModel;
+
+namespace MobileTemplateSelectorIssue.Editors
+{
+	public sealed class Editor2 : IEditor
+	{
+		public string Text
+		{
+			get { return _text; }
+			set
+			{
+				if (_text != value)
+				{
+					_text = value;
+					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+				}
+			}
+		}
+		private string _text = "Editor2";
+
+		public event PropertyChangedEventHandler? PropertyChanged = null;
+	}
+}
---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/TextBox/EditorsIssue/TypedDataTemplateSelector.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+
+#nullable disable
+
+namespace MobileTemplateSelectorIssue.Editors
+{
+	[ContentProperty(Name = nameof(Templates))]
+	public class TypedDataTemplateSelector : DataTemplateSelector
+	{
+		public ObservableCollection<TypedDataTemplate> Templates { get; }
+		  = new ObservableCollection<TypedDataTemplate>();
+
+		public TypedDataTemplateSelector()
+		{
+			var incc = (INotifyCollectionChanged)Templates;
+			incc.CollectionChanged += (sender, e) =>
+			{
+				if (e?.NewItems.Cast<TypedDataTemplate>().Any(tdt => tdt?.DataType == null || tdt?.DataTemplate == null) == true)
+					throw new InvalidOperationException("All items must have all properties set.");
+			};
+		}
+
+		protected override DataTemplate SelectTemplateCore(object item,
+			DependencyObject container)
+		{
+			if (item == null)
+				return null;
+
+			if (!Templates.Any())
+				throw new InvalidOperationException("No DataTemplates found.");
+
+			var result = Templates.FirstOrDefault(t => t.DataType.IsAssignableFrom(item.GetType()));
+			if (result == null)
+				System.Diagnostics.Debug.WriteLine($"Could not find a matching template for type '{item.GetType()}'.");
+			else
+			{
+				Console.WriteLine($"Matched {result.TypeName} DataTemplate {item.GetType()}");
+			}
+
+			return result?.DataTemplate;
+		}
+	}
+
+	[ContentProperty(Name = nameof(DataTemplate))]
+	public class TypedDataTemplate
+	{
+		public string TypeName { get; set; }
+		public Type DataType { get; set; }
+		public DataTemplate DataTemplate { get; set; }
+	}
+}
