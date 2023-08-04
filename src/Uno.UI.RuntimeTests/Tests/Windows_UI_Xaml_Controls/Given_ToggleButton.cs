65929950e770db7e22c05cfbb9055125869b7e6e
Martin Zikmund <martin@zikmund.dev>
2023-08-04 12:57:55 +0200

    test: Verify recycling behavior disabling

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ToggleButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Uno.UI.RuntimeTests.Helpers;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls;
+
+[TestClass]
+[RunsOnUIThread]
+public class Given_ToggleButton
+{
+#if HAS_UNO
+	[TestMethod]
+	public async Task When_Recycling()
+	{
+		using var _ = FeatureConfigurationHelper.UseTemplatePooling();
+
+		var toggleButton = new ToggleButton() { IsChecked = true };
+		toggleButton.OnTemplateRecycled();
+
+		Assert.IsFalse(toggleButton.IsChecked);
+
+		toggleButton.IsTemplateRecyclingEnabled = false;
+
+		toggleButton.IsChecked = true;
+		toggleButton.OnTemplateRecycled();
+
+		Assert.IsTrue(toggleButton.IsChecked);
+	}
+#endif
+}
