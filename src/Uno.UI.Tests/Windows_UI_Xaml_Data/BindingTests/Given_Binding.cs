3df8f7dcc2525d2b0f739c988cb4473b4a33e26a
Youssef Victor <youssefvictor00@gmail.com>
2023-08-07 21:21:05 +0300

    chore: Fix bad merge

--- src/Uno.UI.Tests/Windows_UI_Xaml_Data/BindingTests/Given_Binding.cs
+++ src/Uno.UI.Tests/Windows_UI_Xaml_Data/BindingTests/Given_Binding.cs
@@ -200,14 +200,6 @@ namespace Uno.UI.Tests.Windows_UI_Xaml_Data.BindingTests
 		}
 
 		[TestMethod]
-		public void When_Binding_Empty_Quotes()
-		{
-			var SUT = new Binding_Empty_Quotes();
-			SUT.ForceLoaded();
-			SUT.sut.Text.Should().Be("Current DataContext: MyDataContext");
-		}
-
-		[TestMethod]
 		public void When_TemplateBinding_Attached_Property()
 		{
 			var SUT = new Binding_TemplateBinding_AttachedDP();
