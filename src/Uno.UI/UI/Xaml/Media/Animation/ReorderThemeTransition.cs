ee756d30157762a1478478cf8e12e59670cf9e98
Martin Zikmund <martin@zikmund.dev>
2023-08-08 11:41:29 +0200

    chore: Adjust for CI

--- src/Uno.UI/UI/Xaml/Media/Animation/ReorderThemeTransition.cs
+++ src/Uno.UI/UI/Xaml/Media/Animation/ReorderThemeTransition.cs
@@ -5,10 +5,10 @@ namespace Windows.UI.Xaml.Media.Animation;
 public partial class ReorderThemeTransition : global::Windows.UI.Xaml.Media.Animation.Transition
 {
 	[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
-	public ReorderThemeTransition() 
+	public ReorderThemeTransition()
 	{
 		global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented(
-			"Windows.UI.Xaml.Media.Animation.ReorderThemeTransition", 
+			"Windows.UI.Xaml.Media.Animation.ReorderThemeTransition",
 			"ReorderThemeTransition.ReorderThemeTransition()",
 			LogLevel.Debug);
 	}
