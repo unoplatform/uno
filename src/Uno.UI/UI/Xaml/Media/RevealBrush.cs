67657f1bd1ead606082c1db49cf015f1b3910902
Martin Zikmund <martin@zikmund.dev>
2023-08-10 17:47:15 +0200

    fix: Use fallback color for unsupported brushes
    (cherry picked from commit a6f162abdeedf9234cfe288b5e8c0b0fca185ca4)
    
    # Conflicts:
    #	src/Uno.UI/UI/Xaml/Media/RevealBrush.cs

--- src/Uno.UI/UI/Xaml/Media/RevealBrush.cs
+++ src/Uno.UI/UI/Xaml/Media/RevealBrush.cs
@@ -1,11 +1,11 @@
-﻿
-namespace Windows.UI.Xaml.Media
-{
-	public partial class RevealBrush : XamlCompositionBrushBase
-	{
-		public RevealBrush()
-		{
+﻿namespace Windows.UI.Xaml.Media;
 
+public partial class RevealBrush : XamlCompositionBrushBase
+{
+	public RevealBrush()
+	{
+
+<<<<<<< HEAD
 		}
 
 		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "NET461", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
@@ -27,5 +27,27 @@ namespace Windows.UI.Xaml.Media
 			nameof(Color), typeof(global::Windows.UI.Color),
 			typeof(global::Windows.UI.Xaml.Media.RevealBrush),
 			new FrameworkPropertyMetadata(default(global::Windows.UI.Color)));
+=======
+>>>>>>> a6f162abde (fix: Use fallback color for unsupported brushes)
 	}
+
+	[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
+	public global::Windows.UI.Color Color
+	{
+		get
+		{
+			return (global::Windows.UI.Color)this.GetValue(ColorProperty);
+		}
+		set
+		{
+			this.SetValue(ColorProperty, value);
+		}
+	}
+
+	[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
+	public static global::Windows.UI.Xaml.DependencyProperty ColorProperty { get; } =
+	Windows.UI.Xaml.DependencyProperty.Register(
+		nameof(Color), typeof(global::Windows.UI.Color),
+		typeof(global::Windows.UI.Xaml.Media.RevealBrush),
+		new FrameworkPropertyMetadata(default(global::Windows.UI.Color)));
 }
--- src/Uno.UI/UI/Xaml/Media/XamlCompositionBrushBase.Android.cs
+++ src/Uno.UI/UI/Xaml/Media/XamlCompositionBrushBase.Android.cs
@@ -3,13 +3,12 @@ using Rect = Windows.Foundation.Rect;
 
 namespace Windows.UI.Xaml.Media;
 
-partial class RevealBrush
+public partial class XamlCompositionBrushBase : Brush
 {
 	protected override Paint GetPaintInner(Rect destinationRect)
 	{
-		var color = this.IsDependencyPropertySet(FallbackColorProperty) ?
-			GetColorWithOpacity(FallbackColor) :
-			GetColorWithOpacity(Color);
+		// By default fallback to FallbackColor, unless overridden by a derived class.
+		var color = GetColorWithOpacity(FallbackColor);
 		return new Paint() { Color = color, AntiAlias = true };
 	}
 }
