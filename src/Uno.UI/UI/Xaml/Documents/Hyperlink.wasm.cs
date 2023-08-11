9b4f7209edc117dfcb46543ff9380831a04eb206
Youssef Victor <youssefvictor00@gmail.com>
2023-08-11 21:21:39 +0300

    chore: Fix conflict

--- src/Uno.UI/UI/Xaml/Documents/Hyperlink.wasm.cs
+++ src/Uno.UI/UI/Xaml/Documents/Hyperlink.wasm.cs
@@ -16,15 +16,7 @@ namespace Windows.UI.Xaml.Documents
 			PointerPressed += TextBlock.OnPointerPressed;
 			PointerReleased += TextBlock.OnPointerReleased;
 			PointerCaptureLost += TextBlock.OnPointerCaptureLost;
-<<<<<<< HEAD
-<<<<<<< HEAD
-			ResourceResolver.ApplyResource(this, Hyperlink.ForegroundProperty, "SystemControlHyperlinkTextBrush", isThemeResourceExtension: true);
-
-=======
->>>>>>> ef087507bb (chore: Adjust on Wasm as well)
-=======
 			OnUnderlineStyleChanged();
->>>>>>> cb59b12e84 (chore: Small fix on Wasm)
 		}
 
 		#region NavigationTarget DependencyProperty
