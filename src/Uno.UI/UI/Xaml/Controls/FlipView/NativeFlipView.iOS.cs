8d8cfafc69193b4eabd7792d77e3cf9dddad96e2
Youssef Victor <youssefvictor00@gmail.com>
2023-08-11 11:38:06 +0300

    fix: Fix FlipView background not working on iOS

--- src/Uno.UI/UI/Xaml/Controls/FlipView/NativeFlipView.iOS.cs
+++ src/Uno.UI/UI/Xaml/Controls/FlipView/NativeFlipView.iOS.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Windows.UI.Xaml.Media;
 
 namespace Windows.UI.Xaml.Controls
 {
 	public partial class NativeFlipView : PagedCollectionView
 	{
+		private Brush _background;
+
 		public NativeFlipView()
 		{
 			ShowsHorizontalScrollIndicator = false;
+		}
+
+		public Brush Background
+		{
+			get => _background;
+			set
+			{
+				_background = value;
+				BackgroundColor = Brush.GetColorWithOpacity(_background);
+			}
 		}
 	}
 }
