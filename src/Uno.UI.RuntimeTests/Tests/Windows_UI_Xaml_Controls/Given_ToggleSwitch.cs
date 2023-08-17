41449d50a019cca01f9001525723f62d8af282ad
Youssef Victor <youssefvictor00@gmail.com>
2023-08-17 18:26:09 +0300

    chore: Workaround test failure on iOS
    (cherry picked from commit 8f283557965592a06efbd3cdadd9b81e914d7879)

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ToggleSwitch.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ToggleSwitch.cs
@@ -15,6 +15,7 @@ public class Given_ToggleSwitch
 		var toggleSwitch = new ToggleSwitch() { IsOn = true };
 		WindowHelper.WindowContent = toggleSwitch;
 		await WindowHelper.WaitForLoaded(toggleSwitch);
+		await WindowHelper.WaitForIdle();
 		var minKnobTranslation = (double)typeof(ToggleSwitch).GetField("_minKnobTranslation", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(toggleSwitch);
 		var maxKnobTranslation = (double)typeof(ToggleSwitch).GetField("_maxKnobTranslation", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(toggleSwitch);
 		Assert.AreEqual(0, minKnobTranslation);
