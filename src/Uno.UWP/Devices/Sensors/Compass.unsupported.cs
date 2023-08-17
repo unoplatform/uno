bf8c2b919bb83438921195b2775f19c90082a462
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2023-08-18 00:08:47 +0200

    chore: Return null on unsupported

---
+++ src/Uno.UWP/Devices/Sensors/Compass.unsupported.cs
@@ -0,0 +1,16 @@
+﻿#if __MACOS__ || IS_UNIT_TESTS || __WASM__ || __NETSTD_REFERENCE__
+namespace Windows.Devices.Sensors;
+
+public class Compass
+{
+	private Compass()
+	{
+	}
+
+	/// <summary>
+	/// API not supported, always returns null.
+	/// </summary>
+	/// <returns>Null.</returns>
+	public static Compass GetDefault() => null;
+}
+#endif
