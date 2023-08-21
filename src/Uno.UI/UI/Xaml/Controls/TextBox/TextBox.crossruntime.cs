d62a54fefa8a3847ec31fc980cf2451baf9fddcc
Martin Zikmund <martin@zikmund.dev>
2023-08-21 19:20:24 +0200

    feat(TextBox): Support for EnterKeyHint on WebAssembly

---
+++ src/Uno.UI/UI/Xaml/Controls/TextBox/TextBox.crossruntime.cs
@@ -0,0 +1,31 @@
+﻿using Uno.UI.Xaml.Controls;
+
+namespace Windows.UI.Xaml.Controls;
+
+partial class TextBox
+{
+	/// <summary>
+	/// Gets or sets the valu indicting how the Enter 
+	/// key should appear on the virtual keyboard.
+	/// </summary>
+	public EnterKeyHint EnterKeyHint
+	{
+		get => (EnterKeyHint)GetValue(EnterKeyHintProperty);
+		set => SetValue(EnterKeyHintProperty, value);
+	}
+
+	/// <summary>
+	/// Indentifies the EnterKeyHint dependency property.
+	/// </summary>
+	public static DependencyProperty EnterKeyHintProperty { get; } =
+		DependencyProperty.Register(
+			nameof(EnterKeyHint),
+			typeof(EnterKeyHint),
+			typeof(TextBox),
+			new FrameworkPropertyMetadata(
+				EnterKeyHint.Default,
+				(s, e) => ((TextBox)s)?.OnEnterKeyHintChangedPartial((EnterKeyHint)e.NewValue)
+			));
+
+	partial void OnEnterKeyHintChangedPartial(EnterKeyHint enterKeyHint);
+}
