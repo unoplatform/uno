0cc82966159072c25b2126909333bc8c7cf6ff66
Youssef Victor <youssefvictor00@gmail.com>
2023-08-21 09:01:31 +0300

    chore: Remove workaround

--- src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Media.Animation/RepositionThemeAnimation.cs
+++ src/Uno.UI/Generated/3.0.0.0/Windows.UI.Xaml.Media.Animation/RepositionThemeAnimation.cs
@@ -3,7 +3,7 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.UI.Xaml.Media.Animation
 {
-#if __ANDROID__ || __IOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+#if false || false || false || false || false || false || false
 	[global::Uno.NotImplemented]
 #endif
 	public partial class RepositionThemeAnimation : global::Windows.UI.Xaml.Media.Animation.Timeline
