34002d761fa9bb4b6f5dfc418ca992e7b47a950c
Ramez Ragaa <ramezragaa@proton.me>
2023-08-22 00:52:05 +0300

    chore: make implementation better aligned with WinUI

--- src/Uno.UI/UI/Xaml/Controls/VirtualizingPanel.cs
+++ src/Uno.UI/UI/Xaml/Controls/VirtualizingPanel.cs
@@ -1,5 +1,6 @@
 #pragma warning disable 108 // new keyword hiding
 using System;
+using Windows.Foundation;
 
 namespace Windows.UI.Xaml.Controls
 {
@@ -15,32 +16,5 @@ namespace Windows.UI.Xaml.Controls
 		private protected virtual VirtualizingPanelLayout GetLayouterCore() => throw new NotSupportedException($"This method must be overridden by implementing classes.");
 
 		internal override Orientation? InternalOrientation => GetLayouter().Orientation;
-
-		protected override Size MeasureOverride(Size availableSize)
-		{
-			// Copied from FrameworkElement.MeasureOverride
-			var child = this.FindFirstChild();
-			return child != null ? MeasureElement(child, availableSize) : new Size(0, 0);
-		}
-
-		protected override Size ArrangeOverride(Size finalSize)
-		{
-			// Copied from FrameworkElement.ArrangeOVerride
-			var child = this.FindFirstChild();
-
-			if (child != null)
-			{
-#if UNO_REFERENCE_API
-				child.Arrange(new Rect(0, 0, finalSize.Width, finalSize.Height));
-#else
-				ArrangeElement(child, new Rect(0, 0, finalSize.Width, finalSize.Height));
-#endif
-				return finalSize;
-			}
-			else
-			{
-				return finalSize;
-			}
-		}
 	}
 }
