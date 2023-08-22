9119170a5f98d23be08a5ad7bd021bfa1ca920b5
Youssef Victor <youssefvictor00@gmail.com>
2023-08-22 21:00:46 +0300

    fix: Fix path stroke thickness scaling on Skia when it shouldn't

--- src/Uno.UI.Composition/Composition/CompositionSpriteShape.skia.cs
+++ src/Uno.UI.Composition/Composition/CompositionSpriteShape.skia.cs
@@ -35,11 +35,52 @@ namespace Windows.UI.Composition
 
 					// Generate stroke geometry for bounds that will be passed to a brush.
 					// - [Future]: This generated geometry should also be used for hit testing.
-					using var strokeGeometry = strokePaint.GetFillPath(geometry);
 
+					// If we have something like this:
+					// <Path Data="M 0 0 L 50 0 L 50 50 L 0 50 z"
+					//		 Stroke="Red"
+					//		 StrokeThickness="5"
+					//		 Width="70"
+					//		 Stretch="Fill"
+					//		 HorizontalAlignment="Center"
+					//		 VerticalAlignment="Center" />
+					// The geometry itself is a 50x50 rectangle, and then we set the shape Width to 70 and let it
+					// to stretch over the available height, and we have a stroke thickness as 1px
+					// On Windows, the stroke is simply 1px, it doesn't scale with the height.
+					// So, to get a correct stroke geometry, we first need to scale the geometry, then get the stroke
+					// path from it. Then, before drawing we don't want to apply the scaling.
+					// Note that we only need to do so if there is a scaling (ie, value different than 1)
+					var shouldAdjustStrokeScaling = Scale.X != 1 || Scale.Y != 1;
+					var strokePath = geometry;
+					if (shouldAdjustStrokeScaling)
+					{
+						// We don't want to affect the original SKPath, so we create a new one and apply the scaling transformation on it.
+						strokePath = new SKPath();
+						var transform = SKMatrix.Identity;
+						transform.ScaleX = Scale.X;
+						transform.ScaleY = Scale.Y;
+						geometry.Transform(transform, strokePath);
+					}
+
+					// Get the stroke geometry, after scaling has been applied.
+					using var strokeGeometry = strokePaint.GetFillPath(strokePath);
 					stroke.UpdatePaint(fillPaint, strokeGeometry.Bounds);
 
-					session.Surface.Canvas.DrawPath(strokeGeometry, fillPaint);
+					if (shouldAdjustStrokeScaling)
+					{
+						// Keep the original matrix to set it back after the stroke is drawn.
+						var originalMatrix = session.Surface.Canvas.TotalMatrix;
+
+						// This is copy to not modify originalMatrix. We revert the shape scaling on it.
+						var matrix = originalMatrix;
+						matrix.ScaleX /= Scale.X; // TODO: Unsure if we should set to 1 or divide by Scale.X
+						matrix.ScaleY /= Scale.Y; // TODO: Unsure if we should set to 1 or divide by Scale.Y
+
+						// Use the matrix with reverted scaling before drawing, then restore the original matrix.
+						session.Surface.Canvas.SetMatrix(matrix);
+						session.Surface.Canvas.DrawPath(strokeGeometry, fillPaint);
+						session.Surface.Canvas.SetMatrix(originalMatrix);
+					}
 				}
 			}
 		}
