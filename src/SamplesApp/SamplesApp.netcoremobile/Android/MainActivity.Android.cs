e2797f384641d5a6d75e31ed6c2a951e216bbfbb
Martin Zikmund <martin@zikmund.dev>
2023-08-23 08:56:16 +0200

    chore: Cleanup on Android

--- src/SamplesApp/SamplesApp.netcoremobile/Android/MainActivity.Android.cs
+++ src/SamplesApp/SamplesApp.netcoremobile/Android/MainActivity.Android.cs
@@ -49,7 +49,7 @@ namespace SamplesApp.Droid
 		[Export("GetScreenshot")]
 		public string GetScreenshot(string displayId)
 		{
-			var rootView = Windows.UI.Xaml.Window.Current.MainContent as View;
+			var rootView = WinUIWindow.RootElement as View;
 
 			var bitmap = Android.Graphics.Bitmap.CreateBitmap(rootView.Width, rootView.Height, Android.Graphics.Bitmap.Config.Argb8888);
 			var locationOfViewInWindow = new int[2];
