8ca47b2caf7cefa8b061dd04371619e37702f688
Martin Zikmund <martin@zikmund.dev>
2023-08-23 09:47:14 +0200

    chore: WASM cleanup

--- src/Uno.UI/UI/Xaml/XamlRoot.crossruntime.cs
+++ src/Uno.UI/UI/Xaml/XamlRoot.crossruntime.cs
@@ -14,9 +14,24 @@ public sealed partial class XamlRoot
 
 	internal event Action InvalidateRender = () => { };
 
-	internal void InvalidateMeasure() => ScheduleInvalidateMeasureOrArrange(invalidateMeasure: true);
+	internal void InvalidateMeasure()
+	{
+#if !__WASM__ // TODO:MZ: Can we use the same approach on WASM?
+		ScheduleInvalidateMeasureOrArrange(invalidateMeasure: true);
+#else
+		InnerInvalidateMeasure();
+#endif
+	}
 
-	internal void InvalidateArrange() => ScheduleInvalidateMeasureOrArrange(invalidateMeasure: false);
+	internal void InvalidateArrange()
+	{
+#if !__WASM__ // TODO:MZ: Can we use the same approach on WASM?
+		ScheduleInvalidateMeasureOrArrange(invalidateMeasure: false);
+#else
+		// We are invalidating both arrange and measure the same way on WASM.
+		InnerInvalidateMeasure();
+#endif
+	}
 
 	internal void QueueInvalidateRender()
 	{
