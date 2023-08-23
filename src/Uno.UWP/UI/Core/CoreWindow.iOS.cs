d5ad208fe4c7ef5006d4233193eb9a726710a90f
Martin Zikmund <martin@zikmund.dev>
2023-08-23 10:10:53 +0200

    chore: iOS cleanup

--- src/Uno.UWP/UI/Core/CoreWindow.iOS.cs
+++ src/Uno.UWP/UI/Core/CoreWindow.iOS.cs
@@ -14,16 +14,8 @@ namespace Windows.UI.Core
 
 		public event TypedEventHandler<CoreWindow, KeyEventArgs> KeyUp;
 
-		private readonly UIWindow _window;
-
-		public CoreWindow(UIWindow window) : this()
-		{
-			_window = window;
-		}
-
 		void ICoreWindowEvents.RaiseKeyDown(KeyEventArgs eventArgs) =>
 			KeyDown?.Invoke(this, eventArgs);
-
 
 		void ICoreWindowEvents.RaiseKeyUp(KeyEventArgs eventArgs) =>
 			KeyUp?.Invoke(this, eventArgs);
