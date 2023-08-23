3fc2654f066391b1110291b7c5e68b07610f313b
Martin Zikmund <martin@zikmund.dev>
2023-08-23 10:10:53 +0200

    chore: iOS cleanup

--- src/Uno.UI/Controls/Window.iOS.cs
+++ src/Uno.UI/Controls/Window.iOS.cs
@@ -230,7 +230,10 @@ namespace Uno.UI.Controls
 			}
 		}
 
+
+#if __MACCATALYST__
 		internal void SetOwner(CoreWindow owner) => _ownerEvents = (ICoreWindowEvents)owner;
+#endif
 
 		/// <summary>
 		/// The behavior to use to bring the focused item into view when opening the keyboard.
