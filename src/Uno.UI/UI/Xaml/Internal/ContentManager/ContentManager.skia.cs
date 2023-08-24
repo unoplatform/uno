e55556270643f8763ee44bd0d830fa7e880b0d69
Martin Zikmund <martin@zikmund.dev>
2023-08-24 11:41:34 +0200

    chore: Adjust build issues

--- src/Uno.UI/UI/Xaml/Internal/ContentManager/ContentManager.skia.cs
+++ src/Uno.UI/UI/Xaml/Internal/ContentManager/ContentManager.skia.cs
@@ -1,13 +1,12 @@
 ﻿#nullable enable
 
-using Windows.UI.Core;
 using Windows.UI.Xaml;
 
 namespace Uno.UI.Xaml.Controls;
 
 partial class ContentManager
 {
-	static partial void LoadRootElementPlatform(XamlRoot xamlRoot, UIElement rootElement);
+	static partial void LoadRootElementPlatform(XamlRoot xamlRoot, UIElement rootElement)
 	{
 		UIElement.LoadingRootElement(rootElement);
 
