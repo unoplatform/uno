7826908420f55260696029a9435f189a400dde97
Carl de Billy <carl@platform.uno>
2023-08-24 21:30:23 -0400

    fix(android_clipping): Fixed clipping was applied on Android when opacity (Alpha) where less than 1.
    (cherry picked from commit a50a3327025fa9957fc91586b6a5f5f8b729be14)
    
    # Conflicts:
    #	src/SamplesApp/UITests.Shared/UITests.Shared.projitems
    #	src/Uno.UI/Extensions/ViewExtensions.Android.cs

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/Clipping/ClippingWithOpacity.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Java.IO;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Windows_UI_Xaml.Clipping
+{
+	[SampleControlInfo("Clipping")]
+
+	public sealed partial class ClippingWithOpacity : Page
+	{
+		public ClippingWithOpacity()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
