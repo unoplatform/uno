a05d7cb36ad8beeb2796f9faf674800e126b2cf1
Youssef Victor <youssefvictor00@gmail.com>
2023-08-26 19:50:15 +0300

    chore: A bit more progress

--- src/Uno.UI/UI/Xaml/Controls/Layouter/Layouter.cs
+++ src/Uno.UI/UI/Xaml/Controls/Layouter/Layouter.cs
@@ -130,6 +130,7 @@ namespace Windows.UI.Xaml.Controls
 				_unclippedDesiredSize = desiredSize;
 
 				var clippedDesiredSize = desiredSize
+					.AtMost(maxSize)
 					.Add(marginSize)
 					// Making sure after adding margins that clipped DesiredSize is not bigger than the AvailableSize
 					.AtMost(availableSize)
