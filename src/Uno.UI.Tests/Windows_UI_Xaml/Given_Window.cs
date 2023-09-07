933415c3b4e6daff8f33a2c33bee2e09fc4fc94b
Youssef Victor <youssefvictor00@gmail.com>
2023-09-07 03:10:19 +0300

    chore: Update Given_Window.cs

--- src/Uno.UI.Tests/Windows_UI_Xaml/Given_Window.cs
+++ src/Uno.UI.Tests/Windows_UI_Xaml/Given_Window.cs
@@ -8,15 +8,13 @@ namespace Uno.UI.Tests.Windows_UI_Xaml;
 [TestClass]
 public class Given_Window
 {
-	// New_Window_Becomes_Current expects the created Window to be the first window set.
-	// So for it to pass reliably, we need to cleanup current window so that
-	// we guarantee that the new window becomes "Current".
-	[TestCleanup]
-	public void Cleanup() => Window.CleanupCurrentForTestsOnly();
-
 	[TestMethod]
 	public void New_Window_Becomes_Current()
 	{
+		// This test expects the created Window to be the first window set.
+		// So for it to pass reliably, we need to cleanup current window so that
+		// we guarantee that the new window becomes "Current".
+		Window.CleanupCurrentForTestsOnly();
 		var window = new Windows.UI.Xaml.Window(true);
 		window.Activate();
 		Assert.AreEqual(window, Windows.UI.Xaml.Window.Current);
