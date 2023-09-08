da522a5cefcc0ca3042714a6967220303181a5e0
Martin Zikmund <martin@zikmund.dev>
2023-09-08 13:30:05 +0200

    chore: Pass XamlRoot to Window factory

--- src/Uno.UI.Runtime.Skia.Gtk/UI/Controls/UnoGtkWindow.cs
+++ src/Uno.UI.Runtime.Skia.Gtk/UI/Controls/UnoGtkWindow.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Runtime.InteropServices.JavaScript;
 using Gtk;
 using Uno.Foundation.Logging;
 using Uno.UI.Runtime.Skia.Gtk.UI.Core;
@@ -11,6 +12,7 @@ using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.UI.Core.Preview;
 using Windows.UI.ViewManagement;
+using XamlRoot = Windows.UI.Xaml.XamlRoot;
 using IOPath = System.IO.Path;
 using WinUIApplication = Windows.UI.Xaml.Application;
 using WinUIWindow = Windows.UI.Xaml.Window;
@@ -25,7 +27,7 @@ internal class UnoGtkWindow : Window
 
 	private List<PendingWindowStateChangedInfo>? _pendingWindowStateChanged = new();
 
-	public UnoGtkWindow(WinUIWindow winUIWindow) : base(WindowType.Toplevel)
+	public UnoGtkWindow(WinUIWindow winUIWindow, XamlRoot xamlRoot) : base(WindowType.Toplevel)
 	{
 		_winUIWindow = winUIWindow ?? throw new ArgumentNullException(nameof(winUIWindow));
 		_winUIWindow.Showing += OnShowing;
@@ -52,6 +54,7 @@ internal class UnoGtkWindow : Window
 		WindowStateEvent += OnWindowStateChanged;
 
 		Host = new UnoGtkWindowHost(this, winUIWindow);
+		GtkManager.XamlRootMap.Register(xamlRoot, Host);
 
 		ApplicationView.GetForCurrentView().PropertyChanged += OnApplicationViewPropertyChanged;
 	}
