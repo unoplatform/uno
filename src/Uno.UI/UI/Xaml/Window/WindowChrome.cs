7626409589aae91b32367b77597874199ac2e86d
Martin Zikmund <martin@zikmund.dev>
2023-09-08 13:41:31 +0200

    chore: Adjust alignment

--- src/Uno.UI/UI/Xaml/Window/WindowChrome.cs
+++ src/Uno.UI/UI/Xaml/Window/WindowChrome.cs
@@ -4,11 +4,11 @@ namespace Uno.UI.Xaml.Controls;
 
 internal sealed partial class WindowChrome : ContentControl
 {
-	private WindowChrome()
-	{
-	}
-
 	public WindowChrome(Windows.UI.Xaml.Window parent)
 	{
+		HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Stretch;
+		VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Stretch;
+		HorizontalContentAlignment = Windows.UI.Xaml.HorizontalAlignment.Center;
+		VerticalContentAlignment = Windows.UI.Xaml.VerticalAlignment.Center;
 	}
 }
