3c456f6c0871f702bce942619944c23cdcc79c4b
Martin Zikmund <martin@zikmund.dev>
2023-09-12 16:27:18 +0200

    refactor: Windowing should go through NativeWindowFactory for all targets

---
+++ src/Uno.UI/UI/Xaml/Window/Native/Factory/NativeWindowFactory.wasm.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+
+using System;
+using Uno.Foundation.Extensibility;
+using Windows.UI.Xaml;
+
+namespace Uno.UI.Xaml.Controls;
+
+partial class NativeWindowFactory
+{
+	private static INativeWindowWrapper? CreateWindowPlatform(Windows.UI.Xaml.Window window, XamlRoot xamlRoot) => null;
+}
