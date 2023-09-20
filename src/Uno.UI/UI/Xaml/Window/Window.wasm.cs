0bf787d6bd8ed99527b13b7c44449ae65e99e4c3
Ramez Ragaa <ramezragaa@proton.me>
2023-09-20 19:18:07 +0300

    chore: add NativeWindowWrapper for wasm
    # Conflicts:
    #	src/Uno.UI/UI/Xaml/Window/Window.wasm.cs

--- src/Uno.UI/UI/Xaml/Window/Window.wasm.cs
+++ src/Uno.UI/UI/Xaml/Window/Window.wasm.cs
@@ -28,32 +28,6 @@ namespace Microsoft.UI.Xaml
 	{
 		[JSExport]
 		[Preserve]
-		public static void Resize(double width, double height) => IShouldntUseCurrentWindow.OnNativeSizeChanged(new Size(width, height));
-
-		internal void OnNativeSizeChanged(Size size)
-		{
-			var newBounds = new Rect(0, 0, size.Width, size.Height);
-
-			if (newBounds != Bounds)
-			{
-				if (this.Log().IsEnabled(Uno.Foundation.Logging.LogLevel.Debug))
-				{
-					this.Log().Debug($"OnNativeSizeChanged: {size}");
-				}
-
-				Bounds = newBounds;
-
-				_windowImplementation.XamlRoot?.InvalidateMeasure();
-				RaiseSizeChanged(new Windows.UI.Core.WindowSizeChangedEventArgs(size));
-
-				// Note that UWP raises the ApplicationView.VisibleBoundsChanged event
-				// *after* Window.SizeChanged.
-
-				// TODO: support for "viewport-fix" on devices with a notch.
-				ApplicationView.GetForCurrentView()?.SetVisibleBounds(newBounds);
-			}
-		}
-
-		partial void ShowPartial() => WindowManagerInterop.WindowActivate();
+		public static void Resize(double width, double height) => NativeWindowWrapper.Instance.RaiseNativeSizeChanged(width, height);
 	}
 }
