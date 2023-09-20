f914f6e1af0fdeb0780d1f0eba7e03901529751f
Jérôme Laban <jlaban@gmail.com>
2023-09-20 16:55:03 -0400

    chore: Adjust to use method instead of a property
    (cherry picked from commit 940588de9835972ab9d955702f8f74c3111c85ce)

--- src/Uno.UWP/Storage/ApplicationDataContainer.Android.cs
+++ src/Uno.UWP/Storage/ApplicationDataContainer.Android.cs
@@ -26,7 +26,7 @@ namespace Windows.Storage
 			{
 			}
 
-			private ISharedPreferences Preferences
+			private ISharedPreferences CreatePreferences()
 				=> PreferenceManager.GetDefaultSharedPreferences(ApplicationData.GetAndroidAppContext())!;
 
 			public object this[string key]
@@ -44,7 +44,7 @@ namespace Windows.Storage
 				{
 					if (value != null)
 					{
-						using var preferences = Preferences;
+						using var preferences = CreatePreferences();
 
 						preferences
 							.Edit()
@@ -62,7 +62,7 @@ namespace Windows.Storage
 			{
 				get
 				{
-					using var preferences = Preferences;
+					using var preferences = CreatePreferences();
 
 					return preferences
 						.All
@@ -75,7 +75,7 @@ namespace Windows.Storage
 			{
 				get
 				{
-					using var preferences = Preferences;
+					using var preferences = CreatePreferences();
 
 					return preferences
 						.All
@@ -89,7 +89,7 @@ namespace Windows.Storage
 			{
 				get
 				{
-					using var preferences = Preferences;
+					using var preferences = CreatePreferences();
 
 					return preferences
 						.All
@@ -113,7 +113,7 @@ namespace Windows.Storage
 				}
 				if (value != null)
 				{
-					using var preferences = Preferences;
+					using var preferences = CreatePreferences();
 
 					preferences
 						.Edit()
@@ -127,7 +127,7 @@ namespace Windows.Storage
 
 			public void Clear()
 			{
-				using var preferences = Preferences;
+				using var preferences = CreatePreferences();
 
 				preferences
 					.Edit()
@@ -140,7 +140,7 @@ namespace Windows.Storage
 
 			public bool ContainsKey(string key)
 			{
-				using var preferences = Preferences;
+				using var preferences = CreatePreferences();
 
 				return preferences.All.ContainsKey(key);
 			}
@@ -150,7 +150,7 @@ namespace Windows.Storage
 
 			public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
 			{
-				using var preferences = Preferences;
+				using var preferences = CreatePreferences();
 
 				return preferences
 					.All
@@ -159,7 +159,7 @@ namespace Windows.Storage
 
 			public bool Remove(string key)
 			{
-				using var preferences = Preferences;
+				using var preferences = CreatePreferences();
 
 				preferences
 					.Edit()
@@ -173,7 +173,7 @@ namespace Windows.Storage
 
 			public bool TryGetValue(string key, out object value)
 			{
-				using var preferences = Preferences;
+				using var preferences = CreatePreferences();
 
 				if (preferences.All.TryGetValue(key, out var serializedValue))
 				{
