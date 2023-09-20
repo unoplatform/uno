6517ea7a6f0e8ab600c9e4f32b67e72f91f0c549
ramezgerges <66218781+ramezgerges@users.noreply.github.com>
2023-09-20 11:09:41 +0300

    chore: reenable ScrollViewer horizontal scrolling when Shift is pressed
    Co-authored-by: David <github.site@torick.net>

--- src/Uno.UI/UI/Xaml/Controls/ScrollContentPresenter/ScrollContentPresenter.cs
+++ src/Uno.UI/UI/Xaml/Controls/ScrollContentPresenter/ScrollContentPresenter.cs
@@ -200,7 +200,7 @@ namespace Windows.UI.Xaml.Controls
 				{
 					// TODO: Handle zoom https://github.com/unoplatform/uno/issues/4309
 				}
-				else if (canScrollHorizontally && properties.IsHorizontalMouseWheel)
+				else if (canScrollHorizontally && (!canScrollVertically || properties.IsHorizontalMouseWheel || e.KeyModifiers == VirtualKeyModifiers.Shift))
 				{
 #if __WASM__ // On wasm the scroll might be async (especially with disableAnimation: false), so we need to use the pending value to support high speed multiple wheel events
 					var horizontalOffset = _pendingScrollTo?.horizontal ?? HorizontalOffset;
