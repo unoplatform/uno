4b2176e999a2c6bfefcea17fa72f4dd5702a73a0
Martin Zikmund <martin@zikmund.dev>
2023-09-20 15:10:25 +0200

    refactor: Adjust Android windowing for new infrastructure

--- src/Uno.UI/UI/Xaml/Window/Native/Factory/NativeWindowFactory.cs
+++ src/Uno.UI/UI/Xaml/Window/Native/Factory/NativeWindowFactory.cs
@@ -22,7 +22,7 @@ internal partial class NativeWindowFactory
 		return CreateWindowPlatform(window, xamlRoot);
 	}
 
-#if !__SKIA__
+#if !__SKIA__ && !__ANDROID__
 	private static INativeWindowWrapper? CreateWindowPlatform(Windows.UI.Xaml.Window window, XamlRoot xamlRoot) => null;
 #endif
 }
