6d40d4b4c0dade673eb68fcc86f565d17aab2cc9
ahmed walid <ahmed.walid605@yahoo.com>
2023-09-22 18:25:35 +0200

    chore(composition): fix build error on UWP

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Composition/Given_CompositionNineGridBrush.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Composition/Given_CompositionNineGridBrush.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Private.Infrastructure;
-using Uno.Extensions;
 using Uno.UI.RuntimeTests.Helpers;
-using Uno.UI.Xaml;
 using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
