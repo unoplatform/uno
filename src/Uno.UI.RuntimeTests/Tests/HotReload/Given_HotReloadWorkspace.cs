6161d9084bb699450230a8008721b79a9072ea23
Martin Zikmund <martin@zikmund.dev>
2023-09-22 09:18:30 +0200

    chore: Adjust for CI

--- src/Uno.UI.RuntimeTests/Tests/HotReload/Given_HotReloadWorkspace.cs
+++ src/Uno.UI.RuntimeTests/Tests/HotReload/Given_HotReloadWorkspace.cs
@@ -23,7 +23,7 @@ using System.Linq;
 namespace Uno.UI.RuntimeTests.Tests.HotReload;
 
 [TestClass]
-#if !__SKIA__
+#if !__SKIA__ || HAS_UNO_WINUI // Disabled due to #13757
 [Ignore("Hot reload tests are only available on Skia targets")]
 #endif
 internal partial class Given_HotReloadWorkspace
