e6dee031c6a6e5c9d1e46a04de843d9323c7ce7d
Youssef Victor <youssefvictor00@gmail.com>
2023-09-24 19:39:57 +0300

    fix: Correctly generate nested merged dictionaries

---
+++ src/SourceGenerators/Uno.UI.SourceGenerators.Tests/XamlCodeGeneratorTests/Given_ResourceDictionary.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.CodeAnalysis.Testing;
+using Uno.UI.SourceGenerators.Tests.Verifiers;
+
+namespace Uno.UI.SourceGenerators.Tests;
+
+using Verify = XamlSourceGeneratorVerifier;
+
+[TestClass]
+public class Given_ResourceDictionary
+{
+	[TestMethod]
+	public async Task TestTwoLevelNestedMergedDictionariesWithSingleResourceDictionary()
+	{
+		var xamlFile = new XamlFile("MyResourceDictionary.xaml", """
+			<ResourceDictionary
+				xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+				xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+				xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006">
+				<ResourceDictionary.MergedDictionaries>
+						<ResourceDictionary Source="ms-appx:///Path/To/File1.xaml">
+							<ResourceDictionary.MergedDictionaries>
+								<ResourceDictionary Source="ms-appx:///Path/To/File2.xaml" />
+							</ResourceDictionary.MergedDictionaries>
+						</ResourceDictionary>
+				</ResourceDictionary.MergedDictionaries>
+			</ResourceDictionary>
+			""");
+
+		var test = new Verify.Test(xamlFile)
+		{
+			TestState =
+			{
+				Sources =
+				{
+					string.Empty, // https://github.com/dotnet/roslyn-sdk/issues/1121
+				}
+			}
+		}.AddGeneratedSources();
+
+		await test.RunAsync();
+	}
+
+	[TestMethod]
+	public async Task TestTwoLevelNestedMergedDictionariesWithTwoResourceDictionaries()
+	{
+		var xamlFile = new XamlFile("MyResourceDictionary.xaml", """
+			<ResourceDictionary
+				xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+				xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+				xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006">
+				<ResourceDictionary.MergedDictionaries>
+						<ResourceDictionary Source="ms-appx:///Path/To/File1.xaml">
+							<ResourceDictionary.MergedDictionaries>
+								<ResourceDictionary Source="ms-appx:///Path/To/File2.xaml" />
+								<ResourceDictionary Source="ms-appx:///Path/To/File3.xaml" />
+							</ResourceDictionary.MergedDictionaries>
+						</ResourceDictionary>
+				</ResourceDictionary.MergedDictionaries>
+			</ResourceDictionary>
+			""");
+
+		var test = new Verify.Test(xamlFile)
+		{
+			TestState =
+			{
+				Sources =
+				{
+					string.Empty, // https://github.com/dotnet/roslyn-sdk/issues/1121
+				}
+			}
+		}.AddGeneratedSources();
+
+		await test.RunAsync();
+	}
+
+	[TestMethod]
+	public async Task TestThreeLevelNestedMergedDictionaries()
+	{
+		var xamlFile = new XamlFile("MyResourceDictionary.xaml", """
+			<ResourceDictionary
+				xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+				xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+				xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006">
+				<ResourceDictionary.MergedDictionaries>
+						<ResourceDictionary Source="ms-appx:///Path/To/File1.xaml">
+							<ResourceDictionary.MergedDictionaries>
+								<ResourceDictionary Source="ms-appx:///Path/To/File2.xaml">
+									<ResourceDictionary.MergedDictionaries>
+										<ResourceDictionary Source="ms-appx:///Path/To/File3.xaml" />
+									</ResourceDictionary.MergedDictionaries>
+								</ResourceDictionary>
+							</ResourceDictionary.MergedDictionaries>
+						</ResourceDictionary>
+				</ResourceDictionary.MergedDictionaries>
+			</ResourceDictionary>
+			""");
+
+		var test = new Verify.Test(xamlFile)
+		{
+			TestState =
+			{
+				Sources =
+				{
+					string.Empty, // https://github.com/dotnet/roslyn-sdk/issues/1121
+				}
+			}
+		}.AddGeneratedSources();
+
+		await test.RunAsync();
+	}
+}
---
+++ src/Uno.UI/UI/Xaml/ResourceDictionaryExtensions.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+
+using System.ComponentModel;
+using Windows.UI.Xaml;
+
+namespace Uno.UI.Xaml;
+
+/// <summary>
+/// Helper for XAML code generation. Not intended to be used in apps outside of XAML generator.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class ResourceDictionaryExtensions
+{
+	public static ResourceDictionary AddMergedDictionaries(this ResourceDictionary dictionary, params ResourceDictionary[] mergedDictionaries)
+	{
+		foreach (var mergedDictionary in mergedDictionaries)
+		{
+			dictionary.MergedDictionaries.Add(mergedDictionary);
+		}
+
+		return dictionary;
+	}
+}
