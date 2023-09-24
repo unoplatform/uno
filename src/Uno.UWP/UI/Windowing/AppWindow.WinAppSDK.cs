fde4926c16776483f80fe870483fdf884b2d9184
Martin Zikmund <martin@zikmund.dev>
2023-09-24 13:34:11 +0200

    chore: AppWindow multi-file

---
+++ src/Uno.UWP/UI/Windowing/AppWindow.WinAppSDK.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+#if HAS_UNO_WINUI
+namespace Microsoft.UI.Windowing;
+#else
+namespace Windows.UI.WindowManagement;
+#endif
+
+partial class AppWindow
+{
+	private static readonly Dictionary<WindowId, AppWindow> _appWindowIdMap = new();
+	private static ulong _windowIdIterator;
+
+#if HAS_UNO_WINUI
+	public
+#else
+	internal
+#endif
+	WindowId WindowId
+	{ get; }
+
+#if HAS_UNO_WINUI
+	public
+#else
+	internal
+#endif
+	static AppWindow GetFromWindowId(WindowId windowId) => _appWindowIdMap[windowId];
+}
