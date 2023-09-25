36fe3f33c078ea92bef65919946265ba2248aab6
Martin Zikmund <martin@zikmund.dev>
2023-09-25 17:53:50 +0200

    feat: Partially implement MediaTransdfsafdafassdfasfdasfdsdefa

---
+++ src/Uno.UI/UI/Xaml/Media/MediaTransportControlsThumbnailRequestedEventArgs.Android.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows.UI.Xaml.Media;
+
+partial class MediaTransportControlsThumbnailRequestedEventArgs
+{
+	public global::Windows.Foundation.Deferral GetDeferral()
+	{
+		// CODE!
+		Android.Views.ViewGroup viewGroup = null;
+		Context
+	}
+}
---
+++ src/Uno.UI/UI/Xaml/Media/MediaTransportControlsThumbnailRequestedEventArgs.cs
@@ -0,0 +1,15 @@
+namespace Windows.UI.Xaml.Media
+{
+	public partial class MediaTransportControlsThumbnailRequestedEventArgs
+	{
+//#if __ANDROID__ || __IOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+//		internal MediaTransportControlsThumbnailRequestedEventArgs()
+//		{
+//		}
+//#endif
+		public void SetThumbnailImage(global::Windows.Storage.Streams.IInputStream source)
+		{
+			// IMPLEMENT THIS!
+		}
+	}
+}
