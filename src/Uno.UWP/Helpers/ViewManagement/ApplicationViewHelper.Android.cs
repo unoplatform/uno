a960bf120b4ee7e20d3ff31d5f52af45f789a138
Martin Zikmund <martin@zikmund.dev>
2023-09-26 14:21:15 +0200

    chore: Adjust ApplicationView on Android

--- src/Uno.UWP/Helpers/ViewManagement/ApplicationViewHelper.Android.cs
+++ src/Uno.UWP/Helpers/ViewManagement/ApplicationViewHelper.Android.cs
@@ -1,22 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Android.App;
-using Android.Content;
-using Uno.UI;
-using Windows.UI.ViewManagement;
+﻿namespace Uno.UI.ViewManagement;
 
-namespace Uno.UI.ViewManagement
+public static class ApplicationViewHelper
 {
-	public static class ApplicationViewHelper
-	{
-		/// <summary>
-		/// Gets an instance to <see cref="IBaseActivityEvents"/> which provides a set of events
-		/// raised on key Activity method overrides.
-		/// </summary>
-		public static IBaseActivityEvents GetBaseActivityEvents()
-			=> global::Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().BaseActivityEvents;
-	}
+	/// <summary>
+	/// Gets an instance to <see cref="IBaseActivityEvents"/> which provides a set of events
+	/// raised on key Activity method overrides.
+	/// </summary>
+	public static IBaseActivityEvents GetBaseActivityEvents() => ContextHelper.Current as IBaseActivityEvents;
 }
