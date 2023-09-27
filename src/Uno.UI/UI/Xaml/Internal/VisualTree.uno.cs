ef6a7f5459104497421a63c9567bdea32e15d459
Martin Zikmund <martin@zikmund.dev>
2023-09-27 09:32:45 +0200

    refactor: Avoid ApplicationView.GetForCurrentView usages

--- src/Uno.UI/UI/Xaml/Internal/VisualTree.uno.cs
+++ src/Uno.UI/UI/Xaml/Internal/VisualTree.uno.cs
@@ -1,6 +1,11 @@
 ﻿#nullable enable
 
+using System;
 using Uno.UI.DataBinding;
+using Uno.UI.Xaml.Islands;
+using Windows.Foundation;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace Uno.UI.Xaml.Core;
@@ -11,6 +16,7 @@ internal partial class VisualTree : IWeakReferenceProvider
 	private const int FocusVisualZIndex = UnoTopZIndex + 1;
 
 	private ManagedWeakReference? _selfWeakReference;
+	private ApplicationView? _applicationView;
 
 	public Canvas? FocusVisualRoot { get; private set; }
 
@@ -29,4 +35,69 @@ internal partial class VisualTree : IWeakReferenceProvider
 
 	ManagedWeakReference IWeakReferenceProvider.WeakReference =>
 		_selfWeakReference ??= WeakReferencePool.RentSelfWeakReference(this);
+
+	internal Rect VisibleBounds
+	{
+		get
+		{
+			if (GetApplicationViewForOwnerWindow() is ApplicationView applicationView)
+			{
+				return applicationView.VisibleBounds;
+			}
+			else
+			{
+				var size = Size;
+				return new Rect(0, 0, size.Width, size.Height);
+			}
+		}
+	}
+
+	internal Rect TrueVisibleBounds
+	{
+		get
+		{
+			if (GetApplicationViewForOwnerWindow() is ApplicationView applicationView)
+			{
+				return applicationView.TrueVisibleBounds;
+			}
+			else
+			{
+				var size = Size;
+				return new Rect(0, 0, size.Width, size.Height);
+			}
+		}
+	}
+
+	private ApplicationView? GetApplicationViewForOwnerWindow()
+	{
+		if (_applicationView is not null)
+		{
+			return _applicationView;
+		}
+
+		if (RootElement is XamlIsland xamlIsland)
+		{
+			if (xamlIsland.OwnerWindow is Window ownerWindow)
+			{
+				_applicationView = ApplicationView.GetForWindowId(ownerWindow.AppWindow.Id);
+				return _applicationView;
+			}
+		}
+		else if (RootElement is RootVisual)
+		{
+			if (Window.CurrentSafe is null)
+			{
+				throw new InvalidOperationException("Window.Current must be set.");
+			}
+
+			_applicationView = ApplicationView.GetForCurrentViewSafe();
+			return _applicationView;
+		}
+		else
+		{
+			throw new InvalidOperationException("Invalid VisualTree root type");
+		}
+
+		return null;
+	}
 }
