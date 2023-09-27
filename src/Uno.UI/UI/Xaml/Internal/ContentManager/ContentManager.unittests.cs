777ac1cebf3d81628f193c4d5e8c8175e61f3d6a
Martin Zikmund <martin@zikmund.dev>
2023-09-27 17:14:45 +0200

    refactor: Adjust unit tests windowing

---
+++ src/Uno.UI/UI/Xaml/Internal/ContentManager/ContentManager.unittests.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+
+using Windows.UI.Xaml;
+
+namespace Uno.UI.Xaml.Controls;
+
+partial class ContentManager
+{
+	static partial void LoadRootElementPlatform(XamlRoot xamlRoot, UIElement rootElement)
+	{
+		(rootElement as FrameworkElement)?.ForceLoaded();
+	}
+}
