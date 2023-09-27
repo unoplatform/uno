75dad1e9307582be686e439761b07075d442a1d1
Martin Zikmund <martin@zikmund.dev>
2023-09-27 14:03:01 +0200

    chore: Adjust Window base for WASM

--- src/Uno.UI/UI/Xaml/Window/Native/NativeWindowWrapper.wasm.cs
+++ src/Uno.UI/UI/Xaml/Window/Native/NativeWindowWrapper.wasm.cs
@@ -1,56 +1,37 @@
 using System;
-using System.Runtime.InteropServices.JavaScript;
 using Windows.Foundation;
 using Windows.UI.Core;
-using Windows.UI.ViewManagement;
 
 namespace Uno.UI.Xaml.Controls;
 
-public partial class NativeWindowWrapper : INativeWindowWrapper
+internal partial class NativeWindowWrapper : NativeWindowWrapperBase
 {
 	private static readonly Lazy<NativeWindowWrapper> _instance = new(() => new NativeWindowWrapper());
 
-	private Size _previousWindowSize = new Size(-1, -1);
-
 	internal static NativeWindowWrapper Instance => _instance.Value; // TODO: Temporary until proper multi-window support is added.
 
-	public event EventHandler<Size> SizeChanged;
-	public event EventHandler<CoreWindowActivationState> ActivationChanged;
-	public event EventHandler<bool> VisibilityChanged;
-	public event EventHandler Closed;
-	public event EventHandler Shown;
-
-	public bool Visible => throw new NotImplementedException();
-
-	public void Activate()
+	public override void Activate()
 	{
 		// TODO: Bring window to the foreground?
 	}
 
-	internal void OnNativeClosed() => Closed?.Invoke(this, EventArgs.Empty);
+	internal void OnNativeClosed() => RaiseClosed();
 
-	internal void OnNativeActivated(CoreWindowActivationState state) => ActivationChanged?.Invoke(this, state);
+	internal void OnNativeActivated(CoreWindowActivationState state) => ActivationState = state;
 
-	internal void OnNativeVisibilityChanged(bool visible) => VisibilityChanged?.Invoke(this, visible);
+	internal void OnNativeVisibilityChanged(bool visible) => Visible = visible;
 
 	internal void RaiseNativeSizeChanged(double width, double height)
 	{
-		var windowSize = new Size(width, height);
+		var bounds = new Rect(default, new Size(width, height));
 
-		ApplicationView.GetForCurrentView()?.SetVisibleBounds(new Rect(Point.Zero, windowSize));
-
-		if (_previousWindowSize != windowSize)
-		{
-			_previousWindowSize = windowSize;
-
-			SizeChanged?.Invoke(this, windowSize);
-		}
+		Bounds = bounds;
+		VisibleBounds = bounds;
 	}
 
-	public void Show()
+	protected override void ShowCore()
 	{
 		WindowManagerInterop.WindowActivate();
 		OnNativeActivated(CoreWindowActivationState.CodeActivated);
-		Shown?.Invoke(this, EventArgs.Empty);
 	}
 }
