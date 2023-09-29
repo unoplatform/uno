b2332c312adac293e275a39a4a6e64b7e64370d7
Youssef Victor <youssefvictor00@gmail.com>
2023-09-29 20:00:31 +0300

    chore: Ignore editor browsable attribute

--- src/Uno.UWPSyncGenerator/SyncGenerator.cs
+++ src/Uno.UWPSyncGenerator/SyncGenerator.cs
@@ -225,6 +225,7 @@ namespace Uno.UWPSyncGenerator
 		private static bool IsIgnoredAttribute(AttributeData attributeData)
 		{
 			return attributeData.AttributeClass.ToString() is
+				"System.ComponentModel.EditorBrowsableAttribute" or
 				"Windows.Foundation.Metadata.GuidAttribute" or
 				"Windows.Foundation.Metadata.StaticAttribute" or
 				"Windows.Foundation.Metadata.AllowMultipleAttribute" or
