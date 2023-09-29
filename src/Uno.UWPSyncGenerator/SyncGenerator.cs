d9cec3cc8508b151c467ace9f31022aa71503a64
Youssef Victor <youssefvictor00@gmail.com>
2023-09-29 20:46:34 +0300

    chore: Ignore WindowsRuntimeTypeAttribute

--- src/Uno.UWPSyncGenerator/SyncGenerator.cs
+++ src/Uno.UWPSyncGenerator/SyncGenerator.cs
@@ -225,6 +225,7 @@ namespace Uno.UWPSyncGenerator
 		private static bool IsIgnoredAttribute(AttributeData attributeData)
 		{
 			return attributeData.AttributeClass.ToString() is
+				"WinRT.WindowsRuntimeTypeAttribute" or
 				"System.Runtime.InteropServices.GuidAttribute" or
 				"System.ComponentModel.EditorBrowsableAttribute" or
 				"Windows.Foundation.Metadata.GuidAttribute" or
