9507cbb6eeb78b619e43b0d520d211a7f22fc04e
Youssef Victor <youssefvictor00@gmail.com>
2023-10-03 11:12:37 +0300

    chore: Don't generate base call for structs

--- src/Uno.UWPSyncGenerator/Generator.cs
+++ src/Uno.UWPSyncGenerator/Generator.cs
@@ -1213,7 +1213,7 @@ namespace Uno.UWPSyncGenerator
 
 						var baseParamString = string.Join(", ", q.FirstOrDefault()?.Parameters.Select(p => p.Name) ?? Array.Empty<string>());
 
-						var baseParams = type.BaseType?.SpecialType != SpecialType.System_Object && q.Any() ? $" : base({baseParamString})" : "";
+						var baseParams = type.TypeKind != TypeKind.Struct && type.BaseType?.SpecialType != SpecialType.System_Object && q.Any() ? $" : base({baseParamString})" : "";
 
 						b.AppendLineInvariant($"[global::Uno.NotImplemented({methods.GenerateNotImplementedList()})]");
 						using (b.BlockInvariant($"{visiblity} {type.Name}({parameters}){baseParams}"))
