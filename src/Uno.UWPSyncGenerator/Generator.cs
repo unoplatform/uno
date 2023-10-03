5f783e3cd3136c3acdabe7b782faa0e9de0e56cb
Youssef Victor <youssefvictor00@gmail.com>
2023-10-03 13:28:21 +0300

    chore: Skip Microsoft.Windows namespace

--- src/Uno.UWPSyncGenerator/Generator.cs
+++ src/Uno.UWPSyncGenerator/Generator.cs
@@ -310,6 +310,16 @@ namespace Uno.UWPSyncGenerator
 			{
 				return true;
 			}
+#if HAS_UNO_WINUI
+			else if (@namespace.StartsWith("Microsoft.Windows.", StringComparison.Ordinal))
+			{
+				// Historically, before https://github.com/unoplatform/uno/pull/13867, WinUI generation was not correct.
+				// With this PR, new types in Microsoft.Windows. started to be generated. (e.g, Microsoft.Windows.ApplicationModel.DynamicDependency.PackageVersion)
+				// We want to minimize the changes from #13867, so we skip this namespace, maintaining the old behavior.
+				// We should revise in the future whether we want to include these types.
+				return true;
+			}
+#endif
 			else if (@namespace == "WinRT")
 			{
 				return true;
