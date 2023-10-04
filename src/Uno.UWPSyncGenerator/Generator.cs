90950936357f07ddd09dbf489edd12bdbcf31b0e
Youssef Victor <youssefvictor00@gmail.com>
2023-10-04 16:40:32 +0300

    chore: Skip more

--- src/Uno.UWPSyncGenerator/Generator.cs
+++ src/Uno.UWPSyncGenerator/Generator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
@@ -329,8 +329,9 @@ namespace Uno.UWPSyncGenerator
 					"IUnknownVftbl" or
 					"IWeakReference" or
 					"IWeakReferenceSource" or
-					"_add_EventHandler" or
-					"_remove_EventHandler";
+					"IActivationFactory" or
+					"IAgileOBject" ||
+					namedTypeSymbol.Name[0] == '_';
 			}
 
 			return false;
