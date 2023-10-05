b1cbcc7cf9cf777dfba3d426e4ce9f5b251d9492
Nick Randolph <nick@builttoroam.com>
2023-10-05 15:18:04 +1100

    chore: Adding test to validate changing user control inside viewbox works

--- src/Uno.UI.RuntimeTests/Tests/HotReload/Frame/HRApp/Tests/Given_UserControl.cs
+++ src/Uno.UI.RuntimeTests/Tests/HotReload/Frame/HRApp/Tests/Given_UserControl.cs
@@ -18,8 +18,7 @@ public class Given_UserControl : BaseTestClass
 	public const string UserControl1TextBlockChangedText = UserControl1TextBlockOriginalText + SimpleTextChange;
 
 	/// <summary>
-	/// No Change to Page 1 - just loads Page 1 without triggering any HR changes
-	/// Useful for doing manual xaml changes and validating HR is applied
+	/// Change the Text of a TextBlock inside a UserControl
 	/// </summary>
 	[TestMethod]
 	public async Task Check_Change_UserControl()
@@ -45,4 +44,34 @@ public class Given_UserControl : BaseTestClass
 		await frame.ValidateTextOnChildTextBlock(UserControl1TextBlockOriginalText, 2);
 		await Task.Yield();
 	}
+
+	/// <summary>
+	/// Change the Text of a TextBlock inside a UserControl, where the UserControl 
+	/// is nested inside a Viewbox
+	/// </summary>
+	[TestMethod]
+	public async Task Check_Change_UserControl_Inside_Viewbox()
+	{
+		var ct = new CancellationTokenSource(TimeSpan.FromSeconds(10)).Token;
+
+		var frame = new Windows.UI.Xaml.Controls.Frame();
+		UnitTestsUIContentHelper.Content = frame;
+
+		frame.Navigate(typeof(HR_Frame_Pages_Page2));
+
+		// Check the initial text of the TextBlock
+		await frame.ValidateTextOnChildTextBlock(UserControl1TextBlockOriginalText, 2); // The user control textblock is the third one on page1
+
+		// Check the updated text of the TextBlock
+		await HotReloadHelper.UpdateServerFileAndRevert<HR_Frame_Pages_UC1>(
+			UserControl1TextBlockOriginalText,
+			UserControl1TextBlockChangedText,
+			() => frame.ValidateTextOnChildTextBlock(UserControl1TextBlockChangedText, 2),
+			ct);
+
+		// Validate that the page has been returned to the original text
+		await frame.ValidateTextOnChildTextBlock(UserControl1TextBlockOriginalText, 2);
+		await Task.Yield();
+	}
+
 }
