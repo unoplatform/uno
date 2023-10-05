87f4af878de13175a8d55dc83b419c9a1b44851a
Martin Zikmund <martin@zikmund.dev>
2023-10-05 12:06:05 +0200

    chore: Adjust unit tests on UWP and WinUI

--- src/Uno.UI.Tests/Windows_UI_Xaml/Given_Window.cs
+++ src/Uno.UI.Tests/Windows_UI_Xaml/Given_Window.cs
@@ -4,21 +4,30 @@ using Windows.UI.Xaml;
 
 namespace Uno.UI.Tests.Windows_UI_Xaml;
 
-// These tests will become invalid when multi-window support is added #8341
 [TestClass]
 public class Given_Window
 {
+#if WINUI_WINDOWING
 	[TestMethod]
 	public void New_Window_Becomes_Current()
 	{
+		// This test expects the created Window to be the first window set.
+		// So for it to pass reliably, we need to cleanup current window so that
+		// we guarantee that the new window becomes "Current".
+		Window.CleanupCurrentForTestsOnly();
+
 		var window = new Windows.UI.Xaml.Window();
 		window.Activate();
 		Assert.AreEqual(window, Windows.UI.Xaml.Window.Current);
 	}
+#endif
 
 	[TestMethod]
 	public void New_Window_Does_Not_Override_Current()
 	{
+		var app = UnitTestsApp.App.EnsureApplication();
+
+		Windows.UI.Xaml.Window.InitializeWindowCurrent();
 		var existingCurrent = Windows.UI.Xaml.Window.Current;
 		var window = new Windows.UI.Xaml.Window();
 		window.Activate();
