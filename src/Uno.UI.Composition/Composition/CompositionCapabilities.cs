5970d3164000d8356312936b03cf57c63cc5bcb0
Martin Zikmund <martin@zikmund.dev>
2023-10-06 12:59:42 +0200

    refactor: Code review II

--- src/Uno.UI.Composition/Composition/CompositionCapabilities.cs
+++ src/Uno.UI.Composition/Composition/CompositionCapabilities.cs
@@ -15,6 +15,6 @@ namespace Windows.UI.Composition
 
 		internal CompositionCapabilities(Compositor? compositor) => _compositor = compositor;
 
-		public static CompositionCapabilities GetForCurrentView() => new CompositionCapabilities(Compositor.Current);
+		public static CompositionCapabilities GetForCurrentView() => new CompositionCapabilities(Compositor.GetSharedCompositor());
 	}
 }
