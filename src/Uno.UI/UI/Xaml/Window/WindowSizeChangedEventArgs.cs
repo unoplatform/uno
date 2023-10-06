4ad45d39f78c166c0e42cfc32793200af3e1a4d3
Martin Zikmund <martin@zikmund.dev>
2023-10-06 20:38:17 +0200

    chore: Code review IV

--- src/Uno.UI/UI/Xaml/Window/WindowSizeChangedEventArgs.cs
+++ src/Uno.UI/UI/Xaml/Window/WindowSizeChangedEventArgs.cs
@@ -3,25 +3,24 @@ using System;
 using Windows.Foundation;
 using Windows.Foundation.Metadata;
 
-namespace Microsoft.UI.Xaml
+namespace Microsoft.UI.Xaml;
+
+public sealed partial class WindowSizeChangedEventArgs
 {
-	public sealed partial class WindowSizeChangedEventArgs
+	public WindowSizeChangedEventArgs(Size newSize)
 	{
-		public WindowSizeChangedEventArgs(Size newSize)
-		{
-			Size = newSize;
-		}
+		Size = newSize;
+	}
 
-		public bool Handled
-		{
-			get;
-			set;
-		}
+	public bool Handled
+	{
+		get;
+		set;
+	}
 
-		public Size Size
-		{
-			get;
-		}
+	public Size Size
+	{
+		get;
 	}
 }
 #endif
