a3dbd3218009ba2b3e4279af2ed0862ae39fa6da
Martin Zikmund <martin@zikmund.dev>
2023-10-15 22:49:12 +0200

    chore: Adjust test on Islands

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_Window.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_Window.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.ApplicationModel.Core;
 #if !WINDOWS_UWP
 using Uno.UI.Xaml;
 #endif
@@ -17,8 +18,15 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml
 		[RunsOnUIThread]
 		public void When_CreateNewWindow()
 		{
+			if (!CoreApplication.IsFullFledgedApp)
+			{
+				Assert.Inconclusive("This test can only be run in a full-fledged app");
+				return;
+			}
+
 			// This used to crash on wasm which was trying to create a second D&D extension
-			var sut = new Window(WindowType.CoreWindow);
+			var sut = new Window(WindowType.DesktopXamlSource);
+			sut.Close();
 		}
 #endif
 	}
