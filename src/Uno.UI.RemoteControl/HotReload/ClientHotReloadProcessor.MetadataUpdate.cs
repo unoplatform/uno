13db968e6bab7f48df1db10999454ac3e0d92441
Martin Zikmund <martin@zikmund.dev>
2023-10-15 12:48:48 +0200

    chore: Ensure hot reload runs for all windows

--- src/Uno.UI.RemoteControl/HotReload/ClientHotReloadProcessor.MetadataUpdate.cs
+++ src/Uno.UI.RemoteControl/HotReload/ClientHotReloadProcessor.MetadataUpdate.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using DirectUI;
 using Uno.Extensions;
 using Uno.Foundation.Logging;
 using Uno.UI.Helpers;
