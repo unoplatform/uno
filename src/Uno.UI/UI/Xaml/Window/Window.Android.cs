c905b9089061eb7af9a3d694665f245004d5004c
Martin Zikmund <martin@zikmund.dev>
2023-10-16 09:51:32 +0200

    chore: Continued cleanup

--- src/Uno.UI/UI/Xaml/Window/Window.Android.cs
+++ src/Uno.UI/UI/Xaml/Window/Window.Android.cs
@@ -6,5 +6,5 @@ namespace Windows.UI.Xaml;
 
 public sealed partial class Window
 {
-	public bool IsStatusBarTranslucent() => NativeWindowWrapper.Instance.IsStatusBarTranslucent(); //TODO:MZ: Can remove as breaking change?
+	public bool IsStatusBarTranslucent() => NativeWindowWrapper.Instance.IsStatusBarTranslucent(); //TODO: Can remove as breaking change? #8339
 }
