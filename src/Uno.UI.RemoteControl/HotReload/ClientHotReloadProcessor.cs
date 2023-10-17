fe2dea4a7067e8c8216f337f688bb32fced76101
David <github.site@torick.net>
2023-10-17 12:31:08 -0400

    feat: Give ability to client to listen for the server to be ready to process hot-reload (for testing purposes)

--- src/Uno.UI.RemoteControl/HotReload/ClientHotReloadProcessor.cs
+++ src/Uno.UI.RemoteControl/HotReload/ClientHotReloadProcessor.cs
@@ -12,99 +12,96 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Markup;
 
-namespace Uno.UI.RemoteControl.HotReload
+namespace Uno.UI.RemoteControl.HotReload;
+
+public partial class ClientHotReloadProcessor : IRemoteControlProcessor
 {
-	public partial class ClientHotReloadProcessor : IRemoteControlProcessor
+	private string? _projectPath;
+	private string[]? _xamlPaths;
+	private readonly IRemoteControlClient _rcClient;
+
+	private static readonly Logger _log = typeof(ClientHotReloadProcessor).Log();
+	private Dictionary<string, string>? _msbuildProperties;
+
+	public ClientHotReloadProcessor(IRemoteControlClient rcClient)
 	{
-		private string? _projectPath;
-		private string[]? _xamlPaths;
-		private readonly IRemoteControlClient _rcClient;
+		_rcClient = rcClient;
+		InitializeMetadataUpdater();
+	}
 
-		private static Logger _log = typeof(ClientHotReloadProcessor).Log();
-		private Dictionary<string, string>? _msbuildProperties;
+	partial void InitializeMetadataUpdater();
 
-		public ClientHotReloadProcessor(IRemoteControlClient rcClient)
+	string IRemoteControlProcessor.Scope => HotReloadConstants.HotReload;
+
+	public async Task Initialize()
+		=> await ConfigureServer();
+
+	public async Task ProcessFrame(Messages.Frame frame)
+	{
+		switch (frame.Name)
 		{
-			_rcClient = rcClient;
-			InitializeMetadataUpdater();
-		}
-
-		partial void InitializeMetadataUpdater();
-
-		string IRemoteControlProcessor.Scope => HotReloadConstants.HotReload;
-
-		public async Task Initialize()
-		{
-			await ConfigureServer();
-		}
-
-		public async Task ProcessFrame(Messages.Frame frame)
-		{
-			switch (frame.Name)
-			{
 #if __WASM__ || __SKIA__
-				case AssemblyDeltaReload.Name:
-					AssemblyReload(JsonConvert.DeserializeObject<HotReload.Messages.AssemblyDeltaReload>(frame.Content)!);
-					break;
+			case AssemblyDeltaReload.Name:
+				AssemblyReload(JsonConvert.DeserializeObject<HotReload.Messages.AssemblyDeltaReload>(frame.Content)!);
+				break;
 #endif
 
-				case FileReload.Name:
-					await PartialReload(JsonConvert.DeserializeObject<HotReload.Messages.FileReload>(frame.Content)!);
-					break;
+			case FileReload.Name:
+				await PartialReload(JsonConvert.DeserializeObject<HotReload.Messages.FileReload>(frame.Content)!);
+				break;
 
-				case HotReloadWorkspaceLoadResult.Name:
-					WorkspaceLoadResult(JsonConvert.DeserializeObject<HotReload.Messages.HotReloadWorkspaceLoadResult>(frame.Content)!);
-					break;
+			case HotReloadWorkspaceLoadResult.Name:
+				WorkspaceLoadResult(JsonConvert.DeserializeObject<HotReload.Messages.HotReloadWorkspaceLoadResult>(frame.Content)!);
+				break;
 
-				default:
-					if (this.Log().IsEnabled(LogLevel.Error))
-					{
-						this.Log().LogError($"Unknown frame [{frame.Scope}/{frame.Name}]");
-					}
-					break;
-			}
-
-			return;
-		}
-
-		private async Task ConfigureServer()
-		{
-			var assembly = _rcClient.AppType.Assembly;
-
-			if (assembly.GetCustomAttributes(typeof(ProjectConfigurationAttribute), false) is ProjectConfigurationAttribute[] configs)
-			{
-				var config = configs.First();
-
-				_projectPath = config.ProjectPath;
-				_xamlPaths = config.XamlPaths;
-
-				if (this.Log().IsEnabled(LogLevel.Debug))
-				{
-					this.Log().LogDebug($"ProjectConfigurationAttribute={config.ProjectPath}, Paths={_xamlPaths.Length}");
-				}
-
-				if (this.Log().IsEnabled(LogLevel.Trace))
-				{
-					foreach (var path in _xamlPaths)
-					{
-						this.Log().Trace($"\t- {path}");
-					}
-				}
-
-				ConfigureServer message = new(_projectPath, _xamlPaths, GetMetadataUpdateCapabilities(), config.MSBuildProperties);
-
-				await _rcClient.SendMessage(message);
-
-				_msbuildProperties = message.MSBuildProperties;
-
-				InitializePartialReload();
-			}
-			else
-			{
+			default:
 				if (this.Log().IsEnabled(LogLevel.Error))
 				{
-					this.Log().LogError("Unable to find ProjectConfigurationAttribute");
+					this.Log().LogError($"Unknown frame [{frame.Scope}/{frame.Name}]");
 				}
+				break;
+		}
+
+		return;
+	}
+
+	private async Task ConfigureServer()
+	{
+		var assembly = _rcClient.AppType.Assembly;
+
+		if (assembly.GetCustomAttributes(typeof(ProjectConfigurationAttribute), false) is ProjectConfigurationAttribute[] configs)
+		{
+			var config = configs.First();
+
+			_projectPath = config.ProjectPath;
+			_xamlPaths = config.XamlPaths;
+
+			if (this.Log().IsEnabled(LogLevel.Debug))
+			{
+				this.Log().LogDebug($"ProjectConfigurationAttribute={config.ProjectPath}, Paths={_xamlPaths.Length}");
+			}
+
+			if (this.Log().IsEnabled(LogLevel.Trace))
+			{
+				foreach (var path in _xamlPaths)
+				{
+					this.Log().Trace($"\t- {path}");
+				}
+			}
+
+			ConfigureServer message = new(_projectPath, _xamlPaths, GetMetadataUpdateCapabilities(), config.MSBuildProperties);
+
+			await _rcClient.SendMessage(message);
+
+			_msbuildProperties = message.MSBuildProperties;
+
+			InitializePartialReload();
+		}
+		else
+		{
+			if (this.Log().IsEnabled(LogLevel.Error))
+			{
+				this.Log().LogError("Unable to find ProjectConfigurationAttribute");
 			}
 		}
 	}
