3fc9331c4505dbb31a012c6adb73e011928d2257
Martin Zikmund <martin@zikmund.dev>
2023-10-18 18:26:18 +0200

    test: Sample for TextBox.Paste and PasswordBox.Paste

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/TextBox/TextBox_PasteEvent.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Uno.UI.Samples.Controls;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.UI.Samples.Content.UITests.TextBoxControl
+{
+	[Sample(
+		"TextBox",
+		IsManualTest = true,
+		IgnoreInSnapshotTests = true,
+		Description =
+			"Verifies the Paste event functionality. " +
+			"When the CheckBox is not checked, pasting from clipboard should be possible. " +
+			"When checked, pasting should not happen.")]
+	public sealed partial class TextBox_PasteEvent : UserControl
+	{
+		public TextBox_PasteEvent()
+		{
+			InitializeComponent();
+		}
+
+		private void SampleTextBox_Paste(object sender, TextControlPasteEventArgs e)
+		{
+			if (HandlePasteCheckBox.IsChecked == true)
+			{
+				e.Handled = true;
+			}
+
+			EventLogTextBlock.Text = "TextBox.Paste at " + DateTime.Now.ToString() + " " + (e.Handled ? " handled" : "not handled");
+		}
+
+		private void SamplePasswordBox_Paste(object sender, TextControlPasteEventArgs e)
+		{
+			if (HandlePasteCheckBox.IsChecked == true)
+			{
+				e.Handled = true;
+			}
+
+			EventLogTextBlock.Text = "PasswordBox.Paste at " + DateTime.Now.ToString() + " " + (e.Handled ? " handled" : "not handled");
+		}
+	}
+}
