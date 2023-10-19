380b30bfa449daace5a52b5ca19c15b2cccda8f0
Youssef Victor <youssefvictor00@gmail.com>
2023-10-19 18:36:42 +0300

    perf: Re-use Metadata calculated for getting property inheritance info in DP details

--- src/Uno.UI/UI/Xaml/DependencyPropertyDetails.cs
+++ src/Uno.UI/UI/Xaml/DependencyPropertyDetails.cs
@@ -61,15 +61,45 @@ namespace Windows.UI.Xaml
 		/// Constructor
 		/// </summary>
 		/// <param name="defaultValue">The default value of the Dependency Property</param>
-		internal DependencyPropertyDetails(DependencyProperty property, Type dependencyObjectType, bool hasInherits, bool hasValueInherits, bool hasValueDoesNotInherits)
+		internal DependencyPropertyDetails(DependencyProperty property, Type dependencyObjectType, bool isTemplatedParentOrDataContext)
 		{
 			Property = property;
 			_dependencyObjectType = dependencyObjectType;
+
+			GetPropertyInheritanceConfiguration(isTemplatedParentOrDataContext, out var hasInherits, out var hasValueInherits, out var hasValueDoesNotInherits);
 
 			_flags |= property.HasWeakStorage ? Flags.WeakStorage : Flags.None;
 			_flags |= hasValueInherits ? Flags.ValueInherits : Flags.None;
 			_flags |= hasValueDoesNotInherits ? Flags.ValueDoesNotInherit : Flags.None;
 			_flags |= hasInherits ? Flags.Inherits : Flags.None;
+		}
+
+		private void GetPropertyInheritanceConfiguration(
+			bool isTemplatedParentOrDataContext,
+			out bool hasInherits,
+			out bool hasValueInherits,
+			out bool hasValueDoesNotInherit)
+		{
+			if (isTemplatedParentOrDataContext)
+			{
+				// TemplatedParent is a DependencyObject but does not propagate datacontext
+				hasValueInherits = false;
+				hasValueDoesNotInherit = true;
+				hasInherits = true;
+				return;
+			}
+
+			if (Metadata is FrameworkPropertyMetadata propertyMetadata)
+			{
+				hasValueInherits = propertyMetadata.Options.HasValueInheritsDataContext();
+				hasValueDoesNotInherit = propertyMetadata.Options.HasValueDoesNotInheritDataContext();
+				hasInherits = propertyMetadata.Options.HasInherits();
+				return;
+			}
+
+			hasValueInherits = false;
+			hasValueDoesNotInherit = false;
+			hasInherits = false;
 		}
 
 		private object? GetDefaultValue()
