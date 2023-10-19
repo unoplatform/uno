fd513d5ab72d59aaa304730483c6ed451169a00d
morning4coffe-dev <78549750+morning4coffe-dev@users.noreply.github.com>
2023-10-19 23:09:44 +0200

    feat: Add support for Mica backdrop on Android

---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/Materials/Backdrop/MicaController.unsupported.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference MicaController.cpp, commit b2aab7e
+
+#nullable enable
+
+#if !__ANDROID__
+namespace Microsoft.UI.Xaml.Controls;
+
+public partial class MicaController
+{
+	internal bool SetTarget(Windows.UI.Xaml.Window xamlWindow)
+	{
+		// Uno specific: Actual Mica is not yet supported on any target.
+		return false;
+	}
+}
+#endif
