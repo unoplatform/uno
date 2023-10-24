0abc77d9b029de8498d10dd6348a82267344c129
Youssef Victor <youssefvictor00@gmail.com>
2023-10-24 09:16:39 +0300

    chore: Fix error

--- src/Uno.UWP/UI/Color.skia.cs
+++ src/Uno.UWP/UI/Color.skia.cs
@@ -5,7 +5,7 @@ namespace Windows.UI;
 
 public partial struct Color : IFormattable
 {
-	internal static implicit operator SKColor(Color color) => new SKColor(color.R, color.G, color.B, color.A);
+	public static implicit operator SKColor(Color color) => new SKColor(color.R, color.G, color.B, color.A);
 
-	internal static implicit operator Color(SKColor color) => FromArgb(color.Alpha, color.Red, color.Green, color.Blue);
+	public static implicit operator Color(SKColor color) => FromArgb(color.Alpha, color.Red, color.Green, color.Blue);
 }
