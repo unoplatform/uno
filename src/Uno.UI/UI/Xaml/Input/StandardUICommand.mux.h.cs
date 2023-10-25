ba9c078822204c153c89bd76dbe2c3fdcd2f9d05
Martin Zikmund <martin@zikmund.dev>
2023-10-25 09:42:25 +0200

    feat: StandardUICommand

---
+++ src/Uno.UI/UI/Xaml/Input/StandardUICommand.mux.h.cs
@@ -0,0 +1,15 @@
+﻿using Windows.System;
+
+namespace Windows.UI.Xaml.Input;
+
+partial class StandardUICommand
+{
+	private bool m_ownsLabel = true;
+	private bool m_ownsIconSource = true;
+	private bool m_ownsKeyboardAccelerator = true;
+	private bool m_ownsDescription = true;
+
+	private VirtualKey m_previousAcceleratorKey;
+	private VirtualKeyModifiers m_previousAcceleratorModifiers;
+	private bool m_settingPropertyInternally;
+}
