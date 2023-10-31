582e4d44b1e86227ce931ab24f949e4c57771ec3
Jérôme Laban <jlaban@gmail.com>
2023-10-31 17:00:15 -0400

    chore: Adjust for backport

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -7,17 +7,10 @@ Set-PSDebug -Trace 1
 
 $external_docs = @{
     # use either commit, or branch name to use its latest commit
-<<<<<<< HEAD
     "uno.wasm.bootstrap" = "a7a09c6e755d17331a3bba25df642f1ec42fb6a8"
-    "uno.themes"         = "530204ba38264e66478eebdd2abc9ce05df3b0b9"
-    "uno.toolkit.ui"     = "a0a15f56d1ba4f496a42a678ff981b41bde6396a"
-    "uno.check"          = "baf57490d5cdcb7fd209db0945615c75dcb8accb"
-=======
-    "uno.wasm.bootstrap" = "5ef2048d98df307c738186a5339eedcc4665be72"
     "uno.themes"         = "37e913dea361bcc13e20587184bb3691c6b31d3b"
     "uno.toolkit.ui"     = "31028b21593d885d79873371c3d3271c72fa6482"
-    "uno.check"          = "2ef22f04f65ac954bf505f8ae84a366b99146d21"
->>>>>>> 2b9436d0ba (docs: update themes and toolkit docs)
+    "uno.check"          = "baf57490d5cdcb7fd209db0945615c75dcb8accb"
     "uno.xamlmerge.task" = "7e8ffef206e87dfea90c53805c45e93a7d8c0b46"
     "figma-docs"         = "abb770f1c853e9872d3826699d74107a5deb6538"
     "uno.resizetizer"    = "3eec4aad0b7b3480ec6c2a121911ffde844fc4f8"
