251999f7cd37f5866034cc5b15943896c8818a6c
Martin Zikmund <martin@zikmund.dev>
2023-10-31 16:50:55 +0100

    fix: Empty TextBlock should have non-zero height
    (cherry picked from commit 0dcf4c13a8f6488e609968e6482ec952b9006949)

--- src/Uno.UI/UI/Xaml/Documents/TextFormatting/FontDetails.skia.cs
+++ src/Uno.UI/UI/Xaml/Documents/TextFormatting/FontDetails.skia.cs
@@ -3,4 +3,14 @@ using SkiaSharp;
 
 namespace Windows.UI.Xaml.Documents.TextFormatting;
 
-internal record FontDetails(SKFont SKFont, float SKFontSize, float SKFontScaleX, SKFontMetrics SKFontMetrics, SKTypeface SKTypeface, Font Font, Face Face);
+internal record FontDetails(SKFont SKFont, float SKFontSize, float SKFontScaleX, SKFontMetrics SKFontMetrics, SKTypeface SKTypeface, Font Font, Face Face)
+{
+	internal float LineHeight
+	{
+		get
+		{
+			var metrics = SKFontMetrics;
+			return metrics.Descent - metrics.Ascent;
+		}
+	}
+}
