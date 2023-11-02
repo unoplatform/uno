e9746e06cbcf15c1e6e16b054876ad2eda16e679
Martin Zikmund <martin@zikmund.dev>
2023-11-02 21:45:41 +0100

    chore: Update ItemsRepeater.common to winui3/release/1.4.2

---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/Repeater/ItemsRepeater.common.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference ItemsRepeater.common.cpp, tag winui3/release/1.4.2
+
+#nullable enable
+
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+
+namespace Microsoft.UI.Xaml.Controls;
+
+internal partial class CachedVisualTreeHelpers
+{
+	public static Rect GetLayoutSlot(FrameworkElement element) => 
+		LayoutInformation.GetLayoutSlot(element);
+
+	public static DependencyObject GetParent(DependencyObject child) =>
+		VisualTreeHelper.GetParent(child);
+
+	public static IDataTemplateComponent GetDataTemplateComponent(UIElement element) =>
+		XamlBindingHelper.GetDataTemplateComponent(element);
+}
