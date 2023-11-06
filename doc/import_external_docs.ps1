2fa827eaeb51e5471d7545ae4d4f5a13da4d1617
Jérôme Laban <jlaban@gmail.com>
2023-11-06 08:56:44 -0500

    docs: Bump uno.check and extensions docs

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -10,12 +10,12 @@ $external_docs = @{
     "uno.wasm.bootstrap" = "5ef2048d98df307c738186a5339eedcc4665be72"
     "uno.themes"         = "37e913dea361bcc13e20587184bb3691c6b31d3b"
     "uno.toolkit.ui"     = "38838426c54a8465873442c7f677de457f19adbc"
-    "uno.check"          = "2ef22f04f65ac954bf505f8ae84a366b99146d21"
+    "uno.check"          = "ec524c077487a6156674c9f18fbe073eae025bbf"
     "uno.xamlmerge.task" = "7e8ffef206e87dfea90c53805c45e93a7d8c0b46"
     "figma-docs"         = "abb770f1c853e9872d3826699d74107a5deb6538"
     "uno.resizetizer"    = "3eec4aad0b7b3480ec6c2a121911ffde844fc4f8"
     "uno.uitest"         = "555453c2985ef2745fe44503c5809a6168d063c2"
-    "uno.extensions"     = "3115cb966640c78b362eed7b6a6b25cdd1bcec9a"
+    "uno.extensions"     = "ed4fda192785080f7040279643cd907e9285c232"
     "workshops"          = "c4ec4042bee6195b94d053bfb869faffc2794462"
 }
 
