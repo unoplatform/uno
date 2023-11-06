eae8cf10cd02b32cf108740190f55172d894d9bc
Martin Zikmund <martin@zikmund.dev>
2023-11-06 16:47:51 +0100

    test: Verify KeyboardAccelerators are not null in XamlUICommand

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Input/Given_XamlUICommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Input;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Input;
+
+[TestClass]
+public class Given_XamlUICommand
+{
+	[TestMethod]
+	[RunsOnUIThread]
+	public void When_KeyboardAccelerators_Retrieved()
+	{
+		var xamlUICommand = new XamlUICommand();
+		var keyboardAccelerators = xamlUICommand.KeyboardAccelerators;
+		Assert.IsNotNull(keyboardAccelerators);
+	}
+}
