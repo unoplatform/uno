908a9ac43a0341b045546593b4f5b8ccaa62fe0b
Agnès ZITTE <16295702+agneszitte@users.noreply.github.com>
2023-11-07 11:34:09 -0500

    chore: Update import_external_docs.ps1 for latest workshops doc

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -16,7 +16,7 @@ $external_docs = @{
     "uno.resizetizer"    = "3eec4aad0b7b3480ec6c2a121911ffde844fc4f8"
     "uno.uitest"         = "555453c2985ef2745fe44503c5809a6168d063c2"
     "uno.extensions"     = "ed4fda192785080f7040279643cd907e9285c232"
-    "workshops"          = "8b5722fa297872b1c3a24ad1a0f77ab0d80bb7df"
+    "workshops"          = "86256a9a42547be897e80566de608e886a9e1b52"
 }
 
 $uno_git_url = "https://github.com/unoplatform/"
