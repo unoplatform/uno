e64ec5cf15c6dcb1f03c245cded96a1fbd2002f9
Martin Zikmund <martin@zikmund.dev>
2023-11-14 09:17:47 +0100

    chore: Adjustments to CI

--- src/Uno.UWP/UI/Core/CoreWindow.iOS.cs
+++ src/Uno.UWP/UI/Core/CoreWindow.iOS.cs
@@ -6,19 +6,18 @@ using Windows.Foundation.Collections;
 using Windows.Foundation.Metadata;
 using Windows.UI.Core;
 
-namespace Windows.UI.Core
+namespace Windows.UI.Core;
+
+public partial class CoreWindow : ICoreWindowEvents
 {
-	public partial class CoreWindow : ICoreWindowEvents
-	{
-		public event TypedEventHandler<CoreWindow, KeyEventArgs> KeyDown;
+	public event TypedEventHandler<CoreWindow, KeyEventArgs> KeyDown;
 
-		public event TypedEventHandler<CoreWindow, KeyEventArgs> KeyUp;
+	public event TypedEventHandler<CoreWindow, KeyEventArgs> KeyUp;
 
-		void ICoreWindowEvents.RaiseKeyDown(KeyEventArgs eventArgs) =>
-			KeyDown?.Invoke(this, eventArgs);
+	void ICoreWindowEvents.RaiseKeyDown(KeyEventArgs eventArgs) =>
+		KeyDown?.Invoke(this, eventArgs);
 
-		void ICoreWindowEvents.RaiseKeyUp(KeyEventArgs eventArgs) =>
-			KeyUp?.Invoke(this, eventArgs);
+	void ICoreWindowEvents.RaiseKeyUp(KeyEventArgs eventArgs) =>
+		KeyUp?.Invoke(this, eventArgs);
 
-	}
 }
