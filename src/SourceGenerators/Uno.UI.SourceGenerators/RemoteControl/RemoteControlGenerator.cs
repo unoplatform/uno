e48bf98d4c0166c684ca8c093b76c0a2dc82c648
Jerome Laban <jerome.laban@nventive.com>
2023-11-24 17:35:24 -0500

    fix(HR): Include path to C# files to ensure partial HR picks up CSM changes
    (cherry picked from commit 3b87c73dae6af94703dd4e4b11d77ae3b734a0ae)

--- src/SourceGenerators/Uno.UI.SourceGenerators/RemoteControl/RemoteControlGenerator.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators/RemoteControl/RemoteControlGenerator.cs
@@ -235,7 +235,10 @@ namespace Uno.UI.SourceGenerators.RemoteControl
 					.GetMSBuildItemsWithAdditionalFiles(s)
 					.Select(v => Path.IsPathRooted(v.Identity) ? v.Identity : Path.Combine(msBuildProjectDirectory, v.Identity));
 
-			var xamlPaths = from item in sources.SelectMany(BuildSearchPath)
+			IEnumerable<string> BuildCompilePaths()
+				=> context.Compilation.SyntaxTrees.Select(s => s.FilePath);
+
+			var xamlPaths = from item in sources.SelectMany(BuildSearchPath).Concat(BuildCompilePaths())
 							select Path.GetDirectoryName(item);
 
 			return xamlPaths.Distinct().Where(x => !string.IsNullOrEmpty(x));
