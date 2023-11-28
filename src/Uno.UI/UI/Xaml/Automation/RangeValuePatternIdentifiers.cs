ea91f160d27619cedad80fa72ca83ec97706bc06
morning4coffe-dev <78549750+morning4coffe-dev@users.noreply.github.com>
2023-11-29 00:07:37 +0100

    chore: Update new to use simplified form

--- src/Uno.UI/UI/Xaml/Automation/RangeValuePatternIdentifiers.cs
+++ src/Uno.UI/UI/Xaml/Automation/RangeValuePatternIdentifiers.cs
@@ -8,30 +8,30 @@ public partial class RangeValuePatternIdentifiers
 	/// <summary>
 	/// Identifies the IsReadOnly automation property.
 	/// </summary>
-	public static AutomationProperty IsReadOnlyProperty { get; } = new AutomationProperty();
+	public static AutomationProperty IsReadOnlyProperty { get; } = new();
 
 	/// <summary>
 	/// Identifies the LargeChange automation property.
 	/// </summary>
-	public static AutomationProperty LargeChangeProperty { get; } = new AutomationProperty();
+	public static AutomationProperty LargeChangeProperty { get; } = new();
 
 	/// <summary>
 	/// Identifies the Maximum automation property.
 	/// </summary>
-	public static AutomationProperty MaximumProperty { get; } = new AutomationProperty();
+	public static AutomationProperty MaximumProperty { get; } = new();
 
 	/// <summary>
 	/// Identifies the Minimum automation property.
 	/// </summary>
-	public static AutomationProperty MinimumProperty { get; } = new AutomationProperty();
+	public static AutomationProperty MinimumProperty { get; } = new();
 
 	/// <summary>
 	/// Identifies the SmallChange automation property.
 	/// </summary>
-	public static AutomationProperty SmallChangeProperty { get; } = new AutomationProperty();
+	public static AutomationProperty SmallChangeProperty { get; } = new();
 
 	/// <summary>
 	/// Identifies the Value automation property.
 	/// </summary>
-	public static AutomationProperty ValueProperty { get; } = new AutomationProperty();
+	public static AutomationProperty ValueProperty { get; } = new();
 }
