a4b2275aab0caae1cd1ba57dcc661150f0771c19
Youssef Victor <youssefvictor00@gmail.com>
2023-11-28 17:14:25 +0200

    fix(radiobuttons): Finalizer shouldn't access a DP

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/RadioButtons/RadioButtons.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/RadioButtons/RadioButtons.cs
@@ -75,6 +75,7 @@ namespace Microsoft.UI.Xaml.Controls
 					repeater.ElementClearing += OnRepeaterElementClearing;
 					repeater.ElementIndexChanged += OnRepeaterElementIndexChanged;
 					repeater.Loaded += OnRepeaterLoaded;
+					repeater.Unloaded += OnRepeaterUnloaded;
 					return repeater;
 				}
 				return null;
@@ -664,7 +665,7 @@ namespace Microsoft.UI.Xaml.Controls
 			}
 		}
 
-		~RadioButtons()
+		private void OnRepeaterUnloaded(object sender, RoutedEventArgs args)
 		{
 			var layout = GetLayout();
 			if (layout != null)
