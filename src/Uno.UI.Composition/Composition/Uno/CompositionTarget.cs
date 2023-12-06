baf90b3780b47488a3926e7e006a86ee01e633c2
Ramez Ragaa <ramezragaa@proton.me>
2023-12-06 19:43:37 +0200

    chore: CompositionTarget.Root should set the Root's CompositionTarget to itself

--- src/Uno.UI.Composition/Composition/Uno/CompositionTarget.cs
+++ src/Uno.UI.Composition/Composition/Uno/CompositionTarget.cs
@@ -2,9 +2,18 @@ namespace Windows.UI.Composition;
 
 public partial class CompositionTarget
 {
+	private Visual _root;
 	internal CompositionTarget()
 	{
 	}
 
-	public Visual Root { get; set; }
+	public Visual Root
+	{
+		get => _root;
+		set
+		{
+			_root = value;
+			_root.CompositionTarget = this;
+		}
+	}
 }
