8c1d617607820de427120b383245346678f5e409
Martin Zikmund <martin@zikmund.dev>
2023-12-06 22:17:01 -0500

    chore: Run tests on Uno only
    (cherry picked from commit a9bac3f2a50d67d55d26838988d4e6a607ec1432)

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ContentControl.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ContentControl.cs
@@ -187,7 +187,7 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 			Assert.IsTrue(comboBox.SelectedIndex == -1);
 		}
 
-
+#if HAS_UNO
 		[TestMethod]
 		[RunsOnUIThread]
 		public async Task When_FindName_ContentControl_Without_ContentTemplate()
@@ -222,6 +222,7 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 
 			Assert.IsNull(sut.FindName("ContentElement"));
 		}
+#endif
 
 		private class SignInViewModel
 		{
