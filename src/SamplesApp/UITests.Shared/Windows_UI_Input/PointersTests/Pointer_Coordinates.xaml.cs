7c2a48576040a6e9046b8366703c3a456bba6149
Ramez Ragaa <ramezragaa@proton.me>
2023-12-14 16:16:46 +0200

    chore: set IsManualSet

--- src/SamplesApp/UITests.Shared/Windows_UI_Input/PointersTests/Pointer_Coordinates.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Input/PointersTests/Pointer_Coordinates.xaml.cs
@@ -5,7 +5,7 @@ using Windows.UI.Xaml.Input;
 
 namespace UITests.Windows_UI_Input.PointersTests
 {
-	[Sample("Pointers", Description = "This sample tests a GTK issue where clicking on a native element (such as TextBox) would send incorrect pointer coordinates, leading to pointer events being fired on incorrect controls.")]
+	[Sample("Pointers", IsManualTest = true, Description = "This sample tests a GTK issue where clicking on a native element (such as TextBox) would send incorrect pointer coordinates, leading to pointer events being fired on incorrect controls.")]
 	public sealed partial class Pointer_Coordinates : Page
 	{
 		public Pointer_Coordinates()
