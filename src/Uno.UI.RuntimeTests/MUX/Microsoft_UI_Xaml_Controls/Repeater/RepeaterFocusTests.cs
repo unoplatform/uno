20559f5a791302b3de3692575e225ba9f8b3be42
Martin Zikmund <martin@zikmund.dev>
2023-12-15 14:46:46 +0100

    chore: Re-enable repeater focus tests

--- src/Uno.UI.RuntimeTests/MUX/Microsoft_UI_Xaml_Controls/Repeater/RepeaterFocusTests.cs
+++ src/Uno.UI.RuntimeTests/MUX/Microsoft_UI_Xaml_Controls/Repeater/RepeaterFocusTests.cs
@@ -40,9 +40,6 @@ namespace Windows.UI.Xaml.Tests.MUXControls.ApiTests.RepeaterTests
 	[TestClass]
 	[RequiresFullWindow]
 	[Uno.UI.RuntimeTests.RunsOnUIThread]
-#if WINUI_WINDOWING
-	[Ignore("The test relies on static FocusManager methods which do not work in WinUI3")]
-#endif
 	public class RepeaterFocusTests : MUXApiTestBase
 	{
 #if __MACOS__
