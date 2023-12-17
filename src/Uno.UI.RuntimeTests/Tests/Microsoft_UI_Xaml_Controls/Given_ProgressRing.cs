2145a6794b9c2b1aa054aee51249fb4296863d77
Youssef Victor <youssefvictor00@gmail.com>
2023-12-17 08:56:47 +0200

    chore: De-activate progress ring

--- src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_ProgressRing.cs
+++ src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_ProgressRing.cs
@@ -19,28 +19,35 @@ public class Given_ProgressRing
 	public async Task When_NoProgress_IsIndeterminate_Toggle()
 	{
 		var SUT = new ProgressRing { Width = 20, Height = 20 };
-		(SUT.Minimum, SUT.Value, SUT.Maximum) = (0, 0, 100);
-		SUT.IsActive = true;
-		SUT.IsIndeterminate = false;
-
-		TestServices.WindowHelper.WindowContent = SUT;
-		await TestServices.WindowHelper.WaitForLoaded(SUT);
-		await TestServices.WindowHelper.WaitForIdle();
-
-		var player = SUT.FindFirstDescendant<AnimatedVisualPlayer>(x => x.Name == "LottiePlayer");
-		if (player is null)
+		try
 		{
-			// The test may be invalid due to template change.
-			Assert.IsNotNull(player, "Failed to find template part: AnimatedVisualPlayer#LottiePlayer");
+			(SUT.Minimum, SUT.Value, SUT.Maximum) = (0, 0, 100);
+			SUT.IsActive = true;
+			SUT.IsIndeterminate = false;
+
+			TestServices.WindowHelper.WindowContent = SUT;
+			await TestServices.WindowHelper.WaitForLoaded(SUT);
+			await TestServices.WindowHelper.WaitForIdle();
+
+			var player = SUT.FindFirstDescendant<AnimatedVisualPlayer>(x => x.Name == "LottiePlayer");
+			if (player is null)
+			{
+				// The test may be invalid due to template change.
+				Assert.IsNotNull(player, "Failed to find template part: AnimatedVisualPlayer#LottiePlayer");
+			}
+
+			SUT.IsIndeterminate = true;
+			await TestServices.WindowHelper.WaitForIdle();
+			Assert.IsTrue(player.IsPlaying, "LottiePlayer should be playing.");
+
+			SUT.IsIndeterminate = false;
+			await TestServices.WindowHelper.WaitForIdle();
+			Assert.IsFalse(player.IsPlaying, "LottiePlayer should have stopped playing.");
 		}
-
-		SUT.IsIndeterminate = true;
-		await TestServices.WindowHelper.WaitForIdle();
-		Assert.IsTrue(player.IsPlaying, "LottiePlayer should be playing.");
-
-		SUT.IsIndeterminate = false;
-		await TestServices.WindowHelper.WaitForIdle();
-		Assert.IsFalse(player.IsPlaying, "LottiePlayer should have stopped playing.");
+		finally
+		{
+			SUT.IsActive = false;
+		}
 	}
 
 	[TestMethod]
@@ -50,30 +57,37 @@ public class Given_ProgressRing
 	public async Task When_HalfProgress_IsIndeterminate_Toggle()
 	{
 		var SUT = new ProgressRing { Width = 20, Height = 20 };
-		(SUT.Minimum, SUT.Value, SUT.Maximum) = (0, 50, 100);
-		SUT.IsActive = true;
-		SUT.IsIndeterminate = false;
-
-		TestServices.WindowHelper.WindowContent = SUT;
-		await TestServices.WindowHelper.WaitForLoaded(SUT);
-		await TestServices.WindowHelper.WaitForIdle();
-
-		var player = SUT.FindFirstDescendant<AnimatedVisualPlayer>(x => x.Name == "LottiePlayer");
-		if (player is null)
+		try
 		{
-			// The test may be invalid due to template change.
-			Assert.IsNotNull(player, "Failed to find template part: AnimatedVisualPlayer#LottiePlayer");
+			(SUT.Minimum, SUT.Value, SUT.Maximum) = (0, 50, 100);
+			SUT.IsActive = true;
+			SUT.IsIndeterminate = false;
+
+			TestServices.WindowHelper.WindowContent = SUT;
+			await TestServices.WindowHelper.WaitForLoaded(SUT);
+			await TestServices.WindowHelper.WaitForIdle();
+
+			var player = SUT.FindFirstDescendant<AnimatedVisualPlayer>(x => x.Name == "LottiePlayer");
+			if (player is null)
+			{
+				// The test may be invalid due to template change.
+				Assert.IsNotNull(player, "Failed to find template part: AnimatedVisualPlayer#LottiePlayer");
+			}
+
+			SUT.IsIndeterminate = true;
+			await TestServices.WindowHelper.WaitForIdle();
+			Assert.IsTrue(player.IsPlaying, "LottiePlayer should be playing.");
+
+			// note: unlike in When_NoProgress_IsIndeterminate_Toggle, the progress was (and still is) at 50%.
+			// so upon entering "Determinate" again, it (should animate 0->50%)? before coming to a stop.
+			SUT.IsIndeterminate = false;
+			await TestServices.WindowHelper.WaitForIdle();
+			//Assert.IsTrue(player.IsPlaying, "LottiePlayer should be animating briefly from 0% to 50%."); // not the case for windows, but this is animated on uno
+			await TestServices.WindowHelper.WaitFor(() => player.IsPlaying == false, timeoutMS: 2000, "LottiePlayer should be eventually stop playing.");
 		}
-
-		SUT.IsIndeterminate = true;
-		await TestServices.WindowHelper.WaitForIdle();
-		Assert.IsTrue(player.IsPlaying, "LottiePlayer should be playing.");
-
-		// note: unlike in When_NoProgress_IsIndeterminate_Toggle, the progress was (and still is) at 50%.
-		// so upon entering "Determinate" again, it (should animate 0->50%)? before coming to a stop.
-		SUT.IsIndeterminate = false;
-		await TestServices.WindowHelper.WaitForIdle();
-		//Assert.IsTrue(player.IsPlaying, "LottiePlayer should be animating briefly from 0% to 50%."); // not the case for windows, but this is animated on uno
-		await TestServices.WindowHelper.WaitFor(() => player.IsPlaying == false, timeoutMS: 2000, "LottiePlayer should be eventually stop playing.");
+		finally
+		{
+			SUT.IsActive = false;
+		}
 	}
 }
