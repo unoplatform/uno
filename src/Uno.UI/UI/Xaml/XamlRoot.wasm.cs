ff6be00c2eff87b050c3dac54862383f2e0a3e35
Youssef Victor <youssefvictor00@gmail.com>
2023-12-19 10:31:37 +0200

    chore: Comment unused

--- src/Uno.UI/UI/Xaml/XamlRoot.wasm.cs
+++ src/Uno.UI/UI/Xaml/XamlRoot.wasm.cs
@@ -8,51 +8,51 @@ namespace Windows.UI.Xaml;
 
 partial class XamlRoot
 {
-	private bool _invalidateRequested;
+	//private bool _invalidateRequested;
 
-	private void InnerInvalidateMeasure()
-	{
-		if (!_invalidateRequested)
-		{
-			_invalidateRequested = true;
+	//private void InnerInvalidateMeasure()
+	//{
+	//	if (!_invalidateRequested)
+	//	{
+	//		_invalidateRequested = true;
 
-			if (this.Log().IsEnabled(Uno.Foundation.Logging.LogLevel.Debug))
-			{
-				this.Log().Debug("DispatchInvalidateMeasure scheduled");
-			}
+	//		if (this.Log().IsEnabled(Uno.Foundation.Logging.LogLevel.Debug))
+	//		{
+	//			this.Log().Debug("DispatchInvalidateMeasure scheduled");
+	//		}
 
-			_ = CoreDispatcher.Main.RunAsync(
-				CoreDispatcherPriority.Normal,
-				() =>
-				{
-					_invalidateRequested = false;
+	//		_ = CoreDispatcher.Main.RunAsync(
+	//			CoreDispatcherPriority.Normal,
+	//			() =>
+	//			{
+	//				_invalidateRequested = false;
 
-					DispatchInvalidateMeasure();
-				}
-			);
-		}
-	}
+	//				DispatchInvalidateMeasure();
+	//			}
+	//		);
+	//	}
+	//}
 
-	private void DispatchInvalidateMeasure()
-	{
-		if (VisualTree.RootElement is not { } rootElement)
-		{
-			return;
-		}
+	//private void DispatchInvalidateMeasure()
+	//{
+	//	if (VisualTree.RootElement is not { } rootElement)
+	//	{
+	//		return;
+	//	}
 
-		if (this.Log().IsEnabled(Uno.Foundation.Logging.LogLevel.Debug))
-		{
-			var sw = Stopwatch.StartNew();
-			rootElement.Measure(Bounds.Size);
-			rootElement.Arrange(Bounds);
-			sw.Stop();
+	//	if (this.Log().IsEnabled(Uno.Foundation.Logging.LogLevel.Debug))
+	//	{
+	//		var sw = Stopwatch.StartNew();
+	//		rootElement.Measure(Bounds.Size);
+	//		rootElement.Arrange(Bounds);
+	//		sw.Stop();
 
-			this.Log().Debug($"DispatchInvalidateMeasure: {sw.Elapsed}");
-		}
-		else
-		{
-			rootElement.Measure(Bounds.Size);
-			rootElement.Arrange(Bounds);
-		}
-	}
+	//		this.Log().Debug($"DispatchInvalidateMeasure: {sw.Elapsed}");
+	//	}
+	//	else
+	//	{
+	//		rootElement.Measure(Bounds.Size);
+	//		rootElement.Arrange(Bounds);
+	//	}
+	//}
 }
