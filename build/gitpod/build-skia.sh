b4b704921334e8d941e12e9821dd7df658e4bab3
Jérôme Laban <jlaban@gmail.com>
2023-12-21 15:16:02 -0500

    chore: Adjust default build commands

--- build/gitpod/build-skia.sh
+++ build/gitpod/build-skia.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-pushd $GITPOD_REPO_ROOT/src/SamplesApp/SamplesApp.Skia.Gtk
-
 export NUGET_PACKAGES=/workspace/.nuget
-
-dotnet build /bl SamplesApp.Skia.Gtk.csproj /p:UnoTargetFrameworkOverride=net7.0
+dotnet build /bl src/Uno.UI-Skia-only.slnf /p:UnoTargetFrameworkOverride=net7.0
 
 popd
