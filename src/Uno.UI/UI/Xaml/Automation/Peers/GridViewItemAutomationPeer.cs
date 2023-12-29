369c38f0627bde411b9655fd1d04709f41f40bbf
morning4coffe-dev <78549750+morning4coffe-dev@users.noreply.github.com>
2023-12-29 22:29:25 +0100

    chore: Minor adjustments

--- src/Uno.UI/UI/Xaml/Automation/Peers/GridViewItemAutomationPeer.cs
+++ src/Uno.UI/UI/Xaml/Automation/Peers/GridViewItemAutomationPeer.cs
@@ -1,3 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference GridViewItemAutomationPeer_Partial.cpp, tag winui3/release/1.4.2
+
 using Microsoft.UI.Xaml.Controls;
 
 namespace Microsoft.UI.Xaml.Automation.Peers;
