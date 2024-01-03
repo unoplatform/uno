e38ca4af3d1e64bd87446f3fe02041d77a0625f5
Martin Zikmund <martin@zikmund.dev>
2024-01-03 17:50:00 +0100

    chore: Start porting application initialization sequence

--- src/Uno.UI/UI/Xaml/ApplicationInitializationCallbackParams.cs
+++ src/Uno.UI/UI/Xaml/ApplicationInitializationCallbackParams.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Microsoft.UI.Xaml;
 
-namespace Microsoft.UI.Xaml
+public sealed partial class ApplicationInitializationCallbackParams
 {
-	public sealed partial class ApplicationInitializationCallbackParams
-	{
-	}
 }
