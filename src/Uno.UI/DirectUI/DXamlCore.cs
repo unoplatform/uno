b9c33eea19fabab5b5b746bac3e07fff73e34972
Martin Zikmund <martin@zikmund.dev>
2024-01-05 14:36:47 +0100

    chore: Some additional required infrastructure

--- src/Uno.UI/DirectUI/DXamlCore.cs
+++ src/Uno.UI/DirectUI/DXamlCore.cs
@@ -13,6 +13,7 @@ using Windows.Foundation;
 using Microsoft.UI.Xaml.Controls;
 using static Microsoft.UI.Xaml.Controls._Tracing;
 using Uno.Disposables;
+using Windows.UI.Core;
 
 namespace DirectUI;
 
@@ -25,13 +26,15 @@ internal partial class DXamlCore
 	private BuildTreeService? _buildTreeService;
 	private BudgetManager? _budgetManager;
 
+	public static bool IsInitializedStatic => Current is { } current && current.IsInitialized;
+
 	// UNO: This should **NOT** create the singleton!
 	//		_but_ if we do return a 'null' the 'OnApplyTemplate' of the `CalendarView` will fail.
 	//		As for now our implementation of the 'DXamlCore' is pretty light and stored as a basic singleton,
 	//		we accept to create it even with the "NoCreate" overload.
-	public static DXamlCore Current => _instance;
+	public static DXamlCore Current => _instance!;
 
-	public static DXamlCore GetCurrentNoCreate() => Current;
+	public static DXamlCore? GetCurrentNoCreate() => Current;
 
 	public Uno.UI.Xaml.Core.CoreServices GetHandle() => Uno.UI.Xaml.Core.CoreServices.Instance;
 
@@ -136,7 +139,7 @@ internal partial class DXamlCore
 				// Now that we have a good core, we can register with the ReferenceTrackerManager
 				if (InitializationType.FromIdle != initializationType)
 				{
-					ReferenceTrackerManager.RegisterCore(pCore);
+					// ReferenceTrackerManager.RegisterCore(pCore);
 				}
 
 				// Initialize simple property callbacks for CUIElement.  This needs to happen regardless of initialization type,
@@ -163,7 +166,7 @@ internal partial class DXamlCore
 			// WPF tests can can initialize the DXamlCore on a separate UI thread, which means they don't find the
 			// previous DXamlCore to call InitializeInstanceFromIdle. They also don't go through DllMain again, so
 			// make sure that the DCompSurfaceFactoryManager is initialized.
-			DCompSurfaceFactoryManager.EnsureInitialized();
+			// DCompSurfaceFactoryManager.EnsureInitialized();
 		}
 	}
 
@@ -195,134 +198,200 @@ internal partial class DXamlCore
 		_state = DXamlCore.State.Initializing;
 
 		// Initialize the lock used to access the peer table.
-		InitializeSRWLock(&m_peerReferenceLock);
+		//	InitializeSRWLock(&m_peerReferenceLock);
 
-		// Take a reference on the FinalUnhandledErrorDetected event registration.
-		// This reference will be released when this DXamlCore instance is deinitialized.
-		IFC(ErrorHelper.GetFinalUnhandledErrorDetectedRegistration()->AddRefRegistration());
+		//	// Take a reference on the FinalUnhandledErrorDetected event registration.
+		//	// This reference will be released when this DXamlCore instance is deinitialized.
+		//	IFC(ErrorHelper.GetFinalUnhandledErrorDetectedRegistration()->AddRefRegistration());
 
-		// Create a PLM handler for this core, if this process is packaged -- and if we're not using
-		// "IslandsOnly" initialization, which means we're running in a win32 context.
-		if (gps->IsProcessPackaged() && (initializationType != InitializationType.IslandsOnly))
+		//	// Create a PLM handler for this core, if this process is packaged -- and if we're not using
+		//	// "IslandsOnly" initialization, which means we're running in a win32 context.
+		//	if (gps->IsProcessPackaged() && (initializationType != InitializationType.IslandsOnly))
+		//	{
+		//		initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.packaged_process));
+		//		IFC(XAML.PLM.PLMHandler.CreateForASTA(this, &m_pPLMHandler));
+		//	}
+
+		//	if (initializationType != InitializationType.IslandsOnly)
+		//	{
+		//		initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.init_type_uwp));
+		//		// When running in UWP, we must make sure there's a DispatcherQueueController on the thread, since XAML
+		//		// requires one to be running.  Since we don't support UWP, we don't bother to shutdown the DQC, this is
+		//		// just to keep XAML tests running.
+		//		DispatcherQueueControllerStatics > dispatcherQueueControllerStatics;
+
+		//		IFCFAILFAST(GetActivationFactory(
+		//			wrl.Wrappers.HStringReference(RuntimeClass_Microsoft_UI_Dispatching_DispatcherQueueController).Get(),
+		//					&dispatcherQueueControllerStatics));
+		//		IFCFAILFAST(dispatcherQueueControllerStatics->CreateOnCurrentThread(&m_dispatcherQueueController));
+		//	}
+
+		//	if (initializationType == InitializationType.MainView)
+		//	{
+		//		initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.init_type_main_view));
+		//		// We allow this temporarily only for UWP because we're not supporting it for foward-compat yet.
+		//		//  Task 29643834: Remove use of textinputproducerinternal.h before we open-source and before we fully-support UWP
+		//		//                 (ITextInputConsumer, ITextInputProducer, ITextInputProducerInternal)
+		//		TextInputProducerHelper.SetAllowCallsToPrivateWindowsFunctions(true);
+		//	}
+
+		//	XamlOneCoreTransforms.EnsureInitialized(XamlOneCoreTransforms.InitMode.Normal);
+
+		//	IFC(JupiterControl.Create(&m_pControl));
+
+		//	pCore = m_pControl->GetCoreServices();
+		//	IFCEXPECT(pCore);
+
+		//	IFC(pCore->SetCurrentApplication(NULL));
+
+		//	// Keep a reference from the core context to the DXamlCore
+		//	IFC(CoreImports.SetFrameworkContext(pCore, this));
+		//	ReplaceInterface(m_hCore, pCore);
+		//	pCore = NULL;
+
+
+		//	// TSF3 rely on CoreDispathcer. TextServicesManager checks for a CoreDispatcher to determine UIThread and fails in desktop/ island without CoreWindow, hence
+		//	// in Island mode force to use TSF1
+		//	if (initializationType == InitializationType.IslandsOnly)
+		//	{
+		//		initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.init_type_islands_only));
+		//		m_hCore->ForceDisableTSF3();
+		//		initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.disabled_tsf3));
+		//	}
+
+		//	// initialize the XAML dispatcher
+		//	IFC(ctl.ComObject<DispatcherImpl>.CreateInstance(m_spDispatcherImpl.ReleaseAndGetAddressOf()));
+		//	IFC(m_spDispatcherImpl->Connect(this));
+		//	initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.initialized_dispatcher));
+
+		//	// Disable the legacy IME since the legacy IMEs aren't designed for the immersive environment.
+		//	//
+		//	// We only want to do this when not in design mode, since the design-mode process should use the legacy IMEs.
+		//	ImmDisableLegacyIME();
+
+		//	m_hCore->SetInitializationType(initializationType);
+
+		//	m_pDragDrop = NULL;
+
+		//	m_staticStoreGuard = StaticStore.GetInstance();
+
+		//	// Initialize the default control styles cache
+		//	m_pDefaultStyles = new DefaultStyles();
+
+		//	// Force the theming object to update system color brushes as well as
+		//	// query initial system theme.
+		//	IFC(m_hCore->GetFrameworkTheming()->OnThemeChanged());
+
+		//	IFC(EnsureEventArgs());
+
+		//	IFC(Window.Create(this, &m_uwpWindowNoRef));
+		//	initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.created_uwp_window));
+
+		//	// The Window needs to be pegged because it doesn't have an entry in the PeerTable,
+		//	// and its members can potentially be GCed.
+		//	m_uwpWindowNoRef->UpdatePeg(true);
+
+		//	IFC(ctl.make<UIAffinityReleaseQueue>(&m_spReleaseQueue));
+
+		//	// Initialize Visual Diagnostics
+		//	IFC(RegisterVisualDiagnosticsPort());
+
+		//	_state = DXamlCore.Initialized;
+
+		//	// Now that DXamlCore is initialized, listen to the UISettings' AnimationsEnabledChanged event for future updates
+		//	// and immediately refresh the m_isAnimationEnabled field.
+		//	IFC(AddAnimationsEnabledChangedHandler());
+		//	// m_animationsEnabledChangedToken will be zero if AddAnimationsEnabledChangedHandler() fails to QI IUISettings6
+		//	if (m_animationsEnabledChangedToken.value != 0)
+		//	{
+		//		IFC(UpdateAnimationsEnabled());
+		//	}
+
+		//Cleanup:
+		//	if (FAILED(hr))
+		//	{
+		//		_state = DXamlCore.InitializationFailed;
+		//		initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.failed_dxamlcore_init));
+		//		if (m_uwpWindowNoRef != nullptr)
+		//		{
+		//			m_uwpWindowNoRef->SetDXamlCore(nullptr);
+		//			m_uwpWindowNoRef->UpdatePeg(false);
+		//		}
+		//	}
+
+		//	ReleaseInterface(pCore);
+		//	ReleaseInterface(pSuspendingEventHandler);
+		//	ReleaseInterface(pResumingEventHandler);
+
+		//	TraceInitializeCoreEnd();
+
+		//	// End Tip Test - if cleanup is called
+		//	initDxamlCoreTest.complete();
+	}
+
+	internal void EnsureCoreApplicationInitialized()
+	{
+		//	if (!m_pDOCoreApp)
+		//	{
+		//		FrameworkApplication applicationNoRef = nullptr;
+		//		IFC(CoreImports::CreateObjectByTypeIndex(
+		//			GetHandle(),
+		//			KnownTypeIndex::Application,
+		//			&m_pDOCoreApp));
+
+		//		IFC(CoreImports::Application_JupiterComplete(m_pDOCoreApp));
+
+		//		// automatic loading of app.xaml
+		//		applicationNoRef = FrameworkApplication::GetCurrentNoRef();
+		//		xstring_ptr strAppXaml = applicationNoRef ? applicationNoRef->GetAppXamlPath() : xstring_ptr::EmptyString();
+
+		//		if (ShouldTryLoadAppXaml(strAppXaml))
+		//		{
+		//			IFCPTR(applicationNoRef);
+		//			applicationNoRef->SetRequestedThemeNotSettable();
+		//			applicationNoRef->SetRequiresPointerModeNotSettable();
+
+		//			// Load App.xaml
+		//			IFC(FrameworkApplication::LoadComponent(
+		//				ctl::iinspectable_cast(applicationNoRef),
+		//				strAppXaml));
+		//		}
+
+		//		if (applicationNoRef)
+		//		{
+		//			ctl::ComPtr<xaml::IDebugSettings> debugSettings;
+		//			IGNOREHR(applicationNoRef->get_DebugSettings(&debugSettings));
+		//			if (debugSettings)
+		//			{
+		//				debugSettings.Cast<DebugSettings>()->OnThreadInitialized();
+		//			}
+		//		}
+		//	}
+
+		//Cleanup:
+		//	if (FAILED(hr))
+		//	{
+		//		// This is how errors encountered while parsing app.xaml are bubbled up to app code.
+		//		IGNOREHR(ErrorHelper::ReportUnhandledError(hr));
+		//	}
+	}
+
+	internal void ConfigureJupiterWindow(CoreWindow? pCoreWindow)
+	{
+		if (_control is null)
 		{
-			initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.packaged_process));
-			IFC(XAML.PLM.PLMHandler.CreateForASTA(this, &m_pPLMHandler));
+			throw new InvalidOperationException("JupiterControl must be initialized before window is set");
 		}
 
-		if (initializationType != InitializationType.IslandsOnly)
-		{
-			initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.init_type_uwp));
-			// When running in UWP, we must make sure there's a DispatcherQueueController on the thread, since XAML
-			// requires one to be running.  Since we don't support UWP, we don't bother to shutdown the DQC, this is
-			// just to keep XAML tests running.
-			DispatcherQueueControllerStatics > dispatcherQueueControllerStatics;
+		_control.ConfigureJupiterWindow(pCoreWindow);
 
-			IFCFAILFAST(GetActivationFactory(
-				wrl.Wrappers.HStringReference(RuntimeClass_Microsoft_UI_Dispatching_DispatcherQueueController).Get(),
-						&dispatcherQueueControllerStatics));
-			IFCFAILFAST(dispatcherQueueControllerStatics->CreateOnCurrentThread(&m_dispatcherQueueController));
-		}
-
-		if (initializationType == InitializationType.MainView)
-		{
-			initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.init_type_main_view));
-			// We allow this temporarily only for UWP because we're not supporting it for foward-compat yet.
-			//  Task 29643834: Remove use of textinputproducerinternal.h before we open-source and before we fully-support UWP
-			//                 (ITextInputConsumer, ITextInputProducer, ITextInputProducerInternal)
-			TextInputProducerHelper.SetAllowCallsToPrivateWindowsFunctions(true);
-		}
-
-		XamlOneCoreTransforms.EnsureInitialized(XamlOneCoreTransforms.InitMode.Normal);
-
-		IFC(JupiterControl.Create(&m_pControl));
-
-		pCore = m_pControl->GetCoreServices();
-		IFCEXPECT(pCore);
-
-		IFC(pCore->SetCurrentApplication(NULL));
-
-		// Keep a reference from the core context to the DXamlCore
-		IFC(CoreImports.SetFrameworkContext(pCore, this));
-		ReplaceInterface(m_hCore, pCore);
-		pCore = NULL;
-
-
-		// TSF3 rely on CoreDispathcer. TextServicesManager checks for a CoreDispatcher to determine UIThread and fails in desktop/ island without CoreWindow, hence
-		// in Island mode force to use TSF1
-		if (initializationType == InitializationType.IslandsOnly)
-		{
-			initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.init_type_islands_only));
-			m_hCore->ForceDisableTSF3();
-			initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.disabled_tsf3));
-		}
-
-		// initialize the XAML dispatcher
-		IFC(ctl.ComObject<DispatcherImpl>.CreateInstance(m_spDispatcherImpl.ReleaseAndGetAddressOf()));
-		IFC(m_spDispatcherImpl->Connect(this));
-		initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.initialized_dispatcher));
-
-		// Disable the legacy IME since the legacy IMEs aren't designed for the immersive environment.
-		//
-		// We only want to do this when not in design mode, since the design-mode process should use the legacy IMEs.
-		ImmDisableLegacyIME();
-
-		m_hCore->SetInitializationType(initializationType);
-
-		m_pDragDrop = NULL;
-
-		m_staticStoreGuard = StaticStore.GetInstance();
-
-		// Initialize the default control styles cache
-		m_pDefaultStyles = new DefaultStyles();
-
-		// Force the theming object to update system color brushes as well as
-		// query initial system theme.
-		IFC(m_hCore->GetFrameworkTheming()->OnThemeChanged());
-
-		IFC(EnsureEventArgs());
-
-		IFC(Window.Create(this, &m_uwpWindowNoRef));
-		initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.created_uwp_window));
-
-		// The Window needs to be pegged because it doesn't have an entry in the PeerTable,
-		// and its members can potentially be GCed.
-		m_uwpWindowNoRef->UpdatePeg(true);
-
-		IFC(ctl.make<UIAffinityReleaseQueue>(&m_spReleaseQueue));
-
-		// Initialize Visual Diagnostics
-		IFC(RegisterVisualDiagnosticsPort());
-
-		_state = DXamlCore.Initialized;
-
-		// Now that DXamlCore is initialized, listen to the UISettings' AnimationsEnabledChanged event for future updates
-		// and immediately refresh the m_isAnimationEnabled field.
-		IFC(AddAnimationsEnabledChangedHandler());
-		// m_animationsEnabledChangedToken will be zero if AddAnimationsEnabledChangedHandler() fails to QI IUISettings6
-		if (m_animationsEnabledChangedToken.value != 0)
-		{
-			IFC(UpdateAnimationsEnabled());
-		}
-
-	Cleanup:
-		if (FAILED(hr))
-		{
-			_state = DXamlCore.InitializationFailed;
-			initDxamlCoreTest.set_flag(TIP_reason(DXamlInitializeCoreTest.reason.failed_dxamlcore_init));
-			if (m_uwpWindowNoRef != nullptr)
-			{
-				m_uwpWindowNoRef->SetDXamlCore(nullptr);
-				m_uwpWindowNoRef->UpdatePeg(false);
-			}
-		}
-
-		ReleaseInterface(pCore);
-		ReleaseInterface(pSuspendingEventHandler);
-		ReleaseInterface(pResumingEventHandler);
-
-		TraceInitializeCoreEnd();
-
-		// End Tip Test - if cleanup is called
-		initDxamlCoreTest.complete();
+		//if (pCoreWindow is not null)
+		//{
+		//	SetCoreWindow(pCoreWindow);
+		//}
+		//else // CoreWindow does not exist in Win32/ Islands
+		//{
+		//	XamlPalSetCoreWindow();
+		//}
 	}
 }
