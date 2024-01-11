dae07094743a9f0071b8da5999f5302f8429e797
Martin Zikmund <martin@zikmund.dev>
2024-01-11 16:37:31 +0100

    fix: Looping selector should size panel only when scroll viewer is initialized

--- src/Uno.UI/UI/Xaml/Controls/Primitives/LoopingSelector/LoopingSelector_Partial.cs
+++ src/Uno.UI/UI/Xaml/Controls/Primitives/LoopingSelector/LoopingSelector_Partial.cs
@@ -1795,6 +1795,15 @@ namespace Microsoft.UI.Xaml.Controls.Primitives
 			FrameworkElement spPanelAsFE;
 			UIElement spThisAsUI;
 
+#if HAS_UNO // Uno specific: Due to lifecycle differences, the ScrollViewer is may not be initialized at this point.
+			// If ViewportHeight is 0, we would temporarily size the panel incorrectly, which could cause the selected
+			// item to be changed.
+			if (_tpScrollViewer is null || _tpScrollViewer.ViewportHeight == 0)
+			{
+				return;
+			}
+#endif
+
 			var shouldLoop = false;
 			shouldLoop = ShouldLoop;
 			//QueryInterface(__uuidof(UIElement), &spThisAsUI);
