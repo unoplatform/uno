cbf7694c16740031946486839e451f24a8183a31
Martin Zikmund <martin@zikmund.dev>
2024-01-11 16:38:33 +0100

    test: Looping selector does not change selected item unexpectedly

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_TimePicker.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_TimePicker.cs
@@ -9,6 +9,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Private.Infrastructure;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Data;
+using Uno.UI.Xaml;
+using Uno.UI.RuntimeTests.MUX.Helpers;
 
 namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 {
@@ -32,6 +34,29 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 			TestServices.WindowHelper.WindowContent = root;
 
 			await TestServices.WindowHelper.WaitForIdle();
+		}
+
+		[TestMethod]
+		[RunsOnUIThread]
+		public async Task When_PM_Opened_And_Closed()
+		{
+			// This tests whether the looping selector does not unexpectedly
+			// change the time when the flyout is opened and closed.
+			var timePicker = new TimePicker();
+			var expectedTime = new TimeSpan(15, 0, 0);
+			timePicker.Time = expectedTime;
+			timePicker.SelectedTime = expectedTime;
+			TestServices.WindowHelper.WindowContent = timePicker;
+			await TestServices.WindowHelper.WaitForLoaded(timePicker);
+			await DateTimePickerHelper.OpenDateTimePicker(timePicker);
+			await TestServices.WindowHelper.WaitForIdle();
+
+			// Confirm the "same time"
+			await ControlHelper.ClickFlyoutCloseButton(timePicker, true /* isAccept */);
+			await TestServices.WindowHelper.WaitForIdle();
+
+			Assert.AreEqual(expectedTime, timePicker.SelectedTime);
+			Assert.AreEqual(expectedTime, timePicker.Time);
 		}
 	}
 
