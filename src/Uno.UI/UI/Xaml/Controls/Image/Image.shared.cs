087c102109462ccc1c42e3e3463c0b7a6472b469
morning4coffe-dev <78549750+morning4coffe-dev@users.noreply.github.com>
2024-01-12 17:01:01 +0100

    chore: Mark Description as new

--- src/Uno.UI/UI/Xaml/Controls/Image/Image.shared.cs
+++ src/Uno.UI/UI/Xaml/Controls/Image/Image.shared.cs
@@ -30,7 +30,7 @@ partial class Image
 	/// <summary>
 	/// Returns an Empty string as the Description for the Image.
 	/// </summary>
-	internal string Description
+	internal new string Description
 	{
 		// UNO TODO: Description on Image is not implemented
 		get => string.Empty;
