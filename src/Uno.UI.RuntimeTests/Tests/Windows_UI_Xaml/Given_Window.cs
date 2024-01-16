f25da32e79160658430a60cc254db25574ba3406
Martin Zikmund <martin@zikmund.dev>
2024-01-16 13:56:19 +0100

    chore: Adjust tests to capture non-multiwindow targets

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_Window.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_Window.cs
@@ -4,6 +4,11 @@ using System.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Windows.ApplicationModel.Core;
+
+#if !__SKIA__
+using FluentAssertions;
+#endif
+
 #if !WINDOWS_UWP && !WINAPPSDK
 using Uno.UI.Xaml;
 #endif
@@ -13,7 +18,7 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml
 	[TestClass]
 	public class Given_Window
 	{
-#if !WINAPPSDK
+#if !WINAPPSDK && !__SKIA__
 		[TestMethod]
 		[RunsOnUIThread]
 		public void When_CreateNewWindow()
@@ -24,9 +29,32 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml
 				return;
 			}
 
-			// This used to crash on wasm which was trying to create a second D&D extension
-			var sut = new Window(WindowType.DesktopXamlSource);
-			sut.Close();
+			var act = () => new Window(WindowType.DesktopXamlSource);
+			act.Should().Throw<InvalidOperationException>();
+		}
+#endif
+
+#if __SKIA__
+		[TestMethod]
+		[RunsOnUIThread]
+		public void When_Create_Multiple_Windows()
+		{
+			if (!CoreApplication.IsFullFledgedApp)
+			{
+				Assert.Inconclusive("This test can only be run in a full-fledged app");
+				return;
+			}
+
+			var startingNumberOfWindows = ApplicationHelper.Windows.Count;
+
+			for (int i = 0; i < 10; i++)
+			{
+				var sut = new Window(WindowType.DesktopXamlSource);
+				sut.Close();
+			}
+
+			var endNumberOfWindows = ApplicationHelper.Windows.Count;
+			Assert.AreEqual(startingNumberOfWindows, endNumberOfWindows)
 		}
 #endif
 	}
