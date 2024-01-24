5216c06485fbfaa74b4090e428a941bbb8187813
Andres Pineda <pineda.andres@gmail.com>
2024-01-23 23:51:05 -0500

    test: (DropDownButton) add manual test

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/DropDownButton/DropDownButton_ListView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.DropDownButton;
+
+[SampleControlInfo("Buttons", "DropDownButton_ListView", isManualTest: true, description: "This sample demonstrates the DropDownButton with a ListView")]
+public sealed partial class DropDownButton_ListView : UserControl
+{
+	public DropDownButton_ListView()
+	{
+		this.InitializeComponent();
+	}
+}
