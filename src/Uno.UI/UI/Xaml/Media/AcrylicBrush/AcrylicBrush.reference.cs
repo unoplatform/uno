cfe61920bd0a0b1f68b13751e9127d86e051aa94
ahmed walid <ahmed.walid605@yahoo.com>
2024-01-25 18:59:45 +0200

    chore(composition): fix package build

---
+++ src/Uno.UI/UI/Xaml/Media/AcrylicBrush/AcrylicBrush.reference.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Uno.Disposables;
+using Uno.UI.Xaml;
+
+namespace Microsoft.UI.Xaml.Media;
+
+public partial class AcrylicBrush
+{
+	protected override void OnConnected()
+	{
+		base.OnConnected();
+	}
+
+	protected override void OnDisconnected()
+	{
+		base.OnDisconnected();
+	}
+}
--- src/Uno.UWP/Generated/3.0.0.0/Windows.UI.Composition/CompositionEffectBrush.cs
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.UI.Composition/CompositionEffectBrush.cs
@@ -3,12 +3,29 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.UI.Composition
 {
-#if __ANDROID__ || __IOS__ || IS_UNIT_TESTS || __WASM__ || false || __NETSTD_REFERENCE__ || __MACOS__
+#if __ANDROID__ || __IOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
 	[global::Uno.NotImplemented]
 #endif
 	public partial class CompositionEffectBrush : global::Windows.UI.Composition.CompositionBrush
 	{
-		// Skipping already declared method Windows.UI.Composition.CompositionEffectBrush.GetSourceParameter(string)
-		// Skipping already declared method Windows.UI.Composition.CompositionEffectBrush.SetSourceParameter(string, Windows.UI.Composition.CompositionBrush)
+#if __ANDROID__ || __IOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+		internal CompositionEffectBrush()
+		{
+		}
+#endif
+#if __ANDROID__ || __IOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
+		public global::Windows.UI.Composition.CompositionBrush GetSourceParameter(string name)
+		{
+			throw new global::System.NotImplementedException("The member CompositionBrush CompositionEffectBrush.GetSourceParameter(string name) is not implemented. For more information, visit https://aka.platform.uno/notimplemented#m=CompositionBrush%20CompositionEffectBrush.GetSourceParameter%28string%20name%29");
+		}
+#endif
+#if __ANDROID__ || __IOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
+		public void SetSourceParameter(string name, global::Windows.UI.Composition.CompositionBrush source)
+		{
+			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.UI.Composition.CompositionEffectBrush", "void CompositionEffectBrush.SetSourceParameter(string name, CompositionBrush source)");
+		}
+#endif
 	}
 }
