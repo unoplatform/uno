eda746816ae50cf942a3cfc83ddd89db28f66302
Youssef Victor <youssefvictor00@gmail.com>
2024-01-29 20:06:34 +0200

    fix: Fix TextBlock.Foreground LinearGradientBrush not taking exact text width

--- src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/TextBlockTests/TextBlockTests.cs
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/TextBlockTests/TextBlockTests.cs
@@ -572,10 +572,21 @@ namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.TextBlockTests
 			var rect3 = _app.GetPhysicalRect("test3");
 			var rect4 = _app.GetPhysicalRect("test4");
 			using var screenshot = TakeScreenshot(nameof(When_Foreground_Is_Brush));
-			ImageAssert.HasColorAt(screenshot, rect1.X + 5, rect1.CenterY, Color.Blue, tolerance: 10);
-			ImageAssert.HasColorAt(screenshot, rect2.X + 5, rect2.CenterY, Color.Blue, tolerance: 10);
-			ImageAssert.HasColorAt(screenshot, rect3.X + 5, rect3.CenterY, Color.Blue, tolerance: 10);
-			ImageAssert.HasColorAt(screenshot, rect4.X + 5, rect4.CenterY, Color.Blue, tolerance: 10);
+			ImageAssert.HasColorAt(screenshot, rect1.X + 5, rect1.CenterY, Color.Blue, tolerance: 30);
+			ImageAssert.HasColorAt(screenshot, rect2.X + 5, rect2.CenterY, Color.Blue, tolerance: 30);
+			ImageAssert.HasColorAt(screenshot, rect3.X + 5, rect3.CenterY, Color.Blue, tolerance: 30);
+			ImageAssert.HasColorAt(screenshot, rect4.X + 5, rect4.CenterY, Color.Blue, tolerance: 30);
+		}
+
+		[Test]
+		[AutoRetry]
+		[ActivePlatforms(Platform.Android, Platform.Browser)]
+		public void When_Foreground_Is_Brush_Should_Take_Correct_Width()
+		{
+			Run("UITests.Windows_UI_Xaml_Media.GradientBrushTests.LinearGradientBrush_Width");
+			var rect = _app.GetPhysicalRect("TextBlockShouldContainRed");
+			using var screenshot = TakeScreenshot(nameof(When_Foreground_Is_Brush_Should_Take_Correct_Width));
+			ImageAssert.HasColorInRectangle(screenshot, rect.ToRectangle(), Color.Red, tolerance: 30);
 		}
 	}
 }
