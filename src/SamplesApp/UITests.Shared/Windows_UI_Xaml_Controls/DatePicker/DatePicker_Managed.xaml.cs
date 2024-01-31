d82ae9ad580f525e535a768f1e1a61793157ab29
Martin Zikmund <martin@zikmund.dev>
2024-01-31 14:13:03 +0100

    test: Managed time and date picker samples

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/DatePicker/DatePicker_Managed.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Uno.UI.Samples.Controls;
+using Microsoft.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.DatePicker
+{
+	[Sample("Pickers", IgnoreInSnapshotTests = true)]
+	public sealed partial class DatePicker_Managed : Page
+	{
+		public DatePicker_Managed()
+		{
+			this.InitializeComponent();
+#if HAS_UNO
+			SampleDatePicker.UseNativeStyle = false;
+#endif
+		}
+	}
+}
---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/TimePicker/TimePicker_Managed.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Uno.UI.Samples.Controls;
+using Microsoft.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.TimePicker
+{
+	[Sample("Pickers", IgnoreInSnapshotTests = true)]
+	public sealed partial class TimePicker_Managed : Page
+	{
+		public TimePicker_Managed()
+		{
+			this.InitializeComponent();
+#if HAS_UNO
+			SampleTimePicker.UseNativeStyle = false;
+#endif
+		}
+	}
+}
