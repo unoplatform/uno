4d83ce6a339c452f0b689979b7f00e42090d4db3
Youssef Victor <youssefvictor00@gmail.com>
2024-01-31 21:30:36 +0200

    chore: Adjust from WinUI code

--- src/Uno.UI/UI/Xaml/Style/Style.cs
+++ src/Uno.UI/UI/Xaml/Style/Style.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Data;
 using Uno.Foundation.Logging;
 using Uno.UI;
@@ -64,6 +66,51 @@ namespace Microsoft.UI.Xaml
 			BasedOn?.Seal();
 		}
 
+		private bool TryGetAdjustedSetter(DependencyPropertyValuePrecedences baseValueSource, DependencyObject dependencyObject, SetterBase originalSetter, [NotNullWhen(true)] out SetterBase? adjustedSetter)
+		{
+			if (originalSetter is not Setter { Property: { } property })
+			{
+				adjustedSetter = null;
+				return false;
+			}
+
+			// Replicates CDependencyObject::InvalidateProperty from WinUI
+			if (property == Control.TemplateProperty)
+			{
+				var oldBaseValueSource = dependencyObject.GetBaseValueSource(property);
+				// Uno docs: In WinUI code, this condition is `baseValueSource < oldBaseValueSource`
+				// In Uno, we use >= instead because our precedence enum is opposite order of WinUI's one.
+				if (baseValueSource >= oldBaseValueSource)
+				{
+					adjustedSetter = null;
+					return false;
+				}
+			}
+
+			// On WinUI, when default style is applied and there is an explicit
+			// style that contains a setter for the same DP, the value from that explicit style is used.
+			// Note that having two different precedences isn't sufficient to handle this case.
+			// The setter application could be throwing an exception, and in this case we don't
+			// want the value from the default style to take effect.
+			// This bit of code isn't ported from WinUI, but is the equivalent of the following call chain:
+			// OnStyleChanged -> InvalidateProperty -> UpdateEffectiveValue -> EvaluateEffectiveValue -> EvaluateBaseValue -> GetValueFromSetter
+			// In DependencyObject::EvaluateBaseValue (DependencyObject.cpp file), the value is updated to that returned from GetValueFromStyle
+			// Then, baseValueSource is updated from BaseValueSourceBuiltInStyle to BaseValueSourceStyle
+			// The OverrideLocalPrecedence call below is the equivalent of the baseValueSource update.
+			if (baseValueSource == DependencyPropertyValuePrecedences.ImplicitStyle &&
+				dependencyObject is FrameworkElement fe &&
+				fe.GetActiveStyle() is { } activeStyle &&
+				activeStyle != this &&
+				activeStyle.EnsureSetterMap().TryGetValue(property, out var setter))
+			{
+				adjustedSetter = setter;
+				return true;
+			}
+
+			adjustedSetter = null;
+			return false;
+		}
+
 		internal void ApplyTo(DependencyObject o, DependencyPropertyValuePrecedences precedence)
 		{
 			if (o == null)
@@ -89,26 +136,11 @@ namespace Microsoft.UI.Xaml
 					{
 						try
 						{
-							// On WinUI, when default style is applied and there is an explicit
-							// style that contains a setter for the same DP, the value from that explicit style is used.
-							// Note that having two different precedences isn't sufficient to handle this case.
-							// The setter application could be throwing an exception, and in this case we don't
-							// want the value from the default style to take effect.
-							// This bit of code isn't ported from WinUI, but is the equivalent of the following call chain:
-							// OnStyleChanged -> InvalidateProperty -> UpdateEffectiveValue -> EvaluateEffectiveValue -> EvaluateBaseValue -> GetValueFromSetter
-							// In DependencyObject::EvaluateBaseValue (DependencyObject.cpp file), the value is updated to that returned from GetValueFromStyle
-							// Then, baseValueSource is updated from BaseValueSourceBuiltInStyle to BaseValueSourceStyle
-							// The OverrideLocalPrecedence call below is the equivalent of the baseValueSource update.
-							if (precedence == DependencyPropertyValuePrecedences.ImplicitStyle &&
-								o is FrameworkElement fe &&
-								fe.GetActiveStyle() is { } activeStyle &&
-								activeStyle != this &&
-								_flattenedSetters[i] is Setter { Property: { } property } &&
-								activeStyle.EnsureSetterMap().TryGetValue(property, out var setter))
+							if (TryGetAdjustedSetter(precedence, o, _flattenedSetters[i], out var adjustedSetter))
 							{
 								using (o.OverrideLocalPrecedence(DependencyPropertyValuePrecedences.ExplicitStyle))
 								{
-									setter.ApplyTo(o);
+									adjustedSetter.ApplyTo(o);
 								}
 							}
 							else
