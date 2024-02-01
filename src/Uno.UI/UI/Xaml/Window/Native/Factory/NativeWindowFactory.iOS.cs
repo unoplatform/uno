cfcbe1b141f232b91213cdd65036b215115c6b1c
Martin Zikmund <martin@zikmund.dev>
2024-02-01 18:19:43 +0100

    DRAFT

---
+++ src/Uno.UI/UI/Xaml/Window/Native/Factory/NativeWindowFactory.iOS.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+
+namespace Uno.UI.Xaml.Controls;
+
+partial class NativeWindowFactory
+{
+	public static bool SupportsMultipleWindows => true;
+
+	private static INativeWindowWrapper? CreateWindowPlatform(Microsoft.UI.Xaml.Window window, XamlRoot xamlRoot)
+	{
+
+	}
+}
