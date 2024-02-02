3887af4454da613c7253d3c418ac7e0abcc9ea1c
Martin Zikmund <martin@zikmund.dev>
2024-02-02 16:35:33 +0100

    refactor: Move border-related properties into a IBorderInfoProvider

---
+++ src/Uno.UI/UI/Xaml/Controls/CalendarView/CalendarViewBaseItemChrome.IBorderInfoProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Media;
+using Uno.UI.Xaml.Controls;
+
+namespace Microsoft.UI.Xaml.Controls;
+
+partial class CalendarViewBaseItem : IBorderInfoProvider
+{
+	Brush IBorderInfoProvider.Background
+	{
+		get
+		{
+			var background = Background;
+
+			if (IsClear(background))
+			{
+				if (FindTodaySelectedBackgroundBrush() is { } todaySelectedBackground
+					&& !IsClear(todaySelectedBackground))
+				{
+					background = todaySelectedBackground;
+				}
+				else if (FindSelectedBackgroundBrush() is { } selectedBackground
+					&& !IsClear(selectedBackground))
+				{
+					background = selectedBackground;
+				}
+				else
+				{
+					background = GetItemBackgroundBrush();
+				}
+			}
+			return background;
+		}
+	}
+
+	BackgroundSizing IBorderInfoProvider.BackgroundSizing => BackgroundSizing.InnerBorderEdge;
+
+	Brush IBorderInfoProvider.BorderBrush
+	{
+		get
+		{
+			var borderBrush = GetItemBorderBrush(forFocus: false);
+			if (m_isToday && m_isSelected && GetItemInnerBorderBrush() is { } selectedBrush)
+			{
+				// We don't support inner border yet, so even if not optimal we just use it as border.
+				borderBrush = selectedBrush;
+			}
+			return borderBrush;
+		}
+	}
+
+
+	Thickness IBorderInfoProvider.BorderThickness => GetItemBorderThickness();
+
+	CornerRadius IBorderInfoProvider.CornerRadius => GetItemCornerRadius();
+}
