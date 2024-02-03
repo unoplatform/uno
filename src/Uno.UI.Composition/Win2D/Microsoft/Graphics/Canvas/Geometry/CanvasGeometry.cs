61b3c4e902387a3b2d274e0644557a82465e7baf
ahmed walid <ahmed.walid605@yahoo.com>
2024-02-03 04:16:08 +0200

    feat(composition): Implement more CanvasGeometry methods + more tests

--- src/Uno.UI.Composition/Win2D/Microsoft/Graphics/Canvas/Geometry/CanvasGeometry.cs
+++ src/Uno.UI.Composition/Win2D/Microsoft/Graphics/Canvas/Geometry/CanvasGeometry.cs
@@ -3,7 +3,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using Uno.UI.Composition;
+using Windows.Foundation;
 using Windows.Graphics;
 using Windows.Graphics.Interop;
 using Windows.Graphics.Interop.Direct2D;
@@ -23,7 +25,42 @@ internal class CanvasGeometry : IDisposable, IGeometrySource2D, IGeometrySource2
 	public void Dispose() { }
 
 
+	public static CanvasGeometry CreateEllipse(ICanvasResourceCreator resourceCreator, Vector2 centerPoint, float radiusX, float radiusY) => new(new CanvasEllipseGeometry(new() { Point = centerPoint.ToPoint(), RadiusX = radiusX, RadiusY = radiusY }));
+
+	public static CanvasGeometry CreateEllipse(ICanvasResourceCreator resourceCreator, float x, float y, float radiusX, float radiusY) => new(new CanvasEllipseGeometry(new() { Point = new(x, y), RadiusX = radiusX, RadiusY = radiusY }));
+
+	public static CanvasGeometry CreateCircle(ICanvasResourceCreator resourceCreator, Vector2 centerPoint, float radius) => CreateEllipse(resourceCreator, centerPoint, radius, radius);
+
+	public static CanvasGeometry CreateCircle(ICanvasResourceCreator resourceCreator, float x, float y, float radius) => CreateEllipse(resourceCreator, x, y, radius, radius);
+
 	public static CanvasGeometry CreatePath(CanvasPathBuilder pathBuilder) => new(new CanvasPathGeometry(pathBuilder.Commands));
+
+	public static CanvasGeometry CreatePolygon(ICanvasResourceCreator resourceCreator, Vector2[] points)
+	{
+		CanvasPathBuilder pathBuilder = new(resourceCreator);
+
+		if (points.Length > 0)
+		{
+			pathBuilder.BeginFigure(points[0], CanvasFigureFill.Default);
+
+			foreach (var point in points)
+			{
+				pathBuilder.AddLine(point);
+			}
+
+			pathBuilder.EndFigure(CanvasFigureLoop.Closed);
+		}
+
+		return CreatePath(pathBuilder);
+	}
+
+	public static CanvasGeometry CreateRectangle(ICanvasResourceCreator resourceCreator, Rect rect) => new(new CanvasRectangleGeometry(rect));
+
+	public static CanvasGeometry CreateRectangle(ICanvasResourceCreator resourceCreator, float x, float y, float w, float h) => new(new CanvasRectangleGeometry(new(x, y, w, h)));
+
+	public static CanvasGeometry CreateRoundedRectangle(ICanvasResourceCreator resourceCreator, Rect rect, float radiusX, float radiusY) => new(new CanvasRoundedRectangleGeometry(new() { Rect = rect, RadiusX = radiusX, RadiusY = radiusY }));
+
+	public static CanvasGeometry CreateRoundedRectangle(ICanvasResourceCreator resourceCreator, float x, float y, float w, float h, float radiusX, float radiusY) => new(new CanvasRoundedRectangleGeometry(new() { Rect = new(x, y, w, h), RadiusX = radiusX, RadiusY = radiusY }));
 
 	private class CanvasPathGeometry : ID2D1PathGeometry, ICompositionPathCommandsProvider
 	{
@@ -46,5 +83,32 @@ internal class CanvasGeometry : IDisposable, IGeometrySource2D, IGeometrySource2
 		public ID2D1GeometrySink Open() => throw new NotImplementedException();
 
 		public void Stream(ID2D1GeometrySink geometrySink) => throw new NotImplementedException(); // TODO
+	}
+
+	private class CanvasRectangleGeometry : ID2D1RectangleGeometry
+	{
+		private Rect _rect;
+
+		public CanvasRectangleGeometry(Rect rect) => _rect = rect;
+
+		public Rect GetRect() => _rect;
+	}
+
+	private class CanvasRoundedRectangleGeometry : ID2D1RoundedRectangleGeometry
+	{
+		private D2D1RoundedRect _rect;
+
+		public CanvasRoundedRectangleGeometry(D2D1RoundedRect rect) => _rect = rect;
+
+		public D2D1RoundedRect GetRoundedRect() => _rect;
+	}
+
+	private class CanvasEllipseGeometry : ID2D1EllipseGeometry
+	{
+		private D2D1Ellipse _ellipse;
+
+		public CanvasEllipseGeometry(D2D1Ellipse ellipse) => _ellipse = ellipse;
+
+		public D2D1Ellipse GetEllipse() => _ellipse;
 	}
 }
