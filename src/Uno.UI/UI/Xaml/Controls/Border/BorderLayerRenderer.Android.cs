93f0709f5b33741dfa3f0645aa546e08a1451d92
Martin Zikmund <martin@zikmund.dev>
2024-02-05 15:55:33 +0100

    chore: Android adjustments to namespace change

--- src/Uno.UI/UI/Xaml/Controls/Border/BorderLayerRenderer.Android.cs
+++ src/Uno.UI/UI/Xaml/Controls/Border/BorderLayerRenderer.Android.cs
@@ -17,6 +17,8 @@ using Windows.UI.Core;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
 using Rect = Windows.Foundation.Rect;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 namespace Uno.UI.Xaml.Controls
 {
