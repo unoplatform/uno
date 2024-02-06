390e110096a4de29f6a2c5d5dc5d22aafa1ab114
David <github.site@torick.net>
2024-02-06 11:43:18 -0500

    chore: IDE0055

--- src/Uno.UI/UI/Xaml/Controls/ScrollViewer/ScrollViewer.HotReload.cs
+++ src/Uno.UI/UI/Xaml/Controls/ScrollViewer/ScrollViewer.HotReload.cs
@@ -41,7 +41,7 @@ internal static partial class ScrollViewerMetadataUpdateHandler
 
 			sv.ChangeView((double?)hOffset, (double?)vOffset, sv.ZoomFactor, true);
 		}
-		
+
 		return Task.CompletedTask;
 	}
 }
