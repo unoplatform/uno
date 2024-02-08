966d1ae13e8035c206c3391c085e110455289d34
Ramez Ragaa <ramezragaa@proton.me>
2024-02-08 15:07:20 +0000

    chore: move PickerHelpers and add a Downloads folder

--- src/Uno.UI.Runtime.Skia.Gtk/Extensions/Storage/Pickers/FileSavePickerExtension.cs
+++ src/Uno.UI.Runtime.Skia.Gtk/Extensions/Storage/Pickers/FileSavePickerExtension.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Uno.UI.Runtime.Skia.Gtk;
 using Windows.Storage;
 using Windows.Storage.Pickers;
+using Uno.UI.Helpers;
 
 namespace Uno.Extensions.Storage.Pickers
 {
