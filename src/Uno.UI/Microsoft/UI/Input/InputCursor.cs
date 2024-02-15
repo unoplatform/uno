21db21e2ab6ee17fd95c2ec2bc6c51172ef252ab
Ramez Ragaa <ramezragaa@proton.me>
2024-02-15 14:56:07 +0200

    chore: handle disposing InputCursor

--- src/Uno.UI/Microsoft/UI/Input/InputCursor.cs
+++ src/Uno.UI/Microsoft/UI/Input/InputCursor.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using Windows.UI.Core;
 
 namespace Microsoft.UI.Input;
@@ -9,6 +11,9 @@ public partial class InputCursor
 internal partial class InputCursor
 #endif
 {
+
+	private List<WeakEventHelper.GenericEventHandler> _disposedHandlers = new List<WeakEventHelper.GenericEventHandler>();
+
 	internal bool IsDisposed { get; private set; }
 
 	protected InputCursor()
@@ -17,7 +22,21 @@ internal partial class InputCursor
 
 	public static InputCursor CreateFromCoreCursor(CoreCursor cursor) => InputSystemCursor.Create(cursor.Type.ToInputSystemCursorShape());
 
-	public void Dispose() => IsDisposed = true;
+	public void Dispose()
+	{
+		if (!IsDisposed)
+		{
+			IsDisposed = true;
+			var handlers = new List<WeakEventHelper.GenericEventHandler>(_disposedHandlers);
+			foreach (var action in handlers)
+			{
+				action(this, null);
+			}
+		}
+	}
+
+	internal IDisposable RegisterDisposedEvent(EventHandler handler)
+		=> WeakEventHelper.RegisterEvent(_disposedHandlers, handler, (h, s, a) => (h as EventHandler)?.Invoke(s, (EventArgs)a));
 
 	// This assembly is not visible to CoreCursor's assembly, so we put the method here.
 	internal static CoreCursor CreateCoreCursorFromInputSystemCursorShape(InputSystemCursorShape shape) => new CoreCursor(shape.ToCoreCursorType(), 0);
