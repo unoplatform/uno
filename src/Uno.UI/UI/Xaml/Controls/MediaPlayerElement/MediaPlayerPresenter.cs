354f503189fd3d68978ace333b45889ffb7c9ca4
Andres Pineda <pineda.andres@gmail.com>
2024-02-15 18:14:25 -0500

    chore: make fix only available for iOS

--- src/Uno.UI/UI/Xaml/Controls/MediaPlayerElement/MediaPlayerPresenter.cs
+++ src/Uno.UI/UI/Xaml/Controls/MediaPlayerElement/MediaPlayerPresenter.cs
@@ -35,10 +35,14 @@ namespace Microsoft.UI.Xaml.Controls
 			}
 			else
 			{
+#if __IOS__
 				// There are situations where between measurements scaledDimension and naturalDimension
 				// have a small difference in value (a few pixels) causing the measurement to go into an infinite loop.
 				// Related to: https://github.com/unoplatform/uno/issues/15254
 				var ratio = (float)Math.Round(scaledOneDimension / naturalOneDimension, 1);
+#else
+				var ratio = scaledOneDimension / naturalOneDimension;
+#endif
 				return naturalOtherDimension * ratio;
 			}
 		}
