46e61fbb706da5573fe461fd76a37333a2a25966
Youssef Victor <youssefvictor00@gmail.com>
2024-02-16 10:15:54 +0200

    chore: Simplify PointerPoint

--- src/Uno.UI/UI/Xaml/Internal/InputManager.Pointers.Managed.cs
+++ src/Uno.UI/UI/Xaml/Internal/InputManager.Pointers.Managed.cs
@@ -252,9 +252,7 @@ internal partial class InputManager
 #if HAS_UNO_WINUI
 				recognizer.ProcessDownEvent(new PointerPoint(args.CurrentPoint));
 #else
-				var pointer = args.CurrentPoint;
-				recognizer.ProcessDownEvent(new PointerPoint(
-					pointer.FrameId, pointer.Timestamp, pointer.PointerDevice, pointer.PointerId, pointer.RawPosition, pointer.Position, pointer.IsInContact, new PointerPointProperties(pointer.Properties)));
+				recognizer.ProcessDownEvent(args.CurrentPoint);
 #endif
 				return;
 			}
@@ -296,9 +294,7 @@ internal partial class InputManager
 #if HAS_UNO_WINUI
 				recognizer.ProcessUpEvent(new PointerPoint(args.CurrentPoint));
 #else
-				var pointer = args.CurrentPoint;
-				recognizer.ProcessUpEvent(new PointerPoint(
-					pointer.FrameId, pointer.Timestamp, pointer.PointerDevice, pointer.PointerId, pointer.RawPosition, pointer.Position, pointer.IsInContact, new PointerPointProperties(pointer.Properties)));
+				recognizer.ProcessUpEvent(args.CurrentPoint);
 #endif
 				return;
 			}
@@ -348,9 +344,7 @@ internal partial class InputManager
 #if HAS_UNO_WINUI
 				recognizer.ProcessMoveEvents([new PointerPoint(args.CurrentPoint)]);
 #else
-				var pointer = args.CurrentPoint;
-				recognizer.ProcessMoveEvents([new PointerPoint(
-					pointer.FrameId, pointer.Timestamp, pointer.PointerDevice, pointer.PointerId, pointer.RawPosition, pointer.Position, pointer.IsInContact, new PointerPointProperties(pointer.Properties))]);
+				recognizer.ProcessMoveEvents([args.CurrentPoint]);
 #endif
 				return;
 			}
