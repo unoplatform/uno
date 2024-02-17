e81e8bcfcde89bb388f03a60692610e88a8972af
Youssef Victor <youssefvictor00@gmail.com>
2024-02-17 21:58:42 +0200

    fix(android): Fix NullReferenceException in ProgressBar for custom templates
    (cherry picked from commit fe561607ee0437a5e187f7803da5976632ab2514)
    
    # Conflicts:
    #	src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_ProgressBar.cs

--- src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_ProgressBar.cs
+++ src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_ProgressBar.cs
@@ -6,17 +6,21 @@ using System.Threading.Tasks;
 using Common;
 using MUXControlsTestApp.Utilities;
 using Private.Infrastructure;
+<<<<<<< HEAD:src/Uno.UI.RuntimeTests/MUX/Microsoft_UI_Xaml_Controls/ProgressBar/ProgressBarTest.cs
 using ProgressBar = Microsoft.UI.Xaml.Controls.ProgressBar;
+=======
+using Uno.UI.RuntimeTests.Helpers;
+using ProgressBar = Microsoft/* UWP don't rename */.UI.Xaml.Controls.ProgressBar;
+>>>>>>> fe561607ee (fix(android): Fix NullReferenceException in ProgressBar for custom templates):src/Uno.UI.RuntimeTests/Tests/Microsoft_UI_Xaml_Controls/Given_ProgressBar.cs
 
-namespace Uno.UI.RuntimeTests.MUX.Microsoft_UI_Xaml_Controls.ProgressBar;
+namespace Uno.UI.RuntimeTests.Tests.Microsoft_UI_Xaml_Controls;
 
 using ProgressBar = Microsoft.UI.Xaml.Controls.ProgressBar;
 
 [TestClass]
-public class ProgressBarTest
+[RunsOnUIThread]
+public class Given_ProgressBar
 {
-
-	[RunsOnUIThread]
 	[TestMethod]
 	public async Task ProgressBarLayoutUpdate()
 	{
@@ -44,5 +48,19 @@ public class ProgressBarTest
 		await Task.Delay(1000);
 
 		Verify.IsLessThanOrEqual(layoutCount, 4);
+	}
+
+	[TestMethod]
+	public async Task When_CustomTemplate()
+	{
+		var sut = new ProgressBar();
+		var sizeChangedRaised = false;
+		sut.SizeChanged += (_, _) => sizeChangedRaised = true;
+		sut.Template = new Microsoft.UI.Xaml.Controls.ControlTemplate();
+		sut.Width = 50;
+		sut.Height = 50;
+		TestServices.WindowHelper.WindowContent = sut;
+
+		await TestServices.WindowHelper.WaitFor(() => sizeChangedRaised);
 	}
 }
