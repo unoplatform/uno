59840f6445893df59e9720a99459a2ca887bccc9
Ramez Ragaa <ramezragaa@proton.me>
2024-02-26 17:15:37 +0200

    chore: make SplitViewTemplateSettings properties DependencyProperties

--- src/Uno.UI/UI/Xaml/Controls/SplitView/SplitViewTemplateSettings.cs
+++ src/Uno.UI/UI/Xaml/Controls/SplitView/SplitViewTemplateSettings.cs
@@ -12,13 +12,104 @@ namespace Microsoft.UI.Xaml.Controls.Primitives
 		{
 		}
 
-		public GridLength CompactPaneGridLength { get; internal set; }
-		public GridLength OpenPaneGridLength { get; internal set; }
-		public double NegativeOpenPaneLength { get; internal set; }
-		public double NegativeOpenPaneLengthMinusCompactLength { get; internal set; }
-		public double OpenPaneLengthMinusCompactLength { get; internal set; }
+		public static DependencyProperty CompactPaneGridLengthProperty { get; } =
+			DependencyProperty.Register(
+				nameof(CompactPaneGridLength),
+				typeof(GridLength),
+				typeof(SplitViewTemplateSettings),
+				new FrameworkPropertyMetadata(default(GridLength))
+			);
 
-		public double OpenPaneLength { get; internal set; }
-		public double CompactPaneLength { get; internal set; }
+		public GridLength CompactPaneGridLength
+		{
+			get => (GridLength)GetValue(CompactPaneGridLengthProperty);
+			internal set => SetValue(CompactPaneGridLengthProperty, value);
+		}
+
+		public static DependencyProperty OpenPaneGridLengthProperty { get; } =
+			DependencyProperty.Register(
+				nameof(OpenPaneGridLength),
+				typeof(GridLength),
+				typeof(SplitViewTemplateSettings),
+				new FrameworkPropertyMetadata(default(GridLength), propertyChangedCallback: (dependencyObject, args) =>
+				{
+
+				}));
+
+		public GridLength OpenPaneGridLength
+		{
+			get => (GridLength)GetValue(OpenPaneGridLengthProperty);
+			internal set => SetValue(OpenPaneGridLengthProperty, value);
+		}
+
+		public static DependencyProperty NegativeOpenPaneLengthProperty { get; } =
+			DependencyProperty.Register(
+				nameof(NegativeOpenPaneLength),
+				typeof(double),
+				typeof(SplitViewTemplateSettings),
+				new FrameworkPropertyMetadata(default(GridLength))
+			);
+
+		public double NegativeOpenPaneLength
+		{
+			get => (double)GetValue(NegativeOpenPaneLengthProperty);
+			internal set => SetValue(NegativeOpenPaneLengthProperty, value);
+		}
+
+		public static DependencyProperty NegativeOpenPaneLengthMinusCompactLengthProperty { get; } =
+			DependencyProperty.Register(
+				nameof(NegativeOpenPaneLengthMinusCompactLength),
+				typeof(double),
+				typeof(SplitViewTemplateSettings),
+				new FrameworkPropertyMetadata(default(GridLength))
+			);
+
+		public double NegativeOpenPaneLengthMinusCompactLength
+		{
+			get => (double)GetValue(NegativeOpenPaneLengthMinusCompactLengthProperty);
+			internal set => SetValue(NegativeOpenPaneLengthMinusCompactLengthProperty, value);
+		}
+
+		public static DependencyProperty OpenPaneLengthMinusCompactLengthProperty { get; } =
+			DependencyProperty.Register(
+				nameof(OpenPaneLengthMinusCompactLength),
+				typeof(double),
+				typeof(SplitViewTemplateSettings),
+				new FrameworkPropertyMetadata(default(GridLength))
+			);
+
+		public double OpenPaneLengthMinusCompactLength
+		{
+			get => (double)GetValue(OpenPaneLengthMinusCompactLengthProperty);
+			internal set => SetValue(OpenPaneLengthMinusCompactLengthProperty, value);
+		}
+
+		public static DependencyProperty OpenPaneLengthProperty { get; } =
+			DependencyProperty.Register(
+				nameof(OpenPaneLength),
+				typeof(double),
+				typeof(SplitViewTemplateSettings),
+				new FrameworkPropertyMetadata(default(GridLength))
+			);
+
+		public double OpenPaneLength
+		{
+			get => (double)GetValue(OpenPaneLengthProperty);
+			internal set => SetValue(OpenPaneLengthProperty, value);
+		}
+
+		public static DependencyProperty CompactPaneLengthProperty { get; } =
+			DependencyProperty.Register(
+				nameof(CompactPaneLength),
+				typeof(double),
+				typeof(SplitViewTemplateSettings),
+				new FrameworkPropertyMetadata(default(GridLength))
+			);
+
+		public double CompactPaneLength
+		{
+			get => (double)GetValue(CompactPaneLengthProperty);
+			internal set => SetValue(CompactPaneLengthProperty, value);
+		}
 	}
 }
