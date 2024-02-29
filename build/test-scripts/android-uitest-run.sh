b423fe4b71c6a0fd62df49dc3110f9a4a878cb3c
jay <jerome@platform.uno>
2024-02-28 23:09:05 -0500

    chore: More logging

--- build/test-scripts/android-uitest-run.sh
+++ build/test-scripts/android-uitest-run.sh
@@ -168,7 +168,7 @@ then
 
 	echo "Waiting for $UITEST_RUNTIME_AUTOSTART_RESULT_FILE to be available..."
 
-	while [[ ! $(adb shell test -e "$UITEST_RUNTIME_AUTOSTART_RESULT_FILE" > /dev/null) && $SECONDS -lt $END_TIME ]]; do
+	while [[ ! $(adb shell test -e "$UITEST_RUNTIME_AUTOSTART_RESULT_FILE") && $SECONDS -lt $END_TIME ]]; do
 		echo "Waiting $INTERVAL seconds for test results to be written to $UITEST_RUNTIME_AUTOSTART_RESULT_FILE";
 		sleep $INTERVAL
 
