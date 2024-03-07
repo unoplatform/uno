b2c6adf416d8b6f1715f3bd6783b870dcb639ca7
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2024-03-07 20:41:28 +0100

    fix: NRE on `PickImageFromFile` cancellation

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Media_Imaging/PickImageFromFile.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Media_Imaging/PickImageFromFile.xaml.cs
@@ -5,27 +5,72 @@ using Windows.Storage.Pickers;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Imaging;
+using Uno.UI.Samples.UITests.Helpers;
+using Microsoft.UI.Xaml.Media;
 
 namespace UITests.Windows_UI_Xaml_Media_Imaging;
 
-[Sample("Microsoft.UI.Xaml.Media", IsManualTest = true)]
 public sealed partial class PickImageFromFile : Page
 {
 	public PickImageFromFile()
 	{
 		this.InitializeComponent();
+		this.DataContextChanged += PickImageFromFile_DataContextChanged;
 	}
 
-	private async void SelectImageButton_Click(object sender, RoutedEventArgs e)
+	private void PickImageFromFile_DataContextChanged(DependencyObject sender, DataContextChangedEventArgs args)
 	{
-		var picker = new FileOpenPicker();
-		picker.FileTypeFilter.Add(".jpg");
+		ViewModel = args.NewValue as PickImageFromFileViewModel;
+	}
 
-		var fileName = $"{Guid.NewGuid()}.jpg";
-		StorageFile file = await picker.PickSingleFileAsync();
-		await file.CopyAsync(ApplicationData.Current.LocalFolder, fileName);
-		var uri = new Uri($"ms-appdata:///Local/{fileName}");
-		var bitmapImage = new BitmapImage(uri);
-		SelectedImage.Source = bitmapImage;
+	internal PickImageFromFileViewModel ViewModel { get; private set; }
+}
+
+internal class PickImageFromFileViewModel : ViewModelBase
+{
+	private string _errorMessage = string.Empty;
+	private ImageSource _selectedItemSource;
+
+	public string ErrorMessage
+	{
+		get => _errorMessage;
+		set
+		{
+			_errorMessage = value;
+			RaisePropertyChanged();
+		}
+	}
+
+	public ImageSource SelectedItemSource
+	{
+		get => _selectedItemSource;
+		set
+		{
+			_selectedItemSource = value;
+			RaisePropertyChanged();
+		}
+	}
+
+	public async void SelectImageButton()
+	{
+		try
+		{
+			var picker = new FileOpenPicker()
+			{
+				FileTypeFilter = { ".jpg" }
+			};
+
+			var fileName = $"{Guid.NewGuid()}.jpg";
+			var file = await picker.PickSingleFileAsync();
+			await file.CopyAsync(ApplicationData.Current.LocalFolder, fileName);
+			var uri = new Uri($"ms-appdata:///Local/{fileName}");
+			var bitmapImage = new BitmapImage(uri);
+			SelectedItemSource = bitmapImage;
+		}
+		catch (Exception ex)
+		{
+			ErrorMessage = $"Exception occurred: {ex}.";
+		}
 	}
 }
+
