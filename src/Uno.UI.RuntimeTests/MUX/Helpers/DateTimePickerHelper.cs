82c16d9c57e3b3a6fb085ccb6cda01c40e942a6c
ramezgerges <66218781+ramezgerges@users.noreply.github.com>
2024-03-07 13:48:09 +0000

    chore: build errors

--- src/Uno.UI.RuntimeTests/MUX/Helpers/DateTimePickerHelper.cs
+++ src/Uno.UI.RuntimeTests/MUX/Helpers/DateTimePickerHelper.cs
@@ -117,7 +117,11 @@ internal static class DateTimePickerHelper
 			Assert.AreEqual(3, loopingSelectors.Count, "Expected to find 3 LoopingSelectors");
 
 			// Uno Specific: we use GetOrder as the order may vary
+#if HAS_UNO
 			timePickerFlyoutPresenter.GetOrder(out var hourOrder, out var minuteOrder, out var periodOrder, out _);
+#else
+			var (hourOrder, minuteOrder, periodOrder) = (0, 1, 2);
+#endif
 			hourLoopingResult = loopingSelectors[hourOrder];
 			minuteLoopingResult = loopingSelectors[minuteOrder];
 			periodLoopingResult = loopingSelectors[periodOrder];
