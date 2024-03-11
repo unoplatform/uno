47a58726f7062525d80e167f5e04158db5a9bda6
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2024-03-11 20:00:53 +0100

    feat: Implement `RichTextBlockOverflowAutomationPeer`

--- src/Uno.UI/Generated/3.0.0.0/Microsoft.UI.Xaml.Automation.Peers/RichTextBlockOverflowAutomationPeer.cs
+++ src/Uno.UI/Generated/3.0.0.0/Microsoft.UI.Xaml.Automation.Peers/RichTextBlockOverflowAutomationPeer.cs
@@ -3,12 +3,12 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Microsoft.UI.Xaml.Automation.Peers
 {
-#if __ANDROID__ || __IOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+#if false
 	[global::Uno.NotImplemented]
 #endif
 	public partial class RichTextBlockOverflowAutomationPeer : global::Microsoft.UI.Xaml.Automation.Peers.FrameworkElementAutomationPeer
 	{
-#if __ANDROID__ || __IOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+#if false
 		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 		public RichTextBlockOverflowAutomationPeer(global::Microsoft.UI.Xaml.Controls.RichTextBlockOverflow owner) : base(owner)
 		{
