d22025cbc85fc627cb4c4b9fa693c8d2672be1b0
Martin Zikmund <martin@zikmund.dev>
2024-03-13 13:50:53 -0700

    chore: Add explanation

--- src/Uno.UI/UI/Xaml/Controls/Flyout/FlyoutBase.cs
+++ src/Uno.UI/UI/Xaml/Controls/Flyout/FlyoutBase.cs
@@ -332,6 +332,9 @@ namespace Microsoft.UI.Xaml.Controls.Primitives
 			if (_openFlyouts.Count > 0 && _openFlyouts[0] == this)
 			{
 				_openFlyouts.Remove(this);
+
+				// TODO Uno: Closed should occur on PresenterUnloaded,
+				// but that requires aligned loading/unloading lifecycle. #2895
 				_ = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
 				{
 					Closed?.Invoke(this, EventArgs.Empty);
