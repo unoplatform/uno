8b5274224a783e34bffd39a0e994092e7102abd9
Martin Zikmund <martin@zikmund.dev>
2024-03-18 12:15:01 +0100

    feat: Add missing Windows.Graphics structs

---
+++ src/Uno.UWP/Graphics/PointInt32.cs
@@ -0,0 +1,17 @@
+namespace Windows.Graphics;
+
+/// <summary>
+/// Defines a point in a two-dimensional plane.
+/// </summary>
+public partial struct PointInt32
+{
+	/// <summary>
+	/// The X coordinate value of a point.
+	/// </summary>
+	public int X;
+
+	/// <summary>
+	/// The Y coordinate value of a point.
+	/// </summary>
+	public int Y;
+}
---
+++ src/Uno.UWP/Graphics/RectInt32.cs
@@ -0,0 +1,27 @@
+namespace Windows.Graphics;
+
+/// <summary>
+/// Defines the size and location of a rectangular surface.
+/// </summary>
+public partial struct RectInt32
+{
+	/// <summary>
+	/// The X coordinate of the top-left corner of the rectangle.
+	/// </summary>
+	public int X;
+
+	/// <summary>
+	/// The Y coordinate of the top-left corner of the rectangle.
+	/// </summary>
+	public int Y;
+
+	/// <summary>
+	/// The width of a rectangle.
+	/// </summary>
+	public int Width;
+
+	/// <summary>
+	/// The height of a rectangle.
+	/// </summary>
+	public int Height;
+}
