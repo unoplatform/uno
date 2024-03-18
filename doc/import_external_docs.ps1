12336df0626ad5d597d502999869eab5e1756d51
agneszitte <agnes@platform.uno>
2024-03-18 16:22:23 -0400

    chore: Fix cherry-pick fail

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -7,18 +7,6 @@ Set-PSDebug -Trace 1
 
 $external_docs = @{
     # use either commit, or branch name to use its latest commit
-<<<<<<< HEAD
-    "uno.wasm.bootstrap" = "5ef2048d98df307c738186a5339eedcc4665be72"
-    "uno.themes"         = "6ce3ce8b0f61a7a017893541e1dd901ab04c906e"
-    "uno.toolkit.ui"     = "62a2ec7fb777bf40da7af2ea8fc83576f680e275"
-    "uno.check"          = "0350ba1b78ee7488d88f093e8f92cc4d9cb055f5"
-    "uno.xamlmerge.task" = "7e8ffef206e87dfea90c53805c45e93a7d8c0b46"
-    "figma-docs"         = "909bfd2f272b584e1312b64136c7fb98c66e625d"
-    "uno.resizetizer"    = "3eec4aad0b7b3480ec6c2a121911ffde844fc4f8"
-    "uno.uitest"         = "555453c2985ef2745fe44503c5809a6168d063c2"
-    "uno.extensions"     = "f154d50ca97f5ebf530b299d3ecb30f36604100a"
-    "workshops"          = "f3d80513c7a9a69162bedd6a145462a3a4310f33"
-=======
     "uno.wasm.bootstrap" = "b1bc52b211bfe73e75a285e6fa1c89c7f655c335" #latest main commit
     "uno.themes"         = "fa2eee00b63901c94840633204e0837b32ade719" #latest release branch commit
     "uno.toolkit.ui"     = "d48202c41792148db7ea1b05d5a9e42c651f6218" #latest release branch commit
@@ -29,7 +17,6 @@ $external_docs = @{
     "uno.uitest"         = "9669fd2783187d06c36dd6a717c1b9f08d1fa29c" #latest master commit
     "uno.extensions"     = "f154d50ca97f5ebf530b299d3ecb30f36604100a" #latest release branch commit
     "workshops"          = "f3d80513c7a9a69162bedd6a145462a3a4310f33" #latest master commit
->>>>>>> 4cd5bce817 (docs: Update documentation to latest commits)
 }
 
 $uno_git_url = "https://github.com/unoplatform/"
