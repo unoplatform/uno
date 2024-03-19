a7bad1f416e8fb47cab813504bd37a1ecd36181a
Martin Zikmund <martin@zikmund.dev>
2024-03-19 13:58:47 +0100

    fix: Swap the Asset/Drawable resolution order

---
+++ src/Uno.UWP/Extensions/AssetManagerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.Content.Res;
+
+namespace Uno.Extensions;
+
+internal static class AssetManagerExtensions
+{
+	internal static bool TryOpen(this AssetManager assetManager, string path, out System.IO.Stream? stream)
+	{
+		try
+		{
+			stream = assetManager.Open(path);
+			return true;
+		}
+		catch (Exception)
+		{
+			stream = null;
+			return false;
+		}
+	}
+}
--- src/Uno.UWP/Storage/StorageFile.Android.cs
+++ src/Uno.UWP/Storage/StorageFile.Android.cs
@@ -20,78 +20,87 @@ using Uno.Helpers;
 using Windows.ApplicationModel.Background;
 using System.Diagnostics.CodeAnalysis;
 
-namespace Windows.Storage
+namespace Windows.Storage;
+
+partial class StorageFile
 {
-	partial class StorageFile
+	private static ConcurrentEntryManager _assetGate = new ConcurrentEntryManager();
+	private static string? _currentAppID;
+
+	private static async Task<StorageFile> GetFileFromApplicationUri(CancellationToken ct, Uri uri)
 	{
-		private static ConcurrentEntryManager _assetGate = new ConcurrentEntryManager();
-		private static string? _currentAppID;
-
-		private static async Task<StorageFile> GetFileFromApplicationUri(CancellationToken ct, Uri uri)
+		if (uri.Scheme != "ms-appx")
 		{
-			if (uri.Scheme != "ms-appx")
-			{
-				// ms-appdata is handled by the caller.
-				throw new InvalidOperationException("Uri is not using the ms-appx or ms-appdata scheme");
-			}
-
-			var originalPath = Uri.UnescapeDataString(uri.PathAndQuery).TrimStart('/');
-
-			var path = AndroidResourceNameEncoder.EncodeResourcePath(originalPath);
-
-			EnsureAppID();
-
-			// Read the contents of our asset
-			var outputCachePath = global::System.IO.Path.Combine(Android.App.Application.Context.CacheDir!.AbsolutePath, _currentAppID, path);
-
-			if (typeof(StorageFile).Log().IsEnabled(Uno.Foundation.Logging.LogLevel.Debug))
-			{
-				typeof(StorageFile).Log().Debug($"GetFileFromApplicationUriAsyncTask path:{path} outputCachePath:{outputCachePath}");
-			}
-
-			// Ensure only one generation of the cached file can occur at a time.
-			// The copy of tha android asset should not be necessary, but requires a significant
-			// refactoring of the StorageFile class to make non-local provides opaque.
-			// This will need to be revisited once we add support for other providers.
-			using var gate = await _assetGate.LockForAsset(ct, path);
-
-			if (!File.Exists(outputCachePath))
-			{
-				Directory.CreateDirectory(global::System.IO.Path.GetDirectoryName(outputCachePath)!);
-
-				Stream GetAsset()
-				{
-					if (DrawableHelper.FindResourceIdFromPath(path) is { } resourceId)
-					{
-						return ContextHelper.Current.Resources!.OpenRawResource(resourceId);
-					}
-					else
-					{
-						var assets = global::Android.App.Application.Context.Assets;
-						return assets!.Open(path);
-					}
-				}
-
-				using var input = GetAsset();
-				using var output = File.OpenWrite(outputCachePath);
-
-				await input.CopyToAsync(output);
-			}
-
-			return await StorageFile.GetFileFromPathAsync(outputCachePath);
+			// ms-appdata is handled by the caller.
+			throw new InvalidOperationException("Uri is not using the ms-appx or ms-appdata scheme");
 		}
 
-		[MemberNotNull(nameof(_currentAppID))]
-		private static void EnsureAppID()
-		{
-			if (_currentAppID is null)
-			{
-				var packageVersion = ApplicationModel.Package.Current.Id.Version;
+		var originalPath = Uri.UnescapeDataString(uri.PathAndQuery).TrimStart('/');
 
-				_currentAppID =
-					ContextHelper.Current.GetType().Assembly.GetModules().First().ModuleVersionId.ToString()
-					+ $"_{packageVersion.Major}.{packageVersion.Minor}.{packageVersion.Build}.{packageVersion.Revision}";
+		var path = AndroidResourceNameEncoder.EncodeResourcePath(originalPath);
+
+		EnsureAppID();
+
+		// Read the contents of our asset
+		var outputCachePath = global::System.IO.Path.Combine(Android.App.Application.Context.CacheDir!.AbsolutePath, _currentAppID, path);
+
+		if (typeof(StorageFile).Log().IsEnabled(Uno.Foundation.Logging.LogLevel.Debug))
+		{
+			typeof(StorageFile).Log().Debug($"GetFileFromApplicationUriAsyncTask path:{path} outputCachePath:{outputCachePath}");
+		}
+
+		// Ensure only one generation of the cached file can occur at a time.
+		// The copy of tha android asset should not be necessary, but requires a significant
+		// refactoring of the StorageFile class to make non-local provides opaque.
+		// This will need to be revisited once we add support for other providers.
+		using var gate = await _assetGate.LockForAsset(ct, path);
+
+		if (!File.Exists(outputCachePath))
+		{
+			Directory.CreateDirectory(global::System.IO.Path.GetDirectoryName(outputCachePath)!);
+
+			Stream? GetAsset()
+			{
+				var assets = global::Android.App.Application.Context.Assets;
+				if (assets.TryOpen(path, out var stream))
+				{
+					return stream;
+				}
+				else if (DrawableHelper.FindResourceIdFromPath(path) is { } resourceId)
+				{
+					return ContextHelper.Current.Resources!.OpenRawResource(resourceId);
+				}
+
+				return null;
 			}
+
+			var stream = GetAsset();
+
+			if (stream is null)
+			{
+				throw new FileNotFoundException($"The file [{originalPath}] cannot be found in the package directory");
+			}
+
+			using (stream)
+			{
+				using var output = File.OpenWrite(outputCachePath);
+				await stream.CopyToAsync(output);
+			}
+		}
+
+		return await StorageFile.GetFileFromPathAsync(outputCachePath);
+	}
+
+	[MemberNotNull(nameof(_currentAppID))]
+	private static void EnsureAppID()
+	{
+		if (_currentAppID is null)
+		{
+			var packageVersion = ApplicationModel.Package.Current.Id.Version;
+
+			_currentAppID =
+				ContextHelper.Current.GetType().Assembly.GetModules().First().ModuleVersionId.ToString()
+				+ $"_{packageVersion.Major}.{packageVersion.Minor}.{packageVersion.Build}.{packageVersion.Revision}";
 		}
 	}
 }
