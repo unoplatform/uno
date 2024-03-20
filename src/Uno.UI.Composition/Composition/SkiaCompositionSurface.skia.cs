afae50d9f62edcce6405849aeeed7ea0e50c6338
Youssef Victor <youssefvictor00@gmail.com>
2024-03-20 11:45:34 +0200

    feat(skia): Gif support

--- src/Uno.UI.Composition/Composition/SkiaCompositionSurface.skia.cs
+++ src/Uno.UI.Composition/Composition/SkiaCompositionSurface.skia.cs
@@ -1,27 +1,37 @@
 ﻿#nullable enable
 
-
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Uno.Extensions;
 using Uno.Foundation.Logging;
+using Uno.UI.Dispatching;
 using Windows.Graphics;
 
 namespace Microsoft.UI.Composition
 {
-	internal partial class SkiaCompositionSurface : ICompositionSurface
+	internal partial class SkiaCompositionSurface : CompositionObject, ICompositionSurface
 	{
-		private SKImage? _image;
+		private SKImage[]? _images;
+		private SKCodecFrameInfo[]? _infos;
+		private int _currentFrame;
 
-		public SKImage? Image { get => _image; }
+		private int CurrentFrame
+		{
+			get => _currentFrame;
+			set => SetProperty(ref _currentFrame, value);
+		}
+
+		public SKImage? Image => _images?[_currentFrame];
 
 		internal SkiaCompositionSurface(SKImage image)
 		{
-			_image = image;
+			_images = [image];
 		}
 
 		internal (bool success, object nativeResult) LoadFromStream(int? targetWidth, int? targetHeight, Stream imageStream)
@@ -43,7 +53,7 @@ namespace Microsoft.UI.Composition
 
 				if (result == SKCodecResult.Success)
 				{
-					_image = SKImage.FromBitmap(bitmap);
+					_images = [SKImage.FromBitmap(bitmap)];
 				}
 
 				return (result == SKCodecResult.Success || result == SKCodecResult.IncompleteInput, result);
@@ -52,13 +62,41 @@ namespace Microsoft.UI.Composition
 			{
 				try
 				{
-					_image = SKImage.FromEncodedData(stream);
-					return _image is null
-						? (false, "Failed to decode image")
-						: (true, "Success");
+					using var codec = SKCodec.Create(stream);
+					_infos = codec.FrameInfo;
+					var info = codec.Info;
+					info = new SKImageInfo(info.Width, info.Height, SKColorType.Bgra8888, SKAlphaType.Premul);
+					var bitmap = new SKBitmap(info);
+					_images = GC.AllocateUninitializedArray<SKImage>(_infos.Length);
+
+					for (int i = 0; i < _infos.Length; i++)
+					{
+						var options = new SKCodecOptions(i);
+						codec.GetPixels(info, bitmap.GetPixels(), options);
+						var currentBitmap = SKImage.FromBitmap(bitmap);
+						if (currentBitmap is null)
+						{
+							_images = null;
+							return (false, "Failed to decode image");
+						}
+
+						_images[i] = currentBitmap;
+					}
+
+					_ = Task.Run(() =>
+					{
+						CurrentFrame = 0;
+						while (_images is not null && _images.Length > 1)
+						{
+							Thread.Sleep(_infos[CurrentFrame].Duration);
+							NativeDispatcher.Main.Enqueue(() => CurrentFrame = (CurrentFrame + 1) % _infos.Length, NativeDispatcherPriority.High);
+						}
+					});
+					return (true, "Success");
 				}
 				catch (Exception e)
 				{
+					_images = null;
 					return (true, e.Message);
 				}
 			}
@@ -73,7 +111,8 @@ namespace Microsoft.UI.Composition
 
 			using (var pData = data.Pin())
 			{
-				_image = SKImage.FromPixelCopy(info, (IntPtr)pData.Pointer, pixelWidth * 4);
+				_currentFrame = 0;
+				_images = [SKImage.FromPixelCopy(info, (IntPtr)pData.Pointer, pixelWidth * 4)];
 			}
 		}
 	}
