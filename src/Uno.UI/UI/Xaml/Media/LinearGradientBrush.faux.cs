5f21dbe7b0647f51ba8ec6bf2e69f3859ce9025c
Martin Zikmund <martin@zikmund.dev>
2024-03-20 08:53:52 +0100

    chore: Linear gradient border handling

---
+++ src/Uno.UI/UI/Xaml/Media/LinearGradientBrush.faux.cs
@@ -0,0 +1,87 @@
+﻿#if __WASM__ || __IOS__ || __MACOS__
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Uno.UI.Xaml.Media;
+using Windows.Foundation.Collections;
+
+namespace Windows.UI.Xaml.Media;
+
+public partial class LinearGradientBrush
+{
+	private SolidColorBrush? _majorStopBrush;
+	private SolidColorBrush? _fauxOverlayBrush;
+
+	internal SolidColorBrush? MajorStopBrush => _majorStopBrush ??= CreateMajorBrush();
+
+	internal SolidColorBrush? FauxOverlayBrush => _fauxOverlayBrush ??= CreateFauxOverlayBrush();
+
+	internal bool CanApplySolidColorRendering() => GradientStops.Count == 2;
+
+	/// <summary>
+	/// Returns major stop of given border gradient.
+	/// </summary>
+	/// <returns>Gradient stop.</returns>
+	internal GradientStop? GetMajorStop()
+	{
+		if (!CanApplySolidColorRendering())
+		{
+			return null;
+		}
+
+		var firstStop = GradientStops[0];
+		var secondStop = GradientStops[1];
+
+		if (MappingMode == BrushMappingMode.Absolute)
+		{
+			// When absolute, the major stop is either the one with
+			// larger offset or the second stop in order if same.
+			return firstStop.Offset <= secondStop.Offset ?
+				secondStop : firstStop;
+		}
+		else
+		{
+			if (secondStop.Offset < firstStop.Offset)
+			{
+				(firstStop, secondStop) = (secondStop, firstStop);
+			}
+			return firstStop.Offset > (1 - secondStop.Offset) ?
+				firstStop : secondStop;
+		}
+	}
+
+	internal VerticalAlignment GetMinorStopAlignment()
+	{
+		var scaleTransform = RelativeTransform as ScaleTransform;
+		return scaleTransform?.ScaleY != -1 ?
+				VerticalAlignment.Top : VerticalAlignment.Bottom;
+	}
+
+	private SolidColorBrush? CreateFauxOverlayBrush()
+	{
+		if (!CanApplySolidColorRendering())
+		{
+			return null;
+		}
+
+		var majorStop = GetMajorStop();
+		var minorStop = GradientStops.First(s => s != majorStop);
+
+		return new SolidColorBrush(minorStop.Color);
+	}
+
+	private SolidColorBrush? CreateMajorBrush()
+	{
+		if (!CanApplySolidColorRendering())
+		{
+			return null;
+		}
+
+		var majorStop = GetMajorStop();
+		return new SolidColorBrush(majorStop!.Color);
+	}
+}
+#endif
