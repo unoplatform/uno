270ee9ba57adc5bf081605e87a88f7629c519bfa
Youssef Victor <youssefvictor00@gmail.com>
2024-03-20 08:01:03 +0200

    chore: Fix build

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_FrameworkElement_And_Leak.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_FrameworkElement_And_Leak.cs
@@ -419,7 +419,7 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml
 
 			~Holder()
 			{
-				var counter = Interlocked.Decrement(ref _counter)
+				var counter = Interlocked.Decrement(ref _counter);
 				_update(counter);
 			}
 
