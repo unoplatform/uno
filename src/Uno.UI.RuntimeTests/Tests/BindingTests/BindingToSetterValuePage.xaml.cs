c5b4afc03f30c16eef82a6d9bc5e5ccdd3e463d8
Youssef Victor <youssefvictor00@gmail.com>
2024-03-23 16:42:51 +0200

    chore: Additional test

--- src/Uno.UI.RuntimeTests/Tests/BindingTests/BindingToSetterValuePage.xaml.cs
+++ src/Uno.UI.RuntimeTests/Tests/BindingTests/BindingToSetterValuePage.xaml.cs
@@ -8,6 +8,12 @@ public sealed partial class BindingToSetterValuePage : Page
 	{
 		this.InitializeComponent();
 		this.DataContext = this;
+
+		// Just to make sure x:Name is generated for these.
+		_ = style1;
+		_ = style2;
+		_ = setter1;
+		_ = setter2;
 	}
 
 	public string TestValue => "Hello";
