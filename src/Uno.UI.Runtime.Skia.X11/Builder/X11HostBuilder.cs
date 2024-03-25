3186f881476e8996855b81766d983e02929a2a06
Ramez Ragaa <ramezragaa@proton.me>
2024-03-25 13:44:30 +0200

    chore: make the heuristic more generic for networked X11 setups

--- src/Uno.UI.Runtime.Skia.X11/Builder/X11HostBuilder.cs
+++ src/Uno.UI.Runtime.Skia.X11/Builder/X11HostBuilder.cs
@@ -1,20 +1,21 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using Uno.WinUI.Runtime.Skia.X11;
 
 namespace Uno.UI.Runtime.Skia;
 
 internal class X11HostBuilder : IPlatformHostBuilder
 {
+	private static readonly Regex DisplayRegex = new Regex(@"^(?:(?<hostname>[\w\.-]+):)?(?<displaynumber>\d+)(?:\.(?<screennumber>\d+))?$", RegexOptions.Compiled);
+
 	public X11HostBuilder()
 	{
 	}
 
 	public bool IsSupported
 		=> OperatingSystem.IsLinux() &&
-			// DISPLAY should look like ":0"
 			Environment.GetEnvironmentVariable("DISPLAY") is { } displayString &&
-			displayString.StartsWith(":") &&
-			int.TryParse(displayString[1..], out _);
+			DisplayRegex.Match(displayString).Success;
 
 	public SkiaHost Create(Func<Microsoft.UI.Xaml.Application> appBuilder)
 		=> new X11ApplicationHost(appBuilder);
