755b36d52786f72a3ffa1fb8d3bbe814a5ad7ca4
Ramez Ragaa <ramezragaa@proton.me>
2024-03-25 13:40:01 +0200

    chore: add a DISPLAY heuristic for X11 detection

--- src/Uno.UI.Runtime.Skia.X11/Builder/X11HostBuilder.cs
+++ src/Uno.UI.Runtime.Skia.X11/Builder/X11HostBuilder.cs
@@ -10,7 +10,11 @@ internal class X11HostBuilder : IPlatformHostBuilder
 	}
 
 	public bool IsSupported
-		=> OperatingSystem.IsLinux();
+		=> OperatingSystem.IsLinux() &&
+			// DISPLAY should look like ":0"
+			Environment.GetEnvironmentVariable("DISPLAY") is { } displayString &&
+			displayString.StartsWith(":") &&
+			int.TryParse(displayString[1..], out _);
 
 	public SkiaHost Create(Func<Microsoft.UI.Xaml.Application> appBuilder)
 		=> new X11ApplicationHost(appBuilder);
