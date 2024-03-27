df088f8c65d6ea9e3b3fce8860c74fa6e6a001f0
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2024-03-28 00:21:45 +0100

    chore: Update platform specific FrameworkElementAutomationPeers

--- src/Uno.UI/UI/Xaml/Automation/Peers/FrameworkElementAutomationPeer.Android.cs
+++ src/Uno.UI/UI/Xaml/Automation/Peers/FrameworkElementAutomationPeer.Android.cs
@@ -7,102 +7,101 @@ using Microsoft.UI.Xaml.Automation.Provider;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 
-namespace Microsoft.UI.Xaml.Automation.Peers
+namespace Microsoft.UI.Xaml.Automation.Peers;
+
+public partial class FrameworkElementAutomationPeer : AutomationPeer
 {
-	public partial class FrameworkElementAutomationPeer : AutomationPeer
+	internal override void SendAccessibilityEvent([GeneratedEnum] EventTypes eventType)
 	{
-		internal override void SendAccessibilityEvent([GeneratedEnum] EventTypes eventType)
+		// This is only called if View.Focusable and View.FocusableInTouchMode are set to true.
+
+		base.SendAccessibilityEvent(eventType);
+
+		switch (eventType)
 		{
-			// This is only called if View.Focusable and View.FocusableInTouchMode are set to true.
+			case EventTypes.ViewClicked:
+				InvokeAutomationPeer();
+				break;
+			default:
+				break;
+		}
+	}
 
-			base.SendAccessibilityEvent(eventType);
-
-			switch (eventType)
-			{
-				case EventTypes.ViewClicked:
-					InvokeAutomationPeer();
-					break;
-				default:
-					break;
-			}
+	internal override void OnInitializeAccessibilityNodeInfo(AccessibilityNodeInfo info)
+	{
+		if (!AutomationConfiguration.IsAccessibilityEnabled)
+		{
+			return;
 		}
 
-		internal override void OnInitializeAccessibilityNodeInfo(AccessibilityNodeInfo info)
+		var isAccessible = AutomationProperties.GetAccessibilityView(Owner) != AccessibilityView.Raw;
+		if (isAccessible)
 		{
-			if (!AutomationConfiguration.IsAccessibilityEnabled)
-			{
-				return;
-			}
+			// TODO: Set View.Focusable and View.FocusableInTouchMode to true to ensure SendAccessibilityEvent gets called?
+			info.Focusable = isAccessible;
+			info.ContentDescription = GetName();
+			info.Password = IsPassword();
+			info.Enabled = IsEnabled();
 
-			var isAccessible = AutomationProperties.GetAccessibilityView(Owner) != AccessibilityView.Raw;
-			if (isAccessible)
-			{
-				// TODO: Set View.Focusable and View.FocusableInTouchMode to true to ensure SendAccessibilityEvent gets called?
-				info.Focusable = isAccessible;
-				info.ContentDescription = GetName();
-				info.Password = IsPassword();
-				info.Enabled = IsEnabled();
-
-				// TODO: Use GetAutomationControlType() and GetPattern() instead
-				info.Clickable = GetClickable();
-				info.Checked = GetChecked();
-				info.Checkable = GetCheckable();
-				info.Editable = GetEditable();
-				info.InputType = GetInputType();
-				info.MultiLine = GetMultiline();
-				info.Selected = GetSelected();
-				info.MaxTextLength = GetMaxTextLength();
-				info.Scrollable = GetScrollable();
-			}
+			// TODO: Use GetAutomationControlType() and GetPattern() instead
+			info.Clickable = GetClickable();
+			info.Checked = GetChecked();
+			info.Checkable = GetCheckable();
+			info.Editable = GetEditable();
+			info.InputType = GetInputType();
+			info.MultiLine = GetMultiline();
+			info.Selected = GetSelected();
+			info.MaxTextLength = GetMaxTextLength();
+			info.Scrollable = GetScrollable();
 		}
+	}
 
-		private bool GetScrollable()
-		{
-			return this is IScrollProvider;
-		}
+	private bool GetScrollable()
+	{
+		return this is IScrollProvider;
+	}
 
-		private int GetMaxTextLength()
-		{
-			return Owner is TextBox textBox ? textBox.MaxLength : 0;
-		}
+	private int GetMaxTextLength()
+	{
+		return Owner is TextBox textBox ? textBox.MaxLength : 0;
+	}
 
-		private bool GetSelected()
-		{
-			// TODO: return this is ISelectionItemProvider selectionItemProvider && selectionItemProvider.IsSelected;
-			return Owner is SelectorItem selectorItem && selectorItem.IsSelected;
-		}
+	private bool GetSelected()
+	{
+		// TODO: return this is ISelectionItemProvider selectionItemProvider && selectionItemProvider.IsSelected;
+		return Owner is SelectorItem selectorItem && selectorItem.IsSelected;
+	}
 
-		private bool GetMultiline()
-		{
-			return Owner is TextBox textBox && textBox.AcceptsReturn;
-		}
+	private bool GetMultiline()
+	{
+		return Owner is TextBox textBox && textBox.AcceptsReturn;
+	}
 
-		private InputTypes GetInputType()
-		{
-			return Owner is TextBox textBox ? InputScopeHelper.ConvertInputScope(textBox.InputScope) : InputTypes.Null;
-		}
+	private InputTypes GetInputType()
+	{
+		return Owner is TextBox textBox ? InputScopeHelper.ConvertInputScope(textBox.InputScope) : InputTypes.Null;
+	}
 
-		private bool GetEditable()
-		{
-			return Owner is TextBox
-				|| Owner is PasswordBox;
-		}
+	private bool GetEditable()
+	{
+		return Owner is TextBox
+			|| Owner is PasswordBox;
+	}
 
-		private bool GetCheckable()
-		{
-			return this is IToggleProvider;
-		}
+	private bool GetCheckable()
+	{
+		return this is IToggleProvider;
+	}
 
-		private bool GetChecked()
-		{
-			return this is IToggleProvider toggleProvider && toggleProvider.ToggleState == ToggleState.On;
-		}
+	private bool GetChecked()
+	{
+		return this is IToggleProvider toggleProvider && toggleProvider.ToggleState == ToggleState.On;
+	}
 
-		private bool GetClickable()
-		{
-			return this is IInvokeProvider
-				|| this is IToggleProvider
-				|| this is ISelectionItemProvider;
-		}
+	private bool GetClickable()
+	{
+		return this is IInvokeProvider
+			|| this is IToggleProvider
+			|| this is ISelectionItemProvider;
 	}
 }
--- src/Uno.UI/UI/Xaml/Automation/Peers/FrameworkElementAutomationPeer.iOS.cs
+++ src/Uno.UI/UI/Xaml/Automation/Peers/FrameworkElementAutomationPeer.iOS.cs
@@ -6,89 +6,88 @@ using Uno;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 
-namespace Microsoft.UI.Xaml.Automation.Peers
+namespace Microsoft.UI.Xaml.Automation.Peers;
+
+public partial class FrameworkElementAutomationPeer : AutomationPeer
 {
-	public partial class FrameworkElementAutomationPeer : AutomationPeer
+	internal override bool AccessibilityActivate()
 	{
-		internal override bool AccessibilityActivate()
+		return InvokeAutomationPeer();
+	}
+
+	internal override bool UpdateAccessibilityElement()
+	{
+		if (!AutomationConfiguration.IsAccessibilityEnabled)
 		{
-			return InvokeAutomationPeer();
+			return false;
 		}
 
-		internal override bool UpdateAccessibilityElement()
+		var isAccessible = AutomationProperties.GetAccessibilityView(Owner) != AccessibilityView.Raw;
+		if (isAccessible && Owner is UIView view)
 		{
-			if (!AutomationConfiguration.IsAccessibilityEnabled)
-			{
-				return false;
-			}
+			view.AccessibilityLabel = GetName();
+			view.AccessibilityTraits = GetAccessibilityTraits();
 
-			var isAccessible = AutomationProperties.GetAccessibilityView(Owner) != AccessibilityView.Raw;
-			if (isAccessible && Owner is UIView view)
-			{
-				view.AccessibilityLabel = GetName();
-				view.AccessibilityTraits = GetAccessibilityTraits();
-
-				// TODO
-				//view.AccessibilityHint
-				//view.AccessibilityValue
-				//view.AccessibilityElementsHidden
-				//view.AccessibilityNavigationStyle
-			}
-
-			return isAccessible;
+			// TODO
+			//view.AccessibilityHint
+			//view.AccessibilityValue
+			//view.AccessibilityElementsHidden
+			//view.AccessibilityNavigationStyle
 		}
 
-		private UIAccessibilityTrait GetAccessibilityTraits()
+		return isAccessible;
+	}
+
+	private UIAccessibilityTrait GetAccessibilityTraits()
+	{
+		// TODO: Use GetAutomationControlType() and GetPattern() instead
+
+		UIAccessibilityTrait accessibilityTrait;
+
+		switch (GetAutomationControlType())
 		{
-			// TODO: Use GetAutomationControlType() and GetPattern() instead
-
-			UIAccessibilityTrait accessibilityTrait;
-
-			switch (GetAutomationControlType())
-			{
-				case AutomationControlType.Button:
-					accessibilityTrait = UIAccessibilityTrait.Button;
-					break;
-				case AutomationControlType.CheckBox:
-				case AutomationControlType.RadioButton:
-				case AutomationControlType.ComboBox: // ?
-				case AutomationControlType.ListItem:
-					// TODO: Shouldn't announce as button if not clickable/selectable
-					accessibilityTrait = UIAccessibilityTrait.Button;
-					break;
-				case AutomationControlType.Image:
-					accessibilityTrait = UIAccessibilityTrait.Image;
-					break;
-				case AutomationControlType.Text:
-					accessibilityTrait = UIAccessibilityTrait.StaticText;
-					break;
-				case AutomationControlType.Hyperlink:
-					accessibilityTrait = UIAccessibilityTrait.Link;
-					break;
-				case AutomationControlType.Header:
-					accessibilityTrait = UIAccessibilityTrait.Header;
-					break;
-				case AutomationControlType.Slider:
-					accessibilityTrait = UIAccessibilityTrait.Adjustable;
-					break;
-				default:
-					accessibilityTrait = UIAccessibilityTrait.None;
-					break;
-			}
-
-			if (Owner is SelectorItem selectorItem && selectorItem.IsSelected
-			|| Owner is ToggleButton toggleButton && toggleButton.IsChecked == true
-			|| Owner is ToggleSwitch toggleSwitch && toggleSwitch.IsOn)
-			{
-				accessibilityTrait |= UIAccessibilityTrait.Selected;
-			}
-
-			if (!IsEnabled())
-			{
-				accessibilityTrait |= UIAccessibilityTrait.NotEnabled;
-			}
-
-			return accessibilityTrait;
+			case AutomationControlType.Button:
+				accessibilityTrait = UIAccessibilityTrait.Button;
+				break;
+			case AutomationControlType.CheckBox:
+			case AutomationControlType.RadioButton:
+			case AutomationControlType.ComboBox: // ?
+			case AutomationControlType.ListItem:
+				// TODO: Shouldn't announce as button if not clickable/selectable
+				accessibilityTrait = UIAccessibilityTrait.Button;
+				break;
+			case AutomationControlType.Image:
+				accessibilityTrait = UIAccessibilityTrait.Image;
+				break;
+			case AutomationControlType.Text:
+				accessibilityTrait = UIAccessibilityTrait.StaticText;
+				break;
+			case AutomationControlType.Hyperlink:
+				accessibilityTrait = UIAccessibilityTrait.Link;
+				break;
+			case AutomationControlType.Header:
+				accessibilityTrait = UIAccessibilityTrait.Header;
+				break;
+			case AutomationControlType.Slider:
+				accessibilityTrait = UIAccessibilityTrait.Adjustable;
+				break;
+			default:
+				accessibilityTrait = UIAccessibilityTrait.None;
+				break;
 		}
+
+		if (Owner is SelectorItem selectorItem && selectorItem.IsSelected
+		|| Owner is ToggleButton toggleButton && toggleButton.IsChecked == true
+		|| Owner is ToggleSwitch toggleSwitch && toggleSwitch.IsOn)
+		{
+			accessibilityTrait |= UIAccessibilityTrait.Selected;
+		}
+
+		if (!IsEnabled())
+		{
+			accessibilityTrait |= UIAccessibilityTrait.NotEnabled;
+		}
+
+		return accessibilityTrait;
 	}
 }
