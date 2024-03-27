1035cb0a320d5d79393e9c6afd7a1f2ed6a8c148
Youssef Victor <youssefvictor00@gmail.com>
2024-03-27 20:34:35 +0200

    fix: Fix DatePicker crashing and not having correct initial date

---
+++ src/Uno.UI.RuntimeTests/Helpers/TimeZoneModifier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+namespace Uno.UI.RuntimeTests.Helpers;
+
+public class TimeZoneModifier : IDisposable
+{
+	private readonly TimeZoneInfo _originalTimeZoneInfo;
+
+	public TimeZoneModifier(TimeZoneInfo timeZoneInfo)
+	{
+		_originalTimeZoneInfo = GetLocalTimeZone();
+		SetLocalTimeZone(timeZoneInfo);
+	}
+
+	private static TimeZoneInfo GetLocalTimeZone()
+	{
+		var cachedData = typeof(TimeZoneInfo).GetField("s_cachedData", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
+		return (TimeZoneInfo)cachedData.GetType().GetProperty("Local").GetValue(cachedData);
+	}
+
+	private static void SetLocalTimeZone(TimeZoneInfo timeZoneInfo)
+	{
+		var cachedData = typeof(TimeZoneInfo).GetField("s_cachedData", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
+		cachedData.GetType().GetField("_localTimeZone", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(cachedData, timeZoneInfo);
+	}
+
+	public void Dispose()
+	{
+		SetLocalTimeZone(_originalTimeZoneInfo);
+	}
+}
