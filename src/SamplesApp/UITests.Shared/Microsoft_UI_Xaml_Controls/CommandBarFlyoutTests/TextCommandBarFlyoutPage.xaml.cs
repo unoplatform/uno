8a92c6c86863a5874c19c37058ab38b50f8293aa
Martin Zikmund <martin@zikmund.dev>
2024-03-31 22:59:01 +0200

    chore: Further updates

--- src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/CommandBarFlyoutTests/TextCommandBarFlyoutPage.xaml.cs
+++ src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/CommandBarFlyoutTests/TextCommandBarFlyoutPage.xaml.cs
@@ -8,283 +8,282 @@ using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Metadata;
 using Windows.System;
-using Windows.UI.Text;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Automation;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Documents;
-using Windows.UI.Xaml.Input;
+using Microsoft.UI.Text;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Automation;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.UI.Xaml.Input;
 
 using TextCommandBarFlyout = Microsoft.UI.Xaml.Controls.TextCommandBarFlyout;
 
-namespace MUXControlsTestApp
+namespace MUXControlsTestApp;
+
+[Sample("CommandBarFlyout", "WinUI")]
+public sealed partial class TextCommandBarFlyoutPage : TestPage
 {
-	[Sample("CommandBarFlyout", "WinUI")]
-    public sealed partial class TextCommandBarFlyoutPage : TestPage
-    {
-        public TextCommandBarFlyoutPage()
-        {
-            this.InitializeComponent();
+	public TextCommandBarFlyoutPage()
+	{
+		this.InitializeComponent();
 
 #if !HAS_UNO
-			RichEditBox1.Document.SetText(Windows.UI.Text.TextSetOptions.None, "Lorem ipsum ergo sum");
+		RichEditBox1.Document.SetText(Windows.UI.Text.TextSetOptions.None, "Lorem ipsum ergo sum");
 #endif
-			Clipboard.ContentChanged += OnClipboardContentChanged;
+		Clipboard.ContentChanged += OnClipboardContentChanged;
 
-            if (ApiInformation.IsEnumNamedValuePresent("Windows.UI.Xaml.Controls.Primitives.FlyoutPlacementMode", "BottomEdgeAlignedLeft"))
-            {
-                TextControlContextFlyout.Placement = FlyoutPlacementMode.BottomEdgeAlignedLeft;
-            }
-            else
-            {
-                TextControlContextFlyout.Placement = FlyoutPlacementMode.Top;
-            }
-
-            if (ApiInformation.IsEnumNamedValuePresent("Windows.UI.Xaml.Controls.Primitives.FlyoutPlacementMode", "TopEdgeAlignedLeft"))
-            {
-                TextControlSelectionFlyout.Placement = FlyoutPlacementMode.TopEdgeAlignedLeft;
-            }
-            else
-            {
-                TextControlSelectionFlyout.Placement = FlyoutPlacementMode.Top;
-            }
-
-            if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.UIElement", "ContextFlyout"))
-            {
-                TextBox1.ContextFlyout = TextControlContextFlyout;
-                TextBlock1.ContextFlyout = TextControlContextFlyout;
-#if !HAS_UNO
-				RichEditBox1.ContextFlyout = TextControlContextFlyout;
-#endif
-				RichTextBlock1.ContextFlyout = TextControlContextFlyout;
-                PasswordBox1.ContextFlyout = TextControlContextFlyout;
-            }
-
-            try
-            {
-                if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.TextBox", "SelectionFlyout"))
-                {
-                    TextBox1.SelectionFlyout = TextControlSelectionFlyout;
-                }
-
-                if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.TextBlock", "SelectionFlyout"))
-                {
-                    TextBlock1.SelectionFlyout = TextControlSelectionFlyout;
-                }
-
-                if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.RichEditBox", "SelectionFlyout"))
-                {
-#if !HAS_UNO
-					RichEditBox1.SelectionFlyout = TextControlSelectionFlyout;
-#endif
-				}
-
-                if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.RichTextBlock", "SelectionFlyout"))
-                {
-                    RichTextBlock1.SelectionFlyout = TextControlSelectionFlyout;
-                }
-
-                if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.PasswordBox", "SelectionFlyout"))
-                {
-                    PasswordBox1.SelectionFlyout = TextControlSelectionFlyout;
-                }
-            }
-            catch (InvalidCastException)
-            {
-                // RS5 interfaces can change before release, so we need to make sure we don't crash if they do.
-            }
-        }
-
-        private async void OnClipboardContentChanged(object sender, object e)
-        {
-            var clipboardContent = Clipboard.GetContent();
-
-            if (clipboardContent.Contains(StandardDataFormats.Rtf))
-            {
-                RecordEvent(string.Format("Clipboard content changed to '{0}'", await Clipboard.GetContent().GetRtfAsync()));
-            }
-            else if (clipboardContent.Contains(StandardDataFormats.Text))
-            {
-                RecordEvent(string.Format("Clipboard content changed to '{0}'", await Clipboard.GetContent().GetTextAsync()));
-            }
-            else
-            {
-                RecordEvent(string.Format("Clipboard content changed to ''"));
-            }
-        }
-
-        private void RecordEvent(string eventString)
-        {
-            StatusReportingTextBox.Text = eventString;
-        }
-
-        private void RecordEvent(object sender, string eventString)
-        {
-            DependencyObject senderAsDO = sender as DependencyObject;
-
-            if (senderAsDO != null)
-            {
-                RecordEvent(AutomationProperties.GetAutomationId(senderAsDO) + " " + eventString);
-            }
-        }
-
-        private void OnTextBoxSelectAllClicked(object sender, object args)
-        {
-            TextBox1.SelectAll();
-        }
-
-        private void OnTextBlockSelectAllClicked(object sender, object args)
-        {
-            TextBlock1.SelectAll();
-        }
-
-        private void OnRichEditBoxSelectAllClicked(object sender, object args)
-        {
-#if !HAS_UNO
-			RichEditBox1.Document.Selection.Expand(Windows.UI.Text.TextRangeUnit.Story);
-#endif
+		if (ApiInformation.IsEnumNamedValuePresent("Windows.UI.Xaml.Controls.Primitives.FlyoutPlacementMode", "BottomEdgeAlignedLeft"))
+		{
+			TextControlContextFlyout.Placement = FlyoutPlacementMode.BottomEdgeAlignedLeft;
+		}
+		else
+		{
+			TextControlContextFlyout.Placement = FlyoutPlacementMode.Top;
 		}
 
-        private void OnRichTextBlockSelectAllClicked(object sender, object args)
-        {
-            RichTextBlock1.SelectAll();
-        }
-
-        private void OnTextBoxClearSelectionClicked(object sender, object args)
-        {
-            TextBox1.Select(0, 0);
-        }
-
-        private void OnTextBlockClearSelectionClicked(object sender, object args)
-        {
-            TextBlock1.Select(TextBlock1.ContentStart, TextBlock1.ContentStart);
-        }
-
-        private void OnRichEditBoxClearSelectionClicked(object sender, object args)
-        {
-#if !HAS_UNO
-			RichEditBox1.Document.Selection.Collapse(true);
-#endif
+		if (ApiInformation.IsEnumNamedValuePresent("Windows.UI.Xaml.Controls.Primitives.FlyoutPlacementMode", "TopEdgeAlignedLeft"))
+		{
+			TextControlSelectionFlyout.Placement = FlyoutPlacementMode.TopEdgeAlignedLeft;
+		}
+		else
+		{
+			TextControlSelectionFlyout.Placement = FlyoutPlacementMode.Top;
 		}
 
-        private void OnTextBoxFillWithTextClicked(object sender, object args)
-        {
-            TextBox1.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-        }
+		if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.UIElement", "ContextFlyout"))
+		{
+			TextBox1.ContextFlyout = TextControlContextFlyout;
+			TextBlock1.ContextFlyout = TextControlContextFlyout;
+#if !HAS_UNO
+			RichEditBox1.ContextFlyout = TextControlContextFlyout;
+#endif
+			RichTextBlock1.ContextFlyout = TextControlContextFlyout;
+			PasswordBox1.ContextFlyout = TextControlContextFlyout;
+		}
 
-        private void OnRichEditBoxFillWithTextClicked(object sender, object args)
-        {
+		try
+		{
+			if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.TextBox", "SelectionFlyout"))
+			{
+				TextBox1.SelectionFlyout = TextControlSelectionFlyout;
+			}
+
+			if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.TextBlock", "SelectionFlyout"))
+			{
+				TextBlock1.SelectionFlyout = TextControlSelectionFlyout;
+			}
+
+			if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.RichEditBox", "SelectionFlyout"))
+			{
+#if !HAS_UNO
+				RichEditBox1.SelectionFlyout = TextControlSelectionFlyout;
+#endif
+			}
+
+			if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.RichTextBlock", "SelectionFlyout"))
+			{
+				RichTextBlock1.SelectionFlyout = TextControlSelectionFlyout;
+			}
+
+			if (ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.PasswordBox", "SelectionFlyout"))
+			{
+				PasswordBox1.SelectionFlyout = TextControlSelectionFlyout;
+			}
+		}
+		catch (InvalidCastException)
+		{
+			// RS5 interfaces can change before release, so we need to make sure we don't crash if they do.
+		}
+	}
+
+	private async void OnClipboardContentChanged(object sender, object e)
+	{
+		var clipboardContent = Clipboard.GetContent();
+
+		if (clipboardContent.Contains(StandardDataFormats.Rtf))
+		{
+			RecordEvent(string.Format("Clipboard content changed to '{0}'", await Clipboard.GetContent().GetRtfAsync()));
+		}
+		else if (clipboardContent.Contains(StandardDataFormats.Text))
+		{
+			RecordEvent(string.Format("Clipboard content changed to '{0}'", await Clipboard.GetContent().GetTextAsync()));
+		}
+		else
+		{
+			RecordEvent(string.Format("Clipboard content changed to ''"));
+		}
+	}
+
+	private void RecordEvent(string eventString)
+	{
+		StatusReportingTextBox.Text = eventString;
+	}
+
+	private void RecordEvent(object sender, string eventString)
+	{
+		DependencyObject senderAsDO = sender as DependencyObject;
+
+		if (senderAsDO != null)
+		{
+			RecordEvent(AutomationProperties.GetAutomationId(senderAsDO) + " " + eventString);
+		}
+	}
+
+	private void OnTextBoxSelectAllClicked(object sender, object args)
+	{
+		TextBox1.SelectAll();
+	}
+
+	private void OnTextBlockSelectAllClicked(object sender, object args)
+	{
+		TextBlock1.SelectAll();
+	}
+
+	private void OnRichEditBoxSelectAllClicked(object sender, object args)
+	{
+#if !HAS_UNO
+		RichEditBox1.Document.Selection.Expand(Windows.UI.Text.TextRangeUnit.Story);
+#endif
+	}
+
+	private void OnRichTextBlockSelectAllClicked(object sender, object args)
+	{
+		RichTextBlock1.SelectAll();
+	}
+
+	private void OnTextBoxClearSelectionClicked(object sender, object args)
+	{
+		TextBox1.Select(0, 0);
+	}
+
+	private void OnTextBlockClearSelectionClicked(object sender, object args)
+	{
+		TextBlock1.Select(TextBlock1.ContentStart, TextBlock1.ContentStart);
+	}
+
+	private void OnRichEditBoxClearSelectionClicked(object sender, object args)
+	{
+#if !HAS_UNO
+		RichEditBox1.Document.Selection.Collapse(true);
+#endif
+	}
+
+	private void OnTextBoxFillWithTextClicked(object sender, object args)
+	{
+		TextBox1.Text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+	}
+
+	private void OnRichEditBoxFillWithTextClicked(object sender, object args)
+	{
 #if !HAS_UNO
             RichEditBox1.Document.SetText(TextSetOptions.None, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 #endif
-		}
+	}
 
-        private void OnRichTextBlockClearSelectionClicked(object sender, object args)
-        {
-            RichTextBlock1.Select(RichTextBlock1.ContentStart, RichTextBlock1.ContentStart);
-        }
+	private void OnRichTextBlockClearSelectionClicked(object sender, object args)
+	{
+		RichTextBlock1.Select(RichTextBlock1.ContentStart, RichTextBlock1.ContentStart);
+	}
 
-        private void OnShowTextControlFlyoutOnTextBoxClicked(object sender, object args)
-        {
-            ShowTextControlContextFlyoutTransient(TextBox1);
-        }
+	private void OnShowTextControlFlyoutOnTextBoxClicked(object sender, object args)
+	{
+		ShowTextControlContextFlyoutTransient(TextBox1);
+	}
 
-        private void OnShowTextControlFlyoutOnTextBlockClicked(object sender, object args)
-        {
-            ShowTextControlContextFlyoutTransient(TextBlock1);
-        }
+	private void OnShowTextControlFlyoutOnTextBlockClicked(object sender, object args)
+	{
+		ShowTextControlContextFlyoutTransient(TextBlock1);
+	}
 
-        private void OnShowTextControlFlyoutOnRichEditBoxClicked(object sender, object args)
-        {
+	private void OnShowTextControlFlyoutOnRichEditBoxClicked(object sender, object args)
+	{
 #if !HAS_UNO
-			ShowTextControlContextFlyoutTransient(RichEditBox1);
+		ShowTextControlContextFlyoutTransient(RichEditBox1);
 #endif
-		}
+	}
 
-        private void OnShowTextControlFlyoutOnRichTextBlockClicked(object sender, object args)
-        {
-            ShowTextControlContextFlyoutTransient(RichTextBlock1);
-        }
+	private void OnShowTextControlFlyoutOnRichTextBlockClicked(object sender, object args)
+	{
+		ShowTextControlContextFlyoutTransient(RichTextBlock1);
+	}
 
-        private void OnShowTextControlFlyoutOnPasswordBoxClicked(object sender, object args)
-        {
-            ShowTextControlContextFlyoutTransient(PasswordBox1);
-        }
+	private void OnShowTextControlFlyoutOnPasswordBoxClicked(object sender, object args)
+	{
+		ShowTextControlContextFlyoutTransient(PasswordBox1);
+	}
 
-        private void OnShowStandardTextControlFlyoutOnTextBoxClicked(object sender, object args)
-        {
-            ShowTextControlContextFlyoutStandard(TextBox1);
-        }
+	private void OnShowStandardTextControlFlyoutOnTextBoxClicked(object sender, object args)
+	{
+		ShowTextControlContextFlyoutStandard(TextBox1);
+	}
 
-        private void OnShowStandardTextControlFlyoutOnTextBlockClicked(object sender, object args)
-        {
-            ShowTextControlContextFlyoutStandard(TextBlock1);
-        }
+	private void OnShowStandardTextControlFlyoutOnTextBlockClicked(object sender, object args)
+	{
+		ShowTextControlContextFlyoutStandard(TextBlock1);
+	}
 
-        private void OnShowStandardTextControlFlyoutOnRichEditBoxClicked(object sender, object args)
-        {
+	private void OnShowStandardTextControlFlyoutOnRichEditBoxClicked(object sender, object args)
+	{
 #if !HAS_UNO
-			ShowTextControlContextFlyoutStandard(RichEditBox1);
+		ShowTextControlContextFlyoutStandard(RichEditBox1);
 #endif
+	}
+
+	private void OnShowStandardTextControlFlyoutOnRichTextBlockClicked(object sender, object args)
+	{
+		ShowTextControlContextFlyoutStandard(RichTextBlock1);
+	}
+
+	private void OnShowStandardTextControlFlyoutOnPasswordBoxClicked(object sender, object args)
+	{
+		ShowTextControlContextFlyoutStandard(PasswordBox1);
+	}
+
+	private void ShowTextControlContextFlyoutTransient(FrameworkElement targetElement)
+	{
+		if (PlatformConfiguration.IsOsVersionGreaterThanOrEqual(OSVersion.Redstone5))
+		{
+			TextControlContextFlyout.ShowAt(targetElement, new FlyoutShowOptions { ShowMode = FlyoutShowMode.Transient });
 		}
+		else
+		{
+			TextControlContextFlyout.ShowAt(targetElement);
+		}
+	}
 
-        private void OnShowStandardTextControlFlyoutOnRichTextBlockClicked(object sender, object args)
-        {
-            ShowTextControlContextFlyoutStandard(RichTextBlock1);
-        }
+	private void ShowTextControlContextFlyoutStandard(FrameworkElement targetElement)
+	{
+		if (PlatformConfiguration.IsOsVersionGreaterThanOrEqual(OSVersion.Redstone5))
+		{
+			TextControlSelectionFlyout.ShowAt(targetElement, new FlyoutShowOptions { ShowMode = FlyoutShowMode.Standard });
+		}
+		else
+		{
+			TextControlSelectionFlyout.ShowAt(targetElement);
+		}
+	}
 
-        private void OnShowStandardTextControlFlyoutOnPasswordBoxClicked(object sender, object args)
-        {
-            ShowTextControlContextFlyoutStandard(PasswordBox1);
-        }
+	private void OnClearClipboardContentsClicked(object sender, object args)
+	{
+		Clipboard.Clear();
+	}
 
-        private void ShowTextControlContextFlyoutTransient(FrameworkElement targetElement)
-        {
-            if (PlatformConfiguration.IsOsVersionGreaterThanOrEqual(OSVersion.Redstone5))
-            {
-                TextControlContextFlyout.ShowAt(targetElement, new FlyoutShowOptions { ShowMode = FlyoutShowMode.Transient });
-            }
-            else
-            {
-                TextControlContextFlyout.ShowAt(targetElement);
-            }
-        }
+	private void OnSetClipboardContentsClicked(object sender, object args)
+	{
+		DataPackage dataPackage = new DataPackage();
 
-        private void ShowTextControlContextFlyoutStandard(FrameworkElement targetElement)
-        {
-            if (PlatformConfiguration.IsOsVersionGreaterThanOrEqual(OSVersion.Redstone5))
-            {
-                TextControlSelectionFlyout.ShowAt(targetElement, new FlyoutShowOptions { ShowMode = FlyoutShowMode.Standard });
-            }
-            else
-            {
-                TextControlSelectionFlyout.ShowAt(targetElement);
-            }
-        }
+		dataPackage.RequestedOperation = DataPackageOperation.Copy;
+		dataPackage.SetText("Automatically set clipboard text");
 
-        private void OnClearClipboardContentsClicked(object sender, object args)
-        {
-            Clipboard.Clear();
-        }
+		Clipboard.SetContent(dataPackage);
+	}
 
-        private void OnSetClipboardContentsClicked(object sender, object args)
-        {
-            DataPackage dataPackage = new DataPackage();
-
-            dataPackage.RequestedOperation = DataPackageOperation.Copy;
-            dataPackage.SetText("Automatically set clipboard text");
-
-            Clipboard.SetContent(dataPackage);
-        }
-
-        private void OnGetRichEditBoxRtfContentClicked(object sender, object args)
-        {
+	private void OnGetRichEditBoxRtfContentClicked(object sender, object args)
+	{
 #if !HAS_UNO
-			string rtfContent;
+		string rtfContent;
             RichEditBox1.Document.GetText(TextGetOptions.FormatRtf, out rtfContent);
-			StatusReportingTextBox.Text = rtfContent;
+		StatusReportingTextBox.Text = rtfContent;
 #endif
-		}
-    }
+	}
 }
--- src/Uno.UI/UI/Xaml/Input/StandardUICommand/TextCommandBarFlyout.h.mux.cs
+++ src/Uno.UI/UI/Xaml/Input/StandardUICommand/TextCommandBarFlyout.h.mux.cs
@@ -9,18 +9,18 @@ using Microsoft.UI.Xaml.Controls;
 namespace Microsoft.UI.Xaml.Controls;
 
 [Flags]
-internal enum  TextControlButtons
+internal enum TextControlButtons
 {
 	None = 0x0000,
-    Cut = 0x0001,
-    Copy = 0x0002,
-    Paste = 0x0004,
-    Bold = 0x0008,
-    Italic = 0x0010,
-    Underline = 0x0020,
-    Undo = 0x0040,
-    Redo = 0x0080,
-    SelectAll = 0x0100,
+	Cut = 0x0001,
+	Copy = 0x0002,
+	Paste = 0x0004,
+	Bold = 0x0008,
+	Italic = 0x0010,
+	Underline = 0x0020,
+	Undo = 0x0040,
+	Redo = 0x0080,
+	SelectAll = 0x0100,
 }
 
 partial class TextCommandBarFlyout
