483824886c9d602708ff1b33f79ef4764192849d
Martin Zikmund <martin@zikmund.dev>
2024-04-05 14:38:33 +0200

    refactor: Small improvements to animated bitmap sample

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ImageTests/BitmapImage_IsPlaying.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ImageTests/BitmapImage_IsPlaying.xaml.cs
@@ -1,25 +1,38 @@
-﻿using Microsoft.UI.Xaml;
+﻿using System.ComponentModel;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Uno.UI.Samples.Controls;
 
 namespace UITests.Windows_UI_Xaml_Controls.ImageTests
 {
 	[Sample("Image", IgnoreInSnapshotTests = true)]
-	public sealed partial class BitmapImage_IsPlaying : Page
+	public sealed partial class BitmapImage_IsPlaying : Page, INotifyPropertyChanged
 	{
 		public BitmapImage_IsPlaying()
 		{
 			this.InitializeComponent();
+			this.Loaded += BitmapImage_IsPlaying_Loaded;
+			image.ImageOpened += (s, e) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsAnimatedBitmap)));
 		}
+
+		private void BitmapImage_IsPlaying_Loaded(object sender, RoutedEventArgs e) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsAnimatedBitmap)));
+
+		public bool IsPlaying => image.IsPlaying;
+
+		public bool IsAnimatedBitmap => image.IsAnimatedBitmap;
+
+		public event PropertyChangedEventHandler PropertyChanged;
 
 		private void PlayClicked(object sender, RoutedEventArgs e)
 		{
 			image.Play();
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsPlaying)));
 		}
 
 		private void StopClicked(object sender, RoutedEventArgs e)
 		{
 			image.Stop();
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsPlaying)));
 		}
 	}
 }
