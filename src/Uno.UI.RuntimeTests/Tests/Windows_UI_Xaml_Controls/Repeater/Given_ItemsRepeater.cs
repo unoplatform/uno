680e6c8ed32c17f2bb9d13b2531c54d975d0809d
David <github.site@torick.net>
2024-04-12 10:30:09 -0400

    Revert "Merge pull request #15946 from unoplatform/dev/dr/irVirIssue"
    This reverts commit 15a8de8acd7404ad3534befd532ae1a82688e0fe, reversing
    changes made to 2569bb1fb25c480070f715a9baa75c274c50e152.
    
    (cherry picked from commit 9cffe012d068e4b8b894a4d3e313bdbce2137c52)
    
    # Conflicts:
    #	src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Repeater/Given_ItemsRepeater.cs

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Repeater/Given_ItemsRepeater.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Repeater/Given_ItemsRepeater.cs
@@ -430,16 +430,9 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls.Repeater
 			sut.Materialized.Should().Be(0);
 		}
 
-
-		[TestMethod]
-		[RunsOnUIThread]
-#if __MACOS__
-		[Ignore("Currently fails on macOS, part of #9282 epic")]
-#elif !__SKIA__
-		[Ignore("Fails due to async native scrolling.")]
-#endif
-		public async Task When_ItemSignificantlyHigher_Then_VirtualizeProperly()
+		private record SUT(Border Root, ScrollViewer Scroller, ItemsRepeater Repeater, ObservableCollection<string> Source)
 		{
+<<<<<<< HEAD
 			var sut = SUT.Create(
 				new ObservableCollection<MyItem>
 				{
@@ -496,18 +489,13 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls.Repeater
 		private static class SUT
 		{
 			public static SUT<T> Create<T>(ObservableCollection<T> source, DataTemplate? itemTemplate = null, Size? viewport = default)
+=======
+			public static SUT Create(int itemsCount = 3, Size? viewport = default)
+>>>>>>> 9cffe012d0 (Revert "Merge pull request #15946 from unoplatform/dev/dr/irVirIssue")
 			{
-				itemTemplate ??= new DataTemplate(() => new Border
-				{
-					Width = 100,
-					Height = 100,
-					Background = new SolidColorBrush(Colors.DeepSkyBlue),
-					Margin = new Thickness(10),
-					Child = new TextBlock().Apply(tb => tb.SetBinding(TextBlock.TextProperty, new Binding()))
-				});
-
 				var repeater = default(ItemsRepeater);
 				var scroller = default(ScrollViewer);
+				var source = new ObservableCollection<string>(Enumerable.Range(0, itemsCount).Select(i => $"Item #{i}"));
 				var root = new Border
 				{
 					BorderThickness = new Thickness(5),
@@ -518,7 +506,14 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls.Repeater
 						{
 							ItemsSource = source,
 							Layout = new StackLayout(),
-							ItemTemplate = itemTemplate
+							ItemTemplate = new DataTemplate(() => new Border
+							{
+								Width = 100,
+								Height = 100,
+								Background = new SolidColorBrush(Colors.DeepSkyBlue),
+								Margin = new Thickness(10),
+								Child = new TextBlock().Apply(tb => tb.SetBinding(TextBlock.TextProperty, new Binding()))
+							})
 						})
 					})
 				};
@@ -532,15 +527,9 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls.Repeater
 				return new(root, scroller, repeater, source);
 			}
 
-			public static SUT<string> Create(int itemsCount = 3, Size? viewport = default)
-				=> Create(new ObservableCollection<string>(Enumerable.Range(0, itemsCount).Select(i => $"Item #{i}")), viewport: viewport);
-		}
-
-		private record SUT<T>(Border Root, ScrollViewer Scroller, ItemsRepeater Repeater, ObservableCollection<T> Source)
-		{
 			public int Materialized => Repeater.Children.Count(elt => elt.ActualOffset.X >= 0);
 
-			public IEnumerable<T> MaterializedItems => Repeater.Children.Where(elt => elt.ActualOffset.X >= 0).Select(elt => (T)elt.DataContext);
+			public IEnumerable<string> MaterializedItems => Repeater.Children.Where(elt => elt.ActualOffset.X >= 0).Select(elt => elt.DataContext?.ToString());
 
 			public async ValueTask Load()
 			{
