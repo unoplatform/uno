f58ab6a850e48d99c679b125f999dd146c8c5da6
Youssef Victor <youssefvictor00@gmail.com>
2024-04-15 13:34:43 +0200

    chore: Update generated code
    (cherry picked from commit 85a0c62ec38fbb2aadff234c48f2544c3dd66deb)

--- src/SourceGenerators/Uno.UI.SourceGenerators.Tests/XamlCodeGeneratorTests/Out/TTW/XamlCodeGenerator_MainPage_d6cd66944958ced0c513e0a04797b51d.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators.Tests/XamlCodeGeneratorTests/Out/TTW/XamlCodeGenerator_MainPage_d6cd66944958ced0c513e0a04797b51d.cs
@@ -146,6 +146,7 @@ namespace TestRepro
 			void Update();
 			void UpdateResources();
 			void StopTracking();
+			void NotifyXLoad(string name);
 		}
 		#pragma warning disable 0169 //  Suppress unused field warning in case Bindings is not used.
 		private IMainPage_Bindings Bindings;
@@ -162,6 +163,9 @@ namespace TestRepro
 			public MainPage_Bindings(global::TestRepro.MainPage owner)
 			{
 				Owner = owner;
+			}
+			void IMainPage_Bindings.NotifyXLoad(string name)
+			{
 			}
 			void IMainPage_Bindings.Initialize()
 			{
