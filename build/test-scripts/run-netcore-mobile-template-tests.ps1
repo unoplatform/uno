6aae92a7dd8e21efc73d1b0d9180a1739c828d86
Dan Siegel <me@dansiegel.net>
2024-04-15 10:20:23 -0600

    ci: adding regression testing for app with library reference
    (cherry picked from commit 163b7d54a186d4a2de2c9f55f3e21763397243d0)
    
    # Conflicts:
    #	build/test-scripts/run-netcore-mobile-template-tests.ps1

--- build/test-scripts/run-netcore-mobile-template-tests.ps1
+++ build/test-scripts/run-netcore-mobile-template-tests.ps1
@@ -181,7 +181,11 @@ $projects =
     @("5.2/uno52blank/uno52blank/uno52blank.csproj", @("-f", "net8.0-desktop"), $true),
 
     # Default mode for the template is WindowsAppSDKSelfContained=true, which requires specifying a target platform.
+<<<<<<< HEAD
     @("5.2/uno52blank/uno52blank/uno52blank.csproj", @("-p:Platform=x86" , "-p:TargetFramework=net8.0-windows10.0.19041"), $false)
+=======
+    @("5.2/uno52blank/uno52blank/uno52blank.csproj", @("-p:Platform=x86" , "-p:TargetFramework=net8.0-windows10.0.19041"), $false, $false),
+>>>>>>> 163b7d54a1 (ci: adding regression testing for app with library reference)
 
     #
     # 5.2 Uno Lib
@@ -207,7 +211,22 @@ $projects =
     @("5.2/uno52SingleProjectLib/uno52SingleProjectLib.csproj", @("-f", "net8.0-desktop"), $true),
 
     # Default mode for the template is WindowsAppSDKSelfContained=true, which requires specifying a target platform.
+<<<<<<< HEAD
     @("5.2/uno52SingleProjectLib/uno52SingleProjectLib.csproj", @("-p:Platform=x86" , "-p:TargetFramework=net8.0-windows10.0.19041"), $false)
+=======
+    @("5.2/uno52SingleProjectLib/uno52SingleProjectLib.csproj", @("-p:Platform=x86" , "-p:TargetFramework=net8.0-windows10.0.19041"), $false, $false),
+
+    # 5.2 Uno App with Library reference
+    @("5.2/uno52AppWithLib/uno52AppWithLib/uno52AppWithLib.csproj", @("-f", "net8.0"), $true, $true),
+    @("5.2/uno52AppWithLib/uno52AppWithLib/uno52AppWithLib.csproj", @("-f", "net8.0-browserwasm"), $true, $true),
+    @("5.2/uno52AppWithLib/uno52AppWithLib/uno52AppWithLib.csproj", @("-f", "net8.0-ios"), $true, $true),
+    @("5.2/uno52AppWithLib/uno52AppWithLib/uno52AppWithLib.csproj", @("-f", "net8.0-android"), $true, $true),
+    @("5.2/uno52AppWithLib/uno52AppWithLib/uno52AppWithLib.csproj", @("-f", "net8.0-maccatalyst"), $true, $true),
+    @("5.2/uno52AppWithLib/uno52AppWithLib/uno52AppWithLib.csproj", @("-f", "net8.0-desktop"), $true, $true),
+
+    # Default mode for the template is WindowsAppSDKSelfContained=true, which requires specifying a target platform.
+    @("5.2/uno52AppWithLib/uno52AppWithLib/uno52AppWithLib.csproj", @("-p:Platform=x86" , "-p:TargetFramework=net8.0-windows10.0.19041"), $false, $false)
+>>>>>>> 163b7d54a1 (ci: adding regression testing for app with library reference)
 
     ## Note for contributors
     ##
