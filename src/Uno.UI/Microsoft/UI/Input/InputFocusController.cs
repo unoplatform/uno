7b910a8059344fe95b5e9855ad0870c4d3d2ac80
Martin Zikmund <martin@zikmund.dev>
2024-04-16 13:21:14 +0200

    chore: Members are unused

--- src/Uno.UI/Microsoft/UI/Input/InputFocusController.cs
+++ src/Uno.UI/Microsoft/UI/Input/InputFocusController.cs
@@ -15,11 +15,13 @@ internal
 	[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 	public FocusNavigationResult DepartFocus(FocusNavigationRequest request) => FocusNavigationResult.NotMoved;
 
+#pragma warning disable CS0067 // Unused members
 	[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 	public event TypedEventHandler<InputFocusController, FocusChangedEventArgs> GotFocus;
 
 	[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 	public event TypedEventHandler<InputFocusController, FocusChangedEventArgs> LostFocus;
+#pragma warning restore CS0067
 
 	[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 	public bool HasFocus => false;
