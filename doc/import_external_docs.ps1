54f603c723e630f66e743bc7bca791bd65839d05
Agnès ZITTE <16295702+agneszitte@users.noreply.github.com>
2024-04-16 12:00:18 -0400

    docs: Update documentation for Uno.Themes, Figma and Workshops with latest changes

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -8,15 +8,15 @@ Set-PSDebug -Trace 1
 $external_docs = @{
     # use either commit, or branch name to use its latest commit
     "uno.wasm.bootstrap" = "9b559fc9a8b5e939fd99fa119a243e760ae3770d" #latest main commit
-    "uno.themes"         = "92e9c4dfab6a161f795dddfc1aa9f23844556d5f" #latest release branch commit
+    "uno.themes"         = "98eb4b4aebaf19ad1ce1c67220d7a7beaa772959" #latest release branch commit
     "uno.toolkit.ui"     = "c7af29b7f352fbed1c83bd119e9d10d14736cf26" #latest release branch commit
     "uno.check"          = "4a7dd7290daf0aabfbb8efabcd2b067898b7f45e" #latest main commit
     "uno.xamlmerge.task" = "21f02c98702b875a9942047ca042e41810b6fe56" #latest main commit
-    "figma-docs"         = "782c6869b9c48842d9e82cab87fabe8812ac6fb7" #latest main commit
+    "figma-docs"         = "08aea8467414390673e8f4a917e29bf07ef96680" #latest main commit
     "uno.resizetizer"    = "55c9bac2b19c0bf39f8794684396db3cc9a4e9d0" #latest main commit
     "uno.uitest"         = "9669fd2783187d06c36dd6a717c1b9f08d1fa29c" #latest master commit
     "uno.extensions"     = "71b3bcc76bc599996dfa3329d4bb311a1aeba330" #latest release branch commit
-    "workshops"          = "095fa2a6269e75075e9b568a5d2d82085ff09b50" #latest master commit
+    "workshops"          = "bb4a34a533b1cd7617cc7d1a3911dbd4fb23c70a" #latest master commit
 }
 
 $uno_git_url = "https://github.com/unoplatform/"
