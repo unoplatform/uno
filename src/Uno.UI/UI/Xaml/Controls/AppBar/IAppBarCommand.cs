95f46c5fa17aec3b3122c16f3408b791c41deb60
Martin Zikmund <martin@zikmund.dev>
2024-04-21 22:38:34 +0200

    chore: Clean up unsupported code for SystemBackdrop

---
+++ src/Uno.UI/UI/Xaml/Controls/AppBar/IAppBarCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Uno.UI.Xaml.Controls;
+
+internal interface IAppBarCommand
+{
+	Visibility Visibility { get; }
+
+	string Label { get; }
+
+	CommandBarLabelPosition LabelPosition { get; }
+}
