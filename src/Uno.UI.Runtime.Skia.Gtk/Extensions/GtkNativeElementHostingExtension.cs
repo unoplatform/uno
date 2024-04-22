4356923240b11b6fce46bfadf85d42e5dab79523
Ramez Ragaa <ramezragaa@proton.me>
2024-04-22 13:41:07 +0200

    chore: formatting

--- src/Uno.UI.Runtime.Skia.Gtk/Extensions/GtkNativeElementHostingExtension.cs
+++ src/Uno.UI.Runtime.Skia.Gtk/Extensions/GtkNativeElementHostingExtension.cs
@@ -103,92 +103,92 @@ internal class GtkNativeElementHostingExtension : ContentPresenter.INativeElemen
 	}
 
 	public void ArrangeNativeElement(XamlRoot owner, object content, Rect arrangeRect, Rect clipRect)
+	{
+		if (content is Widget widget
+			&& GetOverlayLayer(owner) is { } overlay)
 		{
-			if (content is Widget widget
-				&& GetOverlayLayer(owner) is { } overlay)
+			if (!IsNativeElementAttached(owner, content))
 			{
-				if (!IsNativeElementAttached(owner, content))
-				{
-					// We do this not on attaching, but on the first arrange, to prevent the overlay from being
-					// misplaced for a split-second before the arrange occurs
-					overlay.Put(widget, 0, 0);
-				}
-
-				if (!widget.Visible)
-				{
-					return;
-				}
-
-				if (this.Log().IsEnabled(LogLevel.Trace))
-				{
-					this.Log().Trace($"ArrangeNativeElement({owner}, {arrangeRect})");
-				}
-
-				var scaleAdjustment = owner.FractionalScaleAdjustment;
-				var rect = new Gdk.Rectangle(
-					(int)(arrangeRect.X * scaleAdjustment),
-					(int)(arrangeRect.Y * scaleAdjustment),
-					(int)(arrangeRect.Width * scaleAdjustment),
-					(int)(arrangeRect.Height * scaleAdjustment));
-
-				if (NativeRenderDisposables.Remove(widget, out var disposable))
-				{
-					disposable.Dispose();
-				}
-
-				// This seems to be the only way to get clipping to work on the initial loading.
-				// It appears that there's something else moving/changing the widget, which causes the clipping
-				// to reset. Also, if the window loses focus and is then refocused, the clipping will be reset unless
-				// a secondary callback to reapply the clipping is called. Initially, the implementation used AddTickCallback,
-				// but it seems to be hogging up the Glib queue too much. This implementation still makes the rendering slow if
-				// there are many gtk elements being rearranged.
-				// TODO: find out what's going on
-				var callback = (object? _, object? _) =>
-				{
-					TimeoutHandler timeoutHandler = () =>
-					{
-						widget.SizeAllocate(rect);
-						if (widget.Visible) // gtk screams if you attempt to SetClip when the widget isn't visible.
-						{
-							widget.SetClip(new Gdk.Rectangle(
-								rect.X + (int)(clipRect.X * scaleAdjustment),
-								rect.Y + (int)(clipRect.Y * scaleAdjustment),
-								(int)(clipRect.Width * scaleAdjustment),
-								(int)(clipRect.Height * scaleAdjustment)));
-						}
-						if (widget.Parent is { }) // i.e. we haven't detached the widget yet
-						{
-							overlay.Move(widget, rect.X, rect.Y);
-						}
-						return false;
-					};
-					GLib.Timeout.Add(0, timeoutHandler, Priority.High); // this gets the positioning right, but not the clipping
-					GLib.Timeout.Add(0, timeoutHandler, Priority.Low); // this fixes the clipping but has to be Low so that it's delayed enough to come after whatever is breaking the clipping
-				};
-
-				callback(null, null);
-
-				var onActivated = new WindowActivatedEventHandler(callback);
-				var onSizeChanged = new WindowSizeChangedEventHandler(callback);
-				if (owner.HostWindow is { } window)
-				{
-					window.Activated += onActivated;
-					window.SizeChanged += onSizeChanged;
-					NativeRenderDisposables[widget] = new DisposableAction(() =>
-					{
-						window.Activated -= onActivated;
-						window.SizeChanged -= onSizeChanged;
-					});
-				}
+				// We do this not on attaching, but on the first arrange, to prevent the overlay from being
+				// misplaced for a split-second before the arrange occurs
+				overlay.Put(widget, 0, 0);
 			}
-			else
+
+			if (!widget.Visible)
 			{
-				if (this.Log().IsEnabled(LogLevel.Debug))
+				return;
+			}
+
+			if (this.Log().IsEnabled(LogLevel.Trace))
+			{
+				this.Log().Trace($"ArrangeNativeElement({owner}, {arrangeRect})");
+			}
+
+			var scaleAdjustment = owner.FractionalScaleAdjustment;
+			var rect = new Gdk.Rectangle(
+				(int)(arrangeRect.X * scaleAdjustment),
+				(int)(arrangeRect.Y * scaleAdjustment),
+				(int)(arrangeRect.Width * scaleAdjustment),
+				(int)(arrangeRect.Height * scaleAdjustment));
+
+			if (NativeRenderDisposables.Remove(widget, out var disposable))
+			{
+				disposable.Dispose();
+			}
+
+			// This seems to be the only way to get clipping to work on the initial loading.
+			// It appears that there's something else moving/changing the widget, which causes the clipping
+			// to reset. Also, if the window loses focus and is then refocused, the clipping will be reset unless
+			// a secondary callback to reapply the clipping is called. Initially, the implementation used AddTickCallback,
+			// but it seems to be hogging up the Glib queue too much. This implementation still makes the rendering slow if
+			// there are many gtk elements being rearranged.
+			// TODO: find out what's going on
+			var callback = (object? _, object? _) =>
+			{
+				TimeoutHandler timeoutHandler = () =>
 				{
-					this.Log().Debug($"Unable to arrange native element {content} in {owner}.");
-				}
+					widget.SizeAllocate(rect);
+					if (widget.Visible) // gtk screams if you attempt to SetClip when the widget isn't visible.
+					{
+						widget.SetClip(new Gdk.Rectangle(
+							rect.X + (int)(clipRect.X * scaleAdjustment),
+							rect.Y + (int)(clipRect.Y * scaleAdjustment),
+							(int)(clipRect.Width * scaleAdjustment),
+							(int)(clipRect.Height * scaleAdjustment)));
+					}
+					if (widget.Parent is { }) // i.e. we haven't detached the widget yet
+					{
+						overlay.Move(widget, rect.X, rect.Y);
+					}
+					return false;
+				};
+				GLib.Timeout.Add(0, timeoutHandler, Priority.High); // this gets the positioning right, but not the clipping
+				GLib.Timeout.Add(0, timeoutHandler, Priority.Low); // this fixes the clipping but has to be Low so that it's delayed enough to come after whatever is breaking the clipping
+			};
+
+			callback(null, null);
+
+			var onActivated = new WindowActivatedEventHandler(callback);
+			var onSizeChanged = new WindowSizeChangedEventHandler(callback);
+			if (owner.HostWindow is { } window)
+			{
+				window.Activated += onActivated;
+				window.SizeChanged += onSizeChanged;
+				NativeRenderDisposables[widget] = new DisposableAction(() =>
+				{
+					window.Activated -= onActivated;
+					window.SizeChanged -= onSizeChanged;
+				});
 			}
 		}
+		else
+		{
+			if (this.Log().IsEnabled(LogLevel.Debug))
+			{
+				this.Log().Debug($"Unable to arrange native element {content} in {owner}.");
+			}
+		}
+	}
 
 	public Size MeasureNativeElement(XamlRoot owner, object content, Size childMeasuredSize, Size availableSize)
 	{
