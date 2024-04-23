a46294f277ff41c5122c9fba8bd8d5a8d8052ec1
Youssef Victor <youssefvictor00@gmail.com>
2024-04-23 15:34:50 +0200

    chore: Adjust for Android

--- src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/ItemsViewTests/LoggingContentControl.cs
+++ src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/ItemsViewTests/LoggingContentControl.cs
@@ -6,40 +6,38 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Windows.Foundation;
 
-using Debug = System.Diagnostics.Debug;
-
 namespace MUXControlsTestApp.Utilities;
 
 public sealed partial class LoggingContentControl : ContentControl
 {
 	public LoggingContentControl()
 	{
-		Debug.WriteLine("LoggingContentControl::LoggingContentControl[" + GetHashCode() + "]()");
+		global::System.Diagnostics.Debug.WriteLine("LoggingContentControl::LoggingContentControl[" + GetHashCode() + "]()");
 	}
 
 	~LoggingContentControl()
 	{
-		Debug.WriteLine("LoggingContentControl::~LoggingContentControl()");
+		global::System.Diagnostics.Debug.WriteLine("LoggingContentControl::~LoggingContentControl()");
 	}
 
 	protected override Size MeasureOverride(Size availableSize)
 	{
-		Debug.WriteLine("LoggingContentControl::MeasureOverride[" + GetHashCode() + "](availableSize: " + availableSize.Width + " x " + availableSize.Height + ")");
+		global::System.Diagnostics.Debug.WriteLine("LoggingContentControl::MeasureOverride[" + GetHashCode() + "](availableSize: " + availableSize.Width + " x " + availableSize.Height + ")");
 
 		Size returnedSize = base.MeasureOverride(availableSize);
 
-		Debug.WriteLine("LoggingContentControl::MeasureOverride[" + GetHashCode() + "] - returnedSize: " + returnedSize.Width + " x " + returnedSize.Height);
+		global::System.Diagnostics.Debug.WriteLine("LoggingContentControl::MeasureOverride[" + GetHashCode() + "] - returnedSize: " + returnedSize.Width + " x " + returnedSize.Height);
 
 		return returnedSize;
 	}
 
 	protected override Size ArrangeOverride(Size finalSize)
 	{
-		Debug.WriteLine("LoggingContentControl::ArrangeOverride[" + GetHashCode() + "](availableSize: " + finalSize.Width + " x " + finalSize.Height + ")");
+		global::System.Diagnostics.Debug.WriteLine("LoggingContentControl::ArrangeOverride[" + GetHashCode() + "](availableSize: " + finalSize.Width + " x " + finalSize.Height + ")");
 
 		Size returnedSize = base.ArrangeOverride(finalSize);
 
-		Debug.WriteLine("LoggingContentControl::ArrangeOverride[" + GetHashCode() + "] - returnedSize: " + returnedSize.Width + " x " + returnedSize.Height);
+		global::System.Diagnostics.Debug.WriteLine("LoggingContentControl::ArrangeOverride[" + GetHashCode() + "] - returnedSize: " + returnedSize.Width + " x " + returnedSize.Height);
 
 		return returnedSize;
 	}
