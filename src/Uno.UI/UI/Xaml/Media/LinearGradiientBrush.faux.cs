ac0d1ae9745a1ba3e8e3b8aeb3893c2cd4e06573
Martin Zikmund <martin@zikmund.dev>
2024-05-03 17:37:46 +0200

    chore: Adjust condition

--- src/Uno.UI/UI/Xaml/Media/LinearGradiientBrush.faux.cs
+++ src/Uno.UI/UI/Xaml/Media/LinearGradiientBrush.faux.cs
@@ -27,7 +27,7 @@ public partial class LinearGradientBrush
 #if __WASM__
 		return cornerRadius == CornerRadius.None;
 #elif __IOS__ || __MACOS__
-		return cornerRadius == CornerRadius.None && RelativeTransform == null;
+		return RelativeTransform == null;
 #else
 		return true;
 #endif
