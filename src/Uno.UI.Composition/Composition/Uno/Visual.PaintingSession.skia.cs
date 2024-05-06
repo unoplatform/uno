27fa0b054f7b3cab48c214385ced3d0354cbe04f
Ramez Ragaa <ramezragaa@proton.me>
2024-05-06 22:28:36 +0300

    chore: remove PaintingSessionWrapper

--- src/Uno.UI.Composition/Composition/Uno/Visual.PaintingSession.skia.cs
+++ src/Uno.UI.Composition/Composition/Uno/Visual.PaintingSession.skia.cs
@@ -6,20 +6,9 @@ namespace Microsoft.UI.Composition;
 
 public partial class Visual
 {
-	/// <remarks>
-	/// ONLY CONSTRUCT THIS IN A USING BLOCK. C# doesn't have RAII, so we can't enforce this.
-	/// The closest thing we can do is make this PaintingSessionWrapper type only visible to the Visual
-	/// and limit the areas where a new PaintingSessionWrapper needs to be created.
-	/// </remarks>
-	private readonly struct PaintingSessionWrapper(PaintingSession session, int saveCount) : IDisposable
-	{
-		public PaintingSession Session { get; } = session;
-		public void Dispose() => Session.Canvas.RestoreToCount(saveCount);
-	}
-
 	private interface IPrivateSessionFactory
 	{
-		PaintingSessionWrapper CreateInstance(Visual visual, SKSurface surface, SKCanvas canvas, in DrawingFilters filters, in Matrix4x4 rootTransform);
+		PaintingSession CreateInstance(Visual visual, SKSurface surface, SKCanvas canvas, in DrawingFilters filters, in Matrix4x4 rootTransform);
 	}
 
 	/// <summary>
@@ -29,20 +18,22 @@ public partial class Visual
 	/// Accessing Surface.Canvas is slow due to SkiaSharp interop.
 	/// Avoid using .Surface.Canvas and use .Canvas right away.
 	/// </remarks>
-	internal readonly struct PaintingSession
+	internal readonly struct PaintingSession: IDisposable
 	{
 		// This dance is done to make it so that only Visual can create a PaintingSession
 		public readonly struct SessionFactory : IPrivateSessionFactory
 		{
-			PaintingSessionWrapper IPrivateSessionFactory.CreateInstance(Visual visual, SKSurface surface, SKCanvas canvas, in DrawingFilters filters, in Matrix4x4 rootTransform)
+			PaintingSession IPrivateSessionFactory.CreateInstance(Visual visual, SKSurface surface, SKCanvas canvas, in DrawingFilters filters, in Matrix4x4 rootTransform)
 			{
-				var saveCount = visual.ShadowState is { } shadow ? canvas.SaveLayer(shadow.Paint) : canvas.Save();
-				return new PaintingSessionWrapper(new PaintingSession(surface, canvas, filters, rootTransform), saveCount);
+				return new PaintingSession(visual, surface, canvas, filters, rootTransform);
 			}
 		}
 
-		private PaintingSession(SKSurface surface, SKCanvas canvas, in DrawingFilters filters, in Matrix4x4 rootTransform)
+		private readonly int _saveCount;
+
+		private PaintingSession(Visual visual, SKSurface surface, SKCanvas canvas, in DrawingFilters filters, in Matrix4x4 rootTransform)
 		{
+			_saveCount = visual.ShadowState is { } shadow ? canvas.SaveLayer(shadow.Paint) : canvas.Save();;
 			Surface = surface;
 			Canvas = canvas;
 			Filters = filters;
@@ -55,5 +46,7 @@ public partial class Visual
 
 		/// <summary>The transform matrix to the root visual of this drawing session (which isn't necessarily the identity matrix due to scaling (DPI) and/or RenderTargetBitmap.</summary>
 		public Matrix4x4 RootTransform { get; }
+
+		public void Dispose() => Canvas.RestoreToCount(_saveCount);
 	}
 }
