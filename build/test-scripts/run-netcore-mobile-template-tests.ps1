f8194a48740f6fcb3fc682365083030998b81baa
Dan Siegel <me@dansiegel.net>
2024-05-09 10:55:29 -0600

    ci: add validation tests

--- build/test-scripts/run-netcore-mobile-template-tests.ps1
+++ build/test-scripts/run-netcore-mobile-template-tests.ps1
@@ -248,6 +248,11 @@ $projects =
     @("5.2/uno52Lib/uno52Lib.csproj", @("-f", "net8.0-maccatalyst"), $true, $true),
     @("5.2/uno52Lib/uno52Lib.csproj", @("-f", "net8.0-desktop"), $true, $true),
 
+     #
+    # 5.2 Uno NuGet Lib
+    #
+    @("5.2/uno52Lib/uno52Lib.csproj", @(), $true, $true),
+
     # Default mode for the template is WindowsAppSDKSelfContained=true, which requires specifying a target platform.
     @("5.2/uno52Lib/uno52Lib.csproj", @("-p:Platform=x86" , "-p:TargetFramework=net8.0-windows10.0.19041"), $false, $true),
 
