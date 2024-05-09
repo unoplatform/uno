c3f1b0e66b3c1b0454d0e2a6a4e2e3d38c3ac506
Dan Siegel <me@dansiegel.net>
2024-05-09 08:28:47 -0600

    chore: refactor Resizetizer includes
    (cherry picked from commit 58478f413cfd3efde1943def34b2c37275d770e7)

--- src/Uno.Sdk/Models/PackageReference.cs
+++ src/Uno.Sdk/Models/PackageReference.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Build.Framework;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
 #nullable enable
 namespace Uno.Sdk.Models;
 
-internal record PackageReference(string PackageId, string Version, string? ExcludeAssets = null)
+internal record PackageReference(string PackageId, string Version, IDictionary<string, string> MetaData)
 {
 	public ITaskItem ToTaskItem()
 	{
@@ -12,13 +14,18 @@ internal record PackageReference(string PackageId, string Version, string? Exclu
 		{
 			ItemSpec = PackageId,
 		};
+
+		foreach(var data in MetaData)
+		{
+			if (data.Key == "ProjectSystem")
+				continue;
+
+			taskItem.SetMetadata(data.Key, data.Value);
+		}
+
 		taskItem.SetMetadata(nameof(Version), Version);
 		taskItem.SetMetadata("IsImplicitlyDefined", bool.TrueString);
 		taskItem.SetMetadata("Sdk", "Uno");
-		if (!string.IsNullOrEmpty(ExcludeAssets))
-		{
-			taskItem.SetMetadata(nameof(ExcludeAssets), ExcludeAssets);
-		}
 		return taskItem;
 	}
 }
