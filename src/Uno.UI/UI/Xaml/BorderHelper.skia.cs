e8b8a155c24dc66f428aa673ba759a5e97538060
Ramez Ragaa <ramezragaa@proton.me>
2024-05-12 15:52:25 +0300

    chore: the T in BorderHelper should be both a UIElement and an IBorderInfoProvider

--- src/Uno.UI/UI/Xaml/BorderHelper.skia.cs
+++ src/Uno.UI/UI/Xaml/BorderHelper.skia.cs
@@ -8,54 +8,54 @@ namespace Microsoft.UI.Xaml;
 
 internal static class BorderHelper
 {
-	public static void UpdateCornerRadius<T>(this T @this) where T : IBorderInfoProvider
+	public static void UpdateCornerRadius<T>(this T @this) where T : UIElement, IBorderInfoProvider
 	{
-		if ((@this as UIElement)!.Visual is not BorderVisual visual)
+		if (@this.Visual is not BorderVisual visual)
 		{
 			throw new InvalidOperationException($"UIElements that have a corner radius should use a {nameof(BorderVisual)}.");
 		}
 		visual.CornerRadius = @this.CornerRadius.ToUnoCompositionCornerRadius();
 	}
 
-	public static void UpdateBorderThickness<T>(this T @this) where T : IBorderInfoProvider
+	public static void UpdateBorderThickness<T>(this T @this) where T : UIElement, IBorderInfoProvider
 	{
-		if ((@this as UIElement)!.Visual is not BorderVisual visual)
+		if (@this.Visual is not BorderVisual visual)
 		{
 			throw new InvalidOperationException($"UIElements that have a border thickness should use a {nameof(BorderVisual)}.");
 		}
 		visual.BorderThickness =
-			((@this as UIElement)!.GetUseLayoutRounding() ? (@this as UIElement)!.LayoutRound(@this.BorderThickness) : @this.BorderThickness)
+			(@this.GetUseLayoutRounding() ? @this.LayoutRound(@this.BorderThickness) : @this.BorderThickness)
 			.ToUnoCompositionThickness();
 	}
 
-	public static void UpdateBackgroundSizing<T>(this T @this) where T : IBorderInfoProvider
+	public static void UpdateBackgroundSizing<T>(this T @this) where T : UIElement, IBorderInfoProvider
 	{
-		if ((@this as UIElement)!.Visual is not BorderVisual visual)
+		if (@this.Visual is not BorderVisual visual)
 		{
 			throw new InvalidOperationException($"UIElements that have a border sizing should use a {nameof(BorderVisual)}.");
 		}
 		visual.UseInnerBorderBoundsAsAreaForBackground = @this.BackgroundSizing == BackgroundSizing.InnerBorderEdge;
 	}
 
-	public static void UpdateBackground<T>(this T @this) where T : IBorderInfoProvider
+	public static void UpdateBackground<T>(this T @this) where T : UIElement, IBorderInfoProvider
 	{
-		if ((@this as UIElement)!.Visual is not BorderVisual visual)
+		if (@this.Visual is not BorderVisual visual)
 		{
 			throw new InvalidOperationException($"UIElements that have a background should use a {nameof(BorderVisual)}.");
 		}
 		@this.BorderBrushSubscriptionDisposable.Disposable = Brush.AssignAndObserveBrush(@this.Background, visual.Compositor, brush => visual.BackgroundBrush = brush);
 	}
 
-	public static void UpdateBorderBrush<T>(this T @this) where T : IBorderInfoProvider
+	public static void UpdateBorderBrush<T>(this T @this) where T : UIElement, IBorderInfoProvider
 	{
-		if ((@this as UIElement)!.Visual is not BorderVisual visual)
+		if (@this.Visual is not BorderVisual visual)
 		{
 			throw new InvalidOperationException($"UIElements that have a border should use a {nameof(BorderVisual)}.");
 		}
 		@this.BorderBrushSubscriptionDisposable.Disposable = Brush.AssignAndObserveBrush(@this.BorderBrush, visual.Compositor, brush => visual.BorderBrush = brush);
 	}
 
-	public static void UpdateAllBorderProperties<T>(this T @this) where T : IBorderInfoProvider
+	public static void UpdateAllBorderProperties<T>(this T @this) where T : UIElement, IBorderInfoProvider
 	{
 		@this.UpdateBorderBrush();
 		@this.UpdateBackground();
