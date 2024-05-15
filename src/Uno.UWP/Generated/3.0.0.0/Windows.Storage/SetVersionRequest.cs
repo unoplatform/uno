61c639fd931af4b7197c3e9cac620ce4c65c9ad6
Martin Zikmund <martin@zikmund.dev>
2024-05-15 20:02:03 +0200

    feat: Windows.Storage.SetVersionRequest

--- src/Uno.UWP/Generated/3.0.0.0/Windows.Storage/SetVersionRequest.cs
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.Storage/SetVersionRequest.cs
@@ -3,41 +3,45 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Windows.Storage
 {
-#if __ANDROID__ || __IOS__ || __TVOS__ || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__
+#if false
 	[global::Uno.NotImplemented]
 #endif
 	public partial class SetVersionRequest
 	{
-#if __ANDROID__ || __IOS__ || __TVOS__ || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__
+#if false
 		internal SetVersionRequest()
 		{
 		}
 #endif
-#if __ANDROID__ || __IOS__ || __TVOS__ || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "__TVOS__", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__")]
+#if false
+		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 		public uint CurrentVersion
 		{
 			get
 			{
-				throw global::Windows.Foundation.Metadata.ApiInformation.CreateNotImplementedException("Windows.Storage.SetVersionRequest", "CurrentVersion");
+				throw new global::System.NotImplementedException("The member uint SetVersionRequest.CurrentVersion is not implemented. For more information, visit https://aka.platform.uno/notimplemented#m=uint%20SetVersionRequest.CurrentVersion");
 			}
 		}
 #endif
-#if __ANDROID__ || __IOS__ || __TVOS__ || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "__TVOS__", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__")]
+#if false
+		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 		public uint DesiredVersion
 		{
 			get
 			{
-				throw global::Windows.Foundation.Metadata.ApiInformation.CreateNotImplementedException("Windows.Storage.SetVersionRequest", "DesiredVersion");
+				throw new global::System.NotImplementedException("The member uint SetVersionRequest.DesiredVersion is not implemented. For more information, visit https://aka.platform.uno/notimplemented#m=uint%20SetVersionRequest.DesiredVersion");
 			}
 		}
 #endif
-#if __ANDROID__ || __IOS__ || __TVOS__ || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "__TVOS__", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__")]
+		// Forced skipping of method Windows.Storage.SetVersionRequest.CurrentVersion.get
+		// Forced skipping of method Windows.Storage.SetVersionRequest.DesiredVersion.get
+#if false
+		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
+#if __ANDROID__ || __IOS__ || __TVOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__
+		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "__TVOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__")]
 		public global::Windows.Storage.SetVersionDeferral GetDeferral()
 		{
-			throw global::Windows.Foundation.Metadata.ApiInformation.CreateNotImplementedException("Windows.Storage.SetVersionRequest", "GetDeferral()");
+			throw new global::System.NotImplementedException("The member SetVersionDeferral SetVersionRequest.GetDeferral() is not implemented. For more information, visit https://aka.platform.uno/notimplemented#m=SetVersionDeferral%20SetVersionRequest.GetDeferral%28%29");
 		}
 #endif
 		// Forced skipping of method Windows.Storage.SetVersionRequest.CurrentVersion.get
