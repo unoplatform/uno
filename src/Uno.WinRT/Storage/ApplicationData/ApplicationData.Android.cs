3e5d4500734561843d8c9e573002b7ecf8347e5c
Martin Zikmund <martin@zikmund.dev>
2024-05-15 20:02:25 +0200

    refactor: Move and adjust ApplicationData types

---
+++ src/Uno.WinRT/Storage/ApplicationData/ApplicationData.Android.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+
+namespace Windows.Storage;
+
+partial class ApplicationData
+{
+	private static string GetLocalCacheFolder()
+		=> GetAndroidAppContext().CacheDir.AbsolutePath;
+
+	private static string GetTemporaryFolder()
+		=> Path.GetTempPath();
+
+	private static string GetLocalFolder()
+		=> GetAndroidAppContext().FilesDir.AbsolutePath;
+
+	private static string GetRoamingFolder()
+		=> Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+	private static string GetSharedLocalFolder()
+		=> Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+	internal static Android.Content.Context GetAndroidAppContext()
+		=> Android.App.Application.Context
+			?? throw new InvalidOperationException(
+				"The Android Application context is not yet available. " +
+				"Your Android head must declare a Microsoft.UI.Xaml.NativeApplication subclass, " +
+				"marked with [Application], overriding CreateHost().");
+}
