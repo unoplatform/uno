46aaa2ac047005f32dbb3902c5c698e2a8681fe0
Martin Zikmund <martin@zikmund.dev>
2024-05-17 10:06:41 +0200

    chore: Start moving ApplicationDataContainer logic to settings class

---
+++ src/Uno.UWP/Storage/ApplicationData/ApplicationDataContainerSettings.iOSmacOS.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows.Storage;
+
+partial class ApplicationDataContainerSettings
+{
+}
