e55dd7855d812a713b9dae5b9cdbc3cba6deab95
Youssef Victor <youssefvictor00@gmail.com>
2024-05-20 13:22:49 +0300

    chore: Use CWT

--- src/Uno.UI/UI/Xaml/Internal/EventManager.uno.cs
+++ src/Uno.UI/UI/Xaml/Internal/EventManager.uno.cs
@@ -6,6 +6,7 @@ using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using Microsoft.UI.Xaml;
 using Uno.UI.Helpers;
@@ -17,7 +18,7 @@ namespace Uno.UI.Xaml.Core;
 // Since we don't yet have these, and this code is very related to events, we add it to this EventManager partial.
 internal sealed partial class EventManager
 {
-	private readonly List<WeakReference<FrameworkElement>> _layoutUpdatedSubscribers = new();
+	private readonly ConditionalWeakTable<FrameworkElement, object?> _layoutUpdatedSubscribers = new();
 	private readonly Queue<SizeChangedQueueItem> _sizeChangedQueue = new(1024);
 
 	internal bool HasPendingSizeChangedEvents => _sizeChangedQueue is { Count: > 0 };
@@ -51,67 +52,20 @@ internal sealed partial class EventManager
 
 	public void AddLayoutUpdatedEventHandler(FrameworkElement element)
 	{
-		var subscriberIndex = GetSubscriberIndex(element);
-		if (subscriberIndex < 0)
-		{
-			_layoutUpdatedSubscribers.Add(new WeakReference<FrameworkElement>(element));
-		}
-		else
-		{
-			// AddLayoutUpdatedEventHandler is only called for a first subscriber.
-			Debug.Fail("This shouldn't happen");
-		}
+		_layoutUpdatedSubscribers.Add(element, null);
 	}
 
 	public void RemoveLayoutUpdatedEventHandler(FrameworkElement element)
 	{
-		var subscriberIndex = GetSubscriberIndex(element);
-		if (subscriberIndex >= 0)
-		{
-			_layoutUpdatedSubscribers.RemoveAt(subscriberIndex);
-		}
-		else
-		{
-			// RemoveLayoutUpdatedEventHandler is only called when removing the last subscriber.
-			Debug.Fail("This shouldn't happen");
-		}
-	}
-
-	private int GetSubscriberIndex(FrameworkElement element)
-	{
-		for (int i = 0; i < _layoutUpdatedSubscribers.Count; i++)
-		{
-			if (_layoutUpdatedSubscribers[i].TryGetTarget(out var target) && target == element)
-			{
-				return i;
-			}
-		}
-
-		return -1;
+		_layoutUpdatedSubscribers.Remove(element);
 	}
 
 	public void RaiseLayoutUpdated()
 	{
-		var count = _layoutUpdatedSubscribers.Count;
-		var clone = ArrayPool<WeakReference<FrameworkElement>>.Shared.Rent(_layoutUpdatedSubscribers.Count);
-		try
+		var clone = _layoutUpdatedSubscribers.ToArray();
+		for (int i = 0; i < clone.Length; i++)
 		{
-			for (int i = 0; i < count; i++)
-			{
-				clone[i] = _layoutUpdatedSubscribers[i];
-			}
-
-			for (int i = 0; i < count; i++)
-			{
-				if (clone[i].TryGetTarget(out var target))
-				{
-					target.OnLayoutUpdated();
-				}
-			}
-		}
-		finally
-		{
-			ArrayPool<WeakReference<FrameworkElement>>.Shared.Return(clone);
+			clone[i].Key.OnLayoutUpdated();
 		}
 	}
 
