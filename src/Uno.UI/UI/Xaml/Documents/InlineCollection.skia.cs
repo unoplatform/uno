5fb267ae2085df3fd9f3d1a56716afeb87f97aa4
Ramez Ragaa <ramezragaa@proton.me>
2024-05-20 16:57:47 +0300

    chore: adjust comment

--- src/Uno.UI/UI/Xaml/Documents/InlineCollection.skia.cs
+++ src/Uno.UI/UI/Xaml/Documents/InlineCollection.skia.cs
@@ -294,8 +294,8 @@ namespace Microsoft.UI.Xaml.Documents
 							}
 						}
 
-						// By this point, we must have at least dealt with the leading spaces.
-						// We either drew all the leading space or we drew as many as we could and we're discarding the rest.
+						// By this point, we must have at least dealt with all the leading spaces. We either drew
+						// all the leading spaces or we drew as many as we could and we're discarding the rest.
 
 						if (x > 0)
 						{
