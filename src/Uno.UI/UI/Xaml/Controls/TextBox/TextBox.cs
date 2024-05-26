57bf505db905a3a363849c9d8a556fd2a34a1934
Ramez Ragaa <ramezragaa@proton.me>
2024-05-26 18:41:36 +0300

    chore: fix edge case with _pendingSelection and \n removal in TextBox

--- src/Uno.UI/UI/Xaml/Controls/TextBox/TextBox.cs
+++ src/Uno.UI/UI/Xaml/Controls/TextBox/TextBox.cs
@@ -3,6 +3,7 @@
 #endif
 
 using System;
+using System.Text;
 using Uno.Extensions;
 using Uno.UI.Common;
 using Uno.UI.DataBinding;
@@ -390,14 +391,52 @@ namespace Microsoft.UI.Xaml.Controls
 				return DependencyProperty.UnsetValue;
 			}
 
+#if __SKIA__
 			if (!AcceptsReturn)
 			{
 				baseString = GetFirstLine(baseString);
+				if (_pendingSelection is { } selection)
+				{
+					_pendingSelection = (Math.Min(selection.start, baseString.Length), Math.Max(0, Math.Min(selection.start + selection.length, baseString.Length)));
+				}
 			}
-#if __SKIA__
 			else if (_isSkiaTextBox)
 			{
-				baseString = baseString.Replace("\r\n", "\r").Replace("\n", "\r");
+				// WinUI replaces all \r's and and \r\n's by \r. This is annoying because
+				// the _pendingSelection uses indices before this removal.
+				var builder = new StringBuilder();
+				for (int i = 0; i < baseString.Length; i++)
+				{
+					if (baseString[i] == '\n')
+					{
+						builder.Append('\r');
+					}
+					else if (baseString[i] == '\r' && baseString[i + 1] == '\n')
+					{
+						if (_pendingSelection is { } selection)
+						{
+							var (start, end) = (selection.start, selection.start + selection.length);
+							if (start > i)
+							{
+								start--;
+							}
+							if (end > i)
+							{
+								end--;
+							}
+							_pendingSelection = (start, end - start);
+						}
+
+						builder.Append('\r');
+						i++;
+
+					}
+				}
+			}
+#else
+			if (!AcceptsReturn)
+			{
+				baseString = GetFirstLine(baseString);
 			}
 #endif
 
