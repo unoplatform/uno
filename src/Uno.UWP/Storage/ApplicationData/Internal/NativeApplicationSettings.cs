e908d6f4499401ac771ba0abf9c8127de4f7723b
Martin Zikmund <martin@zikmund.dev>
2024-05-28 18:13:24 +0200

    chore: Adjust to NativeApplicationSettings

---
+++ src/Uno.UWP/Storage/ApplicationData/Internal/NativeApplicationSettings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Uno.Storage;
+
+internal partial class NativeApplicationSettings
+{
+	private static readonly ConcurrentDictionary<ApplicationDataLocality, NativeApplicationSettings> _instances = new();
+
+	private readonly ApplicationDataLocality _locality;
+
+	private NativeApplicationSettings(ApplicationDataLocality locality)
+	{
+		_locality = locality;
+
+		InitializePlatform();
+	}
+
+	internal static NativeApplicationSettings GetForLocality(ApplicationDataLocality locality)
+	{
+		if (!SupportsLocality())
+		{
+			locality = ApplicationDataLocality.Local;
+		}
+
+		return _instances.GetOrAdd(locality, locality => new NativeApplicationSettings(locality));
+	}
+
+	private static partial bool SupportsLocality();
+
+	partial void InitializePlatform();
+}
