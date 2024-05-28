e15b957bee1b14b34912ba02b9ee3bc07a33d1dd
Martin Zikmund <martin@zikmund.dev>
2024-05-28 14:05:15 +0200

    feat: ApplicationDataContainerSettings

--- src/Uno.UWP/Storage/ApplicationData/ApplicationDataContainerSettings.wasm.cs
+++ src/Uno.UWP/Storage/ApplicationData/ApplicationDataContainerSettings.wasm.cs
@@ -1,214 +1,187 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using Uno.Extensions;
-using Uno.Foundation.Interop;
-
-namespace Windows.Storage;
+﻿namespace Windows.Storage;
 
 partial class ApplicationDataContainerSettings
 {
+	partial void InitializePartial() => ReadFromLegacyFile();
 
-	private class FilePropertySet : IPropertySet
+	public object? this[string key]
 	{
-		private readonly ApplicationDataLocality _locality;
-		private readonly ApplicationData _owner;
-
-		public FilePropertySet(ApplicationData owner, ApplicationDataLocality locality)
+		get
 		{
-			_locality = locality;
-			_owner = owner;
-
-			ReadFromLegacyFile();
+			if (ApplicationDataContainerInterop.TryGetValue(_locality, key, out var value))
+			{
+				return DataTypeSerializer.Deserialize(value);
+			}
+			return null;
 		}
-
-		public object? this[string key]
+		set
 		{
-			get
-			{
-				if (ApplicationDataContainerInterop.TryGetValue(_locality, key, out var value))
-				{
-					return DataTypeSerializer.Deserialize(value);
-				}
-				return null;
-			}
-			set
-			{
-				if (value != null)
-				{
-					ApplicationDataContainerInterop.SetValue(_locality, key, DataTypeSerializer.Serialize(value));
-				}
-				else
-				{
-					Remove(key);
-				}
-			}
-		}
-
-		public ICollection<string> Keys
-		{
-			get
-			{
-				var keys = new List<string>();
-
-				for (int i = 0; i < Count; i++)
-				{
-					keys.Add(ApplicationDataContainerInterop.GetKeyByIndex(_locality, i));
-				}
-
-				return keys.AsReadOnly();
-			}
-		}
-
-		public ICollection<object> Values
-		{
-			get
-			{
-				var values = new List<object>();
-
-				for (int i = 0; i < Count; i++)
-				{
-					var rawValue = ApplicationDataContainerInterop.GetValueByIndex(_locality, i);
-					values.Add(DataTypeSerializer.Deserialize(rawValue));
-				}
-
-				return values.AsReadOnly();
-			}
-		}
-
-		public int Count
-			=> ApplicationDataContainerInterop.GetCount(_locality);
-
-		public bool IsReadOnly => false;
-
-		public event MapChangedEventHandler<string, object>? MapChanged;
-
-		public void Add(string key, object value)
-		{
-			if (ContainsKey(key))
-			{
-				throw new ArgumentException("An item with the same key has already been added.");
-			}
 			if (value != null)
 			{
 				ApplicationDataContainerInterop.SetValue(_locality, key, DataTypeSerializer.Serialize(value));
-				MapChanged?.Invoke(this, null);
 			}
-		}
-
-		public void Add(KeyValuePair<string, object> item)
-			=> Add(item.Key, item.Value);
-
-		public void Clear()
-		{
-			ApplicationDataContainerInterop.Clear(_locality);
-		}
-
-		public bool Contains(KeyValuePair<string, object> item)
-			=> throw new NotSupportedException();
-
-		public bool ContainsKey(string key)
-			=> ApplicationDataContainerInterop.ContainsKey(_locality, key);
-
-		public void CopyTo(KeyValuePair<string, object>[] array, int arrayIndex)
-			=> throw new NotSupportedException();
-
-		public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
-		{
-			List<KeyValuePair<string, object>> kvps = new List<KeyValuePair<string, object>>();
-
-			for (int index = 0; index < Count; index++)
+			else
 			{
-				var key = ApplicationDataContainerInterop.GetKeyByIndex(_locality, index);
-				var value = ApplicationDataContainerInterop.GetValueByIndex(_locality, index);
-				kvps.Add(new KeyValuePair<string, object>(key, value));
-			}
-
-			return kvps.GetEnumerator();
-		}
-
-		public bool Remove(string key)
-		{
-			var ret = ApplicationDataContainerInterop.Remove(_locality, key);
-			return ret;
-		}
-
-		public bool Remove(KeyValuePair<string, object> item) => Remove(item.Key);
-
-		public bool TryGetValue(string key, out object? value)
-		{
-			if (ApplicationDataContainerInterop.TryGetValue(_locality, key, out var innervalue))
-			{
-				value = DataTypeSerializer.Deserialize(innervalue);
-				return true;
-			}
-
-			value = null;
-			return false;
-		}
-
-		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-		private void ReadFromLegacyFile()
-		{
-			const string UWPFileName = ".UWPAppSettings";
-
-			var folder = _locality switch
-			{
-				ApplicationDataLocality.Local => _owner.LocalFolder,
-				ApplicationDataLocality.Roaming => _owner.RoamingFolder,
-				ApplicationDataLocality.LocalCache => _owner.LocalCacheFolder,
-				ApplicationDataLocality.Temporary => _owner.TemporaryFolder,
-				_ => throw new ArgumentOutOfRangeException($"Unsupported locality {_locality}"),
-			};
-
-			var filePath = Path.Combine(folder.Path, UWPFileName);
-
-			try
-			{
-				if (File.Exists(filePath))
-				{
-					using var reader = new BinaryReader(File.OpenRead(filePath));
-
-					var count = reader.ReadInt32();
-
-					if (this.Log().IsEnabled(LogLevel.Debug))
-					{
-						this.Log().Debug($"Reading {count} settings values");
-					}
-
-					for (var i = 0; i < count; i++)
-					{
-						var key = reader.ReadString();
-						var value = reader.ReadString();
-
-						this[key] = value;
-					}
-				}
-				else
-				{
-					if (this.Log().IsEnabled(LogLevel.Debug))
-					{
-						this.Log().Debug($"File {filePath} does not exist, skipping reading legacy settings");
-					}
-				}
-			}
-			catch (Exception e)
-			{
-				if (this.Log().IsEnabled(LogLevel.Error))
-				{
-					this.Log().Error($"Failed to read settings from {filePath}", e);
-				}
+				Remove(key);
 			}
 		}
 	}
 
+	public ICollection<string> Keys
+	{
+		get
+		{
+			var keys = new List<string>();
+
+			for (int i = 0; i < Count; i++)
+			{
+				keys.Add(ApplicationDataContainerInterop.GetKeyByIndex(_locality, i));
+			}
+
+			return keys.AsReadOnly();
+		}
+	}
+
+	public ICollection<object> Values
+	{
+		get
+		{
+			var values = new List<object>();
+
+			for (int i = 0; i < Count; i++)
+			{
+				var rawValue = ApplicationDataContainerInterop.GetValueByIndex(_locality, i);
+				values.Add(DataTypeSerializer.Deserialize(rawValue));
+			}
+
+			return values.AsReadOnly();
+		}
+	}
+
+	public int Count
+		=> ApplicationDataContainerInterop.GetCount(_locality);
+
+	public bool IsReadOnly => false;
+
+	public event MapChangedEventHandler<string, object>? MapChanged;
+
+	public void Add(string key, object value)
+	{
+		if (ContainsKey(key))
+		{
+			throw new ArgumentException("An item with the same key has already been added.");
+		}
+		if (value != null)
+		{
+			ApplicationDataContainerInterop.SetValue(_locality, key, DataTypeSerializer.Serialize(value));
+			MapChanged?.Invoke(this, null);
+		}
+	}
+
+	public void Add(KeyValuePair<string, object> item)
+		=> Add(item.Key, item.Value);
+
+	public void Clear()
+	{
+		ApplicationDataContainerInterop.Clear(_locality);
+	}
+
+	public bool Contains(KeyValuePair<string, object> item)
+		=> throw new NotSupportedException();
+
+	public bool ContainsKey(string key)
+		=> ApplicationDataContainerInterop.ContainsKey(_locality, key);
+
+	public void CopyTo(KeyValuePair<string, object>[] array, int arrayIndex)
+		=> throw new NotSupportedException();
+
+	public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+	{
+		List<KeyValuePair<string, object>> kvps = new List<KeyValuePair<string, object>>();
+
+		for (int index = 0; index < Count; index++)
+		{
+			var key = ApplicationDataContainerInterop.GetKeyByIndex(_locality, index);
+			var value = ApplicationDataContainerInterop.GetValueByIndex(_locality, index);
+			kvps.Add(new KeyValuePair<string, object>(key, value));
+		}
+
+		return kvps.GetEnumerator();
+	}
+
+	public bool Remove(string key)
+	{
+		var ret = ApplicationDataContainerInterop.Remove(_locality, key);
+		return ret;
+	}
+
+	public bool Remove(KeyValuePair<string, object> item) => Remove(item.Key);
+
+	public bool TryGetValue(string key, out object? value)
+	{
+		if (ApplicationDataContainerInterop.TryGetValue(_locality, key, out var innervalue))
+		{
+			value = DataTypeSerializer.Deserialize(innervalue);
+			return true;
+		}
+
+		value = null;
+		return false;
+	}
+
+	private void ReadFromLegacyFile()
+	{
+		const string UWPFileName = ".UWPAppSettings";
+
+		var folder = _locality switch
+		{
+			ApplicationDataLocality.Local => _owner.LocalFolder,
+			ApplicationDataLocality.Roaming => _owner.RoamingFolder,
+			ApplicationDataLocality.LocalCache => _owner.LocalCacheFolder,
+			ApplicationDataLocality.Temporary => _owner.TemporaryFolder,
+			_ => throw new ArgumentOutOfRangeException($"Unsupported locality {_locality}"),
+		};
+
+		var filePath = Path.Combine(folder.Path, UWPFileName);
+
+		try
+		{
+			if (File.Exists(filePath))
+			{
+				using var reader = new BinaryReader(File.OpenRead(filePath));
+
+				var count = reader.ReadInt32();
+
+				if (this.Log().IsEnabled(LogLevel.Debug))
+				{
+					this.Log().Debug($"Reading {count} settings values");
+				}
+
+				for (var i = 0; i < count; i++)
+				{
+					var key = reader.ReadString();
+					var value = reader.ReadString();
+
+					this[key] = value;
+				}
+			}
+			else
+			{
+				if (this.Log().IsEnabled(LogLevel.Debug))
+				{
+					this.Log().Debug($"File {filePath} does not exist, skipping reading legacy settings");
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			if (this.Log().IsEnabled(LogLevel.Error))
+			{
+				this.Log().Error($"Failed to read settings from {filePath}", e);
+			}
+		}
+	}
 }
-
-
 
 class ApplicationDataContainerInterop
 {
