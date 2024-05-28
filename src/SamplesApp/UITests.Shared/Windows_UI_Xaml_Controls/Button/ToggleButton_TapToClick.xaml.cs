d6f9a2481636b50ebe7404d4b4fe85f48c71827a
Martin Zikmund <martin@zikmund.dev>
2024-05-28 13:35:08 +0200

    chore: Adjust test to avoid UI test failure

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Button/ToggleButton_TapToClick.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Button/ToggleButton_TapToClick.xaml.cs
@@ -3,29 +3,27 @@ using Microsoft.UI.Xaml.Controls;
 using SamplesApp.Windows_UI_Xaml_Controls.ToggleSwitchControl.Models;
 using Uno.UI.Samples.Controls;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+namespace UITests.Shared.Windows_UI_Xaml_Controls.ButtonTestsControl;
 
-namespace UITests.Shared.Windows_UI_Xaml_Controls.ButtonTests
+[Sample(
+	"Buttons",
+	IgnoreInSnapshotTests = true,
+	IsManualTest = true,
+	Description =
+		"This sample is specifically for macOS laptops. Go to system Settings -> Trackpad and enable Tap to Click feature. " +
+		"Then move mouse over the button and light-tap the trackpad repeatedly. The button should get toggled on each tap reliably.")]
+public sealed partial class ToggleButton_TapToClick : UserControl
 {
-	[Sample(
-		"Buttons",
-		IsManualTest = true,
-		Description =
-			"This sample is specifically for macOS laptops. Go to system Settings -> Trackpad and enable Tap to Click feature. " +
-			"Then move mouse over the button and light-tap the trackpad repeatedly. The button should get toggled on each tap reliably.")]
-	public sealed partial class ToggleButton_TapToClick : UserControl
+	public ToggleButton_TapToClick()
 	{
-		public ToggleButton_TapToClick()
-		{
-			this.InitializeComponent();
-			TestSwitch.Click += (s, e) => EventLog.Add("Click");
-			TestSwitch.Tapped += (s, e) => EventLog.Add("Tapped");
-			TestSwitch.PointerPressed += (s, e) => EventLog.Add("PointerPressed");
-			TestSwitch.PointerReleased += (s, e) => EventLog.Add("PointerReleased");
-			TestSwitch.PointerEntered += (s, e) => EventLog.Add("PointerEntered");
-			TestSwitch.PointerExited += (s, e) => EventLog.Add("PointerExited");
-		}
-
-		public ObservableCollection<string> EventLog { get; } = new();
+		this.InitializeComponent();
+		MyToggle.Click += (s, e) => EventLog.Add("Click");
+		MyToggle.Tapped += (s, e) => EventLog.Add("Tapped");
+		MyToggle.PointerPressed += (s, e) => EventLog.Add("PointerPressed");
+		MyToggle.PointerReleased += (s, e) => EventLog.Add("PointerReleased");
+		MyToggle.PointerEntered += (s, e) => EventLog.Add("PointerEntered");
+		MyToggle.PointerExited += (s, e) => EventLog.Add("PointerExited");
 	}
+
+	public ObservableCollection<string> EventLog { get; } = new();
 }
