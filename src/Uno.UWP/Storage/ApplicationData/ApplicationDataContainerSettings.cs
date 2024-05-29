fba6bc0ced5d06fae3f36a15b4cf3bfd956cf764
Martin Zikmund <martin@zikmund.dev>
2024-05-29 10:39:36 +0200

    chore: Adjusting serialization and deserialization

--- src/Uno.UWP/Storage/ApplicationData/ApplicationDataContainerSettings.cs
+++ src/Uno.UWP/Storage/ApplicationData/ApplicationDataContainerSettings.cs
@@ -1,27 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Uno.Storage;
 using Windows.Foundation.Collections;
 
 namespace Windows.Storage;
 
 /// <summary>
-/// Provides access to the settings in a settings container. The ApplicationDataContainer.Values property returns an object that can be cast to this type.
+/// Provides access to the settings in a settings container. The ApplicationDataContainer.Values
+/// property returns an object that can be cast to this type.
 /// </summary>
 public partial class ApplicationDataContainerSettings : IPropertySet, IObservableMap<string, object>, IDictionary<string, object>, IEnumerable<KeyValuePair<string, object>>
 {
 	private readonly ApplicationDataContainer _container;
 	private readonly ApplicationDataLocality _locality;
+	private readonly NativeApplicationSettings _nativeApplicationSettings;
 
 	internal ApplicationDataContainerSettings(ApplicationDataContainer container, ApplicationDataLocality locality)
 	{
 		_container = container ?? throw new ArgumentNullException(nameof(container));
 		_locality = locality;
 
-		InitializePlatform();
+		_nativeApplicationSettings = NativeApplicationSettings.GetForLocality(locality);
 	}
-
-	partial void InitializePlatform();
 
 	/// <summary>
 	/// Occurs when the map changes.
@@ -29,9 +30,108 @@ public partial class ApplicationDataContainerSettings : IPropertySet, IObservabl
 	public event MapChangedEventHandler<string, object> MapChanged;
 
 	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="key"></param>
+	/// <returns></returns>
+	public object this[string key]
+	{
+		get
+		{
+			var serializedValue = _nativeApplicationSettings[key];
+			if (serializedValue is null)
+			{
+				return null;
+			}
+
+			return DataTypeSerializer.Deserialize(serializedValue);
+		}
+		set
+		{
+			if (value is null)
+			{
+				_nativeApplicationSettings.Remove(key);
+			}
+			else
+			{
+				var serializedValue = DataTypeSerializer.Serialize(value);
+				_nativeApplicationSettings[key] = serializedValue;
+			}
+		}
+	}
+
+	/// <summary>
 	/// Gets the number of related application settings.
 	/// </summary>
 	public uint Size => (uint)Count;
+
+	public int Count
+	{
+		get
+		{
+			throw new global::System.NotSupportedException();
+		}
+	}
+
+	public bool IsReadOnly => false;
+
+	public global::System.Collections.Generic.ICollection<string> Keys
+	{
+		get
+		{
+			throw new global::System.NotSupportedException();
+		}
+	}
+
+	public global::System.Collections.Generic.ICollection<object> Values
+	{
+		get
+		{
+			throw new global::System.NotSupportedException();
+		}
+	}
+
+	public void Add(global::System.Collections.Generic.KeyValuePair<string, object> item)
+	{
+		throw new global::System.NotSupportedException();
+	}
+
+	public void Add(string key, object value)
+	{
+		throw new global::System.NotSupportedException();
+	}
+
+	public bool Contains(global::System.Collections.Generic.KeyValuePair<string, object> item)
+	{
+		throw new global::System.NotSupportedException();
+	}
+
+	public bool ContainsKey(string key)
+	{
+		throw new global::System.NotSupportedException();
+	}
+
+	public void CopyTo(global::System.Collections.Generic.KeyValuePair<string, object>[] array, int arrayIndex)
+	{
+		throw new global::System.NotSupportedException();
+	}
+
+	public bool Remove(string key)
+	{
+		throw new global::System.NotSupportedException();
+	}
+
+	public bool Remove(KeyValuePair<string, object> item) => Remove(item.Key);
+
+	public bool TryGetValue(string key, out object value)
+	{
+		throw new global::System.NotSupportedException();
+	}
+
+	public void Clear()
+	{
+		throw new global::System.NotSupportedException();
+	}
 
 	public bool Contains(global::System.Collections.Generic.KeyValuePair<string, object> item)
 	{
@@ -43,9 +143,15 @@ public partial class ApplicationDataContainerSettings : IPropertySet, IObservabl
 		throw new global::System.NotSupportedException();
 	}
 
-	public bool Remove(KeyValuePair<string, object> item) => Remove(item.Key);
+	public bool Remove(global::System.Collections.Generic.KeyValuePair<string, object> item)
+	{
+		throw new global::System.NotSupportedException();
+	}
 
-	public bool IsReadOnly => false;
+	public global::System.Collections.Generic.IEnumerator<global::System.Collections.Generic.KeyValuePair<string, object>> GetEnumerator()
+	{
+		throw new global::System.NotSupportedException();
+	}
 
 	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
