f8ff599bd6af50dde681e289840193d932b13c2f
Martin Zikmund <martin@zikmund.dev>
2024-06-04 15:16:54 +0200

    feat: Add IsPointerCaptureRequired attached property
    (cherry picked from commit 85abb78626cfd24ef102ebde055836c4e8f5de00)

---
+++ src/Uno.UI/UI/Xaml/Controls/TextBox/TextBox.Uno.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace Uno.UI.Xaml.Controls;
+
+/// <summary>
+/// Contains additional attached properties that provide Uno-specific
+/// behavior for TextBox control.
+/// </summary>
+public class TextBox
+{
+#if __CROSSRUNTIME__
+	/// <summary>
+	/// Gets the value indicating whether the pointer capture on pointer press should be required.
+	/// </summary>
+	/// <remarks>
+	/// This flag is only applied in case of WebAssembly.
+	/// </remarks>
+	/// <param name="textBox">TextBox.</param>
+	/// <returns>Value.</returns>
+	public static bool GetIsPointerCaptureRequired(Microsoft.UI.Xaml.Controls.TextBox textBox) =>
+		(bool)textBox.GetValue(IsPointerCaptureRequiredProperty);
+
+	/// <summary>
+	/// Sets the value indicating whether the pointer capture on pointer press should be required.
+	/// </summary>
+	/// <remarks>
+	/// This flag is only applied in case of WebAssembly.
+	/// </remarks>
+	/// <param name="textBox">TextBox.</param>
+	/// <param name="value">Value.</param>
+	public static void SetIsPointerCaptureRequired(Microsoft.UI.Xaml.Controls.TextBox textBox, bool value) =>
+		textBox.SetValue(IsPointerCaptureRequiredProperty, value);
+
+	/// <summary>
+	/// Identifies the IsPointerCaptureRequired attached property.
+	/// </summary>
+	public static DependencyProperty IsPointerCaptureRequiredProperty { get; } =
+		DependencyProperty.RegisterAttached(
+			"IsPointerCaptureRequired",
+			typeof(bool),
+			typeof(TextBox),
+			new FrameworkPropertyMetadata(
+				// We should not capture the pointer on WASM by default because it would prevent the user from scrolling through text on selection.
+				// See https://github.com/unoplatform/uno/pull/16982, https://issues.chromium.org/issues/344491566
+				false));
+#endif
+}
