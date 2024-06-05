ee91bdc3ac36242cd311b65c9dec39589a788134
Martin Zikmund <martin@zikmund.dev>
2024-06-05 14:09:39 +0200

    chore: ContentRoot processing, start keyboard accelerator utility

--- src/Uno.UI/UI/Xaml/Internal/ContentRoot.h.mux.cs
+++ src/Uno.UI/UI/Xaml/Internal/ContentRoot.h.mux.cs
@@ -5,7 +5,7 @@ using Uno.UI.DataBinding;
 
 namespace Uno.UI.Xaml.Core;
 
-internal record KACollectionAndRefCountPair(ManagedWeakReference KeyboardAcceleratorCollectionWeak, int Count);
+internal record struct KACollectionAndRefCountPair(ManagedWeakReference KeyboardAcceleratorCollectionWeak, int Count);
 
 internal class VectorOfKACollectionAndRefCountPair : List<KACollectionAndRefCountPair>
 {
