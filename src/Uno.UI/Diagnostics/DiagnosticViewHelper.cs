deaaeb3eba5f79f702895bbf5c73adfd9ce2ddba
David <github.site@torick.net>
2024-06-06 09:20:17 -0400

    chore: Fix UWP build
    (cherry picked from commit 97d0a1da346d3f1d0aa78a2541a60729a4dc28bb)

--- src/Uno.UI/Diagnostics/DiagnosticViewHelper.cs
+++ src/Uno.UI/Diagnostics/DiagnosticViewHelper.cs
@@ -2,22 +2,25 @@
 
 using System;
 using System.Linq;
-using Microsoft.UI;
+using Windows.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
+using Colors = Microsoft.UI.Colors;
 
 namespace Uno.Diagnostics.UI;
 
 internal class DiagnosticViewHelper
 {
+	private static readonly Color _white = new(255, 255, 255, 255);
+
 	public static TextBlock CreateText(string? text)
 		=> new()
 		{
 			Text = text ?? "--",
 			VerticalAlignment = VerticalAlignment.Center,
 			FontSize = 10,
-			Foreground = new SolidColorBrush(Colors.White)
+			Foreground = new SolidColorBrush(_white)
 		};
 
 	public static DiagnosticViewHelper<TextBlock> CreateText<T>(Func<T> value)
