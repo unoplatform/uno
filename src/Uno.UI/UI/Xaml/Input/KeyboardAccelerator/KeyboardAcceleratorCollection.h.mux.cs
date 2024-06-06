97d927057d6f4f4944f6afb1c0a34ef4ccdffcf0
Martin Zikmund <martin@zikmund.dev>
2024-06-06 16:16:33 +0200

    chore: Adjusting test lifecycle to fit Uno

--- src/Uno.UI/UI/Xaml/Input/KeyboardAccelerator/KeyboardAcceleratorCollection.h.mux.cs
+++ src/Uno.UI/UI/Xaml/Input/KeyboardAccelerator/KeyboardAcceleratorCollection.h.mux.cs
@@ -9,32 +9,43 @@ namespace Microsoft.UI.Xaml.Input;
 
 internal class KeyboardAcceleratorCollection : DependencyObjectCollection<KeyboardAccelerator>
 {
+#if HAS_UNO // TODO: Uno specific - workaround for the lack of support for Enter/Leave on DOs.
+	private ParentVisualTreeListener _parentVisualTreeListener;
+
+	public KeyboardAcceleratorCollection()
+	{
+		_parentVisualTreeListener = new ParentVisualTreeListener(this);
+		_parentVisualTreeListener.ParentLoaded += (s, e) => Enter(null, new EnterParams(true));
+		_parentVisualTreeListener.ParentUnloaded += (s, e) => Leave(null, new LeaveParams(true));
+	}
+#endif
+
 	private void Enter(DependencyObject pNamescopeOwner, EnterParams enterParams)
 	{
-		//TODO:MZ Implement this
 		//base.Enter(pNamescopeOwner, enterParams);
 
-		//if (enterParams.IsLive || enterParams.IsForKeyboardAccelerator)
-		//{
-		//	ContentRoot pContentRoot = VisualTree.GetContentRootForElement(this);
-		//	if (pContentRoot != null)
-		//	{
-		//		pContentRoot.AddToLiveKeyboardAccelerators(this);
-		//	}
-		//}
+		if (enterParams.IsLive)// || enterParams.IsForKeyboardAccelerator)
+		{
+			ContentRoot pContentRoot = VisualTree.GetContentRootForElement(this);
+			if (pContentRoot != null)
+			{
+				pContentRoot.AddToLiveKeyboardAccelerators(this);
+			}
+		}
 	}
 
 	private void Leave(DependencyObject pNamescopeOwner, LeaveParams leaveParams)
 	{
 		//TODO:MZ Implement this
 		//base.Leave(pNamescopeOwner, leaveParams);
-		//if (leaveParams.IsLive || leaveParams.IsForKeyboardAccelerator)
-		//{
-		//	ContentRoot pContentRoot = VisualTree.GetContentRootForElement(this);
-		//	if (pContentRoot != null)
-		//	{
-		//		pContentRoot.RemoveFromLiveKeyboardAccelerators(this);
-		//	}
-		//}
+
+		if (leaveParams.IsLive)// || leaveParams.IsForKeyboardAccelerator)
+		{
+			ContentRoot pContentRoot = VisualTree.GetContentRootForElement(this);
+			if (pContentRoot != null)
+			{
+				pContentRoot.RemoveFromLiveKeyboardAccelerators(this);
+			}
+		}
 	}
 }
