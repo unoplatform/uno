4e5bbb1574069e11b926de93408cd483a0de4e0a
Ramez Ragaa <ramezragaa@proton.me>
2024-06-07 16:21:56 +0300

    chore: reorder logic in Visual.skia.cs

--- src/Uno.UI.Composition/Composition/Visual.skia.cs
+++ src/Uno.UI.Composition/Composition/Visual.skia.cs
@@ -87,6 +87,16 @@ public partial class Visual : global::Microsoft.UI.Composition.CompositionObject
 		return !matrixDirty;
 	}
 
+	/// <summary>
+	/// Identifies whether a Visual can paint things. For example, ContainerVisuals don't
+	/// paint on their own (even though they might contain other Visuals that do).
+	/// This is a temporary optimization used with <see cref="_requiresRepaint"/> to reduce unnecessary
+	/// SkPicture allocations. In the future, we should accurately set <see cref="_requiresRepaint"/> to
+	/// only be true when we really have something to paint (and that painting needs to be updated).
+	/// </summary>
+	internal virtual bool CanPaint => false;
+	internal virtual bool RequiresRepaintOnEveryFrame => false;
+
 	internal void InvalidatePaint()
 	{
 		_picture?.Dispose();
@@ -356,16 +366,6 @@ public partial class Visual : global::Microsoft.UI.Composition.CompositionObject
 
 	/// <remarks>You should NOT mutate the list returned by this method.</remarks>
 	internal List<Visual> GetChildrenInRenderOrderTestingOnly() => GetChildrenInRenderOrder();
-
-	/// <summary>
-	/// Identifies whether a Visual can paint things. For example, ContainerVisuals don't
-	/// paint on their own (even though they might contain other Visuals that do).
-	/// This is a temporary optimization used with <see cref="_requiresRepaint"/> to reduce unnecessary
-	/// SkPicture allocations. In the future, we should accurately set <see cref="_requiresRepaint"/> to
-	/// only be true when we really have something to paint (and that painting needs to be updated).
-	/// </summary>
-	internal virtual bool CanPaint => false;
-	internal virtual bool RequiresRepaintOnEveryFrame => false;
 
 	/// <summary>
 	/// Creates a new <see cref="PaintingSession"/> set up with the local coordinates and opacity.
