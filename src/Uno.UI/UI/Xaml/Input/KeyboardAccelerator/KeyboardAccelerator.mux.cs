1860843012d038bfb2b93ab0cfc6479cbffb026e
Martin Zikmund <martin@zikmund.dev>
2024-06-11 17:40:35 +0200

    chore: Start KA ToolTip support

--- src/Uno.UI/UI/Xaml/Input/KeyboardAccelerator/KeyboardAccelerator.mux.cs
+++ src/Uno.UI/UI/Xaml/Input/KeyboardAccelerator/KeyboardAccelerator.mux.cs
@@ -3,6 +3,7 @@
 // MUX Reference dxaml\xcp\core\input\KeyboardAccelerator.cpp, tag winui3/release/1.4.3, commit 685d2bf
 
 using DirectUI;
+using Uno.UI.Extensions;
 using Uno.UI.Xaml;
 using Windows.System;
 using static Microsoft/* UWP don't rename */.UI.Xaml.Controls._Tracing;
@@ -21,9 +22,9 @@ partial class KeyboardAccelerator
 
 		if (enterParams.IsLive)
 		{
-			//TODO:MZ Implement this
-			//// If there are events registered on this element, ask the
-			//// EventManager to extract them and create a request for every event.
+			//TODO:MZ Do we need this part?
+			// If there are events registered on this element, ask the
+			// EventManager to extract them and create a request for every event.
 			//var core = GetContext();
 			//if (m_pEventList)
 			//{
@@ -33,41 +34,40 @@ partial class KeyboardAccelerator
 			//	IFCEXPECT_ASSERT_RETURN(pEventManager);
 			//	IFC_RETURN(pEventManager->AddRequestsInOrder(this, m_pEventList));
 			//}
-			//KeyboardAcceleratorCollection * const pCollection = do_pointer_cast<KeyboardAcceleratorCollection>(this->GetParentInternal(false /* publicParentOnly */));
-			//IFCPTR_RETURN(pCollection);
-			//DependencyObject* pParentElement = pCollection->GetParentInternal(false /* publicParentOnly */);
+			KeyboardAcceleratorCollection collection = (KeyboardAcceleratorCollection)this.GetParentInternal(false /* publicParentOnly */);
+			DependencyObject pParentElement = collection.GetParentInternal(false /* publicParentOnly */);
 
-			//// Do not set tooltip if
-			//// 1. Parent element has disabled the keyboard accelerator tooltip.
-			//// 2. current keyboard accelerator is disabled.
+			// Do not set tooltip if
+			// 1. Parent element has disabled the keyboard accelerator tooltip.
+			// 2. current keyboard accelerator is disabled.
 
-			//KeyboardAcceleratorPlacementMode kaPlacementMode = KeyboardAcceleratorPlacementMode.Auto;
+			UIElement element = (UIElement)pParentElement;
 
-			//var kaPlacementMode = (KeyboardAcceleratorPlacementMode)element.GetValue(UIElement.KeyboardAcceleratorPlacementModeProperty);
+			var kaPlacementMode = (KeyboardAcceleratorPlacementMode)element.GetValue(UIElement.KeyboardAcceleratorPlacementModeProperty);
 
-			//if (kaPlacementMode == KeyboardAcceleratorPlacementMode.Hidden
-			//	|| !this.IsEnabled
-			//	|| this.m_key == VirtualKey.None)
+			if (kaPlacementMode == KeyboardAcceleratorPlacementMode.Hidden
+				|| !this.IsEnabled
+				|| this.Key == VirtualKey.None)
 
-			//{
-			//	// Don't show a tooltip for an accelerator, no need to make the popup
-			//	return;
-			//}
+			{
+				// Don't show a tooltip for an accelerator, no need to make the popup
+				return;
+			}
 
-			//// Create and set tooltip on parent control, only if this is the first tooltip enabled accelerator in the collection.
-			//foreach (DependencyObject accelerator in collection)
-			//{
-			//	MUX_ASSERT(accelerator is KeyboardAccelerator);
+			// Create and set tooltip on parent control, only if this is the first tooltip enabled accelerator in the collection.
+			foreach (DependencyObject accelerator in collection)
+			{
+				MUX_ASSERT(accelerator is KeyboardAccelerator);
 
-			//	if (((KeyboardAccelerator)accelerator).IsEnabled)
-			//	{
-			//		if (accelerator == this)
-			//		{
-			//			FxCallbacks.KeyboardAccelerator_SetToolTip(this, pParentElement);
-			//		}
-			//		break;
-			//	}
-			//}
+				if (((KeyboardAccelerator)accelerator).IsEnabled)
+				{
+					if (accelerator == this)
+					{
+						FxCallbacks.KeyboardAccelerator_SetToolTip(this, pParentElement);
+					}
+					break;
+				}
+			}
 		}
 	}
 }
