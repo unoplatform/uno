e89b98ef2d27fe8322db9f36e7b0f1d509471e95
Martin Zikmund <martin@zikmund.dev>
2024-06-13 15:22:44 +0200

    chore: Switch away from attached property
    (cherry picked from commit 6617750afc06c2491506491783419b3ef2e0d6fe)

--- src/Uno.UI/UI/Xaml/Controls/TextBox/TextBox.Uno.cs
+++ src/Uno.UI/UI/Xaml/Controls/TextBox/TextBox.Uno.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.UI.Xaml;
 
-namespace Uno.UI.Xaml.Controls;
+namespace Microsoft.UI.Xaml.Controls;
 
-/// <summary>
-/// Contains additional attached properties that provide Uno-specific
-/// behavior for TextBox control.
-/// </summary>
-public static class TextBox
+partial class TextBox
 {
 #if __CROSSRUNTIME__
 	/// <summary>
@@ -15,28 +11,18 @@ public static class TextBox
 	/// <remarks>
 	/// This flag is only applied in case of WebAssembly.
 	/// </remarks>
-	/// <param name="textBox">TextBox.</param>
-	/// <returns>Value.</returns>
-	public static bool GetIsPointerCaptureRequired(Microsoft.UI.Xaml.Controls.TextBox textBox) =>
-		(bool)textBox.GetValue(IsPointerCaptureRequiredProperty);
-
-	/// <summary>
-	/// Sets the value indicating whether the pointer capture on pointer press should be required.
-	/// </summary>
-	/// <remarks>
-	/// This flag is only applied in case of WebAssembly.
-	/// </remarks>
-	/// <param name="textBox">TextBox.</param>
-	/// <param name="value">Value.</param>
-	public static void SetIsPointerCaptureRequired(Microsoft.UI.Xaml.Controls.TextBox textBox, bool value) =>
-		textBox.SetValue(IsPointerCaptureRequiredProperty, value);
+	public bool IsPointerCaptureRequired
+	{
+		get => (bool)GetValue(IsPointerCaptureRequiredProperty);
+		set => SetValue(IsPointerCaptureRequiredProperty, value);
+	}
 
 	/// <summary>
 	/// Identifies the IsPointerCaptureRequired attached property.
 	/// </summary>
 	public static DependencyProperty IsPointerCaptureRequiredProperty { get; } =
-		DependencyProperty.RegisterAttached(
-			"IsPointerCaptureRequired",
+		DependencyProperty.Register(
+			nameof(IsPointerCaptureRequired),
 			typeof(bool),
 			typeof(TextBox),
 			new FrameworkPropertyMetadata(
