b8ee214130f6512b4fc5162a2a152b18bd1a9c3b
Martin Zikmund <martin@zikmund.dev>
2024-06-13 14:36:42 +0200

    chore: MenuFlyout adjustments prep

---
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyout.Properties.cs
@@ -0,0 +1,32 @@
+﻿namespace Microsoft.UI.Xaml.Controls;
+
+partial class MenuFlyout
+{
+    public IList<MenuFlyoutItemBase> Items
+	{
+		get => (IList<MenuFlyoutItemBase>)this.GetValue(ItemsProperty);
+		private set => this.SetValue(ItemsProperty, value);
+	}
+
+	public static DependencyProperty ItemsProperty { get; } =
+		DependencyProperty.Register(
+			nameof(Items),
+			typeof(IList<MenuFlyoutItemBase>),
+			typeof(MenuFlyout),
+			new FrameworkPropertyMetadata(defaultValue: null));
+
+	public Style MenuFlyoutPresenterStyle
+	{
+		get => (Style)this.GetValue(MenuFlyoutPresenterStyleProperty);
+		set => SetValue(MenuFlyoutPresenterStyleProperty, value);
+	}
+
+	public static DependencyProperty MenuFlyoutPresenterStyleProperty { get; } =
+		DependencyProperty.Register(
+			nameof(MenuFlyoutPresenterStyle),
+			typeof(Style),
+			typeof(MenuFlyout),
+			new FrameworkPropertyMetadata(
+				null, 
+				FrameworkPropertyMetadataOptions.ValueDoesNotInheritDataContext));
+}
---
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyout.mux.cs
@@ -0,0 +1,72 @@
+﻿namespace Microsoft.UI.Xaml.Controls;
+
+partial class MenuFlyout
+{
+	internal static void KeyboardAcceleratorFlyoutItemEnter(
+		DependencyObject* element,
+		DependencyObject* pNamescopeOwner,
+		KnownPropertyIndex collectionPropertyIndex,
+		EnterParams parameters)
+	{
+		CValue value;
+		IFC_RETURN(element->GetValueByIndex(collectionPropertyIndex, &value));
+
+		if (CMenuFlyoutItemBaseCollection * const items = do_pointer_cast<CMenuFlyoutItemBaseCollection>(value.AsObject()))
+
+	{
+        // This is a dead enter to register any keyboard accelerators that may be present in the MenuFlyout items
+        // to the list of live accelerators
+        params.fIsForKeyboardAccelerator = true;
+        params.fIsLive = false;
+        params.fSkipNameRegistration = true;
+        params.fUseLayoutRounding = false;
+        params.fCoercedIsEnabled = false;
+
+			for (CDependencyObject* item : *items)
+			{
+				IFC_RETURN(item->Enter(pNamescopeOwner, params));
+			}
+		}
+
+		return S_OK;
+	}
+
+	internal static void KeyboardAcceleratorFlyoutItemLeave(
+		DependencyObject element,
+		DependencyObject pNamescopeOwner,
+		KnownPropertyIndex collectionPropertyIndex,
+		LeaveParams parameters)
+	{
+		CValue value;
+		IFC_RETURN(element->GetValueByIndex(collectionPropertyIndex, &value));
+
+		if (CMenuFlyoutItemBaseCollection * const items = do_pointer_cast<CMenuFlyoutItemBaseCollection>(value.AsObject()))
+
+	{
+        // This is a dead leave to remove any keyboard accelerators that may be present in the MenuFlyout items
+        // from the list of live accelerators
+        params.fIsForKeyboardAccelerator = true;
+        params.fIsLive = false;
+        params.fSkipNameRegistration = true;
+        params.fUseLayoutRounding = false;
+        params.fCoercedIsEnabled = false;
+
+			for (CDependencyObject* item : *items)
+			{
+				IFC_RETURN(item->Leave(pNamescopeOwner, params));
+			}
+		}
+	}
+
+	private void EnterImpl(DependencyObject namescopeOwner, EnterParams parameters)
+	{
+		//base.EnterImpl(pNamescopeOwner, params));
+		IFC_RETURN(KeyboardAcceleratorFlyoutItemEnter(this, pNamescopeOwner, KnownPropertyIndex::MenuFlyout_Items, params));
+	}
+
+	private void LeaveImpl(DependencyObject namescopeOwner, LeaveParams parameters)
+	{
+		//base.LeaveImpl(pNamescopeOwner, params);
+		IFC_RETURN(KeyboardAcceleratorFlyoutItemLeave(this, pNamescopeOwner, KnownPropertyIndex::MenuFlyout_Items, params));
+	}
+}
