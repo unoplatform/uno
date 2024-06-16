ba530bd30f48c0152dbe83a94d1e4baf16b585a1
Ramez Ragaa <ramezragaa@proton.me>
2024-06-16 14:05:37 +0300

    chore: add remarks

--- src/Uno.Foundation/Uno.Core.Extensions/Uno.Core.Extensions.Disposables/Disposables/DisposableStruct.cs
+++ src/Uno.Foundation/Uno.Core.Extensions/Uno.Core.Extensions.Disposables/Disposables/DisposableStruct.cs
@@ -4,6 +4,11 @@ namespace Uno.Disposables;
 /// This Disposable is used with using statements to imitate golang-style defer statements and RAII-like logic
 /// without allocating or boxing.
 /// </summary>
+/// <remarks>
+/// Make sure that the <paramref name="disposingAction"/> doesn't capture anything (or even better, is static)
+/// to avoid allocations. If you need multiple parameters for the <paramref name="disposingAction"/>, you can
+/// bundle them in a tuple.
+/// </remarks>
 /// <param name="disposingAction">The action that is fired on disposing.</param>
 /// <param name="argToAction">The argument supplied to the <paramref name="disposingAction"/></param>
 public struct DisposableStruct<T>(Action<T> disposingAction, T argToAction) : IDisposable
