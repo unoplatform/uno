a91a05cb77ba5902479991a949ed11fa17c88258
Martin Zikmund <martin@zikmund.dev>
2024-06-17 16:36:00 +0200

    fix: Ignore settings containing a colon but not valid data type

--- src/Uno.UWP/Storage/DataTypeSerializer.cs
+++ src/Uno.UWP/Storage/DataTypeSerializer.cs
@@ -1,88 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
+﻿#nullable enable
+
+using System;
 using System.Globalization;
 using System.Reflection;
-using System.Text;
 using Uno.Extensions.Specialized;
 
-namespace Windows.Storage
+namespace Windows.Storage;
+
+internal class DataTypeSerializer
 {
-	internal class DataTypeSerializer
+	private const char Separator = ':';
+
+	public readonly static Type[] SupportedTypes = new[]
 	{
-		public readonly static Type[] SupportedTypes = new[]
-		{
-			typeof(bool),
-			typeof(byte),
-			typeof(char),
-			typeof(char),
-			typeof(DateTimeOffset),
-			typeof(double),
-			typeof(Guid),
-			typeof(short),
-			typeof(int),
-			typeof(long),
-			typeof(object),
-			typeof(Foundation.Point),
-			typeof(Foundation.Rect),
-			typeof(float),
-			typeof(Foundation.Size),
-			typeof(string),
-			typeof(TimeSpan),
-			typeof(byte),
-			typeof(ushort),
-			typeof(uint),
-			typeof(ulong),
-			typeof(Uri),
-		};
+		typeof(bool),
+		typeof(byte),
+		typeof(char),
+		typeof(char),
+		typeof(DateTimeOffset),
+		typeof(double),
+		typeof(Guid),
+		typeof(short),
+		typeof(int),
+		typeof(long),
+		typeof(object),
+		typeof(Foundation.Point),
+		typeof(Foundation.Rect),
+		typeof(float),
+		typeof(Foundation.Size),
+		typeof(string),
+		typeof(TimeSpan),
+		typeof(byte),
+		typeof(ushort),
+		typeof(uint),
+		typeof(ulong),
+		typeof(Uri),
+	};
 
-		public static object Deserialize(string value)
+	public static object? Deserialize(string? value)
+	{
+		if (value is null)
 		{
-			var index = value?.IndexOf(':') ?? -1;
+			return null;
+		}
 
-			if (index != -1)
+		var index = value.IndexOf(Separator);
+
+		if (index != -1)
+		{
+			string typeName = value.Substring(0, index);
+			var dataType = Type.GetType(typeName) ?? Type.GetType(typeName + ", " + typeof(Foundation.Point).GetTypeInfo().Assembly.FullName);
+			var valueField = value.Substring(index + 1);
+
+			if (dataType == typeof(DateTimeOffset))
 			{
-				string typeName = value.Substring(0, index);
-				var dataType = Type.GetType(typeName) ?? Type.GetType(typeName + ", " + typeof(Foundation.Point).GetTypeInfo().Assembly.FullName);
-				var valueField = value.Substring(index + 1);
-
-				if (dataType == typeof(DateTimeOffset))
-				{
-					return DateTimeOffset.Parse(valueField, CultureInfo.InvariantCulture);
-				}
-				else if (dataType == typeof(Guid))
-				{
-					return Guid.Parse(valueField);
-				}
-				else if (dataType == typeof(TimeSpan))
-				{
-					return TimeSpan.Parse(valueField, CultureInfo.InvariantCulture);
-				}
-				else
-				{
-					return Convert.ChangeType(valueField, dataType, CultureInfo.InvariantCulture);
-				}
+				return DateTimeOffset.Parse(valueField, CultureInfo.InvariantCulture);
 			}
-			else
+			else if (dataType == typeof(Guid))
 			{
-				return null;
+				return Guid.Parse(valueField);
+			}
+			else if (dataType == typeof(TimeSpan))
+			{
+				return TimeSpan.Parse(valueField, CultureInfo.InvariantCulture);
+			}
+			else if (dataType is not null)
+			{
+				return Convert.ChangeType(valueField, dataType!, CultureInfo.InvariantCulture);
 			}
 		}
 
-		public static string Serialize(object value)
+		return null;
+	}
+
+	public static string Serialize(object value)
+	{
+		if (value is null)
 		{
-			if (value == null)
-			{
-				throw new ArgumentNullException(nameof(value));
-			}
-
-			if (!SupportedTypes.Contains(value.GetType()))
-			{
-				throw new NotSupportedException($"Type {value.GetType()} is not supported");
-			}
-
-			var valueAsString = Convert.ToString(value, CultureInfo.InvariantCulture);
-
-			return value.GetType().FullName + ":" + valueAsString;
+			throw new ArgumentNullException(nameof(value));
 		}
+
+		if (!SupportedTypes.Contains(value.GetType()))
+		{
+			throw new NotSupportedException($"Type {value.GetType()} is not supported");
+		}
+
+		var valueAsString = Convert.ToString(value, CultureInfo.InvariantCulture);
+
+		return value.GetType().FullName + Separator + valueAsString;
 	}
 }
