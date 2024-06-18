44390ad4d8b90289248aebe0245fec5ae8a3d4cc
Youssef Victor <youssefvictor00@gmail.com>
2024-06-18 13:18:26 +0300

    chore: Fix skia

--- src/Uno.UI/UI/Xaml/Documents/TextFormatting/FontDetailsCache.skia.cs
+++ src/Uno.UI/UI/Xaml/Documents/TextFormatting/FontDetailsCache.skia.cs
@@ -67,10 +67,10 @@ internal static class FontDetailsCache
 			// TODO (This comment should be resolved during code review):
 			// Should the user be responsible for adding ".manifest" to the ms-appx path himself?
 			// The benefit of the user doing so is that we will not have to first check if a manifest exists.
-			var path = Uri.UnescapeDataString(uri.PathAndQuery).TrimStart('/');
+			var manifestUri = new Uri(uri.OriginalString + ".manifest");
+			var path = Uri.UnescapeDataString(manifestUri.PathAndQuery).TrimStart('/');
 			if (await StorageFileHelper.ExistsInPackage(path))
 			{
-				var manifestUri = new Uri(uri.OriginalString + ".manifest");
 				var manifestFile = await StorageFile.GetFileFromApplicationUriAsync(manifestUri);
 				var manifestStream = await manifestFile.OpenStreamForReadAsync();
 				uri = new Uri(FontManifestHelpers.GetFamilyNameFromManifest(manifestStream, weight, style, stretch));
