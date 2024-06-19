bb2d95f46b594a62b59edf4417b87eae90a0075e
Martin Zikmund <martin@zikmund.dev>
2024-06-19 15:43:23 +0200

    chore: Properties and docs

--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutSubItem.Properties.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutSubItem.Properties.cs
@@ -14,30 +14,46 @@ namespace Microsoft.UI.Xaml.Controls;
 
 partial class MenuFlyoutSubItem
 {
+	/// <summary>
+	/// Gets or sets the graphic content of the menu flyout subitem.
+	/// </summary>
 	public IconElement Icon
 	{
 		get => (IconElement)this.GetValue(IconProperty);
 		set => this.SetValue(IconProperty, value);
 	}
 
+	/// <summary>
+	/// Identifies the Icon dependency property.
+	/// </summary>
+	public static DependencyProperty IconProperty { get; } =
+		DependencyProperty.Register(
+			nameof(Icon),
+			typeof(IconElement),
+			typeof(MenuFlyoutSubItem),
+			new FrameworkPropertyMetadata(default(IconElement)));
 
+	/// <summary>
+	/// Gets the collection used to generate the content of the sub-menu.
+	/// </summary>
+	public IList<MenuFlyoutItemBase> Items => m_tpItems;
+
+	/// <summary>
+	/// Gets or sets the text content of a MenuFlyoutSubItem.
+	/// </summary>
 	public string Text
 	{
 		get => (string)this.GetValue(TextProperty) ?? "";
 		set => SetValue(TextProperty, value);
 	}
 
-	public static Microsoft.UI.Xaml.DependencyProperty TextProperty { get; } =
-	Microsoft.UI.Xaml.DependencyProperty.Register(
-		"Text",
-		typeof(string),
-		typeof(MenuFlyoutSubItem),
-		new FrameworkPropertyMetadata(default(string)));
-
-	public static Microsoft.UI.Xaml.DependencyProperty IconProperty { get; } =
-	Microsoft.UI.Xaml.DependencyProperty.Register(
-		"Icon",
-		typeof(IconElement),
-		typeof(MenuFlyoutSubItem),
-		new FrameworkPropertyMetadata(default(IconElement)));
+	/// <summary>
+	/// Identifies the Text dependency property.
+	/// </summary>
+	public static DependencyProperty TextProperty { get; } =
+		DependencyProperty.Register(
+			nameof(Text),
+			typeof(string),
+			typeof(MenuFlyoutSubItem),
+			new FrameworkPropertyMetadata(default(string)));
 }
---
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/ToggleMenuFlyoutItem.Properties.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+
+namespace Microsoft.UI.Xaml.Controls;
+
+partial class ToggleMenuFlyoutItem
+{
+	/// <summary>
+	/// Gets or sets whether the ToggleMenuFlyoutItem is checked.
+	/// </summary>
+	public bool IsChecked
+	{
+		get => (bool)GetValue(IsCheckedProperty);
+		set => SetValue(IsCheckedProperty, value);
+	}
+
+	/// <summary>
+	/// Identifies the IsChecked dependency property.
+	/// </summary>
+	public static DependencyProperty IsCheckedProperty { get; } =
+		DependencyProperty.Register(
+			nameof(IsChecked),
+			typeof(bool),
+			typeof(ToggleMenuFlyoutItem),
+			new FrameworkPropertyMetadata(
+				defaultValue: false,
+				propertyChangedCallback: (s, e) => (s as ToggleMenuFlyoutItem)?.OnIsCheckedChanged((bool)e.OldValue, (bool)e.NewValue)));
+}
