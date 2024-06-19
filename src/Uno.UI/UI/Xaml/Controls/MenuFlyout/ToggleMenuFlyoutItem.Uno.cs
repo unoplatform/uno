64a5c2d9ef3735d99df34f78dcad569b3dd5df7e
Martin Zikmund <martin@zikmund.dev>
2024-06-19 16:18:51 +0200

    feat: Update ToggleMenuFlyoutItem to winui3/release/1.5.4

---
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/ToggleMenuFlyoutItem.Uno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.UI.Xaml.Controls;
+
+partial class ToggleMenuFlyoutItem
+{
+	/// <summary>
+	/// Test hook used to determine if the item is a toggle.
+	/// </summary>
+	internal override bool HasToggle() => true;
+}
