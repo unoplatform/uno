08ad3e47adb731f1495e973e539879699687d07d
Martin Zikmund <martin@zikmund.dev>
2024-06-19 18:26:21 +0200

    chore: In progress adjustments of MenuFlyout

--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyout.Android.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyout.Android.cs
@@ -9,94 +9,93 @@ using Uno.Extensions;
 using Uno.UI;
 using Uno.UI.Controls;
 
-namespace Microsoft.UI.Xaml.Controls
+namespace Microsoft.UI.Xaml.Controls;
+
+public partial class MenuFlyout
 {
-	public partial class MenuFlyout
+	private PopupMenu _menu;
+
+	internal override View NativeTarget
 	{
-		private PopupMenu _menu;
-
-		internal override View NativeTarget
+		get
 		{
-			get
+			if (GetActualTarget() is { } actualTarget && actualTarget != Target)
 			{
-				if (GetActualTarget() is { } actualTarget && actualTarget != Target)
+				return actualTarget;
+			}
+
+			return null;
+		}
+	}
+
+	internal protected override void Open()
+	{
+		if (UseNativePopup)
+		{
+			_menu = new PopupMenu(ContextHelper.Current, GetActualTarget());
+
+			_menu.MenuItemClick += OnMenuItemClick;
+			_menu.DismissEvent += OnDismiss;
+
+			Items
+				.OfType<MenuFlyoutItem>()
+				.Where(item => item.Visibility == Visibility.Visible)
+				.ForEach((index, item) =>
 				{
-					return actualTarget;
-				}
+					// Using index as ID
+					_menu.Menu.Add(0, index, index, item.Text);
+				});
 
-				return null;
-			}
+			_menu.Show();
 		}
-
-		internal protected override void Open()
+		else
 		{
-			if (UseNativePopup)
-			{
-				_menu = new PopupMenu(ContextHelper.Current, GetActualTarget());
-
-				_menu.MenuItemClick += OnMenuItemClick;
-				_menu.DismissEvent += OnDismiss;
-
-				Items
-					.OfType<MenuFlyoutItem>()
-					.Where(item => item.Visibility == Visibility.Visible)
-					.ForEach((index, item) =>
-					{
-						// Using index as ID
-						_menu.Menu.Add(0, index, index, item.Text);
-					});
-
-				_menu.Show();
-			}
-			else
-			{
-				base.Open();
-			}
+			base.Open();
 		}
+	}
 
-		internal protected override void Close()
+	internal protected override void Close()
+	{
+		if (UseNativePopup)
 		{
-			if (UseNativePopup)
-			{
 
-				_menu?.Dismiss();
-				if (_menu != null)
-				{
-					_menu.MenuItemClick -= OnMenuItemClick;
-					_menu.DismissEvent -= OnDismiss;
-				}
-			}
-			else
+			_menu?.Dismiss();
+			if (_menu != null)
 			{
-				base.Close();
+				_menu.MenuItemClick -= OnMenuItemClick;
+				_menu.DismissEvent -= OnDismiss;
 			}
 		}
-
-		private void OnDismiss(object sender, PopupMenu.DismissEventArgs e)
+		else
 		{
-			Hide(canCancel: false);
+			base.Close();
+		}
+	}
+
+	private void OnDismiss(object sender, PopupMenu.DismissEventArgs e)
+	{
+		Hide(canCancel: false);
+	}
+
+	private void OnMenuItemClick(object sender, PopupMenu.MenuItemClickEventArgs e)
+	{
+		var items = Items.OfType<MenuFlyoutItem>().Where(i => i.Visibility == Visibility.Visible).ToArray();
+		var item = items[e.Item.ItemId];
+
+		item.InvokeClick();
+	}
+
+	private View GetActualTarget()
+	{
+		if (Target is AppBarButton appBarButton
+		&& ((Android.Views.View)Target).Parent == null // View.Parent (IViewParent) is the visual parent (hidden using `new` modifier).
+		&& Target.Parent is CommandBar commandBar // FrameworkElement.Parent (DependencyObject) is the logical parent.
+		&& commandBar.FindViewById(appBarButton.GetHashCode()) is View actionMenuItemView)
+		{
+			// This AppBarButton doesn't exist in the visual tree and is likely rendered natively as an ActionMenuItemView.
+			return actionMenuItemView;
 		}
 
-		private void OnMenuItemClick(object sender, PopupMenu.MenuItemClickEventArgs e)
-		{
-			var items = Items.OfType<MenuFlyoutItem>().Where(i => i.Visibility == Visibility.Visible).ToArray();
-			var item = items[e.Item.ItemId];
-
-			item.InvokeClick();
-		}
-
-		private View GetActualTarget()
-		{
-			if (Target is AppBarButton appBarButton
-			&& ((AView)Target).Parent == null // View.Parent (IViewParent) is the visual parent (hidden using `new` modifier).
-			&& Target.Parent is CommandBar commandBar // FrameworkElement.Parent (DependencyObject) is the logical parent.
-			&& commandBar.FindViewById(appBarButton.GetHashCode()) is View actionMenuItemView)
-			{
-				// This AppBarButton doesn't exist in the visual tree and is likely rendered natively as an ActionMenuItemView.
-				return actionMenuItemView;
-			}
-
-			return Target;
-		}
+		return Target;
 	}
 }
