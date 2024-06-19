5303011703ad87ef1b077a17c2730fce5092dce7
Martin Zikmund <martin@zikmund.dev>
2024-06-19 17:39:30 +0200

    feat: Update MenuFlyoutSubItem to winui3/release/1.5.4

--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutSubItem.mux.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutSubItem.mux.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference dxaml\xcp\core\core\elements\MenuFlyoutSubItem.cpp, tag winui3/release/1.5.4, commit 98a60c8
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Uno.UI.Xaml;
 
 namespace Microsoft.UI.Xaml.Controls;
 
@@ -10,18 +15,13 @@ partial class MenuFlyoutSubItem
 {
 	private void EnterImpl(DependencyObject pNamescopeOwner, EnterParams parameters)
 	{
-
-		base.EnterImpl(pNamescopeOwner, parameters);
+		//base.EnterImpl(pNamescopeOwner, parameters);
 		MenuFlyout.KeyboardAcceleratorFlyoutItemEnter(this, pNamescopeOwner, MenuFlyoutSubItem.ItemsProperty, parameters);
-
-		return S_OK;
 	}
 
 	private void LeaveImpl(DependencyObject pNamescopeOwner, LeaveParams parameters)
 	{
-		base.LeaveImpl(pNamescopeOwner, parameters);
+		//base.LeaveImpl(pNamescopeOwner, parameters);
 		MenuFlyout.KeyboardAcceleratorFlyoutItemLeave(this, pNamescopeOwner, MenuFlyoutSubItem.ItemsProperty, parameters);
-
-		return S_OK;
 	}
 }
