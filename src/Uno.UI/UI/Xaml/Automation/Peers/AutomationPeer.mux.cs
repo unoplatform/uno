488fa1f2d1a550603b7c54bfebc1ba9d99c03a4f
Martin Zikmund <martin@zikmund.dev>
2024-06-19 16:06:24 +0200

    feat: ToggleMenuFlyoutItemAutomationPeer

--- src/Uno.UI/UI/Xaml/Automation/Peers/AutomationPeer.mux.cs
+++ src/Uno.UI/UI/Xaml/Automation/Peers/AutomationPeer.mux.cs
@@ -331,9 +331,35 @@ partial class AutomationPeer
 			return false;
 		}
 #endif
-
 		return false;
 	}
+
+		/// <summary>
+		/// Removes the leading and trailing spaces in the provided string and returns the trimmed version
+		/// or an empty string when no characters are left.
+		/// Because it is recommended to set an AppBarButton, AppBarToggleButton, MenuFlyoutItem or ToggleMenuFlyoutItem's
+		/// KeyboardAcceleratorTextOverride to a single space to hide their keyboard accelerator UI, this trimming method
+		/// prevents automation tools like Narrator from emitting a space when navigating to such an element.
+		/// </summary>
+		private protected static string GetTrimmedKeyboardAcceleratorTextOverride(string keyboardAcceleratorTextOverride)
+		{
+			// Return an empty string when the provided keyboardAcceleratorTextOverride is already empty.
+			if (!string.IsNullOrEmpty(keyboardAcceleratorTextOverride))
+			{
+				var trimmedKeyboardAcceleratorTextOverride = keyboardAcceleratorTextOverride.TrimStart(' ');
+
+				// Return an empty string when the remaining string is empty.
+				if (!string.IsNullOrEmpty(trimmedKeyboardAcceleratorTextOverride))
+				{
+					// Trim the trailing spaces as well.
+					trimmedKeyboardAcceleratorTextOverride = trimmedKeyboardAcceleratorTextOverride.TrimEnd(' ');
+					return trimmedKeyboardAcceleratorTextOverride;
+				}
+			}
+
+			// Return an empty string
+			return "";
+		}
 
 	//------------------------------------------------------------------------
 	//
