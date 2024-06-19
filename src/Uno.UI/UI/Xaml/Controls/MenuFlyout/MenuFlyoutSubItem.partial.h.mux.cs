70d2a1b741bf6d89222e0e062bfdbbdcabfbdf96
Martin Zikmund <martin@zikmund.dev>
2024-06-19 17:39:30 +0200

    feat: Update MenuFlyoutSubItem to winui3/release/1.5.4

--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutSubItem.partial.h.mux.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutSubItem.partial.h.mux.cs
@@ -1,22 +1,47 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference dxaml\xcp\dxaml\lib\MenuFlyoutSubItem_Partial.h, tag winui3/release/1.5.4, commit 98a60c8
+
 using System.Collections.Generic;
-using Uno.Disposables;
-using Uno.UI.Extensions;
-using Uno.UI.Xaml.Core;
-using Windows.Foundation;
-using Windows.System;
-using Microsoft.UI.Xaml.Automation.Peers;
 using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Media.Animation;
+using Uno.Disposables;
+using Uno.UI.DataBinding;
 
 namespace Microsoft.UI.Xaml.Controls;
 
 partial class MenuFlyoutSubItem
 {
-	
-    // ISubMenuOwner implementation
-	bool ISubMenuOwner.IsSubMenuOpen => IsOpen;
+	internal Popup GetPopup() => m_tpPopup;
 
+	internal Control GetMenuFlyoutPresenter() => m_tpPresenter;
+
+	// ISubMenuOwner implementation
 	bool ISubMenuOwner.IsSubMenuPositionedAbsolutely => true;
+
+	// Collection of the sub menu item
+	private IList<MenuFlyoutItemBase> m_tpItems;
+
+	// Popup for the MenuFlyoutSubItem
+	private Popup m_tpPopup;
+
+	// Presenter for the MenuFlyoutSubItem
+	private Control m_tpPresenter;
+
+	// In Threshold, MenuFlyout uses the MenuPopupThemeTransition.
+	private Transition m_tpMenuPopupThemeTransition;
+
+	// Event pointer for the Loaded event
+	private readonly SerialDisposable m_epLoadedHandler = new();
+
+	// Event pointer for the size changed on the MenuFlyoutSubItem's presenter
+	private readonly SerialDisposable m_epPresenterSizeChangedHandler;
+
+	// Helper to which to delegate cascading menu functionality.
+	private CascadingMenuHelper m_menuHelper;
+
+	// Weak reference the parent that owns the menu that this item belongs to.
+	private ManagedWeakReference m_wrParentOwner;
+
+	private bool _itemsSourceRefreshPending;
 }
