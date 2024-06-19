fdcd24102de2cf16ca0d04eb673ab0833d71083a
Martin Zikmund <martin@zikmund.dev>
2024-06-19 15:43:23 +0200

    chore: Properties and docs

--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyout.Properties.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyout.Properties.cs
@@ -1,32 +1,46 @@
-﻿namespace Microsoft.UI.Xaml.Controls;
+﻿using System.Collections.Generic;
+
+namespace Microsoft.UI.Xaml.Controls;
 
 partial class MenuFlyout
 {
-    public IList<MenuFlyoutItemBase> Items
+	/// <summary>
+	/// Gets the collection used to generate the content of the menu.
+	/// </summary>
+	public IList<MenuFlyoutItemBase> Items
 	{
 		get => (IList<MenuFlyoutItemBase>)this.GetValue(ItemsProperty);
 		private set => this.SetValue(ItemsProperty, value);
 	}
 
-	public static DependencyProperty ItemsProperty { get; } =
+	/// <summary>
+	/// Identifies the Items dependency property.
+	/// </summary>
+	internal static DependencyProperty ItemsProperty { get; } =
 		DependencyProperty.Register(
 			nameof(Items),
 			typeof(IList<MenuFlyoutItemBase>),
 			typeof(MenuFlyout),
 			new FrameworkPropertyMetadata(defaultValue: null));
 
+	/// <summary>
+	/// Gets or sets the style that is used when rendering the MenuFlyout.
+	/// </summary>
 	public Style MenuFlyoutPresenterStyle
 	{
 		get => (Style)this.GetValue(MenuFlyoutPresenterStyleProperty);
 		set => SetValue(MenuFlyoutPresenterStyleProperty, value);
 	}
 
+	/// <summary>
+	/// Identifies the MenuFlyoutPresenterStyle dependency property.
+	/// </summary>
 	public static DependencyProperty MenuFlyoutPresenterStyleProperty { get; } =
 		DependencyProperty.Register(
 			nameof(MenuFlyoutPresenterStyle),
 			typeof(Style),
 			typeof(MenuFlyout),
 			new FrameworkPropertyMetadata(
-				null, 
+				null,
 				FrameworkPropertyMetadataOptions.ValueDoesNotInheritDataContext));
 }
