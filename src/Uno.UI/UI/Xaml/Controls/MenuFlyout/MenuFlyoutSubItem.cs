cfb0b5b8353a75d5c29ac0f98314117dc21b4b6c
Martin Zikmund <martin@zikmund.dev>
2024-06-19 17:39:30 +0200

    feat: Update MenuFlyoutSubItem to winui3/release/1.5.4

--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutSubItem.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutSubItem.cs
@@ -1,18 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using Uno.Disposables;
-using Uno.UI.Extensions;
-using Uno.UI.Xaml.Core;
-using Windows.Foundation;
-using Windows.System;
-using Microsoft.UI.Xaml.Automation.Peers;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Markup;
+﻿using Microsoft.UI.Xaml.Markup;
 
 namespace Microsoft.UI.Xaml.Controls;
 
+/// <summary>
+/// Represents a menu item that displays a sub-menu in a MenuFlyout control.
+/// </summary>
 [ContentProperty(Name = nameof(Items))]
 public partial class MenuFlyoutSubItem : MenuFlyoutItemBase, ISubMenuOwner
 {
+	/// <summary>
+	/// Initializes a new instance of the MenuFlyoutSubItem class.
+	/// </summary>
+	public MenuFlyoutSubItem()
+	{
+		DefaultStyleKey = typeof(MenuFlyoutSubItem);
+
+		PrepareState();
+	}
 }
