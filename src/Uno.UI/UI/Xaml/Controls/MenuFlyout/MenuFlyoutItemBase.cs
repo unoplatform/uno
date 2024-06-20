a8a616f456cfb74c6b361f3c4265fd17050f2dd7
Martin Zikmund <martin@zikmund.dev>
2024-06-20 14:29:56 +0200

    feat: Update MenuFlyoutItem to winui3/release/1.5.4

--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutItemBase.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutItemBase.cs
@@ -1,47 +1,53 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference dxaml\xcp\dxaml\lib\MenuFlyoutItemBase_Partial.cpp, tag winui3/release/1.5.4, commit 98a60c8
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.UI.Xaml.Input;
+using Uno.UI.DataBinding;
 
-namespace Microsoft.UI.Xaml.Controls
+namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Represents the base class for items in a MenuFlyout control.
+/// </summary>
+public abstract partial class MenuFlyoutItemBase : Control
 {
-	public abstract partial class MenuFlyoutItemBase : Control
+	private ManagedWeakReference m_wrParentMenuFlyoutPresenter;
+
+	public MenuFlyoutItemBase()
 	{
-		private WeakReference m_wrParentMenuFlyoutPresenter;
+	}
 
-		public MenuFlyoutItemBase()
+	// Get the parent MenuFlyoutPresenter.
+	internal MenuFlyoutPresenter GetParentMenuFlyoutPresenter()
+		=> m_wrParentMenuFlyoutPresenter is { IsAlive: true } ? m_wrParentMenuFlyoutPresenter.Target as MenuFlyoutPresenter : null;
+
+	// Sets the parent MenuFlyoutPresenter.
+	internal void SetParentMenuFlyoutPresenter(MenuFlyoutPresenter pParentMenuFlyoutPresenter)
+		=> m_wrParentMenuFlyoutPresenter = WeakReferencePool.RentWeakReference(this, pParentMenuFlyoutPresenter);
+
+	internal bool GetShouldBeNarrow()
+	{
+		MenuFlyoutPresenter spPresenter = GetParentMenuFlyoutPresenter();
+
+		var shouldBeNarrow = false;
+
+		if (spPresenter != null)
 		{
+			MenuFlyout spParentFlyout = spPresenter.GetParentMenuFlyout();
 
-		}
-
-		// Get the parent MenuFlyoutPresenter.
-		internal MenuFlyoutPresenter GetParentMenuFlyoutPresenter()
-			=> m_wrParentMenuFlyoutPresenter?.Target as MenuFlyoutPresenter;
-
-		// Sets the parent MenuFlyoutPresenter.
-		internal void SetParentMenuFlyoutPresenter(MenuFlyoutPresenter pParentMenuFlyoutPresenter)
-			=> m_wrParentMenuFlyoutPresenter = new WeakReference(pParentMenuFlyoutPresenter);
-
-		internal bool GetShouldBeNarrow()
-		{
-			MenuFlyoutPresenter spPresenter = GetParentMenuFlyoutPresenter();
-
-			var shouldBeNarrow = false;
-
-			if (spPresenter != null)
+			if (spParentFlyout != null)
 			{
-				MenuFlyout spParentFlyout = spPresenter.GetParentMenuFlyout();
-
-				if (spParentFlyout != null)
-				{
-					shouldBeNarrow =
-						(spParentFlyout.InputDeviceTypeUsedToOpen == FocusInputDeviceKind.Mouse) ||
-						(spParentFlyout.InputDeviceTypeUsedToOpen == FocusInputDeviceKind.Pen) ||
-						(spParentFlyout.InputDeviceTypeUsedToOpen == FocusInputDeviceKind.Keyboard);
-				}
+				shouldBeNarrow =
+					(spParentFlyout.InputDeviceTypeUsedToOpen == FocusInputDeviceKind.Mouse) ||
+					(spParentFlyout.InputDeviceTypeUsedToOpen == FocusInputDeviceKind.Pen) ||
+					(spParentFlyout.InputDeviceTypeUsedToOpen == FocusInputDeviceKind.Keyboard);
 			}
-
-			return shouldBeNarrow;
 		}
+
+		return shouldBeNarrow;
 	}
 }
