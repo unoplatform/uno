9ec280ca3e87c3fb240e30b7ae7fd4c63420f8d3
Jérôme Laban <jlaban@gmail.com>
2024-06-21 08:30:51 -0400

    chore: Restore nuget validation
    (cherry picked from commit 904ba104ff6abe19871bbb4413ab71f83b4a165f)

--- build/test-scripts/run-netcore-mobile-template-tests.ps1
+++ build/test-scripts/run-netcore-mobile-template-tests.ps1
@@ -278,10 +278,10 @@ $projects =
     @("5.2/uno52Lib/uno52Lib.csproj", @("-f", "net8.0-browserwasm", "-p:ImplicitUsings=disable"), $true, $true),
     @("5.2/uno52Lib/uno52Lib.csproj", @("-f", "net8.0-desktop", "-p:ImplicitUsings=disable"), $true, $true),
 
-     #
+    #
     # 5.2 Uno NuGet Lib
     #
-    @("5.2/uno52Lib/uno52Lib.csproj", @(), $true, $true),
+    @("5.2/uno52NuGetLib/uno52NuGetLib.csproj", @(), $true, $true),
 
     # Default mode for the template is WindowsAppSDKSelfContained=true, which requires specifying a target platform.
     @("5.2/uno52Lib/uno52Lib.csproj", @("-p:Platform=x86" , "-p:TargetFramework=net8.0-windows10.0.19041"), $false, $true),
