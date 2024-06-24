6eabb89415b7516a376eed41eed556ee5e6cef73
Martin Zikmund <martin@zikmund.dev>
2024-06-24 14:07:12 +0200

    test: ViewBox sizing issues sample

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ViewBoxTests/ViewBox_Grid_Default.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.ViewBoxTests
+{
+	[Sample("Viewbox")]
+	public sealed partial class ViewBox_Grid_Default : Page
+	{
+		public ViewBox_Grid_Default()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ViewBoxTests/ViewBox_Grid_None.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.ViewBoxTests
+{
+	[Sample("Viewbox")]
+	public sealed partial class ViewBox_Grid_None : Page
+	{
+		public ViewBox_Grid_None()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
