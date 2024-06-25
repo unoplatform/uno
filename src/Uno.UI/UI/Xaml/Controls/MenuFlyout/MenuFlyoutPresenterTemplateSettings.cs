9c3b8a2d15700f04e57871c1ac31ebf65feccff1
Martin Zikmund <martin@zikmund.dev>
2024-06-25 10:49:22 +0200

    chore: Mark additional files updated

--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutPresenterTemplateSettings.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutPresenterTemplateSettings.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.UI.Xaml.Controls.Primitives;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference dxaml\xcp\dxaml\lib\winrtgeneratedclasses\MenuFlyoutPresenterTemplateSettings.g.cpp, tag winui3/release/1.5.4, commit 98a60c8
+
+namespace Microsoft.UI.Xaml.Controls.Primitives;
 
 /// <summary>
 /// Provides calculated values that can be referenced as TemplatedParent sources when defining templates for a MenuFlyoutPresenter control.
@@ -9,5 +13,12 @@ public partial class MenuFlyoutPresenterTemplateSettings : DependencyObject
 	/// <summary>
 	/// Gets the minimum width of flyout content.
 	/// </summary>
-	public double FlyoutContentMinWidth { get; internal set; }
+	public double FlyoutContentMinWidth
+	{
+		get => (double)GetValue(FlyoutContentMinWidthProperty);
+		internal set => SetValue(FlyoutContentMinWidthProperty, value);
+	}
+
+	internal static DependencyProperty FlyoutContentMinWidthProperty { get; } =
+		DependencyProperty.Register(nameof(FlyoutContentMinWidth), typeof(double), typeof(MenuFlyoutPresenterTemplateSettings), new FrameworkPropertyMetadata(0.0));
 }
