afa73f9cb4474d92d24c15f3c0574a12ee1b0ad3
David <github.site@torick.net>
2024-06-25 09:33:02 -0400

    feat: Merge the dev-server status into the hot-reload status view
    (cherry picked from commit 9c49ecf38afc5358b5ac925265b59d6dcaec46a8)

--- src/Uno.UI.RemoteControl/RemoteControlStatusView.cs
+++ src/Uno.UI.RemoteControl/RemoteControlStatusView.cs
@@ -1,42 +1,100 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.UI;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Shapes;
-using static Uno.UI.RemoteControl.RemoteControlClient;
+using static Uno.UI.RemoteControl.RemoteControlStatus;
 
 namespace Uno.UI.RemoteControl;
 
 internal sealed partial class RemoteControlStatusView : Ellipse
 {
-	public RemoteControlStatusView()
+	private readonly RemoteControlClient? _devServer;
+
+	public const string Id = nameof(RemoteControlStatusView);
+
+	#region Status (DP)
+	public static readonly DependencyProperty StatusProperty = DependencyProperty.Register(
+		nameof(Status),
+		typeof(RemoteControlStatus),
+		typeof(RemoteControlStatusView),
+		new PropertyMetadata(default(RemoteControlStatus), (snd, args) => ((RemoteControlStatusView)snd).OnStatusChanged((RemoteControlStatus)args.NewValue)));
+
+	public RemoteControlStatus Status
 	{
+		get => (RemoteControlStatus)GetValue(StatusProperty);
+		private set => SetValue(StatusProperty, value);
+	}
+	#endregion
+
+	#region HeadLine (DP)
+	public static readonly DependencyProperty HeadLineProperty = DependencyProperty.Register(
+		nameof(HeadLine),
+		typeof(string),
+		typeof(RemoteControlStatusView),
+		new PropertyMetadata(default(string)));
+
+	public string HeadLine
+	{
+		get => (string)GetValue(HeadLineProperty);
+		private set => SetValue(HeadLineProperty, value);
+	}
+	#endregion
+
+	internal bool HasServer => _devServer is not null;
+
+	public RemoteControlStatusView()
+		: this(RemoteControlClient.Instance)
+	{
+	}
+
+	public RemoteControlStatusView(RemoteControlClient? devServer)
+	{
+		_devServer = devServer;
+
 		Fill = new SolidColorBrush(Colors.Gray);
 		Width = 20;
 		Height = 20;
+
+		if (_devServer is null)
+		{
+			return;
+		}
+
+		Loading += static (snd, _) =>
+		{
+			if (snd is RemoteControlStatusView that)
+			{
+				that._devServer!.StatusChanged += that.OnDevServerStatusChanged;
+				that.Status = that._devServer.Status;
+			}
+		};
+		Unloaded += static (snd, _) =>
+		{
+			if (snd is RemoteControlStatusView that)
+			{
+				that._devServer!.StatusChanged -= that.OnDevServerStatusChanged;
+			}
+		};
 	}
 
-	public bool IsAutoHideEnabled { get; set; }
+	private void OnDevServerStatusChanged(object? sender, RemoteControlStatus status)
+		=> DispatcherQueue.TryEnqueue(() => Status = status);
 
-	public void Update(Status status)
+	private void OnStatusChanged(RemoteControlStatus status)
 	{
 		var (kind, message) = status.GetSummary();
 		((SolidColorBrush)Fill).Color = kind switch
 		{
-			StatusClassification.Ok => Colors.Green,
-			StatusClassification.Info => Colors.Yellow,
-			StatusClassification.Warning => Colors.Orange,
-			StatusClassification.Error => Colors.Red,
+			Classification.Ok => Colors.Green,
+			Classification.Info => Colors.Yellow,
+			Classification.Warning => Colors.Orange,
+			Classification.Error => Colors.Red,
 			_ => Colors.Gray
 		};
+		HeadLine = message;
 		ToolTipService.SetToolTip(this, message);
-
-		if (IsAutoHideEnabled)
-		{
-			Visibility = kind is StatusClassification.Ok
-				? Microsoft.UI.Xaml.Visibility.Collapsed
-				: Microsoft.UI.Xaml.Visibility.Visible;
-		}
 	}
 }
