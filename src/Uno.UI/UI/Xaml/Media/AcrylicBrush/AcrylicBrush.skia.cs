303d1e2a44e1964917427ddcf039db914d8af97f
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2024-06-26 20:10:36 -0400

    Revert "Merge pull request #17013 from unoplatform/dev/spouliot/workaround-pink-acrylicbrush"
    This reverts commit 3d13a85fdf4261644ed51b90060a00e0fd956549, reversing
    changes made to ef403a8455c1fb89944994ec152365f39c23483e.
    
    (cherry picked from commit a6be0d13086398de418b5c0620b93254fae2facb)

--- src/Uno.UI/UI/Xaml/Media/AcrylicBrush/AcrylicBrush.skia.cs
+++ src/Uno.UI/UI/Xaml/Media/AcrylicBrush/AcrylicBrush.skia.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.IO;
-using System.Runtime.InteropServices;
 using Windows.UI;
 using Microsoft.UI;
 using Microsoft.UI.Composition;
@@ -77,15 +76,12 @@ public partial class AcrylicBrush
 		}
 	}
 
-	// issue specific to macOSarm64 https://github.com/unoplatform/uno/issues/16853
-	static bool macOSarm64 = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) && RuntimeInformation.ProcessArchitecture is Architecture.Arm64;
-
 	private void UpdateAcrylicBrush()
 	{
 		if (_isConnected)
 		{
 			// TODO: Currently we are force recreating the brush even if it exists because Composition animations aren't implemented yet
-			CreateAcrylicBrush(useCrossFadeEffect: false, forceCreateAcrylicBrush: !macOSarm64);
+			CreateAcrylicBrush(false /* useCrossFadeEffect */, true /* forceCreateAcrylicBrush */);
 		}
 	}
 
