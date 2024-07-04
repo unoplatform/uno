ccf2263915a6579140fecca89f549a0adb244a7a
Youssef Victor <youssefvictor00@gmail.com>
2024-07-04 11:06:41 +0300

    fix: Fix Launcher on Wasm Skia

--- src/Uno.UI/ts/WindowManager.ts
+++ src/Uno.UI/ts/WindowManager.ts
@@ -859,19 +859,6 @@ namespace Uno.UI {
 		}
 
 		/**
-			* Load the specified URL into a new tab or window
-			* @param url URL to load
-			* @returns "True" or "False", depending on whether a new window could be opened or not
-			*/
-		public open(url: string): string {
-			const newWindow = window.open(url, "_blank");
-
-			return newWindow != null
-				? "True"
-				: "False";
-		}
-
-		/**
 			* Issue a browser alert to user
 			* @param message message to display
 			*/
---
+++ src/Uno.UWP/ts/Windows/System/Launcher/Launcher.ts
@@ -0,0 +1,19 @@
+﻿
+namespace Windows.System {
+
+	export class Launcher {
+
+		/**
+		* Load the specified URL into a new tab or window
+		* @param url URL to load
+		* @returns "True" or "False", depending on whether a new window could be opened or not
+		*/
+		public open(url: string): string {
+			const newWindow = window.open(url, "_blank");
+
+			return newWindow != null
+				? "True"
+				: "False";
+		}
+	}
+}
