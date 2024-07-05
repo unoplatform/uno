cc9e149b511b62fc78d4b5ca5fedd20ce58b7c1b
Youssef Victor <youssefvictor00@gmail.com>
2024-07-05 21:09:23 +0300

    chore: Post rebase

--- src/Uno.UI.Runtime.Skia.Android/Microsoft/UI/Xaml/ApplicationActivity.cs
+++ src/Uno.UI.Runtime.Skia.Android/Microsoft/UI/Xaml/ApplicationActivity.cs
@@ -225,7 +225,7 @@ namespace Microsoft.UI.Xaml
 				canvas.Clear(SKColors.Red);
 				var scale = _cachedDisplayInformation!.RawPixelsPerViewPixel;
 				canvas.Scale((float)scale);
-				window.Compositor.RenderRootVisual(e.Surface, root.Visual, isPopupSurface: false);
+				window.Compositor.RenderRootVisual(e.Surface, root.Visual, null);
 				_skCanvasView!.ExploreByTouchHelper.InvalidateRoot();
 			}
 		}
