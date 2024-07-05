dc1e4336719ffc69f5eebd45dbcf90dd52ef9346
David <github.site@torick.net>
2024-07-05 13:05:34 -0400

    chore: Fix possible empty notifications
    (cherry picked from commit 5c715fb351b181f83e1ca7d174e90e18ee8a5b8d)

--- src/Uno.UI.RemoteControl/HotReload/HotReloadStatusView.Entries.cs
+++ src/Uno.UI.RemoteControl/HotReload/HotReloadStatusView.Entries.cs
@@ -8,12 +8,56 @@ using System.Linq;
 using System.Text;
 using Uno.UI.RemoteControl.HotReload.Messages;
 using static Uno.UI.RemoteControl.HotReload.ClientHotReloadProcessor;
+using static Uno.UI.RemoteControl.RemoteControlStatus;
 
 namespace Uno.UI.RemoteControl.HotReload;
 
-internal record DevServerEntry() : HotReloadLogEntry(EntrySource.DevServer, -1, DateTimeOffset.Now);
+internal record DevServerEntry() : HotReloadLogEntry(EntrySource.DevServer, -1, DateTimeOffset.Now)
+{
+	public static DevServerEntry? TryCreateNew(RemoteControlStatus? oldStatus, RemoteControlStatus newStatus)
+	{
+		if (oldStatus is not null && oldStatus.State == newStatus.State)
+		{
+			return null;
+		}
 
-internal record EngineEntry() : HotReloadLogEntry(EntrySource.Engine, -1, DateTimeOffset.Now);
+		var (iconState, desc) = (oldStatus, newStatus) switch
+		{
+			(_, { State: ConnectionState.NoServer }) => (EntryIcon.Error, "No endpoint found"),
+			(_, { State: ConnectionState.Connecting }) => (EntryIcon.Loading, "Connecting..."),
+			({ State: not ConnectionState.ConnectionTimeout }, { State: ConnectionState.ConnectionTimeout }) => (EntryIcon.Error, "Timeout"),
+			({ State: not ConnectionState.ConnectionFailed }, { State: ConnectionState.ConnectionFailed }) => (EntryIcon.Error, "Connection error"),
+
+			({ IsVersionValid: not false }, { IsVersionValid: false }) => (EntryIcon.Warning, "Version mismatch"),
+			({ InvalidFrames.Count: 0 }, { InvalidFrames.Count: > 0 }) => (EntryIcon.Warning, "Unknown messages"),
+			({ MissingRequiredProcessors.IsEmpty: true }, { MissingRequiredProcessors.IsEmpty: false }) => (EntryIcon.Warning, "Processors missing"),
+
+			({ KeepAlive.State: KeepAliveState.Idle or KeepAliveState.Ok }, { KeepAlive.State: KeepAliveState.Late }) => (EntryIcon.Error, "Connection lost (> 1000ms)"),
+			({ KeepAlive.State: KeepAliveState.Idle or KeepAliveState.Ok }, { KeepAlive.State: KeepAliveState.Lost }) => (EntryIcon.Error, "Connection lost (> 1s)"),
+			({ KeepAlive.State: KeepAliveState.Idle or KeepAliveState.Ok }, { KeepAlive.State: KeepAliveState.Aborted }) => (EntryIcon.Error, "Connection lost (keep-alive)"),
+			({ State: ConnectionState.Connected }, { State: ConnectionState.Disconnected }) => (EntryIcon.Error, "Connection lost"),
+
+			({ State: ConnectionState.Connected }, { State: ConnectionState.Reconnecting }) => (EntryIcon.Error, "Connection lost (reconnecting)"),
+
+			_ => (default, default)
+		};
+
+		return desc is null 
+			? null
+			: new DevServerEntry { Description = desc, Icon = iconState | EntryIcon.Connection };
+	}
+}
+
+internal record EngineEntry() : HotReloadLogEntry(EntrySource.Engine, -1, DateTimeOffset.Now)
+{
+	public static EngineEntry? TryCreateNew(Status? oldStatus, Status status)
+		=> (oldStatus?.State ?? HotReloadState.Initializing, status.State) switch
+		{
+			( < HotReloadState.Ready, HotReloadState.Ready) => new EngineEntry { Title = "Connected", Icon = EntryIcon.Connection | EntryIcon.Success },
+			(not HotReloadState.Disabled, HotReloadState.Disabled) => new EngineEntry { Title = "Cannot initialize", Icon = EntryIcon.Connection | EntryIcon.Error },
+			_ => null
+		};
+}
 
 internal record ServerEntry : HotReloadLogEntry
 {
@@ -33,11 +77,11 @@ internal record ServerEntry : HotReloadLogEntry
 		};
 		Title = srvOp.Result switch
 		{
-			HotReloadServerResult.NoChanges => "No changes detected.",
-			HotReloadServerResult.RudeEdit => "Rude edit detected, restart required.",
-			HotReloadServerResult.Failed => "Compilation errors.",
-			HotReloadServerResult.Aborted => "Operation cancelled.",
-			HotReloadServerResult.InternalError => "An error occured.",
+			HotReloadServerResult.NoChanges => "No changes detected",
+			HotReloadServerResult.RudeEdit => "Rude edit detected, restart required",
+			HotReloadServerResult.Failed => "Compilation errors",
+			HotReloadServerResult.Aborted => "Operation cancelled",
+			HotReloadServerResult.InternalError => "An error occured",
 			_ => null
 		};
 		Description = Join("file", srvOp.FilePaths.Select(Path.GetFileName).ToArray()!);
@@ -66,8 +110,8 @@ internal record ApplicationEntry : HotReloadLogEntry
 		Title = localOp.Result switch
 		{
 			null => "Processing...",
-			HotReloadClientResult.Success => "Update successful.",
-			HotReloadClientResult.Failed => "An error occured.",
+			HotReloadClientResult.Success => "Update successful",
+			HotReloadClientResult.Failed => "An error occured",
 			_ => null
 		};
 		Description = Join("type", localOp.CuratedTypes, localOp.Types.Length);
@@ -109,8 +153,10 @@ internal record HotReloadLogEntry(EntrySource Source, long Id, DateTimeOffset Ti
 	public bool? IsSuccess { get; set; }
 	public TimeSpan? Duration { get; set; }
 	public EntryIcon Icon { get; set; }
+
 	public string? Title { get; set; }
 	public string? Description { get; set; }
+	public string? ToastDescription => Description ?? Title;
 
 	public string TimeInfo => Duration switch
 	{
@@ -122,9 +168,14 @@ internal record HotReloadLogEntry(EntrySource Source, long Id, DateTimeOffset Ti
 	protected void RaiseChanged()
 		=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(""));
 
-	protected static string Join(string kind, string[] items, int? total = null, int max = 5)
+	protected static string? Join(string kind, string[] items, int? total = null, int max = 5)
 	{
 		const int maxLength = 70;
+
+		if (items is { Length: 0 } && total is null)
+		{
+			return null;
+		}
 
 		var sb = new StringBuilder(maxLength + 12 /* and xx more*/);
 		int count;
