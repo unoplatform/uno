06d2545a1639993270399c7ce485af7f70e608ad
Martin Zikmund <martin@zikmund.dev>
2024-07-16 19:20:32 +0200

    test: Add repro for MenuFlyoutItem enabling
    (cherry picked from commit f91143b3b46f78331126652a4e75e9b0d1fba986)

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/Flyout/Flyout_ToggleMenu_IsEnabled.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Uno.UI.Samples.UITests.Helpers;
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.Flyout;
+
+[Sample("Flyouts", ViewModelType = typeof(Flyout_ToggleMenu_IsEnabledViewModel))]
+public sealed partial class Flyout_ToggleMenu_IsEnabled : Page
+{
+	public Flyout_ToggleMenu_IsEnabled()
+	{
+		this.InitializeComponent();
+
+		DataContextChanged += Flyout_ToggleMenu_IsEnabled_DataContextChanged;
+	}
+
+	internal Flyout_ToggleMenu_IsEnabledViewModel ViewModel { get; private set; }
+
+	private void Flyout_ToggleMenu_IsEnabled_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args) =>
+		ViewModel = args.NewValue as Flyout_ToggleMenu_IsEnabledViewModel;
+}
+
+internal class Flyout_ToggleMenu_IsEnabledViewModel : ViewModelBase
+{
+	private bool _isChecked1;
+	private bool _isChecked2;
+	private bool _isChecked3;
+	private bool _areItemsEnabled;
+
+
+	public Flyout_ToggleMenu_IsEnabledViewModel()
+	{
+	}
+
+	public bool IsChecked1
+	{
+		get => _isChecked1;
+		set
+		{
+			_isChecked1 = value;
+			RaisePropertyChanged();
+		}
+	}
+
+	public bool IsChecked2
+	{
+		get => _isChecked2;
+		set
+		{
+			_isChecked2 = value;
+			RaisePropertyChanged();
+		}
+	}
+
+	public bool IsChecked3
+	{
+		get => _isChecked3;
+		set
+		{
+			_isChecked3 = value;
+			RaisePropertyChanged();
+		}
+	}
+
+	public bool AreItemsEnabled
+	{
+		get => _areItemsEnabled;
+		set
+		{
+			_areItemsEnabled = value;
+			RaisePropertyChanged();
+		}
+	}
+}
