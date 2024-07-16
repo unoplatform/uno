b5c51134359443b850ae5c44e4e6c632c087e052
Ramez Ragaa <ramezragaa@proton.me>
2024-07-16 17:57:09 +0300

    chore: make SplitView's pane interact with native elements like a popup when open

--- src/Uno.UI/UI/Xaml/Controls/SplitView/SplitView.cs
+++ src/Uno.UI/UI/Xaml/Controls/SplitView/SplitView.cs
@@ -405,7 +405,15 @@ namespace Microsoft.UI.Xaml.Controls
 #if __IOS__
 			PatchInvalidFinalState(stateName);
 #endif
-			VisualStateManager.GoToState(this, stateName, useTransitons);
+			var success = VisualStateManager.GoToState(this, stateName, useTransitons);
+#if __SKIA__
+			if (success)
+			{
+				// When the Pane is open in overlay mode, it should interact with native elements
+				// like a popup would, i.e. covering them.
+				Pane?.Visual.SetAsPopupVisual(IsPaneOpen && DisplayMode is SplitViewDisplayMode.Overlay or SplitViewDisplayMode.CompactOverlay);
+			}
+#endif
 
 			if (!IsPaneOpen)
 			{
