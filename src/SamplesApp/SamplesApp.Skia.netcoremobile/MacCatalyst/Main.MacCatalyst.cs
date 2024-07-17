23f17f283f51d37e9d29f3848c0c8f75e30a492c
Martin Zikmund <martin@zikmund.dev>
2024-07-17 13:16:28 +0200

    chore: Include Mac Catalyst in Samples app

---
+++ src/SamplesApp/SamplesApp.Skia.netcoremobile/MacCatalyst/Main.MacCatalyst.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using UIKit;
+using Uno.UI.Runtime.Skia.AppleUIKit;
+
+namespace SamplesApp.MacCatalyst
+{
+	public class Application
+	{
+		// This is the main entry point of the application.
+		static async Task Main(string[] args)
+		{
+			await (new AppleUIKitSkiaHost(() => new SamplesApp.App(), args)).Run();
+		}
+	}
+}
