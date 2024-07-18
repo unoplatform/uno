ea0a522651b2caf0f2aee76fe6d412128f57d0b4
Youssef Victor <youssefvictor00@gmail.com>
2024-07-18 21:30:27 +0300

    refactor: Refactor the way we generate suppressions

--- src/SourceGenerators/SourceGeneratorHelpers/Helpers/AnalyzerSuppressions.cs
+++ src/SourceGenerators/SourceGeneratorHelpers/Helpers/AnalyzerSuppressions.cs
@@ -4,10 +4,10 @@ using Uno.Extensions;
 
 namespace Uno.UI.SourceGenerators
 {
-	internal class AnalyzerSuppressionsGenerator
+	internal static class AnalyzerSuppressionsGenerator
 	{
 		/// <summary>
-		/// Outputs SuppressMessage attributes using the provided <see cref="IndentedStringBuilder"/>, and a list of suppressions.
+		/// Outputs #pragma warning disable statements using the provided <see cref="IndentedStringBuilder"/>, and a list of suppressions.
 		/// </summary>
 		/// <param name="writer">An <see cref="IndentedStringBuilder"/> instance.</param>
 		/// <param name="analyzerSuppressions">A list of analyzers definitions in the format of "Category|AnalyzerId"</param>
@@ -17,28 +17,6 @@ namespace Uno.UI.SourceGenerators
 							 let parts = suppress.Split('|', '-')
 							 where parts.Length == 2
 							 let category = parts[0]
-							 where category != "csharp"
-							 let id = parts[1]
-							 select "[global::System.Diagnostics.CodeAnalysis.SuppressMessage(\"{0}\", \"{1}\", Justification=\"Generated code\")]".InvariantCultureFormat(category, id);
-
-			foreach (var suppress in suppresses)
-			{
-				writer.AppendLineIndented(suppress);
-			}
-		}
-
-		/// <summary>
-		/// Outputs #pragma warning disable statements using the provided <see cref="IndentedStringBuilder"/>, and a list of suppressions.
-		/// </summary>
-		/// <param name="writer">An <see cref="IndentedStringBuilder"/> instance.</param>
-		/// <param name="analyzerSuppressions">A list of analyzers definitions in the format of "Category|AnalyzerId"</param>
-		internal static void GenerateCSharpPragmaSupressions(IIndentedStringBuilder writer, string[] analyzerSuppressions)
-		{
-			var suppresses = from suppress in analyzerSuppressions
-							 let parts = suppress.Split('|', '-')
-							 where parts.Length == 2
-							 let category = parts[0]
-							 where category == "csharp"
 							 let id = parts[1]
 							 select $"#pragma warning disable {id}";
 
