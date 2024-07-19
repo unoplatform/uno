29f7b51da3214751fd683f5f045005ae8f1a120e
Jérôme Laban <jlaban@gmail.com>
2024-07-19 13:46:33 -0400

    chore: Bump external docs
    (cherry picked from commit 7e1cf8345a59d26bc937081fbc57a0857782038c)

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -8,16 +8,16 @@ Set-PSDebug -Trace 1
 $external_docs = @{
     # use either commit, or branch name to use its latest commit
     "uno.wasm.bootstrap" = "82541e4ab6d797ea0a42d36e41ed226fc573b0c7" #latest main commit
-    "uno.themes"         = "9ea2ee38186b55745cd211bcf628b06821067465" #latest release branch commit
-    "uno.toolkit.ui"     = "f3ef5ba7d3890a44acbc53abb02f81e0f13a9af1" #latest release branch commit
-    "uno.check"          = "198eaa13bf041f52001e525c095f3dd3716796da" #latest main commit
+    "uno.themes"         = "7ce3b5215bf5802927d2c65f0e2044cacae8c311" #latest release branch commit
+    "uno.toolkit.ui"     = "a8a382ded333547580b7a4086dc1415d9e8cab04" #latest release branch commit
+    "uno.check"          = "73c3b7960601d16e81e425412755385298edad0d" #latest main commit
     "uno.xamlmerge.task" = "21f02c98702b875a9942047ca042e41810b6fe56" #latest main commit
     "figma-docs"         = "842a2792282b88586a337381b2b3786e779973b4" #latest main commit
-    "uno.resizetizer"    = "f66b9b8cd89c5c1fbfdfeda8f601f998fa9f4395" #latest main commit
+    "uno.resizetizer"    = "ab50fa33bc8a9f8aeed68517ad59e111b1acbb5a" #latest main commit
     "uno.uitest"         = "9669fd2783187d06c36dd6a717c1b9f08d1fa29c" #latest master commit
-    "uno.extensions"     = "f789c95183d95f3d206c90a15ddb9ecfdf640fed" #latest release branch commit
+    "uno.extensions"     = "502d6a3d7e4aeab66d1c943bc5f39227c4022de5" #latest release branch commit
     "workshops"          = "3b5bd04b4833305df698ceec51056b6ef6db3190" #latest master commit
-    "uno.samples"        = "04295c6596272c7686f8e67c1e6398149babbf3e" #latest master commit
+    "uno.samples"        = "d82132390ce1e59f9ab7d66a16fc5ac95651f455" #latest master commit
 }
 
 $uno_git_url = "https://github.com/unoplatform/"
