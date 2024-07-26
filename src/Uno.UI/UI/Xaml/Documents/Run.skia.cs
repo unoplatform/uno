8619a8a1c308e9dcb8e3c56284d70f3e36e3daef
Youssef Victor <youssefvictor00@gmail.com>
2024-07-26 12:14:12 +0300

    refactor: Refactor GetSegments

--- src/Uno.UI/UI/Xaml/Documents/Run.skia.cs
+++ src/Uno.UI/UI/Xaml/Documents/Run.skia.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using HarfBuzzSharp;
@@ -9,7 +11,7 @@ using Uno.Extensions;
 using Buffer = HarfBuzzSharp.Buffer;
 using GlyphInfo = Microsoft.UI.Xaml.Documents.TextFormatting.GlyphInfo;
 
-#nullable enable
+using SegmentInfo = (int LeadingSpaces, int TrailingSpaces, int LineBreakLength, bool WordBreakAfter, SkiaSharp.SKTypeface? Typeface, int NextStartingIndex);
 
 namespace Microsoft.UI.Xaml.Documents
 {
@@ -19,6 +21,129 @@ namespace Microsoft.UI.Xaml.Documents
 
 		internal IReadOnlyList<Segment> Segments => _segments ??= GetSegments();
 
+		private static (int CodePoint, int Length) GetCodePoint(ReadOnlySpan<char> text, int i)
+		{
+			if (i + 1 < text.Length &&
+				char.IsSurrogate(text[i]) &&
+				char.IsSurrogatePair(text[i], text[i + 1]))
+			{
+				var codepoint = (int)((text[i] - 0xD800) * 0x400 + (text[i + 1] - 0xDC00) + 0x10000);
+				return (codepoint, 2);
+			}
+
+			return (text[i], 1);
+		}
+
+		private SegmentInfo GetSegmentStartingFrom(int i, ReadOnlySpan<char> text)
+		{
+			int leadingSpaces = 0;
+			int trailingSpaces = 0;
+			int lineBreakLength = 0;
+			bool wordBreakAfter = false;
+			var fontInfo = FontInfo;
+			SKTypeface? segmentTypeface = null;
+
+			// Count leading spaces
+			while (i < text.Length && char.IsWhiteSpace(text[i]) && !Unicode.IsLineBreak(text[i]) && text[i] != '\t')
+			{
+				leadingSpaces++;
+				i++;
+			}
+
+			// Keep the segment going until we hit a word break opportunity or a line break
+			while (i < text.Length)
+			{
+				if (ProcessLineBreak(text, ref i, ref lineBreakLength))
+				{
+					break;
+				}
+
+				// Since tabs require special handling, we put tabs in separate segments.
+				// Also, we don't consider tabs "spaces" since they don't get the general space treatment.
+				if (text[i] == '\t')
+				{
+					wordBreakAfter = true;
+					i++;
+					break;
+				}
+
+				if (i + 1 < text.Length && text[i + 1] == '\t')
+				{
+					if (char.IsWhiteSpace(text[i]))
+					{
+						trailingSpaces++;
+					}
+					wordBreakAfter = true;
+					i++;
+					break;
+				}
+
+				if (Unicode.HasWordBreakOpportunityAfter(text, i) || (i + 1 < text.Length && Unicode.HasWordBreakOpportunityBefore(text, i + 1)))
+				{
+					if (char.IsWhiteSpace(text[i]))
+					{
+						trailingSpaces++;
+					}
+					wordBreakAfter = true;
+					i++;
+					break;
+				}
+
+				var (codepoint, codepointLength) = GetCodePoint(text, i);
+
+				var currentTypeface = fontInfo.SKFont.ContainsGlyph(codepoint)
+					? fontInfo.SKFont.Typeface
+					: SKFontManager.Default.MatchCharacter(codepoint);
+
+				if (currentTypeface is null)
+				{
+					// The requested glyph isn't found by the OS.
+					if (this.Log().IsEnabled(LogLevel.Trace))
+					{
+						this.Log().Trace($"Failed to match codepoint '{codepoint}' (length: {codepointLength}).");
+					}
+
+					// Move over the current codepoint.
+					i += codepointLength;
+				}
+				else if (segmentTypeface is null || currentTypeface == segmentTypeface)
+				{
+					segmentTypeface = currentTypeface;
+					i += codepointLength;
+				}
+				else
+				{
+					// Always break the current segment if the previous typeface and the current typeface are both non-null
+					// and are different.
+					break;
+				}
+			}
+
+			// Tack on any trailing spaces or line breaks if this segment does not yet end in a line break
+			if (lineBreakLength == 0)
+			{
+				while (i < text.Length)
+				{
+					if (ProcessLineBreak(text, ref i, ref lineBreakLength))
+					{
+						break;
+					}
+
+					if (char.IsWhiteSpace(text[i]) && text[i] != '\t')
+					{
+						trailingSpaces++;
+						i++;
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+
+			return (leadingSpaces, trailingSpaces, lineBreakLength, wordBreakAfter, segmentTypeface, i);
+		}
+
 		private List<Segment> GetSegments()
 		{
 			// TODO: Implement Bidi algorithm here to split segments by direction prior to doing the below processing on each directional piece.
@@ -27,199 +152,58 @@ namespace Microsoft.UI.Xaml.Documents
 			List<Segment> segments = new();
 			using HarfBuzzSharp.Buffer buffer = new();
 			var fontInfo = FontInfo;
+			var defaultTypeface = fontInfo.SKFont.Typeface;
 			var defaultFont = fontInfo.Font;
-			int defaultFontScale;
-			float defaultTextSizeY, defaultTextSizeX;
-			var font = defaultFont;
 			var paint = Paint;
 			var fontSize = fontInfo.SKFontSize;
 
-			font.GetScale(out int fontScale, out _);
-			float textSizeY = fontInfo.SKFontSize / fontScale;
-			float textSizeX = textSizeY * fontInfo.SKFontScaleX;
-			defaultFontScale = fontScale;
-			defaultTextSizeX = textSizeX;
-			defaultTextSizeY = textSizeY;
+			defaultFont.GetScale(out int defaultFontScale, out _);
+			float defaultTextSizeY = fontInfo.SKFontSize / defaultFontScale;
+			float defaultTextSizeX = defaultTextSizeY * fontInfo.SKFontScaleX;
+
 			var text = Text.AsSpan();
-			int s = 0;
 			int i = 0;
-			SKTypeface? symbolTypeface = default;
-			FontDetails? fallbackFont = default;
-			var surrogate = false;
 
 			while (i < text.Length)
 			{
-				int leadingSpaces = 0;
-				int trailingSpaces = 0;
-				int lineBreakLength = 0;
-				bool wordBreakAfter = false;
+				var (leadingSpaces, trailingSpaces, lineBreakLength, wordBreakAfter, typeface, nextStartingIndex) = GetSegmentStartingFrom(i, text);
 
-				// Count leading spaces
-				while (i < text.Length && char.IsWhiteSpace(text[i]) && !Unicode.IsLineBreak(text[i]) && text[i] != '\t')
+				int length = nextStartingIndex - i;
+				FontDetails? fallbackFont = null;
+				Font font;
+				int fontScale;
+				float textSizeY;
+				float textSizeX;
+				if (typeface is not null && typeface != defaultTypeface)
 				{
-					leadingSpaces++;
-					i++;
-				}
-
-				// Keep the segment going until we hit a word break opportunity or a line break
-				while (i < text.Length)
-				{
-					if (ProcessLineBreak(text, ref i, ref lineBreakLength))
+					fallbackFont = FontDetailsCache.GetFont(typeface.FamilyName, (float)FontSize, FontWeight, FontStretch, FontStyle);
+					if (fallbackFont.CanChange)
 					{
-						break;
+						fallbackFont.RegisterElementForFontLoaded(this);
 					}
 
-					// Since tabs require special handling, we put tabs in separate segments.
-					// Also, we don't consider tabs "spaces" since they don't get the general space treatment.
-					if (text[i] == '\t')
-					{
-						wordBreakAfter = true;
-						i++;
-						break;
-					}
-
-					if (i + 1 < text.Length && text[i + 1] == '\t')
-					{
-						if (char.IsWhiteSpace(text[i]))
-						{
-							trailingSpaces++;
-						}
-						wordBreakAfter = true;
-						i++;
-						break;
-					}
-
-					if (Unicode.HasWordBreakOpportunityAfter(text, i) || (i + 1 < text.Length && Unicode.HasWordBreakOpportunityBefore(text, i + 1)))
-					{
-						if (char.IsWhiteSpace(text[i]))
-						{
-							trailingSpaces++;
-						}
-						wordBreakAfter = true;
-						i++;
-						break;
-					}
-
-					if (i + 1 < text.Length
-						&& char.IsSurrogate(text[i])
-						&& char.IsSurrogatePair(text[i], text[i + 1]))
-					{
-						var fontManager = SKFontManager.Default;
-						var codepoint = (int)((text[i] - 0xD800) * 0x400 + (text[i + 1] - 0xDC00) + 0x10000);
-						symbolTypeface = fontManager
-							.MatchCharacter(codepoint);
-
-						if (symbolTypeface is not null)
-						{
-							surrogate = true;
-						}
-						else
-						{
-							// Under some Linux systems, the symbol may not be found
-							// in the default font and
-							// we have to skip the character and continue segments
-							// evaluation.
-
-							if (this.Log().IsEnabled(LogLevel.Trace))
-							{
-								this.Log().Trace($"Failed to match surrogate in the default system font (0x{codepoint:X4}, {(char)codepoint})");
-							}
-
-							i++;
-						}
-						break;
-					}
-					else if (!fontInfo.SKFont.ContainsGlyph(text[i]))
-					{
-						var oldTypeface = symbolTypeface;
-						symbolTypeface = SKFontManager.Default
-							.MatchCharacter(text[i]);
-
-						if (symbolTypeface is null)
-						{
-							symbolTypeface = oldTypeface;
-
-							// Under some Linux systems, the symbol may not be found
-							// in the default font and
-							// we have to skip the character and continue segments
-							// evaluation.
-							if (this.Log().IsEnabled(LogLevel.Trace))
-							{
-								this.Log().Trace($"Failed to match symbol in the default system font (0x{(int)text[i]:X4}, {text[i]})");
-							}
-
-							i++;
-							break;
-						}
-
-						// If we already had a fallback typeface, and this one is different, we need to break the segment.
-						// However, we should keep the same segment while the fallback typeface is the same.
-						if (oldTypeface is not null && symbolTypeface != oldTypeface)
-						{
-							break;
-						}
-					}
-					i++;
-				}
-
-				// Tack on any trailing spaces or line breaks if this segment does not yet end in a line break
-
-				if (lineBreakLength == 0)
-				{
-					while (i < text.Length)
-					{
-						if (ProcessLineBreak(text, ref i, ref lineBreakLength))
-						{
-							break;
-						}
-
-						if (char.IsWhiteSpace(text[i]) && text[i] != '\t')
-						{
-							trailingSpaces++;
-							i++;
-						}
-						else
-						{
-							break;
-						}
-					}
-				}
-
-				if (surrogate)
-				{
-					i += surrogate ? 2 : 0;
-					surrogate = false;
-				}
-
-				int length = i - s;
-
-				if (symbolTypeface is not null)
-				{
-					var fi = FontDetailsCache.GetFont(symbolTypeface.FamilyName, (float)FontSize, FontWeight, FontStretch, FontStyle);
-					if (fi.CanChange)
-					{
-						fi.RegisterElementForFontLoaded(this);
-					}
-
-					font = fi.Font;
-					wordBreakAfter = i < text.Length &&
-						(Unicode.HasWordBreakOpportunityAfter(text, i) || (i + 1 < text.Length && Unicode.HasWordBreakOpportunityBefore(text, i + 1)));
+					font = fallbackFont.Font;
 					font.GetScale(out fontScale, out _);
 					textSizeY = fontSize / fontScale;
 					textSizeX = textSizeY * fontInfo.SKFontScaleX;
-					fallbackFont = fi;
-					symbolTypeface = default;
+				}
+				else
+				{
+					font = defaultFont;
+					fontScale = defaultFontScale;
+					textSizeY = defaultTextSizeY;
+					textSizeX = defaultTextSizeX;
 				}
 
 				if (length > 0)
 				{
 					if (lineBreakLength == 2)
 					{
-						buffer.AddUtf16(text.Slice(s, length - 1)); // Skip second line break char so that it is considered part of the same cluster as the first
+						buffer.AddUtf16(text.Slice(i, length - 1)); // Skip second line break char so that it is considered part of the same cluster as the first
 					}
 					else
 					{
-						buffer.AddUtf16(text.Slice(s, length));
+						buffer.AddUtf16(text.Slice(i, length));
 					}
 
 					// TODO: Set the segment properties instead of using HB guessing like below.
@@ -252,51 +236,26 @@ namespace Microsoft.UI.Xaml.Documents
 						buffer.ReverseClusters();
 					}
 
-					var glyphs = GetGlyphs(buffer, s, textSizeX, textSizeY);
+					var glyphs = GetGlyphs(buffer, i, textSizeX, textSizeY);
 
-					Debug.Assert(!(Text.AsSpan(s, length).Contains('\t')) || length == 1);
-					if (length == 1 && text[s] == '\t')
+					Debug.Assert(!(Text.AsSpan(i, length).Contains('\t')) || length == 1);
+					if (length == 1 && text[i] == '\t')
 					{
 						glyphs[0] = glyphs[0] with { GlyphId = _getSpaceGlyph(fontInfo.Font) };
 					}
 
-					var segment = new Segment(this, direction, s, length, leadingSpaces, trailingSpaces, lineBreakLength, wordBreakAfter, glyphs, fallbackFont);
+					var segment = new Segment(this, direction, i, length, leadingSpaces, trailingSpaces, lineBreakLength, wordBreakAfter, glyphs, fallbackFont);
 
 					segments.Add(segment);
 					buffer.ClearContents();
 				}
-				fallbackFont = default;
-				font = defaultFont;
-				textSizeY = defaultTextSizeY;
-				textSizeX = defaultTextSizeX;
-				fontScale = defaultFontScale;
-				s = i;
+
+				i = nextStartingIndex;
 			}
 
 			return segments;
 
 			// Local functions:
-
-			static bool ProcessLineBreak(ReadOnlySpan<char> text, ref int i, ref int lineBreakLength)
-			{
-				if (Unicode.IsLineBreak(text[i]))
-				{
-					if (text[i] == '\r' && i + 1 < text.Length && text[i + 1] == '\n')
-					{
-						lineBreakLength = 2;
-						i += 2;
-					}
-					else
-					{
-						lineBreakLength = 1;
-						i++;
-					}
-
-					return true;
-				}
-
-				return false;
-			}
 
 			static List<GlyphInfo> GetGlyphs(Buffer buffer, int clusterStart, float textSizeX, float textSizeY)
 			{
@@ -325,6 +284,27 @@ namespace Microsoft.UI.Xaml.Documents
 
 				return glyphs;
 			}
+		}
+
+		private static bool ProcessLineBreak(ReadOnlySpan<char> text, ref int i, ref int lineBreakLength)
+		{
+			if (Unicode.IsLineBreak(text[i]))
+			{
+				if (text[i] == '\r' && i + 1 < text.Length && text[i + 1] == '\n')
+				{
+					lineBreakLength = 2;
+					i += 2;
+				}
+				else
+				{
+					lineBreakLength = 1;
+					i++;
+				}
+
+				return true;
+			}
+
+			return false;
 		}
 
 		partial void InvalidateSegmentsPartial() => _segments = null;
