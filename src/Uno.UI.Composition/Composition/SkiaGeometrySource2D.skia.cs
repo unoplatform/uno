4cc9871cdb79d72faffa30da35e861c809ba19b9
Ramez Ragaa <ramezragaa@proton.me>
2024-07-31 00:01:19 +0300

    chore: make SkiaGeometrySource2D read-only

--- src/Uno.UI.Composition/Composition/SkiaGeometrySource2D.skia.cs
+++ src/Uno.UI.Composition/Composition/SkiaGeometrySource2D.skia.cs
@@ -6,25 +6,17 @@ using Windows.Graphics;
 
 namespace Microsoft.UI.Composition
 {
-	internal class SkiaGeometrySource2D : CompositionObject, IGeometrySource2D
+	internal class SkiaGeometrySource2D : IGeometrySource2D
 	{
-		private SKPath _geometry;
-
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 		public SkiaGeometrySource2D(SKPath source)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 		{
 			Geometry = source ?? throw new ArgumentNullException(nameof(source));
 		}
 
 		/// <remarks>
-		/// DO NOT MODIFY THIS SKPath. CREATE A NEW SKPath INSTEAD.
+		/// DO NOT MODIFY THIS SKPath. CREATE A NEW SkiaGeometrySource2D INSTEAD.
 		/// This can lead to nasty invalidation bugs where the SKPath changes without notifying anyone.
 		/// </remarks>
-		public SKPath Geometry
-		{
-			get => _geometry;
-			set => SetObjectProperty(ref _geometry, value);
-		}
+		public SKPath Geometry { get; }
 	}
 }
