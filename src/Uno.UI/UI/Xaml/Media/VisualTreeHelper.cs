97eb0256142f8cff96815d96b81833a6988fb022
Ramez Ragaa <ramezragaa@proton.me>
2024-08-05 18:12:29 +0300

    chore: adjust comments

--- src/Uno.UI/UI/Xaml/Media/VisualTreeHelper.cs
+++ src/Uno.UI/UI/Xaml/Media/VisualTreeHelper.cs
@@ -659,12 +659,12 @@ namespace Microsoft.UI.Xaml.Media
 				}
 			}
 
+			// We didn't find any child at the given position, validate that element can be touched,
+			// and the position is in actual bounds(which might be different than the clipping bounds)
 #if __SKIA__
-			// We still need to check the renderingBounds in case the element is clipped.
 			if (element.HitTest(transformToElement.Inverse().Transform(testPosition)) && renderingBounds.Contains(testPosition))
 #else
-			// We didn't find any child at the given position, validate that element can be touched (i.e. not HitTestability.Invisible),
-			// and the position is in actual bounds (which might be different than the clipping bounds)
+
 			if (elementHitTestVisibility == HitTestability.Visible && renderingBounds.Contains(testPosition))
 #endif
 			{
