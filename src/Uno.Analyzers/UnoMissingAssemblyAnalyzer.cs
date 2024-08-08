c6e355b756de6ee17d002293043d175bc53e95d6
Ramez Ragaa <ramezragaa@proton.me>
2024-08-08 21:42:24 +0300

    chore: remove /* UWP don't rename */

--- src/Uno.Analyzers/UnoMissingAssemblyAnalyzer.cs
+++ src/Uno.Analyzers/UnoMissingAssemblyAnalyzer.cs
@@ -39,7 +39,7 @@ public class UnoMissingAssemblyAnalyzer : DiagnosticAnalyzer
 		{
 			var assemblies = context.Compilation.ReferencedAssemblyNames.Select(a => a.Name).ToImmutableHashSet();
 			var progressRing = context.Compilation.GetTypeByMetadataName("Microsoft" /* UWP don't rename */ + ".UI.Xaml.Controls.ProgressRing");
-			var glCanvasElement = context.Compilation.GetTypeByMetadataName("Microsoft" /* UWP don't rename */ + ".UI.Xaml.Controls.GLCanvasElement");
+			var glCanvasElement = context.Compilation.GetTypeByMetadataName("Microsoft.UI.Xaml.Controls.GLCanvasElement");
 			var mpe = context.Compilation.GetTypeByMetadataName("Microsoft.UI.Xaml.Controls.MediaPlayerElement");
 			_ = context.Options.AnalyzerConfigOptionsProvider.GlobalOptions.TryGetValue("build_property.UnoRuntimeIdentifier", out var unoRuntimeIdentifier);
 			_ = context.Options.AnalyzerConfigOptionsProvider.GlobalOptions.TryGetValue("build_property.IsUnoHead", out var isUnoHead);
