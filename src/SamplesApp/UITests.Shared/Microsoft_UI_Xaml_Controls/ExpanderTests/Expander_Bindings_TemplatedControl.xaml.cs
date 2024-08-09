a9d8f736a1d7255f4aa7528dec19194a2b69a8dc
Andres Pineda <pineda.andres@gmail.com>
2024-08-09 14:27:19 -0400

    test: add UI sample for Expander and bindings

---
+++ src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/ExpanderTests/Expander_Bindings_TemplatedControl.xaml.cs
@@ -0,0 +1,53 @@
+#nullable enable
+using Uno.UI.Samples.Controls;
+using Microsoft.UI.Xaml.Automation.Peers;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml;
+using Microsoft.Graph.Models;
+using System;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UITests.Shared.Microsoft_UI_Xaml_Controls.ExpanderTests;
+
+[Sample("Expander", "MUX", Name = "Expander_Bindings_TemplatedControl", isManualTest: true)]
+public sealed partial class Expander_Bindings_TemplatedControl : UserControl
+{
+	public Expander_Bindings_TemplatedControl()
+	{
+		this.InitializeComponent();
+	}
+}
+
+public partial class SettingsExpander : Control
+{
+	public SettingsExpander() { }
+
+	public object Header
+	{
+		get => (object)GetValue(HeaderProperty);
+		set => SetValue(HeaderProperty, value);
+	}
+
+	public bool IsExpanded
+	{
+		get => (bool)GetValue(IsExpandedProperty);
+		set => SetValue(IsExpandedProperty, value);
+	}
+	protected virtual void OnIsExpandedPropertyChanged(bool oldValue, bool newValue)
+	{
+	}
+
+	public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+		nameof(Header),
+		typeof(object),
+		typeof(SettingsExpander),
+		new PropertyMetadata(defaultValue: null));
+
+	public static readonly DependencyProperty IsExpandedProperty = DependencyProperty.Register(
+		 nameof(IsExpanded),
+		 typeof(bool),
+		 typeof(SettingsExpander),
+		 new PropertyMetadata(defaultValue: false, (d, e) => ((SettingsExpander)d).OnIsExpandedPropertyChanged((bool)e.OldValue, (bool)e.NewValue)));
+}
