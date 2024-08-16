be78006e96f5f79ad893ebf327131bd981b37f97
Ramez Ragaa <ramezragaa@proton.me>
2024-08-16 22:47:36 +0300

    chore: let wasm keyboard events through

--- src/Uno.UI.Runtime.Skia.WebAssembly.Browser/ts/Runtime/BrowserKeyboardInputSource.ts
+++ src/Uno.UI.Runtime.Skia.WebAssembly.Browser/ts/Runtime/BrowserKeyboardInputSource.ts
@@ -29,10 +29,6 @@
 		}
 
 		private onKeyboardEvent(evt: KeyboardEvent): void {
-			if ((evt.target as HTMLElement)?.id !== "uno-canvas" && (evt.target as HTMLElement)?.id !== "uno-input") {
-				return;
-			}
-
 			let result = BrowserKeyboardInputSource._exports.OnNativeKeyboardEvent(
 				this._source,
 				evt.type == "keydown",
