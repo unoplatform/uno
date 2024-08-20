92645e9fd0be620b4e871607928842a5cf8a8efd
Ramez Ragaa <ramezragaa@proton.me>
2024-08-20 12:44:04 +0300

    chore: more X11 WebView

--- src/Uno.UI.Runtime.Skia.X11/X11NativeWebView.cs
+++ src/Uno.UI.Runtime.Skia.X11/X11NativeWebView.cs
@@ -1,62 +1,105 @@
 using System;
+using System.Collections.Concurrent;
 using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading;
 using System.Threading.Tasks;
+using Windows.UI.Core;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.Web.WebView2.Core;
 using SharpWebview;
 using SharpWebview.Content;
+using Uno.Foundation.Logging;
 using Uno.UI.Runtime.Skia;
 using Uno.UI.Xaml.Controls;
 
 namespace Uno.WinUI.Runtime.Skia.X11;
 
-#pragma warning disable CS9113 // Parameter is unread.
 internal class X11NativeWebViewProvider(CoreWebView2 coreWebView2) : INativeWebViewProvider
-#pragma warning restore CS9113 // Parameter is unread.
 {
-	public INativeWebView CreateNativeWebView(ContentPresenter contentPresenter)
-	{
-		var asd = new X11NativeWebView(coreWebView2, contentPresenter);
-		return null!;
-	}
+	public INativeWebView CreateNativeWebView(ContentPresenter contentPresenter) => new X11NativeWebView(coreWebView2, contentPresenter);
 }
 
 internal class X11NativeWebView : INativeWebView
 {
+	private int _jsInvokeCounter;
+	private readonly ConcurrentDictionary<string, TaskCompletionSource<string?>> _jsInvokeTasks = new();
+
 	private Webview _nativeWebview;
-	private readonly CoreWebView2 _coreWebView2;
+
+	private readonly CoreWebView2 _coreWebView;
 	private readonly ContentPresenter _presenter;
 	private readonly string _title = $"Uno WebView {Random.Shared.Next()}";
 
 	public X11NativeWebView(CoreWebView2 coreWebView2, ContentPresenter presenter)
 	{
-		_coreWebView2 = coreWebView2;
+		_coreWebView = coreWebView2;
 		_presenter = presenter;
 
 		_nativeWebview = null!; // to work around nullability;
-		new Thread(() =>
+		_ = Task.Run(() =>
 		{
-			_nativeWebview = new Webview();
+			// WARNING: The threading for Webview is fragile. If you move the Webview construction outside
+			// of the new thread, it won't work. Be careful about moving anything around.
+#if DEBUG
+			_nativeWebview = new Webview(true);
+#else
+			_nativeWebview = new Webview(false);
+#endif
 			_nativeWebview.SetSize(0, 0, WebviewHint.None);
 			_nativeWebview.SetTitle(_title);
+
+			_nativeWebview.Bind("onSourceChanged", OnSourceChanged);
+			_nativeWebview.Bind("onScriptInvocationDone", OnScriptInvocationDone);
+			_nativeWebview.Bind("onDocumentTitleChanged", OnDocumentTitleChanged);
+			_nativeWebview.Bind("onDocumentLoaded", OnDocumentLoaded);
+
+			// from https://learn.microsoft.com/en-us/dotnet/api/microsoft.web.webview2.core.corewebview2.navigationcompleted?view=webview2-dotnet-1.0.2478.35#microsoft-web-webview2-core-corewebview2-navigationcompleted
+			// "NavigationCompleted is raised when the WebView has completely loaded (body.onload has been raised) or loading stopped with error."
+			_nativeWebview.InitScript(
+				"""
+				onSourceChanged({ 'url': window.location.href });
+				
+				document.addEventListener("DOMContentLoaded", (event) => {
+					onDocumentTitleChanged(document.title);
+
+					new MutationObserver(function(mutations) {
+						onDocumentTitleChanged(document.title);
+					}).observe(
+						document.querySelector('title'),
+						{ subtree: true, characterData: true, childList: true }
+					);
+				});
+				
+				document.onload = (event) => {
+					onDocumentLoaded();
+				};
+				""");
 			_nativeWebview.Run();
-		}).Start();
+		});
 
-		// This doesn't seem to be stalling for very long at all, so doing this on the UI thread is
-		// not problematic.
-		var window = IntPtr.Zero;
-		var host = (X11XamlRootHost)X11Manager.XamlRootMap.GetHostForRoot(presenter.XamlRoot!)!;
-		var display = host.RootX11Window.Display;
-		using var lockDisposable = X11Helper.XLock(display);
-		while (window == IntPtr.Zero)
+		_ = Task.Run(() =>
 		{
-			_ = XLib.XQueryTree(display, host.RootX11Window.Window, out IntPtr root, out _, out var children, out _);
-			_ = XLib.XFree(children);
-			window = X11NativeElementHostingExtension.FindWindowByTitle(display, root, _title);
+			var window = IntPtr.Zero;
+			var host = (X11XamlRootHost)X11Manager.XamlRootMap.GetHostForRoot(presenter.XamlRoot!)!;
+			var display = host.RootX11Window.Display;
+			using var lockDisposable = X11Helper.XLock(display);
+			while (window == IntPtr.Zero)
+			{
+				_ = XLib.XQueryTree(display, host.RootX11Window.Window, out IntPtr root, out _, out var children, out _);
+				_ = XLib.XFree(children);
+				window = X11NativeElementHostingExtension.FindWindowByTitle(display, root, _title);
 
-		}
-		_presenter.Content = new X11NativeWindow(window);
+			}
+
+			_ = _coreWebView.Owner.Dispatcher.RunAsync(CoreDispatcherPriority.High, () =>
+			{
+				_presenter.Content = new X11NativeWindow(window);
+			});
+		});
+
+		SpinWait.SpinUntil(() => _nativeWebview is not null);
 	}
 
 	~X11NativeWebView()
@@ -64,14 +107,152 @@ internal class X11NativeWebView : INativeWebView
 		_nativeWebview.Dispose();
 	}
 
+	private void OnSourceChanged(string id, string req)
+	{
+		var rootNode = JsonSerializer.Deserialize<JsonNode>(req);
+		var urlNode = rootNode?[0]?["url"];
+		if (urlNode?.GetValue<string>() is { } url)
+		{
+			_ = _coreWebView.Owner.Dispatcher.RunAsync(CoreDispatcherPriority.High, () =>
+			{
+				_coreWebView.Source = url;
+			});
+		}
+		else
+		{
+			if (this.Log().IsEnabled(LogLevel.Error))
+			{
+				this.Log().Error($"{nameof(OnSourceChanged)} called with invalid json.");
+			}
+		}
+	}
+
+	private void OnScriptInvocationDone(string _, string req)
+	{
+		var rootNode = JsonSerializer.Deserialize<JsonNode>(req);
+		var objNode = rootNode?[0];
+		var idNode = objNode?["id"];
+		var resultNode = objNode?["result"];
+		if (idNode?.ToString() is { } id && resultNode?.ToString() is { } result)
+		{
+			_jsInvokeTasks.TryRemove(id, out var tcs);
+			tcs?.TrySetResult(result);
+		}
+		else
+		{
+			if (this.Log().IsEnabled(LogLevel.Error))
+			{
+				this.Log().Error($"{nameof(OnSourceChanged)} called with invalid json.");
+			}
+		}
+	}
+
+	private void OnDocumentTitleChanged(string id, string req)
+	{
+		var rootNode = JsonSerializer.Deserialize<JsonNode>(req);
+		var titleNode = rootNode?[0];
+
+		if (titleNode?.ToString() is { } title)
+		{
+			_ = _coreWebView.Owner.Dispatcher.RunAsync(CoreDispatcherPriority.High, () =>
+			{
+				_coreWebView.DocumentTitle = title;
+			});
+		}
+		else
+		{
+			if (this.Log().IsEnabled(LogLevel.Error))
+			{
+				this.Log().Error($"{nameof(OnDocumentTitleChanged)} called with invalid json.");
+			}
+		}
+	}
+
+	// Note: this only works on successful page loads. The Webview API doesn't support intercepting errors
+	// or status codes in general.
+	private void OnDocumentLoaded(string id, string req)
+	{
+		_ = _coreWebView.Owner.Dispatcher.RunAsync(CoreDispatcherPriority.High, () =>
+		{
+			_coreWebView.RaiseNavigationCompleted(new Uri(_coreWebView.Source), true, 200, CoreWebView2WebErrorStatus.Unknown);
+		});
+	}
+
 	public void GoBack() => _nativeWebview.Evaluate("history.back();");
 	public void GoForward() => _nativeWebview.Evaluate("history.forward();");
 	public void Stop() => _nativeWebview.Evaluate("window.stop();");
 	public void Reload() => _nativeWebview.Evaluate("window.location.reload();");
-	public void ProcessNavigation(Uri uri) => _nativeWebview.Navigate(new UrlContent(uri.AbsoluteUri));
-	public void ProcessNavigation(string html) => _nativeWebview.Navigate(new HtmlContent(html));
-	public void ProcessNavigation(HttpRequestMessage httpRequestMessage) { }
-	public Task<string?> ExecuteScriptAsync(string script, CancellationToken token) => Task.FromResult<string?>(null);
-	public Task<string?> InvokeScriptAsync(string script, string[]? arguments, CancellationToken token) => Task.FromResult<string?>(null);
-	public void SetScrollingEnabled(bool isScrollingEnabled) { }
+
+	private void ScheduleNavigationStarting(Uri uri, Action loadAction)
+	{
+		_coreWebView.RaiseNavigationStarting(uri, out var cancel);
+
+		if (!cancel)
+		{
+			loadAction.Invoke();
+		}
+	}
+
+	private void ScheduleNavigationStarting(string html, Action loadAction)
+	{
+		_coreWebView.RaiseNavigationStarting(html, out var cancel);
+
+		if (!cancel)
+		{
+			loadAction.Invoke();
+		}
+	}
+
+	// Note: Webview doesn't support web Navigation APIs, so we cannot set CoreWebView2.CanGo<Back|Forward>
+	public void ProcessNavigation(Uri uri) => ScheduleNavigationStarting(uri, () =>
+	{
+		_nativeWebview.Dispatch(() => _nativeWebview.Navigate(new UrlContent(uri.AbsoluteUri)));
+	});
+	public void ProcessNavigation(string html) => ScheduleNavigationStarting(html, () =>
+	{
+		_nativeWebview.Dispatch(() => _nativeWebview.Navigate(new HtmlContent(html)));
+	});
+	public void ProcessNavigation(HttpRequestMessage httpRequestMessage)
+	{
+		if (this.Log().IsEnabled(LogLevel.Error))
+		{
+			this.Log().Error($"{nameof(ProcessNavigation)} is not supported on the X11 target.");
+		}
+	}
+
+	public Task<string?> ExecuteScriptAsync(string script, CancellationToken token)
+	{
+		// we add an eval call so that we can get a return value even if `script` is not an expression.
+		return InvokeScriptAsync("eval", new[] { script }, token);
+	}
+
+	public Task<string?> InvokeScriptAsync(string script, string[]? arguments, CancellationToken token)
+	{
+		var tcs = new TaskCompletionSource<string?>();
+		var jsInvokeId = Interlocked.Increment(ref _jsInvokeCounter).ToString();
+
+		// JsonSerializer.Serialize safely escapes quotes and concatenates the arguments (with a comma) to be passed to eval
+		// the [1..^1] part is to remove [ and ].
+		var argumentString = arguments is not null ? JsonSerializer.Serialize(arguments)[1..^1] : "";
+
+		_nativeWebview.Dispatch(() => _nativeWebview.Evaluate($"onScriptInvocationDone({{ id: {jsInvokeId}, result: {script}({argumentString}) }} );"));
+		_jsInvokeTasks.TryAdd(jsInvokeId, tcs);
+
+		token.Register(() =>
+		{
+			_jsInvokeTasks.TryRemove(jsInvokeId, out var tcs_);
+			tcs_?.TrySetCanceled();
+		});
+
+		return tcs.Task;
+	}
+
+
+	public void SetScrollingEnabled(bool isScrollingEnabled)
+	{
+		if (this.Log().IsEnabled(LogLevel.Error))
+		{
+			this.Log().Error($"{nameof(SetScrollingEnabled)} is not supported on the X11 target.");
+		}
+	}
 }
