e8329a41d4db4a838ba86ceb5b08212275c9f451
Ramez Ragaa <ramezragaa@proton.me>
2024-08-25 00:23:27 +0300

    chore: use selenium to start up the app instead of chromium-browser

---
+++ build/test-scripts/skia-browserwasm-startup-script.py
@@ -0,0 +1,23 @@
+import sys
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+if len(sys.argv) != 2:
+    printf("Incorrect number of arguments, expected 1 argument for startup url.", sys.argv)
+    sys.exit(1)
+
+service = Service(ChromeDriverManager().install())
+options = webdriver.ChromeOptions()
+options.add_argument('--enable-logging=stderr')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--headless")
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get(sys.argv[1])
+
+
+time.sleep(999999)
+driver.quit()
+
