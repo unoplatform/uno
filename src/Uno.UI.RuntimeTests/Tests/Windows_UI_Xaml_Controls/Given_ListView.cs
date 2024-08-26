f86a6b052917352647ab57fe44ba069800ec9b63
vatsashah45 <vatsashah45@gmail.com>
2024-08-26 17:05:18 -0400

    fix: Refactored LoggingSelectionInfo

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ListView.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ListView.cs
@@ -6,7 +6,6 @@ using Uno.UI.RuntimeTests.Helpers;
 using Windows.Foundation;
 using Windows.UI.Input.Preview.Injection;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
 using System.Linq;
 
 namespace Uno.UI.RuntimeTests.Tests.Microsoft_UI_Xaml_Controls
@@ -17,21 +16,19 @@ namespace Uno.UI.RuntimeTests.Tests.Microsoft_UI_Xaml_Controls
 		[TestMethod]
 		[RunsOnUIThread]
 #if !HAS_INPUT_INJECTOR
-		[Ignore("InputInjector is only supported on Skia #14948")]
+		[Ignore("InputInjector is not supported on this platform.")]
 #endif
 		public async Task When_ItemClicked_SelectsCorrectIndex()
 		{
 			var items = new object[] { "Item 1", "Item 2", "Item 3" };
-			var loggingSelectionInfo = new LoggingSelectionInfo(items, false, null);
+			var loggingSelectionInfo = new LoggingSelectionInfo(items);
 			var listViewBase = new ListView
 			{
-				ItemsSource = items,
+				ItemsSource = loggingSelectionInfo,
 			};
 
 			TestServices.WindowHelper.WindowContent = listViewBase;
 			await TestServices.WindowHelper.WaitForLoaded(listViewBase);
-
-			loggingSelectionInfo.SafeMoveCurrentToPosition(0);
 
 			// We don't use ActualWidth because of https://github.com/unoplatform/uno/issues/15982
 			var tapTarget = listViewBase.TransformToVisual(null).TransformPoint(new Point(112 * 0.9, listViewBase.ActualHeight / 2));
@@ -42,6 +39,7 @@ namespace Uno.UI.RuntimeTests.Tests.Microsoft_UI_Xaml_Controls
 			finger.Release();
 
 			Assert.AreEqual(0, listViewBase.SelectedIndex);
+			Assert.IsTrue(loggingSelectionInfo.IsSelected(0), "The item at index 0 should be selected.");
 			Assert.IsTrue(loggingSelectionInfo.MethodLog.All(m => !m.Contains("MoveCurrent", StringComparison.OrdinalIgnoreCase)));
 		}
 	}
