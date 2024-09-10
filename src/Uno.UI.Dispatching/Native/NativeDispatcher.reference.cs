cd5478d222e62f10c926ce456aefc970bc136e73
Youssef Victor <youssefvictor00@gmail.com>
2024-09-10 23:40:31 +0300

    chore: Cleanup native dispatcher as well

--- src/Uno.UI.Dispatching/Native/NativeDispatcher.reference.cs
+++ src/Uno.UI.Dispatching/Native/NativeDispatcher.reference.cs
@@ -5,12 +5,6 @@ namespace Uno.UI.Dispatching
 {
 	internal sealed partial class NativeDispatcher
 	{
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		internal static Action<Action> DispatchOverride;
-
-		[EditorBrowsable(EditorBrowsableState.Never)]
-		internal static Func<bool> HasThreadAccessOverride;
-
 		private bool GetHasThreadAccess() => throw new NotSupportedException("Ref assembly");
 	}
 }
