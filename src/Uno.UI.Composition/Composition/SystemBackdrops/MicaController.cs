323a7e5a6d89a6bc0f16207ef84cb4379671320e
Martin Zikmund <martin@zikmund.dev>
2024-09-17 10:12:13 +0200

    chore: Interfaces specific to WinUI on MicaController

--- src/Uno.UI.Composition/Composition/SystemBackdrops/MicaController.cs
+++ src/Uno.UI.Composition/Composition/SystemBackdrops/MicaController.cs
@@ -1,6 +1,11 @@
+using System;
+
 namespace Microsoft.UI.Composition.SystemBackdrops;
 
-public partial class MicaController : ISystemBackdropController, IDisposable, ISystemBackdropControllerWithTargets, IClosableNotifier
+public partial class MicaController
+#if HAS_UNO_WINUI
+	: ISystemBackdropController, IDisposable, ISystemBackdropControllerWithTargets, IClosableNotifier
+#endif
 {
 	/// <summary>
 	/// Determines whether the mica material is supported on the current operating system.
