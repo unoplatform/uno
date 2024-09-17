3afc92f08a528b9b6ec9351b7377355a50a6f8c8
Youssef Victor <youssefvictor00@gmail.com>
2024-09-18 01:24:56 +0300

    chore: Fix few tests

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_FrameworkElement.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_FrameworkElement.cs
@@ -638,16 +638,10 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 				Assert.AreEqual(new Size(double.PositiveInfinity, double.PositiveInfinity), SUT.MeasureOverrides.Last());
 				Assert.AreEqual(new Size(0, 0), SUT.DesiredSize);
 
-#if __CROSSRUNTIME__
 				// Unlike WinUI, we don't crash.
 				SUT.Measure(new Size(double.NaN, double.NaN));
 				SUT.Measure(new Size(42.0, double.NaN));
 				SUT.Measure(new Size(double.NaN, 42.0));
-#else
-				Assert.ThrowsException<InvalidOperationException>(() => SUT.Measure(new Size(double.NaN, double.NaN)));
-				Assert.ThrowsException<InvalidOperationException>(() => SUT.Measure(new Size(42.0, double.NaN)));
-				Assert.ThrowsException<InvalidOperationException>(() => SUT.Measure(new Size(double.NaN, 42.0)));
-#endif
 			});
 
 		[TestMethod]
