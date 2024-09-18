c6b0514295a3d8e332bf83caa32b949a7011aaf0
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2024-09-18 21:19:13 +0200

    test: In app review

--- src/SamplesApp/SamplesApp.Shared/App.xaml.cs
+++ src/SamplesApp/SamplesApp.Shared/App.xaml.cs
@@ -545,6 +545,9 @@ namespace SamplesApp
 
 			Uno.UI.FeatureConfiguration.Font.DefaultTextFontFamily = "ms-appx:///Uno.Fonts.OpenSans/Fonts/OpenSans.ttf";
 #endif
+#if __ANDROID__
+			Uno.WinRTFeatureConfiguration.StoreContext.TestMode = true;
+#endif
 		}
 
 		/// <summary>
