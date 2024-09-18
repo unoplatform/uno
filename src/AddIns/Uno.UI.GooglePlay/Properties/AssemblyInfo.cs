61041aee49aafd12028785fbf96120113b6795f1
Martin Zikmund <martin@zikmund.dev>
2024-09-18 09:57:18 +0200

    feat: Uno.UI.GooglePlay

---
+++ src/AddIns/Uno.UI.GooglePlay/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using Uno.Foundation.Extensibility;
+using Uno.UI.Xaml.Media.Imaging.Svg;
+
+[assembly: AssemblyMetadata("IsTrimmable", "True")]
+
+[assembly: ApiExtension(typeof(IInAppReviewExtension), typeof(InAppReviewExtension))]
