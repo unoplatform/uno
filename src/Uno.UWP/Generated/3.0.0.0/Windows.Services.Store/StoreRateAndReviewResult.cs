4bb5ae0f667d7cc184f2b64180836114e4abdee0
Martin Zikmund <martin@zikmund.dev>
2024-09-18 11:29:56 +0200

    chore: Adjust generated

--- src/Uno.UWP/Generated/3.0.0.0/Windows.Services.Store/StoreRateAndReviewResult.cs
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.Services.Store/StoreRateAndReviewResult.cs
@@ -23,7 +23,7 @@ namespace Windows.Services.Store
 			}
 		}
 #endif
-#if false
+#if __ANDROID__ || __IOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
 		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 		public string ExtendedJsonData
 		{
@@ -43,7 +43,7 @@ namespace Windows.Services.Store
 			}
 		}
 #endif
-#if false
+#if __ANDROID__ || __IOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
 		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 		public bool WasUpdated
 		{
