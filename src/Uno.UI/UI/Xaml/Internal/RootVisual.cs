b651e557788a5b33fca4eb03ab7c1fcf531e01ee
Youssef Victor <youssefvictor00@gmail.com>
2024-09-22 17:42:40 +0300

    chore: Fix for orientation changes

--- src/Uno.UI/UI/Xaml/Internal/RootVisual.cs
+++ src/Uno.UI/UI/Xaml/Internal/RootVisual.cs
@@ -105,4 +105,12 @@ internal partial class RootVisual : Panel, IRootElement
 
 		return finalSize;
 	}
+
+	protected override void OnLayoutCore(bool changed, int left, int top, int right, int bottom, bool localIsLayoutRequested)
+	{
+		base.OnLayoutCore(changed, left, top, right, bottom, localIsLayoutRequested);
+
+		// Important for device orientation changes.
+		InvalidateMeasure();
+	}
 }
