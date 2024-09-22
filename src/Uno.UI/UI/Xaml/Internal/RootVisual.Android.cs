a341a6a64810ee97f0982ee886e00c7b9f4f3329
Youssef Victor <youssefvictor00@gmail.com>
2024-09-22 21:37:33 +0300

    chore: Move to partial

---
+++ src/Uno.UI/UI/Xaml/Internal/RootVisual.Android.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+
+namespace Uno.UI.Xaml.Core;
+
+partial class RootVisual
+{
+	protected override void OnLayoutCore(bool changed, int left, int top, int right, int bottom, bool localIsLayoutRequested)
+	{
+		base.OnLayoutCore(changed, left, top, right, bottom, localIsLayoutRequested);
+
+		// Important for device orientation changes.
+		InvalidateMeasure();
+	}
+}
--- src/Uno.UI/UI/Xaml/Internal/RootVisual.cs
+++ src/Uno.UI/UI/Xaml/Internal/RootVisual.cs
@@ -105,12 +105,4 @@ internal partial class RootVisual : Panel, IRootElement
 
 		return finalSize;
 	}
-
-	protected override void OnLayoutCore(bool changed, int left, int top, int right, int bottom, bool localIsLayoutRequested)
-	{
-		base.OnLayoutCore(changed, left, top, right, bottom, localIsLayoutRequested);
-
-		// Important for device orientation changes.
-		InvalidateMeasure();
-	}
 }
