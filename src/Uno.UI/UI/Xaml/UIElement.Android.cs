fa96edb4faa1f72f36b8875c9796cf582c587958
Youssef Victor <youssefvictor00@gmail.com>
2024-09-23 12:33:44 +0300

    chore: Progress

--- src/Uno.UI/UI/Xaml/UIElement.Android.cs
+++ src/Uno.UI/UI/Xaml/UIElement.Android.cs
@@ -192,19 +192,19 @@ namespace Microsoft.UI.Xaml
 			{
 				// Native components could call RequestLayout to invalidate themselves.
 				// Internally, Android will keep calling RequestLayout on parents, then we get to the first managed element in this code path.
-				// However, the native elements in between the managed element and the original RequestLayout call were not marked as measure dirty path.
-				// We take the chance to mark them as measure dirty path here.
+				// However, the native elements in between the managed element and the original RequestLayout call were not marked as measure/arrange dirty path.
+				// We take the chance to mark them as measure/arrange dirty path here.
 				// Note that this code may mark more than needed, but we don't have a way to know which element initiated
 				// the RequestLayout call, we only know of the first managed parent
 				InvalidateMeasure();
-				InvalidateNativeOnlyChildrenRecursive(this);
+				InvalidateMeasureOnNativeOnlyChildrenRecursive(this);
 				return true;
 			}
 
 			return false;
 		}
 
-		private static void InvalidateNativeOnlyChildrenRecursive(ViewGroup view)
+		private static void InvalidateMeasureOnNativeOnlyChildrenRecursive(ViewGroup view)
 		{
 			foreach (var child in view.GetChildren())
 			{
@@ -213,7 +213,7 @@ namespace Microsoft.UI.Xaml
 					LayoutInformation.SetMeasureDirtyPath(child, true);
 					if (child is ViewGroup childAsViewGroup)
 					{
-						InvalidateNativeOnlyChildrenRecursive(childAsViewGroup);
+						InvalidateMeasureOnNativeOnlyChildrenRecursive(childAsViewGroup);
 					}
 				}
 			}
