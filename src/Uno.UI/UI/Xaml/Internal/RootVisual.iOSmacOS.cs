bde08c86936495edbb997a4898a5d6b53988fdac
Youssef Victor <youssefvictor00@gmail.com>
2024-09-26 11:46:41 +0300

    chore: RootVisual for iOS/macOS

---
+++ src/Uno.UI/UI/Xaml/Internal/RootVisual.iOSmacOS.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+
+using CoreGraphics;
+using Windows.Foundation;
+
+namespace Uno.UI.Xaml.Core;
+
+partial class RootVisual
+{
+	private CGRect _lastDimensions;
+
+	public override void LayoutSubviews()
+	{
+		base.LayoutSubviews();
+
+		UpdateLayout();
+
+		if (_lastDimensions != Frame)
+		{
+			// Important for device orientation changes.
+			_lastDimensions = Frame;
+			InvalidateMeasure();
+		}
+	}
+}
