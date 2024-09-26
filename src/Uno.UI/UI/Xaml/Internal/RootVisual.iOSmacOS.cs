9b31ef5a821180a56f4a54586f62ac09ba083c8d
Youssef Victor <youssefvictor00@gmail.com>
2024-09-26 13:17:31 +0300

    chore: Fix build error on macOS

--- src/Uno.UI/UI/Xaml/Internal/RootVisual.iOSmacOS.cs
+++ src/Uno.UI/UI/Xaml/Internal/RootVisual.iOSmacOS.cs
@@ -9,10 +9,15 @@ partial class RootVisual
 {
 	private CGRect _lastDimensions;
 
+#if __IOS__
 	public override void LayoutSubviews()
 	{
 		base.LayoutSubviews();
-
+#else
+	public override void Layout()
+	{
+		base.Layout();
+#endif
 		UpdateLayout();
 
 		if (_lastDimensions != Frame)
