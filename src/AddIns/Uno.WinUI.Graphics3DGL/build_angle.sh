21cd6b0f0d69241d4e31cbec44c98420b95853f4
Ramez Ragaa <ramezragaa@proton.me>
2024-10-04 16:13:55 -0400

    chore: minor angle build script touches

--- src/AddIns/Uno.WinUI.Graphics3DGL/build_angle.sh
+++ src/AddIns/Uno.WinUI.Graphics3DGL/build_angle.sh
@@ -19,7 +19,12 @@ if [ ! -f angle_source/angle_fetched_successfully ]; then
     rm -rf angle_source
     mkdir angle_source
     cd angle_source
-    fetch angle
+    fetch --no-history angle
+
+    # remove unnecessary dirs for space
+    rm -rf third_party/dawn third_party/VK-GL-CTS
+    find ./tools ./third_party -name "*.git" | xargs rm -rf
+
     touch angle_fetched_successfully
 else
     cd angle_source
@@ -32,10 +37,18 @@ do
         rm -rf "out/Release_${arch}"
         mkdir -p "out/Release_${arch}"
         
+        # we remove as much as we can from the build to save space for the space-limited CI machines
         cat << EOF > "out/Release_${arch}/args.gn"
 target_cpu = "${arch}"
 is_debug = false
 angle_assert_always_on = true
+angle_build_tests = false
+build_angle_deqp_tests = false
+build_angle_perftests = false
+angle_enable_vulkan = false
+angle_enable_wgpu = false
+angle_enable_abseil = false
+angle_enable_gl_null = false
 EOF
 
         gn gen "out/Release_${arch}"
@@ -51,7 +64,9 @@ popd > /dev/null
 mkdir -p angle_binaries/osx
 
 # create universal mac binaries
-for lib in libEGL.dylib libGLESv2.dylib
+shopt -s nullglob
+for lib in libEGL.* libGLESv2.*
 do
     lipo -create angle/angle_source/out/Release_arm64/${lib} angle/angle_source/out/Release_x64/${lib} -output angle_binaries/osx/${lib}
 done
+shopt -u nullglob
