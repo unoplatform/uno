2698365fc070058532151b4c5150bfef5e1d0b9f
Ramez Ragaa <ramezragaa@proton.me>
2024-10-07 20:28:54 +0300

    chore: PR comments

--- src/AddIns/Uno.WinUI.Graphics3DGL/build_angle.sh
+++ src/AddIns/Uno.WinUI.Graphics3DGL/build_angle.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 mkdir -p angle_binaries/osx
 pushd angle_binaries/osx
-curl -O -L https://github.com/dotnet/Silk.NET/raw/refs/heads/main/src/Native/Silk.NET.OpenGLES.ANGLE.Native/runtimes/osx/native/libEGL.dylib
-curl -O -L https://github.com/dotnet/Silk.NET/raw/refs/heads/main/src/Native/Silk.NET.OpenGLES.ANGLE.Native/runtimes/osx/native/libGLESv2.dylib
+curl -O -L https://github.com/dotnet/Silk.NET/raw/a9c75a61528ee7b4e55442f5141cb132f10d72ff/src/Native/Silk.NET.OpenGLES.ANGLE.Native/runtimes/osx/native/libEGL.dylib
+curl -O -L https://github.com/dotnet/Silk.NET/raw/a9c75a61528ee7b4e55442f5141cb132f10d72ff/src/Native/Silk.NET.OpenGLES.ANGLE.Native/runtimes/osx/native/libGLESv2.dylib
 popd
