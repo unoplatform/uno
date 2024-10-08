86dfaca257769ab9b23cd978a0e1b5f554a23b7b
David <github.site@torick.net>
2024-10-08 16:25:57 -0400

    feat: Add more info

--- src/Uno.UI.RemoteControl.Messaging/IDEChannel/NotificationIdeMessage.cs
+++ src/Uno.UI.RemoteControl.Messaging/IDEChannel/NotificationIdeMessage.cs
@@ -8,10 +8,11 @@ namespace Uno.UI.RemoteControl.Messaging.IDEChannel;
 /// <summary>
 /// A message sent by the sev-server to the IDE to notify the user.
 /// </summary>
-/// <param name="Title">Title of the notification.</param>
+/// <param name="Title">Kind of the notification.</param>
+/// <param name="Title">Title of the notification (might not be visible on all IDEs).</param>
 /// <param name="Message">The message of the notification.</param>
 /// <param name="Commands">For call-to-action notification, set of commands to show with the notification.</param>
-public record NotificationIdeMessage(string Title, string Message, Command[] Commands) : IdeMessage(WellKnownScopes.Ide);
+public record NotificationIdeMessage(NotificationKind Kind, string Title, string Message, Command[] Commands) : IdeMessage(WellKnownScopes.Ide);
 
 /// <summary>
 /// Description of a command to execute.
@@ -20,3 +21,12 @@ public record NotificationIdeMessage(string Title, string Message, Command[] Com
 /// <param name="Name">The name/id of the command (e.g. uno.hotreload.open_hotreload_window).</param>
 /// <param name="Parameter">A json serialized parameter to execute the command.</param>
 public record Command(string Text, string Name, string? Parameter = null);
+
+/// <summary>
+/// Kind of <see cref="NotificationIdeMessage"/>
+/// </summary>
+public enum NotificationKind
+{
+	Information,
+	Error
+}
