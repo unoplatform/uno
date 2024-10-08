1dfa8242c5d94f392ad6248721e9108b17a75854
Ramez Ragaa <ramezragaa@proton.me>
2024-10-08 12:51:00 +0300

    chore: use DownloadFile instead of a script

--- src/AddIns/Uno.WinUI.Graphics3DGL/get_angle_binaries.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin/env bash
-set -euo pipefail
-
-mkdir -p angle_binaries/osx
-pushd angle_binaries/osx
-curl -O -L https://github.com/dotnet/Silk.NET/raw/a9c75a61528ee7b4e55442f5141cb132f10d72ff/src/Native/Silk.NET.OpenGLES.ANGLE.Native/runtimes/osx/native/libEGL.dylib
-curl -O -L https://github.com/dotnet/Silk.NET/raw/a9c75a61528ee7b4e55442f5141cb132f10d72ff/src/Native/Silk.NET.OpenGLES.ANGLE.Native/runtimes/osx/native/libGLESv2.dylib
-popd
