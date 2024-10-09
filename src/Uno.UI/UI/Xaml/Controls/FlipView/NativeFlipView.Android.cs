a7637f8e2a87f088fd260b61c73278a87b981e8b
Youssef Victor <youssefvictor00@gmail.com>
2024-10-09 18:37:37 +0300

    chore: Progress and cleanup

--- src/Uno.UI/UI/Xaml/Controls/FlipView/NativeFlipView.Android.cs
+++ src/Uno.UI/UI/Xaml/Controls/FlipView/NativeFlipView.Android.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Android.Views;
 
 namespace Microsoft.UI.Xaml.Controls
 {
 	public partial class NativeFlipView : NativePagedView
 	{
-		public override void OnViewAdded(View child) => base.OnViewAdded(child);
 	}
 }
