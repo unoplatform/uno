0433d0929dcb1dee5619c1d6d11fd31ee276fb25
David <github.site@torick.net>
2024-10-09 12:55:38 -0400

    feat: Add message for to add a menu in the IDE

--- src/Uno.UI.RemoteControl.Messaging/IdeChannel/NotificationRequestIdeMessage.cs
+++ src/Uno.UI.RemoteControl.Messaging/IdeChannel/NotificationRequestIdeMessage.cs
@@ -11,18 +11,10 @@ namespace Uno.UI.RemoteControl.Messaging.IdeChannel;
 /// <param name="Title">Title of the notification (might not be visible on all IDEs).</param>
 /// <param name="Message">The message of the notification.</param>
 /// <param name="Commands">For call-to-action notification, set of commands to show with the notification.</param>
-public record NotificationIdeMessage(NotificationKind Kind, string Title, string Message, Command[] Commands) : IdeMessage(WellKnownScopes.Ide);
+public record NotificationRequestIdeMessage(NotificationKind Kind, string Title, string Message, Command[]? Commands = default) : IdeMessage(WellKnownScopes.Ide);
 
 /// <summary>
-/// Description of a command to execute.
-/// </summary>
-/// <param name="Text">Text content to display to the user.</param>
-/// <param name="Name">The name/id of the command (e.g. uno.hotreload.open_hotreload_window).</param>
-/// <param name="Parameter">A json serialized parameter to execute the command.</param>
-public record Command(string Text, string Name, string? Parameter = null);
-
-/// <summary>
-/// Kind of <see cref="NotificationIdeMessage"/>
+/// Kind of <see cref="NotificationRequestIdeMessage"/>
 /// </summary>
 public enum NotificationKind
 {
