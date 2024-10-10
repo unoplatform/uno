bddcb97ea00da3fec3dde4172e44960a173c205c
McNets <mcnets@gmail.com>
2024-10-10 09:22:23 +0200

    feat: Rebasing to master 6.x.
    ^ Conflicts:
    ^	src/Uno.UI.Runtime.Skia.Gtk/GtkHost.cs
    ^	src/Uno.UI.Runtime.Skia.Linux.FrameBuffer/FramebufferHost.cs
    ^	src/Uno.UI.Runtime.Skia.MacOS/MacSkiaHost.cs
    ^	src/Uno.UI.Runtime.Skia.Wpf/WpfHost.cs
    ^	src/Uno.UI.Runtime.Skia.X11/X11ApplicationHost.cs

---
+++ src/Uno.UI/Helpers/PlatformRuntimeHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.UI.Helpers;
+
+public static class PlatformRuntimeHelper
+{
+	internal static UnoRuntimePlatform SkiaPlatform { get; set; } = UnoRuntimePlatform.Skia;
+
+	public static UnoRuntimePlatform Current => GetPlatform();
+
+	private static UnoRuntimePlatform GetPlatform() =>
+#if __ANDROID__
+		UnoRuntimePlatform.Android;
+#elif __IOS__
+		UnoRuntimePlatform.iOS;
+#elif __MACCATALYST__
+		UnoRuntimePlatform.MacCatalyst;
+#elif __MACOS__
+		UnoRuntimePlatform.MacOSX;
+#elif __WASM__
+		UnoRuntimePlatform.WebAssembly;
+#elif __SKIA__
+		SkiaPlatform;
+#elif !HAS_UNO
+		UnoRuntimePlatform.Windows;
+#else
+		UnoRuntimePlatform.Unkonwn;
+#endif
+
+}
