74e3230ceadc76fd1400c6a06931a70522930520
McNets <mcnets@gmail.com>
2024-10-15 16:05:52 +0200

    feat: Added a RuntimeHelper to Toolkit

---
+++ src/Uno.UI.Toolkit/RuntimeHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uno.UI.Helpers;
+
+namespace Uno.UI.Toolkit;
+
+public static class RuntimeHelper
+{
+	public static UnoRuntimePlatform CurrentPlatform => PlatformRuntimeHelper.Current;
+}
