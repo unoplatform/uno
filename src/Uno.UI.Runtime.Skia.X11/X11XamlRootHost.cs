6475d1a4cde0c0111c91794ea8abdb0a748c8f60
Ramez Ragaa <ramezragaa@proton.me>
2024-10-16 15:12:19 +0300

    chore: renaming

--- src/Uno.UI.Runtime.Skia.X11/X11XamlRootHost.cs
+++ src/Uno.UI.Runtime.Skia.X11/X11XamlRootHost.cs
@@ -100,7 +100,7 @@ internal partial class X11XamlRootHost : IXamlRootHost
 		_configureCallback = configureCallback;
 
 		_renderTimer = new DispatcherTimer();
-		_renderTimer.Interval = TimeSpan.FromSeconds(1.0 / X11ApplicationHost.TargetFps); // we're on the UI thread
+		_renderTimer.Interval = TimeSpan.FromSeconds(1.0 / X11ApplicationHost.RenderFrameRate); // we're on the UI thread
 		_renderTimer.Tick += (sender, o) =>
 		{
 			if (Interlocked.Exchange(ref _needsConfigureCallback, 0) == 1)
