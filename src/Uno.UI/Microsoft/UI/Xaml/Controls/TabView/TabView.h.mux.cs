9a41474cbd64e7badfab9af82b6ae69ecd479cde
Martin Zikmund <martin@zikmund.dev>
2024-10-22 14:17:33 +0200

    chore: Update TabView.h to winiu3/release/1.6.1

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/TabView/TabView.h.mux.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/TabView/TabView.h.mux.cs
@@ -1,23 +1,34 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-// MUX Reference TabView.cpp, commit 367bb0d512cd
+// MUX Reference src\controls\dev\TabView\TabView.h, commit d74a0332
 
-using Uno.Disposables;
-using Uno.UI.Helpers.WinUI;
-using Windows.Foundation;
-using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.UI.Content;
+using Microsoft.UI.Input;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml.Controls.Primitives;
+using Uno.Disposables;
+using Uno.UI.DataBinding;
+using Windows.Foundation;
+using Windows.Graphics;
 
 namespace Microsoft/* UWP don't rename */.UI.Xaml.Controls;
 
+internal enum TabTearOutDraggingState
+{
+	Idle,
+	DraggingTabWithinTabView,
+	DraggingTornOutTab,
+};
+
 public partial class TabView
 {
-	internal const double c_tabShadowDepth = 16.0;
-	internal const string c_tabViewShadowDepthName = "TabViewShadowDepth";
-
-	internal UIElement GetShadowReceiver() => m_shadowReceiver;
-
 	internal string GetTabCloseButtonTooltipText() => m_tabCloseButtonTooltipText;
+
+	private static List<ManagedWeakReference> s_tabViewWithTearOutList;
+
+	private static Mutex s_tabWithTearOutListMutex;
 
 	private bool m_updateTabWidthOnPointerLeave = false;
 	private bool m_pointerInTabstrip = false;
@@ -37,13 +48,12 @@ public partial class TabView
 	private Button m_addButton;
 	private ItemsPresenter m_itemsPresenter;
 
-	private Grid m_shadowReceiver;
-
 	private readonly SerialDisposable m_listViewLoadedRevoker = new();
 	private readonly SerialDisposable m_tabStripPointerExitedRevoker = new();
 	private readonly SerialDisposable m_tabStripPointerEnteredRevoker = new();
 	private readonly SerialDisposable m_listViewSelectionChangedRevoker = new();
 	private readonly SerialDisposable m_listViewGettingFocusRevoker = new();
+	private readonly SerialDisposable m_listViewSizeChangedRevoker = new();
 
 	private readonly SerialDisposable m_listViewCanReorderItemsPropertyChangedRevoker = new();
 	private readonly SerialDisposable m_listViewAllowDropPropertyChangedRevoker = new();
@@ -52,6 +62,8 @@ public partial class TabView
 	private readonly SerialDisposable m_listViewDragItemsCompletedRevoker = new();
 	private readonly SerialDisposable m_listViewDragOverRevoker = new();
 	private readonly SerialDisposable m_listViewDropRevoker = new();
+	private readonly SerialDisposable m_listViewDragEnterRevoker = new();
+	private readonly SerialDisposable m_listViewDragLeaveRevoker = new();
 
 	private readonly SerialDisposable m_scrollViewerLoadedRevoker = new();
 	private readonly SerialDisposable m_scrollViewerViewChangedRevoker = new();
@@ -65,13 +77,38 @@ public partial class TabView
 
 	private readonly SerialDisposable m_itemsPresenterSizeChangedRevoker = new();
 
-	private DispatcherHelper m_dispatcherHelper;
-
 	private string m_tabCloseButtonTooltipText;
 
-	private Size m_previousAvailableSize;
+	private new Size m_previousAvailableSize; // TODO:MZ: Avoid new here
 
-	private bool m_isDragging;
+	private bool m_isItemBeingDragged;
+	private bool m_isItemDraggedOver;
+	private double? m_expandedWidthForDragOver;
+
+	private SerialDisposable m_enteringMoveSizeToken = new();
+	private SerialDisposable m_enteredMoveSizeToken = new();
+	private SerialDisposable m_windowRectChangingToken = new();
+	private SerialDisposable m_exitedMoveSizeToken = new();
+
+	private bool m_isInTabTearOutLoop;
+	private AppWindow m_tabTearOutNewAppWindow;
+	private object m_dataItemBeingDragged;
+	private TabViewItem m_tabBeingDragged;
+	private TabView m_tabViewContainingTabBeingDragged;
+	private TabView m_tabViewInNewAppWindow;
+	private Point m_originalTabBeingDraggedPoint;
+	private Point m_dragPositionOffset;
+	private TabTearOutDraggingState m_tabTearOutDraggingState = TabTearOutDraggingState.Idle;
+	private PointInt32 m_tabTearOutInitialPosition;
+
+	private RectInt32 m_nonClientRegion;
+	private bool m_nonClientRegionSet;
+
+	private List<(RectInt32, TabView)> m_tabViewBoundsTuples;
+
+	private WindowId m_lastAppWindowId;
+	private InputNonClientPointerSource m_inputNonClientPointerSource;
+	private ContentCoordinateConverter m_appWindowCoordinateConverter;
 
 #if HAS_UNO
 	//TODO Uno specific: Watches scrollable width to update visibility of scroll buttons.
