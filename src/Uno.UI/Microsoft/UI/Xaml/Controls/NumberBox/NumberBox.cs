e711b220ffa2ce9d063d573628a245bb6a8f8a3f
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2024-10-22 12:08:14 +0200

    chore: Remove UpdateTextToValue IsTypePresent check

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/NumberBox/NumberBox.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/NumberBox/NumberBox.cs
@@ -682,15 +682,7 @@ namespace Microsoft/* UWP don't rename */.UI.Xaml.Controls
 				{
 					// Rounding the value here will prevent displaying digits caused by floating point imprecision.
 					var roundedValue = m_displayRounder.RoundDouble(value);
-
-					if (ApiInformation.IsTypePresent(NumberFormatter.GetType().FullName))
-					{
-						newText = NumberFormatter.FormatDouble(roundedValue);
-					}
-					else
-					{
-						newText = roundedValue.ToString($"0." + new string('#', 6), CultureInfo.CurrentCulture);
-					}
+					newText = NumberFormatter.FormatDouble(roundedValue);
 				}
 
 				m_textBox.Text = newText;
