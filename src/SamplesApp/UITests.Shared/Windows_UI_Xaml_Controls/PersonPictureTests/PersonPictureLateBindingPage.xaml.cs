4b2df2795d7565c9e5d4dcf5e8c6c0950b1fd20a
Andres Pineda <pineda.andres@gmail.com>
2024-10-25 11:55:08 -0400

    test: PersonPicture add manual test

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/PersonPictureTests/PersonPictureLateBindingPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.ObjectModel;
+using MUXControlsTestApp.Utilities;
+using Uno.UI.Samples.Controls;
+using Windows.ApplicationModel.Contacts;
+using System.Threading.Tasks;
+
+namespace MUXControlsTestApp
+{
+	[SampleControlInfo(
+		"MUX",
+		"PersonPictureLateBinding",
+		isManualTest: true)]
+#pragma warning disable UXAML0002 // does not explicitly define the Microsoft.UI.Xaml.Controls.UserControl base type in code behind.
+	public sealed partial class PersonPictureLateBindingPage
+#pragma warning restore UXAML0002 // does not explicitly define the Microsoft.UI.Xaml.Controls.UserControl base type in code behind.
+	{
+		public string ImagePath { get; } = "ms-appx:///Assets/ingredient2.png";
+
+		public string LeName { get; } = "James Bondi";
+
+		public PersonPictureLateBindingPage()
+		{
+			this.InitializeComponent();
+			_ = SetImagePathWithDelay();
+		}
+
+		private async Task SetImagePathWithDelay()
+		{
+			await Task.Delay(1000);
+			DataContext = this;
+		}
+	}
+}
