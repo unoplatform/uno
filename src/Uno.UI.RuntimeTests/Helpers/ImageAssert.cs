2a6a7f90e17959a39176e9185b168261be716a34
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2024-11-05 17:20:28 -0500

    chore: fix xml comment

--- src/Uno.UI.RuntimeTests/Helpers/ImageAssert.cs
+++ src/Uno.UI.RuntimeTests/Helpers/ImageAssert.cs
@@ -258,8 +258,8 @@ public static partial class ImageAssert
 	/// If the error is greater than or equal to 0.022, the differences are visible to human eyes.
 	/// <paramref name="actual">The image to compare with reference</paramref>
 	/// <paramref name="expected">Reference image.</paramref>
-	/// <paramref name="imperceptibilityThreshold">It is the threshold beyond which the compared images are not considered equal. Default value is 0.022.</paramref>>
-	/// <paramref name="resolutionTolerance"/>Limits of resolution (in pixels) difference between the bitmaps</paramref>
+	/// <paramref name="imperceptibilityThreshold">It is the threshold beyond which the compared images are not considered equal. Default value is 0.022.</paramref>
+	/// <paramref name="resolutionTolerance">Limits of resolution (in pixels) difference between the bitmaps</paramref>
 	/// </summary>
 	public static async Task AreSimilarAsync(RawBitmap actual, RawBitmap expected, double imperceptibilityThreshold = 0.022, int resolutionTolerance = 0)
 	{
