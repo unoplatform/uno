26d588da458640344bc1ff1649dc9e736c381e42
Ramez Ragaa <ramezragaa@proton.me>
2024-11-07 19:16:27 +0200

    chore: overriding some methods for Win32WindowWrapper

--- src/Uno.UI.Runtime.Skia.Win32/UI/Controls/WpfWindowWrapper.cs
+++ src/Uno.UI.Runtime.Skia.Win32/UI/Controls/WpfWindowWrapper.cs
@@ -1,14 +1,15 @@
 ﻿#nullable enable
 
 using System;
+using System.Globalization;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using Windows.Win32;
 using Windows.Win32.Foundation;
-using Windows.Win32.System.Diagnostics.Debug;
 using Windows.Win32.UI.WindowsAndMessaging;
 using Microsoft.UI.Xaml;
 using Uno.Disposables;
+using Uno.Foundation.Logging;
 using Uno.UI.Xaml.Controls;
 
 namespace Uno.UI.Runtime.Skia.Win32.UI.Controls;
@@ -41,7 +42,7 @@ internal class Win32WindowWrapper : NativeWindowWrapperBase
 			throw new InvalidOperationException($"{nameof(PInvoke.RegisterClassEx)} failed: {Win32Helper.GetErrorMessage()}");
 		}
 
-		var title = Marshal.StringToHGlobalUni("Uno platfooooooooorm");
+		var title = Marshal.StringToHGlobalUni("Uno Platform");
 		using var titleDisposable = new DisposableStruct<IntPtr>(Marshal.FreeHGlobal, title);
 
 		_hwnd = PInvoke.CreateWindowEx(
@@ -70,4 +71,62 @@ internal class Win32WindowWrapper : NativeWindowWrapperBase
 	}
 
 	public override object NativeWindow => _hwnd;
+
+	public override unsafe string Title
+	{
+		get
+		{
+			char* title = stackalloc char[1024];
+			var readChars = PInvoke.GetWindowText(_hwnd, new PWSTR(title), 1024);
+			if (readChars is 0)
+			{
+				if (this.Log().IsEnabled(LogLevel.Error))
+				{
+					this.Log().Error($"{nameof(PInvoke.GetWindowText)} read 0 chars: {Win32Helper.GetErrorMessage()}");
+				}
+			}
+			return Marshal.PtrToStringUni((IntPtr)title, readChars);
+		}
+		set => PInvoke.SetWindowText(_hwnd, value);
+	}
+
+	public override void Activate()
+	{
+		if (PInvoke.SetActiveWindow(_hwnd) == HWND.Null)
+		{
+			if (this.Log().IsEnabled(LogLevel.Error))
+			{
+				this.Log().Error($"{nameof(PInvoke.SetActiveWindow)} failed: {Win32Helper.GetErrorMessage()}");
+			}
+		}
+	}
+
+	protected override void ShowCore()
+	{
+		if (!PInvoke.ShowWindow(_hwnd, SHOW_WINDOW_CMD.SW_SHOW))
+		{
+			if (this.Log().IsEnabled(LogLevel.Error))
+			{
+				this.Log().Error($"{nameof(PInvoke.ShowWindow)} failed: {Win32Helper.GetErrorMessage()}");
+			}
+		}
+	}
+
+	public override void Close()
+	{
+		base.Close();
+
+		if (this.Log().IsEnabled(LogLevel.Information))
+		{
+			this.Log().Info($"Forcibly closing window {_hwnd.Value.ToString("X", CultureInfo.InvariantCulture)}");
+		}
+
+		if (!PInvoke.DestroyWindow(_hwnd))
+		{
+			if (this.Log().IsEnabled(LogLevel.Error))
+			{
+				this.Log().Error($"{nameof(PInvoke.DestroyWindow)} failed: {Win32Helper.GetErrorMessage()}");
+			}
+		}
+	}
 }
