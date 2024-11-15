8e5cb3ca4259544404e80001661cdd257afd01fe
Jérôme Laban <jlaban@gmail.com>
2024-11-15 15:28:15 -0500

    chore: Adjust end of line

--- build/test-scripts/run-net7-template-linux.ps1
+++ build/test-scripts/run-net7-template-linux.ps1
@@ -78,7 +78,7 @@ $projects =
     @(1, "5.2/uno52AppWithLib/uno52AppWithLib/uno52AppWithLib.csproj", @(), @()),
 
     # 5.3 Blank with net9
-    @(2, "5.3/uno53net9blank/uno53net9blank/uno53net9blank.csproj", @(), @())
+    @(2, "5.3/uno53net9blank/uno53net9blank/uno53net9blank.csproj", @(), @()),
 
     # 5.3 lib
     @(2, "5.3/uno53net9Lib/uno53net9Lib.csproj", @(), @())
