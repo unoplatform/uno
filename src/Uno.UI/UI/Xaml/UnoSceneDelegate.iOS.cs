d2b76df35359da2f63a4cd037abb058ff267c696
Martin Zikmund <martin@zikmund.dev>
2024-11-20 11:57:45 +0100

    chore: Start SceneDelegate support

---
+++ src/Uno.UI/UI/Xaml/UnoSceneDelegate.iOS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Foundation;
+using UIKit;
+
+namespace Microsoft.UI.Xaml;
+
+[Register(nameof(UnoSceneDelegate))]
+public class UnoSceneDelegate : UISceneDelegate
+{
+}
