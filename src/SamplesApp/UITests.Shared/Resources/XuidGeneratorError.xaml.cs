bb52d27804156d42f2b6ea70b4d62d8b3d517ee5
Ramez Ragaa <ramezragaa@proton.me>
2024-11-21 17:15:56 +0200

    test: add a ui test for x:uid

---
+++ src/SamplesApp/UITests.Shared/Resources/XuidGeneratorError.xaml.cs
@@ -0,0 +1,10 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace UITests.Shared.Resources;
+public sealed partial class XuidGeneratorError : ContentDialog
+{
+	public XuidGeneratorError()
+	{
+		this.InitializeComponent();
+	}
+}
