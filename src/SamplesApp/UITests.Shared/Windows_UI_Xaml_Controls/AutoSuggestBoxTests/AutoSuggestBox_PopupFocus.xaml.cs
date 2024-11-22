b543dc75db899b0189e3dcc72db7502cb6e29d70
Ramez Ragaa <ramezragaa@proton.me>
2024-11-22 15:13:40 +0200

    test: add a manual test

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/AutoSuggestBoxTests/AutoSuggestBox_PopupFocus.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Uno.UI.Samples.Controls;
+using Microsoft.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.AutoSuggestBoxTests;
+
+[Sample("AutoSuggestBox", IsManualTest = true)]
+public sealed partial class AutoSuggestBox_PopupFocus : Page
+{
+	public AutoSuggestBox_PopupFocus()
+	{
+		this.InitializeComponent();
+	}
+}
