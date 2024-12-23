b8e65ba4eb2001dec2f96208b756e017cbdde6a1
Ramez Ragaa <ramezragaa@proton.me>
2024-12-24 01:31:43 +0200

    fix(launcher): make Launcher work on skia-wasm

--- src/Uno.UWP/ts/Windows/System/Launcher/Launcher.ts
+++ src/Uno.UWP/ts/Windows/System/Launcher/Launcher.ts
@@ -8,7 +8,7 @@ namespace Windows.System {
 		* @param url URL to load
 		* @returns "True" or "False", depending on whether a new window could be opened or not
 		*/
-		public open(url: string): string {
+		public static open(url: string): string {
 			const newWindow = window.open(url, "_blank");
 
 			return newWindow != null
