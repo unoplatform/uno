1ee63cd836ead47535dd3557cac5cd3a49045c1a
Youssef Victor <youssefvictor00@gmail.com>
2025-01-06 05:17:21 +0100

    chore: Remove unintentional duplicate test

--- src/Uno.UI.Tests/Windows_Globalization/Given_CalendarFormatter.cs
+++ src/Uno.UI.Tests/Windows_Globalization/Given_CalendarFormatter.cs
@@ -1,4 +1,4 @@
-﻿using System.Globalization;
+using System.Globalization;
 using System.Linq;
 using Windows.Globalization.DateTimeFormatting;
 using FluentAssertions;
@@ -24,33 +24,6 @@ namespace Uno.UI.Tests.Windows_Globalization
 			var sut = new DateTimeFormatter(format, new[] { language });
 			var firstPattern = sut.Patterns[0];
 			firstPattern.Should().Be(expectedPattern);
-		}
-
-		[TestMethod]
-		public void When_FormattingDateVariants_ShouldProduceExpectedFormats()
-		{
-			var expectedResults = new Dictionary<string, string>
-		{
-			{ "{day.integer}/{month.integer}/{year.full}", "27/6/2024" },
-			{ "{month.full} {year.full}", "June 2024" },
-			{ "{month.full} {day.integer}", "June 27" },
-			{ "{dayofweek.abbreviated}, {day.integer} {month.abbreviated} {year.full}", "Thu, 27 Jun 2024" },
-			{ "{year.full}-{month.integer}-{day.integer}", "2024-6-27" },
-			{ "{day.integer}/{month.integer}/{year.abbreviated}", "27/6/24" },
-			{ "{day.integer} {month.abbreviated} {year.full}", "27 Jun 2024" },
-			{ "{month.full} {day.integer}, {year.full}", "June 27, 2024" },
-			{ "{month.abbreviated} {day.integer}, {year.full}", "Jun 27, 2024" },
-			{ "{dayofweek.full}, {day.integer} {month.full} {year.full}", "Thursday, 27 June 2024" },
-			{ "{dayofweek.abbreviated}, {day.integer} {month.abbreviated} {year.abbreviated}", "Thu, 27 Jun 24" }
-		};
-			foreach (var kvp in expectedResults)
-			{
-				var template = kvp.Key;
-				var expected = kvp.Value;
-				var formatter = new DateTimeFormatter(template);
-				var formattedDate = formatter.Format(new(2024, 6, 27, 14, 30, 0, TimeSpan.Zero));
-				Assert.AreEqual(expected, formattedDate, $"Mismatch for template: {template}");
-			}
 		}
 
 #if !NET7_0_OR_GREATER // https://github.com/unoplatform/uno/issues/9080
