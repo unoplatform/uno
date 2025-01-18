d5ac50042812ca7efc68d17dd30072771eb5e61d
Martin Zikmund <martin@zikmund.dev>
2025-01-18 18:15:50 +0100

    chore: Adjust text input interface

--- src/Uno.UI.Runtime.Skia.AppleUIKit/UI/Xaml/Controls/TextBox/IInvisibleTextBoxView.cs
+++ src/Uno.UI.Runtime.Skia.AppleUIKit/UI/Xaml/Controls/TextBox/IInvisibleTextBoxView.cs
@@ -1,12 +1,11 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 using UIKit;
-using ITextInput = UIKit.IUITextInput;
 
 namespace Uno.WinUI.Runtime.Skia.AppleUIKit.Controls;
 
 //in iOS, we need to use two different controls to be able to accept return (UITextField vs UITextView)
 //we use this interface to abstract properties that we need to modify in TextBox
-internal interface IInvisibleTextBoxView : ITextInput, IUITextInputTraits
+internal interface IInvisibleTextBoxView
 {
 	string? Text { get; }
 
@@ -21,4 +20,24 @@ internal interface IInvisibleTextBoxView : ITextInput, IUITextInputTraits
 	void SetTextNative(string text);
 
 	void Select(int start, int length);
+
+	UITextAutocapitalizationType AutocapitalizationType { get; set; }
+
+	UIKeyboardType KeyboardType { get; set; }
+
+	UITextAutocorrectionType AutocorrectionType { get; set; }
+
+	UITextSpellCheckingType SpellCheckingType { get; set; }
+
+	bool SecureTextEntry { get; set; }
+
+	UITextRange? SelectedTextRange { get; set; }
+
+	UIReturnKeyType ReturnKeyType { get; set; }
+
+	UIKeyboardAppearance KeyboardAppearance { get; set; }
+
+	UITextPosition BeginningOfDocument { get; }
+
+	nint GetOffsetFromPosition(UITextPosition fromPosition, UITextPosition toPosition);
 }
