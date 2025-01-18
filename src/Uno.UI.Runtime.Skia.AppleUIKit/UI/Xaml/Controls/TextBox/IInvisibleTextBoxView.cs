303ba6c3bd8485f8402a862bd839cb2ae6691b77
Martin Zikmund <martin@zikmund.dev>
2025-01-18 12:04:46 +0100

    chore: Adjust inheritance for .NET 8.0 build

--- src/Uno.UI.Runtime.Skia.AppleUIKit/UI/Xaml/Controls/TextBox/IInvisibleTextBoxView.cs
+++ src/Uno.UI.Runtime.Skia.AppleUIKit/UI/Xaml/Controls/TextBox/IInvisibleTextBoxView.cs
@@ -6,7 +6,7 @@ namespace Uno.WinUI.Runtime.Skia.AppleUIKit.Controls;
 
 //in iOS, we need to use two different controls to be able to accept return (UITextField vs UITextView)
 //we use this interface to abstract properties that we need to modify in TextBox
-internal interface IInvisibleTextBoxView : ITextInput
+internal interface IInvisibleTextBoxView : ITextInput, IUITextInputTraits
 {
 	string? Text { get; }
 
@@ -21,4 +21,20 @@ internal interface IInvisibleTextBoxView : ITextInput
 	void SetTextNative(string text);
 
 	void Select(int start, int length);
+
+#if !NET9_0_OR_GREATER
+	UITextAutocapitalizationType AutocapitalizationType { get; set; }
+
+	UIKeyboardType KeyboardType { get; set; }
+
+	UITextAutocorrectionType AutocorrectionType { get; set; }
+
+	UITextSpellCheckingType SpellCheckingType { get; set; }
+
+	bool SecureTextEntry { get; set; }
+
+	UIReturnKeyType ReturnKeyType { get; set; }
+
+	UIKeyboardAppearance KeyboardAppearance { get; set; }
+#endif
 }
