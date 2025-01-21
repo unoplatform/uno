08bf8927086b1ee5a93d1ce2259790a42c3bc7ea
Martin Zikmund <martin@zikmund.dev>
2025-01-21 11:18:38 +0100

    feat: ConditionalTestAttribute support for new Skia targets

--- src/Uno.UI.RuntimeTests/Helpers/ConditionalTestAttribute.cs
+++ src/Uno.UI.RuntimeTests/Helpers/ConditionalTestAttribute.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Linq;
-using Private.Infrastructure;
 
 namespace Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -16,7 +14,13 @@ public enum RuntimeTestPlatform
 	Wasm = 1 << 6,
 	Android = 1 << 7,
 	iOS = 1 << 8,
-	macOSCatalyst = 1 << 9,
+	MacCatalyst = 1 << 9,
+	tvOS = 1 << 10,
+	SkiaWasm = 1 << 11,
+	SkiaAndroid = 1 << 12,
+	SkiaiOS = 1 << 13,
+	SkiatvOS = 1 << 14,
+	SkiaMacCatalyst = 1 << 15,
 }
 
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false)]
@@ -24,8 +28,10 @@ public partial class ConditionalTestAttribute : TestMethodAttribute
 {
 	private static readonly RuntimeTestPlatform _currentPlatform;
 
+	public const RuntimeTestPlatform SkiaUIKit = RuntimeTestPlatform.SkiaiOS | RuntimeTestPlatform.SkiatvOS | RuntimeTestPlatform.SkiaMacCatalyst;
+	public const RuntimeTestPlatform SkiaMobile = RuntimeTestPlatform.SkiaAndroid | SkiaUIKit;
 	public const RuntimeTestPlatform SkiaDesktop = RuntimeTestPlatform.SkiaGtk | RuntimeTestPlatform.SkiaWpf | RuntimeTestPlatform.SkiaX11 | RuntimeTestPlatform.SkiaMacOS | RuntimeTestPlatform.SkiaIslands;
-	public const RuntimeTestPlatform Skia = SkiaDesktop | RuntimeTestPlatform.SkiaBrowser;
+	public const RuntimeTestPlatform Skia = SkiaDesktop | RuntimeTestPlatform.SkiaBrowser | SkiaMobile;
 
 	static ConditionalTestAttribute()
 	{
@@ -64,10 +70,16 @@ public partial class ConditionalTestAttribute : TestMethodAttribute
 			RuntimeTestPlatform.SkiaMacOS => IsSkiaMacOS(),
 			RuntimeTestPlatform.SkiaBrowser => IsSkiaBrowser(),
 			RuntimeTestPlatform.SkiaIslands => IsSkiaIslands(),
-			RuntimeTestPlatform.Wasm => IsWasm(),
-			RuntimeTestPlatform.Android => IsAndroid(),
-			RuntimeTestPlatform.iOS => IsIOS(),
-			RuntimeTestPlatform.macOSCatalyst => IsMacOSCatalyst(),
+			RuntimeTestPlatform.Wasm => IsWasmNative(),
+			RuntimeTestPlatform.Android => IsAndroidNative(),
+			RuntimeTestPlatform.iOS => IsIOSNative(),
+			RuntimeTestPlatform.MacCatalyst => IsMacCatalystNative(),
+			RuntimeTestPlatform.tvOS => IsTvOSNative(),
+			RuntimeTestPlatform.SkiaAndroid => IsSkia() && OperatingSystem.IsAndroid(),
+			RuntimeTestPlatform.SkiaiOS => IsSkia() && OperatingSystem.IsIOS(),
+			RuntimeTestPlatform.SkiatvOS => IsSkia() && OperatingSystem.IsTvOS(),
+			RuntimeTestPlatform.SkiaMacCatalyst => IsSkia() && OperatingSystem.IsMacCatalyst(),
+			RuntimeTestPlatform.SkiaWasm => IsSkia() && OperatingSystem.IsBrowser(),
 			_ => throw new ArgumentException(nameof(singlePlatform)),
 		};
 	}
@@ -77,6 +89,13 @@ public partial class ConditionalTestAttribute : TestMethodAttribute
 		=> Microsoft.UI.Xaml.Application.Current.Host?.GetType().Assembly.GetName().Name == name;
 #else
 		=> false;
+#endif
+
+	private static bool IsSkia() =>
+#if __SKIA__
+		true;
+#else
+		false;
 #endif
 
 	private static bool IsSkiaGtk()
@@ -101,7 +120,7 @@ public partial class ConditionalTestAttribute : TestMethodAttribute
 		=> false;
 #endif
 
-	private static bool IsWasm()
+	private static bool IsWasmNative()
 	{
 #if __WASM__
 		return true;
@@ -110,7 +129,7 @@ public partial class ConditionalTestAttribute : TestMethodAttribute
 #endif
 	}
 
-	private static bool IsAndroid()
+	private static bool IsAndroidNative()
 	{
 #if __ANDROID__
 		return true;
@@ -119,18 +138,27 @@ public partial class ConditionalTestAttribute : TestMethodAttribute
 #endif
 	}
 
-	private static bool IsIOS()
+	private static bool IsIOSNative()
 	{
-#if __APPLE_UIKIT__
+#if __IOS__
 		return true;
 #else
 		return false;
 #endif
 	}
 
-	private static bool IsMacOSCatalyst()
+	private static bool IsTvOSNative()
 	{
-#if __MACOS__
+#if __TVOS__
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static bool IsMacCatalystNative()
+	{
+#if __MACCATALYST__
 		return true;
 #else
 		return false;
