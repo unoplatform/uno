dbd56e73bd91e559e48e5867505a0da367c8df17
Martin Zikmund <martin@zikmund.dev>
2025-01-27 14:02:50 +0100

    fix: UpdateSourceTrigger.LostFocus should not be marked NotImplemented

--- src/Uno.UI/UI/Xaml/Data/UpdateSourceTrigger.cs
+++ src/Uno.UI/UI/Xaml/Data/UpdateSourceTrigger.cs
@@ -1,29 +1,28 @@
-﻿using Uno;
+﻿namespace Microsoft.UI.Xaml.Data;
 
-namespace Microsoft.UI.Xaml.Data
+/// <summary>
+/// Defines constants that indicate when a binding source is updated by its binding target in two-way binding.
+/// </summary>
+public enum UpdateSourceTrigger
 {
 	/// <summary>
-	/// Defines constants that indicate when a binding source is updated by its binding target in two-way binding.
+	/// Use default behavior from the dependency property that uses the binding. In Windows Runtime, this evaluates the same as a value with PropertyChanged.
 	/// </summary>
-	public enum UpdateSourceTrigger
-	{
-		/// <summary>
-		/// Use default behavior from the dependency property that uses the binding. In Windows Runtime, this evaluates the same as a value with PropertyChanged.
-		/// </summary>
-		Default = 0,
-		/// <summary>
-		/// The binding source is updated whenever the binding target value changes. This is detected automatically by the binding system.
-		/// </summary>
-		PropertyChanged = 1,
-		/// <summary>
-		/// The binding source is updated only when you call the BindingExpression.UpdateSource method.
-		/// </summary>
-		Explicit = 2,
-		/// <summary>	
-		/// The binding source is updated whenever the binding target element loses focus.
-		/// </summary>
-		[NotImplemented]
-		LostFocus = 3,
-	}
+	Default = 0,
+
+	/// <summary>
+	/// The binding source is updated whenever the binding target value changes. This is detected automatically by the binding system.
+	/// </summary>
+	PropertyChanged = 1,
+
+	/// <summary>
+	/// The binding source is updated only when you call the BindingExpression.UpdateSource method.
+	/// </summary>
+	Explicit = 2,
+
+	/// <summary>	
+	/// The binding source is updated whenever the binding target element loses focus.
+	/// </summary>
+	LostFocus = 3,
 }
 
