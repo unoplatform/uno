29fbc86abcea8f3e1fd50e4aa4b27a891f343f7b
Martin Zikmund <martin@zikmund.dev>
2025-01-29 15:14:16 +0100

    feat: Update AppBarToggleButton to winui3/release/1.6.4

---
+++ src/Uno.UI/UI/Xaml/Controls/AppBar/IAppBarButtonHelpersProvider.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+
+namespace Microsoft.UI.Xaml.Controls;
+
+internal interface IAppBarButtonHelpersProvider
+{
+	bool m_ownsToolTip { get; internal set; }
+
+	TextBlock? m_tpKeyboardAcceleratorTextLabel { get; set; }
+
+	DependencyProperty GetIsCompactDependencyProperty();
+
+	DependencyProperty GetUseOverflowStyleDependencyProperty();
+
+	DependencyProperty GetLabelPositionDependencyProperty();
+
+	DependencyProperty GetLabelDependencyProperty();
+
+	DependencyProperty GetIconDependencyProperty();
+
+	DependencyProperty GetKeyboardAcceleratorTextDependencyProperty();
+}
