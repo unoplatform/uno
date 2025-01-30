bbf9325579b95e387189994e854266de25af7e6f
Martin Zikmund <martin@zikmund.dev>
2025-01-30 17:08:48 +0100

    feat!: Drop GTK
    BREAKING CHANGE: Dropping legacy GTK target

--- src/Uno.UI.Runtime.Skia.Gtk/Extensions/System/Profile/AnalyticsInfoExtension.cs
+++
@@ -1,8 +0,0 @@
-﻿using Windows.System.Profile.Internal;
-
-namespace Uno.UI.Runtime.Skia.Gtk.System.Profile;
-
-internal class AnalyticsInfoExtension : IAnalyticsInfoExtension
-{
-	public UnoDeviceForm GetDeviceForm() => UnoDeviceForm.Desktop;
-}
--- src/Uno.UI.Runtime.Skia.Gtk/UI/Controls/PendingWindowStateChangedInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Uno.UI.Runtime.Skia.Gtk.UI.Controls;
-
-internal record PendingWindowStateChangedInfo(Gdk.WindowState newState, Gdk.WindowState changedMask);
