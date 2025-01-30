088fc8f7421aca3a6cee57b66e746b43a57def88
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2025-01-30 22:25:07 +0100

    chore: Update `TabViewItem` to winui 1.7

--- src/Uno.UI/UI/Xaml/Controls/TabView/TabViewItem.Header.cs
+++ src/Uno.UI/UI/Xaml/Controls/TabView/TabViewItem.Header.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-// MUX Reference: TabViewItem.h, commit 27052f7
+// MUX Reference: TabViewItem.h, commit 6909712
 
 #nullable enable
 
@@ -8,17 +8,34 @@ using Uno.Disposables;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Shapes;
 using Uno.UI.Helpers.WinUI;
+using Windows.Foundation;
 
 namespace Microsoft/* UWP don't rename */.UI.Xaml.Controls;
 
 public partial class TabViewItem
 {
+	private static readonly string s_dragDropVisualVisibleStateName = "DragDropVisualVisible";
+	private static readonly string s_dragDropVisualNotVisibleStateName = "DragDropVisualNotVisible";
+	private static readonly string s_closeButtonCollapsedStateName = "CloseButtonCollapsed";
+	private static readonly string s_closeButtonVisibleStateName = "CloseButtonVisible";
+	private static readonly string s_compactStateName = "Compact";
+	private static readonly string s_standardWidthStateName = "StandardWidth";
+	private static readonly string s_iconStateName = "Icon";
+	private static readonly string s_noIconStateName = "NoIcon";
+	private static readonly string s_foregroundSetStateName = "ForegroundNotSet";
+	private static readonly string s_foregroundNotSetStateName = "ForegroundSet";
+	private static readonly string s_selectedBackgroundPathName = "SelectedBackgroundPath";
+	private static readonly string s_contentPresenterName = "ContentPresenter";
+	private static readonly string s_closeButtonName = "CloseButton";
+	//private static readonly string s_tabDragVisualContainer = "TabDragVisualContainer";
+
 	internal Button? GetCloseButton() => m_closeButton;
 
 	private bool m_hasPointerCapture = false;
 	private bool m_isMiddlePointerButtonPressed = false;
 	private bool m_isBeingDragged = false;
 	private bool m_isPointerOver = false;
+	private bool m_isCheckingforDrag = false;
 	private bool m_firstTimeSettingToolTip = true;
 
 	private Path? m_selectedBackgroundPath;
@@ -32,10 +49,11 @@ public partial class TabViewItem
 	private readonly SerialDisposable m_closeButtonClickRevoker = new();
 	private readonly SerialDisposable m_tabDragStartingRevoker = new();
 	private readonly SerialDisposable m_tabDragCompletedRevoker = new();
-
-	private object? m_shadow;
+	private Point m_lastPointerPressedPosition = new();
 
 	private TabView? m_parentTabView;
 
 	private DispatcherHelper m_dispatcherHelper;
+
+	private uint m_dragPointerId = 0;
 }
