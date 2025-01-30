a6daec4c5752b7c6b5e3a654379a05ee72eded3f
Martin Zikmund <martin@zikmund.dev>
2025-01-30 12:41:35 +0100

    chore: Start updating AppBarButton

---
+++ src/Uno.UI/UI/Xaml/Controls/AppBar/AppBarButton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Represents a templated button control to be displayed in an AppBar.
+/// </summary>
+public partial class AppBarButton : Button, ICommandBarElement
+{
+}
