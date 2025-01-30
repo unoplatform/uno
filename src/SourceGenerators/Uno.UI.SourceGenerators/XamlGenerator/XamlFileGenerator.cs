3f47b2c9946d27009c36d0a380383887813eabd9
Carl de Billy <carl@platform.uno>
2025-01-30 14:11:38 -0500

    ci: Fixed formatting issue.
    (cherry picked from commit 1c45c3775a8356b857f43258aff5da1b7a42a2ea)

--- src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlFileGenerator.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlFileGenerator.cs
@@ -5996,13 +5996,11 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 					var contentOwner = xamlObjectDefinition.Members.FirstOrDefault(m => m.Member.Name == "_UnknownContent");
 					var contentLocation = (IXamlLocation)xamlObjectDefinition.Members.FirstOrDefault(m => m.Member.Name == "Key") ?? xamlObjectDefinition;
 
-					if (contentOwner != null || _isHotReloadEnabled) // If Hot Reload is enabled, we still need to attach the source location, even on empty elements
+					if (contentOwner is not null || _isHotReloadEnabled) // If Hot Reload is enabled, we still need to attach the source location, even on empty elements
 					{
-						var resourceOwner = CurrentResourceOwnerName;
-
-
-						if(contentOwner is { })
+						if (contentOwner is not null)
 						{
+							var resourceOwner = CurrentResourceOwnerName;
 #if USE_NEW_TP_CODEGEN
 							writer.Append($"{resourceOwner}, (__owner, __settings) => ");
 #else
@@ -6018,10 +6016,8 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 
 						if (_isHotReloadEnabled)
 						{
-							using (var applyWriter = CreateApplyBlock(writer, null, out var closureName))
-							{
-								TrySetOriginalSourceLocation(applyWriter, closureName, contentLocation.LineNumber, contentLocation.LinePosition);
-							}
+							using var applyWriter = CreateApplyBlock(writer, null, out var closureName);
+							TrySetOriginalSourceLocation(applyWriter, closureName, contentLocation.LineNumber, contentLocation.LinePosition);
 						}
 
 					}
