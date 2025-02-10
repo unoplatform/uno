d2aba26042ef9789b0956f5eb245fa809bb497fa
Martin Zikmund <martin@zikmund.dev>
2025-02-10 20:12:01 +0100

    chore: Adjust for UWP build

--- src/SamplesApp/UITests.Shared/Windows_UI_Xaml/WindowTests/Window_Metrics.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/WindowTests/Window_Metrics.xaml.cs
@@ -27,12 +27,14 @@ namespace UITests.Windows_UI_Xaml.WindowTests
 
 		private void GetMetricsClick()
 		{
+#if HAS_UNO_WINUI // AppWindow APIs are only available in WinUI flavor of Uno Platform
 			AppWindowSize.Text = GetSafe(() => $"{_window.AppWindow.Size.Width:0.##} x {_window.AppWindow.Size.Height:0.##}");
 			AppWindowPosition.Text = GetSafe(() => $"{_window.AppWindow.Position.X:0.##}, {_window.AppWindow.Position.Y:0.##}");
 			AppWindowClientSize.Text = GetSafe(() => $"{_window.AppWindow.ClientSize.Width:0.##} x {_window.AppWindow.ClientSize.Height:0.##}");
+			TitleBarHeight.Text = GetSafe(() => $"{_window.AppWindow.TitleBar.Height:0.##}");
+#endif
 			WindowBounds.Text = GetSafe(() => $"{_window.Bounds.X:0.##}, {_window.Bounds.Y:0.##}, {_window.Bounds.Width:0.##}, {_window.Bounds.Height:0.##}");
 			XamlRootSize.Text = GetSafe(() => $"{XamlRoot.Size.Width:0.##} x {XamlRoot.Size.Height:0.##}");
-			TitleBarHeight.Text = GetSafe(() => $"{_window.AppWindow.TitleBar.Height:0.##}");
 			var padding = VisibleBoundsPadding.WindowPadding;
 			VisibleBoundsPaddingValue.Text = GetSafe(() => $"{padding.Left:0.##}, {padding.Top:0.##}, {padding.Right:0.##}, {padding.Bottom:0.##}");
 		}
