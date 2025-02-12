d0c2736084e091089e23a988357254ef9d5d9767
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2025-02-12 11:51:10 +0100

    feat: Implement missing args

--- src/Uno.UI/Generated/3.0.0.0/Microsoft.UI.Input/EnteredMoveSizeEventArgs.cs
+++ src/Uno.UI/Generated/3.0.0.0/Microsoft.UI.Input/EnteredMoveSizeEventArgs.cs
@@ -3,37 +3,11 @@
 #pragma warning disable 114 // new keyword hiding
 namespace Microsoft.UI.Input
 {
-#if __ANDROID__ || __IOS__ || __TVOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__
+#if false
 	[global::Uno.NotImplemented]
 #endif
 	public partial class EnteredMoveSizeEventArgs
 	{
-#if __ANDROID__ || __IOS__ || __TVOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__
-		internal EnteredMoveSizeEventArgs()
-		{
-		}
-#endif
-#if __ANDROID__ || __IOS__ || __TVOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "__TVOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__")]
-		public global::Microsoft.UI.Input.MoveSizeOperation MoveSizeOperation
-		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member MoveSizeOperation EnteredMoveSizeEventArgs.MoveSizeOperation is not implemented. For more information, visit https://aka.platform.uno/notimplemented#m=MoveSizeOperation%20EnteredMoveSizeEventArgs.MoveSizeOperation");
-			}
-		}
-#endif
-#if __ANDROID__ || __IOS__ || __TVOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "__TVOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__")]
-		public global::Windows.Graphics.PointInt32 PointerScreenPoint
-		{
-			get
-			{
-				throw new global::System.NotImplementedException("The member PointInt32 EnteredMoveSizeEventArgs.PointerScreenPoint is not implemented. For more information, visit https://aka.platform.uno/notimplemented#m=PointInt32%20EnteredMoveSizeEventArgs.PointerScreenPoint");
-			}
-		}
-#endif
-		// Forced skipping of method Microsoft.UI.Input.EnteredMoveSizeEventArgs.PointerScreenPoint.get
-		// Forced skipping of method Microsoft.UI.Input.EnteredMoveSizeEventArgs.MoveSizeOperation.get
+
 	}
 }
--- src/Uno.UI/Microsoft/UI/Input/InputNonClientPointerSource.cs
+++ src/Uno.UI/Microsoft/UI/Input/InputNonClientPointerSource.cs
@@ -5,17 +5,43 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
 
+#pragma warning disable CS0067
+
 namespace Microsoft.UI.Input;
+
+/// <summary>
+/// Processes pointer input and window messages in the non-client area of a window.
+/// </summary>
 public partial class InputNonClientPointerSource
 {
+	/// <summary>
+	/// Retrieves an InputNonClientPointerSource object for the specified window.
+	/// </summary>
+	/// <param name="windowId">The ID of the specified window.</param>
+	/// <returns>The non-client window pointer input source.</returns>
 	public static InputNonClientPointerSource GetForWindowId(WindowId windowId)
 	{
 		// UNO TODO: Port InputNonClientPointerSource properly from WinUI
 		return new InputNonClientPointerSource();
 	}
 
+	/// <summary>
+	/// Occurs when the window has entered a move-size loop.
+	/// </summary>
 	public event TypedEventHandler<InputNonClientPointerSource, EnteredMoveSizeEventArgs> EnteredMoveSize;
+
+	/// <summary>
+	/// Occurs when the window is about to enter a move-size loop.
+	/// </summary>
 	public event TypedEventHandler<InputNonClientPointerSource, EnteringMoveSizeEventArgs> EnteringMoveSize;
+
+	/// <summary>
+	/// Occurs when the window has exited a move-size loop.
+	/// </summary>
 	public event TypedEventHandler<InputNonClientPointerSource, ExitedMoveSizeEventArgs> ExitedMoveSize;
+
+	/// <summary>
+	/// Occurs when the window rect is about to change.
+	/// </summary>
 	public event TypedEventHandler<InputNonClientPointerSource, WindowRectChangingEventArgs> WindowRectChanging;
 }
