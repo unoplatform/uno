feea8c3f8d30f839d4be18fb2d41fc5bd15f0e7c
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2025-02-12 11:51:10 +0100

    feat: Implement missing args

--- src/Uno.UI/Microsoft/UI/Content/ContentCoordinateConverter.cs
+++ src/Uno.UI/Microsoft/UI/Content/ContentCoordinateConverter.cs
@@ -5,8 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Microsoft.UI.Content;
+
+/// <summary>
+/// Converts between a local coordinate space and the system screen coordinates.
+/// </summary>
 public partial class ContentCoordinateConverter
 {
+	/// <summary>
+	/// Creates a new ContentCoordinateConverter object for the specified WindowId.
+	/// </summary>
+	/// <param name="windowId">The top-level window identifier.</param>
+	/// <returns>An object used to convert between a local coordinate space and the system screen coordinates.</returns>
 	public static ContentCoordinateConverter CreateForWindowId(WindowId windowId)
 	{
 		// UNO TODO: Port ContentCoordinateConverter properly from WinUI
---
+++ src/Uno.UI/UI/Input/EnteringMoveSizeEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using Windows.Graphics;
+
+#pragma warning disable CS0067
+
+namespace Microsoft.UI.Input;
+
+/// <summary>
+/// Contains event data for the EnteringMoveSize event.
+/// </summary>
+public partial class EnteringMoveSizeEventArgs
+{
+	/// <summary>
+	/// Gets or sets the ID of the window for which to enter the move-size loop.
+	/// </summary>
+	public WindowId MoveSizeWindowId { get; set; }
+
+	/// <summary>
+	/// Gets the type of operation being performed in the move-size loop.
+	/// </summary>
+	public MoveSizeOperation MoveSizeOperation { get; }
+
+	/// <summary>
+	/// Gets the position of the pointer upon entering the move-size loop.
+	/// </summary>
+	public PointInt32 PointerScreenPoint { get; }
+}
