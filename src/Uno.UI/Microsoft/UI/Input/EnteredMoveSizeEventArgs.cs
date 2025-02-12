f1a6259bf31079e912fb83592a2fd9bc292c1765
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2025-02-12 11:51:10 +0100

    feat: Implement missing args

---
+++ src/Uno.UI/Microsoft/UI/Input/EnteredMoveSizeEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using Windows.Graphics;
+
+#pragma warning disable CS0067
+
+namespace Microsoft.UI.Input;
+
+/// <summary>
+/// Contains event data for the EnteredMoveSize event.
+/// </summary>
+public partial class EnteredMoveSizeEventArgs
+{
+	/// <summary>
+	/// Gets the type of operation being performed in the move-size loop.
+	/// </summary>
+	public MoveSizeOperation MoveSizeOperation { get; }
+
+	/// <summary>
+	/// Gets the position of the pointer upon entering the move-size loop.
+	/// </summary>
+	public PointInt32 PointerScreenPoint { get; }
+}
