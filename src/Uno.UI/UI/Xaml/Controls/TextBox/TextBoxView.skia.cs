07d66646d3756799949082ca76916bf19952a17e
Ramez Ragaa <ramezragaa@proton.me>
2025-02-22 20:07:07 +0200

    chore: make When_PasswordBox_TextRevealed pass

--- src/Uno.UI/UI/Xaml/Controls/TextBox/TextBoxView.skia.cs
+++ src/Uno.UI/UI/Xaml/Controls/TextBox/TextBoxView.skia.cs
@@ -28,7 +28,7 @@ namespace Microsoft.UI.Xaml.Controls
 		// However, this character can't be retrieved on Android (doesn't exist in any system font) and on some browser/OS combinations.
 		// We use \u2022 instead, which is already the one normally used by Android and all the major browsers.
 		// See https://github.com/mozilla/gecko-dev/blob/1d4c27f9f166ce6e967fb0e8c8d6e0795dbbd12e/widget/android/nsLookAndFeel.cpp#L441
-		private static readonly char _passwordChar = OperatingSystem.IsAndroid() || OperatingSystem.IsBrowser() ? '\u2022' : '\u25CF';
+		internal static readonly char PasswordChar = OperatingSystem.IsAndroid() || OperatingSystem.IsBrowser() ? '\u2022' : '\u25CF';
 
 		public TextBoxView(TextBox textBox)
 		{
@@ -220,7 +220,7 @@ namespace Microsoft.UI.Xaml.Controls
 			if (IsPasswordBox && !_isPasswordRevealed)
 			{
 				// TODO: PasswordChar isn't currently implemented. It should be used here when implemented.
-				DisplayBlock.Text = new string(_passwordChar, text.Length);
+				DisplayBlock.Text = new string(PasswordChar, text.Length);
 			}
 			else
 			{
