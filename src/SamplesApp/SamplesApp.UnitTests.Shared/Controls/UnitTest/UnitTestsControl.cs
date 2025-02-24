080fc19302c8f3543d63f944c5ddc3a40637afd6
Ramez Ragaa <ramezragaa@proton.me>
2025-02-24 11:28:31 +0200

    chore: revert UnitTestsControl output column width reduction

--- src/SamplesApp/SamplesApp.UnitTests.Shared/Controls/UnitTest/UnitTestsControl.cs
+++ src/SamplesApp/SamplesApp.UnitTests.Shared/Controls/UnitTest/UnitTestsControl.cs
@@ -126,12 +126,6 @@ namespace Uno.UI.Samples.Tests
 #else
 				false;
 #endif
-
-			if (unitTestContentRoot.ActualWidth < 450)
-			{
-				// Reduce the size of outputColumn to just 50px so that unitTestContentRoot takes larger width
-				outputColumn.Width = new GridLength(50);
-			}
 		}
 
 		private static void OverrideDebugProviderAsserts()
