99dcfafe0d1df52dc8270c7adfca897275116685
Ramez Ragaa <ramezragaa@proton.me>
2025-02-26 17:37:54 +0200

    fix: scaling for wasm/skia native element hosting

--- src/Uno.UI.Runtime.Skia.WebAssembly.Browser/BrowserRenderer.cs
+++ src/Uno.UI.Runtime.Skia.WebAssembly.Browser/BrowserRenderer.cs
@@ -149,6 +149,10 @@ internal partial class BrowserRenderer
 			if (_host.RootElement?.Visual is { } rootVisual)
 			{
 				var negativePath = SkiaRenderHelper.RenderRootVisualAndReturnNegativePath(_renderTarget.Width, _renderTarget.Height, rootVisual, _surface);
+				// Unlike other skia platforms, on skia/wasm we need to undo the scaling  adjustment that happens inside
+				// RenderRootVisualAndReturnNegativePath since the numbers we get from native are already scaled, so we
+				// don't need to do our own scaling in RenderRootVisualAndReturnNegativePath.
+				negativePath.Transform(SKMatrix.CreateScale((float)(1 / scale), (float)(1 / scale)), negativePath);
 				BrowserNativeElementHostingExtension.SetSvgClipPathForNativeElementHost(!negativePath.IsEmpty ? negativePath.ToSvgPathData() : "");
 			}
 		}
