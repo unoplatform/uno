ba17df1c99d6d6ac20319f66e8b6dfe9b15b5942
Martin Zikmund <martin@zikmund.dev>
2025-02-28 14:11:33 +0100

    chore: Adjust usings

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/NavigationView/NavigationViewItemAutomationPeer.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/NavigationView/NavigationViewItemAutomationPeer.cs
@@ -2,9 +2,12 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 // MUX reference NavigationViewItemAutomationPeer.cpp, commit a564c49
 
-using Microsoft.UI.Xaml.Automation.Provider;
 using Microsoft/* UWP don't rename */.UI.Xaml.Controls;
 using Uno.UI.Helpers.WinUI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Automation;
+using Microsoft.UI.Xaml.Automation.Peers;
+using Microsoft.UI.Xaml.Automation.Provider;
 
 namespace Microsoft/* UWP don't rename */.UI.Xaml.Automation.Peers;
 
