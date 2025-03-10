728d91e3d4a2fe6be6ede07af2cd0cb5beb95d84
Jerome Laban <jerome@platform.uno>
2025-03-10 07:33:40 -0400

    chore: Ignore test for iOS

--- src/Uno.UI.RuntimeTests/Tests/Windows_ApplicationModel_Resources/Given_ResourceLoader.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_ApplicationModel_Resources/Given_ResourceLoader.cs
@@ -158,7 +158,7 @@ namespace Uno.UI.RuntimeTests.Tests
 			Assert.AreEqual(@"Text in 'es-MX'", SUT.GetString("Given_ResourceLoader/When_LocalizedResource"));
 		}
 
-		[TestMethod]
+		[ConditionalTest(IgnoredPlatforms = RuntimeTestPlatforms.SkiaIOS)] // Unstable test due to device language settings: NSLocale.PreferredLanguages can be 'en' or 'en-US'.
 #if __APPLE_UIKIT__
 		[Ignore("Unstable test due to device language settings: NSLocale.PreferredLanguages can be 'en' or 'en-US'.")]
 #endif
