cfd9ce3ea4163eefd3033d01990c4e60cfe15ae3
Jérôme Laban <jerome@platform.uno>
2025-03-12 15:16:58 -0400

    fix(webview2): [wasm] Fix ExecuteScriptAsync

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_WebView2.cs
@@ -0,0 +1,58 @@
+﻿#if HAS_UNO_WINUI
+using System;
+using System.Threading.Tasks;
+using Private.Infrastructure;
+using Microsoft/* UWP don't rename */.UI.Xaml.Controls;
+using System.Linq;
+using Microsoft.Web.WebView2.Core;
+using System.Diagnostics;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls;
+
+[TestClass]
+[RunsOnUIThread]
+public class Given_WebView
+{
+	[TestMethod]
+	public async Task When_InvokeScriptAsync()
+	{
+		var border = new Border();
+		var webView = new WebView2();
+		webView.Width = 200;
+		webView.Height = 200;
+		border.Child = webView;
+		TestServices.WindowHelper.WindowContent = border;
+		bool navigated = false;
+		await TestServices.WindowHelper.WaitForLoaded(border);
+		webView.NavigationCompleted += (sender, e) => navigated = true;
+		webView.NavigateToString("<html><body><div id='test' style='width: 100px; height: 100px; background-color: blue;' /></body></html>");
+		await TestServices.WindowHelper.WaitFor(() => navigated);
+
+		var sw = Stopwatch.StartNew();
+		string color = null;
+
+		do
+		{
+			// We need to wait for the element to be available, navigated
+			// may be set to true too early on wasm.
+			color = await webView.ExecuteScriptAsync(
+				"""
+				let testElement = document.getElementById('test');
+				if(testElement){
+					return testElement.style.backgroundColor.toString();
+				}
+				return "";
+				""");
+
+		} while (sw.Elapsed < TimeSpan.FromSeconds(5) && string.IsNullOrEmpty(color));
+
+		Assert.AreEqual("blue", color);
+
+		// Change color to red
+		await webView.ExecuteScriptAsync("document.getElementById('test').style.backgroundColor = 'red'");
+		color = await webView.ExecuteScriptAsync("document.getElementById('test').style.backgroundColor.toString()");
+
+		Assert.AreEqual("red", color);
+	}
+}
+#endif
