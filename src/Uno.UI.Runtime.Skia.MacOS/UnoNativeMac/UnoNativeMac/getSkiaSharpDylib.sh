756db461787e91050b7a7f63eed939794f03ae4c
jay <jerome@platform.uno>
2025-03-19 22:31:04 -0400

    chore: Adjust skia download with configured feeds

--- src/Uno.UI.Runtime.Skia.MacOS/UnoNativeMac/UnoNativeMac/getSkiaSharpDylib.sh
+++ src/Uno.UI.Runtime.Skia.MacOS/UnoNativeMac/UnoNativeMac/getSkiaSharpDylib.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+set -e
+
 VERSION=${VERSION:-3.119.0-preview.0.11}
 
 filename="skiasharp_"${VERSION}"_nativeassets"
-if [ ! -f "${filename}"/runtimes/osx/native/libSkiaSharp.dylib ]; then
-    curl -o "${filename}.zip" -L "https://www.nuget.org/api/v2/package/SkiaSharp.NativeAssets.macOS/${VERSION}"
-    unzip -d "${filename}" "${filename}.zip"
+build_dir="${filename}-tmp-build"
+
+if [ ! -f "${filename}"/libSkiaSharp.dylib ]; then
+    # download the SkiaSharp.NativeAssets.macOS using the configured nuget feeds
+    mkdir -p "${build_dir}"
+    cd "${build_dir}"
+    dotnet new console
+    dotnet add package SkiaSharp.NativeAssets.macOS --version ${VERSION}
+    dotnet build
+    cd ..
+    rm -fr "${build_dir}"
+    rm -fr "${filename}"
+    mkdir -p "${filename}"
+    cp -r ~/.nuget/packages/skiasharp.nativeassets.macos/${VERSION}/runtimes/osx/native/* "${filename}"
 fi
-cp "${filename}/runtimes/osx/native/libSkiaSharp.dylib" .
+cp "${filename}/libSkiaSharp.dylib" .
