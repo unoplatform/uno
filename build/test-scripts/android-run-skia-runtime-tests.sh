fa9626141885673bf021988ebaf396d1b116a1b5
Jerome Laban <jerome@platform.uno>
2025-03-22 20:28:18 -0400

    chore: Increment interim logcat

--- build/test-scripts/android-run-skia-runtime-tests.sh
+++ build/test-scripts/android-run-skia-runtime-tests.sh
@@ -136,11 +136,15 @@ END_TIME=$((SECONDS+TIMEOUT))
 
 echo "Waiting for $UITEST_RUNTIME_AUTOSTART_RESULT_PATH to be available..."
 
+ITERATION_COUNT=0
+
 while [[ ! $($ANDROID_HOME/platform-tools/adb shell test -e "$UITEST_RUNTIME_AUTOSTART_RESULT_PATH" > /dev/null) && $SECONDS -lt $END_TIME ]]; do
     sleep 15
 
 	## Dump the emulator's system log, in case it crashes
-	$ANDROID_HOME/platform-tools/adb shell logcat -d > $LOGS_PATH/android-device-log-$UNO_UITEST_BUCKET_ID-$UITEST_RUNTIME_TEST_GROUP-$UITEST_TEST_MODE_NAME.interim.txt
+	$ANDROID_HOME/platform-tools/adb shell logcat -d > $LOGS_PATH/android-device-log-$UNO_UITEST_BUCKET_ID-$UITEST_RUNTIME_TEST_GROUP-$UITEST_TEST_MODE_NAME.interim.$ITERATION_COUNT.txt
+
+	ITERATION_COUNT=$((ITERATION_COUNT+1))
 
     # exit loop if the APP_PID is not running anymore
     if ! $ANDROID_HOME/platform-tools/adb shell ps | grep "$UNO_UITEST_APP_ID" > /dev/null; then
