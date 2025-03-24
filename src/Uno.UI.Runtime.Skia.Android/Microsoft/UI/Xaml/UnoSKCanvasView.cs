ceb0fed490d8fb20594b9ba6ca4bfc47b2156d9c
Ramez Ragaa <ramezragaa@proton.me>
2025-03-24 15:18:16 -0400

    chore: copy SKGLSurfaceView source code

--- src/Uno.UI.Runtime.Skia.Android/Microsoft/UI/Xaml/UnoSKCanvasView.cs
+++ src/Uno.UI.Runtime.Skia.Android/Microsoft/UI/Xaml/UnoSKCanvasView.cs
@@ -1,9 +1,9 @@
 ﻿// #define FPS_DISPLAY
 
-using System;
 using Windows.Graphics.Display;
 using Android.Content;
 using Android.Graphics;
+using Android.Opengl;
 using Android.OS;
 using Android.Runtime;
 using Android.Util;
@@ -11,6 +11,7 @@ using Android.Views;
 using Android.Views.Autofill;
 using Android.Views.InputMethods;
 using AndroidX.Core.View;
+using Javax.Microedition.Khronos.Opengles;
 using Microsoft.UI.Xaml;
 using SkiaSharp;
 using SkiaSharp.Views.Android;
@@ -18,7 +19,7 @@ using Uno.UI.Helpers;
 
 namespace Uno.UI.Runtime.Skia.Android;
 
-internal sealed class UnoSKCanvasView : SKGLSurfaceView
+internal sealed class UnoSKCanvasView : GLSurfaceView
 {
 #if FPS_DISPLAY
 	private long _counter;
@@ -35,6 +36,12 @@ internal sealed class UnoSKCanvasView : SKGLSurfaceView
 
 	public UnoSKCanvasView(Context context) : base(context)
 	{
+		SetEGLContextClientVersion(2);
+		SetEGLConfigChooser(8, 8, 8, 8, 0, 8);
+
+		var renderer = new InternalRenderer(this);
+		SetRenderer(renderer);
+
 		Instance = this;
 		ExploreByTouchHelper = new UnoExploreByTouchHelper(this);
 		TextInputPlugin = new TextInputPlugin(this);
@@ -47,57 +54,51 @@ internal sealed class UnoSKCanvasView : SKGLSurfaceView
 		}
 
 		SetWillNotDraw(false);
-
-		PaintSurface += (_, e) =>
-		{
-			if (_picture != null)
-			{
-				e.Surface.Canvas.DrawPicture(_picture);
-			}
-		};
 	}
 
 	protected override void OnDraw(Canvas c)
 	{
 		base.OnDraw(c);
 
+		if (Microsoft.UI.Xaml.Window.CurrentSafe is not { RootElement: { } root } window)
+		{
+			return;
+		}
+
+		ExploreByTouchHelper.InvalidateRoot();
+
 		var recorder = new SKPictureRecorder();
 		var canvas = recorder.BeginRecording(new SKRect(-9999, -9999, 9999, 9999));
 		using (new SKAutoCanvasRestore(canvas, true))
 		{
-			if (Microsoft.UI.Xaml.Window.CurrentSafe is { RootElement: { } root } window)
+			canvas.Clear(SKColors.Transparent);
+			var scale = DisplayInformation.GetForCurrentViewSafe()!.RawPixelsPerViewPixel;
+			canvas.Scale((float)scale);
+			var negativePath = SkiaRenderHelper.RenderRootVisualAndReturnNegativePath((int)window.Bounds.Width,
+				(int)window.Bounds.Height, root.Visual, canvas);
+			if (ApplicationActivity.Instance.NativeLayerHost is { } nativeLayerHost)
 			{
-				ExploreByTouchHelper.InvalidateRoot();
-
-				canvas.Clear(SKColors.Transparent);
-				var scale = DisplayInformation.GetForCurrentViewSafe()!.RawPixelsPerViewPixel;
-				canvas.Scale((float)scale);
-				var negativePath = SkiaRenderHelper.RenderRootVisualAndReturnNegativePath((int)window.Bounds.Width,
-					(int)window.Bounds.Height, root.Visual, canvas);
-				if (ApplicationActivity.Instance.NativeLayerHost is { } nativeLayerHost)
-				{
-					nativeLayerHost.Path = negativePath;
-					nativeLayerHost.Invalidate();
-				}
+				nativeLayerHost.Path = negativePath;
+				nativeLayerHost.Invalidate();
+			}
 
 #if FPS_DISPLAY
-				// This naively calculates the difference in time every 100 frames, so to get
-				// a usable number, open a sample with a continuously-running animation.
-				_counter++;
-				if (_counter % 100 == 0)
-				{
-					var newTime = DateTime.UtcNow;
-					_fpsText = $"{100 / (newTime - _time).TotalSeconds}";
-					_time = newTime;
-				}
-				canvas.DrawText(
-					_fpsText,
-					(float)(window.Bounds.Width / 2),
-					(float)(window.Bounds.Height / 2),
-					new SKFont(SKTypeface.Default, size: 20F),
-					new SKPaint { Color = SKColors.Red});
-#endif
+			// This naively calculates the difference in time every 100 frames, so to get
+			// a usable number, open a sample with a continuously-running animation.
+			_counter++;
+			if (_counter % 100 == 0)
+			{
+				var newTime = DateTime.UtcNow;
+				_fpsText = $"{100 / (newTime - _time).TotalSeconds}";
+				_time = newTime;
 			}
+			canvas.DrawText(
+				_fpsText,
+				(float)(window.Bounds.Width / 2),
+				(float)(window.Bounds.Height / 2),
+				new SKFont(SKTypeface.Default, size: 20F),
+				new SKPaint { Color = SKColors.Red});
+#endif
 
 			_picture = recorder.EndRecording();
 		}
@@ -155,7 +156,7 @@ internal sealed class UnoSKCanvasView : SKGLSurfaceView
 			return;
 		}
 
-		TextInputPlugin?.OnProvideAutofillVirtualStructure(structure);
+		TextInputPlugin.OnProvideAutofillVirtualStructure(structure);
 	}
 
 	public override void Autofill(SparseArray values)
@@ -174,4 +175,114 @@ internal sealed class UnoSKCanvasView : SKGLSurfaceView
 
 	public override IInputConnection? OnCreateInputConnection(EditorInfo? outAttrs)
 		=> TextInputPlugin.OnCreateInputConnection(outAttrs!);
+
+	// Copied from https://github.com/mono/SkiaSharp/blob/main/source/SkiaSharp.Views/SkiaSharp.Views/Platform/Android/SKGLSurfaceView.cs
+	private class InternalRenderer(UnoSKCanvasView surfaceView) : Java.Lang.Object, IRenderer
+	{
+		private const SKColorType ColorType = SKColorType.Rgba8888;
+		private const GRSurfaceOrigin SurfaceOrigin = GRSurfaceOrigin.BottomLeft;
+
+		private GRContext? _context;
+		private GRGlFramebufferInfo _glInfo;
+		private GRBackendRenderTarget? _renderTarget;
+		private SKSurface? _surface;
+		private SKCanvas? _canvas;
+
+		private SKSizeI _lastSize;
+		private SKSizeI _newSize;
+
+		void IRenderer.OnDrawFrame(IGL10? gl)
+		{
+			GLES20.GlClear(GLES20.GlColorBufferBit | GLES20.GlDepthBufferBit | GLES20.GlStencilBufferBit);
+
+			// create the contexts if not done already
+			if (_context == null)
+			{
+				var glInterface = GRGlInterface.Create();
+				_context = GRContext.CreateGl(glInterface);
+			}
+
+			// manage the drawing surface
+			if (_renderTarget == null || _lastSize != _newSize || !_renderTarget.IsValid)
+			{
+				// create or update the dimensions
+				_lastSize = _newSize;
+
+				// read the info from the buffer
+				var buffer = new int[3];
+				GLES20.GlGetIntegerv(GLES20.GlFramebufferBinding, buffer, 0);
+				GLES20.GlGetIntegerv(GLES20.GlStencilBits, buffer, 1);
+				GLES20.GlGetIntegerv(GLES20.GlSamples, buffer, 2);
+				var samples = buffer[2];
+				var maxSamples = _context.GetMaxSurfaceSampleCount(ColorType);
+				if (samples > maxSamples)
+				{
+					samples = maxSamples;
+				}
+
+				_glInfo = new GRGlFramebufferInfo((uint)buffer[0], ColorType.ToGlSizedFormat());
+
+				// destroy the old surface
+				_surface?.Dispose();
+				_surface = null;
+				_canvas = null;
+
+				// re-create the render target
+				_renderTarget?.Dispose();
+				_renderTarget = new GRBackendRenderTarget(_newSize.Width, _newSize.Height, samples, buffer[1], _glInfo);
+			}
+
+			// create the surface
+			if (_surface == null)
+			{
+				_surface = SKSurface.Create(_context, _renderTarget, SurfaceOrigin, ColorType);
+				_canvas = _surface.Canvas;
+			}
+
+			using (new SKAutoCanvasRestore(_canvas, true))
+			{
+				// start drawing
+				var e = new SKPaintGLSurfaceEventArgs(_surface, _renderTarget, SurfaceOrigin, ColorType);
+				if (surfaceView._picture is { } picture)
+				{
+					e.Surface.Canvas.DrawPicture(picture);
+				}
+			}
+
+			// flush the SkiaSharp contents to GL
+			_canvas?.Flush();
+			_context.Flush();
+		}
+
+		void IRenderer.OnSurfaceChanged(IGL10? gl, int width, int height)
+		{
+			GLES20.GlViewport(0, 0, width, height);
+
+			// get the new surface size
+			_newSize = new SKSizeI(width, height);
+		}
+
+		void IRenderer.OnSurfaceCreated(IGL10? gl, Javax.Microedition.Khronos.Egl.EGLConfig? config)
+		{
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				FreeContext();
+			}
+			base.Dispose(disposing);
+		}
+
+		private void FreeContext()
+		{
+			_surface?.Dispose();
+			_surface = null;
+			_renderTarget?.Dispose();
+			_renderTarget = null;
+			_context?.Dispose();
+			_context = null;
+		}
+	}
 }
