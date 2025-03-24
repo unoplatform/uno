29797a0751458a0db64620fcd70dffc644b49609
Jerome Laban <jerome@platform.uno>
2025-03-24 06:44:31 -0400

    chore: Adjust permissions

--- build/test-scripts/android-run-skia-runtime-tests.sh
+++ build/test-scripts/android-run-skia-runtime-tests.sh
@@ -121,6 +121,7 @@ $ANDROID_HOME/platform-tools/adb push samplesapp-environment.txt /sdcard/samples
 # grant the storage permission to the app to write the test results and read the environment file
 $ANDROID_HOME/platform-tools/adb shell pm grant $UNO_UITEST_APP_ID android.permission.WRITE_EXTERNAL_STORAGE
 $ANDROID_HOME/platform-tools/adb shell pm grant $UNO_UITEST_APP_ID android.permission.READ_EXTERNAL_STORAGE
+$ANDROID_HOME/platform-tools/adb shell pm grant $UNO_UITEST_APP_ID android.permission.MANAGE_EXTERNAL_STORAGE
 $ANDROID_HOME/platform-tools/adb shell pm grant $UNO_UITEST_APP_ID android.permission.READ_MEDIA_IMAGES
 $ANDROID_HOME/platform-tools/adb shell pm grant $UNO_UITEST_APP_ID android.permission.READ_MEDIA_VIDEO
 $ANDROID_HOME/platform-tools/adb shell pm grant $UNO_UITEST_APP_ID android.permission.READ_MEDIA_AUDIO
