365af4396c2437e741a98eac88f4e0516b65cf3e
David <github.site@torick.net>
2025-03-24 15:04:10 -0400

    chore: Misc logging and comments updates

--- src/Uno.UI/UI/Input/ManipulationVelocities.cs
+++ src/Uno.UI/UI/Input/ManipulationVelocities.cs
@@ -40,7 +40,7 @@ namespace Windows.UI.Input
 
 		/// <inheritdoc />
 		public override string ToString()
-			=> $"x:{Linear.X:N0};y:{Linear.Y:N0};θ:{Angular};e:{Expansion:F2}";
+			=> $"x:{Linear.X:F2};y:{Linear.Y:F2};θ:{Angular};e:{Expansion:F2}";
 
 		#region Equality Members
 		public override bool Equals(object obj) => obj is ManipulationVelocities velocities && Equals(velocities);
