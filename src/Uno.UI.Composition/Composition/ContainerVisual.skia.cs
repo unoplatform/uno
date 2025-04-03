9802240c25d1ebd3334f13f9fd878410da6bb9e4
Ramez Ragaa <ramezragaa@proton.me>
2025-04-03 11:11:00 -0400

    feat(skia): collapse visual subtrees into one SKPicture when the subtree doesn't change for a bit

--- src/Uno.UI.Composition/Composition/ContainerVisual.skia.cs
+++ src/Uno.UI.Composition/Composition/ContainerVisual.skia.cs
@@ -14,6 +14,7 @@ public partial class ContainerVisual : Visual
 {
 	private List<Visual>? _childrenInRenderOrder;
 	private bool _hasCustomRenderOrder;
+	private int? _subtreeVisualCount;
 
 	private (Rect rect, bool isAncestorClip)? _layoutClip;
 
@@ -24,6 +25,13 @@ public partial class ContainerVisual : Visual
 		Children.CollectionChanged += (s, e) =>
 		{
 			IsChildrenRenderOrderDirty = true;
+
+			var parent = this;
+			while (parent is not null && parent._subtreeVisualCount is not null)
+			{
+				parent._subtreeVisualCount = null;
+				parent = parent.Parent;
+			}
 
 			if (e.Action is NotifyCollectionChangedAction.Remove or NotifyCollectionChangedAction.Reset
 				&& e.OldItems is not null)
@@ -160,5 +168,11 @@ public partial class ContainerVisual : Visual
 		}
 
 		return false;
+	}
+
+	internal override int GetSubTreeVisualCount()
+	{
+		_subtreeVisualCount ??= Children.Count + Children.InnerList.Aggregate(0, (acc, visual) => acc + visual.GetSubTreeVisualCount());
+		return _subtreeVisualCount.Value;
 	}
 }
