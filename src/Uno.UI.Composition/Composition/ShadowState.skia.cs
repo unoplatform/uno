8647e8ff72fb02bf3e207f2e6bcff949d1fffa0b
Ramez Ragaa <ramezragaa@proton.me>
2025-04-10 04:51:43 -0400

    chore: comment

--- src/Uno.UI.Composition/Composition/ShadowState.skia.cs
+++ src/Uno.UI.Composition/Composition/ShadowState.skia.cs
@@ -17,6 +17,9 @@ internal record ShadowState(float Dx, float Dy, float SigmaX, float SigmaY, Colo
 		_paint ??= new SKPaint()
 		{
 			// Equivalent (I think) to SKImageFilter.CreateDropShadow(Dx, Dy, SigmaX, SigmaY, Color.ToSKColor()) but much much faster
+			// Writing our own shader that does the same (basically takes the alpha value of the given pixel
+			// adjust by (Dx, Dy) and multiplies it by ShadowState.Color and "modulates" it with the original
+			// pixel) did not improve the numbers one bit.
 			ImageFilter = SKImageFilter.CreateMerge(SKImageFilter.CreateOffset(Dx, Dy, SKImageFilter.CreateCompose(SKImageFilter.CreateBlur(SigmaX, SigmaY), SKImageFilter.CreateColorFilter(SKColorFilter.CreateBlendMode(Color.ToSKColor(), SKBlendMode.Modulate)))), SKImageFilter.CreateOffset(0, 0))
 		};
 }
