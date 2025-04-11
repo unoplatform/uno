cfc1d64343e008fc54dc2ea7ece4dac61e3350d4
Martin Zikmund <martin@zikmund.dev>
2025-04-11 10:19:57 +0200

    chore: Adjust loading lifecycle on iOS

--- src/Uno.UI/Controls/RootViewController.UIKit.cs
+++ src/Uno.UI/Controls/RootViewController.UIKit.cs
@@ -7,91 +7,90 @@ using UIKit;
 using Uno.Helpers.Theming;
 using WinUICoreServices = Uno.UI.Xaml.Core.CoreServices;
 
-namespace Uno.UI.Controls
+namespace Uno.UI.Controls;
+
+public partial class RootViewController : UINavigationController, IRotationAwareViewController
 {
-	public partial class RootViewController : UINavigationController, IRotationAwareViewController
+	private XamlRoot _xamlRoot;
+
+	internal event Action VisibleBoundsChanged;
+
+	public RootViewController()
 	{
-		private XamlRoot _xamlRoot;
+		Initialize();
+	}
 
-		internal event Action VisibleBoundsChanged;
+	public RootViewController(UIViewController rootViewController)
+		: base(rootViewController)
+	{
+		Initialize();
+	}
 
-		public RootViewController()
-		{
-			Initialize();
-		}
+	public RootViewController(NSObjectFlag t)
+		: base(t)
+	{
+		Initialize();
+	}
 
-		public RootViewController(UIViewController rootViewController)
-			: base(rootViewController)
-		{
-			Initialize();
-		}
+	public RootViewController(NSCoder coder)
+		: base(coder)
+	{
+		Initialize();
+	}
 
-		public RootViewController(NSObjectFlag t)
-			: base(t)
-		{
-			Initialize();
-		}
+	public RootViewController(NativeHandle handle)
+		: base(handle)
+	{
+		Initialize();
+	}
 
-		public RootViewController(NSCoder coder)
-			: base(coder)
-		{
-			Initialize();
-		}
-
-		public RootViewController(NativeHandle handle)
-			: base(handle)
-		{
-			Initialize();
-		}
-
-		private void Initialize()
-		{
-			// TODO Uno: When we support multi-window, this should close popups for the appropriate XamlRoot #13847.
+	private void Initialize()
+	{
+		// TODO Uno: When we support multi-window, this should close popups for the appropriate XamlRoot #13847.
 
 #if !__TVOS__
-			// Dismiss on device rotation: this reproduces the windows behavior
-			UIApplication.Notifications
-				.ObserveDidChangeStatusBarOrientation(DismissPopups);
+		// Dismiss on device rotation: this reproduces the windows behavior
+		UIApplication.Notifications
+			.ObserveDidChangeStatusBarOrientation(DismissPopups);
 #endif
 
-			// Dismiss when the app is entering background
-			UIApplication.Notifications
-				.ObserveWillResignActive(DismissPopups);
+		// Dismiss when the app is entering background
+		UIApplication.Notifications
+			.ObserveWillResignActive(DismissPopups);
 
 #if NET9_0_OR_GREATER
-			// iOS 17+ only
-			if (UIDevice.CurrentDevice.CheckSystemVersion(17, 0))
-			{
-				((IUITraitChangeObservable)this).RegisterForTraitChanges((env, traits) => SystemThemeHelper.RefreshSystemTheme(), typeof(UITraitUserInterfaceStyle));
-			}
+		// iOS 17+ only
+		if (UIDevice.CurrentDevice.CheckSystemVersion(17, 0))
+		{
+			((IUITraitChangeObservable)this).RegisterForTraitChanges((env, traits) => SystemThemeHelper.RefreshSystemTheme(), typeof(UITraitUserInterfaceStyle));
+		}
 #endif
-		}
+	}
 
-		private void DismissPopups(object sender, object args)
+	private void DismissPopups(object sender, object args)
+	{
+		if (_xamlRoot is not null)
 		{
-			if (_xamlRoot is not null)
-			{
-				VisualTreeHelper.CloseLightDismissPopups(_xamlRoot);
-			}
+			VisualTreeHelper.CloseLightDismissPopups(_xamlRoot);
 		}
+	}
 
-		internal void SetXamlRoot(XamlRoot xamlRoot) => _xamlRoot = xamlRoot;
+	internal void SetXamlRoot(XamlRoot xamlRoot) => _xamlRoot = xamlRoot;
 
-		// This will handle when the status bar is showed / hidden by the system on iPhones
-		public override void ViewSafeAreaInsetsDidChange()
-		{
-			base.ViewSafeAreaInsetsDidChange();
-			VisibleBoundsChanged?.Invoke();
-		}
+	// This will handle when the status bar is showed / hidden by the system on iPhones
+	public override void ViewSafeAreaInsetsDidChange()
+	{
+		base.ViewSafeAreaInsetsDidChange();
+		VisibleBoundsChanged?.Invoke();
+	}
 
-		public bool CanAutorotate { get; set; } = true;
+	public bool CanAutorotate { get; set; } = true;
 
 #pragma warning disable CA1422 // Deprecated in iOS 17+, replaced by RegisterForTraitChanges in Initialize()
-		public override void TraitCollectionDidChange(UITraitCollection previousTraitCollection)
-		{
-			base.TraitCollectionDidChange(previousTraitCollection);
-			SystemThemeHelper.RefreshSystemTheme();
-		}
-#pragma warning restore CA1422 // Deprecated in iOS 17+
+	public override void TraitCollectionDidChange(UITraitCollection previousTraitCollection)
+	{
+		base.TraitCollectionDidChange(previousTraitCollection);
+		SystemThemeHelper.RefreshSystemTheme();
 	}
+#pragma warning restore CA1422 // Deprecated in iOS 17+
 }
