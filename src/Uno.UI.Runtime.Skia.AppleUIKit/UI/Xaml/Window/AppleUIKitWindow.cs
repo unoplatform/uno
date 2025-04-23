bfe328e3abd43d0474f2b7678f34ec796cd575e5
Martin Zikmund <martin@zikmund.dev>
2025-04-23 19:30:57 +0200

    feat: Support scene delegate in other iOS variant
    (cherry picked from commit a2c8982c770769375b7670172d804ac26d629d3b)
    
    # Conflicts:
    #	src/Uno.UI.Runtime.Skia.AppleUIKit/UI/Xaml/Window/AppleUIKitWindow.cs
    #	src/Uno.UI.Runtime.Skia.AppleUIKit/Uno.UI.Runtime.Skia.AppleUIKit.csproj

---
+++ src/Uno.UI.Runtime.Skia.AppleUIKit/UI/Xaml/Window/AppleUIKitWindow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CoreGraphics;
+using Microsoft.UI.Input;
+using Microsoft.UI.Xaml;
+using UIKit;
+using Uno.UI.Xaml.Extensions;
+using Windows.Devices.Input;
+using Windows.Foundation;
+
+namespace Uno.UI.Runtime.Skia.AppleUIKit.UI.Xaml;
+
+internal partial class AppleUIKitWindow : UIWindow
+{
+	internal event Action? FrameChanged;
+
+	public AppleUIKitWindow()
+	{
+	}
+
+	public AppleUIKitWindow(UIWindowScene scene) : base(scene)
+	{
+	}
+
+	public override CGRect Frame
+	{
+		get => base.Frame;
+		set
+		{
+			var frameChanged = base.Frame != value;
+
+			base.Frame = value;
+
+			if (frameChanged)
+			{
+				FrameChanged?.Invoke();
+			}
+		}
+	}
+}
