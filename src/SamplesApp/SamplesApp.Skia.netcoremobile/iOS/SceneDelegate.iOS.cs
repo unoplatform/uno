e908bdb694d948031c901c4dbef4479315d190da
Martin Zikmund <martin@zikmund.dev>
2025-04-23 17:16:08 +0200

    chore: Add missing components for launch

---
+++ src/SamplesApp/SamplesApp.Skia.netcoremobile/iOS/SceneDelegate.iOS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Foundation;
+using UIKit;
+
+namespace SamplesApp.iOS;
+
+[Register("SceneDelegate")]
+public class SceneDelegate : UnoSceneDelegate
+{
+}
