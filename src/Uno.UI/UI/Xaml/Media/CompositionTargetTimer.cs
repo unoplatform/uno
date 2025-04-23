894bb35c2b7fa845c36cc1706ec2c438b02df488
Jerome Laban <jerome@platform.uno>
2025-04-23 17:18:01 -0400

    chore: Adjust for timer location
    (cherry picked from commit 0e7177bf009043b31e57f4a9908290108a92e3a3)

---
+++ src/Uno.UI/UI/Xaml/Media/CompositionTargetTimer.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+
+using System;
+using System.Threading;
+using Uno.UI;
+using Uno.UI.Dispatching;
+
+namespace Microsoft.UI.Xaml.Media;
+
+/// <summary>
+/// Use a generic frame timer instead of the native one, generally 
+/// in the context of desktop targets.
+/// </summary>
+internal class CompositionTargetTimer
+{
+	private static Timer? _renderTimer;
+
+	internal static void Start()
+	{
+		CompositionTarget.RenderingActiveChanged += UpdateTimer;
+	}
+
+	private static void UpdateTimer()
+	{
+		if (CompositionTarget.IsRenderingActive)
+		{
+			_renderTimer ??= new Timer(_ => NativeDispatcher.Main.DispatchRendering());
+			_renderTimer.Change(TimeSpan.Zero, TimeSpan.FromSeconds(1 / FeatureConfiguration.CompositionTarget.FrameRate));
+		}
+		else
+		{
+			_renderTimer?.Change(Timeout.Infinite, Timeout.Infinite);
+		}
+	}
+}
