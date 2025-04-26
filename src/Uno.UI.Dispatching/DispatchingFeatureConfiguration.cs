b4dbfeae333db5d6548ec9c93205e92ad0845a8a
Jerome Laban <jerome@platform.uno>
2025-04-26 18:37:37 -0400

    chore: Throttle render frame

---
+++ src/Uno.UI.Dispatching/DispatchingFeatureConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Uno;
+
+public static class DispatchingFeatureConfiguration
+{
+	internal static class DispatcherQueue
+	{
+		public static TimeSpan FrameDuration { get; set; } = TimeSpan.FromMilliseconds(1 / 60.0);
+	}
+}
