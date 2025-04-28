4e597e9e89d4a4a99ec09344767fa899d0a4e9e7
Jerome Laban <jerome@platform.uno>
2025-04-28 14:23:56 -0400

    chore: Combine changes to CoreApplication

--- src/Uno.UWP/ApplicationModel/Core/CoreApplication.Reference.cs
+++ src/Uno.UWP/ApplicationModel/Core/CoreApplication.Reference.cs
@@ -11,12 +11,4 @@ namespace Windows.ApplicationModel.Core;
 /// </summary>
 public static partial class CoreApplication
 {
-	internal static void SetContinuousRender(object? visual, bool enabled)
-		=> throw new NotSupportedException();
-
-	internal static void QueueInvalidateRender(object? visual)
-		=> throw new NotSupportedException();
-
-	internal static void SetInvalidateRender(Action<object?> invalidateRender, Action<object?, bool>? setContinuousRender)
-		=> throw new NotSupportedException();
 }
--- src/Uno.UWP/ApplicationModel/Core/CoreApplication.wasm.cs
+++ src/Uno.UWP/ApplicationModel/Core/CoreApplication.wasm.cs
@@ -11,8 +11,6 @@ using System.Diagnostics;
 namespace Windows.ApplicationModel.Core;
 partial class CoreApplication
 {
-	private static Action<object?>? _invalidateRender;
-	private static Action<object?, bool>? _setContinuousRender;
 	private static ICoreApplicationExtension? _coreApplicationExtension;
 
 	static partial void InitializePlatform()
@@ -23,19 +21,4 @@ partial class CoreApplication
 		{
 		}
 	}
-
-	internal static void SetInvalidateRender(Action<object?> invalidateRender, Action<object?, bool>? setContinuousRender)
-	{
-		Debug.Assert(_invalidateRender is null);
-		Debug.Assert(_setContinuousRender is null);
-
-		_invalidateRender ??= invalidateRender;
-		_setContinuousRender ??= setContinuousRender;
-	}
-
-	internal static void SetContinuousRender(object? visual, bool enabled)
-		=> _setContinuousRender?.Invoke(visual, enabled);
-
-	internal static void QueueInvalidateRender(object? visual)
-		=> _invalidateRender?.Invoke(visual);
 }
