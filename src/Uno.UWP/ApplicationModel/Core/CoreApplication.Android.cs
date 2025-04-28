47a6cb0615ebbd2d82df431a43302f06da94d4a6
Jerome Laban <jerome@platform.uno>
2025-04-28 14:23:56 -0400

    chore: Combine changes to CoreApplication

--- src/Uno.UWP/ApplicationModel/Core/CoreApplication.Android.cs
+++ src/Uno.UWP/ApplicationModel/Core/CoreApplication.Android.cs
@@ -7,24 +7,5 @@ namespace Windows.ApplicationModel.Core;
 
 partial class CoreApplication
 {
-	private static Action<object?>? _invalidateRender;
-	private static Action<object?, bool>? _setContinuousRender;
-
 	private static void ExitPlatform() => Android.OS.Process.KillProcess(Android.OS.Process.MyPid());
-
-	internal static void SetInvalidateRender(Action<object?> invalidateRender, Action<object?, bool>? setContinuousRender)
-	{
-		Debug.Assert(_invalidateRender is null);
-		Debug.Assert(_setContinuousRender is null);
-
-		_invalidateRender ??= invalidateRender;
-		_setContinuousRender ??= setContinuousRender;
-	}
-
-	internal static void SetContinuousRender(object? visual, bool enabled)
-		=> _setContinuousRender?.Invoke(visual, enabled);
-
-	internal static void QueueInvalidateRender(object? visual)
-		=> _invalidateRender?.Invoke(visual);
-
 }
