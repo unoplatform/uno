aa10c103e6d51622c1484fd88256e4c920f9dbb9
Martin Zikmund <martin@zikmund.dev>
2025-04-29 09:45:36 +0200

    feat: Add Cookie handling support to WASM runtime
    (cherry picked from commit ea10cc9768a23600c95ee91b204f3fcbdaddad5d)

--- src/Uno.UI.Runtime.WebAssembly/Web/Http/CookieManager.cs
+++ src/Uno.UI.Runtime.WebAssembly/Web/Http/CookieManager.cs
@@ -6,114 +6,113 @@ using System.Linq;
 using Uno.Foundation;
 using Windows.Web.Http;
 
-namespace Uno.Web.Http
+namespace Uno.Web.Http;
+
+/// <summary>
+/// Provides read-write access to browser Cookies in WebAssembly.
+/// </summary>
+public class CookieManager
 {
-	/// <summary>
-	/// Provides read-write access to browser Cookies in WebAssembly.
-	/// </summary>
-	public class CookieManager
+	private const char ValueSeparator = '=';
+	private const char AttributeSeparator = ';';
+
+	private static readonly Lazy<CookieManager> _cookieManager = new Lazy<CookieManager>(() => new CookieManager());
+
+	private CookieManager()
 	{
-		private const char ValueSeparator = '=';
-		private const char AttributeSeparator = ';';
+	}
 
-		private static readonly Lazy<CookieManager> _cookieManager = new Lazy<CookieManager>(() => new CookieManager());
+	/// <summary>
+	/// Retrieves the default instance of CookieManager.
+	/// </summary>
+	/// <returns>Cookie manager instance.</returns>
+	public static CookieManager GetDefault() => _cookieManager.Value;
 
-		private CookieManager()
+	/// <summary>
+	/// Sets a cookie given attributes.
+	/// </summary>
+	/// <param name="request">Set cookie request</param>
+	public void SetCookie(SetCookieRequest request)
+	{
+		var httpCookie = new HttpCookie(request.Cookie.Name, request.Domain ?? string.Empty, request.Path ?? string.Empty)
 		{
+			Secure = request.Secure,
+			Expires = request.Expires,
+			Value = request.Cookie.Value,
+		};
+		var serializedCookie = httpCookie.ToString();
+
+		if (request.MaxAge != null)
+		{
+			serializedCookie += $"; max-age={request.MaxAge.Value.ToString(CultureInfo.InvariantCulture)}";
+		}
+		if (request.SameSite != null)
+		{
+			serializedCookie += $"; samesite={request.SameSite.Value.ToString("g").ToLowerInvariant()}";
 		}
 
-		/// <summary>
-		/// Retrieves the default instance of CookieManager.
-		/// </summary>
-		/// <returns>Cookie manager instance.</returns>
-		public static CookieManager GetDefault() => _cookieManager.Value;
+		var escapedCookie = WebAssemblyRuntime.EscapeJs(serializedCookie);
+		var jsInvoke = $"document.cookie = '{escapedCookie}'";
+		WebAssemblyRuntime.InvokeJS(jsInvoke);
+	}
 
-		/// <summary>
-		/// Sets a cookie given attributes.
-		/// </summary>
-		/// <param name="request">Set cookie request</param>
-		public void SetCookie(SetCookieRequest request)
+	/// <summary>
+	/// Deletes a cookies by name.
+	/// </summary>
+	/// <param name="name">Name of the cookie.</param>
+	/// <param name="domain">Domain of the cookie (optional).</param>
+	/// <param name="path">Path of the cookie (optional).</param>
+	public void DeleteCookie(string name, string? domain = null, string? path = null)
+	{
+		var setCookieRequest = new SetCookieRequest(new Cookie(name, string.Empty))
 		{
-			var httpCookie = new HttpCookie(request.Cookie.Name, request.Domain ?? string.Empty, request.Path ?? string.Empty)
-			{
-				Secure = request.Secure,
-				Expires = request.Expires,
-				Value = request.Cookie.Value,
-			};
-			var serializedCookie = httpCookie.ToString();
+			Expires = DateTimeOffset.MinValue,
+			Path = path,
+			Domain = domain
+		};
 
-			if (request.MaxAge != null)
+		SetCookie(setCookieRequest);
+	}
+
+	/// <summary>
+	/// Retrieves a cookie by name.
+	/// </summary>
+	/// <param name="name">Cookie name.</param>
+	/// <returns>Cookie or null if not found.</returns>
+	public Cookie? FindCookie(string name) => GetCookies().FirstOrDefault(c => c.Name == name);
+
+	/// <summary>
+	/// Gets array of currently active cookies.
+	/// </summary>
+	/// <returns>Active cookies.</returns>
+	public Cookie[] GetCookies()
+	{
+		Cookie? ParseCookie(string cookieString)
+		{
+			cookieString = cookieString.Trim();
+			var valueSeparatorIndex = cookieString.IndexOf(ValueSeparator);
+			if (valueSeparatorIndex == -1)
 			{
-				serializedCookie += $"; max-age={request.MaxAge.Value.ToString(CultureInfo.InvariantCulture)}";
-			}
-			if (request.SameSite != null)
-			{
-				serializedCookie += $"; samesite={request.SameSite.Value.ToString("g").ToLowerInvariant()}";
+				return null;
 			}
 
-			var escapedCookie = WebAssemblyRuntime.EscapeJs(serializedCookie);
-			var jsInvoke = $"document.cookie = '{escapedCookie}'";
-			WebAssemblyRuntime.InvokeJS(jsInvoke);
+			var name = cookieString.Substring(0, valueSeparatorIndex);
+			var valueStartIndex = valueSeparatorIndex + 1;
+			var value = cookieString.Substring(valueStartIndex, cookieString.Length - valueStartIndex);
+			return new Cookie(name, value);
 		}
 
-		/// <summary>
-		/// Deletes a cookies by name.
-		/// </summary>
-		/// <param name="name">Name of the cookie.</param>
-		/// <param name="domain">Domain of the cookie (optional).</param>
-		/// <param name="path">Path of the cookie (optional).</param>
-		public void DeleteCookie(string name, string? domain = null, string? path = null)
+		var cookies = WebAssemblyRuntime.InvokeJS("document.cookie");
+		if (string.IsNullOrWhiteSpace(cookies))
 		{
-			var setCookieRequest = new SetCookieRequest(new Cookie(name, string.Empty))
-			{
-				Expires = DateTimeOffset.MinValue,
-				Path = path,
-				Domain = domain
-			};
-
-			SetCookie(setCookieRequest);
+			return Array.Empty<Cookie>();
 		}
 
-		/// <summary>
-		/// Retrieves a cookie by name.
-		/// </summary>
-		/// <param name="name">Cookie name.</param>
-		/// <returns>Cookie or null if not found.</returns>
-		public Cookie? FindCookie(string name) => GetCookies().FirstOrDefault(c => c.Name == name);
-
-		/// <summary>
-		/// Gets array of currently active cookies.
-		/// </summary>
-		/// <returns>Active cookies.</returns>
-		public Cookie[] GetCookies()
-		{
-			Cookie? ParseCookie(string cookieString)
-			{
-				cookieString = cookieString.Trim();
-				var valueSeparatorIndex = cookieString.IndexOf(ValueSeparator);
-				if (valueSeparatorIndex == -1)
-				{
-					return null;
-				}
-
-				var name = cookieString.Substring(0, valueSeparatorIndex);
-				var valueStartIndex = valueSeparatorIndex + 1;
-				var value = cookieString.Substring(valueStartIndex, cookieString.Length - valueStartIndex);
-				return new Cookie(name, value);
-			}
-
-			var cookies = WebAssemblyRuntime.InvokeJS("document.cookie");
-			if (string.IsNullOrWhiteSpace(cookies))
-			{
-				return Array.Empty<Cookie>();
-			}
-
-			var cookieStrings = cookies.Split(new char[] { AttributeSeparator }, StringSplitOptions.RemoveEmptyEntries);
-			return cookieStrings
-				.Select(part => ParseCookie(part))
-				.Where(cookie => cookie != null)
-				.OfType<Cookie>()
-				.ToArray();
-		}
+		var cookieStrings = cookies.Split(new char[] { AttributeSeparator }, StringSplitOptions.RemoveEmptyEntries);
+		return cookieStrings
+			.Select(part => ParseCookie(part))
+			.Where(cookie => cookie != null)
+			.OfType<Cookie>()
+			.ToArray();
 	}
 }
