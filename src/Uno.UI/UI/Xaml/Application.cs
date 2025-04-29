b372b6925e09ff4fb110d506c3b820e6f8cf766e
Martin Zikmund <martin@zikmund.dev>
2025-04-29 14:28:42 +0200

    refactor: Propagate WasLaunched, adjust Samples app activation handling

--- src/Uno.UI/UI/Xaml/Application.cs
+++ src/Uno.UI/UI/Xaml/Application.cs
@@ -66,6 +66,7 @@ namespace Microsoft.UI.Xaml
 
 		[ThreadStatic]
 		private static string _argumentsOverride;
+
 		static Application()
 		{
 			ApiInformation.RegisterAssembly(typeof(Application).Assembly);
@@ -115,7 +116,11 @@ namespace Microsoft.UI.Xaml
 
 		internal bool InitializationComplete => _initializationComplete;
 
-		internal bool WasLaunched { get; private set; }
+		internal bool WasLaunched
+		{
+			get => CoreApplication.WasLaunched;
+			private set => CoreApplication.WasLaunched = value;
+		}
 
 		partial void InitializePartial();
 
@@ -164,6 +169,8 @@ namespace Microsoft.UI.Xaml
 		}
 
 		internal bool IsSuspended { get; set; }
+
+		internal static void SetArguments(string arguments) => _argumentsOverride = arguments;
 
 		private void InitializeSystemTheme()
 		{
@@ -291,9 +298,9 @@ namespace Microsoft.UI.Xaml
 
 		static partial void StartPartial(ApplicationInitializationCallback callback);
 
-		protected internal virtual void OnActivated(IActivatedEventArgs args) { }
+		protected virtual void OnActivated(IActivatedEventArgs args) { }
 
-		protected internal virtual void OnLaunched(LaunchActivatedEventArgs args) { }
+		protected virtual void OnLaunched(LaunchActivatedEventArgs args) { }
 
 		internal void InvokeOnActivated(IActivatedEventArgs args)
 		{
