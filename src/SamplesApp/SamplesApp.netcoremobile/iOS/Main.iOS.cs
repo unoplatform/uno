aa184186d0ccdea1f5a5f47158638046dbde785f
Martin Zikmund <martin@zikmund.dev>
2025-04-30 11:56:44 +0200

    feat: Use host builder on UIKit

--- src/SamplesApp/SamplesApp.netcoremobile/iOS/Main.iOS.cs
+++ src/SamplesApp/SamplesApp.netcoremobile/iOS/Main.iOS.cs
@@ -1,21 +1,17 @@
-﻿using UIKit;
-
-namespace SamplesApp.iOS
-{
-	public class Application
-	{
-		// This is the main entry point of the application.
-		static void Main(string[] args)
-		{
-#if __IOS__ && !__MACCATALYST__ && !TESTFLIGHT && !DEBUG
-			// requires Xamarin Test Cloud Agent
-			Xamarin.Calabash.Start();
+﻿#if __IOS__ && !__MACCATALYST__ && !TESTFLIGHT && !DEBUG
+// requires Xamarin Test Cloud Agent
+Xamarin.Calabash.Start();
 #endif
 
+using SamplesApp;
+using Uno.UI.Hosting;
 
-			// if you want to use a different Application Delegate class from "AppDelegate"
-			// you can specify it here.
-			UIApplication.Main(args, null, typeof(App));
-		}
-	}
-}
+var host = UnoPlatformHostBuilder.Create()
+	.App(() => new App())
+	.UseAppleUIKit(builder =>
+	{
+		builder.UseUIApplicationDelegate<App>();
+	})
+	.Build();
+
+host.Run();
