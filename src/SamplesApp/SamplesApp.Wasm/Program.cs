5c040a62b34175e6262c1dd2ef22017072e80e0e
Martin Zikmund <martin@zikmund.dev>
2025-04-30 12:17:52 +0200

    chore: Adjust native startup
    (cherry picked from commit 53309ad5a09ced12959297537438950423ae001d)

--- src/SamplesApp/SamplesApp.Wasm/Program.cs
+++ src/SamplesApp/SamplesApp.Wasm/Program.cs
@@ -1,30 +1,25 @@
 ﻿using System;
 using Microsoft.UI.Xaml;
+using Uno.UI.Hosting;
 
-namespace SamplesApp.Wasm
-{
-	public class Program
-	{
-		private static App _app;
+// Workaround a net9 nuget bug where :
+// <Import Project="$(NuGetPackageRoot)/uno.fonts.fluent/2.6.1/buildTransitive/Uno.Fonts.Fluent.props" Condition="Exists('$(NuGetPackageRoot)/uno.fonts.fluent/2.6.1/buildTransitive/Uno.Fonts.Fluent.props')" />
+// is not imported properly. Linux only?
+Uno.UI.FeatureConfiguration.Font.SymbolsFont = "ms-appx:///Uno.Fonts.Fluent/Fonts/uno-fluentui-assets.ttf";
 
-		public static void Main(string[] args)
-		{
-			// Workaround a net9 nuget bug where :
-			// <Import Project="$(NuGetPackageRoot)/uno.fonts.fluent/2.6.1/buildTransitive/Uno.Fonts.Fluent.props" Condition="Exists('$(NuGetPackageRoot)/uno.fonts.fluent/2.6.1/buildTransitive/Uno.Fonts.Fluent.props')" />
-			// is not imported properly. Linux only?
-			Uno.UI.FeatureConfiguration.Font.SymbolsFont = "ms-appx:///Uno.Fonts.Fluent/Fonts/uno-fluentui-assets.ttf";
-
-			// Required to allow for Puppeteer to select XAML elements in the HTML DOM.
-			Uno.UI.FeatureConfiguration.UIElement.AssignDOMXamlName = true;
+// Required to allow for Puppeteer to select XAML elements in the HTML DOM.
+Uno.UI.FeatureConfiguration.UIElement.AssignDOMXamlName = true;
 #if !DEBUG
-			Uno.UI.FeatureConfiguration.UIElement.RenderToStringWithId = false;
+Uno.UI.FeatureConfiguration.UIElement.RenderToStringWithId = false;
 #endif
 #if DEBUG
-			Uno.UI.FeatureConfiguration.UIElement.AssignDOMXamlProperties = true;
+Uno.UI.FeatureConfiguration.UIElement.AssignDOMXamlProperties = true;
 #endif
 
-			Microsoft.UI.Xaml.Application.Start(_ => _app = new App());
 
-		}
-	}
-}
+var host = UnoPlatformHostBuilder.Create()
+	.App(() => new SamplesApp.App())
+	.UseWebAssembly()
+	.Build();
+
+await host.RunAsync();
