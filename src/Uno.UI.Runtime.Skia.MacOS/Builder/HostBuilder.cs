84a9929a33fd7dd8c6197728988f45376e87c89a
Martin Zikmund <martin@zikmund.dev>
2025-04-30 10:30:03 +0200

    chore: Adjustments to updated targets
    (cherry picked from commit 1ce50b2b2d6efcbe171cb8baff94e357398d2e4e)

--- src/Uno.UI.Runtime.Skia.MacOS/Builder/HostBuilder.cs
+++ src/Uno.UI.Runtime.Skia.MacOS/Builder/HostBuilder.cs
@@ -2,13 +2,15 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Uno.UI.Hosting;
+using Uno.UI.Runtime.Skia;
 using Windows.UI.WebUI;
 
-namespace Uno.UI.Runtime.Skia;
+namespace Uno.UI.Hosting;
 
 public static class HostBuilder
 {
-	public static ISkiaHostBuilder UseMacOS(this ISkiaHostBuilder builder)
+	public static IUnoPlatformHostBuilder UseMacOS(this IUnoPlatformHostBuilder builder)
 	{
 		builder.AddHostBuilder(() => new MacOSHostBuilder());
 		return builder;
