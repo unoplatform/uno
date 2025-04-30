d180b415d284584fef167dec0fa0ad6b01897e40
Martin Zikmund <martin@zikmund.dev>
2025-04-30 10:43:46 +0200

    feat: Add hosting support to Apple UIKit
    (cherry picked from commit bcf25ddb2973f20ba9e70bfbc7bcea6939ab4209)

---
+++ src/Uno.UI.Runtime.Skia.AppleUIKit/Builder/AppleUIKitHostBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Uno.UI.Runtime.Skia.AppleUIKit;
+
+namespace Uno.UI.Hosting;
+
+internal partial class AppleUIKitHostBuilder : IPlatformHostBuilder
+{
+	public AppleUIKitHostBuilder()
+	{
+	}
+
+	public bool IsSupported => true;
+
+	public UnoPlatformHost Create(Func<Microsoft.UI.Xaml.Application> appBuilder)
+		=> new AppleUIKitHost(appBuilder);
+}
