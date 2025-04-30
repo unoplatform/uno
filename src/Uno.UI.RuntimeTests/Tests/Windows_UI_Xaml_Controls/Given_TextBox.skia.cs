666786d68162900990f136f1e0e2adc2c9e46396
Ramez Ragaa <ramezragaa@proton.me>
2025-04-29 21:13:30 -0400

    chore: fix build

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_TextBox.skia.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_TextBox.skia.cs
@@ -3843,7 +3843,7 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 			SUT.RaiseEvent(UIElement.KeyUpEvent, new KeyRoutedEventArgs(SUT, VirtualKey.Escape, VirtualKeyModifiers.None));
 		}
 
-		[ConditionalTest(IgnoredPlatforms = RuntimeTestPlatforms.SkiaWasm)] // needs paste permission
+		[TestMethod]
 		public async Task When_MaxLine_Paste()
 		{
 			using var _ = new TextBoxFeatureConfigDisposable();
