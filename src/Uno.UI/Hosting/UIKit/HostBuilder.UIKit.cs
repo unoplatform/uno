c979e3e61dc30b835d9828f422422d6f1e582a89
Martin Zikmund <martin@zikmund.dev>
2025-04-30 11:56:44 +0200

    feat: Use host builder on UIKit

---
+++ src/Uno.UI/Hosting/UIKit/HostBuilder.UIKit.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+
+using System;
+
+namespace Uno.UI.Hosting;
+
+public static class HostBuilder
+{
+	public static IUnoPlatformHostBuilder UseAppleUIKit(this IUnoPlatformHostBuilder builder, Action<IAppleUIKitHostBuilder>? appleUIKitBuilder = null)
+	{
+		builder.AddHostBuilder(() =>
+		{
+			var platformBuilder = new AppleUIKitHostBuilder();
+			if (appleUIKitBuilder is not null)
+			{
+				appleUIKitBuilder?.Invoke(platformBuilder);
+			}
+			return platformBuilder;
+		});
+
+		return builder;
+	}
+}
