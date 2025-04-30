e96983556d11b35dbaf22454d576ac1e8be5753b
Martin Zikmund <martin@zikmund.dev>
2025-04-30 13:10:02 +0200

    feat: Use app builder for WebAssembly

---
+++ src/Uno.UI/Hosting/UIKit/AppleUIKitHostBuilder.UIKit.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+using Uno.UI.Helpers;
+
+namespace Uno.UI.Hosting;
+
+internal partial class AppleUIKitHostBuilder : IPlatformHostBuilder
+{
+	private Type? _uiApplicationDelegateOverride;
+
+	public AppleUIKitHostBuilder()
+	{
+	}
+
+	public bool IsSupported => DeviceTargetHelper.IsUIKit();
+
+	public UnoPlatformHost Create(Func<Microsoft.UI.Xaml.Application> appBuilder, Type appType) =>
+		new AppleUIKitHost(appBuilder, _uiApplicationDelegateOverride);
+}
---
+++ src/Uno.UI/Hosting/WebAssembly/HostBuilder.wasm.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+
+using System;
+
+namespace Uno.UI.Hosting;
+
+public static class HostBuilder
+{
+	public static IUnoPlatformHostBuilder UseWebAssembly(this IUnoPlatformHostBuilder builder)
+	{
+		builder.AddHostBuilder(() => new WebAssemblyHostBuilder());
+
+		return builder;
+	}
+}
