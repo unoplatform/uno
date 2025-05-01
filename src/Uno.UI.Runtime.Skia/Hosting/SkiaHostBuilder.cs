f4b190f143b4231d5826778add797a941905648d
Jérôme Laban <jlaban@gmail.com>
2025-05-01 12:18:12 -0400

    chore: Adjust for deprecated SkiaHostBuilder and back compat

---
+++ src/Uno.UI.Runtime.Skia/Hosting/SkiaHostBuilder.cs
@@ -0,0 +1,8 @@
+namespace Uno.UI.Runtime.Skia;
+
+/// <summary>
+/// Legacy builder for desktop applications, use <see ref="UnoPlatformHostBuilder" /> instead.
+/// </summary>
+public class SkiaHostBuilder : UnoPlatformHostBuilder
+{
+}
