23bc5b97a4b05ce4bb3ace67e7b5a3231e38c290
Ramez Ragaa <ramezragaa@proton.me>
2025-05-02 11:21:07 -0400

    test: add When_Child_Removed_TotalMatrix_Updated

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Composition/Given_ContainerVisual.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Composition/Given_ContainerVisual.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
 using Private.Infrastructure;
 using Microsoft.UI.Composition;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Shapes;
+using Uno.UI.RuntimeTests.Helpers;
 
 namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Composition;
 
@@ -39,6 +44,24 @@ public class Given_ContainerVisual
 		children = containerVisual.GetChildrenInRenderOrderTestingOnly();
 		Assert.IsFalse(containerVisual.IsChildrenRenderOrderDirty);
 		Assert.AreEqual(1, children.Count());
+	}
+
+	[TestMethod]
+	[RunsOnUIThread]
+	public async Task When_Child_Removed_TotalMatrix_Updated()
+	{
+		var compositor = TestServices.WindowHelper.XamlRoot.Compositor;
+		var parent1 = compositor.CreateContainerVisual();
+		var parent2 = compositor.CreateContainerVisual();
+		var child = compositor.CreateSpriteVisual();
+
+		parent2.Offset = new Vector3(100, 0, 0);
+
+		parent1.Children.InsertAtTop(child);
+		Assert.IsTrue(child.TotalMatrix.IsIdentity);
+
+		parent2.Children.InsertAtTop(child);
+		Assert.IsFalse(child.TotalMatrix.IsIdentity);
 	}
 }
 #endif
