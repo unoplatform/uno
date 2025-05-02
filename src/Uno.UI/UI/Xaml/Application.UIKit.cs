43abe0206c4762cc266303b0e60ccf723fedb2f8
Martin Zikmund <martin@zikmund.dev>
2025-05-02 14:18:14 +0200

    refactor: Move constants to shared location

--- src/Uno.UI/UI/Xaml/Application.UIKit.cs
+++ src/Uno.UI/UI/Xaml/Application.UIKit.cs
@@ -15,9 +15,6 @@ namespace Microsoft.UI.Xaml
 	[Register("UnoAppDelegate")]
 	public partial class Application : UIApplicationDelegate
 	{
-		internal const string UIApplicationSceneManifestKey = "UIApplicationSceneManifest";
-		private const string GetConfigurationSelectorName = "application:configurationForConnectingSceneSession:options:";
-
 		private bool _preventSecondaryActivationHandling;
 
 		partial void InitializePartial()
