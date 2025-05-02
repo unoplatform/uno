1c185a072f70fe1e991ca7468142f18a6684ab97
Martin Zikmund <martin@zikmund.dev>
2025-05-02 14:18:29 +0200

    feat: Request secondary windows on iOS

--- src/Uno.UI.Runtime.Skia.AppleUIKit/UI/Xaml/Window/NativeWindowFactoryExtension.cs
+++ src/Uno.UI.Runtime.Skia.AppleUIKit/UI/Xaml/Window/NativeWindowFactoryExtension.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Foundation;
 using Microsoft.UI.Xaml;
 using UIKit;
+using Uno.Foundation.Logging;
 using Uno.UI.Xaml.Controls;
 
 namespace Uno.WinUI.Runtime.Skia.AppleUIKit.UI.Xaml;
@@ -15,6 +17,22 @@ internal class NativeWindowFactoryExtension : INativeWindowFactoryExtension
 
 	public bool SupportsMultipleWindows => true;
 
-	public INativeWindowWrapper CreateWindow(Window window, XamlRoot xamlRoot) =>
-		new NativeWindowWrapper(window, xamlRoot);
+	public INativeWindowWrapper CreateWindow(Window window, XamlRoot xamlRoot)
+	{
+		var wrapper = new NativeWindowWrapper(window, xamlRoot);
+
+		if (window != Window.InitialWindow)
+		{
+			// Request scene for the new window
+			var userActivity = new NSUserActivity(UnoSceneDelegate.UIApplicationSceneManifestKey);
+			var request = UISceneSessionActivationRequest.Create();
+			request.UserActivity = userActivity;
+			Action<NSError> errorAction = err => typeof(NativeWindowFactory).LogError()?.LogError($"Failed to create new window: {err}");
+			UIApplication.SharedApplication.ActivateSceneSession(
+				request,
+				errorAction);
+		}
+
+		return wrapper;
+	}
 }
