fb3c9b532e7d5e012ade55b53ec5f2a7bf6f9689
Martin Zikmund <martin@zikmund.dev>
2025-05-06 11:57:24 +0200

    feat: Update AppBar header to MUX 1.7.1

---
+++ src/Uno.UI/UI/Xaml/Controls/AppBar/AppBar.uno.cs
@@ -0,0 +1,27 @@
+﻿#if __APPLE_UIKIT__ || __ANDROID__
+#define HAS_NATIVE_COMMANDBAR
+#endif
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.UI.Xaml.Controls;
+
+partial class AppBar
+#if HAS_NATIVE_COMMANDBAR
+	: ICustomClippingElement
+#endif
+{
+#pragma warning disable CS0414
+#pragma warning disable CS0649
+#pragma warning disable CS0169
+	//UIElement? m_layoutTransitionElement;
+	private bool _isNativeTemplate;
+	//UIElement m_parentElementForLTEs;
+#pragma warning restore CS0414
+#pragma warning restore CS0649
+#pragma warning restore CS0169
+}
