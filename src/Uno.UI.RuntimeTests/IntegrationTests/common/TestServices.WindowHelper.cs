aaf9b7751164f16541d8d39f0bef2f94f29a5a32
Martin Zikmund <martin@zikmund.dev>
2025-05-06 20:43:54 +0200

    test: Add many new tests, adjust flyout helper further

--- src/Uno.UI.RuntimeTests/IntegrationTests/common/TestServices.WindowHelper.cs
+++ src/Uno.UI.RuntimeTests/IntegrationTests/common/TestServices.WindowHelper.cs
@@ -16,6 +16,8 @@ using Microsoft.UI.Xaml.Media.Imaging;
 #if HAS_UNO
 using DirectUI;
 using Uno.UI.Xaml.Input;
+using Windows.Foundation;
+
 
 
 #endif
@@ -122,6 +124,8 @@ namespace Private.Infrastructure
 			public static UnitTestDispatcherCompat RootElementDispatcher => UseActualWindowRoot
 				? (CurrentTestWindow is { } ? UnitTestDispatcherCompat.From(CurrentTestWindow) : UnitTestDispatcherCompat.Instance)
 				: UnitTestDispatcherCompat.From(EmbeddedTestRoot.control);
+
+			public static Rect WindowBounds => CurrentTestWindow?.Bounds ?? default;
 
 			internal static Page SetupSimulatedAppPage()
 			{
