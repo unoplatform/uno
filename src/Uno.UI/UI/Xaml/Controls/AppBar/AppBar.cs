ba4c463c6d46d5e1a5d959c86626505ccd88bb64
Martin Zikmund <martin@zikmund.dev>
2025-05-06 11:57:24 +0200

    feat: Update AppBar header to MUX 1.7.1

---
+++ src/Uno.UI/UI/Xaml/Controls/AppBar/AppBar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Represents the container control that holds app UI components for commanding and experiences.
+/// </summary>
+public partial class AppBar : ContentControl
+{
+}
