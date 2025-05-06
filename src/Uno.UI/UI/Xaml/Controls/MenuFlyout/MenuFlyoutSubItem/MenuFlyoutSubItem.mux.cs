d874c1facc406d38efff766076a361f6f30fed07
Martin Zikmund <martin@zikmund.dev>
2025-05-06 16:20:20 +0200

    chore: Update to latest

--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutSubItem/MenuFlyoutSubItem.mux.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutSubItem/MenuFlyoutSubItem.mux.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-// MUX Reference dxaml\xcp\core\core\elements\MenuFlyoutSubItem.cpp, tag winui3/release/1.5.4, commit 98a60c8
+// MUX Reference dxaml\xcp\core\core\elements\MenuFlyoutSubItem.cpp, tag winui3/release/1.7.1, commit 5f27a786ac96c
 
 using System;
 using System.Collections.Generic;
