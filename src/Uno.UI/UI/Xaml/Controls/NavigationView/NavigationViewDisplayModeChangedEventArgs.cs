e13d41d81b29d415603690796390805a854d2420
Martin Zikmund <martin@zikmund.dev>
2025-05-07 10:18:19 +0200

    refactor: Remove UWP-only code

--- src/Uno.UI/UI/Xaml/Controls/NavigationView/NavigationViewDisplayModeChangedEventArgs.cs
+++
@@ -1,7 +0,0 @@
-namespace Windows.UI.Xaml.Controls
-{
-	public partial class NavigationViewDisplayModeChangedEventArgs
-	{
-		public NavigationViewDisplayMode DisplayMode { get; internal set; }
-	}
-}
