2be8c81db0729c9332489baab3f6828e3301cfd1
Steve Bilogan <steve.bilogan@gmail.com>
2025-05-08 11:42:30 -0400

    chore: update commit

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -18,7 +18,7 @@ $external_docs = @{
     "uno.extensions"     = "caadf295630fb154d7d75c8d820f75aa8e014d92" #latest release/stable/5.2 branch commit
     "workshops"          = "e3c2a11a588b184d8cd3a6f88813e5615cca891d" #latest master commit
     "uno.samples"        = "e9ccf60d7d830acf7db4108a8aa5a2a1fc90c481" #latest master commit
-    "uno.chefs"          = "1398ee61d932d0b389d7858233636ecd8d9cdf42" #latest main commit
+    "uno.chefs"          = "fb42546bd11452872ab05016419af95c9977d574" #latest main commit
 }
 
 $uno_git_url = "https://github.com/unoplatform/"
