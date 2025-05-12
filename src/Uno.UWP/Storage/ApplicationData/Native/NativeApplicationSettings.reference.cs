1fef23aee19e3c9a0f83b158d6237be2f09ec433
Martin Zikmund <martin@zikmund.dev>
2025-05-12 17:41:12 +0200

    chore: Continue implementing ApplicationDataContainerSettings

---
+++ src/Uno.UWP/Storage/ApplicationData/Native/NativeApplicationSettings.reference.cs
@@ -0,0 +1,31 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.Storage;
+
+partial class NativeApplicationSettings
+{
+	private partial void SetSetting(string key, string value)
+	{
+		throw new NotImplementedException();
+	}
+
+	private partial bool TryGetSetting(string key, out string? value)
+	{
+		throw new NotImplementedException();
+	}
+
+	private partial bool RemoveSetting(string key)
+	{
+		throw new NotImplementedException();
+	}
+
+	private static partial bool SupportsLocality() => false;
+
+	private partial bool ContainsSetting(string key) => throw new NotImplementedException();
+}
