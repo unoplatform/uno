54006d47e1a2c4a2eb67f3354c8188d80a6482cf
Ramez Ragaa <ramezragaa@proton.me>
2025-05-12 14:04:36 -0400

    test: add Performance_1000ButtonsContinuousRendering
    (cherry picked from commit 17f35b52e14d35b267d055896188c72c3a863beb)

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/Performance/Performance_1000ButtonsContinuousRendering.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Uno.UI.Samples.Controls;
+using Microsoft.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UITests.Windows_UI_Xaml.Performance
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Sample("Performance")]
+	public sealed partial class Performance_1000ButtonsContinuousRendering : Page
+	{
+		public Performance_1000ButtonsContinuousRendering()
+		{
+			this.InitializeComponent();
+			for (var i = 0; i < 1000; i++)
+			{
+				(wp as Panel).Children.Add(new Button { Content = i.ToString() });
+			}
+
+			Loaded += (s, e) => colorStoryboard.Begin();
+		}
+	}
+}
