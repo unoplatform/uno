a8b42d64827a87ecdc43df754f0f3cbbb6b6b1a0
Ramez Ragaa <ramezragaa@proton.me>
2025-05-14 11:41:56 -0400

    test: add TextBox_Shortcuts_MacOSSkiaWasm
    (cherry picked from commit 65293348633c5041fcc87c645df8c87f941c077c)

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/TextBox/TextBox_Shortcuts_MacOSSkiaWasm.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Uno.UI.Samples.Controls;
+using Microsoft.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.TextBox;
+
+[Sample("TextBox", IsManualTest = true)]
+public sealed partial class TextBox_Shortcuts_MacOSSkiaWasm : Page
+{
+	public TextBox_Shortcuts_MacOSSkiaWasm()
+	{
+		this.InitializeComponent();
+	}
+}
