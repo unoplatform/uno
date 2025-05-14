b55fcafe7302ac83aea97b0e7e2307cc97ad2cc8
Ramez Ragaa <ramezragaa@proton.me>
2025-05-14 14:04:51 -0400

    fix(mpe): fix coord translation between vlc window and uno window on win32

--- src/AddIns/Uno.UI.MediaPlayer.Skia.Win32/Win32MediaPlayerPresenterExtension.cs
+++ src/AddIns/Uno.UI.MediaPlayer.Skia.Win32/Win32MediaPlayerPresenterExtension.cs
@@ -149,10 +149,7 @@ public class Win32MediaPlayerPresenterExtension : IMediaPlayerPresenterExtension
 		{
 			case PInvoke.WM_POINTERDOWN or PInvoke.WM_POINTERUP or PInvoke.WM_POINTERWHEEL or PInvoke.WM_POINTERHWHEEL
 				or PInvoke.WM_POINTERENTER or PInvoke.WM_POINTERLEAVE or PInvoke.WM_POINTERUPDATE:
-				var rect = _presenter.GetAbsoluteBoundsRect();
-				var scale = _presenter.XamlRoot!.RasterizationScale;
-				var point = new Point((int)(rect.Location.X / scale), (int)(rect.Location.Y / scale));
-				Win32WindowWrapper.XamlRootMap.GetHostForRoot(_presenter.XamlRoot!)!.OnPointer(msg, wParam, new Point(-(int)point.X, -(int)point.Y));
+				Win32WindowWrapper.XamlRootMap.GetHostForRoot(_presenter.XamlRoot!)!.OnPointer(msg, wParam, _hwnd);
 				return new LRESULT(0);
 		}
 
