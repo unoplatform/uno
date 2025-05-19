86dfa45565b959a58ac4d5205f7d377ba68f30ef
Martin Zikmund <martin@zikmund.dev>
2025-05-19 14:56:39 +0200

    feat: Update NumberBox helpers to winui3/release/1.7.1

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/NumberBox/NumberBoxSpinButtonPlacementMode.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/NumberBox/NumberBoxSpinButtonPlacementMode.cs
@@ -1,9 +1,23 @@
-﻿namespace Microsoft/* UWP don't rename */.UI.Xaml.Controls
+﻿namespace Microsoft/* UWP don't rename */.UI.Xaml.Controls;
+
+/// <summary>
+/// Defines values that specify how the spin buttons used to increment or decrement the Value of a NumberBox are displayed.
+/// </summary>
+public enum NumberBoxSpinButtonPlacementMode
 {
-	public enum NumberBoxSpinButtonPlacementMode
-	{
-		Hidden,
-		Compact,
-		Inline
-	};
-}
+	/// <summary>
+	/// The spin buttons are not displayed.
+	/// </summary>
+	Hidden = 0,
+
+	/// <summary>
+	/// The spin buttons have two visual states, depending on focus. By default, the spin buttons are displayed in a compact,
+	/// vertical orientation. When the Numberbox gets focus, the spin buttons expand.
+	/// </summary>
+	Compact = 1,
+
+	/// <summary>
+	/// The spin buttons are displayed in an expanded, horizontal orientation.
+	/// </summary>
+	Inline = 2
+};
--- src/Uno.UI/Microsoft/UI/Xaml/Controls/NumberBox/NumberBoxValueChangedEventArgs.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/NumberBox/NumberBoxValueChangedEventArgs.cs
@@ -1,19 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Microsoft/* UWP don't rename */.UI.Xaml.Controls;
 
-namespace Microsoft/* UWP don't rename */.UI.Xaml.Controls
+/// <summary>
+/// Provides event data for the NumberBox.ValueChanged event.
+/// </summary>
+public partial class NumberBoxValueChangedEventArgs
 {
-	public partial class NumberBoxValueChangedEventArgs
+	internal NumberBoxValueChangedEventArgs(double oldValue, double newValue)
 	{
-		internal NumberBoxValueChangedEventArgs(double oldValue, double newValue)
-		{
-			OldValue = oldValue;
-			NewValue = newValue;
-		}
-
-		public double OldValue { get; }
-
-		public double NewValue { get; }
+		OldValue = oldValue;
+		NewValue = newValue;
 	}
+
+	/// <summary>
+	/// Contains the old Value being replaced in a NumberBox.
+	/// </summary>
+	public double OldValue { get; }
+
+	/// <summary>
+	/// Contains the new Value to be set for a NumberBox.
+	/// </summary>
+	public double NewValue { get; }
 }
