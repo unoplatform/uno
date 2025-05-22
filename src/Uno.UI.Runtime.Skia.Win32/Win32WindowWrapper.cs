a6f1efae052c437894df3333c723bc1230a5a843
Ramez Ragaa <ramezragaa@proton.me>
2025-05-22 16:15:51 -0400

    fix(win32): avoid showing an initial white frame on startup

--- src/Uno.UI.Runtime.Skia.Win32/Win32WindowWrapper.cs
+++ src/Uno.UI.Runtime.Skia.Win32/Win32WindowWrapper.cs
@@ -230,6 +230,10 @@ internal partial class Win32WindowWrapper : NativeWindowWrapperBase, IXamlRootHo
 				MINMAXINFO* info = (MINMAXINFO*)lParam.Value;
 				info->ptMinTrackSize = new Point((int)_applicationView.PreferredMinSize.Width, (int)_applicationView.PreferredMinSize.Height);
 				return new LRESULT(0);
+			case PInvoke.WM_ERASEBKGND: // Without painting also on WM_ERASEBKGND, we get an initial white frame
+				this.LogTrace()?.Trace($"WndProc received a {nameof(PInvoke.WM_ERASEBKGND)} message.");
+				Paint();
+				break;
 			case PInvoke.WM_PAINT:
 				this.LogTrace()?.Trace($"WndProc received a {nameof(PInvoke.WM_PAINT)} message.");
 				Paint();
