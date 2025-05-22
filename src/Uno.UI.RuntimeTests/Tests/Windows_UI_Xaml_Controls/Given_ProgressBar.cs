e0b705957fb7b6242e8c290eb665aa60cc3d0555
Martin Zikmund <martin@zikmund.dev>
2025-05-22 12:43:35 +0200

    test: Validate automation peer behavior

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ProgressBar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Automation;
+using Microsoft.UI.Xaml.Automation.Peers;
+using Microsoft.UI.Xaml.Controls;
+using Private.Infrastructure;
+using Uno.UI.Xaml;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls;
+
+[TestClass]
+public class Given_ProgressBar
+{
+#if HAS_UNO
+	[TestMethod]
+	[RunsOnUIThread]
+	public async Task When_ProgressBar_Automation_Listener_Attached()
+	{
+		try
+		{
+			var grid = new Grid() { Width = 100, Height = 100 };
+			var progressBar = new ProgressBar();
+			grid.Children.Add(progressBar);
+			TestServices.WindowHelper.WindowContent = grid;
+			await TestServices.WindowHelper.WaitForLoaded(grid);
+
+			var stubListener = new StubListener();
+			AutomationPeer.TestAutomationPeerListener = stubListener;
+			progressBar.Maximum = 10;
+			Assert.IsTrue(stubListener.Notified);
+		}
+		finally
+		{
+			AutomationPeer.TestAutomationPeerListener = null;
+		}
+	}
+
+	private class StubListener : IAutomationPeerListener
+	{
+		public bool Notified { get; private set; }
+
+		public bool ListenerExistsHelper(AutomationEvents eventId) => true;
+		public void NotifyPropertyChangedEvent(AutomationPeer peer, AutomationProperty automationProperty, object oldValue, object newValue) =>
+			Notified = true;
+	}
+#endif
+}
