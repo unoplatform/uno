b0d8217de9f2e6dc77d1ba42e229c1a1670278ea
Ramez Ragaa <ramezragaa@proton.me>
2025-05-23 09:45:40 -0400

    test: add Window_Resizing_Flickerg
    (cherry picked from commit fcb0aff9dce8b683625286c922250bb15deeb40c)

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml/WindowTests/Window_Resizing_Flicker.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UITests.Windows_UI_Xaml.WindowTests
+{
+	[Sample("Windowing", IsManualTest = true)]
+	public sealed partial class Window_Resizing_Flicker : UserControl
+	{
+		public Window_Resizing_Flicker()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
