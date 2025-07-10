af90c7efa0fdf68029d15ffebe9bf0520f5ffcc5
Ramez Ragaa <ramezragaa@proton.me>
2025-07-10 08:00:18 -0400

    chore: remove UpdatePaint

--- src/Uno.UI.Composition/Composition/CompositionBrush.skia.cs
+++ src/Uno.UI.Composition/Composition/CompositionBrush.skia.cs
@@ -7,13 +7,6 @@ namespace Microsoft.UI.Composition
 	public partial class CompositionBrush
 	{
 		/// <summary>
-		/// Must be overriden by all supported brushes even if <see cref="SupportsRender"/> is true.
-		/// </summary>
-		internal virtual void UpdatePaint(SKPaint paint, SKRect bounds)
-		{
-		}
-
-		/// <summary>
 		/// Only used when <see cref="SupportsRender"/> is true and is not
 		/// supported by all brushes.
 		/// </summary>
@@ -22,14 +15,5 @@ namespace Microsoft.UI.Composition
 		internal virtual bool CanPaint() => false;
 
 		internal virtual bool RequiresRepaintOnEveryFrame => false;
-
-		/// <summary>
-		/// Indicates support for a fast path that uses <see cref="Render"/> during the render cycle
-		/// instead of using <see cref="UpdatePaint"/> to fill an SKPaint with what needs to be drawn
-		/// ahead of time and then using that SKPaint later during the render cycle.
-		/// This cannot be true for all brushes since some brushes don't draw their own content but
-		/// are effectively arbitrary shaders that read pixels and map them to new values
-		/// </summary>
-		internal virtual bool SupportsRender => false;
 	}
 }
--- src/Uno.UI.Composition/Composition/SpriteVisual.skia.cs
+++ src/Uno.UI.Composition/Composition/SpriteVisual.skia.cs
@@ -9,52 +9,7 @@ namespace Microsoft.UI.Composition
 {
 	public partial class SpriteVisual : ContainerVisual
 	{
-		private readonly SKPaint _paint = new() { IsAntialias = true };
-
-		partial void OnBrushChangedPartial(CompositionBrush? brush) => UpdatePaint();
-
-		private void UpdatePaint()
-		{
-			if (!Brush?.SupportsRender ?? false)
-			{
-				Brush?.UpdatePaint(_paint, new SKRect(left: 0, top: 0, right: Size.X, bottom: Size.Y));
-			}
-		}
-
-		internal override void Paint(in PaintingSession session)
-		{
-			if (Brush is null)
-			{
-				return;
-			}
-
-			if (Brush.SupportsRender)
-			{
-				Brush.Render(session.Canvas, new SKRect(left: 0, top: 0, right: Size.X, bottom: Size.Y));
-				return;
-			}
-
-			if (Brush is IOnlineBrush onlineBrush && onlineBrush.IsOnline)
-			{
-				onlineBrush.Paint(session, new SKRect(left: 0, top: 0, right: Size.X, bottom: Size.Y));
-				return;
-			}
-
-			_paint.ColorFilter = session.OpacityColorFilter;
-
-			if (Brush is CompositionEffectBrush { HasBackdropBrushInput: true })
-			{
-				session.Canvas.SaveLayer(new SKCanvasSaveLayerRec { Backdrop = _paint.ImageFilter });
-				session.Canvas.Restore();
-			}
-			else
-			{
-				session.Canvas.DrawRect(
-					new SKRect(left: 0, top: 0, right: Size.X, bottom: Size.Y),
-					_paint
-				);
-			}
-		}
+		internal override void Paint(in PaintingSession session) => Brush?.Render(session.Canvas, new SKRect(left: 0, top: 0, right: Size.X, bottom: Size.Y));
 
 		internal override bool CanPaint() => Brush?.CanPaint() ?? false;
 		internal override bool RequiresRepaintOnEveryFrame => Brush?.RequiresRepaintOnEveryFrame ?? false;
