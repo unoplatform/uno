c7ed82e4ec38c57352bc85914f5430bd64536028
Martin Zikmund <martin@zikmund.dev>
2025-07-22 15:33:13 +0200

    feat: `NavigationViewItemPresenter` update to latest WinUI sources
    ^ Conflicts:
    ^	src/Uno.UI/Microsoft/UI/Xaml/Controls/NavigationView/NavigationViewItemPresenter.cs
    ^	src/Uno.UI/Microsoft/UI/Xaml/Controls/NavigationView/NavigationViewItemSeparator.cs
    ^	src/Uno.UI/Microsoft/UI/Xaml/Controls/NavigationView/TopNavigationViewDataProvider.cs

--- src/Uno.UI/UI/Xaml/Controls/NavigationView/NavigationViewItemPresenter.Header.cs
+++ src/Uno.UI/UI/Xaml/Controls/NavigationView/NavigationViewItemPresenter.Header.cs
@@ -1,26 +1,29 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-// MUX Reference NavigationViewItemPresenter.h, commit d3fef08
+// MUX Reference NavigationViewItemPresenter.h, commit 65718e2813
 
-using Uno.Disposables;
-using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Animation;
+using Uno.Disposables;
 
 namespace Microsoft.UI.Xaml.Controls.Primitives;
 
 public partial class NavigationViewItemPresenter
 {
+	private bool m_isChevronPressed;
 	private double m_compactPaneLengthValue = 40;
+	private double m_leftIndentation;
+	private uint m_trackedPointerId;
 
 	private NavigationViewItemHelper<NavigationViewItemPresenter> m_helper;
-
 	private Grid m_contentGrid;
 	private ContentPresenter m_infoBadgePresenter;
 	private Grid m_expandCollapseChevron;
 
-	private SerialDisposable m_expandCollapseChevronTappedToken = new();
-
-	private double m_leftIndentation;
+	private readonly SerialDisposable m_expandCollapseChevronPointerPressedRevoker = new();
+	private readonly SerialDisposable m_expandCollapseChevronPointerReleasedRevoker = new();
+	private readonly SerialDisposable m_expandCollapseChevronPointerExitedRevoker = new();
+	private readonly SerialDisposable m_expandCollapseChevronPointerCanceledRevoker = new();
+	private readonly SerialDisposable m_expandCollapseChevronPointerCaptureLostRevoker = new();
 
 	private Storyboard m_chevronExpandedStoryboard;
 	private Storyboard m_chevronCollapsedStoryboard;
