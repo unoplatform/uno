d5de501dd6e488b659b047b13d305e5b439ef190
Martin Zikmund <martin@zikmund.dev>
2025-07-22 15:33:13 +0200

    feat: `NavigationViewItemPresenter` update to latest WinUI sources

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/NavigationView/NavigationViewItemPresenterTemplateSettings.Properties.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/NavigationView/NavigationViewItemPresenterTemplateSettings.Properties.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-// MUX Reference NavigationViewItemPresenterTemplateSettings.properties.cpp, commit 465f0d7
+// MUX Reference NavigationViewItemPresenterTemplateSettings.properties.cpp, commit 65718e2813
 
 using Microsoft.UI.Xaml;
 
