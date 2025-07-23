9685a2e1050c8c7aee15d84abcd9104b983afc18
Martin Zikmund <martin@zikmund.dev>
2025-07-23 14:20:17 +0200

    chore: Undo missing API changes for now

--- src/Uno.UI/Microsoft/UI/Content/ContentEnvironmentSettingChangedEventArgs.cs
+++
@@ -1,7 +0,0 @@
-namespace Microsoft.UI.Content;
-
-public partial class ContentEnvironmentSettingChangedEventArgs
-{
-	//UNO TODO: Properly implement ContentEnvironmentSettingChangedEventArgs
-	public string SettingName => string.Empty;
-}
