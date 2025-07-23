5090802ba00c3736301adbf3be9979b0bfe9c021
Martin Zikmund <martin@zikmund.dev>
2025-07-23 14:20:17 +0200

    chore: Undo missing API changes for now

--- src/Uno.UI/Microsoft/UI/Content/ContentEnvironmentStateChangedEventArgs.cs
+++
@@ -1,9 +0,0 @@
-namespace Microsoft.UI.Content;
-
-public partial class ContentEnvironmentStateChangedEventArgs
-{
-	//UNO TODO: Properly implement ContentEnvironmentStateChangedEventArgs
-	public bool DidAppWindowIdChange => false;
-
-	public bool DidDisplayIdChange => false;
-}
