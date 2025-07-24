5893bfedda0ea281218924d2bfb146d8fb0fc460
Jonathan Pryor <jonpryor@vt.edu>
2025-07-24 15:15:56 -0400

    chore: fix local-android-uitest-run.sh et al to work on macOS
    Export and use `$ANDROID_AVD_HOME`, so that we have a "known" location
    for whare Emulator images are created.  (Not sure what's wrong with
    my environment, but they are not reliably created or loaded from
    `$HOME/.android/avd`.)
    
    Update `local-android-uitest-run.sh` so that it doesn't require running
    the script from the `build/test-scripts` directory.  You can now run it
    from topdir:
    
    	UITEST_IS_LOCAL=true \
    	  build/test-scripts/local-android-uitest-run.sh
    
    Update the expected paths to match .NET 9 output.

--- build/test-scripts/local-android-uitest-run.sh
+++ build/test-scripts/local-android-uitest-run.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-export BUILD_SOURCESDIRECTORY=`pwd`/../..
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+export BUILD_SOURCESDIRECTORY="$SCRIPT_DIR/../.."
 export BUILD_ARTIFACTSTAGINGDIRECTORY=/tmp/uno-uitests-results
 export UITEST_SNAPSHOTS_ONLY=false
 export UITEST_SNAPSHOTS_GROUP=01
@@ -17,16 +19,16 @@ export ANDROID_SIMULATOR_APILEVEL=28
 export SAMPLEAPP_ARTIFACT_NAME=android-netcoremobile-sampleapp
 export UITEST_IS_LOCAL=true
 
-mkdir -p $BUILD_ARTIFACTSTAGINGDIRECTORY
+mkdir -p "$BUILD_ARTIFACTSTAGINGDIRECTORY"
 
 # Comment out the following line to avoid full rebuild for subsequent runs
 # pushd $BUILD_SOURCESDIRECTORY/src/SamplesApp/SamplesApp.netcoremobile
-# dotnet publish -f net7.0-android -c Release -p:UnoTargetFrameworkOverride=net7.0-android /bl:$BUILD_ARTIFACTSTAGINGDIRECTORY/android-netcoremobile-sampleapp.binlog
+# dotnet publish -f net9.0-android -c Release -p:UnoTargetFrameworkOverride=net9.0-android /bl:$BUILD_ARTIFACTSTAGINGDIRECTORY/android-netcoremobile-sampleapp.binlog
 # popd
 
-mkdir -p $BUILD_SOURCESDIRECTORY/build/$SAMPLEAPP_ARTIFACT_NAME/android
-cp -f $BUILD_SOURCESDIRECTORY/src/SamplesApp/SamplesApp.netcoremobile/bin/Release/net7.0-android/uno.platform.unosampleapp-Signed.apk $BUILD_SOURCESDIRECTORY/build/$SAMPLEAPP_ARTIFACT_NAME/android/uno.platform.unosampleapp-Signed.apk
+mkdir -p "$BUILD_SOURCESDIRECTORY/build/$SAMPLEAPP_ARTIFACT_NAME/android"
+cp -f "$BUILD_SOURCESDIRECTORY/src/SamplesApp/SamplesApp.netcoremobile/bin/Release/net9.0-android/publish/uno.platform.unosampleapp-Signed.apk" "$BUILD_SOURCESDIRECTORY/build/$SAMPLEAPP_ARTIFACT_NAME/android/uno.platform.unosampleapp-Signed.apk"
 
 rm -fR ~/.android/avd/xamarin_android_emulator.avd
 
-./android-uitest-run.sh
+exec "$SCRIPT_DIR/android-uitest-run.sh"
