4df081707370d76c53cd4441876e161b9746d8cf
McNets <mcnets@gmail.com>
2025-07-24 18:58:37 +0200

    fix: Windows enum has to be NativeWinUJI

--- src/Uno.UI.RuntimeTests/Tests/Uno_UI_Toolkit/Given_RuntimePlatformDetected.cs
+++ src/Uno.UI.RuntimeTests/Tests/Uno_UI_Toolkit/Given_RuntimePlatformDetected.cs
@@ -60,7 +60,7 @@ namespace Uno.UI.RuntimeTests.Tests.Uno_UI_Toolkit
 		[TestMethod]
 		public void When_IsWindows()
 		{
-			Uno.UI.Toolkit.PlatformRuntimeHelper.Current.Should().Be(Uno.UI.Toolkit.UnoRuntimePlatform.Windows);
+			Uno.UI.Toolkit.PlatformRuntimeHelper.Current.Should().Be(Uno.UI.Toolkit.UnoRuntimePlatform.NativeWinUI);
 		}
 #else
 		[TestMethod]
