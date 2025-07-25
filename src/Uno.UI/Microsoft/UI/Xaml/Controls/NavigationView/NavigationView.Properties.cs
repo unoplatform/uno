294e010112f4d6d4de1e97d417d38db5633ffb62
Martin Zikmund <martin@zikmund.dev>
2025-07-25 17:36:52 +0200

    chore: Adjust other updates

--- src/Uno.UI/Microsoft/UI/Xaml/Controls/NavigationView/NavigationView.Properties.cs
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/NavigationView/NavigationView.Properties.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-// MUX reference NavigationView.properties.cpp, commit 2ec9b1c
+// MUX reference NavigationView.properties.cpp, commit 65718e2813
 
 using System.Collections.Generic;
 using Windows.Foundation;
