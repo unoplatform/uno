5e09e9ac10eb0c9e1efbfd0efb91da3d81e995eb
Martin Zikmund <martin@zikmund.dev>
2025-07-25 17:36:52 +0200

    chore: Adjust other updates

---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/NavigationView/NavigationViewPaneClosingEventArgs.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX reference NavigationViewPaneClosingEventArgs.cpp, commit 65718e2813
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Provides data for the NavigationView.PaneClosing event.
+/// </summary>
+public partial class NavigationViewPaneClosingEventArgs
+{
+	private bool m_cancelled;
+	private SplitViewPaneClosingEventArgs m_splitViewClosingArgs;
+
+	internal NavigationViewPaneClosingEventArgs()
+	{
+	}
+
+	/// <summary>
+	/// Gets or sets a value that indicates whether
+	/// the event should be canceled.
+	/// </summary>
+	public bool Cancel
+	{
+		get => m_cancelled;
+		set
+		{
+			m_cancelled = value;
+
+			var args = m_splitViewClosingArgs;
+			if (args != null)
+			{
+				args.Cancel = value;
+			}
+		}
+	}
+
+	internal void SplitViewClosingArgs(SplitViewPaneClosingEventArgs value)
+	{
+		m_splitViewClosingArgs = value;
+	}
+}
---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/NavigationView/NavigationViewPaneDisplayMode.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference NavigationView.idl, commit 65718e2813
+
+namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Defines constants that specify how and where the NavigationView pane is shown.
+/// </summary>
+public enum NavigationViewPaneDisplayMode
+{
+	/// <summary>
+	/// The pane is shown on the left side of the control,
+	/// and changes between minimal, compact, and full
+	/// states depending on the width of the window.
+	/// </summary>
+	Auto = 0,
+
+	/// <summary>
+	/// The pane is shown on the left side of the control
+	/// in its fully open state.
+	/// </summary>
+	Left = 1,
+
+	/// <summary>
+	/// The pane is shown at the top of the control.
+	/// </summary>
+	Top = 2,
+
+	/// <summary>
+	/// The pane is shown on the left side of the control.
+	/// Only the pane icons are shown by default.
+	/// </summary>
+	LeftCompact = 3,
+
+	/// <summary>
+	/// The pane is shown on the left side of the control.
+	/// Only the pane menu button is shown by default.
+	/// </summary>
+	LeftMinimal = 4
+}
