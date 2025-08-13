7afed43646e9fe925ea04058b93f02799d9246a0
Ramez Ragaa <ramezragaa@proton.me>
2025-08-13 14:15:53 -0400

    chore: X11 adjustment

--- src/Uno.UI.Runtime.Skia.X11/Rendering/X11XamlRootHost.Rendering.cs
+++ src/Uno.UI.Runtime.Skia.X11/Rendering/X11XamlRootHost.Rendering.cs
@@ -10,42 +10,23 @@ internal partial class X11XamlRootHost
 {
 	private readonly EventLoop _renderingEventLoop;
 	private int _renderingScheduled;
-	private readonly object _nextRenderParamsLock = new();
-	private RenderParams? _nextRenderParams;
 
 	void IXamlRootHost.InvalidateRender()
 	{
-		if (_renderer is not null && (this as IXamlRootHost).RootElement is { XamlRoot.LastRenderedFrame: { } lastRenderedFrame } rootElement)
+		if (_renderer is not null && (this as IXamlRootHost).RootElement is { } rootElement)
 		{
 			using var lockDisposable = X11Helper.XLock(TopX11Window.Display);
 			XWindowAttributes attributes = default;
 			_ = XLib.XGetWindowAttributes(TopX11Window.Display, TopX11Window.Window, ref attributes);
-
-			var scale = rootElement?.XamlRoot is { } root
-				? root.RasterizationScale
-				: 1;
-
-			lock (_nextRenderParamsLock)
-			{
-				_nextRenderParams = new RenderParams(lastRenderedFrame.frame, lastRenderedFrame.nativeElementClipPath, (float)scale);
-			}
 
 			if (Interlocked.Exchange(ref _renderingScheduled, 1) == 0)
 			{
 				_renderingEventLoop.Schedule(() =>
 				{
 					Volatile.Write(ref _renderingScheduled, 0);
-					if (_nextRenderParams is { } renderParams)
-					{
-						_renderer.Render(renderParams.Picture, renderParams.NativeClippingPath, renderParams.Scale);
-					}
+					_renderer.Render();
 				});
 			}
 		}
 	}
-
-	private readonly record struct RenderParams(
-		SKPicture Picture,
-		SKPath NativeClippingPath,
-		float Scale);
 }
