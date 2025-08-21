ed2b4f195612ab58f7221188b2bc8c937a3836e4
Martin Zikmund <martin@zikmund.dev>
2025-08-21 09:58:08 +0200

    chore: Wip

---
+++ src/Uno.UI/UI/Xaml/Controls/Repeater/CollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// ElementManager.cpp, commit 864c068
+
+using System.Collections.Generic;
+
+namespace Microsoft.UI.Xaml.Controls
+{
+	internal static partial class CollectionExtensions
+	{
+		public static bool TryGetElementAt<T>(this IList<T> collection, int index, out T element)
+			where T : class
+		{
+			if (index < collection.Count)
+			{
+				element = collection[index];
+				return element != default; // TODO UNO
+			}
+			else
+			{
+				element = default;
+				return false;
+			}
+		}
+
+		public static void AddOrInsert<T>(this IList<T> collection, int index, T element)
+		{
+			if (index >= collection.Count)
+			{
+				collection.Add(element);
+			}
+			else
+			{
+				collection.Insert(index, element);
+			}
+		}
+	}
+
+}
---
+++ src/Uno.UI/UI/Xaml/Controls/Repeater/ElementManager.h.mux.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Windows.Foundation;
+
+namespace Microsoft.UI.Xaml.Controls;
+
+internal partial class ElementManager
+{
+	internal ElementManager(ITrackerHandleManager owner, bool useLayoutBounds = true)
+	{
+		m_owner = owner;
+		m_useLayoutBounds = useLayoutBounds;
+	}
+
+	private bool m_useLayoutBounds;
+	private readonly List<UIElement> m_realizedElements = new List<UIElement>(); // TODO MZ: These are tracked refs!
+	private readonly List<Rect> m_realizedElementLayoutBounds = new List<Rect>();
+	private int m_firstRealizedDataIndex = -1;
+	private VirtualizingLayoutContext m_context;
+}
