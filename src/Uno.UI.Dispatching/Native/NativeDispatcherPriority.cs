38ca2f7859e531d97d4f2a412dc5df3bfeccb4cf
Ramez Ragaa <ramezragaa@proton.me>
2025-08-24 18:44:29 -0400

    chore: queue on Normal instead of High every while

--- src/Uno.UI.Dispatching/Native/NativeDispatcherPriority.cs
+++ src/Uno.UI.Dispatching/Native/NativeDispatcherPriority.cs
@@ -2,11 +2,9 @@
 {
 	internal enum NativeDispatcherPriority
 	{
-		Render = 0,
-		High = 1,
-		Normal = 2,
-		Low = 3,
-		Idle = 4,
-		EnumLength
+		High = 0,
+		Normal = 1,
+		Low = 2,
+		Idle = 3
 	}
 }
