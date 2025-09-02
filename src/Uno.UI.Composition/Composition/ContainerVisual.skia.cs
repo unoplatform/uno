c16e2c7947bf6598a07e95dd1b5f34bc8a4d982b
David <github.site@torick.net>
2025-09-02 04:50:29 -0400

    chore(perf): Avoid usgae of SKIA structure when not required

--- src/Uno.UI.Composition/Composition/ContainerVisual.skia.cs
+++ src/Uno.UI.Composition/Composition/ContainerVisual.skia.cs
@@ -7,6 +7,7 @@ using System.Numerics;
 using System.Runtime.InteropServices;
 using SkiaSharp;
 using Windows.Foundation;
+using Uno.Extensions;
 
 namespace Microsoft.UI.Composition;
 
@@ -124,6 +125,27 @@ public partial class ContainerVisual : Visual
 		return true;
 	}
 
+	/// <returns>true if a ViewBox exists</returns>
+	internal Rect? GetArrangeClipPathInElementCoordinateSpace()
+	{
+		if (LayoutClip is not { isAncestorClip: var isAncestorClip, rect: var rect })
+		{
+			return default;
+		}
+
+		if (isAncestorClip)
+		{
+			Matrix4x4.Invert(TotalMatrix, out var totalMatrixInverted);
+			var childToParentTransform = Parent!.TotalMatrix * totalMatrixInverted;
+			if (!childToParentTransform.IsIdentity)
+			{
+				rect = rect.Transform(childToParentTransform.ToMatrix3x2());
+			}
+		}
+
+		return rect;
+	}
+
 	private static SKPath _sparePrePaintingClippingPath = new SKPath();
 
 	internal override bool GetPrePaintingClipping(SKPath dst) // TODO: Do not use SKPath here, bad for perf and prevents usage for IDirectManipulationHandler.IsInBoundsForResume
@@ -134,6 +156,12 @@ public partial class ContainerVisual : Visual
 
 		if (base.GetPrePaintingClipping(dst))
 		{
+			// TODO: SKPath-less
+			//if (GetArrangeClipPathInElementCoordinateSpace() is {} clipping)
+			//{
+			//	dst.AddRect(clipping.ToSKRect());
+			//}
+
 			if (GetArrangeClipPathInElementCoordinateSpace(prePaintingClipPath))
 			{
 				dst.Op(prePaintingClipPath, SKPathOp.Intersect, dst);
@@ -143,6 +171,15 @@ public partial class ContainerVisual : Visual
 		}
 		else
 		{
+			// TODO: SKPath-less
+			//if (GetArrangeClipPathInElementCoordinateSpace() is {} clipping)
+			//{
+			//	dst.Reset();
+			//	dst.AddRect(clipping.ToSKRect());
+
+			//	return true;
+			//}
+
 			if (GetArrangeClipPathInElementCoordinateSpace(prePaintingClipPath))
 			{
 				dst.Reset();
