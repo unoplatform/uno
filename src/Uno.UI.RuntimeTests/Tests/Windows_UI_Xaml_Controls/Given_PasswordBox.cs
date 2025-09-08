0c72a095b8549f99afba20b7944735d6613ab4e4
Martin Zikmund <martin@zikmund.dev>
2025-09-08 16:11:32 +0200

    chore: Adjust types

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_PasswordBox.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_PasswordBox.cs
@@ -131,9 +131,9 @@ public class Given_PasswordBox
 		var passwordBox = new PasswordBox();
 
 #if !HAS_UNO
-		char defaultPasswordBoxChar = '\u25CF';
+		string defaultPasswordBoxChar = "\u25CF";
 #else
-		char defaultPasswordBoxChar = PasswordBox.DefaultPasswordChar[0];
+		string defaultPasswordBoxChar = PasswordBox.DefaultPasswordChar;
 #endif
 		// Test default value
 		Assert.AreEqual(defaultPasswordBoxChar, passwordBox.PasswordChar);
