9e69ed1f8c74a9a4a9967d9156c8534d31ae3292
Carl de Billy <carl@platform.uno>
2025-09-18 14:48:38 -0400

    fix: Add detailed logging for WebSocket connection reset errors in dev server and client paths

--- src/Uno.UI.RemoteControl/Helpers/WebSocketHelper.cs
+++ src/Uno.UI.RemoteControl/Helpers/WebSocketHelper.cs
@@ -70,8 +70,21 @@ public static class WebSocketHelper
 				}
 			}
 		}
-		catch (IOException)
+		catch (IOException ex)
 		{
+#if IS_DEVSERVER
+			var log = Uno.Extensions.LogExtensionPoint.Log(typeof(Frame));
+			if (log.IsEnabled(Microsoft.Extensions.Logging.LogLevel.Debug))
+			{
+				Microsoft.Extensions.Logging.LoggerExtensions.LogDebug(log, "Connection reset by peer.", []);
+			}
+#else // Client
+			var log = Uno.Foundation.Logging.LogExtensionPoint.Log(typeof(Frame));
+			if (log.IsEnabled(Uno.Foundation.Logging.LogLevel.Debug))
+			{
+				log.LogDebug("Connection reset by peer.", ex);
+			}
+#endif
 			return null; // Connection reset by peer, no need to report this.
 		}
 		finally
