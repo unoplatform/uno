27ad6e5ac0d0cdab0b7398d5da952f7e5fc76f15
McNets <mcnets@gmail.com>
2025-09-18 17:42:33 +0200

    fix: Removed unnecesary usings.

--- src/Uno.UI.Toolkit/RuntimeHelper.cs
+++ src/Uno.UI.Toolkit/RuntimeHelper.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Uno.UI.Helpers;
-
-namespace Uno.UI.Toolkit;
+﻿namespace Uno.UI.Toolkit;
 
 public static class PlatformRuntimeHelper
 {
