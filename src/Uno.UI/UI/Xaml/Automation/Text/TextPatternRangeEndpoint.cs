85eea366740e8f5c32e3dffa31af93e9ba2086f8
Martin Zikmund <martin@zikmund.dev>
2025-09-24 16:17:47 +0200

    feat: Add auxiliary automation types

---
+++ src/Uno.UI/UI/Xaml/Automation/Text/TextPatternRangeEndpoint.cs
@@ -0,0 +1,18 @@
+namespace Microsoft.UI.Xaml.Automation.Text;
+
+/// <summary>
+/// Specifies the endpoints of a text range.
+/// </summary>
+public enum TextPatternRangeEndpoint
+{
+	/// <summary>
+	/// The starting endpoint of the text range.
+	/// </summary>
+	Start = 0,
+
+	/// <summary>
+	/// The ending endpoint of the text range.
+	/// </summary>
+	End = 1,
+}
+
