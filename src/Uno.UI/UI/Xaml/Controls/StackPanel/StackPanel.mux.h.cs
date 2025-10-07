cbece8be44fb8b4b6bcb61a5d88455cebe68a390
Martin Zikmund <martin@zikmund.dev>
2025-10-07 16:59:38 +0200

    chore: File headers

--- src/Uno.UI/UI/Xaml/Controls/StackPanel/StackPanel.mux.h.cs
+++ src/Uno.UI/UI/Xaml/Controls/StackPanel/StackPanel.mux.h.cs
@@ -1,4 +1,7 @@
-﻿// MUX Reference StackPanel.h
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+// MUX Reference StackPanel.h, tag winui3/release/1.8.0, commit 61382c07
 
 namespace Microsoft.UI.Xaml.Controls;
 
