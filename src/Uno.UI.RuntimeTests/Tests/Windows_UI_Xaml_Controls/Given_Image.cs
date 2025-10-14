ffa8dda1d7ae52ef16ac593520af455fd66ec894
Ramez Ragaa <ramezragaa@proton.me>
2025-10-14 13:11:11 -0400

    test: add When_Path_Contains_Space

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_Image.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_Image.cs
@@ -243,6 +243,34 @@ namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
 			Assert.IsTrue(img.ActualHeight > 0);
 		}
 
+#if HAS_UNO
+		[TestMethod]
+		[RunsOnUIThread]
+		public async Task When_Path_Contains_Space()
+		{
+			var img = new Image { Source = "ms-appx:///Assets/image with space in path.png" };
+
+			var imageOpened = false;
+			var imageFailed = false;
+
+			img.ImageOpened += (sender, args) => imageOpened = true;
+			img.ImageFailed += (sender, args) => imageFailed = true;
+
+			await UITestHelper.Load(img);
+			await UITestHelper.WaitFor(() => imageOpened, 3000);
+			await UITestHelper.WaitForIdle();
+
+			Assert.IsTrue(img.ActualHeight > 0);
+			Assert.IsTrue(imageOpened);
+			Assert.IsFalse(imageFailed);
+			if (ApiInformation.IsTypePresent("Microsoft.UI.Xaml.Media.Imaging.RenderTargetBitmap, Uno.UI"))
+			{
+				var screenshot = await UITestHelper.ScreenShot(img);
+				ImageAssert.HasColorAt(screenshot, screenshot.Width / 2, screenshot.Height / 2, Microsoft.UI.Colors.Blue);
+			}
+		}
+#endif
+
 		[TestMethod]
 		[RunsOnUIThread]
 		public async Task When_Explicit_BitmapImage_Relative_NonRooted()
