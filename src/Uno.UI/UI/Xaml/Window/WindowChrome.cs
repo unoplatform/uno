e8bb48259dfefe10ee79a9c538e7b92cb3791758
Martin Zikmund <martin@zikmund.dev>
2025-10-16 15:23:10 +0200

    chore: Progress on Window chrome

--- src/Uno.UI/UI/Xaml/Window/WindowChrome.cs
+++ src/Uno.UI/UI/Xaml/Window/WindowChrome.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Uno.UI.Xaml.Core;
 
@@ -9,12 +10,32 @@ internal sealed partial class WindowChrome : ContentControl
 {
 	public WindowChrome(Microsoft.UI.Xaml.Window parent)
 	{
+		DefaultStyleKey = typeof(WindowChrome);
+
 		HorizontalAlignment = Microsoft.UI.Xaml.HorizontalAlignment.Stretch;
 		VerticalAlignment = Microsoft.UI.Xaml.VerticalAlignment.Stretch;
 		HorizontalContentAlignment = Microsoft.UI.Xaml.HorizontalAlignment.Stretch;
 		VerticalContentAlignment = Microsoft.UI.Xaml.VerticalAlignment.Stretch;
 
 		IsTabStop = false;
+		CaptionVisibility = Visibility.Visible;
+	}
+
+	public Visibility CaptionVisibility
+	{
+		get => (Visibility)GetValue(CaptionVisibilityProperty);
+		set => SetValue(CaptionVisibilityProperty, value);
+	}
+
+	public static DependencyProperty CaptionVisibilityProperty { get; } =
+		DependencyProperty.Register(nameof(CaptionVisibility), typeof(Visibility), typeof(WindowChrome), new FrameworkPropertyMetadata(Visibility.Collapsed));
+
+	// to apply min, max and close style definitions to custom titlebar,
+	// one needs to apply Content Control style with key WindowChromeStyle defined in generic.xaml
+	internal void ApplyStylingForMinMaxCloseButtons()
+	{
+		var style = (Style)Application.Current.Resources["WindowChromeStyle"];
+		SetValue(StyleProperty, style);
 	}
 
 	protected override void OnContentChanged(object oldContent, object newContent)
