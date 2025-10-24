fbafba2e3d990201fc3d5db5380ab134f0bc9b2b
Martin Zikmund <martin@zikmund.dev>
2025-10-24 18:12:41 +0200

    chore: Adjust `SetBorderAndTitleBar` behavior

---
+++ src/Uno.UI/UI/Input/Internal/INativeInputNonClientPointerSource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Input;
+using Windows.Graphics;
+
+namespace Uno.UI.UI.Input.Internal;
+
+internal interface INativeInputNonClientPointerSource
+{
+	void ClearAllRegionRects();
+
+	void ClearRegionRects(NonClientRegionKind region);
+
+	RectInt32[] GetRegionRects(NonClientRegionKind region);
+
+	void SetRegionRects(NonClientRegionKind region, RectInt32[] rects);
+}
