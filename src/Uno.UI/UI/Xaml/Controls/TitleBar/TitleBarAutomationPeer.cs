c7b0cc6163d1d822621e9df26ea3d0f7c303acdb
Martin Zikmund <martin@zikmund.dev>
2025-11-04 17:31:06 +0100

    chore: Add `TitleBar` properties

---
+++ src/Uno.UI/UI/Xaml/Controls/TitleBar/TitleBarAutomationPeer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.UI.Xaml.Automation.Peers;
+
+namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Exposes TitleBar types to Microsoft UI Automation.
+/// </summary>
+public partial class TitleBarAutomationPeer : FrameworkElementAutomationPeer
+{
+	/// <summary>
+	/// Initializes a new instance of the TitleBarAutomationPeer class.
+	/// </summary>
+	/// <param name="owner"></param>
+	public TitleBarAutomationPeer(TitleBar owner) : base(owner)
+	{
+	}
+
+	protected override AutomationControlType GetAutomationControlTypeCore() => AutomationControlType.TitleBar;
+
+	protected override string GetClassNameCore() => nameof(TitleBar);
+
+	protected override string GetNameCore()
+	{
+		var name = base.GetNameCore();
+
+		if (string.IsNullOrEmpty(name))
+		{
+			if (Owner is TitleBar titleBar)
+			{
+				name = titleBar.Title;
+			}
+		}
+
+		return name;
+	}
+}
