6d8086e0db11f909956d076a076b46ae54581892
Martin Zikmund <martin@zikmund.dev>
2025-11-06 13:19:24 +0100

    chore: Remove test methods
    The functionality is not testable in CI as it requires accepting persmission dialog

--- src/Uno.UI.RuntimeTests/Tests/Windows_ApplicationModel/DataTransfer/Given_Clipboard.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_ApplicationModel/DataTransfer/Given_Clipboard.cs
@@ -1,4 +1,4 @@
-#if __ANDROID__ || __IOS__ || __WASM__
+#if __ANDROID__ || __IOS__
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -65,64 +65,6 @@ namespace Uno.UI.RuntimeTests.Tests
 				var uriFromClipboard = await clipboardView.GetUriAsync();
 
 				Assert.AreEqual(uri, uriFromClipboard);
-			}
-			finally
-			{
-				Clipboard.Clear();
-			}
-		}
-#endif
-
-#if __ANDROID__ || __WASM__
-		[TestMethod]
-		[RunsOnUIThread]
-		public async Task When_Html_Content()
-		{
-			try
-			{
-				var html = "<p>This is <strong>bold</strong> text</p>";
-				var package = new DataPackage();
-				package.SetHtmlFormat(html);
-#if WINAPPSDK
-				Clipboard.SetContent(package);
-#else
-				await Clipboard.SetContentAsync(package);
-#endif
-
-				var clipboardView = Clipboard.GetContent();
-				var htmlFromClipboard = await clipboardView.GetHtmlFormatAsync();
-
-				Assert.AreEqual(html, htmlFromClipboard);
-			}
-			finally
-			{
-				Clipboard.Clear();
-			}
-		}
-
-		[TestMethod]
-		[RunsOnUIThread]
-		public async Task When_Html_And_Text_Content()
-		{
-			try
-			{
-				var html = "<p>This is <strong>bold</strong> text</p>";
-				var text = "This is bold text";
-				var package = new DataPackage();
-				package.SetHtmlFormat(html);
-				package.SetText(text);
-#if WINAPPSDK
-				Clipboard.SetContent(package);
-#else
-				await Clipboard.SetContentAsync(package);
-#endif
-
-				var clipboardView = Clipboard.GetContent();
-				var htmlFromClipboard = await clipboardView.GetHtmlFormatAsync();
-				var textFromClipboard = await clipboardView.GetTextAsync();
-
-				Assert.AreEqual(html, htmlFromClipboard);
-				Assert.AreEqual(text, textFromClipboard);
 			}
 			finally
 			{
