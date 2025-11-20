e55aab74f3e1bc93b5ea49c927f899755d6cf318
Jerome Laban <jerome@platform.uno>
2025-11-20 14:12:58 -0500

    chore: Move helpers to a separate file

--- src/Uno.UI.RemoteControl.VS/EntryPoint.cs
+++ src/Uno.UI.RemoteControl.VS/EntryPoint.cs
@@ -761,102 +761,6 @@ public partial class EntryPoint : IDisposable
 		}
 	}
 
-	private System.Text.Encoding DetectFileEncoding(string filePath)
-	{
-		if (!File.Exists(filePath))
-		{
-			return new UTF8Encoding(encoderShouldEmitUTF8Identifier: true);
-		}
-
-		Encoding encoding;
-		using (var reader = new StreamReader(filePath, detectEncodingFromByteOrderMarks: true))
-		{
-			reader.Peek(); // Need to read at least one char to detect encoding
-			encoding = reader.CurrentEncoding;
-		}
-
-		// Confirm BOM presence for UTF encodings
-		switch (encoding)
-		{
-			case UTF8Encoding:
-			{
-				using var file = File.OpenRead(filePath);
-				encoding = (file.Length < 3 ? (0x00, 0x00, 0x00) : (file.ReadByte(), file.ReadByte(), file.ReadByte())) switch
-				{
-					(0xEF, 0xBB, 0xBF) => new UTF8Encoding(encoderShouldEmitUTF8Identifier: true),
-					_ => new UTF8Encoding(encoderShouldEmitUTF8Identifier: false),
-				};
-				break;
-			}
-			case UnicodeEncoding: // UTF16
-			{
-				using var file = File.OpenRead(filePath);
-				encoding = (file.Length < 2 ? (0x00, 0x00) : (file.ReadByte(), file.ReadByte())) switch
-				{
-					(0xFE, 0xFF) => new UnicodeEncoding(bigEndian: true, byteOrderMark: true),
-					(0xFF, 0xFE) => new UnicodeEncoding(bigEndian: false, byteOrderMark: true),
-					_ => new UnicodeEncoding(bigEndian: encoding.CodePage is 1201, byteOrderMark: false),
-				};
-				break;
-			}
-			case UTF32Encoding:
-			{
-				using var file = File.OpenRead(filePath);
-				encoding = (file.Length < 2 ? (0x00, 0x00) : (file.ReadByte(), file.ReadByte())) switch
-				{
-					(0xFE, 0xFF) => new UTF32Encoding(bigEndian: true, byteOrderMark: true),
-					(0xFF, 0xFE) => new UTF32Encoding(bigEndian: false, byteOrderMark: true),
-					_ => new UTF32Encoding(bigEndian: encoding.CodePage is 12001, byteOrderMark: false),
-				};
-				break;
-			}
-		}
-
-		return encoding;
-	}
-
-	private bool SupportsUnicode(System.Text.Encoding encoding)
-	{
-		// Check if the encoding supports Unicode characters (including emojis)
-		// UTF-8, UTF-16, and UTF-32 all support the full Unicode range
-		var isSupported = encoding is UTF8Encoding
-			|| encoding is UnicodeEncoding
-			|| encoding is UTF32Encoding
-			|| encoding.CodePage == 65001  // UTF-8
-			|| encoding.CodePage == 1200   // UTF-16 LE
-			|| encoding.CodePage == 1201   // UTF-16 BE
-			|| encoding.CodePage == 12000  // UTF-32 LE
-			|| encoding.CodePage == 12001; // UTF-32 BE
-
-		// Even if supported, we validate if the BOM has been enabled
-		if (!isSupported || encoding.GetPreamble() == Array.Empty<byte>())
-		{
-			return false;
-		}
-
-		return true;
-	}
-
-	private bool ContainsSpecialCharacters(string content)
-	{
-		// Check if content contains characters outside the Basic Multilingual Plane (BMP)
-		// This includes emojis and other special Unicode characters
-		foreach (var c in content)
-		{
-			if (char.IsSurrogate(c))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private Encoding GetCompatibleEncoding(Encoding currentEncoding, string newContent)
-		// If content has special characters and encoding doesn't support Unicode, upgrade to UTF-8 with BOM
-		=> ContainsSpecialCharacters(newContent) && !SupportsUnicode(currentEncoding)
-			? new UTF8Encoding(encoderShouldEmitUTF8Identifier: true)
-			: currentEncoding;
-
 	private async Task OnUpdateFileRequestedAsync(UpdateFileIdeMessage request)
 	{
 		try
@@ -866,8 +770,8 @@ public partial class EntryPoint : IDisposable
 				var filePath = Path.GetFullPath(request.FileFullName);
 
 				// Determine the appropriate encoding for the file
-				var currentEncoding = DetectFileEncoding(filePath);
-				var targetEncoding = GetCompatibleEncoding(currentEncoding, fileContent);
+				var currentEncoding = EncodingHelpers.DetectFileEncoding(filePath);
+				var targetEncoding = EncodingHelpers.GetCompatibleEncoding(currentEncoding, fileContent);
 
 				// Check if document is already open in IDE
 				var document = _dte2
