10d6a7a1bc5b803297ced87fb451754c6b1f90d0
Ramez Ragaa <ramezragaa@proton.me>
2025-11-26 15:35:38 -0500

    Revert "chore: remove initial InvalidateRender call"
    This reverts commit 7cb9a530c3064744af4ebc864e5e31b42a51ae7f.

--- src/Uno.UI.Runtime.Skia.Linux.FrameBuffer/Hosting/FramebufferHost.cs
+++ src/Uno.UI.Runtime.Skia.Linux.FrameBuffer/Hosting/FramebufferHost.cs
@@ -132,6 +132,9 @@ namespace Uno.UI.Runtime.Skia.Linux.FrameBuffer
 			{
 				var app = _appBuilder();
 				app.Host = this;
+
+				// Force the first render once the app has been setup
+				Dispatch(() => _renderer.InvalidateRender(), NativeDispatcherPriority.High);
 			}
 
 			Windows.UI.Core.CoreDispatcher.DispatchOverride = Dispatch;
