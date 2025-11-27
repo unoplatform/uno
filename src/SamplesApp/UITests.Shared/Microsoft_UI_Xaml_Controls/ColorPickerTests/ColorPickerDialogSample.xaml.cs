9f6e5757dd1499e7c8f2e20158257f958c438a9a
Martin Zikmund <martin@zikmund.dev>
2025-11-27 16:52:43 +0100

    test: Dialog prevents scroll

---
+++ src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/ColorPickerTests/ColorPickerDialogSample.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Uno.UI.Samples.Controls;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
+
+namespace UITests.Microsoft_UI_Xaml_Controls.ColorPickerTests
+{
+	[Sample("ColorPicker", "MUX", Name = "ColorPickerDialogSample")]
+	public sealed partial class ColorPickerDialogSample : UserControl
+	{
+		public ColorPickerDialogSample()
+		{
+			this.InitializeComponent();
+		}
+
+		private async void ShowDialogClick(object sender, RoutedEventArgs args)
+		{
+			ColorPickerDialog colorPickerDialog = new ColorPickerDialog();
+			colorPickerDialog.XamlRoot = this.XamlRoot;
+			await colorPickerDialog.ShowAsync();
+		}
+	}
+}
