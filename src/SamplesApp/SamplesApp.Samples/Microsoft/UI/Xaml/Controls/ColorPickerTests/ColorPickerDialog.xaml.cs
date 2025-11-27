375ff9ef8b3f939b1c5ac280b6f5e9791a23ebc8
Martin Zikmund <martin@zikmund.dev>
2025-11-27 16:52:43 +0100

    test: Dialog prevents scroll

---
+++ src/SamplesApp/SamplesApp.Samples/Microsoft/UI/Xaml/Controls/ColorPickerTests/ColorPickerDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Windows.UI;
+
+namespace UITests.Microsoft_UI_Xaml_Controls.ColorPickerTests
+{
+	public sealed partial class ColorPickerDialog : ContentDialog
+	{
+		public ColorPickerDialog()
+		{
+			this.InitializeComponent();
+		}
+
+		public ColorPicker ColorPicker => Picker;
+
+		public Color SelectedColor
+		{
+			get => Picker.Color;
+			set => Picker.Color = value;
+		}
+
+		private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+		{
+		}
+
+		private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+		{
+		}
+	}
+}
