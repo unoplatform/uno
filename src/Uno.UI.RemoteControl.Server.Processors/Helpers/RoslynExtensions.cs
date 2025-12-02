1279f65cae25af1f4549df57dc4bfea3143f91da
David <github.site@torick.net>
2025-12-02 14:18:40 -0500

    feat(hr): Make support of HRinfo check more reliable

---
+++ src/Uno.UI.RemoteControl.Server.Processors/Helpers/RoslynExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace Uno.UI.RemoteControl.Helpers;
+
+public static class RoslynExtensions
+{
+	public static ProjectInfo GetInfo(this Project project)
+		=> ProjectInfo.Create(
+			ProjectId.CreateNewId(),
+			project.Version,
+			project.Name,
+			project.AssemblyName,
+			project.Language,
+			project.FilePath,
+			project.OutputFilePath,
+			project.CompilationOptions,
+			project.ParseOptions,
+			[..project.Documents.Select(GetInfo)],
+			project.ProjectReferences,
+			project.MetadataReferences,
+			project.AnalyzerReferences,
+			[..project.AdditionalDocuments.Select(GetInfo)],
+			project.IsSubmission,
+			default);
+
+	public static DocumentInfo GetInfo(this Document document)
+		=> DocumentInfo.Create(
+			document.Id,
+			document.Name,
+			document.Folders,
+			document.SourceCodeKind,
+			document.FilePath is null ? null : new FileTextLoader(document.FilePath!, Encoding.UTF8),
+			document.FilePath);
+
+	public static DocumentInfo GetInfo(this TextDocument document)
+		=> DocumentInfo.Create(
+			document.Id,
+			document.Name,
+			document.Folders,
+			default,
+			document.FilePath is null ? null : new FileTextLoader(document.FilePath!, Encoding.UTF8),
+			document.FilePath);
+}
