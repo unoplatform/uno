fd2c13ecd013eb7348dfd017b3f8dfbe5e233cfa
Jerome Laban <jerome@platform.uno>
2025-12-04 11:36:08 -0500

    chore: Adjust wasm native

--- src/Uno.UI/UI/Xaml/Application.wasm.cs
+++ src/Uno.UI/UI/Xaml/Application.wasm.cs
@@ -82,29 +82,35 @@ namespace Microsoft.UI.Xaml
 			return 0;
 		}
 
-		static async partial Application StartPartial(Func<ApplicationInitializationCallbackParams, Application> callback)
+		private static partial Application? StartPartial(Func<ApplicationInitializationCallbackParams, Application> callback)
 		{
-			try
+			async void Start()
 			{
-				_startInvoked = true;
-
-				SynchronizationContext.SetSynchronizationContext(NativeDispatcher.Main.SynchronizationContext);
-
-				await WindowManagerInterop.InitAsync();
-
-				_ = global::Windows.Storage.ApplicationData.Current.EnablePersistenceAsync();
-
-				return callback(new ApplicationInitializationCallbackParams());
-			}
-			catch (Exception exception)
-			{
-				if (typeof(Application).Log().IsEnabled(LogLevel.Error))
+				try
 				{
-					typeof(Application).Log().LogError("Application initialization failed.", exception);
-				}
+					_startInvoked = true;
 
-				return null;
+					SynchronizationContext.SetSynchronizationContext(NativeDispatcher.Main.SynchronizationContext);
+
+					await WindowManagerInterop.InitAsync();
+
+					_ = global::Windows.Storage.ApplicationData.Current.EnablePersistenceAsync();
+
+					callback(new ApplicationInitializationCallbackParams());
+				}
+				catch (Exception exception)
+				{
+					if (typeof(Application).Log().IsEnabled(LogLevel.Error))
+					{
+						typeof(Application).Log().LogError("Application initialization failed.", exception);
+					}
+				}
 			}
+
+			Start();
+
+			// With native wasm, we do not need to use the Start return value.
+			return null;
 		}
 
 		private void Initialize()
