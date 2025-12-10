0a7091dd59d23621f0aaa54ac9e45142ddfe5db7
Martin Zikmund <martin@zikmund.dev>
2025-12-10 14:15:08 +0100

    chore: Log test progress in debug output

--- src/SamplesApp/SamplesApp.UnitTests.Shared/Controls/UnitTest/UnitTestsControl.cs
+++ src/SamplesApp/SamplesApp.UnitTests.Shared/Controls/UnitTest/UnitTestsControl.cs
@@ -311,8 +311,9 @@ namespace Uno.UI.Samples.Tests
 
 		private async Task ReportMessage(string message, bool isRunning = true)
 		{
-#if HAS_UNO
 			_log?.Info(message);
+#if DEBUG
+			global::System.Diagnostics.Debug.WriteLine(message);
 #endif
 
 			void Setter()
