bbc6eb3ef810b63b77c8873b2bda9961bb40fcb6
Martin Zikmund <martin@zikmund.dev>
2025-12-12 14:29:19 +0100

    feat: Track Inconclusive runtime tests

--- src/SamplesApp/SamplesApp.UnitTests.Shared/Controls/UnitTest/TestRun.cs
+++ src/SamplesApp/SamplesApp.UnitTests.Shared/Controls/UnitTest/TestRun.cs
@@ -1,15 +1,29 @@
-﻿namespace Uno.UI.Samples.Tests
+﻿using System;
+
+namespace Uno.UI.Samples.Tests
 {
 	public sealed partial class UnitTestsControl
 	{
 		private class TestRun
 		{
+			public TestRun()
+			{
+				StartTime = DateTimeOffset.UtcNow;
+			}
+
 			public int Run { get; set; }
+
 			public int Ignored { get; set; }
+
+			public int Inconclusive { get; set; }
+
 			public int Succeeded { get; set; }
+
 			public int Failed { get; set; }
 
 			public int CurrentRepeatCount { get; set; }
+
+			public DateTimeOffset StartTime { get; }
 		}
 	}
 }
