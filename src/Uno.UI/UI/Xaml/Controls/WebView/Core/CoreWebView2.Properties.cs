a2acdb1199b44cbb5e56d02fa0b723af333868bb
Sonja Schweitzer <info@technische-konstruktion.com>
2025-12-12 14:25:09 +0100

    chore(CoreWebView2): Updates the xml documentation for the event handlers to improve clarity
    - Referencing code names with xml tags provides build in lookup capability
    - adds notes regarding the DocumentTitleChanged event arguments which are expected to always be null
    - adds Remarks to the target usage of the Event Handler Events

--- src/Uno.UI/UI/Xaml/Controls/WebView/Core/CoreWebView2.Properties.cs
+++ src/Uno.UI/UI/Xaml/Controls/WebView/Core/CoreWebView2.Properties.cs
@@ -39,41 +39,48 @@ public partial class CoreWebView2
 	}
 
 	/// <summary>
-	/// NavigationStarting is raised when the WebView main frame is requesting permission to navigate to a different URI.
+	/// <see cref="CoreWebView2.NavigationStarting"/> is raised when the WebView main frame is requesting permission to navigate to a different URI.
 	/// </summary>
 	public event TypedEventHandler<CoreWebView2, CoreWebView2NavigationStartingEventArgs> NavigationStarting;
 
 	/// <summary>
-	/// NavigationCompleted is raised when the WebView has completely loaded (body.onload has been raised) or loading stopped with error.
+	/// <see cref="CoreWebView2.NavigationCompleted"/> is raised when the WebView has completely loaded (body.onload has been raised) or loading stopped with error.
 	/// </summary>
 	public event TypedEventHandler<CoreWebView2, CoreWebView2NavigationCompletedEventArgs> NavigationCompleted;
 
 	/// <summary>
-	/// NewWindowRequested is raised when content inside the WebView requests to open a new window, such as through window.open().
+	/// <see cref="CoreWebView2.NewWindowRequested"/> is raised when content inside the WebView requests to open a new window, such as through window.open().
 	/// </summary>
 	public event TypedEventHandler<CoreWebView2, CoreWebView2NewWindowRequestedEventArgs> NewWindowRequested;
 
 	/// <summary>
-	/// DocumentTitleChanged is raised when the CoreWebView2.DocumentTitle property changes and may be raised
-	/// before or after the CoreWebView2.NavigationCompleted event.
+	/// <see cref="CoreWebView2.DocumentTitleChanged"/> event is raised when the <see cref="CoreWebView2.DocumentTitle"/> property changes and may be raised
+	/// before or after the <see cref="CoreWebView2.NavigationCompleted"/> event.
 	/// </summary>
+	/// <remarks>
+	/// The event data will always be <see langword="null"/>.<br/>
+	/// The targeted Event Handling is meant to use the <see cref="CoreWebView2.DocumentTitle"/> property to get the current title directly from the sender instance.
+	/// </remarks>
 	public event TypedEventHandler<CoreWebView2, object> DocumentTitleChanged;
 
 	/// <summary>
-	/// HistoryChanged is raised for changes to joint session history, which consists of top-level and manual frame navigation.
+	/// <see cref="CoreWebView2.HistoryChanged"/> is raised for changes to joint session history, which consists of top-level and manual frame navigation.
 	/// </summary>
 	public event TypedEventHandler<CoreWebView2, object> HistoryChanged;
 
 	/// <summary>
-	/// SourceChanged is raised when the CoreWebView2.Source property changes. SourceChanged is raised when
-	/// navigating to a different site or fragment navigation.
+	/// <see cref="CoreWebView2.SourceChanged"/> is raised when the <see cref="CoreWebView2.Source"/> property changes<br/>
+	/// and when navigating to a different site or fragment navigation.
 	/// </summary>
 	public event TypedEventHandler<CoreWebView2, CoreWebView2SourceChangedEventArgs> SourceChanged;
 
 	/// <summary>
-	/// Dispatches after web content sends a message to the app host.
+	/// <see cref="CoreWebView2.WebMessageReceived"/> dispatches after web content sends a message to the app host.
 	/// </summary>
 	public event TypedEventHandler<CoreWebView2, CoreWebView2WebMessageReceivedEventArgs> WebMessageReceived;
 
+	/// <summary>
+	/// <see cref="CoreWebView2.UnsupportedUriSchemeIdentified"/> is raised when the WebView encounters a URI with an unsupported scheme.
+	/// </summary>
 	internal event TypedEventHandler<CoreWebView2, WebViewUnsupportedUriSchemeIdentifiedEventArgs> UnsupportedUriSchemeIdentified;
 }
