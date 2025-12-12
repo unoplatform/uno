a7aa97b8d80dd2b667f8aa670344e7f1f3103cce
DevTKSS <info@technische-konstruktion.com>
2025-12-12 09:53:21 +0100

    chore: Add xml docs and apply codestyling

--- src/Uno.UI/Extensions/WebView2Extensions.cs
+++ src/Uno.UI/Extensions/WebView2Extensions.cs
@@ -12,7 +12,9 @@ namespace Uno.UI.Extensions;
 public static class WebView2Extensions
 {
 	#region DependencyProperty: IsNavigating
-
+	/// <summary>  
+	/// Identifies the IsNavigating attached dependency property.  
+	/// </summary>
 	public static DependencyProperty IsNavigatingProperty { [DynamicDependency(nameof(GetIsNavigating))] get; } = DependencyProperty.RegisterAttached(
 		"IsNavigating",
 		typeof(bool),
@@ -26,18 +28,22 @@ public static class WebView2Extensions
 	/// <returns><see langword="true"/> if the WebView2 is currently navigating; otherwise, <see langword="false"/>.</returns>
 	[DynamicDependency(nameof(SetIsNavigating))]
 	public static bool GetIsNavigating(DependencyObject obj) => (bool)obj.GetValue(IsNavigatingProperty);
-	[DynamicDependency(nameof(GetIsNavigating))]
+	
 	/// <summary>
 	/// Sets the value of the IsNavigating attached property for the specified <see cref="DependencyObject"/>.
 	/// When set, enables automatic tracking of navigation state on the WebView2 control.
 	/// </summary>
 	/// <param name="obj">The object on which to set the property value.</param>
 	/// <param name="value"><see langword="true"/> to enable navigation tracking; otherwise, <see langword="false"/>.</param>
+	[DynamicDependency(nameof(GetIsNavigating))]
 	public static void SetIsNavigating(DependencyObject obj, bool value) => obj.SetValue(IsNavigatingProperty, value);
 
 	private static void OnIsNavigatingChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
 	{
-		if (sender is not WebView2 control) throw new InvalidOperationException("The attached property 'IsNavigating' can only be applied to a WebView2 control.");
+		if (sender is not WebView2 control)
+		{
+			throw new InvalidOperationException("The attached property 'IsNavigating' can only be applied to a WebView2 control.");
+		}
 
 		// Remove previous event handlers to prevent duplicate subscriptions
 		control.NavigationStarting -= OnNavigationStarting;
@@ -61,32 +67,38 @@ public static class WebView2Extensions
 	#endregion
 
 	#region DependencyProperty: DocumentTitle
-
+	/// <summary>
+	/// Identifies the DocumentTitle attached dependency property.
+	/// </summary>
 	public static DependencyProperty DocumentTitleProperty { [DynamicDependency(nameof(GetDocumentTitle))] get; } = DependencyProperty.RegisterAttached(
 		"DocumentTitle",
 		typeof(string),
 		typeof(WebView2Extensions),
 		new PropertyMetadata(string.Empty, OnDocumentTitleChanged));
 
-	[DynamicDependency(nameof(SetDocumentTitle))]
 	/// <summary>
 	/// Gets the value of the DocumentTitle attached property for the specified <see cref="DependencyObject"/>.
 	/// </summary>
 	/// <param name="obj">The object from which to get the property value.</param>
 	/// <returns>The document title of the WebView2 control.</returns>
+	[DynamicDependency(nameof(SetDocumentTitle))]
 	public static string GetDocumentTitle(DependencyObject obj) => (string)(obj.GetValue(DocumentTitleProperty) ?? string.Empty);
-	[DynamicDependency(nameof(GetDocumentTitle))]
+	
 	/// <summary>
 	/// Sets the value of the DocumentTitle attached property for the specified <see cref="DependencyObject"/>.
 	/// When set, enables automatic tracking of document title changes on the WebView2 control.
 	/// </summary>
 	/// <param name="obj">The object on which to set the property value.</param>
 	/// <param name="value">The initial document title value.</param>
+	[DynamicDependency(nameof(GetDocumentTitle))]
 	public static void SetDocumentTitle(DependencyObject obj, string value) => obj.SetValue(DocumentTitleProperty, value);
 
 	private static void OnDocumentTitleChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
 	{
-		if (sender is not WebView2 control) throw new InvalidOperationException("The attached property 'DocumentTitle' can only be applied to a WebView2 control.");
+		if (sender is not WebView2 control)
+		{
+			throw new InvalidOperationException("The attached property 'DocumentTitle' can only be applied to a WebView2 control.");
+		}
 
 		if (control.CoreWebView2 is null)
 		{
