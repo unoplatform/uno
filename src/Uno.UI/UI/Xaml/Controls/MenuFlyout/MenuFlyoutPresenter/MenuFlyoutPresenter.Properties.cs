2b24f37b29b6e9f155167ad4962a68655238710a
Martin Zikmund <martin@zikmund.dev>
2025-12-16 13:22:46 +0100

    chore: Update commit references

--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutPresenter/MenuFlyoutPresenter.Properties.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutPresenter/MenuFlyoutPresenter.Properties.cs
@@ -4,5 +4,9 @@ namespace Microsoft.UI.Xaml.Controls;
 
 partial class MenuFlyoutPresenter
 {
+	/// <summary>
+	/// Gets an object that provides calculated values that can be referenced
+	/// as TemplateBinding sourceswhen defining templates for a MenuFlyoutPresenter control.
+	/// </summary>
 	public MenuFlyoutPresenterTemplateSettings TemplateSettings { get; private set; }
 }
