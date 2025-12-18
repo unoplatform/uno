becc4fabfbb3d2e964700fd02eae5fb090fb91bd
Martin Zikmund <martin@zikmund.dev>
2025-12-18 17:05:04 +0100

    chore: Adjustments for build

--- src/Uno.UI/UI/Xaml/Controls/Flyout/FlyoutBase.cs
+++ src/Uno.UI/UI/Xaml/Controls/Flyout/FlyoutBase.cs
@@ -398,6 +398,7 @@ namespace Microsoft.UI.Xaml.Controls.Primitives
 				UIElement rootElement = XamlRoot?.Content;
 				visualRoot = rootElement as FrameworkElement;
 			}
+			EnsureAssociatedXamlRoot(placementTarget);
 			ShowAtCore(visualRoot, showOptions);
 		}
 
