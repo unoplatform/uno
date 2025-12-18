451f40a9deaa705c4be29fe388a53c9cafd29072
Martin Zikmund <martin@zikmund.dev>
2025-12-18 11:53:36 +0100

    fix: Correct default value for `MenuFlyout` shadow

--- src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutPresenter/MenuFlyoutPresenter.Properties.cs
+++ src/Uno.UI/UI/Xaml/Controls/MenuFlyout/MenuFlyoutPresenter/MenuFlyoutPresenter.Properties.cs
@@ -5,6 +5,25 @@ namespace Microsoft.UI.Xaml.Controls;
 partial class MenuFlyoutPresenter
 {
 	/// <summary>
+	/// Gets or sets a value that indicates whether the default shadow effect is shown.
+	/// </summary>
+	public bool IsDefaultShadowEnabled
+	{
+		get => (bool)this.GetValue(IsDefaultShadowEnabledProperty);
+		set => this.SetValue(IsDefaultShadowEnabledProperty, value);
+	}
+
+	/// <summary>
+	/// Identifies the IsDefaultShadowEnabled dependency property.
+	/// </summary>
+	public static DependencyProperty IsDefaultShadowEnabledProperty { get; } =
+		DependencyProperty.Register(
+			nameof(IsDefaultShadowEnabled),
+			typeof(bool),
+			typeof(MenuFlyoutPresenter),
+			new FrameworkPropertyMetadata(defaultValue: true));
+
+	/// <summary>
 	/// Gets an object that provides calculated values that can be referenced
 	/// as TemplateBinding sourceswhen defining templates for a MenuFlyoutPresenter control.
 	/// </summary>
