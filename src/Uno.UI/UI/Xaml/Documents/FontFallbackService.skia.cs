0b07a58a065dec3a23bbed73645d171fcc90dc2e
Ramez Ragaa <ramezragaa@proton.me>
2026-01-05 06:53:04 -0500

    chore: add license

--- src/Uno.UI/UI/Xaml/Documents/FontFallbackService.skia.cs
+++ src/Uno.UI/UI/Xaml/Documents/FontFallbackService.skia.cs
@@ -1,3 +1,34 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without modification,
+// 	are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright
+// 	notice, this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following
+// disclaimer in the documentation and/or other materials provided
+// 	with the distribution.
+// * Neither the name of Google Inc. nor the names of its
+// 	contributors may be used to endorse or promote products derived
+// 	from this software without specific prior written permission.
+//
+// 	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// 	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// 	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// 	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// 	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// 	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// Loosely based on Flutter's implementation:
+// https://github.com/flutter/engine/blob/ae5c3603d013477d37ae301993fc0967d4ad7ed2/lib/web_ui/lib/src/engine/canvaskit/fonts.dart
+// https://github.com/flutter/engine/blob/ae5c3603d013477d37ae301993fc0967d4ad7ed2/lib/web_ui/dev/roll_fallback_fonts.dart
+// https://github.com/flutter/engine/blob/main/lib/web_ui/lib/src/engine/font_fallbacks.dart
+
 #nullable enable
 using System;
 using System.Collections.Generic;
