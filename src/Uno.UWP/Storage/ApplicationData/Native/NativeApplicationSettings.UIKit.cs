c868280c680d67419af5220b36fe643fdc507ced
Martin Zikmund <martin@zikmund.dev>
2026-01-07 17:31:06 +0100

    refactor: Align Android and iOS NativeApplicationSettings with new abstraction pattern
    - Refactored Android implementation to use partial method pattern
    - Refactored iOS (UIKit) implementation to use partial method pattern
    - Added missing Count property to WASM implementation
    - All platforms now consistently implement the NativeApplicationSettings interface

--- src/Uno.UWP/Storage/ApplicationData/Native/NativeApplicationSettings.UIKit.cs
+++ src/Uno.UWP/Storage/ApplicationData/Native/NativeApplicationSettings.UIKit.cs
@@ -1,105 +1,46 @@
-﻿using System;
+#nullable enable
+
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Foundation;
 
 namespace Uno.Storage;
 
 partial class NativeApplicationSettings
 {
-	private static partial bool SupportsLocality() => false;
+	private static partial bool SupportsLocalityPlatform() => false;
 
-	public object this[string key]
-	{
-		get
-		{
-			var value = NSUserDefaults.StandardUserDefaults.ValueForKey((NSString)key)?.ToString();
+	partial void InitializePlatform() { }
 
-			return DataTypeSerializer.Deserialize(value);
-		}
-		set
-		{
-			if (value != null)
-			{
-				var nativeObject = NSObject.FromObject(DataTypeSerializer.Serialize(value));
-				NSUserDefaults.StandardUserDefaults.SetValueForKey(nativeObject, (NSString)key);
-			}
-			else
-			{
-				Remove(key);
-			}
-
-			MapChanged?.Invoke(this, new MapChangedEventArgs(CollectionChange.ItemChanged, key));
-		}
-	}
-
-	public ICollection<string> Keys
-		=> NSUserDefaults.StandardUserDefaults.ToDictionary().Keys.Select(k => k.ToString()).ToList();
-
-	public ICollection<object> Values
-		=> NSUserDefaults.StandardUserDefaults
-		.ToDictionary()
-		.Values
-		.Select(k => DataTypeSerializer.Deserialize(k?.ToString()))
-		.ToList();
-
-	public int Count
-		=> (int)NSUserDefaults.StandardUserDefaults.ToDictionary().Count;
-
-
-	public void Add(string key, object value)
-	{
-		if (ContainsKey(key))
-		{
-			throw new ArgumentException("An item with the same key has already been added.");
-		}
-		if (value != null)
-		{
-			var nativeObject = NSObject.FromObject(DataTypeSerializer.Serialize(value));
-			NSUserDefaults.StandardUserDefaults.SetValueForKey(nativeObject, (NSString)key);
-		}
-	}
-
-	public void Add(KeyValuePair<string, object> item)
-		=> Add(item.Key, item.Value);
-
-	public void Clear()
-	{
-		foreach (var pair in NSUserDefaults.StandardUserDefaults.ToDictionary())
-		{
-			Remove(pair.Key.ToString());
-		}
-	}
-
-	public bool ContainsKey(string key)
+	private partial bool ContainsSettingPlatform(string key)
 		=> NSUserDefaults.StandardUserDefaults.ToDictionary().ContainsKey((NSString)key);
 
-	public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
-	{
-		return NSUserDefaults.StandardUserDefaults
-			.ToDictionary()
-			.Select(k => new KeyValuePair<string, object>(k.Key.ToString(), DataTypeSerializer.Deserialize(k.Key.ToString())))
-			.GetEnumerator();
-	}
-
-	public bool Remove(string key)
+	private partial bool RemoveSettingPlatform(string key)
 	{
 		NSUserDefaults.StandardUserDefaults.RemoveObject((NSString)key);
 		NSUserDefaults.StandardUserDefaults.Synchronize();
-
 		return true;
 	}
 
-	public bool TryGetValue(string key, out object value)
+	private partial IEnumerable<string> GetKeysPlatform()
+		=> NSUserDefaults.StandardUserDefaults.ToDictionary().Keys.Select(k => k.ToString()).ToArray();
+
+	private partial bool TryGetSettingPlatform(string key, out string? value)
 	{
-		if (NSUserDefaults.StandardUserDefaults.ToDictionary().TryGetValue((NSString)key, out var nsvalue))
+		var nsValue = NSUserDefaults.StandardUserDefaults.ValueForKey((NSString)key);
+		if (nsValue is not null)
 		{
-			value = DataTypeSerializer.Deserialize(nsvalue?.ToString());
+			value = nsValue.ToString();
 			return true;
 		}
-
 		value = null;
 		return false;
+	}
+
+	private partial void SetSettingPlatform(string key, string value)
+	{
+		var nativeObject = NSObject.FromObject(value);
+		NSUserDefaults.StandardUserDefaults.SetValueForKey(nativeObject, (NSString)key);
+		NSUserDefaults.StandardUserDefaults.Synchronize();
 	}
 }
