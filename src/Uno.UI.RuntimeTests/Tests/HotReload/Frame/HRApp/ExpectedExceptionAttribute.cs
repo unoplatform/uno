206d32f50f79af4196960a329ec24b53e23270ad
Youssef Victor <youssefvictor00@gmail.com>
2026-01-07 11:54:05 +0100

    chore: Fix build error

--- src/Uno.UI.RuntimeTests/Tests/HotReload/Frame/HRApp/ExpectedExceptionAttribute.cs
+++ src/Uno.UI.RuntimeTests/Tests/HotReload/Frame/HRApp/ExpectedExceptionAttribute.cs
@@ -1,10 +1,11 @@
+using System;
+
 namespace Microsoft.VisualStudio.TestTools.UnitTesting;
 
 // Workaround for https://github.com/unoplatform/uno.ui.runtimetests.engine/issues/182
 // Do not use this attribute.
-// Intentionally not inheriting from Attribute to prevent accidental use of the class.
 // This only provides the minimal API so that runtimetests.engine can work.
-public sealed class ExpectedExceptionAttribute
+public sealed class ExpectedExceptionAttribute : Attribute
 {
     public Type? ExceptionType => null;
 }
