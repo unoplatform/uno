dad3a7b318aaa9f4ed320633daa08e8554e4a296
Ramez Ragaa <ramezragaa@proton.me>
2026-01-07 09:42:21 -0500

    chore: refactor into split classes for software and webgl rendering

--- src/Uno.UI.Runtime.Skia.WebAssembly.Browser/Rendering/BrowserRenderer.cs
+++ src/Uno.UI.Runtime.Skia.WebAssembly.Browser/Rendering/BrowserRenderer.cs
@@ -5,28 +5,18 @@ using Microsoft.UI.Xaml.Media;
 using SkiaSharp;
 using Uno.Foundation.Logging;
 using Uno.UI.Hosting;
+using Uno.UI.Runtime.Skia.WebAssembly.Browser;
 
 namespace Uno.UI.Runtime.Skia;
 
 internal partial class BrowserRenderer
 {
-	private readonly IXamlRootHost _host;
-	private int _renderCount;
-
-	private const int ResourceCacheBytes = 256 * 1024 * 1024; // 256 MB
-	private const SKColorType colorType = SKColorType.Rgba8888;
-	private const GRSurfaceOrigin surfaceOrigin = GRSurfaceOrigin.BottomLeft;
-
-	private readonly JSObject _nativeSwapChainPanel;
 	private readonly Stopwatch _renderStopwatch = new Stopwatch();
+	private readonly IXamlRootHost _host;
+	private readonly IBrowserRenderer _renderer;
+	private readonly JSObject _nativeInstance;
 
-	private GRGlInterface? _glInterface;
-	private GRContext? _context;
-	private JsInfo? _jsInfo;
-	private GRGlFramebufferInfo _glInfo;
-	private GRBackendRenderTarget? _renderTarget;
-	private SKBitmap? _bitmap;
-	private SKSurface? _surface;
+	private int _renderCount;
 	private SKCanvas? _canvas;
 
 	public BrowserRenderer(IXamlRootHost host)
@@ -38,13 +28,23 @@ internal partial class BrowserRenderer
 
 		_host = host;
 
-		_nativeSwapChainPanel = NativeMethods.CreateInstance(this);
+		if (WebGlBrowserRenderer.TryCreate(out var webGlBrowserRenderer))
+		{
+			_renderer = webGlBrowserRenderer;
+		}
+		else if (SoftwareBrowserRenderer.TryCreate(out var softwareBrowserRenderer))
+		{
+			_renderer = softwareBrowserRenderer;
+		}
+		else
+		{
+			throw new InvalidOperationException("Unable to create renderer");
+		}
+
+		_nativeInstance = NativeMethods.CreateInstance(this, WebAssemblyWindowWrapper.Instance.CanvasId);
 	}
 
-	internal void InvalidateRender()
-	{
-		NativeMethods.Invalidate(_nativeSwapChainPanel);
-	}
+	internal void InvalidateRender() => NativeMethods.Invalidate(_nativeInstance);
 
 	[JSExport]
 	internal static void RenderFrame([JSMarshalAs<JSType.Any>] object instance)
@@ -57,11 +57,6 @@ internal partial class BrowserRenderer
 
 	private void RenderFrame()
 	{
-		if (_jsInfo is null)
-		{
-			_jsInfo = NativeMethods.CreateContext(this, _nativeSwapChainPanel, WebAssemblyWindowWrapper.Instance?.CanvasId ?? "invalid");
-		}
-
 		_renderStopwatch.Restart();
 
 		if (this.Log().IsEnabled(LogLevel.Trace))
@@ -69,59 +64,17 @@ internal partial class BrowserRenderer
 			this.Log().Trace($"Render {_renderCount++}");
 		}
 
-		// create the SkiaSharp context
-		if (!_jsInfo.Value.UseSoftware && _context == null)
+		_renderer.MakeCurrent();
+
+		var currentClipPath = ((CompositionTarget)_host.RootElement!.Visual.CompositionTarget!).OnNativePlatformFrameRequested(_canvas, size =>
 		{
-			_glInterface = GRGlInterface.Create();
-			_context = GRContext.CreateGl(_glInterface);
-
-			// bump the default resource cache limit
-			_context.SetResourceCacheLimit(ResourceCacheBytes);
-		}
-
-		var currentClipPath = ((CompositionTarget)_host.RootElement!.Visual.CompositionTarget!).OnNativePlatformFrameRequested(_surface?.Canvas, size =>
-		{
-			if (_jsInfo.Value.UseSoftware)
-			{
-				_bitmap?.Dispose();
-				_canvas?.Dispose();
-				var pixels = NativeMethods.ResizePixelBuffer(_nativeSwapChainPanel, (int)size.Width, (int)size.Height);
-				_bitmap = new SKBitmap();
-				_bitmap.InstallPixels(new SKImageInfo((int)size.Width, (int)size.Height, colorType, SKAlphaType.Premul), pixels);
-				_canvas = new SKCanvas(_bitmap);
-			}
-			else
-			{
-				_glInfo = new GRGlFramebufferInfo(_jsInfo.Value.FboId, colorType.ToGlSizedFormat());
-
-				// destroy the old surface
-				_surface?.Dispose();
-				_surface = null;
-				_canvas = null;
-
-				// re-create the render target
-				_renderTarget?.Dispose();
-				_renderTarget = new GRBackendRenderTarget((int)size.Width, (int)size.Height, _jsInfo.Value.Samples, _jsInfo.Value.Stencil, _glInfo);
-
-				// create the surface
-				_surface = SKSurface.Create(_context, _renderTarget, surfaceOrigin, colorType);
-				_canvas = _surface.Canvas;
-			}
-
-			return _canvas;
+			return _canvas = _renderer.Resize((int)size.Width, (int)size.Height);
 		});
 
-		if (_jsInfo.Value.UseSoftware)
+		if (_canvas is not null)
 		{
-			if (_canvas is not null)
-			{
-				_canvas.Flush();
-				NativeMethods.BlitSoftware(_nativeSwapChainPanel, _bitmap!.Width, _bitmap.Height);
-			}
-		}
-		else
-		{
-			_context!.Flush();
+			_canvas?.Flush();
+			_renderer.Flush();
 		}
 
 		var (path, fillType) = !currentClipPath.IsEmpty ? (currentClipPath.ToSvgPathData(), currentClipPath.FillType is SKPathFillType.EvenOdd ? "evenodd" : "nonzero") : ("", "nonzero");
@@ -133,49 +86,13 @@ internal partial class BrowserRenderer
 		}
 	}
 
-	internal record struct JsInfo(bool UseSoftware, uint FboId, int Stencil, int Samples, int Depth);
 
 	internal static partial class NativeMethods
 	{
-		public static JSObject CreateInstance(BrowserRenderer owner)
-		{
-			return CreateInstanceInternal(owner);
-		}
-
 		[JSImport($"globalThis.Uno.UI.Runtime.Skia.{nameof(BrowserRenderer)}.createInstance")]
-		internal static partial JSObject CreateInstanceInternal([JSMarshalAs<JSType.Any>] object owner);
-
-		internal static JsInfo CreateContext(BrowserRenderer owner, JSObject nativeSwapChainPanel, string canvasId)
-		{
-			var jsObject = CreateContextStatic(nativeSwapChainPanel, canvasId);
-
-			if (jsObject.GetPropertyAsBoolean("success"))
-			{
-				return new JsInfo(
-					UseSoftware: false,
-					FboId: (uint)jsObject.GetPropertyAsInt32("fboId"),
-					Stencil: jsObject.GetPropertyAsInt32("stencil"),
-					Samples: jsObject.GetPropertyAsInt32("samples"),
-					Depth: jsObject.GetPropertyAsInt32("depth")
-				);
-			}
-			else
-			{
-				typeof(BrowserRenderer).LogError()?.Error($"Failed to create WebGL context: {jsObject.GetPropertyAsString("error")}");
-				return new JsInfo() { UseSoftware = true };
-			}
-		}
-
-		[JSImport($"globalThis.Uno.UI.Runtime.Skia.{nameof(BrowserRenderer)}.createContextStatic")]
-		internal static partial JSObject CreateContextStatic(JSObject nativeSwapChainPanel, string canvasId);
+		internal static partial JSObject CreateInstance([JSMarshalAs<JSType.Any>] object owner, string canvadId);
 
 		[JSImport($"globalThis.Uno.UI.Runtime.Skia.{nameof(BrowserRenderer)}.invalidate")]
 		internal static partial void Invalidate(JSObject nativeSwapChainPanel);
-
-		[JSImport($"globalThis.Uno.UI.Runtime.Skia.{nameof(BrowserRenderer)}.resizePixelBuffer")]
-		internal static partial IntPtr ResizePixelBuffer(JSObject nativeSwapChainPanel, int width, int height);
-
-		[JSImport($"globalThis.Uno.UI.Runtime.Skia.{nameof(BrowserRenderer)}.blitSoftware")]
-		internal static partial void BlitSoftware(JSObject nativeSwapChainPanel, int width, int height);
 	}
 }
--- src/Uno.UI.Runtime.Skia.WebAssembly.Browser/ts/Runtime/BrowserRenderer.ts
+++ src/Uno.UI.Runtime.Skia.WebAssembly.Browser/ts/Runtime/BrowserRenderer.ts
@@ -1,34 +1,32 @@
 namespace Uno.UI.Runtime.Skia {
 	export class BrowserRenderer {
-		managedHandle: number;
-		canvas: HTMLCanvasElement;
-		requestRender: () => void;
-		static anyGL: any;
-		glCtx: any;
-		ctx2D: CanvasRenderingContext2D;
-		pixelBuffer: number;
-		clampedArray: Uint8ClampedArray;
+		private readonly managedHandle: number;
+		private readonly canvas: HTMLCanvasElement;
+		private requestRender: () => void;
 
-		constructor(managedHandle: number) {
+		constructor(managedHandle: number, canvas: HTMLCanvasElement) {
+			this.canvas = canvas;
 			this.managedHandle = managedHandle;
-			this.canvas = undefined;
-			this.requestRender = undefined;
-			BrowserRenderer.anyGL = (<any>window).GL;
 			this.buildImports();
+
+			this.setCanvasSize();
+			window.addEventListener("resize", x => this.setCanvasSize());
 		}
 
 		async buildImports() {
-			let anyModule = <any>window.Module;
-
-			if (anyModule.getAssemblyExports !== undefined) {
-				const skiaSharpExports = await anyModule.getAssemblyExports("Uno.UI.Runtime.Skia.WebAssembly.Browser");
-
-				this.requestRender = () => skiaSharpExports.Uno.UI.Runtime.Skia.BrowserRenderer.RenderFrame(this.managedHandle);
-			}
+			const skiaSharpExports = await (<any>window).Module.getAssemblyExports("Uno.UI.Runtime.Skia.WebAssembly.Browser");
+			this.requestRender = () => skiaSharpExports.Uno.UI.Runtime.Skia.BrowserRenderer.RenderFrame(this.managedHandle);
 		}
 
-		public static createInstance(managedHandle: number) {
-			return new BrowserRenderer(managedHandle);
+		public static createInstance(managedHandle: number, canvasId: string) {
+			if (!canvasId)
+				throw 'No <canvas> element or ID was provided';
+
+			const canvas = <HTMLCanvasElement>document.getElementById(canvasId);
+			if (!canvas)
+				throw `No <canvas> with id ${canvasId} was found`;
+
+			return new BrowserRenderer(managedHandle, canvas);
 		}
 
 		private setCanvasSize() {
@@ -52,105 +50,8 @@ namespace Uno.UI.Runtime.Skia {
 
 		static invalidate(instance: BrowserRenderer) {
 			window.requestAnimationFrame(() => {
-				(<any>window).GL.makeContextCurrent(instance.glCtx);
 				instance.requestRender();
 			});
-		}
-
-		public static createContextStatic(instance: BrowserRenderer, canvasOrCanvasId: any) {
-			return instance.createContext(canvasOrCanvasId);
-		}
-
-		private createContext(canvasOrCanvasId: string) {
-			if (!canvasOrCanvasId)
-				throw 'No <canvas> element or ID was provided';
-
-			this.canvas = <HTMLCanvasElement>document.getElementById(canvasOrCanvasId);
-
-			if (!this.canvas)
-				throw `No <canvas> with id ${canvasOrCanvasId} was found`;
-
-			try {
-				this.glCtx = BrowserRenderer.createWebGLContext(this.canvas);
-				if (!this.glCtx || this.glCtx < 0)
-					throw `Failed to create WebGL context: err ${this.glCtx}`;
-
-				// make current
-				BrowserRenderer.anyGL.makeContextCurrent(this.glCtx);
-
-				// Starting from .NET 7 the GLctx is defined in an inaccessible scope
-				// when the current GL context changes. We need to pick it up from the
-				// GL.currentContext instead.
-				let currentGLctx = BrowserRenderer.anyGL.currentContext && BrowserRenderer.anyGL.currentContext.GLctx;
-
-				if (!currentGLctx)
-					throw `Failed to get current WebGL context`;
-
-				// read values
-				this.setCanvasSize();
-				window.addEventListener("resize", x => this.setCanvasSize());
-				return {
-					success: true,
-					fbo: currentGLctx.getParameter(currentGLctx.FRAMEBUFFER_BINDING),
-					stencil: currentGLctx.getParameter(currentGLctx.STENCIL_BITS),
-					sample: 0, // TODO: currentGLctx.getParameter(GLctx.SAMPLES)
-					depth: currentGLctx.getParameter(currentGLctx.DEPTH_BITS),
-				};
-			} catch (e) {
-				this.ctx2D = this.canvas.getContext("2d");
-				return {
-					success: false,
-					error: e.toString()
-				}
-			}
-		}
-
-		static createWebGLContext(canvas: any) {
-			var contextAttributes = {
-				alpha: 1,
-				depth: 1,
-				stencil: 8,
-				antialias: 1,
-				premultipliedAlpha: 1,
-				preserveDrawingBuffer: 0,
-				preferLowPowerToHighPerformance: 0,
-				failIfMajorPerformanceCaveat: 0,
-				majorVersion: 2,
-				minorVersion: 0,
-				enableExtensionsByDefault: 1,
-				explicitSwapControl: 0,
-				renderViaOffscreenBackBuffer: 0,
-			};
-
-			var ctx = BrowserRenderer.anyGL.createContext(canvas, contextAttributes);
-			if (!ctx && contextAttributes.majorVersion > 1) {
-				console.warn('Falling back to WebGL 1.0');
-				contextAttributes.majorVersion = 1;
-				contextAttributes.minorVersion = 0;
-				ctx = BrowserRenderer.anyGL.createContext(canvas, contextAttributes);
-			}
-
-			return ctx;
-		}
-
-		public static resizePixelBuffer(instance: BrowserRenderer, width: number, height: number): number {
-			if (instance.pixelBuffer !== 0) {
-				Module._free(instance.pixelBuffer);
-			}
-			instance.canvas.width = width;
-			instance.canvas.height = height;
-			instance.pixelBuffer = Module._malloc(width * height * 4);
-			instance.clampedArray = new Uint8ClampedArray(Module.HEAPU8.buffer, instance.pixelBuffer, width * height * 4);
-			return instance.pixelBuffer;
-		}
-
-		public static blitSoftware(instance: BrowserRenderer, width: number, height: number) {
-			const imageData = new ImageData(
-				instance.clampedArray,
-				width,
-				height
-			);
-			instance.ctx2D.putImageData(imageData, 0, 0);
 		}
 	}
 }
