891773a5c0717f6f63b1ab13bd261c09332ecf62
Martin Zikmund <martin@zikmund.dev>
2026-01-07 20:42:58 +0100

    fix: Build issues

--- src/Uno.UWP/Generated/3.0.0.0/Windows.Storage/ApplicationData.cs
+++ src/Uno.UWP/Generated/3.0.0.0/Windows.Storage/ApplicationData.cs
@@ -166,8 +166,8 @@ namespace Windows.Storage
 		}
 #endif
 		// Forced skipping of method Windows.Storage.ApplicationData.SharedLocalFolder.get
-#if __ANDROID__ || __IOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
-		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
+#if __ANDROID__ || __IOS__ || __TVOS__ || IS_UNIT_TESTS || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
+		[global::Uno.NotImplemented("__ANDROID__", "__IOS__", "__TVOS__", "IS_UNIT_TESTS", "__WASM__", "__SKIA__", "__NETSTD_REFERENCE__", "__MACOS__")]
 		public void Dispose()
 		{
 			global::Windows.Foundation.Metadata.ApiInformation.TryRaiseNotImplemented("Windows.Storage.ApplicationData", "void ApplicationData.Dispose()");
