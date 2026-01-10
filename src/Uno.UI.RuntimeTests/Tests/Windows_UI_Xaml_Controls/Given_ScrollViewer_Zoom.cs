bc02fa8bf4df0cc83d285c79509ca30befeb3fc8
Martin Zikmund <martin@zikmund.dev>
2026-01-10 16:04:37 +0100

    test: Additional test coverage for offsets

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ScrollViewer_Zoom.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_ScrollViewer_Zoom.cs
@@ -292,4 +292,267 @@ public class Given_ScrollViewer_Zoom
 		Assert.AreEqual(2.0f, visual.Scale.Y, 0.01f, "Visual Scale.Y should be 2.0");
 #endif
 	}
+
+	[TestMethod]
+	public async Task When_ZoomedOut_ScrollableExtent_Is_Zero()
+	{
+		// Content is 400x400, viewport is 400x400
+		// At zoom 0.5, scaled content is 200x200 which is smaller than viewport
+		// So ScrollableWidth/Height should be 0
+		var content = new Border
+		{
+			Width = 400,
+			Height = 400,
+			Background = new SolidColorBrush(Colors.Blue)
+		};
+
+		var sut = new ScrollViewer
+		{
+			Width = 400,
+			Height = 400,
+			ZoomMode = ZoomMode.Enabled,
+			MinZoomFactor = 0.25f,
+			MaxZoomFactor = 4.0f,
+			HorizontalScrollBarVisibility = ScrollBarVisibility.Auto,
+			VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
+			Content = content
+		};
+
+		TestServices.WindowHelper.WindowContent = sut;
+		await TestServices.WindowHelper.WaitForLoaded(sut);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		// At zoom 1.0, content exactly fills viewport, so scrollable should be 0
+		Assert.AreEqual(0, sut.ScrollableWidth, 1, "ScrollableWidth should be 0 at zoom 1.0 when content equals viewport");
+		Assert.AreEqual(0, sut.ScrollableHeight, 1, "ScrollableHeight should be 0 at zoom 1.0 when content equals viewport");
+
+		// Zoom out to 0.5 - scaled content is now 200x200, smaller than 400x400 viewport
+		sut.ChangeView(null, null, 0.5f, disableAnimation: true);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		Assert.AreEqual(0, sut.ScrollableWidth, 1, "ScrollableWidth should be 0 when zoomed out content is smaller than viewport");
+		Assert.AreEqual(0, sut.ScrollableHeight, 1, "ScrollableHeight should be 0 when zoomed out content is smaller than viewport");
+	}
+
+	[TestMethod]
+	public async Task When_ZoomedIn_ScrollableExtent_Scales_With_Zoom()
+	{
+		// Content is 400x400, viewport is 200x200
+		// At zoom 1.0: scrollable = 400 - 200 = 200
+		// At zoom 2.0: scrollable = 800 - 200 = 600
+		var content = new Border
+		{
+			Width = 400,
+			Height = 400,
+			Background = new SolidColorBrush(Colors.Green)
+		};
+
+		var sut = new ScrollViewer
+		{
+			Width = 200,
+			Height = 200,
+			ZoomMode = ZoomMode.Enabled,
+			MinZoomFactor = 0.5f,
+			MaxZoomFactor = 4.0f,
+			HorizontalScrollBarVisibility = ScrollBarVisibility.Auto,
+			VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
+			Content = content
+		};
+
+		TestServices.WindowHelper.WindowContent = sut;
+		await TestServices.WindowHelper.WaitForLoaded(sut);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		// At zoom 1.0: scrollable = 400 - 200 = 200
+		Assert.AreEqual(200, sut.ScrollableWidth, 5, "ScrollableWidth should be ~200 at zoom 1.0");
+		Assert.AreEqual(200, sut.ScrollableHeight, 5, "ScrollableHeight should be ~200 at zoom 1.0");
+
+		// Zoom to 2.0: scrollable = 800 - 200 = 600
+		sut.ChangeView(null, null, 2.0f, disableAnimation: true);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		Assert.AreEqual(600, sut.ScrollableWidth, 5, "ScrollableWidth should be ~600 at zoom 2.0");
+		Assert.AreEqual(600, sut.ScrollableHeight, 5, "ScrollableHeight should be ~600 at zoom 2.0");
+	}
+
+	[TestMethod]
+	public async Task When_ZoomedOut_ScrollOffset_Is_Clamped()
+	{
+		// Start with scrollable content, scroll to an offset, then zoom out
+		// The offset should be clamped when content becomes smaller than viewport
+		var content = new Border
+		{
+			Width = 600,
+			Height = 600,
+			Background = new SolidColorBrush(Colors.Red)
+		};
+
+		var sut = new ScrollViewer
+		{
+			Width = 400,
+			Height = 400,
+			ZoomMode = ZoomMode.Enabled,
+			MinZoomFactor = 0.25f,
+			MaxZoomFactor = 4.0f,
+			HorizontalScrollBarVisibility = ScrollBarVisibility.Auto,
+			VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
+			Content = content
+		};
+
+		TestServices.WindowHelper.WindowContent = sut;
+		await TestServices.WindowHelper.WaitForLoaded(sut);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		// At zoom 1.0, content is 600x600 in 400x400 viewport, scrollable = 200
+		// Scroll to offset 100
+		sut.ChangeView(100, 100, null, disableAnimation: true);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		Assert.AreEqual(100, sut.HorizontalOffset, 5, "HorizontalOffset should be ~100");
+		Assert.AreEqual(100, sut.VerticalOffset, 5, "VerticalOffset should be ~100");
+
+		// Zoom out to 0.5 - scaled content is 300x300, smaller than 400x400 viewport
+		// Scrollable becomes 0, offset should be clamped to 0
+		sut.ChangeView(null, null, 0.5f, disableAnimation: true);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		Assert.AreEqual(0, sut.ScrollableWidth, 1, "ScrollableWidth should be 0 when zoomed out");
+		Assert.AreEqual(0, sut.ScrollableHeight, 1, "ScrollableHeight should be 0 when zoomed out");
+		Assert.AreEqual(0, sut.HorizontalOffset, 1, "HorizontalOffset should be clamped to 0 when zoomed out");
+		Assert.AreEqual(0, sut.VerticalOffset, 1, "VerticalOffset should be clamped to 0 when zoomed out");
+	}
+
+	[TestMethod]
+	public async Task When_Zoomed_ExtentSize_Does_Not_Change()
+	{
+		// ExtentWidth/ExtentHeight represent the logical content size
+		// They should NOT change when zoom changes
+		var content = new Border
+		{
+			Width = 500,
+			Height = 500,
+			Background = new SolidColorBrush(Colors.Purple)
+		};
+
+		var sut = new ScrollViewer
+		{
+			Width = 200,
+			Height = 200,
+			ZoomMode = ZoomMode.Enabled,
+			MinZoomFactor = 0.25f,
+			MaxZoomFactor = 4.0f,
+			Content = content
+		};
+
+		TestServices.WindowHelper.WindowContent = sut;
+		await TestServices.WindowHelper.WaitForLoaded(sut);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		var initialExtentWidth = sut.ExtentWidth;
+		var initialExtentHeight = sut.ExtentHeight;
+
+		// Zoom to 2.0
+		sut.ChangeView(null, null, 2.0f, disableAnimation: true);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		Assert.AreEqual(initialExtentWidth, sut.ExtentWidth, 1, "ExtentWidth should not change with zoom");
+		Assert.AreEqual(initialExtentHeight, sut.ExtentHeight, 1, "ExtentHeight should not change with zoom");
+
+		// Zoom to 0.5
+		sut.ChangeView(null, null, 0.5f, disableAnimation: true);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		Assert.AreEqual(initialExtentWidth, sut.ExtentWidth, 1, "ExtentWidth should not change with zoom");
+		Assert.AreEqual(initialExtentHeight, sut.ExtentHeight, 1, "ExtentHeight should not change with zoom");
+	}
+
+	[TestMethod]
+	public async Task When_Zoomed_ViewportSize_Does_Not_Change()
+	{
+		// ViewportWidth/ViewportHeight represent the visible area size
+		// They should NOT change when zoom changes
+		var content = new Border
+		{
+			Width = 500,
+			Height = 500,
+			Background = new SolidColorBrush(Colors.Orange)
+		};
+
+		var sut = new ScrollViewer
+		{
+			Width = 200,
+			Height = 200,
+			ZoomMode = ZoomMode.Enabled,
+			MinZoomFactor = 0.25f,
+			MaxZoomFactor = 4.0f,
+			Content = content
+		};
+
+		TestServices.WindowHelper.WindowContent = sut;
+		await TestServices.WindowHelper.WaitForLoaded(sut);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		var initialViewportWidth = sut.ViewportWidth;
+		var initialViewportHeight = sut.ViewportHeight;
+
+		// Zoom to 2.0
+		sut.ChangeView(null, null, 2.0f, disableAnimation: true);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		Assert.AreEqual(initialViewportWidth, sut.ViewportWidth, 1, "ViewportWidth should not change with zoom");
+		Assert.AreEqual(initialViewportHeight, sut.ViewportHeight, 1, "ViewportHeight should not change with zoom");
+
+		// Zoom to 0.5
+		sut.ChangeView(null, null, 0.5f, disableAnimation: true);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		Assert.AreEqual(initialViewportWidth, sut.ViewportWidth, 1, "ViewportWidth should not change with zoom");
+		Assert.AreEqual(initialViewportHeight, sut.ViewportHeight, 1, "ViewportHeight should not change with zoom");
+	}
+
+	[TestMethod]
+	public async Task When_ScrollOffset_Exceeds_NewScrollable_After_ZoomOut()
+	{
+		// Start scrolled to max, then zoom out - offset should be reduced
+		var content = new Border
+		{
+			Width = 800,
+			Height = 800,
+			Background = new SolidColorBrush(Colors.Cyan)
+		};
+
+		var sut = new ScrollViewer
+		{
+			Width = 400,
+			Height = 400,
+			ZoomMode = ZoomMode.Enabled,
+			MinZoomFactor = 0.5f,
+			MaxZoomFactor = 4.0f,
+			HorizontalScrollBarVisibility = ScrollBarVisibility.Auto,
+			VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
+			Content = content
+		};
+
+		TestServices.WindowHelper.WindowContent = sut;
+		await TestServices.WindowHelper.WaitForLoaded(sut);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		// At zoom 1.0, scrollable = 800 - 400 = 400
+		// Scroll to max offset (400)
+		sut.ChangeView(400, 400, null, disableAnimation: true);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		Assert.AreEqual(400, sut.HorizontalOffset, 5, "Should be scrolled to max");
+		Assert.AreEqual(400, sut.VerticalOffset, 5, "Should be scrolled to max");
+
+		// Zoom out to 0.5 - scaled content is 400x400, same as viewport
+		// Scrollable becomes 0, offset should be clamped to 0
+		sut.ChangeView(null, null, 0.5f, disableAnimation: true);
+		await TestServices.WindowHelper.WaitForIdle();
+
+		Assert.AreEqual(0, sut.ScrollableWidth, 1, "ScrollableWidth should be 0");
+		Assert.AreEqual(0, sut.ScrollableHeight, 1, "ScrollableHeight should be 0");
+		Assert.AreEqual(0, sut.HorizontalOffset, 1, "HorizontalOffset should be clamped to 0");
+		Assert.AreEqual(0, sut.VerticalOffset, 1, "VerticalOffset should be clamped to 0");
+	}
 }
