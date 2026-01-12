1bc63f04a41a09b6766f2b4c33e18b9e60e15fab
morning4coffe-dev <78549750+morning4coffe-dev@users.noreply.github.com>
2026-01-12 11:07:27 +0100

    chore: Add `GridView` ItemContainerStyle sample

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/GridView/GridView_ItemContainerStyle_Selection.xaml.cs
@@ -0,0 +1,14 @@
+using Uno.UI.Samples.Controls;
+using Microsoft.UI.Xaml.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.GridView
+{
+	[Sample("GridView", Description = "Tests that GridViewItem selection visual indicator works correctly when ItemContainerStyle is set")]
+	public sealed partial class GridView_ItemContainerStyle_Selection : Page
+	{
+		public GridView_ItemContainerStyle_Selection()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
