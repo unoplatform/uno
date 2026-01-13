ac5d508605e238da0390a9ae8a66c949090d128e
David <github.site@torick.net>
2026-01-12 20:47:02 -0500

    feat(hr): Split Data

--- src/Uno.UI.RemoteControl.Server.Processors/Helpers/RoslynExtensions.cs
+++ src/Uno.UI.RemoteControl.Server.Processors/Helpers/RoslynExtensions.cs
@@ -95,7 +95,7 @@ public record MetadataReferenceData(
 public record AnalyzerReferenceData(
 	string FilePath);
 
-public static class RoslynExtensions
+public static partial class RoslynExtensions
 {
 	[Flags]
 	public enum RoslynInfoOptions
@@ -170,50 +170,6 @@ public static class RoslynExtensions
 	}
 
 
-	public static SolutionData GetData(this Solution solution)
-		=> new(
-			solution.Id,
-			solution.Version,
-			solution.FilePath,
-			[.. solution.Projects.Select(GetData)],
-			[.. solution.AnalyzerReferences.Select(GetData)]);
-
-	public static ProjectData GetData(this Project project)
-		=> new(
-			project.Id,
-			project.Version,
-			project.Name,
-			project.AssemblyName,
-			project.Language,
-			project.FilePath,
-			project.OutputFilePath,
-			project.CompilationOptions,
-			project.CompilationOutputInfo,
-			project.ParseOptions,
-			[.. project.Documents.Select(GetData)],
-			[.. project.ProjectReferences.Select(GetData)],
-			[.. project.MetadataReferences.Select(GetData)],
-			[.. project.AnalyzerReferences.Select(GetData)],
-			[.. project.AnalyzerConfigDocuments.Select(GetData)],
-			[.. project.AdditionalDocuments.Select(GetData)],
-			project.IsSubmission);
-
-	public static DocumentData GetData(this Document document)
-		=> new(
-			document.Id,
-			document.Name,
-			[.. document.Folders],
-			document.SourceCodeKind,
-			document.FilePath);
-
-	public static DocumentData GetData(this TextDocument document)
-		=> new(
-			document.Id,
-			document.Name,
-			[.. document.Folders],
-			default,
-			document.FilePath);
-
 	public static DocumentInfo GetInfo(this DocumentData document, ProjectId projectId)
 		=> DocumentInfo.Create(
 			document.Id ?? DocumentId.CreateNewId(projectId),
@@ -256,28 +212,5 @@ public static class RoslynExtensions
 		}
 
 		return new AnalyzerFileReference(reference.FilePath, analyzerLoader);
-	}
-
-	private static ProjectReferenceData GetData(ProjectReference reference)
-		=> new(reference.ProjectId, reference.Aliases, reference.EmbedInteropTypes);
-
-	private static MetadataReferenceData GetData(MetadataReference reference)
-	{
-		if (reference is not PortableExecutableReference { FilePath: { Length: > 0 } filePath })
-		{
-			throw new InvalidOperationException("Only PortableExecutableReference with valid file path are supported.");
-		}
-
-		return new MetadataReferenceData(filePath, reference.Properties);
-	}
-
-	private static AnalyzerReferenceData GetData(AnalyzerReference reference)
-	{
-		if (reference is not AnalyzerFileReference { FullPath: { Length: > 0 } filePath })
-		{
-			throw new InvalidOperationException("Only AnalyzerFileReference with valid file path are supported.");
-		}
-
-		return new AnalyzerReferenceData(filePath);
 	}
 }
