50953399167d4171af1feaa7d953cfdea7dd2b60
Ramez Ragaa <ramezragaa@proton.me>
2026-01-16 10:46:22 -0500

    chore: fix build
    (cherry picked from commit 6886f24f81b20c9261e40086a0d097e5d949eb7f)

--- src/SourceGenerators/Uno.UI.SourceGenerators/IcuDataInitializerGenerator.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators/IcuDataInitializerGenerator.cs
@@ -21,13 +21,14 @@ public class IcuDataInitializerGenerator : IIncrementalGenerator
 			{
 				return;
 			}
+			var icuType = Type.GetType("Microsoft.UI.Xaml.Documents.UnicodeText+ICU, Uno.UI");
 			var code = """
 						internal static class __IcuDataInitializer
 						{
 							[global::System.Runtime.CompilerServices.ModuleInitializerAttribute]
 							internal static void Initialize()
 							{
-								var icuType = Type.GetType("Microsoft.UI.Xaml.Documents.UnicodeText+ICU, Uno.UI");
+								var icuType = System.Type.GetType("Microsoft.UI.Xaml.Documents.UnicodeText+ICU, Uno.UI");
 								var setMethod = icuType?.GetMethod("SetDataAssembly",
 									System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
 								setMethod?.Invoke(null, new object[] { typeof(__IcuDataInitializer).Assembly });
