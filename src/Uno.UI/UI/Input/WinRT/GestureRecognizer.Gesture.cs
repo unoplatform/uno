4fda4562d3ece43567e73741da7abf87da6652cd
Martin Zikmund <martin@zikmund.dev>
2026-01-19 09:29:04 +0100

    chore: Adjust namespacing
    Removed conditional compilation for UWP and WinUI branches.

--- src/Uno.UI/UI/Input/WinRT/GestureRecognizer.Gesture.cs
+++ src/Uno.UI/UI/Input/WinRT/GestureRecognizer.Gesture.cs
@@ -1,5 +1,3 @@
-// On the UWP branch, only include this file in Uno.UWP (as public Window.whatever). On the WinUI branch, include it in both Uno.UWP (internal as Windows.whatever) and Uno.UI (public as Microsoft.whatever)
-#if HAS_UNO_WINUI || !IS_UNO_UI_PROJECT
 #nullable enable
 
 using System;
@@ -14,7 +12,11 @@ using Windows.System;
 using Uno;
 using Uno.Foundation.Logging;
 
+#if IS_UNO_UI_PROJECT
 namespace Microsoft.UI.Input
+#else
+namespace Windows.UI.Input
+#endif
 {
 	public partial class GestureRecognizer
 	{
@@ -395,4 +397,3 @@ namespace Microsoft.UI.Input
 		}
 	}
 }
-#endif
