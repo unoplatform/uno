8646e72867598f022fa3a46063d68b6f3399da76
Martin Zikmund <martin@zikmund.dev>
2026-01-24 18:38:43 +0100

    chore: Adjust namespaces

--- src/Uno.UI/UI/Input/WinRT/GestureRecognizer.Manipulation.InertiaProcessor.cs
+++ src/Uno.UI/UI/Input/WinRT/GestureRecognizer.Manipulation.InertiaProcessor.cs
@@ -14,7 +14,13 @@ using static System.Double;
 using Microsoft.UI.Composition;
 using Microsoft.UI.Xaml.Media;
 
+#if IS_UNO_UI_PROJECT
+using Microsoft.UI.Composition;
+
 namespace Microsoft.UI.Input;
+#else
+namespace Windows.UI.Input;
+#endif
 
 public partial class GestureRecognizer
 {
