d5c6aa12de1c63cc86e47ecbf697dbe7748ba31c
Martin Zikmund <martin@zikmund.dev>
2026-02-04 11:20:19 +0100

    fix: Avoid NRE on WASM startup

--- src/Uno.UI.Runtime.Skia.WebAssembly.Browser/Rendering/BrowserRenderer.cs
+++ src/Uno.UI.Runtime.Skia.WebAssembly.Browser/Rendering/BrowserRenderer.cs
@@ -55,6 +55,13 @@ internal partial class BrowserRenderer
 
 	private void RenderFrame()
 	{
+		// The RootElement may not be set yet during startup because the JavaScript
+		// requestAnimationFrame can fire before the app initialization completes.
+		if (_host.RootElement is not { Visual.CompositionTarget: CompositionTarget compositionTarget })
+		{
+			return;
+		}
+
 		_renderStopwatch.Restart();
 
 		if (this.Log().IsEnabled(LogLevel.Trace))
@@ -70,7 +77,7 @@ internal partial class BrowserRenderer
 			_canvas = null;
 		}
 
-		var currentClipPath = ((CompositionTarget)_host.RootElement!.Visual.CompositionTarget!).OnNativePlatformFrameRequested(_canvas, size =>
+		var currentClipPath = compositionTarget.OnNativePlatformFrameRequested(_canvas, size =>
 		{
 			return _canvas = _renderer.Resize((int)size.Width, (int)size.Height);
 		});
