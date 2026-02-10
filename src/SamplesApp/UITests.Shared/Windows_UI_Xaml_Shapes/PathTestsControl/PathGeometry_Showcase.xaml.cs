7ed005af1118f1160ffa46464a0e3e7c0107f8aa
Ramez Ragaa <ramezragaa@proton.me>
2026-02-10 11:13:07 -0500

    test: add PathGeometry_Showcase

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Shapes/PathTestsControl/PathGeometry_Showcase.xaml.cs
@@ -0,0 +1,14 @@
+using Uno.UI.Samples.Controls;
+using Microsoft.UI.Xaml.Controls;
+
+namespace SamplesApp.Windows_UI_Xaml_Shapes.PathTestsControl
+{
+	[Sample("Path")]
+	public sealed partial class PathGeometry_Showcase : UserControl
+	{
+		public PathGeometry_Showcase()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
