64c41d0af3fb7b38426a48652b23758c8e51776d
Martin Zikmund <martin@zikmund.dev>
2026-02-18 20:34:37 +0100

    chore: Cleanup Program.cs

--- src/Uno.UWPSyncGenerator/Program.cs
+++ src/Uno.UWPSyncGenerator/Program.cs
@@ -31,35 +31,12 @@ namespace Uno.UWPSyncGenerator
 
 			if (mode == SyncMode || mode == AllMode)
 			{
-#if HAS_UNO_WINUI
 				await new SyncGenerator().Build();
-#else
-				await new SyncGenerator().Build("Uno.Foundation", "Windows.Foundation.FoundationContract");
-				await new SyncGenerator().Build("Uno", "Windows.Foundation.UniversalApiContract");
-				await new SyncGenerator().Build("Uno", "Windows.Phone.PhoneContract");
-				await new SyncGenerator().Build("Uno", "Windows.Networking.Connectivity.WwanContract");
-				await new SyncGenerator().Build("Uno", "Windows.ApplicationModel.Calls.CallsPhoneContract");
-				await new SyncGenerator().Build("Uno", "Windows.Services.Store.StoreContract");
-
-				// When adding support for a new WinRT contract here, ensure to add it to the list of supported contracts in ApiInformation.shared.cs
-
-				await new SyncGenerator().Build("Uno.UI.Composition", "Windows.Foundation.UniversalApiContract");
-				await new SyncGenerator().Build("Uno.UI.Dispatching", "Windows.Foundation.UniversalApiContract");
-				await new SyncGenerator().Build("Uno.UI", "Windows.Foundation.UniversalApiContract");
-				await new SyncGenerator().Build("Uno.UI", "Windows.UI.Xaml.Hosting.HostingContract");
-				await new SyncGenerator().Build("Uno.UI", "Microsoft.UI.Xaml");
-				await new SyncGenerator().Build("Uno.UI", "Microsoft.Web.WebView2.Core");
-#endif
 			}
 
 			if (mode == DocMode || mode == AllMode)
 			{
-#if HAS_UNO_WINUI
 				await new DocGenerator().Build();
-#else
-				await new DocGenerator().Build("Uno.UI", "Windows.Foundation.UniversalApiContract");
-#endif
-
 			}
 		}
 
