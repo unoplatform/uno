dd44cbf2e732cd69b94c7b1ca7b3874c8654c474
Martin Zikmund <martin@zikmund.dev>
2026-02-23 19:00:35 +0100

    chore: Adjust for unload

--- src/Uno.UI.RuntimeTests/AssemblyInfo.cs
+++ src/Uno.UI.RuntimeTests/AssemblyInfo.cs
@@ -1,2 +1,6 @@
 ﻿
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("SamplesApp.Windows")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("SamplesApp.Skia")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("SamplesApp.Wasm")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("SamplesApp.netcoremobile")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UnoIslandsSamplesApp.Skia")]
