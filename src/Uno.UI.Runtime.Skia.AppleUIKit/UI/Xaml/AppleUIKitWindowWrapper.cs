62134b6a2e6ac96713b5720e936d8f3b5494f6ba
Martin Zikmund <martin@zikmund.dev>
2026-02-24 13:27:36 +0100

    chore: Address comments
    (cherry picked from commit 3083bd731e003646f88ee3f66d0fd27a475e4aff)

--- src/Uno.UI.Runtime.Skia.AppleUIKit/UI/Xaml/AppleUIKitWindowWrapper.cs
+++ src/Uno.UI.Runtime.Skia.AppleUIKit/UI/Xaml/AppleUIKitWindowWrapper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading;
 using CoreGraphics;
 using Foundation;
 using Microsoft.UI.Dispatching;
@@ -110,7 +112,7 @@ internal class NativeWindowWrapper : NativeWindowWrapperBase
 		}
 	}
 
-	public static Queue<NativeWindowWrapper> AwaitingScene { get; } = new();
+	public static ConcurrentQueue<NativeWindowWrapper> AwaitingScene { get; } = new();
 
 	private static int _visibleWindowCount;
 
@@ -130,7 +132,7 @@ internal class NativeWindowWrapper : NativeWindowWrapperBase
 		}
 
 		_isPendingShow = false;
-		_visibleWindowCount++;
+		Interlocked.Increment(ref _visibleWindowCount);
 
 		if (_xamlRoot.Content is FrameworkElement { IsLoaded: false } fe)
 		{
@@ -274,8 +276,7 @@ internal class NativeWindowWrapper : NativeWindowWrapperBase
 	{
 		OnNativeVisibilityChanged(false);
 
-		_visibleWindowCount--;
-		if (_visibleWindowCount == 0)
+		if (Interlocked.Decrement(ref _visibleWindowCount) == 0)
 		{
 			// Last window backgrounded - raise app-level events
 			Application.Current?.RaiseEnteredBackground(() => Application.Current?.RaiseSuspending());
@@ -295,7 +296,7 @@ internal class NativeWindowWrapper : NativeWindowWrapperBase
 			OnNativeVisibilityChanged(true);
 		}
 
-		_visibleWindowCount++;
+		Interlocked.Increment(ref _visibleWindowCount);
 	}
 
 	private void OnActivated(NSNotification notification) => OnNativeActivated(CoreWindowActivationState.CodeActivated);
