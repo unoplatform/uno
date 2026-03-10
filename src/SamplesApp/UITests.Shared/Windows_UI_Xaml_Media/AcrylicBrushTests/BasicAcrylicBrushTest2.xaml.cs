119830051b9db9fe155487570d432eae8a60c4fa
Ramez Ragaa <ramezragaa@proton.me>
2026-03-10 13:34:18 -0400

    test: BasicAcrylicBrushTest2

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Media/AcrylicBrushTests/BasicAcrylicBrushTest2.xaml.cs
@@ -0,0 +1,19 @@
+using Uno.UI.Samples.Controls;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+
+namespace UITests.Windows_UI_Xaml_Media.AcrylicBrushTests
+{
+	[Sample("Brushes", Description = "Demonstrates a basic Acrylic brush")]
+	public sealed partial class BasicAcrylicBrushTest2 : Page
+	{
+		public BasicAcrylicBrushTest2()
+		{
+			this.InitializeComponent();
+		}
+		private void OpacitySlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+		{
+			InteractiveAcrylic.TintOpacity = e.NewValue;
+		}
+	}
+}
