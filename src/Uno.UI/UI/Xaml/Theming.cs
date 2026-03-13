a5d1fdc306f828fad10e6ee652b6fe366d94d855
Martin Zikmund <martin@zikmund.dev>
2026-03-13 11:17:20 +0100

    chore: Address comment

--- src/Uno.UI/UI/Xaml/Theming.cs
+++ src/Uno.UI/UI/Xaml/Theming.cs
@@ -8,20 +8,20 @@ namespace Microsoft.UI.Xaml;
 internal enum Theme : byte
 {
 	// Mask to separate High Contrast and base themes
-	BaseMask                    = 0x03,
+	BaseMask = 0x03,
 	// Uses initial theme, which may be Light or Dark
-	None                        = 0x00,
+	None = 0x00,
 	// Base themes
-	Light                       = 0x01,
-	Dark                        = 0x02,
+	Light = 0x01,
+	Dark = 0x02,
 	// High Contrast themes can be set in addition to Light & Dark, and
 	// they take precedence.
-	HighContrastMask            = 0x1C,
-	HighContrastNone            = 0x00,
-	HighContrast                = 0x04,
-	HighContrastWhite           = 0x08,
-	HighContrastBlack           = 0x0C,
-	HighContrastCustom          = 0x10,
+	HighContrastMask = 0x1C,
+	HighContrastNone = 0x00,
+	HighContrast = 0x04,
+	HighContrastWhite = 0x08,
+	HighContrastBlack = 0x0C,
+	HighContrastCustom = 0x10,
 
 	// This should always be the last. If adding a new theme, add before Unused
 	Unused = 0x20,
@@ -30,18 +30,18 @@ internal enum Theme : byte
 internal static class Theming
 {
 	// Theme enum is stored on CDependencyObject and packed to 5 bits. 
-    // If a new theme needs to be added, then modifying the theme will
-    // require the bitfield on CDependencyObject to be increased
+	// If a new theme needs to be added, then modifying the theme will
+	// require the bitfield on CDependencyObject to be increased
 
-    // Theme::None:                 0x00000
-    // Theme::Light:                0x00001
-    // Theme::Dark:                 0x00010
-    // Theme::BaseMask:             0x00011
-    // Theme::HighContrastMask:     0x11100
-    // Theme::HighContrast:         0x00100
-    // Theme::HighContrastWhite:    0x01000
-    // Theme::HighContrastBlack:    0x01100
-    // Theme::HighContrastCustom:   0x10000
+	// Theme::None:                 0x00000
+	// Theme::Light:                0x00001
+	// Theme::Dark:                 0x00010
+	// Theme::BaseMask:             0x00011
+	// Theme::HighContrastMask:     0x11100
+	// Theme::HighContrast:         0x00100
+	// Theme::HighContrastWhite:    0x01000
+	// Theme::HighContrastBlack:    0x01100
+	// Theme::HighContrastCustom:   0x10000
 	public static Theme GetBaseValue(Theme theme)
 		=> (Theme)((byte)theme & 0x03);
 
