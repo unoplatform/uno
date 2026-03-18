f23b1c3f52341215ccf59d991ef617bcd263d344
Martin Zikmund <martin@zikmund.dev>
2026-03-18 16:50:44 +0100

    chore: Remove WASM
    Currently it is not possible to retrieve the OS accent color due to fingerprinting protection

--- src/Uno.UWP/Helpers/Theming/AccentColorHelper.wasm.cs
+++ src/Uno.UWP/Helpers/Theming/AccentColorHelper.wasm.cs
@@ -1,39 +1,8 @@
 #nullable enable
 
-using System;
-using System.Runtime.InteropServices.JavaScript;
-using Windows.UI;
-
-using NativeMethods = __Uno.Helpers.Theming.AccentColorHelper.NativeMethods;
-
 namespace Uno.Helpers.Theming;
 
 internal static partial class AccentColorHelper
 {
-	private static partial AccentColorPalette? GetPlatformAccentColorPalette()
-	{
-		var hex = NativeMethods.GetAccentColor();
-		if (hex is null || hex.Length != 7 || hex[0] != '#')
-		{
-			return null;
-		}
-
-		var r = Convert.ToByte(hex.Substring(1, 2), 16);
-		var g = Convert.ToByte(hex.Substring(3, 2), 16);
-		var b = Convert.ToByte(hex.Substring(5, 2), 16);
-		var accent = Color.FromArgb(0xFF, r, g, b);
-		return AccentColorPalette.FromAccentColor(accent);
-	}
-
-	static partial void ObserveAccentColorChanges()
-	{
-		NativeMethods.ObserveAccentColor();
-	}
-
-	[JSExport]
-	public static int DispatchAccentColorChange()
-	{
-		RefreshAccentColor();
-		return 0;
-	}
+	private static partial AccentColorPalette? GetPlatformAccentColorPalette() => null;
 }
