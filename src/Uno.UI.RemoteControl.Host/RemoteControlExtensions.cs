00749bd8d5ee79c0dd51584c41fc1d3b86a64301
Jerome Laban <jerome@platform.uno>
2026-03-26 12:33:40 -0400

    chore: Remove unused using directives from Program.cs and RemoteControlExtensions.cs

--- src/Uno.UI.RemoteControl.Host/RemoteControlExtensions.cs
+++ src/Uno.UI.RemoteControl.Host/RemoteControlExtensions.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
