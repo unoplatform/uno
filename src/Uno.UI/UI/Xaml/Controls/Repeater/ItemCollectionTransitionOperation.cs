b81b0e1df9fd42ac0164c4f3b16ab51c3f64270c
Martin Zikmund <martin@zikmund.dev>
2026-03-27 12:44:53 +0100

    feat: `ItemCollectionTransition`

---
+++ src/Uno.UI/UI/Xaml/Controls/Repeater/ItemCollectionTransitionOperation.cs
@@ -0,0 +1,24 @@
+// MUX Reference (generated), tag winui3/release/1.6-stable
+
+namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Defines constants that specify the type of transition operation to animate.
+/// </summary>
+public enum ItemCollectionTransitionOperation
+{
+	/// <summary>
+	/// A data object was added to the collection.
+	/// </summary>
+	Add = 0,
+
+	/// <summary>
+	/// A data object was removed from the collection.
+	/// </summary>
+	Remove = 1,
+
+	/// <summary>
+	/// A data object was moved to a different location in the collection.
+	/// </summary>
+	Move = 2,
+}
