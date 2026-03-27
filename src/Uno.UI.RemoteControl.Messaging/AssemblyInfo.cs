641b4b08f1e04d92b36bd5498f534c042a7b665d
Jerome Laban <jerome@platform.uno>
2026-03-27 16:42:07 -0400

    chore: Adjust for merge

--- src/Uno.UI.RemoteControl.Messaging/AssemblyInfo.cs
+++ src/Uno.UI.RemoteControl.Messaging/AssemblyInfo.cs
@@ -3,3 +3,4 @@
 [assembly: InternalsVisibleTo("Uno.UI.RemoteControl.VS")]
 [assembly: InternalsVisibleTo("Uno.UI.RemoteControl.Host")]
 [assembly: InternalsVisibleTo("Uno.Rider")]
+[assembly: InternalsVisibleTo("Uno.UI.RemoteControl.DevServer.Tests")]
