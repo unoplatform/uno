ce9250f1f9166feb457c7ccae8e6e75b670ae347
Jerome Laban <jerome@platform.uno>
2026-03-29 21:46:59 -0400

    chore: Add internal IIdeChannelServer interface and update project references

--- src/Uno.UI.RemoteControl.Messaging/AssemblyInfo.cs
+++ src/Uno.UI.RemoteControl.Messaging/AssemblyInfo.cs
@@ -3,4 +3,3 @@
 [assembly: InternalsVisibleTo("Uno.UI.RemoteControl.VS")]
 [assembly: InternalsVisibleTo("Uno.UI.RemoteControl.Host")]
 [assembly: InternalsVisibleTo("Uno.Rider")]
-[assembly: InternalsVisibleTo("Uno.UI.RemoteControl.DevServer.Tests")]
