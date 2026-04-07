7b928d04fe6e22acd87196181aa15f415d5c9cc4
Martin Zikmund <martin@zikmund.dev>
2026-04-07 14:09:25 +0200

    test: Reproduce BlockCollection indexer shadowed by NotImplemented stub
    Relates to https://github.com/unoplatform/uno/issues/22932

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_DependencyObjectCollection.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml/Given_DependencyObjectCollection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.ObjectiveC;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Documents;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml;
@@ -43,5 +44,43 @@ public class Given_DependencyObjectCollection
 	public void When_Indexer_Get_NegativeIndex()
 	{
 		Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => _ = new DependencyObjectCollection()[-1]);
+	}
+
+	// Reproduces https://github.com/unoplatform/uno/issues/22932
+	// UWPSyncGenerator emits a [NotImplemented] stub for BlockCollection.this[int] that
+	// shadows the working DependencyObjectCollection<Block> base class implementation,
+	// causing NotSupportedException when accessing elements by index.
+
+	[TestMethod]
+	[GitHubWorkItem("https://github.com/unoplatform/uno/issues/22932")]
+	[PlatformCondition(ConditionMode.Exclude, RuntimeTestPlatforms.NativeWinUI)]
+	public void When_BlockCollection_Indexer_Get_Returns_Added_Item()
+	{
+		var collection = new BlockCollection();
+		var paragraph = new Paragraph();
+		collection.Add(paragraph);
+
+		// This should return the paragraph but currently throws NotSupportedException
+		// because the generated stub in BlockCollection.cs shadows DependencyObjectCollection<Block>.this[int]
+		var retrieved = collection[0];
+
+		Assert.AreSame(paragraph, retrieved);
+	}
+
+	[TestMethod]
+	[GitHubWorkItem("https://github.com/unoplatform/uno/issues/22932")]
+	[PlatformCondition(ConditionMode.Exclude, RuntimeTestPlatforms.NativeWinUI)]
+	public void When_BlockCollection_Indexer_Set_Replaces_Item()
+	{
+		var collection = new BlockCollection();
+		var original = new Paragraph();
+		var replacement = new Paragraph();
+		collection.Add(original);
+
+		// This should replace the item but currently throws NotSupportedException
+		// because the generated stub in BlockCollection.cs shadows DependencyObjectCollection<Block>.this[int]
+		collection[0] = replacement;
+
+		Assert.AreSame(replacement, collection[0]);
 	}
 }
