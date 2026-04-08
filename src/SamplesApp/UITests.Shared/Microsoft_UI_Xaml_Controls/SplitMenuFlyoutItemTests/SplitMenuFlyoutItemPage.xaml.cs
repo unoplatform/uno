4df15de46d6b946046b6d6755df75bdf6425820a
Martin Zikmund <martin@zikmund.dev>
2026-04-08 14:29:24 +0200

    chore: Disable sample page on WinUI

--- src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/SplitMenuFlyoutItemTests/SplitMenuFlyoutItemPage.xaml.cs
+++ src/SamplesApp/UITests.Shared/Microsoft_UI_Xaml_Controls/SplitMenuFlyoutItemTests/SplitMenuFlyoutItemPage.xaml.cs
@@ -3,15 +3,20 @@ using Uno.UI.Samples.Controls;
 
 namespace UITests.Shared.Microsoft_UI_Xaml_Controls.SplitMenuFlyoutItemTests
 {
+#if HAS_UNO
 	[Sample("Flyouts")]
+#endif
 	public sealed partial class SplitMenuFlyoutItemPage : Page
 	{
+#if HAS_UNO
 		private int _clickCount;
+#endif
 
 		public SplitMenuFlyoutItemPage()
 		{
 			this.InitializeComponent();
 
+#if HAS_UNO
 			SaveItem.Click += (s, e) => BasicStatus.Text = "Primary action: Save clicked";
 			OpenWithItem.Click += (s, e) => BasicStatus.Text = "Primary action: Open With Photos clicked";
 			SaveAsItem.Click += (s, e) => BasicStatus.Text = "Sub-item: Save As clicked";
@@ -20,6 +25,7 @@ namespace UITests.Shared.Microsoft_UI_Xaml_Controls.SplitMenuFlyoutItemTests
 				_clickCount++;
 				ClickStatus.Text = $"Clicked {_clickCount} time(s)";
 			};
+#endif
 		}
 	}
 }
