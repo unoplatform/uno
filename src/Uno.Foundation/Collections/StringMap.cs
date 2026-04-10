157e346bfebf5e6dba2dd5ec6f15b4f0b9101653
Martin Zikmund <martin@zikmund.dev>
2026-04-10 11:00:33 +0200

    chore: Cleanup

--- src/Uno.Foundation/Collections/StringMap.cs
+++ src/Uno.Foundation/Collections/StringMap.cs
@@ -26,14 +26,6 @@ public sealed partial class StringMap :
 	/// </summary>
 	public event MapChangedEventHandler<string, string?>? MapChanged;
 
-#if HAS_UNO_WINUI
-	event MapChangedEventHandler<string, string?> IObservableMap<string, string?>.MapChanged
-	{
-		add => MapChanged += value;
-		remove => MapChanged -= value;
-	}
-#endif
-
 	/// <summary>
 	/// Gets the number of items contained in the string map.
 	/// </summary>
