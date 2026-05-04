0bc55f612f92721eb7598f568100bd23ee275133
Martin Zikmund <martin@zikmund.dev>
2026-05-04 09:00:36 +0200

    chore: Re-add Layout

---
+++ src/Uno.UI/UI/Xaml/Controls/Repeater/Layout.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference Layout.h, commit 5f9e851133b3
+
+namespace Microsoft.UI.Xaml.Controls;
+
+/// <summary>
+/// Represents the base class for an object that sizes and arranges child elements
+/// for a host container.
+/// </summary>
+public partial class Layout : global::Microsoft.UI.Xaml.DependencyObject
+{
+}
