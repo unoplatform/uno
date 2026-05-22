fbb8a44c2c9f5820e534288c9a1142e824d1d236
Martin Zikmund <martin@zikmund.dev>
2026-05-22 08:50:56 +0200

    feat: Support `SKCanvasElement` on WinUI

--- src/AddIns/Uno.WinUI.Graphics2DSK/SKCanvasElement.cs
+++ src/AddIns/Uno.WinUI.Graphics2DSK/SKCanvasElement.cs
@@ -14,8 +14,16 @@ namespace Uno.WinUI.Graphics2DSK;
 /// <summary>
 /// A <see cref="FrameworkElement"/> that exposes the ability to draw directly on the window using SkiaSharp.
 /// </summary>
-/// <remarks>This is only available on skia-based targets.</remarks>
-public abstract partial class SKCanvasElement : FrameworkElement
+/// <remarks>
+/// On skia-based targets the drawing happens through the Uno composition pipeline.
+/// On WinUI (WinAppSDK) the drawing happens through <see cref="SkiaSharp.Views.Windows.SKSwapChainPanel"/>.
+/// </remarks>
+public abstract partial class SKCanvasElement
+#if WINAPPSDK
+	: SkiaSharp.Views.Windows.SKSwapChainPanel
+#else
+	: FrameworkElement
+#endif
 {
 #if CROSSRUNTIME
 	private SKCanvasVisualBase? _skCanvasVisual;
@@ -41,23 +49,72 @@ public abstract partial class SKCanvasElement : FrameworkElement
 		{
 			throw new PlatformNotSupportedException($"This platform does not support {nameof(SKCanvasElement)}. For more information: https://aka.platform.uno/skcanvaselement");
 		}
+
+#if WINAPPSDK
+		PaintSurface += OnPaintSurface;
+#endif
 	}
 
 #if CROSSRUNTIME
 	public static bool IsSupportedOnCurrentPlatform() => ApiExtensibility.IsRegistered<SKCanvasVisualBaseFactory>();
+#elif WINAPPSDK
+	public static bool IsSupportedOnCurrentPlatform() => true;
 #else
 	public static bool IsSupportedOnCurrentPlatform() => false;
 #endif
 
+#if CROSSRUNTIME
 	/// <summary>
 	/// Invalidates the element and triggers a redraw.
 	/// </summary>
-#if CROSSRUNTIME
 	public void Invalidate() => _skCanvasVisual?.Invalidate();
-#else
+#elif !WINAPPSDK
+	/// <summary>
+	/// Invalidates the element and triggers a redraw.
+	/// </summary>
 #pragma warning disable CS0109 // Member does not hide an inherited member; new keyword is not required
 	public new void Invalidate() { }
 #pragma warning restore CS0109 // Member does not hide an inherited member; new keyword is not required
+#endif
+
+#if WINAPPSDK
+	// Mirrors the `_rendering` guard in Uno's Win32WindowWrapper.Rendering.cs:
+	// if user code blocks inside RenderOverride (e.g. Task.Wait or Monitor.Enter),
+	// a COM/WndProc message-pump turn could re-enter PaintSurface and corrupt
+	// the canvas state.
+	private bool _isRendering;
+
+	private void OnPaintSurface(object? sender, SkiaSharp.Views.Windows.SKPaintGLSurfaceEventArgs e)
+	{
+		if (_isRendering)
+		{
+			return;
+		}
+
+		_isRendering = true;
+		try
+		{
+			var canvas = e.Surface.Canvas;
+			var area = new Size(ActualWidth, ActualHeight);
+
+			// We save and restore the canvas state ourselves so that the inheritor doesn't accidentally forget to.
+			canvas.Save();
+			// clipping here guarantees that drawing doesn't get outside the intended area
+			canvas.ClipRect(new SKRect(0, 0, (float)area.Width, (float)area.Height), antialias: true);
+			try
+			{
+				RenderOverride(canvas, area);
+			}
+			finally
+			{
+				canvas.Restore();
+			}
+		}
+		finally
+		{
+			_isRendering = false;
+		}
+	}
 #endif
 
 	/// <summary>
