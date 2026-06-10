f66f945b87134768fbc19032adb0c3d3db37dd90
Andres Pineda <pineda.andres@gmail.com>
2026-06-10 12:28:40 -0400

    test: add CheckBox disabled-in-ListView sample
    (cherry picked from commit 51b33fb48c7bf2689f935635b2527e51af8936de)
    
    # Conflicts:
    #	src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/CheckBoxTests/CheckBox_DisabledInDisabledListView.xaml
    #	src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/CheckBoxTests/CheckBox_DisabledInDisabledListView.xaml.cs

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/CheckBoxTests/CheckBox_DisabledInDisabledListView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Windows_UI_Xaml_Controls.CheckBoxTests;
+
+[Sample("Buttons", Description = "A checked, disabled CheckBox should render identically whether standalone or inside a disabled ListView; the list item must not add extra dimming.", IsManualTest = true)]
+public sealed partial class CheckBox_DisabledInDisabledListView : Page
+{
+	public CheckBox_DisabledInDisabledListView()
+	{
+		this.InitializeComponent();
+		DataContext = this;
+	}
+
+	public IReadOnlyList<string> Items { get; } = new[]
+	{
+		"Disabled checkbox in list 1",
+		"Disabled checkbox in list 2",
+		"Disabled checkbox in list 3",
+	};
+}
