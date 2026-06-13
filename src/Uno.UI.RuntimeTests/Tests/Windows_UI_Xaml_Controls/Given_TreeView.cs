3228112b0c0e42368a27c5d5834f58349bda300e
xiaoy312 <xiaoyao312@gmail.com>
2026-06-13 03:11:25 -0400

    fix(TreeView): node selection causes random node to expand/collapse

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_TreeView.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/Given_TreeView.cs
@@ -1,24 +1,30 @@
 ﻿using System;
-using System.Linq;
-using Windows.UI;
-using System.Threading.Tasks;
-using Private.Infrastructure;
-using Uno.UI.RuntimeTests.Helpers;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TreeView = Microsoft.UI.Xaml.Controls.TreeView;
-using TreeViewItem = Microsoft.UI.Xaml.Controls.TreeViewItem;
-using Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls.TreeViewTests;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using Uno.UI;
-using MUXControlsTestApp.Utilities;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MUXControlsTestApp.Utilities;
+using Private.Infrastructure;
+using Uno.UI;
+using Uno.UI.Helpers;
+using Uno.UI.RuntimeTests.Helpers;
+using Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls.TreeViewTests;
 using Windows.System;
+using Windows.UI;
+using TreeView = Microsoft.UI.Xaml.Controls.TreeView;
+using TreeViewItem = Microsoft.UI.Xaml.Controls.TreeViewItem;
+
 
 #if HAS_UNO && !HAS_UNO_WINUI
 using Microsoft.UI.Xaml.Controls;
@@ -253,6 +259,69 @@ public class Given_TreeView
 			MUXTestPage.FindVisualChildrenByType<TextBlock>(SUT.myTree).Count(c => c.Text == "Child 21"));
 #endif
 	}
+
+	[TestMethod]
+	public async Task When_AsdAsd()
+	{
+		// We had an issue where in a treeview of mixed expanded/collapsed nodes,
+		// sometimes selecting a node could cause the viewport to shift and
+		// toggling the IsExpanded of the nodes above. Happens only with selection, and not scrolling...
+
+		// What happened was that TreeViewList::PrepareContainerForItemOverride syncs the IsExpanded state
+		// between the container and the internal node bi-directionally, and the container was associated to a stale item.
+
+		var sources = Enumerable.Range('A', 'Z' - 'A' + 1)
+			.Select(x => new TreeNodeVM(((char)x).ToString(), isExpanded: true)
+			{
+				Children = [..Enumerable.Range('A', 'Z' - 'A' + 1).Select(y =>
+					new TreeNodeVM($"{(char)x}{(char)y}")
+				)]
+			})
+			.ToArray();
+		var flattened = Flatten(sources).ToArray();
+
+		var SUT = new TreeView
+		{
+			Width = 150,
+			Height = 500,
+			SelectionMode = TreeViewSelectionMode.Single,
+			ItemsSource = sources,
+			ItemTemplate = XamlHelper.LoadXaml<DataTemplate>("""
+				<DataTemplate>
+					<TreeViewItem Height="50" ItemsSource="{Binding Children}" IsExpanded="{Binding IsExpanded, Mode=TwoWay}">
+						<TextBlock Text="{Binding Name}" />
+					</TreeViewItem>
+				</DataTemplate>
+			"""),
+		};
+		await UITestHelper.Load(SUT);
+		await TestServices.WindowHelper.WaitFor(() => IsWithinViewport(SUT.ContainerFromItem(sources[0])));
+
+		foreach (var c in "BCDEFGHI")
+		{
+			// select and wait for the selection BringIntoView to occur
+			var node = flattened.First(x => x.Name == $"{c}A");
+			SUT.SelectedItem = node;
+			await TestServices.WindowHelper.WaitForIdle();
+			await TestServices.WindowHelper.WaitFor(() => SUT.ContainerFromItem(node) is { });
+		}
+
+		Assert.IsTrue(sources.All(x => x.IsExpanded), "All top-level nodes should remain expanded.");
+
+		IEnumerable<TreeNodeVM> Flatten(IEnumerable<TreeNodeVM> nodes) =>
+			nodes.SelectMany(x => Flatten(x.Children).Prepend(x));
+		bool IsWithinViewport(DependencyObject x)
+		{
+			if (x is FrameworkElement fe)
+			{
+				var offset = fe.TransformToVisual(SUT).TransformPoint(default);
+
+				return 0 <= offset.Y && (offset.Y + fe.ActualHeight - 1.0) <= SUT.ActualHeight;
+			}
+
+			return false;
+		}
+	}
 }
 
 public class MyNode
@@ -262,6 +331,35 @@ public class MyNode
 	public bool IsDirectory { get; set; }
 
 	public ObservableCollection<MyNode> Children { get; } = new ObservableCollection<MyNode>();
+}
+
+public class TreeNodeVM : INotifyPropertyChanged
+{
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	private string _name;
+	private bool _isExpanded;
+	public string Name { get => _name; set => SetAndRaisePropertyChanged(ref _name, value); }
+	public bool IsExpanded { get => _isExpanded; set => SetAndRaisePropertyChanged(ref _isExpanded, value); }
+
+	public TreeNodeVM(string name, bool isExpanded = false)
+	{
+		_name = name;
+		_isExpanded = isExpanded;
+	}
+
+	public ObservableCollection<TreeNodeVM> Children { get; init; } = new();
+
+	public override string ToString() => $"[{(IsExpanded ? "+" : "-")}]{Name}";
+
+	private void SetAndRaisePropertyChanged<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+	{
+		if (!EqualityComparer<T>.Default.Equals(field, value))
+		{
+			field = value;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
 }
 
 public class FSObjectTemplateSelector : DataTemplateSelector
