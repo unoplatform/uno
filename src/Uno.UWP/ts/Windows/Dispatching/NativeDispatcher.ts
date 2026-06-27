ac0c1ea5364b524575099b45cc37e202a786f07b
Kevin Turcios <turcioskevinr@gmail.com>
2026-06-27 00:17:15 -0500

    perf(wasm): Improve dispatcher wake-up scheduling

--- src/Uno.UWP/ts/Windows/Dispatching/NativeDispatcher.ts
+++ src/Uno.UWP/ts/Windows/Dispatching/NativeDispatcher.ts
@@ -4,6 +4,8 @@
 
 		static _isReady: boolean;
 
+		private static _schedule: (callback: () => void) => void = NativeDispatcher.createScheduler();
+
 		public static init(isReady : Promise<boolean>) {
 
 			isReady.then(() => {
@@ -18,7 +20,7 @@
 		public static WakeUp(force: boolean) {
 
 			if (NativeDispatcher._isReady || force) {
-				(<any>window).setImmediate(() => {
+				NativeDispatcher._schedule(() => {
 					try {
 						NativeDispatcher._dispatcherCallback();
 					}
@@ -28,6 +30,47 @@
 					}
 				});
 			}
+		}
+
+		private static createScheduler(): (callback: () => void) => void {
+			const scheduler = (<any>globalThis).scheduler;
+
+			if (scheduler?.postTask instanceof Function) {
+				return callback => scheduler.postTask(() => {
+					try {
+						callback();
+					}
+					catch (e) {
+						window.setTimeout(() => { throw e; }, 0);
+					}
+				});
+			}
+
+			if (globalThis.MessageChannel instanceof Function) {
+				const channel = new MessageChannel();
+				const callbacks: (() => void)[] = [];
+
+				channel.port1.onmessage = () => {
+					const callback = callbacks.shift();
+
+					if (callback) {
+						callback();
+					}
+				};
+
+				return callback => {
+					callbacks.push(callback);
+					channel.port2.postMessage(undefined);
+				};
+			}
+
+			const setImmediate = (<any>window).setImmediate;
+
+			if (setImmediate instanceof Function) {
+				return callback => setImmediate(callback);
+			}
+
+			return callback => window.setTimeout(callback, 0);
 		}
 	}
 }
