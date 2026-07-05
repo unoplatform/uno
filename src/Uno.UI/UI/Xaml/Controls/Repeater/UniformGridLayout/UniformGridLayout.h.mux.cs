f48b3603837402560812576c3978776fe8ad290b
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2026-07-05 19:20:12 +0200

    refactor(uniformgridlayout): modernize file layout to .mux convention

--- src/Uno.UI/UI/Xaml/Controls/Repeater/UniformGridLayout/UniformGridLayout.h.mux.cs
+++ src/Uno.UI/UI/Xaml/Controls/Repeater/UniformGridLayout/UniformGridLayout.h.mux.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference UniformGridLayout.h, commit b8cfb8490
 
 using Microsoft.UI.Xaml.Controls;
 
--- src/Uno.UI/UI/Xaml/Controls/Repeater/UniformGridLayout/UniformGridLayoutState.h.mux.cs
+++ src/Uno.UI/UI/Xaml/Controls/Repeater/UniformGridLayout/UniformGridLayoutState.h.mux.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
+// MUX Reference UniformGridLayoutState.h, commit b8cfb8490
 
 #nullable enable
 
