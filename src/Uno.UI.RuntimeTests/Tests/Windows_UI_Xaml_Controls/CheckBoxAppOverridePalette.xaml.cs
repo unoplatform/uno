dc2ece3790296fb9402615ffe1ad331bedee74e4
Andres Pineda <pineda.andres@gmail.com>
2026-07-09 14:55:19 -0400

    test(theming): cover CheckBox app-override keyframe

---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/CheckBoxAppOverridePalette.xaml.cs
@@ -0,0 +1,9 @@
+using Microsoft.UI.Xaml;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
+{
+	public sealed partial class CheckBoxAppOverridePalette : ResourceDictionary
+	{
+		public CheckBoxAppOverridePalette() => this.InitializeComponent();
+	}
+}
---
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Controls/CheckBoxAppOverrideThemeResources.xaml.cs
@@ -0,0 +1,9 @@
+using Microsoft.UI.Xaml;
+
+namespace Uno.UI.RuntimeTests.Tests.Windows_UI_Xaml_Controls
+{
+	public sealed partial class CheckBoxAppOverrideThemeResources : ResourceDictionary
+	{
+		public CheckBoxAppOverrideThemeResources() => this.InitializeComponent();
+	}
+}
