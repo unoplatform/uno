fdc6e04911c180dd4092de82b6f425100b558d02
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2026-07-12 09:32:37 -0400

    feat: Add WebAssemblyThreading helper

---
+++ src/Uno.Foundation/WebAssemblyThreading.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace Uno.Foundation;
+
+public static partial class WebAssemblyThreading
+{
+	public static void Initialize()
+	{
+		WindowObject = NativeMethods.GetWindowObject();
+		IsThreadingEnabled = NativeMethods.IsThreadingEnabled();
+	}
+
+	public static bool IsThreadingEnabled { get; private set; }
+
+	public static JSObject WindowObject { get; private set; }
+
+	internal static partial class NativeMethods
+	{
+		[JSImport("globalThis.Uno.UI.Runtime.Skia.WebAssemblyThreading.isThreadingEnabled")]
+		internal static partial bool IsThreadingEnabled();
+
+		[JSImport("globalThis.Uno.UI.Runtime.Skia.WebAssemblyThreading.getWindowObject")]
+		internal static partial JSObject GetWindowObject();
+	}
+}
