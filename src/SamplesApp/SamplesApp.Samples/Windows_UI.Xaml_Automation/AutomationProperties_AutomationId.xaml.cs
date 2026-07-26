de7d5a987313f66257bf8f860695a31258bb1746
morning4coffe-dev <78549750+morning4coffe-dev@users.noreply.github.com>
2026-07-26 12:06:07 +0200

    test(a11y): extend Android UIAutomator action coverage

--- src/SamplesApp/SamplesApp.Samples/Windows_UI.Xaml_Automation/AutomationProperties_AutomationId.xaml.cs
+++ src/SamplesApp/SamplesApp.Samples/Windows_UI.Xaml_Automation/AutomationProperties_AutomationId.xaml.cs
@@ -18,9 +18,29 @@ namespace UITests.Shared.Windows_UI.Xaml_Automation
 				new MyItem { AutomationId = "Item01", Text = "Item 01" },
 				new MyItem { AutomationId = "Item02", Text = "Item 02" },
 				new MyItem { AutomationId = "Item03", Text = "Item 03" },
+				new MyItem { AutomationId = "Item04", Text = "Item 04" },
+				new MyItem { AutomationId = "Item05", Text = "Item 05" },
+				new MyItem { AutomationId = "Item06", Text = "Item 06" },
+				new MyItem { AutomationId = "Item07", Text = "Item 07" },
+				new MyItem { AutomationId = "Item08", Text = "Item 08" },
+				new MyItem { AutomationId = "Item09", Text = "Item 09" },
+				new MyItem { AutomationId = "Item10", Text = "Item 10" },
 			};
 
 			ActionButton.Click += (_, _) => result.Text = "Fixture action invoked.";
+			VolumeSlider.ValueChanged += (_, e) =>
+			{
+				var label = $"Volume {(int)e.NewValue}";
+				VolumeValue.Text = label;
+				AutomationProperties.SetName(VolumeValue, label);
+			};
+			ChoiceBox.SelectionChanged += (_, _) =>
+			{
+				if (ChoiceBox.SelectedItem is ComboBoxItem { Content: string content })
+				{
+					result.Text = content;
+				}
+			};
 			AutomationProperties.GetControlledPeers(ActionButton).Add(ControlledField);
 			AutomationProperties.GetDescribedBy(ControlledField).Add(RelationLabel);
 			AutomationProperties.GetFlowsTo(ActionButton).Add(ControlledField);
