ab685a6f6d703ffffc2b122ba352e0a25a83fce3
morning4coffe-dev <78549750+morning4coffe-dev@users.noreply.github.com>
2026-07-27 13:12:25 +0200

    feat(uwp): Port globalization preferences

---
+++ src/Uno.UI.UnitTests/Windows_System/UserProfile/Given_GlobalizationPreferences.cs
@@ -0,0 +1,44 @@
+#nullable enable
+
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.System.UserProfile;
+
+namespace Uno.UI.Tests.Windows_System.UserProfile;
+
+[TestClass]
+public class Given_GlobalizationPreferences
+{
+	[TestMethod]
+	[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+	public void When_HomeGeographicRegion_Then_ReturnsValidRegion()
+	{
+		var region = GlobalizationPreferences.HomeGeographicRegion;
+
+		Assert.IsTrue(
+			region.Length == 2 && region.All(char.IsAsciiLetterUpper) ||
+			region.Length == 3 && region.All(char.IsAsciiDigit));
+	}
+
+	[TestMethod]
+	[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+	public void When_HomeGeographicRegion_Then_ReturnsNonEmptyRegion()
+	{
+		Assert.AreEqual(CultureInfo.InvariantCulture, CultureInfo.CurrentCulture);
+
+		var region = GlobalizationPreferences.HomeGeographicRegion;
+
+		Assert.IsFalse(string.IsNullOrWhiteSpace(region));
+	}
+
+	[TestMethod]
+	[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+	public void When_Languages_Then_ReturnsValidLanguage()
+	{
+		var languages = GlobalizationPreferences.Languages;
+
+		Assert.IsTrue(languages.Count > 0);
+		Assert.IsFalse(string.IsNullOrWhiteSpace(languages[0]));
+	}
+}
