523ca2ff02463cdd7e89c22f527069964decbc63
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2026-08-07 10:09:14 -0400

    feat: Add WebAssemblyThreading helper

--- src/Uno.UI.Runtime.Skia.WebAssembly.Browser/Hosting/WebAssemblyBrowserHost.cs
+++ src/Uno.UI.Runtime.Skia.WebAssembly.Browser/Hosting/WebAssemblyBrowserHost.cs
@@ -7,6 +7,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.Web.WebView2.Core;
 using Uno.Extensions.ApplicationModel.Core;
+using Uno.Foundation;
 using Uno.Foundation.Extensibility;
 using Uno.Foundation.Logging;
 using Uno.Graphics;
@@ -66,6 +67,9 @@ internal partial class WebAssemblyBrowserHost : SkiaHost, ISkiaApplicationHost, 
 		if (!_isInitialized)
 		{
 			_isInitialized = true;
+
+			WebAssemblyThreading.Initialize();
+
 			NativeMethods.PersistBootstrapperLoader();
 
 			ApiExtensibility.Register(typeof(Uno.ApplicationModel.Core.ICoreApplicationExtension), o => _coreApplicationExtension!);
