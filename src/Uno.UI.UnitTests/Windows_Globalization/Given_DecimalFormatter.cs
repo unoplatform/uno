d9386f896525f83878802cc4c317ed97a7d80eef
Dominik Titl <t-dotitl@microsoft.com>
2026-08-25 03:46:02 +0200

    test(numberformatting): Add WinRT conformance coverage

--- src/Uno.UI.UnitTests/Windows_Globalization/Given_DecimalFormatter.cs
+++ src/Uno.UI.UnitTests/Windows_Globalization/Given_DecimalFormatter.cs
@@ -571,5 +571,239 @@ namespace Uno.UI.Tests.Windows_Globalization
 
 			Assert.AreEqual(1.5, sut.ParseDouble(formatted));
 		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		// ISO 3166-1 alpha-3 codes, including the user-assigned AAA-AAZ/QMA-QZZ/XAA-XZZ/ZZA-ZZZ blocks
+		// and the Windows-specific "OOO" operator pseudo-region, are all accepted by native WinRT.
+		[DataRow("USA")]
+		[DataRow("FRA")]
+		[DataRow("IND")]
+		[DataRow("ANT")]
+		[DataRow("OOO")]
+		[DataRow("AAA")]
+		[DataRow("QQQ")]
+		[DataRow("XYZ")]
+		[DataRow("ZZZ")]
+		public void When_GeographicRegionIsAlpha3_Then_Accept(string geographicRegion)
+		{
+			var sut = new DecimalFormatter(new[] { "en-US" }, geographicRegion);
+
+			Assert.AreEqual(geographicRegion, sut.GeographicRegion);
+			Assert.AreEqual("ZZ", sut.ResolvedGeographicRegion);
+			Assert.AreEqual("1234.50", sut.FormatDouble(1234.5));
+		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		// Native WinRT only accepts ISO 3166-1 assigned alpha-2/alpha-3 codes plus the user-assigned
+		// ranges - an arbitrary uppercase letter pair is rejected.
+		[DataRow("AB")]
+		[DataRow("EU")]
+		[DataRow("UK")]
+		[DataRow("DA")]
+		[DataRow("ZB")]
+		[DataRow("USa")]
+		[DataRow("usa")]
+		[DataRow("AB1")]
+		[DataRow("A1B")]
+		[DataRow("ABCD")]
+		public void When_GeographicRegionIsUnassigned_Then_Throw(string geographicRegion)
+		{
+			Assert.ThrowsExactly<ArgumentException>(() => new DecimalFormatter(new[] { "en-US" }, geographicRegion));
+		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		// Native WinRT's decimal parser has no exponent syntax, so scientific notation is not a number.
+		[DataRow("1e2")]
+		[DataRow("1E2")]
+		[DataRow("1.5e-1")]
+		[DataRow("0e-1")]
+		[DataRow("0E-1")]
+		[DataRow("-0e1")]
+		[DataRow("0.0e-5")]
+		public void When_ParseDoubleWithExponent_Then_Null(string text)
+		{
+			var sut = MakeFormatter();
+
+			Assert.IsNull(sut.ParseDouble(text));
+		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		public void When_ParseDoubleSpecialValues_Then_RoundTripFormatting()
+		{
+			var sut = MakeFormatter();
+
+			Assert.IsTrue(double.IsNaN(sut.ParseDouble(sut.FormatDouble(double.NaN))!.Value));
+			Assert.AreEqual(double.PositiveInfinity, sut.ParseDouble(sut.FormatDouble(double.PositiveInfinity)));
+			Assert.AreEqual(double.NegativeInfinity, sut.ParseDouble(sut.FormatDouble(double.NegativeInfinity)));
+		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		// WinRT parses back only the exact symbols it formats: the comparison is case-sensitive and
+		// no sign variation is accepted.
+		[DataRow("nan")]
+		[DataRow("NAN")]
+		[DataRow("+\u221e")]
+		[DataRow("\u2212\u221e")]
+		[DataRow("Infinity")]
+		[DataRow("-Infinity")]
+		[DataRow("+Infinity")]
+		[DataRow("infinity")]
+		[DataRow("inf")]
+		[DataRow(" NaN")]
+		[DataRow("NaN ")]
+		public void When_ParseDoubleSpecialValueVariant_Then_Null(string text)
+		{
+			var sut = MakeFormatter();
+
+			Assert.IsNull(sut.ParseDouble(text));
+		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		// ICU (and therefore .NET) reports U+2212 MINUS SIGN for these locales while the Windows NLS
+		// data that WinRT reads uses the ASCII hyphen-minus for every locale.
+		[DataRow("sv-SE", "SE")]
+		[DataRow("fi-FI", "FI")]
+		[DataRow("lt-LT", "LT")]
+		[DataRow("et-EE", "EE")]
+		[DataRow("nb-NO", "NO")]
+		public void When_LocaleUsesNonAsciiMinusSign_Then_FormatsAsciiHyphen(string language, string geographicRegion)
+		{
+			var sut = new DecimalFormatter(new[] { language }, geographicRegion)
+			{
+				IsGrouped = true,
+				IntegerDigits = 1,
+				FractionDigits = 2,
+			};
+
+			var formatted = sut.FormatDouble(-1234.5);
+
+			Assert.AreEqual('-', formatted[0], $"Expected an ASCII hyphen-minus in '{formatted}'.");
+			Assert.AreEqual(-1234.5, sut.ParseDouble(formatted));
+		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		public void When_GroupedWithSingleIntegerDigit_Then_ThousandsAreSeparated()
+		{
+			var sut = new DecimalFormatter(new[] { "en-US" }, "US")
+			{
+				IsGrouped = true,
+				IntegerDigits = 1,
+				FractionDigits = 2,
+			};
+
+			Assert.AreEqual("1,234.50", sut.FormatDouble(1234.5));
+			Assert.AreEqual("1,234,567.00", sut.FormatDouble(1234567));
+			Assert.AreEqual("-1,234.50", sut.FormatDouble(-1234.5));
+			Assert.AreEqual(1234.5, sut.ParseDouble("1,234.50"));
+		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		public void When_GroupedWithoutIntegerDigits_Then_ThousandsAreSeparated()
+		{
+			var sut = new DecimalFormatter(new[] { "en-US" }, "US")
+			{
+				IsGrouped = true,
+				IntegerDigits = 0,
+				FractionDigits = 2,
+			};
+
+			Assert.AreEqual("1,234.50", sut.FormatDouble(1234.5));
+			Assert.AreEqual(".50", sut.FormatDouble(0.5));
+			Assert.AreEqual(".00", sut.FormatDouble(0d));
+		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		[DataRow(2147483648d, "2,147,483,648.00")]
+		[DataRow(-2147483649d, "-2,147,483,649.00")]
+		[DataRow(4294967296d, "4,294,967,296.00")]
+		[DataRow(1e15, "1,000,000,000,000,000.00")]
+		[DataRow(9007199254740992d, "9,007,199,254,740,992.00")]
+		public void When_ValueExceedsInt32Range_Then_IntegerPartIsPreserved(double value, string expected)
+		{
+			var sut = new DecimalFormatter(new[] { "en-US" }, "US")
+			{
+				IsGrouped = true,
+				IntegerDigits = 1,
+				FractionDigits = 2,
+			};
+
+			var formatted = sut.FormatDouble(value);
+
+			Assert.AreEqual(expected, formatted);
+			Assert.AreEqual(value, sut.ParseDouble(formatted));
+		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		public void When_ValueIsNegativeFraction_Then_SignIsPreserved()
+		{
+			var sut = new DecimalFormatter(new[] { "en-US" }, "US")
+			{
+				IntegerDigits = 1,
+				FractionDigits = 2,
+			};
+
+			Assert.AreEqual("-0.125", sut.FormatDouble(-0.125));
+			Assert.AreEqual("-0.50", sut.FormatDouble(-0.5));
+			Assert.AreEqual(-0.125, sut.ParseDouble("-0.125"));
+
+			sut.IntegerDigits = 0;
+
+			Assert.AreEqual("-.50", sut.FormatDouble(-0.5));
+			Assert.AreEqual(".50", sut.FormatDouble(0.5));
+		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		[DataRow("en-US", "US", "00,000,000")]
+		[DataRow("en-IN", "IN", "0,00,00,000")]
+		public void When_GroupedZeroIsPadded_Then_LocaleGroupSizesApply(string language, string geographicRegion, string expected)
+		{
+			var sut = new DecimalFormatter(new[] { language }, geographicRegion)
+			{
+				IsGrouped = true,
+				IntegerDigits = 8,
+				FractionDigits = 0,
+			};
+
+			Assert.AreEqual(expected, sut.FormatDouble(0d));
+			Assert.AreEqual(0d, sut.ParseDouble(expected));
+		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		public void When_GroupedSignedZeroIsPadded_Then_SignPrefixesTheGroups()
+		{
+			var sut = new DecimalFormatter(new[] { "en-US" }, "US")
+			{
+				IsZeroSigned = true,
+				IsGrouped = true,
+				IntegerDigits = 8,
+				FractionDigits = 0,
+			};
+
+			Assert.AreEqual("-00,000,000", sut.FormatDouble(-0d));
+			Assert.AreEqual("00,000,000", sut.FormatDouble(0d));
+		}
+
+		[TestMethod]
+		[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+		public void When_ParseDoubleOverflowsDoubleRange_Then_Null()
+		{
+			var sut = MakeFormatter();
+
+			Assert.IsNull(sut.ParseDouble(new string('9', 400)));
+			Assert.IsNull(sut.ParseDouble("1" + new string('0', 400)));
+			Assert.AreEqual(0d, sut.ParseDouble("0." + new string('0', 400) + "1"));
+		}
 	}
 }
--- src/Uno.UI.UnitTests/Windows_System/UserProfile/Given_GlobalizationPreferences.cs
+++ src/Uno.UI.UnitTests/Windows_System/UserProfile/Given_GlobalizationPreferences.cs
@@ -2,6 +2,7 @@
 
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.Globalization.NumberFormatting;
 using Windows.System.UserProfile;
 
 namespace Uno.UI.Tests.Windows_System.UserProfile;
@@ -37,5 +38,27 @@ public class Given_GlobalizationPreferences
 
 		Assert.IsTrue(languages.Count > 0);
 		Assert.IsFalse(string.IsNullOrWhiteSpace(languages[0]));
+	}
+
+	[TestMethod]
+	[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+	public void When_Languages_Then_ContainsNoEmptyEntry()
+	{
+		var languages = GlobalizationPreferences.Languages;
+
+		Assert.IsFalse(
+			languages.Any(string.IsNullOrWhiteSpace),
+			$"Expected no empty language tag, got [{string.Join(", ", languages)}].");
+	}
+
+	[TestMethod]
+	[GitHubWorkItem("https://github.com/unoplatform/uno/issues/6908")]
+	public void When_Languages_Then_EveryEntryIsAcceptedByDecimalFormatter()
+	{
+		var languages = GlobalizationPreferences.Languages;
+
+		var sut = new DecimalFormatter(new[] { languages[0].Split('_')[0] }, GlobalizationPreferences.HomeGeographicRegion);
+
+		Assert.AreEqual(GlobalizationPreferences.HomeGeographicRegion, sut.GeographicRegion);
 	}
 }
