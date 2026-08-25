3694ad74358c7f5eafcb41647f551ec808c05f33
morning4coffe-dev <78549750+morning4coffe-dev@users.noreply.github.com>
2026-08-25 04:14:21 +0200

    test(a11y): fix nullable annotation and text usings

--- src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Automation/Given_AccessibleTextBox.cs
+++ src/Uno.UI.RuntimeTests/Tests/Windows_UI_Xaml_Automation/Given_AccessibleTextBox.cs
@@ -5,6 +5,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Automation;
 using Microsoft.UI.Xaml.Automation.Peers;
 using Microsoft.UI.Xaml.Automation.Provider;
+using Microsoft.UI.Xaml.Automation.Text;
 using Microsoft.UI.Xaml.Controls;
 using Private.Infrastructure;
 using Uno.UI;
